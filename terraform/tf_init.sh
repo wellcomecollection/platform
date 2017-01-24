abff075e459b5b992c9737ee66a97fe0089b7264
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-01-24 16:23:35 +0000

    push init step into script

---
+++ terraform/tf_init.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+INFRA_BUCKET=$1
+
+set +e
+
+pip install awscli
+aws s3 cp s3://$INFRA_BUCKET/terraform_0.8.4_linux_amd64.zip .
+unzip terraform_0.8.4_linux_amd64.zip
+
+./terraform remote config \
+  -backend=S3 \
+  -backend-config="bucket=$INFRA_BUCKET" \
+  -backend-config="key=terraform.tfstate" \
+  -backend-config="region=eu-west-1"
+
+./terraform get
+
+set -e
