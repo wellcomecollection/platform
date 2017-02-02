b5df019f5e0052a2e8a09ee26c9dc9dd1e4b6946
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-02-02 17:36:50 +0000

    push something out on record endpoint

--- app/src/main/scala/uk/ac/wellcome/platformapi/services/CalmService.scala
+++ app/src/main/scala/uk/ac/wellcome/platformapi/services/CalmService.scala
@@ -2,6 +2,8 @@ package uk.ac.wellcome.platform.api.services
 
 import javax.inject.{Inject, Singleton}
 import com.sksamuel.elastic4s.ElasticDsl._
+
+import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 import uk.ac.wellcome.platform.api.models._
@@ -11,19 +13,12 @@ import uk.ac.wellcome.platform.api.models._
 class CalmService @Inject()(
   elasticsearchService: ElasticsearchService
 ) {
-  def findByAltRefNo(altRefNo: String) = elasticsearchService.client.execute {
-    search in "records" -> "item" query {
-      bool {
-        must(
-          matchQuery("AltRefNo.keyword", altRefNo)
-        )
-      }
-    }
-  }.map { result =>
-    if (result.isEmpty) {
-      Nil
-    } else {
-      result.hits.map { Record(_) }
-    }
-  }
+
+  def findRecordsByAltRefNo(altRefNo: String): Future[Array[Record]] =
+    elasticsearchService.client.execute {
+      search("records/item").query(
+        boolQuery().must(matchQuery("AltRefNo.keyword", altRefNo))
+      )
+    }.map { _.hits.map { Record(_) }}
+
 }
