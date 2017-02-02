75d2722b6d63a04205a576bde9506765c93930cc
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-02-02 17:36:50 +0000

    push something out on record endpoint

--- app/src/main/scala/uk/ac/wellcome/platformapi/controllers/MainController.scala
+++ app/src/main/scala/uk/ac/wellcome/platformapi/controllers/MainController.scala
@@ -10,15 +10,38 @@ import scala.concurrent.Future
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse
 import scala.concurrent.ExecutionContext.Implicits.global
 
+import com.twitter.finatra.request.QueryParam
+import com.twitter.finatra.validation.NotEmpty
+
+
+import uk.ac.wellcome.platform.api.models._
+
+case class RecordRequest(
+  @NotEmpty @QueryParam altRefNo: String
+)
+
+case class Response(
+  refno: String,
+  entries: Record,
+  parent: Option[Collection]
+)
+
 @Singleton
 class MainController @Inject()(
-  elasticsearchService: ElasticsearchService
+  calmService: CalmService
 ) extends Controller {
 
   val apiBaseUrl = "/api/v0"
 
-  get("/") { request: Request =>
+  get(s"${apiBaseUrl}/record") { request: RecordRequest =>
+    calmService.findRecordsByAltRefNo(request.altRefNo).map { records =>
+      response.ok.json(records)
+    }
+  }
+
+  get(s"${apiBaseUrl}/collection") { request: Request =>
     response.ok.json(Map("message" -> "ok"))
   }
+
 
 }
--- app/src/main/scala/uk/ac/wellcome/platformapi/models/Record.scala
+++ app/src/main/scala/uk/ac/wellcome/platformapi/models/Record.scala
@@ -6,19 +6,21 @@ import com.sksamuel.elastic4s.searches.RichSearchHit
 case class Record(
   refno: String,
   title: String,
-  material_type: String,
-  date: String,
-  acquisition: String
+  material_type: String
+//  date: String,
+//  acquisition: String
 )
 case object Record {
   def apply(esResult: RichSearchHit): Record = {
     val data = esResult.sourceAsMap
+
     Record(
       refno=data("AltRefNo").toString,
       title=data("Title").toString,
-      material_type=data("Material").toString,
-      date=data.getOrElse("Date", "Unknown").toString,
-      acquisition=data("Acquisition").toString
+      material_type=data("Material").toString
+
+      //date=data.getOrElse("Date", "Unknown").toString,
+      //acquisition=data.getOrElse("Acquisition", "Unknown").toString
     )
   }
 }
