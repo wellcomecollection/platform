d611e6614d6de32cac61af9a64b15e9ee1537972
Alex Chan <a.chan@wellcome.ac.uk>
2017-02-07 10:58:58 +0000

    Add a Terraform plan for the Calm DynamoDB table

---
+++ calm_table.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_dynamodb_table" "calm-dynamodb-table" {
+  name = "CalmData"
+  read_capacity = 5
+  write_capacity = 5
+  hash_key = "RecordID"
+  range_key = "RecordType"
+  attribute {
+    name = "RecordID"
+    type = "S"
+  }
+  attribute {
+    name = "RecordType"
+    type = "S"
+  }
+  attribute {
+    name = "RefNo"
+    type = "S"
+  }
+  attribute {
+    name = "AltRefNo"
+    type = "S"
+  }
+  attribute {
+    name = "data"
+    type = "S"
+  }
+  global_secondary_index = {
+    name = "RefNo"
+    hash_key = "RefNo"
+    read_capacity = 5
+    write_capacity = 5
+    projection_type = "ALL"
+  }
+  global_secondary_index = {
+    name = "AltRefNo"
+    hash_key = "AltRefNo"
+    read_capacity = 5
+    write_capacity = 5
+    projection_type = "ALL"
+  }
+  local_secondary_index {
+    name = "Data-index"
+    range_key = "data"
+    projection_type = "ALL"
+  }
+}
