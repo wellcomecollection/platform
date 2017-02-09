f3e8242f16f78d550cb80cebef2cf0d711b34956
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-02-09 13:20:29 +0000

    remove unused vars in plan script

--- terraform/tf_plan.sh
+++ terraform/tf_plan.sh
@@ -2,11 +2,8 @@
 
 KEY_NAME=$1
 ADMIN_CIDR=$2
-INFRA_BUCKET=$3
-BUILD_SHA1=$5
 
 ./terraform plan \
   --var "key_name=$KEY_NAME" \
   --var "admin_cidr_ingress=$ADMIN_CIDR" \
-  --var "platform_api_container_url=$CONTAINER_URL" \
   -out=terraform.plan
