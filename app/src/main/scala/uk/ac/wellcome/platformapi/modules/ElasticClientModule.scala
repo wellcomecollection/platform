f085baf6dfe0d920fcb69f7b10cf7ebecffb5d4c
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-02-15 13:10:20 +0000

    log settings, match example config exactly

--- app/src/main/scala/uk/ac/wellcome/platformapi/modules/ElasticClientModule.scala
+++ app/src/main/scala/uk/ac/wellcome/platformapi/modules/ElasticClientModule.scala
@@ -9,6 +9,7 @@ import com.sksamuel.elastic4s.ElasticsearchClientUri
 import com.twitter.inject.TwitterModule
 import com.twitter.inject.Logging
 import org.elasticsearch.common.settings.Settings
+import scala.collection.JavaConverters._
 
 
 case class XPackConfig(user: String, ssl: Boolean)
@@ -32,6 +33,7 @@ object ElasticClientModule extends TwitterModule {
   private val host = flag[String]("es.host", "localhost", "host name of ES")
   private val port = flag[Int]("es.port", 9300, "port no of ES")
   private val sniff = flag[Boolean]("es.sniff", false, "sniff ES nodes")
+  private val compress = flag[Boolean]("es.transport.compress", false, "compress transport")
   private val clusterName = flag[String]("es.name", "elasticsearch", "cluster name")
 
   val timeout = flag("es.timeout", 30, "default timeout duration of execution")
@@ -45,12 +47,17 @@ object ElasticClientModule extends TwitterModule {
 
     val defaultSettings = Settings.builder()
       .put("client.transport.sniff", sniff())
+      .put("transport.tcp.compress", compress())
       .put("cluster.name", clusterName())
 
     val settings = xpackConfig.foldLeft(defaultSettings)((defaults, config) => defaults
       .put("xpack.security.transport.ssl.enabled", config.ssl)
-      .put("request.headers.X-Found-Cluster", clusterName())
+      .put("request.headers.X-Found-Cluster", "${cluster.name}")
       .put("xpack.security.user", config.user)).build()
+
+    settings.getAsMap.asScala.map {
+      case (k,v) => info(s"ElasticClient setting: ${k}=${v}")
+    }
 
     XPackElasticClient(settings, clientUri)
   }
