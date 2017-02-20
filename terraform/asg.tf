54fd40418b38088c94fadfa94dd84e72cd2db287
Robert Kenny <kenoir@users.noreply.github.com>
2017-02-20 14:26:31 +0000

    ASGs in Terraform (#8)
    * asgs in terraform
    
    * address review

---
+++ terraform/asg.tf
@@ -0,0 +1,37 @@
+/* Autoscaling groups described as seperate Cloudformation stacks
+   in order that rolling updates are possible. Note: there is an
+   upper limit on the number of Cloudformation stacks. */
+
+resource "aws_cloudformation_stack" "tools_cluster_asg" {
+  name = "tools-cluster-asg"
+  template_body = "${data.template_file.tools_cluster_asg.rendered}"
+}
+
+data "template_file" "tools_cluster_asg" {
+  template = "${file("${path.module}/cloudformation/asg.json.template")}"
+
+  vars {
+    launch_config_name = "${aws_launch_configuration.tools.name}"
+    vpc_zone_identifier = "${jsonencode(aws_subnet.tools.*.id)}"
+    asg_min_size = "${var.asg_min}"
+    asg_desired_size = "${var.asg_desired}"
+    asg_max_size = "${var.asg_max}"
+  }
+}
+
+resource "aws_cloudformation_stack" "platform_cluster_asg" {
+  name = "platform-cluster-asg"
+  template_body = "${data.template_file.platform_cluster_asg.rendered}"
+}
+
+data "template_file" "platform_cluster_asg" {
+  template = "${file("${path.module}/cloudformation/asg.json.template")}"
+
+  vars {
+    launch_config_name = "${aws_launch_configuration.platform.name}"
+    vpc_zone_identifier = "${jsonencode(aws_subnet.main.*.id)}"
+    asg_min_size = "${var.asg_min}"
+    asg_desired_size = "${var.asg_desired}"
+    asg_max_size = "${var.asg_max}"
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,10 +6,6 @@ output "launch_configuration" {
   value = "${aws_launch_configuration.platform.id}"
 }
 
-output "asg_name" {
-  value = "${aws_autoscaling_group.platform_asg.id}"
-}
-
 output "elb_hostname" {
   value = "${aws_alb.main.dns_name}"
 }
