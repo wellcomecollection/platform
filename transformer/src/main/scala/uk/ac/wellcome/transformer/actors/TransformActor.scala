d752b293df365ca52632c95e36ca96299877b8ab
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-03-02 13:51:22 +0000

    adds transform actor

---
+++ transformer/src/main/scala/uk/ac/wellcome/transformer/actors/TransformActor.scala
@@ -0,0 +1,27 @@
+package uk.ac.wellcome.platform.transformer.actors
+
+import akka.actor.Actor
+import com.twitter.inject.Logging
+
+import uk.ac.wellcome.platform.transformer.models._
+import scala.util.Success
+import scala.util.Failure
+
+
+class TransformActor extends Actor with Logging {
+  def receive = {
+    case dirtyRecord: Transformable => {
+      dirtyRecord.transform match {
+        case Success(cleanRecord) => {
+          info(s"Cleaned record ${cleanRecord}")
+          // Send to next actor
+        }
+        case Failure(e) => {
+          // Send to dead letter queue or just error
+          error("Failed to perform transform to clean record",e)
+        }
+      }
+    }
+    case record => error(s"Received non-transformable record ${record}")
+  }
+}
