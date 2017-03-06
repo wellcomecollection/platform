f72356b45f172739e26970591a69fe4f7d6d289f
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-03-06 09:51:23 +0000

    Create new namespaced repo for transform app

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -30,7 +30,7 @@ data "template_file" "platform_task_definition" {
 
   vars {
     container_name   = "platform_api"
-    image_uri        = "${aws_ecr_repository.platform.repository_url}:${var.release_id}"
+    image_uri        = "${aws_ecr_repository.api.repository_url}:${var.release_id}"
     log_group_region = "${var.aws_region}"
     log_group_name   = "${aws_cloudwatch_log_group.app.name}"
   }
