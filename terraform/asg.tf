bd3a42aac340148fba808a76a9fe1ce193c0bd7a
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-03-08 18:42:41 +0000

    push asgs to modules

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -1,36 +1,19 @@
-/* Autoscaling groups described as separate Cloudformation stacks
-   in order that rolling updates are possible. */
-
-resource "aws_cloudformation_stack" "tools_cluster_asg" {
-  name          = "tools-cluster-asg"
-  template_body = "${data.template_file.tools_cluster_asg.rendered}"
+module "platform_cluster_asg" {
+  source             = "./ecs_asg"
+  asg_name           = "platform-ecs-cluster"
+  asg_min            = "1"
+  asg_desired        = "1"
+  asg_max            = "2"
+  launch_config_name = "${aws_launch_configuration.platform.name}"
+  subnet_list        = ["${aws_subnet.main.*.id}"]
 }
 
-data "template_file" "tools_cluster_asg" {
-  template = "${file("${path.module}/cloudformation/asg.json.template")}"
-
-  vars {
-    launch_config_name  = "${aws_launch_configuration.tools.name}"
-    vpc_zone_identifier = "${jsonencode(aws_subnet.tools.*.id)}"
-    asg_min_size        = "${var.asg_min}"
-    asg_desired_size    = "${var.asg_desired}"
-    asg_max_size        = "${var.asg_max}"
-  }
-}
-
-resource "aws_cloudformation_stack" "platform_cluster_asg" {
-  name          = "platform-cluster-asg"
-  template_body = "${data.template_file.platform_cluster_asg.rendered}"
-}
-
-data "template_file" "platform_cluster_asg" {
-  template = "${file("${path.module}/cloudformation/asg.json.template")}"
-
-  vars {
-    launch_config_name  = "${aws_launch_configuration.platform.name}"
-    vpc_zone_identifier = "${jsonencode(aws_subnet.main.*.id)}"
-    asg_min_size        = "${var.asg_min}"
-    asg_desired_size    = "${var.asg_desired}"
-    asg_max_size        = "${var.asg_max}"
-  }
+module "tools_cluster_asg" {
+  source             = "./ecs_asg"
+  asg_name           = "tools-ecs-cluster"
+  asg_min            = "1"
+  asg_desired        = "1"
+  asg_max            = "2"
+  launch_config_name = "${aws_launch_configuration.tools.name}"
+  subnet_list        = ["${aws_subnet.tools.*.id}"]
 }
