9f7e79cdd3087f9cca16f2e4f5b6afee99d1ce5b
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-03-09 14:46:53 +0000

    modularise userdata

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,24 +1,10 @@
-data "template_file" "platform_userdata" {
-  template = "${file("${path.module}/userdata/ecs-agent.yml")}"
-
-  vars {
-    aws_region         = "${var.aws_region}"
-    ecs_cluster_name   = "${aws_ecs_cluster.main.name}"
-    ecs_log_level      = "info"
-    ecs_agent_version  = "latest"
-    ecs_log_group_name = "${aws_cloudwatch_log_group.ecs.name}"
-  }
+module "platform_userdata" {
+  source       = "./userdata"
+  cluster_name = "${aws_ecs_cluster.main.name}"
 }
 
-data "template_file" "tools_userdata" {
-  template = "${file("${path.module}/userdata/ecs-agent-tools.yml")}"
-
-  vars {
-    aws_region         = "${var.aws_region}"
-    ecs_cluster_name   = "${aws_ecs_cluster.tools.name}"
-    ecs_log_level      = "info"
-    ecs_agent_version  = "latest"
-    ecs_log_group_name = "${aws_cloudwatch_log_group.ecs_tools.name}"
-    efs_fs_name        = "${aws_efs_file_system.jenkins.id}"
-  }
+module "tools_userdata" {
+  source        = "./userdata"
+  template_name = "ecs-agent-tools"
+  cluster_name  = "${aws_ecs_cluster.main.name}"
 }
--- terraform/ecs_asg/outputs.tf
+++ terraform/ecs_asg/outputs.tf
@@ -1,7 +1,7 @@
 output "instance_sg_id" {
-  value = "aws_security_group.instance_sg.id"
+  value = "${aws_security_group.instance_sg.id}"
 }
 
 output "loadbalancer_sg_id" {
-  value = "aws_security_group.loadbalancer_sg.id"
+  value = "${aws_security_group.loadbalancer_sg.id}"
 }
