c956c0e25b342901e6b32bb9f53292f3a7e2ef97
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-03-09 13:38:02 +0000

    move security groups to module

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -1,19 +1,21 @@
 module "platform_cluster_asg" {
-  source             = "./ecs_asg"
-  asg_name           = "platform-ecs-cluster"
-  asg_min            = "1"
-  asg_desired        = "1"
-  asg_max            = "2"
-  launch_config_name = "${aws_launch_configuration.platform.name}"
-  subnet_list        = ["${module.vpc_main.subnets}"]
+  source                = "./ecs_asg"
+  asg_name              = "platform-ecs-cluster"
+  subnet_list           = ["${module.vpc_main.subnets}"]
+  key_name              = "${var.key_name}"
+  instance_profile_name = "${module.ecs_main_iam.instance_profile_name}"
+  user_data             = "${data.template_file.platform_userdata.rendered}"
+  vpc_id                = "${module.vpc_main.vpc_id}"
 }
 
 module "tools_cluster_asg" {
-  source             = "./ecs_asg"
-  asg_name           = "tools-ecs-cluster"
-  asg_min            = "1"
-  asg_desired        = "1"
-  asg_max            = "2"
-  launch_config_name = "${aws_launch_configuration.tools.name}"
-  subnet_list        = ["${module.vpc_tools.subnets}"]
+  source                = "./ecs_asg"
+  asg_name              = "tools-ecs-cluster"
+  subnet_list           = ["${module.vpc_tools.subnets}"]
+  key_name              = "${var.key_name}"
+  instance_type         = "t2.medium"
+  instance_profile_name = "${module.ecs_tools_iam.instance_profile_name}"
+  user_data             = "${data.template_file.tools_userdata.rendered}"
+  vpc_id                = "${module.vpc_tools.vpc_id}"
+  admin_cidr_ingress    = "${var.admin_cidr_ingress}"
 }
--- terraform/ecs_asg/asg.tf
+++ terraform/ecs_asg/asg.tf
@@ -7,10 +7,27 @@ data "template_file" "cluster_asg" {
   template = "${file("${path.module}/asg.json.template")}"
 
   vars {
-    launch_config_name  = "${var.launch_config_name}"
+    launch_config_name  = "${aws_launch_configuration.launch_config.name}"
     vpc_zone_identifier = "${jsonencode(var.subnet_list)}"
     asg_min_size        = "${var.asg_min}"
     asg_desired_size    = "${var.asg_desired}"
     asg_max_size        = "${var.asg_max}"
+  }
+}
+
+resource "aws_launch_configuration" "launch_config" {
+  security_groups = [
+    "${aws_security_group.instance_sg.id}",
+  ]
+
+  key_name                    = "${var.key_name}"
+  image_id                    = "${data.aws_ami.stable_coreos.id}"
+  instance_type               = "${var.instance_type}"
+  iam_instance_profile        = "${var.instance_profile_name}"
+  user_data                   = "${var.user_data}"
+  associate_public_ip_address = "${var.public_ip}"
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
