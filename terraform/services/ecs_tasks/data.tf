fd9e339d674624e1b41bb49ea8982e0c679b78ee
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-03-10 16:30:19 +0000

    ensure template uses task name to refer to primary container

--- terraform/services/ecs_tasks/data.tf
+++ terraform/services/ecs_tasks/data.tf
@@ -5,5 +5,6 @@ data "template_file" "definition" {
     image_uri        = "${var.image_uri}"
     log_group_region = "${var.aws_region}"
     log_group_name   = "${aws_cloudwatch_log_group.task.name}"
+    task_name        = "${var.task_name}"
   }
 }
