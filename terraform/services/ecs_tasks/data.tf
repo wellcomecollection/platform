8842f7f2e08b7bf3e55f76251079c48cb8800268
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-03-10 16:25:30 +0000

    make api task default template

--- terraform/services/ecs_tasks/data.tf
+++ terraform/services/ecs_tasks/data.tf
@@ -1,5 +1,5 @@
 data "template_file" "definition" {
-  template = "${file("${path.module}/templates/${var.task_name}.json.template")}"
+  template = "${file("${path.module}/templates/${var.template_name}.json.template")}"
 
   vars {
     image_uri        = "${var.image_uri}"
