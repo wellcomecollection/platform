7aedeca4d067cfcc211ba0685546a51e49de9d13
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-03-10 16:02:17 +0000

    nest service dependent modules

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -5,18 +5,3 @@ resource "aws_ecs_cluster" "main" {
 resource "aws_ecs_cluster" "tools" {
   name = "tools_cluster"
 }
-
-module "api_task" {
-  source        = "./ecs_tasks"
-  task_name     = "api"
-  image_uri     = "${aws_ecr_repository.api.repository_url}:${var.release_id}"
-  task_role_arn = "${module.ecs_main_iam.task_role_arn}"
-}
-
-module "jenkins_task" {
-  source           = "./ecs_tasks"
-  task_name        = "jenkins"
-  task_role_arn    = "${module.ecs_tools_iam.task_role_arn}"
-  volume_name      = "jenkins-home"
-  volume_host_path = "/mnt/efs"
-}
