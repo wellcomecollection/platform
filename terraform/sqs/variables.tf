16816f5c7d2b98ed3ce50325003d51a8cd6a70ea
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-03-13 15:32:27 +0000

    hand list of topic names to sqs module for multiple subscriptions

--- terraform/sqs/variables.tf
+++ terraform/sqs/variables.tf
@@ -2,12 +2,13 @@ variable "queue_name" {
   description = "Name of the SQS queue to create"
 }
 
-variable "topic_name" {
-  description = "Name of the SNS topic to subscribe the queue to"
+variable "topic_count" {
+  default = "1"
 }
 
-variable "topic_arn" {
-  description = "ARN of the SNS topic to subscribe the queue to"
+variable "topic_names" {
+  type        = "list"
+  description = "Topic name for the SNS topic to subscribe the queue to"
 }
 
 variable "aws_region" {
