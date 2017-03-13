46a6bc76d204f6bc78e1b85e396f1515f3834d64
Alex Chan <a.chan@wellcome.ac.uk>
2017-03-13 15:27:44 +0000

    Remove some code left over from the skeleton

--- calm_adapter/src/main/scala/uk/ac/wellcome/calm_adapter/modules/CalmAdapterWorker.scala
+++ calm_adapter/src/main/scala/uk/ac/wellcome/calm_adapter/modules/CalmAdapterWorker.scala
@@ -1,72 +1,18 @@
 package uk.ac.wellcome.platform.calm_adapter.modules
 
-import scala.concurrent.duration.Duration
-
-import java.util.concurrent.TimeUnit
-
-import akka.actor.{Actor, ActorSystem, Props}
-
 import java.nio.charset.{ Charset => JCharset }
 import java.util.{ List => JList }
-
+import java.util.concurrent.TimeUnit
 import scala.collection.JavaConverters._
-
-import com.amazonaws.auth.{AWSCredentials, AWSCredentialsProvider, DefaultAWSCredentialsProviderChain}
-import com.amazonaws.regions._
-import com.amazonaws.services.cloudwatch._
-import com.amazonaws.services.dynamodbv2._
-import com.amazonaws.services.dynamodbv2.streamsadapter.AmazonDynamoDBStreamsAdapterClient
-import com.amazonaws.services.dynamodbv2.streamsadapter.model.RecordAdapter
-import com.amazonaws.services.kinesis.clientlibrary.interfaces._
-import com.amazonaws.services.kinesis.clientlibrary.lib.worker._
-import com.amazonaws.services.kinesis.model.Record
-
+import scala.concurrent.duration.Duration
 import scala.concurrent.ExecutionContext.Implicits.global
 
-import com.gu.scanamo.ScanamoFree
-
+import akka.actor.{ActorSystem, Props}
+import com.amazonaws.auth.{AWSCredentials, AWSCredentialsProvider, DefaultAWSCredentialsProviderChain}
+import com.amazonaws.regions._
 import com.twitter.inject.{Injector, TwitterModule}
 
 import uk.ac.wellcome.platform.calm_adapter.actors._
-
-
-class StreamsRecordProcessor(client: AmazonDynamoDB) extends IRecordProcessor {
-
-  case class ExampleRecord(identifier: String)
-
-  override def initialize(shardId: String) = Unit
-  override def shutdown(checkpointer: IRecordProcessorCheckpointer, reason: ShutdownReason): Unit = {
-    if (reason == ShutdownReason.TERMINATE) {
-      checkpointer.checkpoint()
-    }
-  }
-
-  override def processRecords(records: JList[Record], checkpointer: IRecordProcessorCheckpointer): Unit = {
-    records.asScala.map { record =>
-      if (record.isInstanceOf[RecordAdapter]) {
-        val recordAdapter = record.asInstanceOf[RecordAdapter]
-        val keys = recordAdapter
-          .getInternalObject()
-          .getDynamodb()
-          .getKeys()
-        // TODO: proper pattern matching and map on result
-        val newRecord: ExampleRecord = ScanamoFree.read[ExampleRecord](keys).right.get
-        println(newRecord)
-      }
-    }
-  }
-}
-
-
-class StreamsRecordProcessorFactory() extends IRecordProcessorFactory {
-  override def createProcessor(): IRecordProcessor = {
-    new StreamsRecordProcessor(
-      AmazonDynamoDBClientBuilder
-        .standard()
-        .withRegion("eu-west-1")
-        .build())
-    }
-}
 
 
 object CalmAdapterWorker extends TwitterModule {
