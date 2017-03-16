ad84db9b4783d908d3826eaa7698ca2708b47de0
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-03-16 07:47:31 +0000

    Add SlowDown message

--- calm_adapter/src/main/scala/uk/ac/wellcome/calm_adapter/actors/PipelineWatcherActor.scala
+++ calm_adapter/src/main/scala/uk/ac/wellcome/calm_adapter/actors/PipelineWatcherActor.scala
@@ -5,16 +5,26 @@ import com.twitter.inject.Logging
 import com.google.inject.name.Named
 import javax.inject.Inject
 
+import uk.ac.wellcome.platform.calm_adapter.modules.ActorRegister
+
+
+case class SlowDown(m: DeadLetter)
 
 @Named("PipelineWatcherActor")
-class PipelineWatcherActor  @Inject()()
+class PipelineWatcherActor @Inject()(
+  actorRegister: ActorRegister
+)
   extends Actor
   with Logging {
 
   def receive = {
     case d: DeadLetter => {
-     info(s"Dead Letter: ${d}")
+     info(s"Watcher recieved dead Letter: ${d}")
+
+     actorRegister.actors
+       .get("OaiHarvestActor")
+       .map(_ ! SlowDown(d))
     }
-    case unknown => error(s"Received unknown object ${unknown}")
+    case m => error(s"Received unknown message ${m}")
   }
 }
