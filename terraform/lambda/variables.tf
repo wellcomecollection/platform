50051347cf613f0771dda1a7a1b4ad7d406f2668
Alex Chan <a.chan@wellcome.ac.uk>
2017-03-21 14:41:47 +0000

    Modularise the Lambda Terraform code

---
+++ terraform/lambda/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  description = "Name of the Lambda"
+}
+
+variable "description" {
+  description = "Description of the Lambda function"
+}
+
+variable "filename" {
+  description = "ZIP file containing the Lambda source code"
+}
--- terraform/lambdas.tf
+++ terraform/lambdas.tf
@@ -1,26 +1,19 @@
-resource "aws_iam_role" "awlc_iam_role" {
-  name = "awlc_iam_role"
+/*
+  Lambda for updating ECS service size.
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  This is triggered by updates to an SNS topic.
+ */
+
+module "update_ecs_service_size_lambda" {
+  source      = "./lambda"
+  name        = "update_ecs_service_size"
+  description = "Update the desired count of an ECS service"
+  filename    = "../lambdas/update_ecs_service_size.py.zip"
 }
 
-resource "aws_iam_role_policy" "awlc_iam_role_policy" {
+resource "aws_iam_role_policy" "update_ecs_service_size_policy" {
   name = "update_ecs_service_size"
-  role = "${aws_iam_role.awlc_iam_role.name}"
+  role = "${module.update_ecs_service_size_lambda.role_name}"
 
   policy = <<EOF
 {
@@ -32,107 +25,48 @@ resource "aws_iam_role_policy" "awlc_iam_role_policy" {
         "ecs:UpdateService"
       ],
       "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": "logs:CreateLogGroup",
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": ["${aws_cloudwatch_log_group.update_ecs_service_size.arn}"]
     }
   ]
 }
 EOF
-}
-
-
-resource "aws_lambda_function" "update_ecs_service_size" {
-  filename      = "../lambdas/update_ecs_service_size.py.zip"
-  description   = "Update the desired count of a service running in ECS"
-  function_name = "update_ecs_service_size"
-  role          = "${aws_iam_role.awlc_iam_role.arn}"
-  handler       = "update_ecs_service_size.main"
-  runtime       = "python2.7"
-  depends_on = [
-    "aws_iam_role_policy.awlc_iam_role_policy"
-  ]
 }
 
 resource "aws_sns_topic_subscription" "topic_lambda" {
   topic_arn = "${aws_sns_topic.service_scheduler_topic.arn}"
   protocol  = "lambda"
-  endpoint  = "${aws_lambda_function.update_ecs_service_size.arn}"
+  endpoint  = "${module.update_ecs_service_size_lambda.arn}"
 }
 
 resource "aws_lambda_permission" "with_sns" {
-    statement_id = "AllowExecutionFromSNS"
-    action = "lambda:InvokeFunction"
-    function_name = "${aws_lambda_function.update_ecs_service_size.arn}"
-    principal = "sns.amazonaws.com"
-    source_arn = "${aws_sns_topic.service_scheduler_topic.arn}"
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = "${module.update_ecs_service_size_lambda.arn}"
+  principal     = "sns.amazonaws.com"
+  source_arn    = "${aws_sns_topic.service_scheduler_topic.arn}"
 }
 
+/*
+  Lambda for publishing ECS service schedules to an SNS topic.
+ */
 
-
-
-
-resource "aws_cloudwatch_event_rule" "once_a_day" {
-  name = "once-a-day"
-  description = "Fires once a day"
-  schedule_expression = "rate(1 minute)"
-}
-
-resource "aws_cloudwatch_event_target" "check_foo_every_five_minutes" {
-    rule = "${aws_cloudwatch_event_rule.once_a_day.name}"
-    arn = "${aws_lambda_function.publish_to_sns.arn}"
-    input = "{\"hello\": \"world\"}"
+module "publish_to_sns_lambda" {
+  source      = "./lambda"
+  name        = "publish_to_sns"
+  description = "Publish an ECS service schedule to SNS"
+  filename    = "../lambdas/publish_to_sns.py.zip"
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_foo" {
-    statement_id = "AllowExecutionFromCloudWatch"
-    action = "lambda:InvokeFunction"
-    function_name = "${aws_lambda_function.publish_to_sns.function_name}"
-    principal = "events.amazonaws.com"
-    source_arn = "${aws_cloudwatch_event_rule.once_a_day.arn}"
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = "${module.publish_to_sns_lambda.function_name}"
+  principal     = "events.amazonaws.com"
+  source_arn    = "${aws_cloudwatch_event_rule.once_a_day.arn}"
 }
 
-
-
-
-
-
-
-
-
-resource "aws_iam_role" "awlc_iam_role_trigger" {
-  name = "awlc_iam_role_trigger"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "awlc_iam_role_policy_trigger" {
+resource "aws_iam_role_policy" "publish_to_sns_lambda_policy" {
   name = "publish_to_sns_policy"
-  role = "${aws_iam_role.awlc_iam_role_trigger.name}"
+  role = "${module.publish_to_sns_lambda.role_name}"
 
   policy = <<EOF
 {
@@ -143,48 +77,27 @@ resource "aws_iam_role_policy" "awlc_iam_role_policy_trigger" {
       "Action": [
         "sns:Publish"
       ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": ["${aws_cloudwatch_log_group.publish_to_sns.arn}"]
+      "Resource": "${aws_sns_topic.service_scheduler_topic.arn}"
     }
   ]
 }
 EOF
 }
 
+/*
+  Schedules for individual services.
+ */
 
-resource "aws_lambda_function" "publish_to_sns" {
-  filename      = "../lambdas/publish_to_sns.py.zip"
-  description   = "Publish to SNS"
-  function_name = "publish_to_sns"
-  role          = "${aws_iam_role.awlc_iam_role_trigger.arn}"
-  handler       = "publish_to_sns.main"
-  runtime       = "python2.7"
-  depends_on = [
-    "aws_iam_role_policy.awlc_iam_role_policy_trigger"
-  ]
+resource "aws_cloudwatch_event_target" "trigger_calm_adapter_daily" {
+  rule = "${aws_cloudwatch_event_rule.once_a_day.name}"
+  arn  = "${module.publish_to_sns_lambda.arn}"
+
+  input = <<EOF
+{
+  "topic_arn": "${aws_sns_topic.service_scheduler_topic.arn}",
+  "cluster": "${aws_ecs_cluster.services.name}",
+  "service": "calm_adapter",
+  "desired_count": 1
 }
-
-resource "aws_cloudwatch_log_group" "publish_to_sns" {
-  name = "/aws/lambda/publish_to_sns"
+EOF
 }
-
-resource "aws_cloudwatch_log_group" "update_ecs_service_size" {
-  name = "/aws/lambda/update_ecs_service_size"
-}
-
-
-
-
-
-
-
-
-
-
