398dd56c1d3cd62c3ac77ccb13819fe6bfce1316
Alex Chan <a.chan@wellcome.ac.uk>
2017-03-23 12:40:34 +0000

    Review markups for @kenoir

--- terraform/lambda/outputs.tf
+++ terraform/lambda/outputs.tf
@@ -1,19 +1,11 @@
 output "arn" {
+  description = "ARN of the Lambda function"
   value = "${aws_lambda_function.lambda_function.arn}"
 }
 
 output "function_name" {
+  description = "Name of the Lambda function"
   value = "${aws_lambda_function.lambda_function.function_name}"
-}
-
-output "cloudwatch_arn" {
-  description = "ARN of the CloudWatch log group for this Lambda"
-  value       = "${aws_cloudwatch_log_group.cloudwatch_log_group.arn}"
-}
-
-output "role_arn" {
-  description = "ARN of the IAM role for this Lambda"
-  value       = "${aws_iam_role.iam_role.arn}"
 }
 
 output "role_name" {
--- terraform/lambda/variables.tf
+++ terraform/lambda/variables.tf
@@ -9,13 +9,3 @@ variable "description" {
 variable "filename" {
   description = "Path to the Python file containing the Lambda source code"
 }
-
-variable "sns_trigger_arn" {
-  description = "If this Lambda is triggered by SNS, the ARN of the trigger topic"
-  default     = ""
-}
-
-variable "cloudwatch_trigger_arn" {
-  description = "If this Lambda is triggered by CloudWatch events, the ARN of the CloudWatch rule"
-  default     = ""
-}
