4799675b914223a57c404e3864773619c9d7be52
Robert Kenny <kenoir@users.noreply.github.com>
2017-03-29 13:37:23 +0100

    Adds policy for ingestor role (#22)
    That allows access to SQS
    In order that it can poll the ingestion queue and process updates to
    the API

--- terraform/services/ecs_service/outputs.tf
+++ terraform/services/ecs_service/outputs.tf
@@ -1,3 +1,7 @@
 output "target_group_arn" {
   value = "${aws_alb_target_group.ecs_service.arn}"
 }
+
+output "role_name" {
+  value = "${aws_iam_role.ecs_service.name}"
+}
--- terraform/services/outputs.tf
+++ terraform/services/outputs.tf
@@ -1,3 +1,7 @@
 output "target_group_arn" {
   value = "${module.service.target_group_arn}"
 }
+
+output "role_name" {
+  value = "${module.service.role_name}"
+}
