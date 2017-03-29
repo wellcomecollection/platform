b8a4a5c86628a9fbb391c7c8952266883a20dad6
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-03-29 14:06:14 +0100

    Add a comment about the Dynamo warmup routine

--- calm_adapter/src/main/scala/uk/ac/wellcome/calm_adapter/modules/DynamoWarmupModule.scala
+++ calm_adapter/src/main/scala/uk/ac/wellcome/calm_adapter/modules/DynamoWarmupModule.scala
@@ -7,6 +7,20 @@ import com.twitter.inject.{Injector, Logging, TwitterModule}
 import uk.ac.wellcome.finatra.modules.{DynamoClientModule, DynamoConfigModule}
 import uk.ac.wellcome.utils._
 
+/** Scale up/down Dynamo write capacity while the adapter is running.
+  *
+  * Dynamo pricing is based on "provisioned read/write throughput capacity" --
+  * essentially, how fast you can read/write the database.  Faster writes are
+  * more expensive.  Details: https://aws.amazon.com/dynamodb/pricing/
+  *
+  * When the adapter is running, we want lots of write capacity so that we can
+  * finish quickly.  But most of the time that write capacity isn't being used,
+  * so leaving it up would be a waste of money.
+  *
+  * This actor increases our write capacity at the start of a run, then resets
+  * it to 1 (the lowest value) after we're finished, so we're only running with
+  * a high write capacity for as short a period as possible.
+  */
 object DynamoWarmupModule extends TwitterModule {
   override val modules = Seq(DynamoClientModule, DynamoConfigModule)
 
