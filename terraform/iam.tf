19fc9667f2dcbea0e8212ffdd308df2168fae282
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-03-30 15:44:15 +0100

    Adds permissions for transformer (#27)
    Publishing to SNS & reading from kinesis stream.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -71,3 +71,41 @@ data "aws_iam_policy_document" "allow_all_calm_db" {
     ]
   }
 }
+
+resource "aws_iam_role_policy" "ecs_transformer_task_sns" {
+  name = "ecs_task_jenkins_policy"
+  role = "${module.transformer.role_name}"
+
+  policy = "${data.aws_iam_policy_document.publish_to_calm_sns.json}"
+}
+
+data "aws_iam_policy_document" "publish_to_calm_sns" {
+  statement {
+    actions = [
+      "SNS:Publish",
+    ]
+
+    resources = [
+      "${aws_sns_topic.ingest_topic.arn}",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "ecs_transformer_task_kinesis_stream" {
+  name = "ecs_task_jenkins_policy"
+  role = "${module.transformer.role_name}"
+
+  policy = "${data.aws_iam_policy_document.read_calm_kinesis_stream.json}"
+}
+
+data "aws_iam_policy_document" "read_calm_kinesis_stream" {
+  statement {
+    actions = [
+      "dynamodb:*",
+    ]
+
+    resources = [
+      "${aws_dynamodb_table.calm-dynamodb-table.stream_arn}",
+    ]
+  }
+}
