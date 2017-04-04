18977f73dd913d832ac5accbcc599aca71a109aa
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-04-04 10:24:24 +0100

    Review markups
    *   Ensure that we'll spot a list that doesn't use underscores, by warning
        on non-unique keys in dicts.
    *   Add an extra check for null values.

--- adapter_miro/test_miro_adapter.py
+++ adapter_miro/test_miro_adapter.py
@@ -6,7 +6,7 @@ import pytest
 from utils import elem_to_dict
 
 
-@pytest.mark.parametrize('xml_string, expected_dict', [
+@pytest.mark.parametrize('xml_string, expected_data', [
     # Example with a list
     (
         '''
@@ -79,7 +79,62 @@ from utils import elem_to_dict
             'cox': {'color': 'orange', 'country': 'United Kingdom'},
         }
     ),
+
+    # Whitespace is trimmed from records
+    (
+        '''
+        <apple>
+            <variety>braeburn\n</variety>
+            <color>\tred</color>
+            <count>\r\r5\n</count>
+        </apple>
+        ''',
+        {
+            'variety': 'braeburn',
+            'color': 'red',
+            'count': '5',
+        }
+    ),
+
+    # Empty or whitespace-only values are replaced with None
+    (
+        '''
+        <apple>
+            <variety>\n\n\r</variety>
+            <color></color>
+        </apple>
+        ''',
+        {
+            'variety': None,
+            'color': None,
+        }
+    ),
 ])
-def test_elem_to_dict(xml_string, expected_dict):
+def test_elem_to_dict(xml_string, expected_data):
+    """Parsing XML strings gives the correct data."""
     elem = etree.fromstring(xml_string)
-    assert elem_to_dict(elem) == expected_dict
+    assert elem_to_dict(elem) == expected_data
+
+
+@pytest.mark.parametrize('xml_string', [
+    # List-like structure where not all the tags are correct
+    '''
+    <apple>
+        <_>braeburn</_>
+        <__>not an underscore</__>
+    </apple>
+    ''',
+
+    # Dict-like structure with non-unique keys
+    '''
+    <apple>
+        <variety>braeburn</variety>
+        <variety>cox</variety>
+    </apple>
+    ''',
+])
+def test_bad_elem_throws_assert_error(xml_string):
+    """Parsing malformed XML strings throws an AssertionError."""
+    elem = etree.fromstring(xml_string)
+    with pytest.raises(AssertionError):
+        elem_to_dict(elem)
