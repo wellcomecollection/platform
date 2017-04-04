8cc8a17077edb8eb2cf896723c979df1d8352196
Alice Fuzier Cayla <a.fuzier-cayla@wellcome.ac.uk>
2017-04-04 09:30:51 +0100

    formatted with scalafmt

--- transformer/src/test/scala/uk/ac/wellcome/transformer/receive/RecordReceiverTest.scala
+++ transformer/src/test/scala/uk/ac/wellcome/transformer/receive/RecordReceiverTest.scala
@@ -1,6 +1,10 @@
 package uk.ac.wellcome.transformer.receive
 
-import com.amazonaws.services.dynamodbv2.model.{AttributeValue, Record, StreamRecord}
+import com.amazonaws.services.dynamodbv2.model.{
+  AttributeValue,
+  Record,
+  StreamRecord
+}
 import com.amazonaws.services.dynamodbv2.streamsadapter.model.RecordAdapter
 import com.amazonaws.services.sns.AmazonSNS
 import com.amazonaws.services.sns.model.{PublishRequest, PublishResult}
@@ -14,46 +18,62 @@ import org.scalatest.{FunSpec, Matchers}
 import uk.ac.wellcome.models.aws.SNSConfig
 import uk.ac.wellcome.models.{Identifier, UnifiedItem}
 
-class RecordReceiverTest extends FunSpec with MockitoSugar with ScalaFutures with Matchers with IntegrationPatience{
+class RecordReceiverTest
+    extends FunSpec
+    with MockitoSugar
+    with ScalaFutures
+    with Matchers
+    with IntegrationPatience {
 
   val config = SNSConfig("eu-west-1", "arn:test:test:test:test:test")
   val mockSNS = createMockSNS
 
-  it(" should receive a message and send it to SNS client") {
+  it("should receive a message and send it to SNS client") {
     val recordReceiver = new RecordReceiver(config, mockSNS)
-    val future = recordReceiver.receiveRecord(new RecordAdapter(createMockRecord))
+    val future =
+      recordReceiver.receiveRecord(new RecordAdapter(createMockRecord))
 
-    whenReady(future) {_ =>
-      val unifiedItem = UnifiedItem("id", List(Identifier("source", "key", "value")), Some("TopSekrit"))
-      Mockito.verify(mockSNS).publish(new PublishRequest(config.topicArn, UnifiedItem.json(unifiedItem), "Foo"))
+    whenReady(future) { _ =>
+      val unifiedItem = UnifiedItem("id",
+                                    List(Identifier("source", "key", "value")),
+                                    Some("TopSekrit"))
+      Mockito
+        .verify(mockSNS)
+        .publish(
+          new PublishRequest(config.topicArn,
+                             UnifiedItem.json(unifiedItem),
+                             "Foo"))
     }
   }
 
   it("should return a failed future if it's unable to parse the dynamo record") {
     val recordReceiver = new RecordReceiver(config, mockSNS)
-    val future = recordReceiver.receiveRecord(new RecordAdapter(createInvalidRecord))
+    val future =
+      recordReceiver.receiveRecord(new RecordAdapter(createInvalidRecord))
 
-    whenReady(future.failed) { x=>
-      x.getMessage should startWith ("Unable to parse record")
+    whenReady(future.failed) { x =>
+      x.getMessage should startWith("Unable to parse record")
     }
   }
 
   it("should return a failed future if it's unable to transform the parsed record") {
     val recordReceiver = new RecordReceiver(config, mockSNS)
-    val future = recordReceiver.receiveRecord(new RecordAdapter(createNonTransformableRecord))
+    val future = recordReceiver.receiveRecord(
+      new RecordAdapter(createNonTransformableRecord))
 
-    whenReady(future.failed) { x=>
-      x shouldBe a [JsonMappingException]
+    whenReady(future.failed) { x =>
+      x shouldBe a[JsonMappingException]
     }
   }
 
   it("should return a failed future if it's unable to publish the unified item") {
     val mockSNS = mockFailPublishMessage
     val recordReceiver = new RecordReceiver(config, mockSNS)
-    val future = recordReceiver.receiveRecord(new RecordAdapter(createMockRecord))
+    val future =
+      recordReceiver.receiveRecord(new RecordAdapter(createMockRecord))
 
-    whenReady(future.failed) { x=>
-      x.getMessage should be ("Failed publishing message")
+    whenReady(future.failed) { x =>
+      x.getMessage should be("Failed publishing message")
     }
   }
 
@@ -67,7 +87,8 @@ class RecordReceiverTest extends FunSpec with MockitoSugar with ScalaFutures wit
 
   private def mockFailPublishMessage = {
     val mockSNS = mock[AmazonSNS]
-    when(mockSNS.publish(any[PublishRequest]())).thenThrow(new RuntimeException("Failed publishing message"))
+    when(mockSNS.publish(any[PublishRequest]()))
+      .thenThrow(new RuntimeException("Failed publishing message"))
     mockSNS
   }
 
@@ -80,7 +101,8 @@ class RecordReceiverTest extends FunSpec with MockitoSugar with ScalaFutures wit
 
   private def createMockRecord = {
     val record = new Record()
-    val streamRecord = createStreamRecord("""{"foo": ["bar"], "AccessStatus": ["TopSekrit"]}""")
+    val streamRecord = createStreamRecord(
+      """{"foo": ["bar"], "AccessStatus": ["TopSekrit"]}""")
     record.withDynamodb(streamRecord)
     record
   }
@@ -88,7 +110,8 @@ class RecordReceiverTest extends FunSpec with MockitoSugar with ScalaFutures wit
   private def createStreamRecord(data: String) = {
     val streamRecord = new StreamRecord()
     streamRecord.addNewImageEntry("RecordID", new AttributeValue("abcdef"))
-    streamRecord.addNewImageEntry("RecordType", new AttributeValue("collection"))
+    streamRecord.addNewImageEntry("RecordType",
+                                  new AttributeValue("collection"))
     streamRecord.addNewImageEntry("RefNo", new AttributeValue("AB/CD/12"))
     streamRecord.addNewImageEntry("AltRefNo", new AttributeValue("AB/CD/12"))
     streamRecord.addNewImageEntry("data", new AttributeValue(data))
@@ -98,7 +121,8 @@ class RecordReceiverTest extends FunSpec with MockitoSugar with ScalaFutures wit
   private def createInvalidRecord = {
     val record = new Record()
     val streamRecord = new StreamRecord()
-    streamRecord.addNewImageEntry("something", new AttributeValue("something-else"))
+    streamRecord.addNewImageEntry("something",
+                                  new AttributeValue("something-else"))
     record.withDynamodb(streamRecord)
     record
   }
