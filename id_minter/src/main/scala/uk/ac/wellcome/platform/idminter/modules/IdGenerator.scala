02bc37f66ee1bad3865acf0d46c18ac728aa1d75
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-04-05 16:16:36 +0100

    Some refactoring

--- id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/modules/IdGenerator.scala
+++ id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/modules/IdGenerator.scala
@@ -15,21 +15,32 @@ case class Id(CanonicalID: String, MiroID: String)
 class IdGenerator(dynamoDBClient: AmazonDynamoDB) extends Logging {
 
   def generateId(unifiedItem: UnifiedItem): Future[String] = Future {
-    unifiedItem.identifiers.find(identifier => identifier.sourceId == "MiroID") match {
-      case Some(identifier) => Scanamo.queryIndex[Id](dynamoDBClient)("Identifiers", "MiroID")('MiroID -> identifier.value) match {
+    findMiroID(unifiedItem) match {
+      case Some(identifier) => findMiroIdInDynamo(identifier.value) match {
         case Right(id) :: Nil => id.CanonicalID
-        case Nil =>
-          val canonicalId = Identifiable.generate
-          Scanamo.put(dynamoDBClient)("Identifiers")(Id(canonicalId, identifier.value))
-          canonicalId
-        case Right(_) :: tail => error(s"Found more than one record with MiroID ${identifier.value}")
-          throw new Exception(s"Found more than one record with MiroID ${identifier.value}")
-        case _ =>  error(s"Error in parsing the object with MiroID ${identifier.value}")
-          throw new Exception(s"Error in parsing the object with MiroID ${identifier.value}")
+        case Nil => generateAndSaveCanonicalId(identifier.value)
+        case Right(_) :: tail => logAndThrowError(s"Found more than one record with MiroID ${identifier.value}")
+        case _ =>  logAndThrowError(s"Error in parsing the object with MiroID ${identifier.value}")
       }
-      case None => error(s"Item $unifiedItem did not contain a MiroID")
-        throw new Exception(s"Item $unifiedItem did not contain a MiroID")
+      case None => logAndThrowError(s"Item $unifiedItem did not contain a MiroID")
     }
   }
 
+  private def findMiroID(unifiedItem: UnifiedItem) =
+    unifiedItem.identifiers.find(identifier => identifier.sourceId == "MiroID")
+
+  private def findMiroIdInDynamo(miroId: String) = {
+    Scanamo.queryIndex[Id](dynamoDBClient)("Identifiers", "MiroID")('MiroID -> miroId)
+  }
+
+  private def generateAndSaveCanonicalId(miroId: String) = {
+    val canonicalId = Identifiable.generate
+    Scanamo.put(dynamoDBClient)("Identifiers")(Id(canonicalId, miroId))
+    canonicalId
+  }
+
+  private def logAndThrowError(errorMessage: String) = {
+    error(errorMessage)
+    throw new Exception(errorMessage)
+  }
 }
