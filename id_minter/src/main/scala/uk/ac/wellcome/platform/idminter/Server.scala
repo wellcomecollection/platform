69c07b5f15abbfe7142e11756f19888c5b6bbe2a
Alice Fuzier Cayla <a.fuzier-cayla@wellcome.ac.uk>
2017-04-06 09:57:08 +0100

    Adding files somehow not committed until now

---
+++ id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/Server.scala
@@ -0,0 +1,30 @@
+package uk.ac.wellcome.platform.idminter
+
+import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finatra.http.HttpServer
+import com.twitter.finatra.http.filters.{
+  CommonFilters,
+  LoggingMDCFilter,
+  TraceIdMDCFilter
+}
+import com.twitter.finatra.http.routing.HttpRouter
+import uk.ac.wellcome.platform.idminter.controllers.ManagementController
+
+object ServerMain extends Server
+
+class Server extends HttpServer {
+  override val name = "uk.ac.wellcome.platform.ingestor Ingestor"
+  override val modules = Seq()
+
+  private final val servicePrefix = flag(name = "service.prefix",
+                                         default = "/idminter",
+                                         help = "API path prefix")
+
+  override def configureHttp(router: HttpRouter) {
+    router
+      .filter[CommonFilters]
+      .filter[LoggingMDCFilter[Request, Response]]
+      .filter[TraceIdMDCFilter[Request, Response]]
+      .add[ManagementController]
+  }
+}
