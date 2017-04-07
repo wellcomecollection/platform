2cb967c70195cc1cb91958b74d77748cc6105f06
Alice Fuzier Cayla <a.fuzier-cayla@wellcome.ac.uk>
2017-04-07 16:06:57 +0100

    Fixed cleanup before tests

--- id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/SNSLocal.scala
+++ id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/SNSLocal.scala
@@ -2,20 +2,20 @@ package uk.ac.wellcome.platform.idminter
 
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
 import com.amazonaws.services.sns.AmazonSNSClientBuilder
-import org.apache.http.client.methods.HttpPost
+import org.apache.http.client.methods.HttpDelete
 import org.apache.http.impl.client.DefaultHttpClient
 import org.scalatest.{BeforeAndAfterEach, Suite}
-
-import scala.util.Try
 
 trait SNSLocal extends Suite with BeforeAndAfterEach {
   val localSNSEnpointUrl = "http://localhost:9292"
   val amazonSNS = AmazonSNSClientBuilder.standard().withEndpointConfiguration(new EndpointConfiguration(localSNSEnpointUrl, "local")).build()
-  val ingestTopicArn = amazonSNS.createTopic("es_ingest").getTopicArn
+  private var topic = amazonSNS.createTopic("es_ingest")
+  def ingestTopicArn = topic.getTopicArn
 
   override def beforeEach(): Unit = {
     super.beforeEach()
-    new DefaultHttpClient().execute(new HttpPost(localSNSEnpointUrl+"/drain"))
+    new DefaultHttpClient().execute(new HttpDelete(localSNSEnpointUrl))
+    topic = amazonSNS.createTopic("es_ingest")
   }
 
 }
