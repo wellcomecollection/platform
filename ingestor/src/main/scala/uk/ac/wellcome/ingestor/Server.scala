d8aed9f43f32e196be6fc6bf3ecb2ba40d487056
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-04-07 17:36:35 +0100

    Used SQSReder in the ingestor

--- ingestor/src/main/scala/uk/ac/wellcome/ingestor/Server.scala
+++ ingestor/src/main/scala/uk/ac/wellcome/ingestor/Server.scala
@@ -2,23 +2,17 @@ package uk.ac.wellcome.platform.ingestor
 
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.HttpServer
-import com.twitter.finatra.http.filters.{
-  CommonFilters,
-  LoggingMDCFilter,
-  TraceIdMDCFilter
-}
+import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
 import com.twitter.finatra.http.routing.HttpRouter
-
 import uk.ac.wellcome.platform.ingestor.controllers._
 import uk.ac.wellcome.platform.ingestor.modules._
-
-import uk.ac.wellcome.finatra.modules.ElasticClientModule
+import uk.ac.wellcome.finatra.modules._
 
 object ServerMain extends Server
 
 class Server extends HttpServer {
   override val name = "uk.ac.wellcome.platform.ingestor Ingestor"
-  override val modules = Seq(SQSWorker, ElasticClientModule)
+  override val modules = Seq(SQSConfigModule, SQSClientModule, AkkaModule, SQSReaderModule, SQSWorker, ElasticClientModule)
 
   private val esIndex = flag[String]("es.index", "records", "ES index name")
   private val esType = flag[String]("es.type", "item", "ES document type")
