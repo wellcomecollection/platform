d4839e99d1e4254c7ab89af366941bf670bc07ea
Alice Fuzier Cayla <a.fuzier-cayla@wellcome.ac.uk>
2017-04-10 14:18:57 +0100

    Changed SQSReaderTest to use local SQS instead of mocks

--- common/src/test/scala/uk/ac/wellcome/sqs/SQSReaderTest.scala
+++ common/src/test/scala/uk/ac/wellcome/sqs/SQSReaderTest.scala
@@ -1,62 +1,42 @@
 package uk.ac.wellcome.sqs
 
-import com.amazonaws.services.sqs.AmazonSQS
-import com.amazonaws.services.sqs.model.{Message, ReceiveMessageRequest, ReceiveMessageResult}
-import org.mockito.Matchers.any
-import org.mockito.Mockito
-import org.mockito.Mockito.{verify, when}
+import com.amazonaws.services.sqs.model.{Message, ReceiveMessageResult}
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.{FunSpec, Matchers}
-import org.scalatest.mockito.MockitoSugar
 import uk.ac.wellcome.models.aws.SQSConfig
+import uk.ac.wellcome.test.utils.SQSLocal
 
 import scala.collection.JavaConversions._
 import scala.concurrent.duration._
 
-class SQSReaderTest extends FunSpec with MockitoSugar with Matchers with ScalaFutures with IntegrationPatience{
+class SQSReaderTest extends FunSpec with Matchers with ScalaFutures with IntegrationPatience with SQSLocal{
 
-  it("should send a request to the sqs client with the number messages and the waiting time, and return the messages returned by the client"){
-    val sqsClient = mock[AmazonSQS]
-    val sqsConfig = SQSConfig("eu-west-1", "blah")
-    mockSqsClient(sqsClient, sqsConfig, """["somejson"]""")
-    val sqsReader = new SQSReader(sqsClient, sqsConfig, waitTime = 20 seconds, 1)
+  it("should get messages from the SQS queue, limited by the maximum number of messages and return them"){
+    val sqsConfig = SQSConfig("eu-west-1", idMinterQueueUrl)
+    sqsClient.sendMessage(idMinterQueueUrl, "someMessage1")
+    sqsClient.sendMessage(idMinterQueueUrl, "someMessage2")
+    sqsClient.sendMessage(idMinterQueueUrl, "someMessage3")
+    val sqsReader = new SQSReader(sqsClient, sqsConfig, waitTime = 20 seconds, 2)
 
     val futureMessages = sqsReader.retrieveMessages()
 
     whenReady(futureMessages) {messages =>
-      verifyMockCalled(sqsClient, sqsConfig)
-      messages should have size 1
-      messages.head.getBody should be("""["somejson"]""")
+      messages should have size 2
+      val messageBodies = messages.map{_.getBody}
+      messageBodies  should contain ("someMessage1")
+      messageBodies should contain ("someMessage2")
     }
   }
 
   it("should return a failed future if reading from the sns client fails"){
-    val sqsClient = mock[AmazonSQS]
-    val sqsConfig = SQSConfig("eu-west-1", "blah")
-    when(sqsClient.receiveMessage(any[ReceiveMessageRequest]())).thenThrow(new RuntimeException("failed to connect to the queue"))
+    val sqsConfig = SQSConfig("eu-west-1", "not a valid queue url")
     val sqsReader = new SQSReader(sqsClient, sqsConfig, waitTime = 20 seconds, 1)
 
     val futureMessages = sqsReader.retrieveMessages()
 
     whenReady(futureMessages.failed) {exception =>
-      exception.getMessage should be ("failed to connect to the queue")
+      exception.getMessage should not be (empty)
     }
-  }
-
-  private def verifyMockCalled(sqsClient: AmazonSQS, sqsConfig: SQSConfig) = {
-    verify(sqsClient).receiveMessage(new ReceiveMessageRequest(sqsConfig.queueUrl).withWaitTimeSeconds(20).withMaxNumberOfMessages(1))
-  }
-
-  private def mockSqsClient(sqsClient: AmazonSQS, sqsConfig: SQSConfig, jsonMessages: String*): Any = {
-    when(sqsClient.receiveMessage(
-      new ReceiveMessageRequest(sqsConfig.queueUrl).withWaitTimeSeconds(20).withMaxNumberOfMessages(1)))
-      .thenReturn(createMessageResult(jsonMessages: _*))
-  }
-
-  private def createMessageResult(jsonMessages: String*) = {
-    new ReceiveMessageResult().withMessages(jsonMessages.toList.map { jsonMessage =>
-      createMessage(jsonMessage)
-    })
   }
 
   private def createMessage(jsonMessage: String) = new Message().withBody(jsonMessage)
