d250dc34383e789eae33bd523d1e02695446710d
Alex Chan <a.chan@wellcome.ac.uk>
2017-04-11 17:12:38 +0100

    Break up the IAM role stuff in iam.tf
    The pattern in the rest of the repo is to have a single type of
    resource per file -- this applies that standard to `iam.tf`, which
    was a bit of a mess of IAM roles, role policies and policy documents.

---
+++ terraform/iam_role_documents.tf
@@ -0,0 +1,112 @@
+data "aws_iam_policy_document" "allow_everything" {
+  statement {
+    actions = [
+      "*",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+/* TODO: Scope this policy document more tightly */
+data "aws_iam_policy_document" "allow_dynamodb_all" {
+  statement {
+    actions = [
+      "dynamodb:*",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "allow_calm_db_all" {
+  statement {
+    actions = [
+      "dynamodb:DescribeTable",
+      "dynamodb:PutItem",
+      "dynamodb:UpdateTable",
+    ]
+
+    resources = [
+      "${aws_dynamodb_table.calm_table.arn}",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "read_calm_kinesis_stream" {
+  statement {
+    actions = [
+      "dynamodb:*",
+    ]
+
+    resources = [
+      "${aws_dynamodb_table.calm_table.stream_arn}",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "read_ingestor_q" {
+  statement {
+    actions = [
+      "sqs:SendMessage",
+      "sqs:ReceiveMessage",
+    ]
+
+    resources = [
+      "${module.ingest_queue.q_arn}",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "read_id_minter_q" {
+  statement {
+    actions = [
+      "sqs:SendMessage",
+      "sqs:ReceiveMessage",
+    ]
+
+    resources = [
+      "${module.id_minter_queue.q_arn}",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "publish_to_id_minter_sns" {
+  statement {
+    actions = [
+      "SNS:Publish",
+    ]
+
+    resources = [
+      "${aws_sns_topic.id_minter_topic.arn}",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "publish_to_ingest_sns" {
+  statement {
+    actions = [
+      "sns:Publish",
+    ]
+
+    resources = [
+      "${aws_sns_topic.ingest_topic.arn}",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "publish_to_scheduler_sns" {
+  statement {
+    actions = [
+      "sns:Publish",
+    ]
+
+    resources = [
+      "${aws_sns_topic.service_scheduler_topic.arn}",
+    ]
+  }
+}
---
+++ terraform/iam_role_policy.tf
@@ -0,0 +1,71 @@
+# Role policies for Jenkins
+
+resource "aws_iam_role_policy" "ecs_jenkins_task" {
+  name   = "ecs_task_jenkins_policy"
+  role   = "${module.ecs_tools_iam.task_role_name}"
+  policy = "${data.aws_iam_policy_document.allow_everything.json}"
+}
+
+# Role policies for the Elasticsearch ingestor
+
+resource "aws_iam_role_policy" "ecs_ingestor_task_read_ingestor_q" {
+  name   = "ecs_task_ingestor_policy"
+  role   = "${module.ecs_ingestor_iam.task_role_name}"
+  policy = "${data.aws_iam_policy_document.read_ingestor_q.json}"
+}
+
+# Role policies for the Calm adapter
+
+resource "aws_iam_role_policy" "ecs_calm_adapter_task" {
+  name   = "ecs_task_calm_adapter_policy"
+  role   = "${module.ecs_calm_adapter_iam.task_role_name}"
+  policy = "${data.aws_iam_policy_document.allow_calm_db_all.json}"
+}
+
+resource "aws_iam_role_policy" "ecs_calm_adapter_publish_to_sns" {
+  name   = "publish_to_sns_policy"
+  role   = "${module.ecs_calm_adapter_iam.task_role_name}"
+  policy = "${data.aws_iam_policy_document.publish_to_scheduler_sns.json}"
+}
+
+# Role policies for the transformer
+
+resource "aws_iam_role_policy" "ecs_transformer_task_dynamo" {
+  name   = "ecs_task_transformer_task_dynamo_policy"
+  role   = "${module.ecs_transformer_iam.task_role_name}"
+  policy = "${data.aws_iam_policy_document.allow_dynamodb_all.json}"
+}
+
+resource "aws_iam_role_policy" "ecs_transformer_task_sns" {
+  name   = "ecs_task_task_sns_policy"
+  role   = "${module.ecs_transformer_iam.task_role_name}"
+  policy = "${data.aws_iam_policy_document.publish_to_id_minter_sns.json}"
+}
+
+resource "aws_iam_role_policy" "ecs_transformer_task_kinesis_stream" {
+  name   = "ecs_task_kinesis_stream_policy"
+  role   = "${module.ecs_transformer_iam.task_role_name}"
+  policy = "${data.aws_iam_policy_document.read_calm_kinesis_stream.json}"
+}
+
+# Role policies for the ID minter
+
+resource "aws_iam_role_policy" "ecs_id_minter_task_sns" {
+  name   = "ecs_task_task_sns_policy"
+  role   = "${module.ecs_id_minter_iam.task_role_name}"
+  policy = "${data.aws_iam_policy_document.publish_to_ingest_sns.json}"
+}
+
+resource "aws_iam_role_policy" "ecs_id_minter_task_read_id_minter_q" {
+  name   = "ecs_task_id_minter_policy"
+  role   = "${module.ecs_id_minter_iam.task_role_name}"
+  policy = "${data.aws_iam_policy_document.read_id_minter_q.json}"
+}
+
+# Role policies for the Publish to SNS Lambda
+
+resource "aws_iam_role_policy" "publish_to_sns_lambda_policy" {
+  name   = "publish_to_sns_policy"
+  role   = "${module.publish_to_sns_lambda.role_name}"
+  policy = "${data.aws_iam_policy_document.publish_to_scheduler_sns.json}"
+}
