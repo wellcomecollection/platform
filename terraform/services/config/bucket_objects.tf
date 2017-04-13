0eb1a8ebc967b041a8a462b780449e5b53ff6cad
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-04-13 15:10:14 +0100

    Review markups

--- terraform/services/config/bucket_objects.tf
+++ terraform/services/config/bucket_objects.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket_object" "config_file" {
   bucket  = "${var.infra_bucket}"
   acl     = "private"
-  key     = "config/alex/${var.app_name}.conf"
+  key     = "config/prod/${var.app_name}.conf"
   content = "${data.template_file.config.rendered}"
 }
