30885091d35541f90cb2da3cc3ff7ee6809d718c
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-04-13 11:43:26 +0100

    Moved some tests around now that parsing of the dynamo record is handled outside RecordReceiver

--- transformer/src/test/scala/uk/ac/wellcome/transformer/parsers/CalmParserTest.scala
+++ transformer/src/test/scala/uk/ac/wellcome/transformer/parsers/CalmParserTest.scala
@@ -1,21 +1,45 @@
 package uk.ac.wellcome.transformer.parsers
 
-import com.amazonaws.services.dynamodbv2.streamsadapter.model.RecordAdapter
-import com.fasterxml.jackson.databind.JsonMappingException
-import org.scalatest.{FunSpec, FunSuite}
-import uk.ac.wellcome.models.{SourceIdentifier, UnifiedItem}
-import uk.ac.wellcome.transformer.receive.RecordReceiver
+import com.amazonaws.services.dynamodbv2.model.Record
+import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.models.CalmTransformable
+import uk.ac.wellcome.transformer.receive.{CalmRecordUtils, RecordMap}
 
-class CalmParserTest extends FunSpec {
+class CalmParserTest extends FunSpec with CalmRecordUtils with Matchers {
 
+  val RecordID = "abcdef"
+  val RecordType = "collection"
+  val RefNo = "AB/CD/12"
+  val AltRefNo = "AB/CD/12"
+  val data = """{"foo": ["bar"], "AccessStatus": ["TopSekrit"]}"""
+  val calmRecord =
+    createValidCalmRecord(RecordID, RecordType, AltRefNo, RefNo, data)
 
-//  it("should return a failed future if it's unable to transform the parsed record") {
-//    val recordReceiver = new RecordReceiver(mockSNSWriter, mockTransformableParser(mockRecord, UnifiedItem(List(SourceIdentifier("Calm", "AltRefNo", "1234")), None)))
-//    val future = recordReceiver.receiveRecord(
-//      new RecordAdapter(createNonTransformableRecord))
-//
-//    whenReady(future.failed) { x =>
-//      x shouldBe a[JsonMappingException]
-//    }
-//  }
+  it("should parse a record into a calm case class") {
+    val calmParser = new CalmParser
+    val triedCalmTransformable =
+      calmParser.extractTransformable(toRecordMap(calmRecord))
+
+    triedCalmTransformable.isSuccess should be(true)
+    triedCalmTransformable.get shouldBe a[CalmTransformable]
+    triedCalmTransformable.get
+      .asInstanceOf[CalmTransformable] shouldBe CalmTransformable(RecordID,
+                                                                  RecordType,
+                                                                  AltRefNo,
+                                                                  RefNo,
+                                                                  data)
+  }
+
+  it("should return a failed try if it's unable to transform the parsed record") {
+    val calmParser = new CalmParser
+
+    val triedCalmTransformable =
+      calmParser.extractTransformable(toRecordMap(createInvalidRecord))
+
+    triedCalmTransformable.isFailure should be(true)
+  }
+
+  private def toRecordMap(calmRecord: Record) = {
+    RecordMap(calmRecord.getDynamodb.getNewImage)
+  }
 }
