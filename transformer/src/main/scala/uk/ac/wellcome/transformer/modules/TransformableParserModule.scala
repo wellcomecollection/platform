04254392cc073f2c3414aa023af2901e609290b8
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-04-18 17:34:44 +0100

    Adding integration test around CalmData

--- transformer/src/main/scala/uk/ac/wellcome/transformer/modules/TransformableParserModule.scala
+++ transformer/src/main/scala/uk/ac/wellcome/transformer/modules/TransformableParserModule.scala
@@ -4,7 +4,7 @@ import com.google.inject.{Provides, Singleton}
 import com.twitter.inject.TwitterModule
 import uk.ac.wellcome.models.Transformable
 import uk.ac.wellcome.models.aws.DynamoConfig
-import uk.ac.wellcome.transformer.parsers.{MiroParser, TransformableParser}
+import uk.ac.wellcome.transformer.parsers.{CalmParser, MiroParser, TransformableParser}
 
 object TransformableParserModule extends TwitterModule {
 
@@ -13,6 +13,7 @@ object TransformableParserModule extends TwitterModule {
   def providesTransformableParser(dynamoConfig: DynamoConfig): TransformableParser[Transformable] = {
     dynamoConfig.table match{
       case "MiroData" => new MiroParser
+      case "CalmData" => new CalmParser
       case _ => throw new RuntimeException("Source not recognised")
     }
   }
