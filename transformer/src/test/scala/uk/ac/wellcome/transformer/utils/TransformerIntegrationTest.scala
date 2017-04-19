5e816696ff11b48d9ee283231761dcb92086f5b6
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-04-19 11:54:48 +0100

    Prevent kinesis adapter from creating tables in the real aws dynamo db

--- transformer/src/test/scala/uk/ac/wellcome/transformer/utils/TransformerIntegrationTest.scala
+++ transformer/src/test/scala/uk/ac/wellcome/transformer/utils/TransformerIntegrationTest.scala
@@ -1,12 +1,12 @@
 package uk.ac.wellcome.transformer.utils
 
+import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker.KinesisClientLibConfiguration
 import com.amazonaws.services.kinesis.metrics.interfaces.MetricsLevel
 import com.google.inject.{Provides, Singleton}
 import com.twitter.inject.TwitterModule
 import org.scalatest.concurrent.{Eventually, IntegrationPatience}
 import uk.ac.wellcome.models.aws.DynamoConfig
-import uk.ac.wellcome.platform.transformer.modules.KinesisClientLibConfigurationModule
 import uk.ac.wellcome.test.utils.IntegrationTestBase
 
 trait TransformerIntegrationTest extends IntegrationTestBase
@@ -17,6 +17,12 @@ trait TransformerIntegrationTest extends IntegrationTestBase
     @Provides
     @Singleton
     def provideKinesisClientLibConfiguration(dynamoConfig: DynamoConfig): KinesisClientLibConfiguration =
-      KinesisClientLibConfigurationModule.provideKinesisClientLibConfiguration(dynamoConfig).withMetricsLevel(MetricsLevel.NONE)
+      new KinesisClientLibConfiguration(
+        dynamoConfig.applicationName,
+        dynamoConfig.arn,
+        new AWSStaticCredentialsProvider(
+          new BasicAWSCredentials("access", "secret")),
+        java.util.UUID.randomUUID.toString
+      ).withMetricsLevel(MetricsLevel.NONE)
   }
 }
