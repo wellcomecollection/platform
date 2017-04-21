d4b0561c7f11e16ec183a6c2664b0c369e0ce103
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-04-21 14:54:27 +0100

    Make sure we get the remote state, just don't write it

--- terraform/plan.sh
+++ terraform/plan.sh
@@ -30,6 +30,18 @@ fi
 
 terraform init
 terraform get
+
+# When running in Travis, move the config telling us about the remote
+# state file.  Otherwise, Travis will attempt to update the state file,
+# which is:
+#  1) Not something we want to do
+#  2) Not allowed with its IAM permissions
+if [[ ! -z "$TRAVIS" && "$TRAVIS" == "true" ]]
+then
+    mv terraform.tf terraform.tf.bak
+    terraform init -force-copy
+fi
+
 terraform plan -out terraform.plan
 
 echo "Please review the above plan. If you are happy then run 'terraform apply terraform.plan'"
