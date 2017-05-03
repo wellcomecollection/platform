f2f045a4a2295209f19bf93d55be8c9aedd21879
Alex Chan <a.chan@wellcome.ac.uk>
2017-05-03 18:21:12 +0100

    Fix permissions for Travis

--- terraform/iam_policy_document.tf
+++ terraform/iam_policy_document.tf
@@ -180,12 +180,22 @@ data "aws_iam_policy_document" "travis_permissions" {
 
   statement {
     actions = [
+      "ecr:GetAuthorizationToken",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    actions = [
       "s3:PutObject",
       "s3:GetObject",
     ]
 
     resources = [
-      "${aws_s3_bucket.infra.arn}",
+      "${aws_s3_bucket.infra.arn}/releases/*",
     ]
   }
 }
