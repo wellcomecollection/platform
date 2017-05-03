27d1d536e4556f0c989abed4bfd6479d35cb7d08
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-05-03 10:15:49 +0100

    Move the Travis config out into a script (#218)

---
+++ .travis/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o verbose
+
+# This is needed for the Elasticsearch docker container to start.
+# See https://github.com/travis-ci/travis-ci/issues/6534
+sudo sysctl -w vm.max_map_count=262144
+
+# Install Docker on Travis.  Merely exposing Docker through the Travis
+# settings is insufficient, because the docker-compose tests we use are
+# unable to see it in that case.
+curl -sSL "https://get.docker.com/gpg" | sudo -E apt-key add -
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --assume-yes install docker-engine
+docker version
+
+# Install the AWS tools so we can log in to ECR
+pip install --upgrade --user awscli
