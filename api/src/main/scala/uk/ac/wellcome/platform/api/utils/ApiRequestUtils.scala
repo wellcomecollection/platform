f541cd5534a99d5e95b43d54353d33bb8a55a884
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-05-03 17:45:16 +0100

    Fix the API hostname when running behind ELB
    Previously, the @context parameter in our JSON-LD responses would
    point to http://app:8888, which is both wrong and unhelpful.
    This patch detects if we're running in ALB, and swaps it out for
    our public hostname.

--- api/src/main/scala/uk/ac/wellcome/platform/api/utils/ApiRequestUtils.scala
+++ api/src/main/scala/uk/ac/wellcome/platform/api/utils/ApiRequestUtils.scala
@@ -11,9 +11,21 @@ object ApiRequestUtils {
       case _ => "http"
     }
 
-    val hostHeader = request.host match {
-      case Some(host) => host
-      case _ => throw new BadRequestException("Host header not set")
+    // Most users will access our API through a DNS record that comes through
+    // https://api.wellcomecollection.org.  I can't find a way to detect
+    // the DNS name through the request headers, so just hard-code it if we
+    // detect the request came through ELB.
+    //
+    // Specifically, we look for a header that is added to all ELB requests:
+    // http://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-request-tracing.html
+    val hostHeader: String = request.headerMap.get("X-Amzn-Trace-Id") match {
+      case Some(_) => "api.wellcomecollection.org"
+      case None => {
+        request.host match {
+          case Some(host) => host
+          case _ => throw new BadRequestException("Host header not set")
+        }
+      }
     }
 
     scheme + "://" + hostHeader
