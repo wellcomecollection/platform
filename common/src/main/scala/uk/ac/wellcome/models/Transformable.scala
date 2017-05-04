22114e336aa053c63e9f31d6dc8910ddaff92dc5
Alex Chan <a.chan@wellcome.ac.uk>
2017-05-04 14:58:42 +0100

    Update the MiroTransformableData and UnifiedItem classes

--- common/src/main/scala/uk/ac/wellcome/models/Transformable.scala
+++ common/src/main/scala/uk/ac/wellcome/models/Transformable.scala
@@ -1,13 +1,20 @@
 package uk.ac.wellcome.models
 
-import uk.ac.wellcome.utils.JsonUtil
 import scala.util.Try
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+import uk.ac.wellcome.utils.JsonUtil
 
 trait Transformable {
   def transform: Try[Work]
 }
 
-case class MiroTransformableData(image_title: Option[String])
+case class MiroTransformableData(
+  @JsonProperty("image_title") title: Option[String],
+  @JsonProperty("image_image_desc") imageDesc: Option[String],
+  @JsonProperty("image_supp_lettering") suppLettering: Option[String]
+)
 
 case class MiroTransformable(MiroID: String,
                              MiroCollection: String,
@@ -16,7 +23,7 @@ case class MiroTransformable(MiroID: String,
   override def transform: Try[Work] =
     JsonUtil.fromJson[MiroTransformableData](data).map { miroData =>
       MiroCollection match {
-        case "Images-A" => transform_imagesA(miroData)
+        case "Images-A" => transformImagesA(miroData)
         case _ => throw new Exception(s"Unable to transform unknown collection $MiroCollection")
       }
     }
@@ -25,12 +32,12 @@ case class MiroTransformable(MiroID: String,
   // and based on a cursory expection of the Miro data.
   // TODO: Get a proper mapping of fields.
 
-  private def transform_imagesA(miroData: MiroTransformableData): Try[UnifiedItem] =
-    UnifiedItem(
+  private def transformImagesA(miroData: MiroTransformableData): Work =
+    Work(
       identifiers = List(SourceIdentifier("Miro", "MiroID", MiroID)),
-      label = miroData.image_title.getOrElse("<no label found>"),
-      description = miroData.image_image_desc.getOrElse("<no description found>"),
-      lettering = miroData.image_supp_lettering.getOrElse("<no lettering found>"),
+      label = miroData.title.getOrElse("no label found"),
+      description = miroData.imageDesc,
+      lettering = miroData.suppLettering,
       hasCreatedDate = None,
       hasCreator = List()
     )
