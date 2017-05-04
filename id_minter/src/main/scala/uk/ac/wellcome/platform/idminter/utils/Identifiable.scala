cc9bf0ecb76842176526bf004f9b895de3a08073
Alex Chan <a.chan@wellcome.ac.uk>
2017-05-04 09:35:57 +0100

    Review markups

--- id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/utils/Identifiable.scala
+++ id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/utils/Identifiable.scala
@@ -16,9 +16,12 @@ object Identifiable {
   def generate = {
     (1 to identifierLength)
       .map(x => x match {
-        // In certain contexts, IDs are used as a string.  This can cause
-        // problems if the first character is a digit, so we have a rule
-        // that the first character must be a letter.
+        // One of the serialization formats of RDF is XML, so for
+        // compatibility, our identifiers have to comply with XML rules.
+        // XML identifiers cannot start with numbers, so we apply the same
+        // rule to the identifiers we generate.
+        //
+        // See: http://stackoverflow.com/a/1077111/1558022
         case 1 => firstCharacterSet(Random.nextInt(firstCharacterSet.length))
         case _ => allowedCharacterSet(Random.nextInt(allowedCharacterSet.length))
       })
