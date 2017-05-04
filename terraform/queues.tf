47f4c5c4ad8253221144238870cb8c6550551c91
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-05-04 11:04:23 +0100

    Fix some broken names of resources (#72)

--- terraform/queues.tf
+++ terraform/queues.tf
@@ -1,9 +1,9 @@
-module "ingest_queue" {
+module "es_ingest_queue" {
   source      = "./sqs"
   queue_name  = "es_ingest_queue"
   aws_region  = "${var.aws_region}"
   account_id  = "${data.aws_caller_identity.current.account_id}"
-  topic_names = ["${module.ingest_topic.name}"]
+  topic_names = ["${module.es_ingest_topic.name}"]
 }
 
 module "id_minter_queue" {
