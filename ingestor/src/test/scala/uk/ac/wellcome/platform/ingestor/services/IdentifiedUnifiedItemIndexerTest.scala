fe81e366d49d34f1e3bb9b9840bbb8aaba520245
caylaa <alicefuzier@gmail.com>
2017-05-04 14:33:28 +0100

    Ingestor should not create a new item if one already exists in elastic search with the same id

--- ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/services/IdentifiedUnifiedItemIndexerTest.scala
+++ ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/services/IdentifiedUnifiedItemIndexerTest.scala
@@ -9,24 +9,32 @@ import uk.ac.wellcome.test.utils.ElasticSearchLocal
 import uk.ac.wellcome.utils.GlobalExecutionContext.context
 import uk.ac.wellcome.utils.JsonUtil
 
+import scala.concurrent.Future
+
 class IdentifiedUnifiedItemIndexerTest
     extends FunSpec
     with ScalaFutures
     with Matchers
     with ElasticSearchLocal {
 
-  it("should insert an identified unified item into Elasticsearch") {
-    val index = "records"
-    val itemType = "item"
-    val identifiedUnifiedItemIndexer =
-      new IdentifiedUnifiedItemIndexer(index, itemType, elasticClient)
-    val identifiedUnifiedItemString = JsonUtil
-      .toJson(
+  val itemType = "item"
+
+  val identifiedUnifiedItemIndexer =
+    new IdentifiedUnifiedItemIndexer(index, itemType, elasticClient)
+
+  def identifiedUnifiedItemJson(canonicalId: String, sourceId: String, label: String): String = {
+    JsonUtil.toJson(
         IdentifiedUnifiedItem(
-          canonicalId = "5678",
+          canonicalId = canonicalId,
           unifiedItem = UnifiedItem(
-            identifiers = List(SourceIdentifier("Miro", "MiroID", "1234")), label = "some label")))
+            identifiers = List(SourceIdentifier("Miro", "MiroID", sourceId)), label = label)))
       .get
+  }
+
+  it("should insert an identified unified item into Elasticsearch") {
+
+    val identifiedUnifiedItemString =
+      identifiedUnifiedItemJson("5678", "1234", "some label")
 
     val future = identifiedUnifiedItemIndexer.indexIdentifiedUnifiedItem(
       identifiedUnifiedItemString)
@@ -44,9 +52,29 @@ class IdentifiedUnifiedItemIndexerTest
 
   }
 
+  it("should add only one record when multiple records with same id are ingested") {
+    val identifiedUnifiedItemString =
+      identifiedUnifiedItemJson("5678", "1234", "some label")
+
+    val future = Future.sequence(
+      (1 to 2).map(_ => identifiedUnifiedItemIndexer.indexIdentifiedUnifiedItem(
+        identifiedUnifiedItemString))
+    )
+
+    whenReady(future) { _ =>
+      eventually {
+        val hits = elasticClient
+          .execute(search(s"$index/$itemType").matchAll().limit(100))
+          .map { _.hits }
+          .await
+        hits should have size 1
+        hits.head.sourceAsString shouldBe identifiedUnifiedItemString
+      }
+    }
+
+  }
+
   it("should return a failed future if the input string is not an identified unified item") {
-    val identifiedUnifiedItemIndexer =
-      new IdentifiedUnifiedItemIndexer("records", "item", elasticClient)
     val future = identifiedUnifiedItemIndexer.indexIdentifiedUnifiedItem("a document")
 
     whenReady(future.failed) { exception =>
