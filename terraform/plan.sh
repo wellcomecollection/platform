20009cb65f5e613984843cb45a3e544fec4eff33
Robert Kenny <kenoir@users.noreply.github.com>
2017-05-04 10:07:32 +0100

    Get release IDs from S3 (#70)
    * build tfvars release ids file from remote state
    
    * pay attention to variable map in servicies app_uri

--- terraform/plan.sh
+++ terraform/plan.sh
@@ -5,6 +5,7 @@ set -o nounset
 
 # Name of tfvars file
 TF_VARS=terraform.tfvars
+RELEASE_IDS_FILE="release_ids.tfvars"
 
 # Are we running in Travis?  This environment variable is set to "true"
 # in the Travis environment.
@@ -13,32 +14,24 @@ TRAVIS=${TRAVIS:-false}
 
 # Ensure we don't have stale variables from a previous run
 rm -f $TF_VARS
+rm -f $RELEASE_IDS_FILE
+rm -rf releases
 
 echo "Getting variables from S3"
 aws s3 cp s3://platform-infra/terraform.tfvars .
 
-# This uses the ECR API to get the image ID of the most recently pushed
-# image in our ECR repo.  This uses the API repo because every change pushes
-# a new image for _every_ application.
-latest_image_id="$(
-    aws ecr describe-images --repository-name uk.ac.wellcome/api |
-    jq -r '.imageDetails | max_by(.imagePushedAt) | .imageTags[0]')"
+# Download releases from S3
+mkdir releases
+aws s3 cp s3://platform-infra/releases releases --recursive
 
-if ! grep -q "$latest_image_id" terraform.tfvars
-then
-    echo ""
-    echo "WARNING! You may be about to downgrade to an older version of the API."
-    echo "The latest ECR image ID is $latest_image_id."
-    echo "This does not match the image ID in your terraform.tfvars file."
-    echo ""
-    echo "To continue, type 'downgrade'."
-    read resp
-    if [[ "$resp" != "downgrade" ]]
-    then
-        echo "Aborting..."
-        exit 1
-    fi
-fi
+# Build a tfvars file containing the release ids
+echo "release_ids = {" > "$RELEASE_IDS_FILE"
+for f in releases/*;
+do
+  echo "Processing $f: $(cat $f)"
+  echo "  $(basename $f) = \"$(cat $f)\"" >> "$RELEASE_IDS_FILE"
+done
+echo "}" >> "$RELEASE_IDS_FILE"
 
 terraform init
 terraform get
@@ -50,6 +43,6 @@ then
     terraform init -force-copy -lock=false
 fi
 
-terraform plan -out terraform.plan
+terraform plan -var-file="$RELEASE_IDS_FILE" -out terraform.plan
 
 echo "Please review the above plan. If you are happy then run 'terraform apply terraform.plan'"
