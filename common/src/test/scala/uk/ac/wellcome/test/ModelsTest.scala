12ce73840bbe6db10c9b8f44eab389e09bf73cd1
Alex Chan <a.chan@wellcome.ac.uk>
2017-05-05 17:07:35 +0100

    Don't include null or empty fields in serialized JSON

---
+++ common/src/test/scala/uk/ac/wellcome/test/ModelsTest.scala
@@ -0,0 +1,31 @@
+package uk.ac.wellcome.platform.common
+
+import org.scalatest.{FunSpec, Matchers}
+
+import uk.ac.wellcome.models.Work
+import uk.ac.wellcome.utils.JsonUtil
+
+
+class WorkTest extends FunSpec with Matchers {
+  it("should have an LD type 'Work' when serialised to JSON") {
+    val work = Work(
+      identifiers=List(),
+      label="A book about a blue whale"
+    )
+    val jsonString = JsonUtil.toJson(work).get
+
+    jsonString.contains("""type":"Work"""") should be (true)
+  }
+
+  it("should not include fields where the value is empty or None") {
+    val work = Work(
+      identifiers=List(),
+      label="A haiku about a heron",
+      accessStatus=None
+    )
+    val jsonString = JsonUtil.toJson(work).get
+
+    jsonString.contains(""""accessStatus":null""") should be (false)
+    jsonString.contains(""""identifiers":[]""") should be (false)
+  }
+}
