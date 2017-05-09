b1c3eb9a118ee31e4bc4c559423a6cbd55ed66ad
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-05-09 14:42:48 +0100

    adds miro sync user and permissions (#76)

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,6 +7,11 @@ resource "aws_s3_bucket" "miro-data" {
   }
 }
 
+resource "aws_s3_bucket" "miro-images-sync" {
+  bucket = "miro-images-sync"
+  acl    = "private"
+}
+
 resource "aws_s3_bucket" "infra" {
   bucket = "${var.infra_bucket}"
   acl    = "private"
