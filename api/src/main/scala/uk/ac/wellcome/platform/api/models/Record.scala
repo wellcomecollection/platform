58d33171cabffc32093f495b5647fc0e685ee549
caylaa <alicefuzier@gmail.com>
2017-05-09 14:05:42 +0100

    Use get instead of search for the id lookup endpoint

--- api/src/main/scala/uk/ac/wellcome/platform/api/models/Record.scala
+++ api/src/main/scala/uk/ac/wellcome/platform/api/models/Record.scala
@@ -2,6 +2,7 @@ package uk.ac.wellcome.platform.api.models
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.sksamuel.elastic4s.searches.RichSearchHit
+import org.elasticsearch.action.get.GetResponse
 import uk.ac.wellcome.models.{IdentifiedWork, Work}
 import uk.ac.wellcome.utils.JsonUtil
 
@@ -12,8 +13,16 @@ case class Record(
 )
 case object Record {
   def apply(hit: RichSearchHit): Record = {
+    toRecord(hit.sourceAsString)
+  }
+
+  def apply(got: GetResponse): Record = {
+    toRecord(got.getSourceAsString)
+  }
+
+  private def toRecord(document: String) = {
     val identifiedWork =
-      JsonUtil.fromJson[IdentifiedWork](hit.sourceAsString).get
+      JsonUtil.fromJson[IdentifiedWork](document).get
 
     Record(
       id = identifiedWork.canonicalId,
