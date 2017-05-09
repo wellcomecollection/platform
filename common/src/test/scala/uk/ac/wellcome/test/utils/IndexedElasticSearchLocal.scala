8caf83a41ddaf963537d38efe6c59f72f3e7142f
caylaa <alicefuzier@gmail.com>
2017-05-09 10:31:28 +0100

    Fix some more issues with tests starting up and cleanup in between tests

--- common/src/test/scala/uk/ac/wellcome/test/utils/IndexedElasticSearchLocal.scala
+++ common/src/test/scala/uk/ac/wellcome/test/utils/IndexedElasticSearchLocal.scala
@@ -1,34 +1,21 @@
 package uk.ac.wellcome.test.utils
 
 import com.sksamuel.elastic4s.ElasticDsl._
-import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, Suite}
+import org.scalatest.{BeforeAndAfterEach, Suite}
 import uk.ac.wellcome.elasticsearch.mappings.WorksIndex
 import uk.ac.wellcome.utils.GlobalExecutionContext.context
 
 trait IndexedElasticSearchLocal
     extends ElasticSearchLocal
-    with BeforeAndAfterEach
-    with BeforeAndAfterAll { this: Suite =>
+    with BeforeAndAfterEach { this: Suite =>
 
   val indexName = "records"
   val itemType = "item"
 
-  override def beforeAll(): Unit = {
-    elasticClient
-      .execute(indexExists(indexName))
-      .map { result =>
-        if (result.isExists) elasticClient.execute(deleteIndex(indexName))
-      }
-
-    eventually {
-      elasticClient
-        .execute(indexExists(indexName)).await.isExists should be (false)
-    }
-    new WorksIndex(elasticClient, indexName, itemType).create.await
-  }
-
   override def beforeEach(): Unit = {
-    elasticClient.execute(deleteIn(indexName).by(matchAllQuery())).await
-    super.beforeEach()
+    ensureIndexDeleted(indexName)
+    new WorksIndex(elasticClient, indexName, itemType).create.map { _ =>
+      elasticClient.execute(indexExists(indexName)).await.isExists should be(true)
+    }.await
   }
 }
