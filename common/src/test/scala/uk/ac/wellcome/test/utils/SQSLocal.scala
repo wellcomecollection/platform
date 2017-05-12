d9a3ae0174847f9b913d00461fa9adebc1d0d745
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-05-12 17:25:39 +0100

    Create functional test for the ingestor to check that when processing a message fails, it ends up in the dead letter queue

--- common/src/test/scala/uk/ac/wellcome/test/utils/SQSLocal.scala
+++ common/src/test/scala/uk/ac/wellcome/test/utils/SQSLocal.scala
@@ -2,17 +2,18 @@ package uk.ac.wellcome.test.utils
 
 import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
-import com.amazonaws.services.sqs.model.PurgeQueueRequest
+import com.amazonaws.services.sqs.model.{ListQueuesResult, PurgeQueueRequest}
 import com.amazonaws.services.sqs.{AmazonSQS, AmazonSQSClientBuilder}
-import com.google.inject.{Provides, Singleton}
-import com.twitter.inject.TwitterModule
 import org.scalatest.concurrent.{Eventually, IntegrationPatience}
-import org.scalatest.{BeforeAndAfterEach, Suite}
+import org.scalatest.{BeforeAndAfterEach, Matchers, Suite}
+
+import scala.collection.JavaConversions._
 
 trait SQSLocal
     extends BeforeAndAfterEach
     with Eventually
-    with IntegrationPatience { this: Suite =>
+    with IntegrationPatience
+    with Matchers { this: Suite =>
 
   val sqsClient: AmazonSQS = AmazonSQSClientBuilder
     .standard()
@@ -22,20 +23,38 @@ trait SQSLocal
       new EndpointConfiguration(s"http://localhost:9324", "localhost"))
     .build()
 
+  // Use eventually to allow some time for the local SQS to start up.
+  // If it is not started all suites using this will crash at start up time.
+  eventually {
+    sqsClient.listQueues() shouldBe a[ListQueuesResult]
+  }
+
   private var queueUrls: List[String] = Nil
 
-  def createQueueAndReturnUrl(queueName: String): String = {
-    // Use eventually to allow some time for the local SQS to start up.
-    // If it is not started all suites using this will crash at start up time.
-    val queueUrl = eventually {
-      sqsClient.createQueue(queueName).getQueueUrl
-    }
+  def createQueueAndDlqReturnUrls(queueName: String): QueueInfo = {
+    val deadLetterQueueUrl = sqsClient.createQueue(queueName).getQueueUrl
+    val queueUrl = sqsClient.createQueue(s"$queueName-dlq").getQueueUrl
+    val deadLetterQueueArn = sqsClient
+      .getQueueAttributes(deadLetterQueueUrl, List("QueueArn"))
+      .getAttributes
+      .get("QueueArn")
+    val redrivePolicy =
+      s"""
+        |{
+        | "maxReceiveCount":"1",
+        | "deadLetterTargetArn":"$deadLetterQueueArn"
+        |}""".stripMargin
+    sqsClient.setQueueAttributes(queueUrl,
+                                 Map("RedrivePolicy" -> redrivePolicy))
     queueUrls = queueUrl :: queueUrls
-    queueUrl
+    QueueInfo(queueUrl, deadLetterQueueUrl)
   }
 
   override def beforeEach(): Unit = {
     super.beforeEach()
-    queueUrls.foreach(queueUrl => sqsClient.purgeQueue(new PurgeQueueRequest().withQueueUrl(queueUrl)))
+    queueUrls.foreach(queueUrl =>
+      sqsClient.purgeQueue(new PurgeQueueRequest().withQueueUrl(queueUrl)))
   }
 }
+
+case class QueueInfo(queueUrl: String, deadLetterQueueUrl: String)
