9409ab636a24b2121c3200291086e4f27101af99
caylaa <alicefuzier@gmail.com>
2017-05-12 10:45:17 +0100

    Optimize imports

--- common/src/test/scala/uk/ac/wellcome/test/utils/StartupLogbackOverride.scala
+++ common/src/test/scala/uk/ac/wellcome/test/utils/StartupLogbackOverride.scala
@@ -2,10 +2,7 @@ package uk.ac.wellcome.test.utils
 
 import ch.qos.logback.classic.LoggerContext
 import ch.qos.logback.classic.joran.JoranConfigurator
-import org.apache.commons.io.IOUtils
 import org.slf4j.LoggerFactory
-
-import scala.tools.nsc.classpath.FileUtils
 
 trait StartupLogbackOverride {
   val loggerContext =
