e070915e753b075f5d1432d9195e1e85950e4310
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-05-12 09:39:33 +0100

    Increase timeout for elastic search to start. 15 seconds seems to often not be enough on my local machine

---
+++ common/src/test/scala/uk/ac/wellcome/test/utils/ExtendedPatience.scala
@@ -0,0 +1,12 @@
+package uk.ac.wellcome.test.utils
+
+import org.scalatest.concurrent.PatienceConfiguration
+import org.scalatest.time.{Millis, Seconds, Span}
+
+trait ExtendedPatience extends PatienceConfiguration{
+
+  override implicit val patienceConfig = PatienceConfig(
+    timeout = scaled(Span(40, Seconds)),
+    interval = scaled(Span(150, Millis))
+  )
+}
