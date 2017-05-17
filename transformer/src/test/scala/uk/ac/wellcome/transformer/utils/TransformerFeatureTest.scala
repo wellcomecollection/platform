9170221cc531dd0283688b3534e00d385b073e7f
caylaa <alicefuzier@gmail.com>
2017-05-17 11:14:33 +0100

    Refactor TransformerFeatureTests so we have less code repetition

--- transformer/src/test/scala/uk/ac/wellcome/transformer/utils/TransformerFeatureTest.scala
+++ transformer/src/test/scala/uk/ac/wellcome/transformer/utils/TransformerFeatureTest.scala
@@ -1,11 +1,22 @@
 package uk.ac.wellcome.transformer.utils
 
 import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
+import com.amazonaws.services.cloudwatch.{
+  AmazonCloudWatch,
+  AmazonCloudWatchClientBuilder
+}
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.streamsadapter.AmazonDynamoDBStreamsAdapterClient
+import com.amazonaws.services.kinesis.AmazonKinesis
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker.KinesisClientLibConfiguration
 import com.amazonaws.services.kinesis.metrics.interfaces.MetricsLevel
+import com.amazonaws.services.sns.AmazonSNS
+import com.twitter.finatra.http.EmbeddedHttpServer
 import com.twitter.inject.server.FeatureTestMixin
 import org.scalatest.Suite
 import org.scalatest.concurrent.Eventually
+import uk.ac.wellcome.platform.transformer.Server
 import uk.ac.wellcome.test.utils.{DynamoDBLocal, ExtendedPatience, SNSLocal}
 
 trait TransformerFeatureTest
@@ -14,6 +25,18 @@ trait TransformerFeatureTest
     with Eventually
     with SNSLocal
     with DynamoDBLocal { this: Suite =>
+  val flags: Map[String, String]
+  val kinesisClientLibConfiguration: KinesisClientLibConfiguration
+
+  lazy val server: EmbeddedHttpServer =
+    new EmbeddedHttpServer(
+      new Server(),
+      flags
+    ).bind[AmazonSNS](amazonSNS)
+      .bind[AmazonDynamoDB](dynamoDbClient)
+      .bind[AmazonCloudWatch](amazonCloudWatch)
+      .bind[AmazonKinesis](new AmazonDynamoDBStreamsAdapterClient(streamsClient))
+      .bind[KinesisClientLibConfiguration](kinesisClientLibConfiguration)
 
   val idMinterTopicArn: String = createTopicAndReturnArn("test_id_minter")
 
@@ -28,4 +51,11 @@ trait TransformerFeatureTest
       java.util.UUID.randomUUID.toString)
     //turn off metric logging in tests so we don't see error logs about not being able to publish to cloudwatch
       .withMetricsLevel(MetricsLevel.NONE)
+
+  private val amazonCloudWatch = AmazonCloudWatchClientBuilder
+    .standard()
+    .withEndpointConfiguration(
+      // use a fake endpoind in tests so that we don't send metrics to the real AWS
+      new EndpointConfiguration("http://localhost:6789", "local"))
+    .build()
 }
