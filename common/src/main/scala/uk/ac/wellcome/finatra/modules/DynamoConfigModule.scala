7ec7334a76119b99aa4eaf96569252e25ae3366a
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-05-18 16:37:07 +0100

    Use Binding annotation to avoid multiple item od same type DI issue

--- common/src/main/scala/uk/ac/wellcome/finatra/modules/DynamoConfigModule.scala
+++ common/src/main/scala/uk/ac/wellcome/finatra/modules/DynamoConfigModule.scala
@@ -7,6 +7,23 @@ import com.twitter.app.Flag
 import com.twitter.inject.TwitterModule
 import uk.ac.wellcome.models.aws.DynamoConfig
 
+import com.google.inject.BindingAnnotation
+import java.lang.annotation.Target
+import java.lang.annotation.Retention
+import java.lang.annotation.ElementType.PARAMETER
+import java.lang.annotation.RetentionPolicy.RUNTIME
+
+@BindingAnnotation
+@Target(Array(PARAMETER))
+@Retention(RUNTIME) class MiroDynamoConfig {}
+
+@BindingAnnotation
+@Target(Array(PARAMETER))
+@Retention(RUNTIME) class IdentifierDynamoConfig {}
+
+@BindingAnnotation
+@Target(Array(PARAMETER))
+@Retention(RUNTIME) class CalmDynamoConfig {}
 
 trait DynamoConfigModule extends TwitterModule {
   val tableName: String
@@ -15,12 +32,7 @@ trait DynamoConfigModule extends TwitterModule {
   val arn: Flag[String]
   val table: Flag[String]
 
-  @Singleton
-  @Provides
-  def providesDynamoConfig(): DynamoConfig =
-    DynamoConfig(applicationName(), arn(), table())
-
-  val applicationNameTpl: String  = "aws.dynamo.%s.streams.appName"
+  val applicationNameTpl: String = "aws.dynamo.%s.streams.appName"
   val arnTpl: String = "aws.dynamo.%s.streams.arn"
   val tableTpl: String = "aws.dynamo.%s.tableName"
 
@@ -32,24 +44,41 @@ trait DynamoConfigModule extends TwitterModule {
 object MiroTableDynamoConfigModule extends DynamoConfigModule {
   val tableName = "miroData"
 
-  val applicationName = flag[String](appNameFlagId, "", "Name of the Kinesis app")
+  val applicationName =
+    flag[String](appNameFlagId, "", "Name of the Kinesis app")
   val arn = flag[String](arnFlagId, "", "ARN of the DynamoDB stream")
   val table = flag[String](tableFlagId, "", "Name of the DynamoDB table")
+
+  @Singleton
+  @Provides
+  @MiroDynamoConfig
+  def providesDynamoConfig(): DynamoConfig =
+    DynamoConfig(applicationName(), arn(), table())
 }
 
 object IdentifierTableDynamoConfigModule extends DynamoConfigModule {
   val tableName = "identifiers"
 
-  val applicationName = flag[String](appNameFlagId, "", "Name of the Kinesis app")
+  val applicationName =
+    flag[String](appNameFlagId, "", "Name of the Kinesis app")
   val arn = flag[String](arnFlagId, "", "ARN of the DynamoDB stream")
   val table = flag[String](tableFlagId, "", "Name of the DynamoDB table")
+
+  @Singleton
+  @Provides
+  @IdentifierDynamoConfig
+  def providesDynamoConfig(): DynamoConfig =
+    DynamoConfig(applicationName(), arn(), table())
 }
 
 object CalmTableDynamoConfigModule extends DynamoConfigModule {
   val tableName = "calmData"
 
-  val applicationName = flag[String](appNameFlagId, "", "Name of the Kinesis app")
+  @Singleton
+  @Provides
+  @CalmDynamoConfig
+  val applicationName =
+    flag[String](appNameFlagId, "", "Name of the Kinesis app")
   val arn = flag[String](arnFlagId, "", "ARN of the DynamoDB stream")
   val table = flag[String](tableFlagId, "", "Name of the DynamoDB table")
 }
-
