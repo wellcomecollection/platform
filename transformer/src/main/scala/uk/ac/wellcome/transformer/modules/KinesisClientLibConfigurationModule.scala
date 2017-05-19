92c00db001307ec495faa63faea537cd5c99bb79
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-05-19 11:39:45 +0100

    remove unused annotations

--- transformer/src/main/scala/uk/ac/wellcome/transformer/modules/KinesisClientLibConfigurationModule.scala
+++ transformer/src/main/scala/uk/ac/wellcome/transformer/modules/KinesisClientLibConfigurationModule.scala
@@ -3,16 +3,11 @@ package uk.ac.wellcome.platform.transformer.modules
 import javax.inject.Singleton
 
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
-import com.amazonaws.services.kinesis.clientlibrary.interfaces._
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker._
 import com.google.inject.Provides
 import com.twitter.inject.TwitterModule
 import uk.ac.wellcome.finatra.annotations.{CalmDynamoConfig, MiroDynamoConfig}
 import uk.ac.wellcome.models.aws.DynamoConfig
-import uk.ac.wellcome.platform.transformer.annotations.{
-  CalmKinesisClientLibConfiguration,
-  MiroKinesisClientLibConfiguration
-}
 
 object KinesisClientLibConfigurationModule extends TwitterModule {
 
