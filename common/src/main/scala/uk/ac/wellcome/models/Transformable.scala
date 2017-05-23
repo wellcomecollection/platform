9cf9359f288674b65f1b2911b480a4c7900c2d8c
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-05-23 15:29:43 +0100

    update reindex version as necessary

--- common/src/main/scala/uk/ac/wellcome/models/Transformable.scala
+++ common/src/main/scala/uk/ac/wellcome/models/Transformable.scala
@@ -6,7 +6,24 @@ trait Transformable {
   def transform: Try[Work]
 }
 
-trait Reindexable {
+case class HashKey(keyName: String, keyValue: String)
+case class RangeKey[T](keyName: String, keyValue: T)
+case class ItemIdentifier[T](hashKey: HashKey, rangeKey: RangeKey[T])
+
+trait Reindexable[T] {
+  val id: ItemIdentifier[T]
   val ReindexShard: String
   val ReindexVersion: Int
+
+  def getReindexItem: ReindexItem[T] =
+    ReindexItem(id, ReindexShard, ReindexVersion)
+}
+
+case class ReindexItem[T](id: ItemIdentifier[T],
+                          ReindexShard: String,
+                          ReindexVersion: Int)
+    extends Reindexable[T] {
+
+  def hashKey = Symbol(id.hashKey.keyName) -> id.hashKey.keyValue
+  def rangeKey = Symbol(id.rangeKey.keyName) -> id.rangeKey.keyValue
 }
