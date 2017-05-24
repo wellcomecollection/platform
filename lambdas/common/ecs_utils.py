9780149b2236a14a818ddaf30ec2e262bd2231bb
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-05-24 17:53:40 +0100

    Upgrade config in a more graceful way (#87)
    * Restart an application for new config in a graceful way
    
    * Fail the Lambda if the update fails
    
    * Remove an unused import
    
    * Tidy up the config change script
    
    * Update IAM config for graceful config restart
    
    * Remove the old Lambda

--- lambdas/common/ecs_utils.py
+++ lambdas/common/ecs_utils.py
@@ -1,5 +1,7 @@
 # -*- encoding: utf-8 -*-
 
+import operator
+
 
 def identify_cluster_by_app_name(client, app_name):
     """
@@ -22,3 +24,66 @@ def identify_cluster_by_app_name(client, app_name):
                 return cluster
 
     raise RuntimeError(f'Unable to find ECS cluster for {app_name}')
+
+
+def get_latest_task_definition(client, cluster, service):
+    """
+    Given the name of a cluster and a service, return the ARN
+    for its latest task definition.
+    """
+    resp = client.describe_services(cluster=cluster, services=[service])
+
+    # The top-level structure of a describeServices API response is of the form
+    #
+    #     {
+    #       "failures": [],
+    #       "services": [
+    #         ...
+    #       ]
+    #     }
+    #
+    # Because we only asked for a description of a single service, we expect
+    # there to only be a single service.
+    services = resp['services']
+    assert len(services) == 1, resp
+    service = services[0]
+
+    # Within a 'service' description, the following structure is what we're
+    # interested in:
+    #
+    #     "deployments": [
+    #       {
+    #         "createdAt": <date>,
+    #         "taskDefinition": <task definition ARN>,
+    #         "updatedAt": <date>
+    #         ...
+    #       },
+    #       ... other running tasks
+    #     ],
+    #
+    # Each "deployment" corresponds to a running task, so we pick the
+    # container with the most recent creation date.
+    deployments = service['deployments']
+    assert len(deployments) > 0, resp
+    newest_deployment = max(deployments, key=operator.itemgetter('createdAt'))
+
+    return newest_deployment['taskDefinition']
+
+
+def clone_task_definition(client, task_definition):
+    """
+    Given a task definition ARN, clone the associated task.
+
+    Returns the new task definition ARN.
+    """
+    resp = client.describe_task_definition(taskDefinition=task_definition)
+    taskDefinition = resp['taskDefinition']
+
+    # The task definition contains two key fields: "family" and
+    # "containerDefinitions" which full describe the task.
+    new_task = client.register_task_definition(
+        family=taskDefinition['family'],
+        containerDefinitions=taskDefinition['containerDefinitions']
+    )
+
+    return new_task['taskDefinition']['taskDefinitionArn']
---
+++ lambdas/update_task_for_config_change/ecs_utils.py
@@ -0,0 +1,1 @@
+../common/ecs_utils.py
