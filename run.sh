e01e4ecfa10f2844101da7b806ce2403a8e43f38
Alex Chan <a.chan@wellcome.ac.uk>
2017-05-24 09:03:02 +0100

    Log the contents of config from S3

--- run.sh
+++ run.sh
@@ -9,5 +9,9 @@ echo "Fetching config from AWS..."
 aws s3 ls s3://$CONFIG_BUCKET/config/$BUILD_ENV/$PROJECT.ini
 aws s3 cp s3://$CONFIG_BUCKET/config/$BUILD_ENV/$PROJECT.ini /opt/docker/conf/application.ini
 
+echo "=== config ==="
+cat /opt/docker/conf/application.ini
+echo "=============="
+
 echo "Starting Java binary..."
 /opt/docker/bin/$PROJECT
