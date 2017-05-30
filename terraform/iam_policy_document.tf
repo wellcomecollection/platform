8d9e0ad8c5a31ab3505f5737ffb63a670fdc59c0
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-05-30 14:23:32 +0100

    Adds permissions for miro reindexer to update tracker and target table

--- terraform/iam_policy_document.tf
+++ terraform/iam_policy_document.tf
@@ -25,6 +25,30 @@ data "aws_iam_policy_document" "allow_calm_db_all" {
   }
 }
 
+data "aws_iam_policy_document" "reindex_tracker_table" {
+  statement {
+    actions = [
+      "dynamodb:*",
+    ]
+
+    resources = [
+      "${aws_dynamodb_table.reindex_tracker.arn}",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "reindex_target_miro" {
+  statement {
+    actions = [
+      "dynamodb:*",
+    ]
+
+    resources = [
+      "${aws_dynamodb_table.miro_table.arn}/index/ReindexTracker",
+    ]
+  }
+}
+
 data "aws_iam_policy_document" "allow_cloudwatch_push_metrics" {
   statement {
     actions = [
