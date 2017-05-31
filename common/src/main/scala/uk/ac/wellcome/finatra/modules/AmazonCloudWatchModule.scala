0356605908d6b1d2790bd36b998589eefd9f66e5
caylaa <alicefuzier@gmail.com>
2017-05-31 10:04:52 +0100

    Formatting

--- common/src/main/scala/uk/ac/wellcome/finatra/modules/AmazonCloudWatchModule.scala
+++ common/src/main/scala/uk/ac/wellcome/finatra/modules/AmazonCloudWatchModule.scala
@@ -1,12 +1,14 @@
 package uk.ac.wellcome.finatra.modules
 
-import com.amazonaws.services.cloudwatch.{AmazonCloudWatch, AmazonCloudWatchClientBuilder}
+import com.amazonaws.services.cloudwatch.{
+  AmazonCloudWatch,
+  AmazonCloudWatchClientBuilder
+}
 import com.google.inject.{Provides, Singleton}
 import com.twitter.inject.TwitterModule
 import uk.ac.wellcome.models.aws.AWSConfig
 
-object
-AmazonCloudWatchModule extends TwitterModule {
+object AmazonCloudWatchModule extends TwitterModule {
 
   @Provides
   @Singleton
