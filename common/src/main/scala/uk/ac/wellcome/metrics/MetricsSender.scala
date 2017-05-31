a7cff6fd33bbbe9c53dc92d29444348d13e4824d
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-05-31 18:16:33 +0100

    Fail safe recording metrics

--- common/src/main/scala/uk/ac/wellcome/metrics/MetricsSender.scala
+++ common/src/main/scala/uk/ac/wellcome/metrics/MetricsSender.scala
@@ -6,6 +6,7 @@ import com.amazonaws.services.cloudwatch.AmazonCloudWatch
 import com.amazonaws.services.cloudwatch.model._
 import com.google.inject.Inject
 import com.twitter.inject.annotations.Flag
+import com.twitter.inject.Logging
 import uk.ac.wellcome.utils.GlobalExecutionContext.context
 
 import scala.collection.JavaConversions._
@@ -14,30 +15,38 @@ import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
 class MetricsSender @Inject()(@Flag("aws.metrics.namespace") namespace: String,
-                              amazonCloudWatch: AmazonCloudWatch) {
+                              amazonCloudWatch: AmazonCloudWatch)
+    extends Logging {
 
   def timeAndCount[T](metricName: String, fun: () => Future[T]): Future[T] = {
     val start = new Date()
     val future = Future.successful(()).flatMap(_ => fun())
-    future.onComplete {
-      case Success(_) =>
-        val end = new Date()
-        incrementCount("success")
-        sendTime(metricName,
-          (end.getTime - start.getTime) milliseconds,
-          Map("success" -> "true"))
+    try {
+      future.onComplete {
+        case Success(_) =>
+          val end = new Date()
+          incrementCount("success")
+          sendTime(metricName,
+                   (end.getTime - start.getTime) milliseconds,
+                   Map("success" -> "true"))
 
-      case Failure(_) =>
-        val end = new Date()
-        incrementCount("failure")
-        sendTime(metricName,
-          (end.getTime - start.getTime) milliseconds,
-          Map("success" -> "false"))
+        case Failure(_) =>
+          val end = new Date()
+          incrementCount("failure")
+          sendTime(metricName,
+                   (end.getTime - start.getTime) milliseconds,
+                   Map("success" -> "false"))
+
+      }
+    } catch {
+      case e: RuntimeException => error("Failed to report metrics!", e)
     }
     future
   }
 
-  def incrementCount(metricName: String): PutMetricDataResult = {
+  def incrementCount(metricName: String,
+                     count: Double = 1.0): PutMetricDataResult = {
+
     amazonCloudWatch.putMetricData(
       new PutMetricDataRequest()
         .withNamespace(namespace)
