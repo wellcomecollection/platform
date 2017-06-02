85d063b178385e71de64a24917dc002584122dc3
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-02 11:12:49 +0100

    Ensures reindexer doesn't retry when successful

--- reindexer/src/test/scala/uk/ac/wellcome/platform/reindexer/modules/ReindexModuleTest.scala
+++ reindexer/src/test/scala/uk/ac/wellcome/platform/reindexer/modules/ReindexModuleTest.scala
@@ -3,21 +3,20 @@ package uk.ac.wellcome.platform.reindexer.modules
 import com.gu.scanamo.Scanamo
 import com.gu.scanamo.syntax._
 import com.twitter.finatra.http.EmbeddedHttpServer
-import org.mockito.Mockito.when
-import org.scalatest.concurrent.{Eventually, IntegrationPatience}
+import org.scalatest.concurrent.Eventually
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{FunSpec, Matchers}
 import uk.ac.wellcome.models.aws.DynamoConfig
 import uk.ac.wellcome.models.{CalmTransformable, Reindex}
 import uk.ac.wellcome.platform.reindexer.Server
 import uk.ac.wellcome.platform.reindexer.models.ReindexAttempt
-import uk.ac.wellcome.platform.reindexer.services.{CalmReindexTargetService, ReindexTargetService}
+import uk.ac.wellcome.platform.reindexer.services.{CalmReindexTargetService, ReindexService, ReindexTargetService}
 import uk.ac.wellcome.test.utils.{AmazonCloudWatchFlag, DynamoDBLocal, ExtendedPatience}
 
 import scala.concurrent.Future
 
 class ReindexModuleTest
-    extends FunSpec
+  extends FunSpec
     with MockitoSugar
     with DynamoDBLocal
     with Eventually
@@ -25,28 +24,30 @@ class ReindexModuleTest
     with AmazonCloudWatchFlag
     with Matchers {
 
-  val reindexTargetService = mock[CalmReindexTargetService]
-  val server: EmbeddedHttpServer =
-    new EmbeddedHttpServer(
-      new Server(),
-      flags = Map(
-        "aws.dynamo.reindexTracker.tableName" -> "ReindexTracker",
-        "aws.dynamo.calmData.tableName" -> "CalmData",
-        "reindex.target.tableName" -> "CalmData"
-      ) ++ cloudWatchLocalEndpointFlag ++ dynamoDbLocalEndpointFlags
-    ).bind[ReindexTargetService[CalmTransformable]](reindexTargetService)
+  import org.mockito.Mockito._
 
   val currentVersion = 1
   val requestedVersion = 2
 
   val dynamoConfigs = Map(
     "reindex" -> DynamoConfig("applicationName",
-                              "streamArn",
-                              reindexTableName),
+      "streamArn",
+      reindexTableName),
     "calm" -> DynamoConfig("applicationName", "streamArn", calmDataTableName)
   )
 
   it("should retry if the target service returns a failed future") {
+    val reindexTargetService = mock[CalmReindexTargetService]
+    val server: EmbeddedHttpServer =
+      new EmbeddedHttpServer(
+        new Server(),
+        flags = Map(
+          "aws.dynamo.reindexTracker.tableName" -> "ReindexTracker",
+          "aws.dynamo.calmData.tableName" -> "CalmData",
+          "reindex.target.tableName" -> "CalmData"
+        ) ++ cloudWatchLocalEndpointFlag ++ dynamoDbLocalEndpointFlags
+      ).bind[ReindexTargetService[CalmTransformable]](reindexTargetService)
+
     val reindex = Reindex(calmDataTableName, requestedVersion, currentVersion)
     Scanamo.put(dynamoDbClient)(reindexTableName)(reindex)
 
@@ -62,6 +63,34 @@ class ReindexModuleTest
       Scanamo.get[Reindex](dynamoDbClient)(reindexTableName)(
         'TableName -> "CalmData") shouldBe Some(
         Right(reindex.copy(CurrentVersion = requestedVersion)))
+    }
+
+    server.close()
+  }
+
+  it("should call reindexService.run only once if successful") {
+    val reindexService = mock[ReindexService[CalmTransformable]]
+    val server: EmbeddedHttpServer =
+      new EmbeddedHttpServer(
+        new Server(),
+        flags = Map(
+          "aws.dynamo.reindexTracker.tableName" -> "ReindexTracker",
+          "aws.dynamo.calmData.tableName" -> "CalmData",
+          "reindex.target.tableName" -> "CalmData"
+        ) ++ cloudWatchLocalEndpointFlag ++ dynamoDbLocalEndpointFlags
+      ).bind[ReindexService[CalmTransformable]](reindexService)
+
+    val reindex = Reindex(calmDataTableName, requestedVersion, currentVersion)
+    val reindexAttempt = ReindexAttempt(reindex, Nil, 0)
+
+    Scanamo.put(dynamoDbClient)(reindexTableName)(reindex)
+
+    when(reindexService.run).thenReturn(Future.successful(()))
+
+    server.start()
+
+    eventually {
+      verify(reindexService, times(1)).run
     }
 
     server.close()
