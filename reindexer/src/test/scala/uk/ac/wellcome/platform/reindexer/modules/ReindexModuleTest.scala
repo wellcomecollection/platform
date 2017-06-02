c6758e34655dc35a32b1d64276341a2ece959977
caylaa <alicefuzier@gmail.com>
2017-06-02 15:52:40 +0100

    Better test cleanup

--- reindexer/src/test/scala/uk/ac/wellcome/platform/reindexer/modules/ReindexModuleTest.scala
+++ reindexer/src/test/scala/uk/ac/wellcome/platform/reindexer/modules/ReindexModuleTest.scala
@@ -3,50 +3,57 @@ package uk.ac.wellcome.platform.reindexer.modules
 import com.gu.scanamo.Scanamo
 import com.gu.scanamo.syntax._
 import com.twitter.finatra.http.EmbeddedHttpServer
+import org.mockito.Mockito._
 import org.scalatest.concurrent.Eventually
 import org.scalatest.mockito.MockitoSugar
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.{BeforeAndAfterEach, FunSpec, Matchers}
 import uk.ac.wellcome.models.aws.DynamoConfig
 import uk.ac.wellcome.models.{CalmTransformable, Reindex}
 import uk.ac.wellcome.platform.reindexer.Server
 import uk.ac.wellcome.platform.reindexer.models.ReindexAttempt
-import uk.ac.wellcome.platform.reindexer.services.{CalmReindexTargetService, ReindexService, ReindexTargetService}
-import uk.ac.wellcome.test.utils.{AmazonCloudWatchFlag, DynamoDBLocal, ExtendedPatience}
+import uk.ac.wellcome.platform.reindexer.services.{
+  CalmReindexTargetService,
+  ReindexService,
+  ReindexTargetService
+}
+import uk.ac.wellcome.test.utils.{
+  AmazonCloudWatchFlag,
+  DynamoDBLocal,
+  ExtendedPatience
+}
 
 import scala.concurrent.Future
 
 class ReindexModuleTest
-  extends FunSpec
+    extends FunSpec
     with MockitoSugar
     with DynamoDBLocal
     with Eventually
     with ExtendedPatience
     with AmazonCloudWatchFlag
-    with Matchers {
-
-  import org.mockito.Mockito._
+    with Matchers
+    with BeforeAndAfterEach {
 
   val currentVersion = 1
   val requestedVersion = 2
 
   val dynamoConfigs = Map(
     "reindex" -> DynamoConfig("applicationName",
-      "streamArn",
-      reindexTableName),
+                              "streamArn",
+                              reindexTableName),
     "calm" -> DynamoConfig("applicationName", "streamArn", calmDataTableName)
   )
+
+  var maybeServer: Option[EmbeddedHttpServer] = None
+
+  override def afterEach(): Unit = maybeServer.foreach(_.close())
 
   it("should retry if the target service returns a failed future") {
     val reindexTargetService = mock[CalmReindexTargetService]
     val server: EmbeddedHttpServer =
-      new EmbeddedHttpServer(
-        new Server(),
-        flags = Map(
-          "aws.dynamo.reindexTracker.tableName" -> "ReindexTracker",
-          "aws.dynamo.calmData.tableName" -> "CalmData",
-          "reindex.target.tableName" -> "CalmData"
-        ) ++ cloudWatchLocalEndpointFlag ++ dynamoDbLocalEndpointFlags
-      ).bind[ReindexTargetService[CalmTransformable]](reindexTargetService)
+      defineServer.bind[ReindexTargetService[CalmTransformable]](
+        reindexTargetService)
+    maybeServer = Some(server)
 
     val reindex = Reindex(calmDataTableName, requestedVersion, currentVersion)
     Scanamo.put(dynamoDbClient)(reindexTableName)(reindex)
@@ -64,21 +71,13 @@ class ReindexModuleTest
         'TableName -> "CalmData") shouldBe Some(
         Right(reindex.copy(CurrentVersion = requestedVersion)))
     }
-
-    server.close()
   }
 
   it("should call reindexService.run only once if successful") {
     val reindexService = mock[ReindexService[CalmTransformable]]
     val server: EmbeddedHttpServer =
-      new EmbeddedHttpServer(
-        new Server(),
-        flags = Map(
-          "aws.dynamo.reindexTracker.tableName" -> "ReindexTracker",
-          "aws.dynamo.calmData.tableName" -> "CalmData",
-          "reindex.target.tableName" -> "CalmData"
-        ) ++ cloudWatchLocalEndpointFlag ++ dynamoDbLocalEndpointFlags
-      ).bind[ReindexService[CalmTransformable]](reindexService)
+      defineServer.bind[ReindexService[CalmTransformable]](reindexService)
+    maybeServer = Some(server)
 
     when(reindexService.run).thenReturn(Future.successful(()))
 
@@ -87,7 +86,16 @@ class ReindexModuleTest
     eventually {
       verify(reindexService, times(1)).run
     }
+  }
 
-    server.close()
+  private def defineServer = {
+    new EmbeddedHttpServer(
+      new Server(),
+      flags = Map(
+        "aws.dynamo.reindexTracker.tableName" -> "ReindexTracker",
+        "aws.dynamo.calmData.tableName" -> "CalmData",
+        "reindex.target.tableName" -> "CalmData"
+      ) ++ cloudWatchLocalEndpointFlag ++ dynamoDbLocalEndpointFlags
+    )
   }
 }
