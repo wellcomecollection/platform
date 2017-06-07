699e53a55a6d1ebb597a2d3dc6b6f808aa8c0a4b
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-07 12:08:04 +0100

    Make sure our config-updater Lambda can pass the IAM role
    When creating a new task definition for one of our services after
    the config changes, we need to be able to copy the IAM role from
    the previous task definition.  That only works if the copying
    Lambda has the "PassRole" permission.

--- terraform/iam_policy_document.tf
+++ terraform/iam_policy_document.tf
@@ -108,6 +108,7 @@ data "aws_iam_policy_document" "stop_running_tasks" {
       "ecs:ListServices",
       "ecs:RegisterTaskDefinition",
       "ecs:UpdateService",
+      "iam:PassRole",
     ]
 
     resources = [
