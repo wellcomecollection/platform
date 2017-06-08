810893e9d6219be9469bb37cadadd7863d2226f8
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-08 11:40:10 +0100

    Adds Metrics to Ingestor

--- ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/StartupTest.scala
+++ ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/StartupTest.scala
@@ -3,12 +3,21 @@ package uk.ac.wellcome.platform.ingestor
 import com.google.inject.Stage
 import com.twitter.finatra.http.EmbeddedHttpServer
 import com.twitter.inject.server.FeatureTest
-import uk.ac.wellcome.test.utils.{SQSLocal, StartupLogbackOverride}
+import uk.ac.wellcome.test.utils.{
+  AmazonCloudWatchFlag,
+  SQSLocal,
+  StartupLogbackOverride
+}
 
-class StartupTest extends FeatureTest with StartupLogbackOverride with SQSLocal {
+class StartupTest
+    extends FeatureTest
+    with StartupLogbackOverride
+    with SQSLocal
+    with AmazonCloudWatchFlag {
 
   val server = new EmbeddedHttpServer(stage = Stage.PRODUCTION,
-                                      twitterServer = new Server, flags = sqsLocalFlags)
+                                      twitterServer = new Server,
+                                      flags = sqsLocalFlags ++ cloudWatchLocalEndpointFlag)
 
   test("server starts up correctly") {
     server.assertHealthy()
