148dc5ef48b9a53cbfc1f7ef0aa83217d213661f
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-09 16:31:06 +0100

    First attempt at adding alarms for dlqs

--- terraform/sqs/sqs.tf
+++ terraform/sqs/sqs.tf
@@ -7,3 +7,18 @@ resource "aws_sqs_queue" "q" {
 resource "aws_sqs_queue" "dlq" {
   name = "${var.queue_name}_dlq"
 }
+
+resource "aws_cloudwatch_metric_alarm" "dlq_not_empty" {
+  alarm_name = "${aws_sqs_queue.dlq.name}_not_empty"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = 1
+  metric_name = "NumberOfMessagesSent"
+  namespace = "SQS"
+  period = 60
+  threshold = 1
+  statistic = "SampleCount"
+
+  dimensions {
+    QueueName = "${aws_sqs_queue.dlq.name}"
+  }
+}
