ad574260932527785aca2d5a77314e5fc491bda4
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-12 17:27:48 +0100

    add brief description and use env variables for s3 target

--- lambdas/update_service_list/update_service_list.py
+++ lambdas/update_service_list/update_service_list.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+"""
+Publish a file containing a summary of ECS service status to S3.
+
+This script runs when triggered from an ECS task state change stream.
+It does not use the event data from the event.
+"""
+
 import boto3
 
 import json
+
+import pprint
 
 
 client = boto3.client('ecs')
@@ -33,15 +42,16 @@ def get_service_names(cluster_name):
     return service_names
 
 
-def describe_service(cluster_name, service_name):
+def describe_services(cluster_name, service_name):
     response = client.describe_services(
         cluster=cluster_name,
         services=[service_name]
     )
+
     return response['services']
 
 
-def create_service_dict(service):
+def _create_service_dict(service):
     return {
         'serviceName': service['serviceName'],
         'desiredCount': service['desiredCount'],
@@ -55,7 +65,7 @@ def get_service_list():
     for cluster_name in get_cluster_names():
         service_list = []
         for service_name in get_service_names(cluster_name):
-            service = describe_service(cluster_name, service_name)
+            service = describe_services(cluster_name, service_name)
             service_list.append(create_service_dict(service))
 
         return service_list
@@ -63,10 +73,17 @@ def get_service_list():
 
 def send_service_list_to_s3():
     client = boto3.client('s3')
+
+    bucket_name = os.environ["BUCKET_NAME"]
+    object_key = os.environ["OBJECT_KEY"]
+
+    json_service_list = get_service_list
+    print(json_service_list)
+
     return client.put_object(
-        Bucket='platform-infra',
-        Key='services.json',
-        Body=json.dumps(get_service_list),
+        Bucket=bucket_name,
+        Key=object_key,
+        Body=json.dumps(json_service_list),
         ContentType='application/json'
     )
 
