47b3d846f16a8ca871ad5b23070c6e1d49c28505
Alex Chan <a.chan@wellcome.ac.uk>
2017-06-14 10:50:22 +0100

    Use the HttpClient in tests as well

--- common/src/test/scala/uk/ac/wellcome/test/utils/ElasticSearchLocal.scala
+++ common/src/test/scala/uk/ac/wellcome/test/utils/ElasticSearchLocal.scala
@@ -2,7 +2,7 @@ package uk.ac.wellcome.test.utils
 
 import com.sksamuel.elastic4s.ElasticDsl._
 import com.sksamuel.elastic4s.ElasticsearchClientUri
-import com.sksamuel.elastic4s.xpack.security.XPackElasticClient
+import com.sksamuel.elastic4s.http.HttpClient
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse
 import org.elasticsearch.common.settings.Settings
 import org.scalatest.concurrent.{Eventually, IntegrationPatience}
@@ -22,8 +22,7 @@ trait ElasticSearchLocal
     .put("xpack.security.user", "elastic:changeme")
     .build()
 
-  val elasticClient =
-    XPackElasticClient(settings, ElasticsearchClientUri("localhost", 9300))
+  val elasticClient = HttpClient(ElasticsearchClientUri("localhost", 9300))
 
   // Elasticsearch takes a while to start up so check that it actually started before running tests
   eventually {
