135ade47851d1f1367d502fa9be2b35a95ee2f8d
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-15 21:47:24 +0100

    remove service worker (was caching status), add error when status unavailable

--- dashboard/service-status/src/ServiceList.js
+++ dashboard/service-status/src/ServiceList.js
@@ -3,23 +3,28 @@
 import React, { Component } from 'react';
 import type { Services } from './models'
 import ServiceBox from './ServiceBox'
+import ErrorBox from './ErrorBox'
 import './ServiceList.css';
 
 class ServiceList extends Component {
   props: {
     services: Services,
+    isError: boolean,
+    error: ?Error
   }
 
   render() {
-    return (
-      <div className='ServiceList'>
-        {this.props.services.map(service =>
-          <ServiceBox key={service.serviceName}
-          {...service}
-          />
-        )}
-      </div>
-    );
+      if(this.props.isError) {
+        return <ErrorBox error={this.props.error}/>
+      } else {
+        return <div className='ServiceList'>
+          {this.props.services.map(service =>
+            <ServiceBox key={service.serviceName}
+            {...service}
+            />
+          )}
+        </div>
+    }
   }
 }
 
--- dashboard/service-status/src/index.js
+++ dashboard/service-status/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 import { Provider } from 'react-redux'
@@ -31,7 +30,6 @@ ReactDOM.render(
   ,
   document.getElementById('root')
 );
-registerServiceWorker();
 
 Store.dispatch({ type: "UPDATE" })
 Store.dispatch(fetchServices('reactjs'))
