edab837253798aa092565565ef6ee3c43f983f50
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-15 16:03:25 +0100

    add acl actions to s3_put_dashboard_status policy

--- terraform/iam_policy_document.tf
+++ terraform/iam_policy_document.tf
@@ -212,6 +212,8 @@ data "aws_iam_policy_document" "s3_put_dashboard_status" {
   statement {
     actions = [
       "s3:PutObject",
+      "s3:GetObjectACL",
+      "s3:PutObjectACL",
     ]
 
     resources = [
