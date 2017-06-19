adb6d67349e41c9d9fe073976f2823af7c33941b
caylaa <alicefuzier@gmail.com>
2017-06-19 12:29:45 +0100

    Add permission to read cloudwatch metrics to the ec2 instance role

--- terraform/ecs_iam/outputs.tf
+++ terraform/ecs_iam/outputs.tf
@@ -2,6 +2,10 @@ output "task_role_name" {
   value = "${aws_iam_role.task_role.name}"
 }
 
+output "instance_role_name" {
+  value = "${aws_iam_role.role.name}"
+}
+
 output "task_role_arn" {
   value = "${aws_iam_role.task_role.arn}"
 }
