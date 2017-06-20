ecb969d9a471ae454be41bf34633eea83d68df4d
Alex Chan <a.chan@wellcome.ac.uk>
2017-06-20 10:43:57 +0100

    Add a validate method for WorksIncludes

--- api/src/main/scala/uk/ac/wellcome/platform/api/models/WorksIncludes.scala
+++ api/src/main/scala/uk/ac/wellcome/platform/api/models/WorksIncludes.scala
@@ -4,12 +4,7 @@ case class WorksIncludes(identifiers: Boolean = false)
 
 case object WorksIncludes {
 
-  def apply(queryParam: Option[String]): WorksIncludes = {
-    queryParam match {
-      case Some(s) => WorksIncludes(s)
-      case None => WorksIncludes()
-    }
-  }
+  val recognisedIncludes = List("identifiers")
 
   /// Parse an ?includes query-parameter string.
   ///
@@ -19,5 +14,34 @@ case object WorksIncludes {
     WorksIncludes(
       identifiers = includesList.contains("identifiers")
     )
+  }
+
+  def apply(queryParam: Option[String]): WorksIncludes = {
+    queryParam match {
+      case Some(s) => WorksIncludes(s)
+      case None => WorksIncludes()
+    }
+  }
+
+  /// Validate an ?includes query-parameter string.
+  ///
+  /// Specifically, this checks that every element in the query parameter
+  /// is a known include.  Returns a Left if this is true, or a Right
+  /// containing a list of any unrecognised includes.
+  def validate(queryParam: String): Either[Unit, List[String]] = {
+    val includesList = queryParam.split(",").toList
+    val unrecognisedIncludes = includesList
+      .filterNot(recognisedIncludes.contains)
+    unrecognisedIncludes.length match {
+      case 0 => Left(Unit)
+      case _ => Right(unrecognisedIncludes)
+    }
+  }
+
+  def validate(queryParam: Option[String]): Either[Unit, List[String]] = {
+    queryParam match {
+      case Some(s) => validate(s)
+      case None => Left(Unit)
+    }
   }
 }
