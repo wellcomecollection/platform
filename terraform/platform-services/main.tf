2988c889918dcfd977eb6114ff35bd4bba16761f
caylaa <alicefuzier@gmail.com>
2017-06-20 17:03:29 +0100

    modifying platform-services to use service-tasks

--- terraform/platform-services/main.tf
+++ terraform/platform-services/main.tf
@@ -1,8 +1,9 @@
-module "service" {
-  source              = "./ecs_service"
-  service_name        = "${var.name}"
-  cluster_id          = "${var.cluster_id}"
-  task_definition_arn = "${module.task.arn}"
+module "service-tasks"{
+  source = "../service-tasks"
+  name        = "${var.name}"
+  task_role_arn    = "${var.task_role_arn}"
+  volume_name      = "${var.volume_name}"
+  volume_host_path = "${var.volume_host_path}"
   vpc_id              = "${var.vpc_id}"
   container_name      = "${var.container_name}"
   container_port      = "${var.container_port}"
@@ -11,15 +12,14 @@ module "service" {
   alb_priority        = "${var.alb_priority}"
   desired_count       = "${var.desired_count}"
   healthcheck_path    = "${var.healthcheck_path}"
-  infra_bucket        = "${var.infra_bucket}"
+  cluster_id          = "${var.cluster_id}"
+  container_definitions = "${module.container_definition.rendered}"
 }
 
-module "task" {
-  source           = "./ecs_tasks"
-  task_name        = "${var.name}"
-  task_role_arn    = "${var.task_role_arn}"
+module "container_definition"{
+  source = "../container_definitions"
+  name = "${var.name}"
   volume_name      = "${var.volume_name}"
-  volume_host_path = "${var.volume_host_path}"
   app_uri          = "${var.app_uri}"
   nginx_uri        = "${var.nginx_uri}"
   template_name    = "${var.template_name}"
