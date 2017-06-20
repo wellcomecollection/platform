ff5e975ac9f813eac3a9f398abe669ed854d4207
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-20 16:06:52 +0100

    Actually use the custom Jackson deserializer for parsing the parameter

--- api/src/test/scala/uk/ac/wellcome/platform/api/models/WorksIncludesTest.scala
+++ api/src/test/scala/uk/ac/wellcome/platform/api/models/WorksIncludesTest.scala
@@ -4,28 +4,19 @@ import org.scalatest.{FunSpec, Matchers}
 
 class WorksIncludesTest extends FunSpec with Matchers {
 
-  it("should default a missing value to false") {
-    val includes = WorksIncludes("")
-    includes.identifiers shouldBe false
-  }
-
   it("should parse a present value as true") {
     val includes = WorksIncludes("identifiers")
     includes.identifiers shouldBe true
   }
 
-  it("should successfully create from a correct string") {
-    val result = WorksIncludes.create(queryParam = "identifiers")
-    result.isRight shouldBe true
-  }
-
   it("should successfully create if no query parameter is provided") {
-    val result = WorksIncludes.create(queryParam = None)
-    result.isRight shouldBe true
+    val includes = WorksIncludes(queryParam = None)
+    includes.identifiers shouldBe false
   }
 
   it("should successfully reject an correct string") {
-    val result = WorksIncludes.create(queryParam = "foo,bar")
-    result shouldBe Left(List("foo", "bar"))
+    intercept[WorksIncludesParsingException] {
+      WorksIncludes(queryParam = "foo,bar")
+    }
   }
 }
