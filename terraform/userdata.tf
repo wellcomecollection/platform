9b3a0b21f492af6b2aac19c7dca152ee966bb8c9
alicefuzier <alicefuzier@gmail.com>
2017-06-20 16:00:16 +0100

    Persist Grafana database in AWS (#422)
    * Add efs and security group rules to mount efs
    
    * Add efs cloud config and use it for grafana
    
    * Modularise efs config
    
    * Set efs mount directory as an output of the userdata module
    
    * Correct variable description
    
    * Remove security group rule, as cluster instances are already allowed to make requests everywhere
    
    * Formatted

--- terraform/userdata.tf
+++ terraform/userdata.tf
@@ -4,8 +4,10 @@ module "services_userdata" {
 }
 
 module "monitoring_userdata" {
-  source       = "./userdata"
-  cluster_name = "${aws_ecs_cluster.monitoring.name}"
+  source            = "./userdata"
+  cluster_name      = "${aws_ecs_cluster.monitoring.name}"
+  efs_filesystem_id = "${module.grafana_efs.efs_id}"
+  template_name     = "ecs-agent-with-efs"
 }
 
 module "api_userdata" {
--- terraform/userdata/outputs.tf
+++ terraform/userdata/outputs.tf
@@ -1,3 +1,7 @@
 output "rendered" {
   value = "${data.template_file.template.rendered}"
 }
+
+output "efs_mount_directory" {
+  value = "${data.template_file.template.vars.efs_mount_directory}"
+}
