d720678ba00442587639bdc5f620204976726448
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-20 16:04:41 +0100

    Add events view to dashboard

--- dashboard/service-status/src/ServiceBox.js
+++ dashboard/service-status/src/ServiceBox.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import TaskCounter from './TaskCounter'
+import type { Event } from './models'
 import './ServiceBox.css'
 
 class ServiceBox extends Component {
@@ -10,6 +11,7 @@ class ServiceBox extends Component {
     desiredCount: number,
     pendingCount: number,
     runningCount: number,
+    events: Array<Event>,
     status: string
   }
 
@@ -28,6 +30,14 @@ class ServiceBox extends Component {
             <TaskCounter type="Pending" count={this.props.pendingCount} shouldBeZero={true}/>
           </div>
           <div className="ServiceBox--Status ">{this.props.status}</div>
+          <div className="EventBox">
+            <div className="EventList">
+              {this.props.events.reverse().map(event => {
+                return <div className="EventList--Event" key={event.timestamp}>&gt; {event.message}</div>
+              })}
+              <div className="EventList--Event">&gt;</div>
+            </div>
+          </div>
         </div>
       </div>
     );
