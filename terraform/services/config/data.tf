c340d9576a3c6460614088fce694403eff6e63b9
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-21 10:16:13 +0100

    Services refactoring (#431)
    * Add efs and security group rules to mount efs
    
    * Add efs cloud config and use it for grafana
    
    * Modularise efs config
    
    * Set efs mount directory as an output of the userdata module
    
    * Correct variable description
    
    * Remove security group rule, as cluster instances are already allowed to make requests everywhere
    
    * Formatted
    
    * Move services to prod-services
    
    * Creating a service-tasks module and using it for grafana
    
    * Fix grafana service start
    
    * Going back to services, otherwise everything breaks
    
    * delete service-tasks
    
    * make generation of config file based on templates optional
    
    * Some more sensible variablie names, descriptions, and cleanup
    
    * Formatted
    
    * Moving stuff in the right places

--- terraform/services/config/data.tf
+++ terraform/services/config/data.tf
@@ -1,4 +1,5 @@
 data "template_file" "config" {
   template = "${file("${path.module}/templates/${var.app_name}.ini.template")}"
   vars     = "${var.template_vars}"
+  count    = "${var.is_config_managed}"
 }
--- terraform/services/variables.tf
+++ terraform/services/variables.tf
@@ -86,3 +86,23 @@ variable "config_vars" {
   type        = "map"
   default     = {}
 }
+
+variable "docker_image" {
+  description = "Name of the docker image to run"
+  default     = ""
+}
+
+variable "container_path" {
+  description = "Path of the mounted volume in the docker container"
+  default     = ""
+}
+
+variable "environment_vars" {
+  description = "Environment variables to pass to the container"
+  default     = ""
+}
+
+variable "is_config_managed" {
+  description = "Flag to tell whether the config should be generated using templates and put in S3"
+  default     = true
+}
