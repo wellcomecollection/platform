5ca1f679aff2a17d6da892802fbe5514991b482f
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-23 13:18:48 +0100

    can't have multiple resources named the same

--- terraform/lambda/trigger_cloudwatch/cloudwatch.tf
+++ terraform/lambda/trigger_cloudwatch/cloudwatch.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_permission" "allow_cloudwatch_trigger" {
 
 # The count here is substituting for the availability of an if statement in terraform
 # See https://blog.gruntwork.io/terraform-tips-tricks-loops-if-statements-and-gotchas-f739bbae55f9
-resource "aws_cloudwatch_event_target" "event_trigger" {
+resource "aws_cloudwatch_event_target" "event_trigger_custom" {
   count = "${var.custom_input}"
 
   rule  = "${var.cloudwatch_trigger_name}"
