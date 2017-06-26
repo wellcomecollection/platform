03fadf9f4b3e0381603c51c6c2d3afa9b81da1cb
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-26 16:09:18 +0100

    Add a user with readonly permissions for the Miro images bucket

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
 output "ecr_nginx" {
   value = "${aws_ecr_repository.nginx.repository_url}"
 }
+
+output "miro_readonly_key_id" {
+	value = "${aws_iam_access_key.miro_images_readonly.id}"
+}
+
+output "miro_readonly_key_secret" {
+	value = "${aws_iam_access_key.miro_images_readonly.secret}"
+}
