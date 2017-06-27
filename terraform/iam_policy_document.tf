d07dfa75bcef54f2fa84577549a4c50985e430e3
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-27 17:46:17 +0100

    Set up a way of doing blue green deployments of EC2 instances (#436)
    * Create cluster to test blue green deployments
    
    * Copy ecs_asg folder and redefine test cluster based on this so we don't break existing clusters
    
    * Create lifecycle hook to send to sns topic
    
    * lambda to set a container instance to draining and check that tasks are not running on it before shutting down the ec2 instance
    
    * Fix lambda and set permissions for it
    
    * Send heartbeat to in reset wait timer before instance is shut down
    
    * If there are no clusterArn and containerInstanceArn tags, go on killing the ec2 instance
    
    * Chaneg underlying ec2 instance to test blue green
    
    * Fix imports and add documentation
    
    * Refactoring
    
    * Wait 30 seconds before rechecking
    
    * Looks like the resending to the topic is not needed
    
    * Nope, it's needed
    
    * sleep 30 seconds before resending the message
    
    * Modifying real asg module to define a lifecycle hook
    
    * Deleting stuff for testing
    
    * Nit and formatting
    
    * Newlines
    
    * Creating alerts for asg when an instance is waiting to terminate state for longer than an hour
    
    * review comments
    
    * review comments
    
    * renaming
    
    * Better description for parameter
    
    * Create separate topic for each alarm
    
    * Oops unbreak monitorin asg
    
    * Assume autoscaling role
    
    * Refactor to use dicts instead of lists
    
    * Increase granularity of alarm otherwise we get too many false positives

--- terraform/iam_policy_document.tf
+++ terraform/iam_policy_document.tf
@@ -248,6 +248,54 @@ data "aws_iam_policy_document" "write_ec2_tags" {
   }
 }
 
+data "aws_iam_policy_document" "complete_lifecycle_hook" {
+  statement {
+    actions = ["autoscaling:CompleteLifecycleAction"]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "ecs_list_container_tasks" {
+  statement {
+    actions = [
+      "ecs:UpdateContainerInstancesState",
+      "ecs:ListTasks",
+      "ecs:DescribeContainerInstances",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "ec2_describe_instances" {
+  statement {
+    actions = [
+      "ec2:DescribeInstances",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "send_asg_heartbeat" {
+  statement {
+    actions = [
+      "autoscaling:RecordLifecycleActionHeartbeat",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
 data "aws_iam_policy_document" "deployments_table" {
   statement {
     actions = [
--- terraform/lambda/variables.tf
+++ terraform/lambda/variables.tf
@@ -19,3 +19,8 @@ variable "environment_variables" {
     EMPTY_VARIABLE = ""
   }
 }
+
+variable "timeout" {
+  description = "The amount of time your Lambda function has to run in seconds"
+  default     = 3
+}
