c30381c4afcbdb06671bc08943b06734068579af
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-30 16:33:33 +0100

    Allow HTTP access to the ALB

--- terraform/ecs_asg/outputs.tf
+++ terraform/ecs_asg/outputs.tf
@@ -2,6 +2,10 @@ output "instance_sg_id" {
   value = "${aws_security_group.instance_sg.id}"
 }
 
-output "loadbalancer_sg_id" {
-  value = "${aws_security_group.loadbalancer_sg.id}"
+output "loadbalancer_sg_https_id" {
+  value = "${aws_security_group.https.id}"
+}
+
+output "loadbalancer_sg_http_id" {
+  value = "${aws_security_group.http.id}"
 }
