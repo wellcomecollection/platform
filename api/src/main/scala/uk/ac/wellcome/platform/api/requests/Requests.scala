c291c0d96315f0fb5ba4552fe12a5235665c48c0
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-30 09:48:02 +0100

    Adds pagination links to the API results where appropriate (#456)
    * Adds pagination links to the API results where appropriate
    
    * remove reflection
    
    * append Page to next/prev result in paginated restult

---
+++ api/src/main/scala/uk/ac/wellcome/platform/api/requests/Requests.scala
@@ -0,0 +1,27 @@
+package uk.ac.wellcome.platform.api.requests
+
+import com.twitter.finagle.http.Request
+import com.twitter.finatra.request.{QueryParam, RouteParam}
+import com.twitter.finatra.validation.{Max, Min}
+import uk.ac.wellcome.platform.api.models.WorksIncludes
+
+sealed trait ApiRequest {
+  val request: Request
+}
+
+case class MultipleResultsRequest(
+                                   @Min(1) @QueryParam page: Int = 1,
+                                   @Min(1) @Max(100) @QueryParam pageSize: Option[Int],
+                                   @QueryParam includes: Option[WorksIncludes],
+                                   @RouteParam id: Option[String],
+                                   @QueryParam query: Option[String],
+                                   @QueryParam _index: Option[String],
+                                   request: Request
+                                 ) extends ApiRequest
+
+case class SingleWorkRequest(
+                              @RouteParam id: String,
+                              @QueryParam includes: Option[WorksIncludes],
+                              @QueryParam _index: Option[String],
+                              request: Request
+                            ) extends ApiRequest
