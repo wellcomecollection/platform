08875fcc6137a99fe792a2bd995d8dcdba953997
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-07-04 12:21:40 +0100

    Refactor lambda as per comments

--- lambdas/post_to_slack/post_to_slack.py
+++ lambdas/post_to_slack/post_to_slack.py
@@ -11,34 +11,53 @@ import os
 from botocore.vendored import requests
 
 
+class Alarm:
+    def __init__(self, json_message):
+        self.message = json.loads(json_message)
+
+    @property
+    def name(self):
+        return self.message['AlarmName']
+
+    @property
+    def namespace(self):
+        return self.message['Trigger']['Namespace']
+
+    @property
+    def metric_name(self):
+        return self.message['Trigger']['MetricName']
+
+    @property
+    def dimensions(self):
+        return self.message['Trigger']['Dimensions']
+
+    @property
+    def state_reason(self):
+        return self.message['NewStateReason']
+
+
 def main(event, _):
     print(f'Received event:\n{pprint.pformat(event)}')
-    message = event['Records'][0]['Sns']['Message']
-    message_data = json.loads(message)
-    alarm_name = message_data['AlarmName']
-    namespace = message_data['Trigger']['Namespace']
-    metric_name = message_data['Trigger']['MetricName']
-    dimensions = message_data['Trigger']['Dimensions']
-    state_reason = message_data['NewStateReason']
+    alarm = Alarm(event['Records'][0]['Sns']['Message'])
 
     slack_data = {'username': 'cloudwatch-alert',
                   "icon_emoji": ":slack:",
                   "attachments": [{
                       'color': 'danger',
-                      'fallback': alarm_name,
-                      "title": alarm_name,
+                      'fallback': alarm.name,
+                      "title": alarm.name,
                       "fields": [
                           {
                               "title": "Metric",
-                              "value": f"{namespace}/{metric_name}"
+                              "value": f"{alarm.namespace}/{alarm.metric_name}"
                           },
                           {
                               "title": "Dimensions",
-                              "value": f"{pprint.pformat(dimensions)}"
+                              "value": f"{pprint.pformat(alarm.dimensions)}"
                           },
                           {
                               "title": "Reason",
-                              "value": state_reason
+                              "value": alarm.state_reason
                           }
                       ]
                   }]}
@@ -48,8 +67,4 @@ def main(event, _):
         data=json.dumps(slack_data),
         headers={'Content-Type': 'application/json'}
     )
-    if response.status_code != 200:
-        raise ValueError(
-            'Request to slack returned an error %s, the response is:\n%s'
-            % (response.status_code, response.text)
-        )
+    response.raise_for_status()
