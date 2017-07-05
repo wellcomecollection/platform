073330f841b5fce0337ce03dc265413b9738c49e
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-07-05 12:56:43 +0100

    Per-variant ECR repos and a nicer Python script

--- scripts/tooling.py
+++ scripts/tooling.py
@@ -3,6 +3,11 @@
 import subprocess
 
 
+# Root of the Git repository
+ROOT = subprocess.check_output([
+    'git', 'rev-parse', '--show-toplevel']).decode('ascii').strip()
+
+
 def changed_files(commit_range):
     """
     Returns a set of changed files in a given commit range.
@@ -18,3 +23,25 @@ def changed_files(commit_range):
         if filepath:
             files.add(filepath)
     return files
+
+
+def ecr_repo_uri_from_name(ecr_client, name):
+    """
+    Given the name of an ECR repo (e.g. uk.ac.wellcome/api), return the URI
+    for the repo.
+    """
+    resp = ecr_client.describe_repositories(repositoryNames=[name])
+    try:
+        return resp['repositories'][0]['repositoryUri']
+    except (KeyError, IndexError) as e:
+        raise RuntimeError(f'Unable to look up repo URI for {name!r}: {e}')
+
+
+def authenticate_for_ecr_pushes(ecr_client, docker_client, repo_uri):
+    """
+    Get a login token from ECR, and authenticate ourselves to do 'docker push'.
+    """
+    resp = ecr_client.get_authorization_token()
+    token = resp['authorizationData'][0]['authorizationToken']
+
+    docker_client.login(username='AWS', password='token', registry=repo_uri)
