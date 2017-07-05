109d89d26f2e619cf8e680557a16b863390c897e
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-07-05 11:35:34 +0100

    Docs

--- terraform/lambdas.tf
+++ terraform/lambdas.tf
@@ -249,11 +249,11 @@ module "lambda_dynamo_to_sns" {
 
   environment_variables = {
     STREAM_TOPIC_MAP = <<EOF
-{
-  "${aws_dynamodb_table.miro_table.stream_arn}": "${module.miro_transformer_topic.arn}",
-  "${aws_dynamodb_table.calm_table.stream_arn}": "${module.calm_transformer_topic.arn}"
-}
-EOF
+      {
+        "${aws_dynamodb_table.miro_table.stream_arn}": "${module.miro_transformer_topic.arn}",
+        "${aws_dynamodb_table.calm_table.stream_arn}": "${module.calm_transformer_topic.arn}"
+      }
+      EOF
   }
 }
 
