9fe84e42571f306335c799c21b38996179cf8095
caylaa <alicefuzier@gmail.com>
2017-07-06 15:54:20 +0100

    Formatting of bash script

--- scripts/run_docker_with_aws_credentials.sh
+++ scripts/run_docker_with_aws_credentials.sh
@@ -6,5 +6,9 @@ if [[  -z ${AWS_ACCESS_KEY_ID+x} ]]
         docker run -v $HOME/.aws:/root/.aws "$@"
     else
         echo "AWS_ACCESS_KEY_ID defined!"
-        docker run -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_REGION=$AWS_REGION -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION "$@"
+        docker run \
+        -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+        -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+        -e AWS_REGION=$AWS_REGION \
+        -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION "$@"
 fi
