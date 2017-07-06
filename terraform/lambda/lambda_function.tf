6bccc63382ae3bdfb706c555629b3dee36d53b19
Robert Kenny <kenoir@users.noreply.github.com>
2017-07-06 12:19:15 +0100

    Adds alarms to lambda functions (#586)

--- terraform/lambda/lambda_function.tf
+++ terraform/lambda/lambda_function.tf
@@ -18,3 +18,21 @@ resource "aws_lambda_function" "lambda_function" {
     variables = "${var.environment_variables}"
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "lambda_alarm" {
+  alarm_name          = "lambda-${var.name}-errors"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "1"
+
+  dimensions {
+    FunctionName = "${var.name}"
+  }
+
+  alarm_description = "This metric monitors lambda errors for function: ${var.name}"
+  alarm_actions     = ["${var.alarm_topic_arn}"]
+}
