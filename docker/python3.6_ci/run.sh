9d01a1f7bf0aaadb617457dd80baadb8aaa1e767
caylaa <alicefuzier@gmail.com>
2017-07-06 13:16:09 +0100

    test lambdas in makefile! will not work in ci

--- docker/python3.6_ci/run.sh
+++ docker/python3.6_ci/run.sh
@@ -4,4 +4,16 @@ set -o errexit
 set -o nounset
 set -o verbose
 
-flake8 /data
+export OP="${OP:-plan}"
+
+cd /data
+
+if [[ "$OP" == "lint" ]]
+then
+  echo "Linting Lambdas"
+  flake8 .
+elif [[ "$OP" == "test" ]]
+then
+  echo "Testing Lambdas"
+  find **/test_*.py | py.test
+fi
