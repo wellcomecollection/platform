09d092f3982fab550a744ad8a6ec407a138b2077
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-07-07 11:43:56 +0100

    Fix date serialization issue with sns_utils

--- terraform/lambdas.tf
+++ terraform/lambdas.tf
@@ -241,7 +241,7 @@ module "trigger_post_to_slack_dlqs_not_empty" {
 module "trigger_post_to_slack_esg_not_terminating" {
   source               = "./lambda/trigger_sns"
   lambda_function_name = "${module.lambda_post_to_slack.function_name}"
-  lambda_function_arn  = "${module.lambda_post_to_slack.arn}"
+  lambda_function_arn  = "${mo[]dule.lambda_post_to_slack.arn}"
   sns_trigger_arn      = "${module.ec2_instance_terminating_for_too_long_alarm.arn}"
 }
 
