0e3314f8d7aba7e20fdfa060d54cdf9de178aece
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-07-07 10:51:58 +0100

    Create cloudwatch log groups for nginx containers

--- terraform/services/ecs_tasks/cloudwatch.tf
+++ terraform/services/ecs_tasks/cloudwatch.tf
@@ -1,3 +1,7 @@
 resource "aws_cloudwatch_log_group" "task" {
   name = "platform/${var.task_name}"
 }
+
+resource "aws_cloudwatch_log_group" "nginx_task" {
+  name = "platform/nginx_${var.task_name}"
+}
