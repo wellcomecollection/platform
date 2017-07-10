990b706b8bbbf1b9fafac65940561c9e508f8396
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-07-10 12:19:37 +0100

    Adds tags to network resource to make them easier to identify

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,16 +2,19 @@ module "vpc_services" {
   source     = "./network"
   cidr_block = "10.50.0.0/16"
   az_count   = "2"
+  name       = "services"
 }
 
 module "vpc_monitoring" {
   source     = "./network"
   cidr_block = "10.40.0.0/16"
   az_count   = "2"
+  name       = "monitoring"
 }
 
 module "vpc_api" {
   source     = "./network"
   cidr_block = "10.30.0.0/16"
   az_count   = "2"
+  name       = "monitoring"
 }
