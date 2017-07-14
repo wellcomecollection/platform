6b7ca36ef0d642c70839e23fe15a7e93f420e309
caylaa <alicefuzier@gmail.com>
2017-07-14 15:30:22 +0100

    More fixtures!

--- lambdas/drain_ecs_container_instance/test_drain_ecs_container_instance.py
+++ lambdas/drain_ecs_container_instance/test_drain_ecs_container_instance.py
@@ -28,6 +28,7 @@ def moto_start():
 @pytest.fixture()
 def autoscaling_group_info(moto_start):
     auto_scaling_group_name = 'TestGroup1'
+    print(f"creating autoscaling group {auto_scaling_group_name}")
     fake_asg_client = boto3.client('autoscaling')
     fake_asg_client.create_launch_configuration(
         LaunchConfigurationName='TestLC'
@@ -50,13 +51,14 @@ def autoscaling_group_info(moto_start):
 def sns_sqs_info(moto_start):
     fake_sns_client = boto3.client('sns')
     fake_sqs_client = boto3.client('sqs')
+    queue_name = "test-queue"
+    topic_name = "test-topic"
+    print(f"Creating topic {topic_name} and queue {queue_name}")
 
-    fake_sns_client.create_topic(Name="test-topic")
-
+    fake_sns_client.create_topic(Name=topic_name)
     response = fake_sns_client.list_topics()
     topic_arn = response["Topics"][0]['TopicArn']
 
-    queue_name = "test-queue"
     queue = fake_sqs_client.create_queue(QueueName=queue_name)
 
     fake_sns_client.subscribe(
@@ -65,6 +67,93 @@ def sns_sqs_info(moto_start):
         Endpoint=f"arn:aws:sqs:eu-west-1:123456789012:{queue_name}"
     )
     yield topic_arn, queue['QueueUrl']
+
+
+@pytest.fixture()
+def ecs_cluster(autoscaling_group_info):
+    fake_ecs_client = boto3.client('ecs')
+    fake_ec2_client = boto3.client('ec2')
+    cluster_name = 'test_ecs_cluster'
+    print(f"Creating ecs cluster {cluster_name}")
+    instance_id = autoscaling_group_info[1]
+    cluster_response = fake_ecs_client.create_cluster(
+        clusterName=cluster_name
+    )
+
+    ec2 = boto3.resource('ec2')
+    instance = ec2.Instance(instance_id)
+
+    instance_id_document = json.dumps(
+        ec2_utils.generate_instance_identity_document(instance)
+    )
+
+    register_container_response = fake_ecs_client.register_container_instance(
+        cluster=cluster_name,
+        instanceIdentityDocument=instance_id_document
+    )
+
+    container_instance_arn = \
+        register_container_response['containerInstance']['containerInstanceArn']
+    cluster_arn = cluster_response['cluster']['clusterArn']
+
+    fake_ec2_client.create_tags(
+        Resources=[
+            instance_id,
+        ],
+        Tags=[
+            {
+                'Key': 'clusterArn',
+                'Value': cluster_arn
+            },
+            {
+                'Key': 'containerInstanceArn',
+                'Value': container_instance_arn
+            }
+        ]
+    )
+    yield cluster_arn, cluster_arn, container_instance_arn
+
+
+@pytest.fixture()
+def ecs_task(ecs_cluster):
+    fake_ecs_client = boto3.client('ecs')
+    task_name = 'test_ecs_task'
+
+    cluster_name = ecs_cluster[0]
+    cluster_arn = ecs_cluster[1]
+    container_instance_arn = ecs_cluster[2]
+    fake_ecs_client.register_task_definition(
+        family=task_name,
+        containerDefinitions=[
+            {
+                'name': 'hello_world',
+                'image': 'docker/hello-world:latest',
+                'cpu': 1024,
+                'memory': 400,
+                'essential': True,
+                'environment': [{
+                    'name': 'AWS_ACCESS_KEY_ID',
+                    'value': 'SOME_ACCESS_KEY'
+                }],
+                'logConfiguration': {'logDriver': 'json-file'}
+            }
+        ]
+    )
+
+    fake_ecs_client.run_task(
+        cluster=cluster_name,
+        overrides={},
+        taskDefinition=task_name,
+        count=1,
+        startedBy='moto'
+    )
+
+    tasks = fake_ecs_client.list_tasks(
+        cluster=cluster_arn,
+        containerInstance=container_instance_arn
+    )
+    assert len(tasks['taskArns']) == 1
+    yield
 
 
 def test_complete_ec2_shutdown_if_no_ecs_cluster(autoscaling_group_info):
@@ -126,7 +215,7 @@ def test_complete_ec2_shutdown_if_no_ecs_cluster(autoscaling_group_info):
         )
 
 
-def test_drain_ecs_instance_if_running_tasks(sns_sqs_info, autoscaling_group_info):
+def test_drain_ecs_instance_if_running_tasks(sns_sqs_info, autoscaling_group_info, ecs_task):
     fake_ec2_client = boto3.client('ec2')
     fake_ecs_client = boto3.client('ecs')
     fake_sqs_client = boto3.client('sqs')
@@ -134,75 +223,7 @@ def test_drain_ecs_instance_if_running_tasks(sns_sqs_info, autoscaling_group_inf
     queue_url = sns_sqs_info[1]
     auto_scaling_group_name = autoscaling_group_info[0]
     instance_id = autoscaling_group_info[1]
-    cluster_name = 'test_ecs_cluster'
-    cluster_response = fake_ecs_client.create_cluster(
-        clusterName=cluster_name
-    )
 
-    ec2 = boto3.resource('ec2')
-    instance = ec2.Instance(instance_id)
-
-    instance_id_document = json.dumps(
-        ec2_utils.generate_instance_identity_document(instance)
-    )
-
-    register_container_response = fake_ecs_client.register_container_instance(
-        cluster=cluster_name,
-        instanceIdentityDocument=instance_id_document
-    )
-
-    container_instance_arn = \
-        register_container_response['containerInstance']['containerInstanceArn']
-    cluster_arn = cluster_response['cluster']['clusterArn']
-
-    fake_ec2_client.create_tags(
-        Resources=[
-            instance_id,
-        ],
-        Tags=[
-            {
-                'Key': 'clusterArn',
-                'Value': cluster_arn
-            },
-            {
-                'Key': 'containerInstanceArn',
-                'Value': container_instance_arn
-            }
-        ]
-    )
-
-    task_name = 'test_ecs_task'
-    fake_ecs_client.register_task_definition(
-        family=task_name,
-        containerDefinitions=[
-            {
-                'name': 'hello_world',
-                'image': 'docker/hello-world:latest',
-                'cpu': 1024,
-                'memory': 400,
-                'essential': True,
-                'environment': [{
-                    'name': 'AWS_ACCESS_KEY_ID',
-                    'value': 'SOME_ACCESS_KEY'
-                }],
-                'logConfiguration': {'logDriver': 'json-file'}
-            }
-        ]
-    )
-
-    fake_ecs_client.run_task(
-        cluster=cluster_name,
-        overrides={},
-        taskDefinition=task_name,
-        count=1,
-        startedBy='moto'
-    )
-
-    tasks = fake_ecs_client.list_tasks(
-        cluster=cluster_arn,
-        containerInstance=container_instance_arn
-    )
-    assert len(tasks['taskArns']) == 1
     lifecycle_action_token = "78c16884-6bd4-4296-ac0c-2da9eb6a0d29"
 
     lifecycle_hook_name = "monitoring-cluster-LifecycleHook-OENP6M5XGYVM"
