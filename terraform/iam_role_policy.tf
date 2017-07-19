dbd5487abd131a8815f8de15942fdee798958060
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-07-19 11:32:34 +0100

    dedupe role policy name

--- terraform/iam_role_policy.tf
+++ terraform/iam_role_policy.tf
@@ -142,12 +142,12 @@ EOF
 
 # Role policies for ecs_ec2_instance_tagger lambda
 
-resource "aws_iam_role_policy" "ecs_ec2_instance_tagger_thing" {
+resource "aws_iam_role_policy" "ecs_ec2_instance_tagger_write_tags" {
   role   = "${module.lambda_ecs_ec2_instance_tagger.role_name}"
   policy = "${data.aws_iam_policy_document.write_ec2_tags.json}"
 }
 
-resource "aws_iam_role_policy" "ecs_ec2_instance_tagger_thing" {
+resource "aws_iam_role_policy" "ecs_ec2_instance_tagger_use_tmp" {
   role   = "${module.lambda_ecs_ec2_instance_tagger.role_name}"
   policy = "${data.aws_iam_policy_document.s3_put_infra_tmp.json}"
 }
