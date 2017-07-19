ce98de18db8ca7339e9e7ec186006e5281cb3f46
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-07-19 11:41:12 +0100

    Prevent EC2 tagging rate limit in tagger lambda (#666)
    * Prevent EC2 tagging rate limit in tagger lambda
    
    * add new perms for tagger
    
    * adds tests
    
    * add correct config to lambda
    
    * use head_object rather than get as we don't use the body
    
    * remove unnecessary reference to exception in raise block
    
    * adds lifecycle rule for infra tmp directory to expire objects after 30 days
    
    * terraform fmt
    
    * dedupe role policy name

--- lambdas/ecs_ec2_instance_tagger/ecs_ec2_instance_tagger.py
+++ lambdas/ecs_ec2_instance_tagger/ecs_ec2_instance_tagger.py
@@ -19,7 +19,11 @@ Requires a Cloudwatch Event with pattern:
 
 """
 
+import json
+import os
+
 import boto3
+from botocore.exceptions import ClientError
 
 
 def create_tags(ec2_client, ec2_instance_id, event_detail):
@@ -30,6 +34,15 @@ def create_tags(ec2_client, ec2_instance_id, event_detail):
 
     Returns the SDK response to the create_tags call.
     """
+
+    cluster_arn = event_detail['clusterArn']
+    container_instance_arn = event_detail['containerInstanceArn']
+
+    print(f'Tag {ec2_instance_id} clusterArn: {cluster_arn}')
+    print(
+        f'Tag {ec2_instance_id} containerInstanceArn: {container_instance_arn}'
+    )
+
     return ec2_client.create_tags(
         Resources=[ec2_instance_id],
         Tags=[{
@@ -46,8 +59,35 @@ def main(event, _):
     print(f'event = {event!r}')
 
     ec2_client = boto3.client('ec2')
+    s3_client = boto3.client('s3')
+
+    bucket_name = os.environ["BUCKET_NAME"]
+    object_path = os.environ["OBJECT_PATH"]
 
     ec2_instance_id = event['detail']['ec2InstanceId']
 
-    response = create_tags(ec2_client, ec2_instance_id, event['detail'])
-    print(f'response = {response!r}')
+    try:
+        s3_client.head_object(
+            Bucket=bucket_name,
+            Key=f'{object_path}/{ec2_instance_id}'
+        )
+    except ClientError as ex:
+        if ex.response['Error']['Code'] == '404':
+            print(f'{ec2_instance_id} not seen yet, tagging!')
+
+            response = create_tags(
+                ec2_client,
+                ec2_instance_id,
+                event['detail'])
+
+            print(f'response = {response!r}')
+        else:
+            raise
+
+    s3_client.put_object(
+        Bucket=bucket_name,
+        Key=f'{object_path}/{ec2_instance_id}',
+        Body=json.dumps(event)
+    )
+
+    print(f'{ec2_instance_id} tagged.')
