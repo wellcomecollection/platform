d29c9d395aeeb2c287c2ec9c626c9473470ed689
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-07-19 11:09:04 +0100

    Adds Loris EFS (#668)
    * Adds Loris EFS
    
    * add findutils to loris dockerfile
    
    * add efs volume to loris service

--- terraform/services.tf
+++ terraform/services.tf
@@ -211,6 +211,10 @@ module "loris" {
   alb_priority       = "109"
   host_name          = "iiif.wellcomecollection.org"
 
+  volume_name      = "loris"
+  volume_host_path = "${module.api_userdata.efs_mount_directory}/loris"
+  container_path   = "/usr/local/share/images/loris"
+
   loadbalancer_cloudwatch_id   = "${module.api_alb.cloudwatch_id}"
   server_error_alarm_topic_arn = "${module.alb_server_error_alarm.arn}"
   client_error_alarm_topic_arn = "${module.alb_client_error_alarm.arn}"
