31cfaa990f9c71efa21d76ed408ebd4898b596fc
caylaa <alicefuzier@gmail.com>
2017-07-19 10:27:11 +0100

    Tests to use SQSLocal instead of dynamodb

--- transformer/src/test/scala/uk/ac/wellcome/transformer/StartupTest.scala
+++ transformer/src/test/scala/uk/ac/wellcome/transformer/StartupTest.scala
@@ -3,28 +3,20 @@ package uk.ac.wellcome.platform.transformer
 import com.google.inject.Stage
 import com.twitter.finatra.http.EmbeddedHttpServer
 import com.twitter.inject.server.FeatureTest
-import uk.ac.wellcome.test.utils.{
-  AmazonCloudWatchFlag,
-  DynamoDBLocal,
-  SNSLocal,
-  StartupLogbackOverride
-}
+import uk.ac.wellcome.test.utils._
 
 class StartupTest
     extends FeatureTest
     with StartupLogbackOverride
     with AmazonCloudWatchFlag
-    with DynamoDBLocal
+    with SQSLocal
     with SNSLocal {
 
   val server = new EmbeddedHttpServer(
     stage = Stage.PRODUCTION,
     twitterServer = new Server,
     flags = Map(
-      "aws.dynamo.miroData.streams.appName" -> "miro",
-      "aws.dynamo.miroData.streams.arn" -> "miro",
-      "aws.dynamo.miroData.tableName" -> "MiroData"
-    ) ++ cloudWatchLocalEndpointFlag ++ dynamoDbLocalEndpointFlags ++ snsLocalEndpointFlags
+    ) ++ cloudWatchLocalEndpointFlag ++ sqsLocalFlags ++ snsLocalEndpointFlags
   )
 
   test("server starts up correctly") {
