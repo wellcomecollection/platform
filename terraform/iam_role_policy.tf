2883849b74df64940abb730237edd01b75699c87
Robert Kenny <kenoir@users.noreply.github.com>
2017-07-19 11:41:12 +0100

    Prevent EC2 tagging rate limit in tagger lambda (#666)
    * Prevent EC2 tagging rate limit in tagger lambda
    
    * add new perms for tagger
    
    * adds tests
    
    * add correct config to lambda
    
    * use head_object rather than get as we don't use the body
    
    * remove unnecessary reference to exception in raise block
    
    * adds lifecycle rule for infra tmp directory to expire objects after 30 days
    
    * terraform fmt
    
    * dedupe role policy name

--- terraform/iam_role_policy.tf
+++ terraform/iam_role_policy.tf
@@ -142,9 +142,14 @@ EOF
 
 # Role policies for ecs_ec2_instance_tagger lambda
 
-resource "aws_iam_role_policy" "ecs_ec2_instance_tagger_thing" {
+resource "aws_iam_role_policy" "ecs_ec2_instance_tagger_write_tags" {
   role   = "${module.lambda_ecs_ec2_instance_tagger.role_name}"
   policy = "${data.aws_iam_policy_document.write_ec2_tags.json}"
+}
+
+resource "aws_iam_role_policy" "ecs_ec2_instance_tagger_use_tmp" {
+  role   = "${module.lambda_ecs_ec2_instance_tagger.role_name}"
+  policy = "${data.aws_iam_policy_document.s3_put_infra_tmp.json}"
 }
 
 # Role policies for service_deployment_status lambda
