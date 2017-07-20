aa510a9ccea0dac0af08e11f7c6f5038d644af3e
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-07-20 15:49:36 +0100

    Add the scripts.tf, oops

---
+++ terraform/scripts.tf
@@ -0,0 +1,16 @@
+module "loris_cache_cleaner" {
+  source           = "./ecs_script_task"
+  task_name        = "loris_cache_cleaner"
+  app_uri          = "${module.ecr_repository_cache_cleaner.repository_url}:${var.release_ids["cache_cleaner"]}"
+  task_role_arn    = "${module.ecs_cache_cleaner_iam.task_role_arn}"
+  volume_name      = "loris"
+  volume_host_path = "${module.api_userdata.efs_mount_directory}/loris"
+  container_path   = "/data"
+  cpu              = 256
+  memory           = 256
+
+  env_vars = [
+    "{\"name\": \"MAX_AGE\", \"value\": \"30\"}",
+    "{\"name\": \"MAX_SIZE\", \"value\": \"50000\"}",
+  ]
+}
