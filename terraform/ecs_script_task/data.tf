4c60cf6b1eb899d513808e72b6e5e0ef7d3cc4a0
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-07-20 14:33:43 +0100

    Terraform module for scripted tasks

---
+++ terraform/ecs_script_task/data.tf
@@ -0,0 +1,14 @@
+data "template_file" "definition" {
+  template = "${file("${path.module}/templates/default.json.template")}"
+
+  vars {
+    log_group_region = "${var.aws_region}"
+    log_group_name   = "${aws_cloudwatch_log_group.task.name}"
+    app_uri          = "${var.app_uri}"
+    volume_name      = "${var.volume_name}"
+    container_path   = "${var.container_path}"
+    environment_vars = "[${join(",", concat(var.service_vars,var.extra_vars))}]"
+    cpu              = "${var.cpu}"
+    memory           = "${var.memory}"
+  }
+}
