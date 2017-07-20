40ebf79a9bae618cee3a237efbce497406016c87
Alex Chan <a.chan@wellcome.ac.uk>
2017-07-20 16:24:03 +0100

    Have the module decide whether the ASG instances need EFS

--- terraform/userdata.tf
+++ terraform/userdata.tf
@@ -7,12 +7,10 @@ module "monitoring_userdata" {
   source            = "./userdata"
   cluster_name      = "${aws_ecs_cluster.monitoring.name}"
   efs_filesystem_id = "${module.grafana_efs.efs_id}"
-  template_name     = "ecs-agent-with-efs"
 }
 
 module "api_userdata" {
   source            = "./userdata"
   cluster_name      = "${aws_ecs_cluster.api.name}"
   efs_filesystem_id = "${module.loris_efs.efs_id}"
-  cluster_name      = "${aws_ecs_cluster.api.name}"
 }
