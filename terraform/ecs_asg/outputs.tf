ea737187014d1b9238baf2a22ef89c1ad3e916d2
Alex Chan <a.chan@wellcome.ac.uk>
2017-07-21 10:35:40 +0100

    Use more variables

--- terraform/ecs_asg/outputs.tf
+++ terraform/ecs_asg/outputs.tf
@@ -14,6 +14,10 @@ output "asg_name" {
   value = "${aws_cloudformation_stack.ecs_asg.outputs["AsgName"]}"
 }
 
+output "asg_desired" {
+  value = "${var.asg_desired}"
+}
+
 output "asg_max" {
   value = "${var.asg_max}"
 }
