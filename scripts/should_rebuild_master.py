6bef5128b1dc4a5af7e6f1585c5c4e0df782fba6
Alex Chan <a.chan@wellcome.ac.uk>
2017-07-27 15:19:28 +0100

    Tidy up the scripts for deciding whether Travis should run

--- scripts/should_rebuild_master.py
+++ scripts/should_rebuild_master.py
@@ -6,37 +6,28 @@ Decide whether we should re-build tests for a project on master.
 Exits with code 1 if there are changes that require a rebuild, 0 if not.
 """
 
+from __future__ import print_function
+
 import os
 import sys
 
-from should_rerun_tests import should_retest_project
+from should_rerun_tests import should_run_tests, ShouldRebuild
 import tooling
 
 
-def should_rebuild_project(changed_files, project):
+def should_run_deploy(changed_files, task):
     """
-    Given a set of changed files, return True/False if we should
-    rebuild this project.
+    Given a set of changed files, check if we need to run tests.
     """
-    if should_retest_project(changed_files, project):
-        return True
+    should_run_tests(changed_files=changed_files, task=task)
 
-    elif ('docker/' in changed_files):
-        print("*** Changes to the docker directory mean we should rebuild")
-        return True
-
-    elif any(f.startswith('scripts/') for f in changed_files):
-        print("*** Changes to the scripts directory mean we should rebuild")
-        return True
-
-    elif any(f.startswith('.travis') for f in changed_files):
-        print("*** Changes to the Travis config mean we should rebuild")
-        return True
-    elif 'Makefile' in changed_files:
-        print("*** Changes to the Makefile mean we should rebuild")
-        return True
-
-    return False
+    if (
+        task.startswith('sbt-') and
+        any(f.startswith('docker/scala_service') in changed_files
+    ):
+        raise ShouldRebuild(
+            'Changes to docker/scala_service mean we should deploy'
+        )
 
 
 if __name__ == '__main__':
@@ -46,12 +37,12 @@ if __name__ == '__main__':
     # value and passing it to `git diff`, we can determine which files
     # have changed.
     changed_files = tooling.changed_files([os.environ['TRAVIS_COMMIT_RANGE']])
+    task = os.environ['TASK']
 
-    should_rebuild = should_rebuild_project(
-        changed_files=changed_files,
-        project=os.environ['PROJECT']
-    )
-    if should_rebuild:
+    try:
+        should_run_deploy(changed_files=changed_files, task=task)
+    except ShouldRebuild as err:
+        print('*** %s' % err, file=sys.stderr)
         sys.exit(1)
     else:
         sys.exit(0)
