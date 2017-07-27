8faf3d2ad7cce89c5f68daa88770a16bfd9c8373
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-07-27 14:51:14 +0100

    Consolidate docker build scripts

--- scripts/build_docker_image.py
+++ scripts/build_docker_image.py
@@ -4,7 +4,7 @@
 Builds a docker image for a simple project
 
 Usage:
-  build_docker_image.py --project=<name>
+  build_docker_image.py --project=<name> [--variant=<variant>]
   build_docker_image.py -h | --help
 
 Options:
@@ -12,6 +12,8 @@ Options:
   --project=<project>      Name of the project (e.g. api, loris).  Assumes
                            there's a folder containing a Dockerfile with the
                            same name.
+  --variant=<variant>      The optional variant of this project (e.g. nginx).
+                           Relies on the Dockerfile accepting a variant ENV.
 """
 
 import os
@@ -26,19 +28,25 @@ from tooling import write_release_id, CURRENT_COMMIT, ROOT
 if __name__ == '__main__':
     args = docopt.docopt(__doc__)
 
+    variant = args['--variant']
     project = args['--project']
 
-    print('*** Building Docker image for %s' % project)
+    print('*** Building image for %s' % project)
 
     release_id = CURRENT_COMMIT
 
-    tag = '%s:%s' % (project, release_id)
+    if variant is not None:
+        tag = '%s_%s:%s' % (project, variant, release_id)
+    else:
+        tag = '%s:%s' % (project, release_id)
+
     print('*** Image will be tagged %s' % tag)
 
-    print('*** Building the new Docker image')
+    print('*** Building the new image')
     client = docker.from_env()
     client.images.build(
         path=os.path.join(ROOT, 'docker', project),
+        buildargs={'variant': variant},
         tag=tag
     )
 
