24f0711540b8f4e6b7d00696552d9f6ed9cdcfd0
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-08-01 15:22:53 +0100

    change AWS_REGION -> AWS_DEFAULT_REGION

--- terraform/scripts.tf
+++ terraform/scripts.tf
@@ -26,6 +26,6 @@ module "gatling" {
 
   env_vars = [
     "{\"name\": \"SIMULATION\", \"value\": \"testing.load.LorisSimulation\"}",
-    "{\"name\": \"AWS_REGION\", \"value\": \"${var.aws_region}\"}",
+    "{\"name\": \"AWS_DEFAULT_REGION\", \"value\": \"${var.aws_region}\"}",
   ]
 }
