261e1cac5ab2e3c50f259062a56182a8d6cff4c6
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-08-02 11:31:34 +0100

    Use our new Gatling assertions (#741)
    * Use our new Gatling assertions
    
    * Add rotation to the different requests
    
    * Update the data CSVs

--- docker/gatling/user-files/simulations/LorisSimulation.scala
+++ docker/gatling/user-files/simulations/LorisSimulation.scala
@@ -58,15 +58,16 @@ class LorisSimulation extends Simulation {
 
   val regionFeeder = csv("region.csv").random
   val sizeFeeder = csv("size.csv").random
+  val rotationFeeder = csv("rotate.csv").random
 
   // And a handful of requests that involve more image processing
-  // TODO Allow different rotation parameters
   val complexScn = scenario("complex")
     .feed(identFeeder)
     .feed(regionFeeder)
     .feed(sizeFeeder)
+    .feed(rotationFeeder)
     .exec(http("complex-scenario")
-      .get("/image/${ident}/${region}/${size}/0/default.jpg")
+      .get("/image/${ident}/${region}/${size}/${rotate}/default.jpg")
       .check(status.in(200, 304))
     )
 
@@ -78,7 +79,8 @@ class LorisSimulation extends Simulation {
   ).protocols(
     httpConf
   ).assertions(
-    global.responseTime.max.lt(1500),
-    global.successfulRequests.percent.gt(95)
+    percentile1.responseTime.max.lt(1000),  // 95th percentile
+    percentile2.responseTime.max.lt(1500),  // 99th percentile
+    global.successfulRequests.percent.gt(99)
   )
 }
