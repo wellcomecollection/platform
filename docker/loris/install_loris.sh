e0c89444c1d2888b942bddc796edc659fcac835e
Alex Chan <a.chan@wellcome.ac.uk>
2017-08-02 15:32:03 +0100

    Point the Loris directories at EFS

---
+++ docker/loris/install_loris.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Install Loris on Ubuntu
+
+set -o errexit
+set -o nounset
+
+# Install dependencies.  We don't include Apache because we're running
+# Loris with UWSGI and nginx, not Apache.
+apt-get install -y libjpeg-turbo8-dev libfreetype6-dev zlib1g-dev \
+    liblcms2-dev liblcms2-utils libtiff5-dev libwebp-dev
+
+# Download and install the Loris code itself
+apt-get install -y unzip wget
+wget https://github.com/loris-imageserver/loris/archive/v2.1.0-final.zip
+unzip v2.1.0-final.zip
+rm v2.1.0-final.zip
+apt-get uninstall -y unzip wget
+
+# Required or setup.py complains
+useradd -d /var/www/loris -s /sbin/false loris
+
+cd loris-2.1.0-final
+pip install -r requirements.txt
+python setup.py install --image-cache=/mnt/efs/image_cache --info-cache=/mnt/efs/info_cache
