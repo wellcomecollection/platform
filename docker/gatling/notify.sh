dff4fac3aef6dc443ade2d8276190a1132a90d75
Robert Kenny <kenoir@users.noreply.github.com>
2017-08-02 10:17:09 +0100

    Use ARN rather than lookup from name in Gatling container (#739)
    * Use ARN rather than lookup from name in gatling container
    
    In order that we don't need to give listTopics permission to tasks.
    
    * push out AWS resources to variables, consolidate notify.sh

--- docker/gatling/notify.sh
+++ docker/gatling/notify.sh
@@ -4,16 +4,14 @@ set -o errexit
 set -o nounset
 set -o verbose
 
-TOPIC_NAME=$1
+TOPIC_ARN=$1
 MESSAGE_FILE=$2
-
-TOPIC_ARN=$(aws sns list-topics | jq .Topics[].TopicArn -r | grep "$TOPIC_NAME" | tail -n 1)
 
 if [ -e $MESSAGE_FILE ]; then
     aws sns publish \
         --topic-arn "$TOPIC_ARN" \
         --message "file://$MESSAGE_FILE"
-    echo "Notification sent to $TOPIC_NAME"
+    echo "Notification sent to $TOPIC_ARN"
 else
     echo "$MESSAGE_FILE result not found!"
     exit 1
