c50ff853490e338d2d6d62783164e89b8494895a
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-08-03 15:25:24 +0100

    Variables in Terraform suck

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -31,6 +31,16 @@ variable "dash_bucket" {
   default     = "wellcome-platform-dash"
 }
 
+variable "api_host" {
+  description = "Hostname to use for the production API"
+  default     = "api.wellcomecollection.org"
+}
+
+variable "api_host_stage" {
+  description = "Hostname to use for the staging version of the API"
+  default     = "api-stage.wellcomecollection.org"
+}
+
 variable "es_config" {
   description = "Elasticcloud config"
   type        = "map"
