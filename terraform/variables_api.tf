d1e6af0e01517eed34e792a2b51ea6c35d87f493
Alex Chan <a.chan@wellcome.ac.uk>
2017-08-03 15:25:24 +0100

    Variables in Terraform suck

--- terraform/variables_api.tf
+++
@@ -1,9 +0,0 @@
-variable "api_host" {
-  description = "Hostname to use for the production API"
-  default     = "api.wellcomecollection.org"
-}
-
-variable "api_host_stage" {
-  description = "Hostname to use for the staging version of the API"
-  default     = "api-stage.wellcomecollection.org"
-}
