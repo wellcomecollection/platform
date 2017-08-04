e8bb2c19c77d99dc0a5bf47a49855dd3111ffb91
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-08-04 09:07:04 +0100

    Separate release IDs for Romulus and Remus

--- terraform/services.tf
+++ terraform/services.tf
@@ -145,21 +145,21 @@ module "id_minter" {
   client_error_alarm_topic_arn = "${module.alb_client_error_alarm.arn}"
 }
 
-data "template_file" "es_cluster_host" {
+data "template_file" "es_cluster_host_romulus" {
   template = "$${name}.$${region}.aws.found.io"
 
   vars {
-    name   = "${var.es_config["name"]}"
-    region = "${var.es_config["region"]}"
+    name   = "${var.es_config_romulus["name"]}"
+    region = "${var.es_config_romulus["region"]}"
   }
 }
 
-data "template_file" "es_cluster_host_stage" {
+data "template_file" "es_cluster_host_remus" {
   template = "$${name}.$${region}.aws.found.io"
 
   vars {
-    name   = "${var.es_config_stage["name"]}"
-    region = "${var.es_config_stage["region"]}"
+    name   = "${var.es_config_remus["name"]}"
+    region = "${var.es_config_remus["region"]}"
   }
 }
 
@@ -169,13 +169,13 @@ module "api_romulus" {
   cluster_id         = "${aws_ecs_cluster.api.id}"
   task_role_arn      = "${module.ecs_api_iam.task_role_arn}"
   vpc_id             = "${module.vpc_api.vpc_id}"
-  app_uri            = "${module.ecr_repository_api.repository_url}:${var.production_api == "romulus" ? var.release_ids["api"] : var.staging_app_version}"
-  nginx_uri          = "${module.ecr_repository_nginx_api.repository_url}:${var.production_api == "romulus" ? var.release_ids["nginx_api"] : var.staging_nginx_version}"
+  app_uri            = "${module.ecr_repository_api.repository_url}:${var.release_ids["api_romulus"]}"
+  nginx_uri          = "${module.ecr_repository_nginx_api.repository_url}:${var.release_ids["nginx_api_romulus"]}"
   listener_https_arn = "${module.api_alb.listener_https_arn}"
   listener_http_arn  = "${module.api_alb.listener_http_arn}"
   infra_bucket       = "${var.infra_bucket}"
   config_key         = "config/${var.build_env}/api_romulus.ini"
-  alb_priority       = "110"
+  alb_priority       = "112"
   host_name          = "${var.production_api == "romulus" ? var.api_host : var.api_host_stage}"
 
   cpu    = 1792
@@ -188,14 +188,14 @@ module "api_romulus" {
 
   config_vars = {
     api_host    = "${var.production_api == "romulus" ? var.api_host : var.api_host_stage}"
-    es_host     = "${var.production_api == "romulus" ? data.template_file.es_cluster_host.rendered : data.template_file.es_cluster_host_stage.rendered}"
-    es_port     = "${var.production_api == "romulus" ? var.es_config["port"] : var.es_config_stage["port"]}"
-    es_name     = "${var.production_api == "romulus" ? var.es_config["name"] : var.es_config_stage["name"]}"
-    es_index    = "${var.production_api == "romulus" ? var.es_config["index"] : var.es_config_stage["index"]}"
-    es_doc_type = "${var.production_api == "romulus" ? var.es_config["doc_type"] : var.es_config_stage["doc_type"]}"
-    es_username = "${var.production_api == "romulus" ? var.es_config["username"] : var.es_config_stage["username"]}"
-    es_password = "${var.production_api == "romulus" ? var.es_config["password"] : var.es_config_stage["password"]}"
-    es_protocol = "${var.production_api == "romulus" ? var.es_config["protocol"] : var.es_config_stage["protocol"]}"
+    es_host     = "${data.template_file.es_cluster_host_romuluus.rendered}"
+    es_port     = "${var.es_config_romulus["port"]}"
+    es_name     = "${var.es_config_romulus["name"]}"
+    es_index    = "${var.es_config_romulus["index"]}"
+    es_doc_type = "${var.es_config_romulus["doc_type"]}"
+    es_username = "${var.es_config_romulus["username"]}"
+    es_password = "${var.es_config_romulus["password"]}"
+    es_protocol = "${var.es_config_romulus["protocol"]}"
   }
 
   loadbalancer_cloudwatch_id   = "${module.api_alb.cloudwatch_id}"
@@ -209,8 +209,8 @@ module "api_remus" {
   cluster_id         = "${aws_ecs_cluster.api.id}"
   task_role_arn      = "${module.ecs_api_iam.task_role_arn}"
   vpc_id             = "${module.vpc_api.vpc_id}"
-  app_uri            = "${module.ecr_repository_api.repository_url}:${var.production_api == "remus" ? var.release_ids["api"] : var.staging_app_version}"
-  nginx_uri          = "${module.ecr_repository_nginx_api.repository_url}:${var.production_api == "remus" ? var.release_ids["nginx_api"] : var.staging_nginx_version}"
+  app_uri            = "${module.ecr_repository_api.repository_url}:${var.release_ids["api_remus"]}"
+  nginx_uri          = "${module.ecr_repository_nginx_api.repository_url}:${var.release_ids["nginx_api_remus"]}"
   listener_https_arn = "${module.api_alb.listener_https_arn}"
   listener_http_arn  = "${module.api_alb.listener_http_arn}"
   infra_bucket       = "${var.infra_bucket}"
@@ -228,14 +228,14 @@ module "api_remus" {
 
   config_vars = {
     api_host    = "${var.production_api == "remus" ? var.api_host : var.api_host_stage}"
-    es_host     = "${var.production_api == "remus" ? data.template_file.es_cluster_host.rendered : data.template_file.es_cluster_host_stage.rendered}"
-    es_port     = "${var.production_api == "remus" ? var.es_config["port"] : var.es_config_stage["port"]}"
-    es_name     = "${var.production_api == "remus" ? var.es_config["name"] : var.es_config_stage["name"]}"
-    es_index    = "${var.production_api == "remus" ? var.es_config["index"] : var.es_config_stage["index"]}"
-    es_doc_type = "${var.production_api == "remus" ? var.es_config["doc_type"] : var.es_config_stage["doc_type"]}"
-    es_username = "${var.production_api == "remus" ? var.es_config["username"] : var.es_config_stage["username"]}"
-    es_password = "${var.production_api == "remus" ? var.es_config["password"] : var.es_config_stage["password"]}"
-    es_protocol = "${var.production_api == "remus" ? var.es_config["protocol"] : var.es_config_stage["protocol"]}"
+    es_host     = "${data.template_file.es_cluster_host_remus.rendered}"
+    es_port     = "${var.es_config_remus["port"]}"
+    es_name     = "${var.es_config_remus["name"]}"
+    es_index    = "${var.es_config_remus["index"]}"
+    es_doc_type = "${var.es_config_remus["doc_type"]}"
+    es_username = "${var.es_config_remus["username"]}"
+    es_password = "${var.es_config_remus["password"]}"
+    es_protocol = "${var.es_config_remus["protocol"]}"
   }
 
   loadbalancer_cloudwatch_id   = "${module.api_alb.cloudwatch_id}"
