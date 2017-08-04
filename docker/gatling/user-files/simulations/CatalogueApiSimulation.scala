2d0387d2608f52b5b96531ad3c164b24f647ed45
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-08-04 13:29:26 +0100

    Adds simple load test for the catalague API (#753)

---
+++ docker/gatling/user-files/simulations/CatalogueApiSimulation.scala
@@ -0,0 +1,39 @@
+package testing.load
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class CatalogueApiSimulation extends Simulation {
+
+  val defaultUsersPerSec = 100
+  val defaultDuration = 120
+
+  val usersPerSec = sys.env.get("USERS_PER_SEC").map(_.toInt)
+    .getOrElse(defaultUsersPerSec)
+
+  val duration = sys.env.get("DEFAULT_DURATION").map(_.toInt)
+    .getOrElse(defaultDuration)
+
+  val httpConf = http
+    .baseURL("https://api.wellcomecollection.org/catalogue/v0")
+    .inferHtmlResources()
+
+  val searchFeeder = csv("terms.csv").random
+
+  val searchScn = scenario("simple-search")
+    .feed(searchFeeder)
+    .exec(http("works")
+      .get("/works?query=${term}")
+      .check(status.in(200, 304))
+    )
+
+  setUp(
+    searchScn.inject(constantUsersPerSec(usersPerSec) during(duration seconds) randomized)
+  ).protocols(
+    httpConf
+  ).assertions(
+    global.responseTime.max.lt(500),
+    global.successfulRequests.percent.gt(99)
+  )
+}
