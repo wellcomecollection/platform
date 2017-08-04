05ce16fa470802051dc5c0df3efee0c360360e16
Alex Chan <a.chan@wellcome.ac.uk>
2017-08-04 10:58:22 +0100

    Fix the Loris installation script

--- docker/loris/install_loris.sh
+++ docker/loris/install_loris.sh
@@ -12,13 +12,13 @@ apt-get install -y libjpeg-turbo8-dev libfreetype6-dev zlib1g-dev \
 # Download and install the Loris code itself
 apt-get install -y unzip wget
 wget https://github.com/loris-imageserver/loris/archive/development.zip
-unzip v2.1.0-final.zip
-rm v2.1.0-final.zip
+unzip development.zip
+rm development.zip
 apt-get remove -y unzip wget
 
 # Required or setup.py complains
 useradd -d /var/www/loris -s /sbin/false loris
 
-cd loris-2.1.0-final
+cd loris-development
 pip install -r requirements.txt
 python setup.py install --image-cache=/mnt/efs/image_cache --info-cache=/mnt/efs/info_cache
