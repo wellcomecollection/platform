438cbf39b0dcf03dc0b2dbfff6a822674569582b
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-08-07 14:34:19 +0100

    Revert "Remove EFS config from Loris infra as we no longer use it (#758)"
    This reverts commit 130079e35dbe7603d1fd259569f5e688e6f108b0.

--- terraform/userdata.tf
+++ terraform/userdata.tf
@@ -10,6 +10,7 @@ module "monitoring_userdata" {
 }
 
 module "api_userdata" {
-  source       = "./userdata"
-  cluster_name = "${aws_ecs_cluster.api.name}"
+  source            = "./userdata"
+  cluster_name      = "${aws_ecs_cluster.api.name}"
+  efs_filesystem_id = "${module.loris_efs.efs_id}"
 }
