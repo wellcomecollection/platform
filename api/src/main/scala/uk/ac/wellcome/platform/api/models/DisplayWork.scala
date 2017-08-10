d449d9d0fe0fb39271bf0921087231c483b841fd
Jonathan Tweed <j.tweed@wellcome.ac.uk>
2017-08-10 18:14:01 +0100

    Added initial docs for Work model and connected to /works/{id} response

--- api/src/main/scala/uk/ac/wellcome/platform/api/models/DisplayWork.scala
+++ api/src/main/scala/uk/ac/wellcome/platform/api/models/DisplayWork.scala
@@ -12,16 +12,16 @@ import uk.ac.wellcome.utils.JsonUtil
   description="An individual work such as a text, archive item or picture; or a grouping of individual works (so, for instance, an archive collection counts as a work, as do all the series and individual files within it).  Each work may exist in multiple instances (e.g. copies of the same book).  N.B. this is not synonymous with \\\"work\\\" as that is understood in the International Federation of Library Associations and Institutions' Functional Requirements for Bibliographic Records model (FRBR) but represents something lower down the FRBR hierarchy, namely manifestation. Groups of related items are also included as works because they have similar properties to the individual ones."
 )
 case class DisplayWork(
-  id: String,
+  @ApiModelProperty(readOnly = true, value = "The canonical identifier given to a thing.") id: String,
   @ApiModelProperty(value = "The title or other short label of a work, including labels not present in the actual work or item but applied by the cataloguer for the purposes of search or description.") title: String,
   @ApiModelProperty(dataType = "String", value = "A description given to a thing.") description: Option[String] = None,
   @ApiModelProperty(dataType = "String", value = "Recording written text on a (usually visual) work.") lettering: Option[String] = None,
   @ApiModelProperty(dataType = "uk.ac.wellcome.models.Period", value = "Relates the creation of a work to a date, when the date of creation does not cover a range.") createdDate: Option[Period] = None,
   @ApiModelProperty(value = "Relates a work to its author, compiler, editor, artist or other entity responsible for its coming into existence in the form that it has.") creators: List[Agent] = List(),
-  @ApiModelProperty(value = "Relates the item to a unique system-generated identifier that governs interaction between systems and is regarded as canonical within the Wellcome data ecosystem.") identifiers: Option[List[DisplayIdentifier]] = None),
-  subjects: List[Concept] = List(),
-  genres: List[Concept] = List()) {
-  @ApiModelProperty(value = "A type of thing") @JsonProperty("type") val ontologyType: String = "Work"
+  @ApiModelProperty(dataType = "List[uk.ac.wellcome.platform.api.models.DisplayIdentifier]", value = "Relates the item to a unique system-generated identifier that governs interaction between systems and is regarded as canonical within the Wellcome data ecosystem.") identifiers: Option[List[DisplayIdentifier]] = None,
+  @ApiModelProperty(value = "Relates a work to the general thesaurus-based concept that describes the work's content.") subjects: List[Concept] = List(),
+  @ApiModelProperty(value = "Relates a work to the genre that describes the work's content.") genres: List[Concept] = List()) {
+  @ApiModelProperty(readOnly = true, value = "A type of thing") @JsonProperty("type") val ontologyType: String = "Work"
 }
 
 case object DisplayWork {
@@ -59,9 +59,14 @@ case object DisplayWork {
   }
 }
 
-case class DisplayIdentifier(identifierScheme: String, value: String) {
-  @JsonProperty("type")
-  val ontologyType: String = "Identifier"
+@ApiModel(
+  value="Identifier",
+  description="A unique system-generated identifier that governs interaction between systems and is regarded as canonical within the Wellcome data ecosystem."
+)
+case class DisplayIdentifier(
+  @ApiModelProperty(value = "Relates a Identifier to a particular authoritative source identifier scheme: for example, if the identifier is MS.49 this property might indicate that this identifier has its origins in the Wellcome Library's CALM archive management system.") identifierScheme: String,
+  @ApiModelProperty(value = "The value of the thing. e.g. an identifier") value: String) {
+  @ApiModelProperty(readOnly = true, value = "A type of thing") @JsonProperty("type") val ontologyType: String = "Identifier"
 }
 
 object DisplayIdentifier {
