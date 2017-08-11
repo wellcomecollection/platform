992e1b311aad7b4103ad361365245cb0dbadcfca
Alex Chan <a.chan@wellcome.ac.uk>
2017-08-11 14:25:11 +0100

    Also get human-friendly reasons for errors

--- lambdas/post_to_slack/post_to_slack.py
+++ lambdas/post_to_slack/post_to_slack.py
@@ -13,6 +13,22 @@ from urllib.parse import quote
 from botocore.vendored import requests
 
 
+# Alarm reasons are sometimes of the form:
+#
+#     Threshold Crossed: 1 datapoint [1.0 (11/08/18 10:55:00)] was
+#     greater than or equal to the threshold (1.0).
+#
+# This regex is meant to match the datapoint in square brackets.
+DATAPOINT_RE = re.compile(r'''
+    \[
+      (?P<value>[0-9.]+)\s
+      \(
+        (?P<timestamp>[0-9]{2}/[0-9]{2}/[0-9]{2}\s[0-9]{2}:[0-9]{2}:[0-9]{2})
+      \)
+    \]
+''', flags=re.VERBOSE)
+
+
 class Alarm:
     def __init__(self, json_message):
         self.message = json.loads(json_message)
@@ -41,22 +57,39 @@ class Alarm:
     def state_change_time(self):
         return self.message['StateChangeTime']
 
+    def human_reason(self):
+        """
+        Try to return a more human-readable explanation for the alarm.
+        """
+        match = DATAPOINT_RE.search(self.state_reason)
+        if match is None:
+            return
+
+        value = int(float(match.group('value')))
+
+        timestamp = match.group('timestamp')
+        time = dt.datetime.strptime(timestamp, '%d/%m/%y %H:%M:%S')
+        display_time = time.strftime('at %H:%M:%S on %d %b %Y')
+
+        if self.name.endswith('-alb-target-500-errors'):
+            if self.name.startswith('loris'):
+                service = 'Loris'
+            elif self.name.startswith('api_'):
+                service = 'the API'
+            else:
+                return
+
+            if value == 1:
+                return f'The ALB spotted a 500 error in {service} {display_time}.'
+            else:
+                return f'The ALB spotted multiple 500 errors ({value}) in {service} {display_time}.'
+
     def cloudwatch_url(self):
         """
         Sometimes there's enough data in the alarm to make an educated guess
         about useful CloudWatch logs to check.  This method tries to do that.
         """
-        # We can get the time from the datapoint in the reason, which is
-        # typically of the form
-        #
-        #     Threshold Crossed: 1 datapoint [1.0 (11/08/18 10:55:00)] was
-        #     greater than or equal to the threshold (1.0).
-        #
-        # So try to get the timestamp.
-        match = re.search(
-            r'\[(?P<datapoint>[0-9.]+) \((?P<timestamp>[0-9]{2}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})\)\]',
-            self.state_reason
-        )
+        match = DATAPOINT_RE.search(self.state_reason)
         if match is None:
             return
 
@@ -108,7 +141,7 @@ def main(event, _):
                       "fields": [
                           {
                               "title": "Reason",
-                              "value": alarm.state_reason
+                              "value": alarm.human_reason() or alarm.state_reason
                           },
                       ]
                   }]}
