216b1c57a868ab798163a09d61236caa3ee008df
Alex Chan <a.chan@wellcome.ac.uk>
2017-08-11 11:01:25 +0100

    Break the code up into distinct functions

--- tasks/elasticsearch_cleaner/elasticsearch_cleaner.py
+++ tasks/elasticsearch_cleaner/elasticsearch_cleaner.py
@@ -11,42 +11,85 @@ import requests
 
 Cluster = collections.namedtuple('Cluster', ['url', 'username', 'password'])
 
-# Get a list of the indices defined in Terraform
-s3 = boto3.client('s3')
-tfvars_obj = s3.get_object(Bucket='platform-infra', Key='terraform.tfvars')
-tfvars_body = tfvars_obj['Body'].read()
-tfvars = hcl.loads(tfvars_body)
 
-indices_in_use = collections.defaultdict(set)
-for key, v in tfvars.items():
-    if not key.startswith('es_config_'):
-        continue
-    url = f'https://{v["name"]}.{v["region"]}.aws.found.io:{v["port"]}'
-    cluster = Cluster(url=url, username=v['username'], password=v['password'])
-    indices_in_use[cluster].add(v['index'])
+def parse_terraform_config(bucket, key):
+    """
+    Return parsed Terraform config from S3.
+    """
+    s3 = boto3.client('s3')
+    tfvars_obj = s3.get_object(Bucket=bucket, Key=key)
+    tfvars_body = tfvars_obj['Body'].read()
+    return hcl.loads(tfvars_body)
 
-# Now ask each cluster: what indices do you know about?
-for cluster, indices in indices_in_use.items():
-    resp = requests.get(
-        urljoin(cluster.url, '/_cat/indices'),
-        params={'format': 'json'},
-        auth=(cluster.username, cluster.password)
-    )
 
-    # Get the names of the indices.  Names beginning with a . are used for
-    # internal ES bookkeeping; ignore them.
-    index_names = [
-        r['index']
-        for r in resp.json()
-        if not r['index'].startswith('.')
-    ]
+def identify_es_config_in_tfvars(tfvars):
+    """
+    Identify the Elasticsearch clusters that are in use in our Terraform.
+    """
+    known_indexes = collections.defaultdict(set)
 
-    # If an index isn't in use, delete it!
-    for i in index_names:
-        if i not in indices:
+    for key, data in tfvars.items():
+        # Elasticsearch config blocks are of the form es_config_{variant},
+        # e.g. es_config_ingestor, es_config_remus
+        if not key.startswith('es_config_'):
+            continue
+
+        url = f'https://{v["name"]}.{v["region"]}.aws.found.io:{v["port"]}'
+        cluster = Cluster(
+            url=url,
+            username=data['username'],
+            password=data['password']
+        )
+        known_indexes[cluster].add(data['index'])
+
+    return known_indexes
+
+
+def find_unused_es_indexes(known_indexes):
+    unused_indexes = collections.defaultdict(set)
+
+    for cluster, indexes in known_indexes.items():
+
+        # Ask the cluster for all the indexes it knows about
+        resp = requests.get(
+            urljoin(cluster.url, '/_cat/indexes'),
+            params={'format': 'json'},
+            auth=(cluster.username, cluster.password)
+        )
+
+        # Get the names of the indexes.  Names beginning with a . are used for
+        # internal ES bookkeeping; ignore them.
+        index_names = [
+            r['index']
+            for r in resp.json()
+            if not r['index'].startswith('.')
+        ]
+
+        for i in index_names:
+            if i not in indexes:
+                unused_indexes[cluster].add(i)
+
+    return unused_indexes
+
+
+def delete_indexes(unused_indexes):
+    for cluster, indexes in unused_indexes.items():
+        for i in indexes:
             print(f'Deleting unused index {i}')
             resp = requests.delete(
                 urljoin(cluster.url, i),
                 auth=(cluster.username, cluster.password)
             )
             resp.raise_for_status()
+
+
+tfvars = parse_terraform_config(
+    bucket='platform-infra',
+    key='terraform.tfvars'
+)
+
+known_indexes = identify_es_config_in_tfvars(tfvars)
+
+unused_indexes = find_unused_es_indexes(known_indexes)
+
+delete_indexes(unused_indexes)
