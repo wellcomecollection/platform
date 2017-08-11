3be60366f9d7e521831d60c184b02cff64d13d81
Alex Chan <a.chan@wellcome.ac.uk>
2017-08-11 13:44:57 +0100

    Try to guess a helpful CloudWatch URL in Slack alerts

--- lambdas/post_to_slack/post_to_slack.py
+++ lambdas/post_to_slack/post_to_slack.py
@@ -4,8 +4,11 @@
 Sends slack notifications for alarms events
 """
 
+import datetime as dt
 import json
 import os
+import re
+from urllib.parse import quote
 
 from botocore.vendored import requests
 
@@ -39,6 +42,54 @@ class Alarm:
         return self.message['StateChangeTime']
 
 
+def guess_cloudwatch_url(alarm):
+    """
+    Sometimes there's enough data in the alarm to make an educated guess
+    about useful CloudWatch logs to check.  This function tries to do that.
+    """
+    # We can get the time from the datapoint in the reason, which is
+    # typically of the form
+    #
+    #     Threshold Crossed: 1 datapoint [1.0 (11/08/18 10:55:00)] was
+    #     greater than or equal to the threshold (1.0).
+    #
+    # So try to get the timestamp.
+    match = re.search(
+        r'\[(?P<datapoint>[0-9.]+) \((?P<timestamp>[0-9]{2}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})\)\]',
+        alarm.state_reason
+    )
+    if match is None:
+        return
+
+    timestamp = match.group('timestamp')
+    time = dt.datetime.strptime(timestamp, '%d/%m/%y %H:%M:%S')
+    start = time - dt.timedelta(seconds=300)
+    end = time + dt.timedelta(seconds=300)
+
+    if alarm.name == 'loris-alb-target-500-errors':
+        group = 'platform/loris'
+        search_term = '"HTTP/1.0 500"'
+    elif alarm.name.startswith('lambda'):
+        lambda_name = alarm.name.split('-')[1]
+        group = f'/aws/lambda/{lambda_name}'
+        search_term = 'Traceback'
+    else:
+        return
+
+    return (
+        'https://eu-west-1.console.aws.amazon.com/cloudwatch/home'
+        '?region=eu-west-1'
+        f'#logEventViewer:group={group};'
+
+        # Look for strings matching 'HTTP/1.0 500'
+        f'filter={quote(search_term)};'
+
+        # And add the date parameters to filter to the exact time
+        f'start={start.strftime("%Y-%m-%dT%H:%M:%SZ")};'
+        f'end={end.strftime("%Y-%m-%dT%H:%M:%SZ")};'
+    )
+
+
 def main(event, _):
     print(f'event = {event!r}')
     alarm = Alarm(event['Records'][0]['Sns']['Message'])
@@ -68,6 +119,13 @@ def main(event, _):
                           }
                       ]
                   }]}
+
+    cloudwatch_url = guess_cloudwatch_url(alarm)
+    if cloudwatch_url is not None:
+        slack_data['attachments'][0]['fields'].append({
+            'title': 'CloudWatch URL',
+            'value': cloudwatch_url
+        })
 
     response = requests.post(
         os.environ["SLACK_INCOMING_WEBHOOK"],
