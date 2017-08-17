9f002f3b4254eff7227afce71b7a8500f7a4c410
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-08-17 14:38:20 +0100

    Add models for Location and License

---
+++ common/src/main/scala/uk/ac/wellcome/models/License.scala
@@ -0,0 +1,11 @@
+package uk.ac.wellcome.models
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+case class License(
+  val licenseType: String,
+  val label: String,
+  val url: String
+) {
+  @JsonProperty("type") val ldType: String = "License"
+}
