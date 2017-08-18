ec5776dceb1d255d2aa5a003380337b6c6746221
Alex Chan <alex@alexwlchan.net>
2017-08-18 20:26:07 +0100

    And some more dead code

--- scripts/autogen/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-from jinja2 import Environment, PackageLoader
-
-from tooling import ROOT, REFERENCE_DATA
-
-env = Environment(loader=PackageLoader('autogen', 'templates'))
