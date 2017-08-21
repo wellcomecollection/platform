07c11c2544e2984c973348a6e8f5aae5c0ea6421
Alex Chan <a.chan@wellcome.ac.uk>
2017-08-21 12:35:37 +0100

    Location, not License

--- common/src/main/scala/uk/ac/wellcome/models/Work.scala
+++ common/src/main/scala/uk/ac/wellcome/models/Work.scala
@@ -21,7 +21,7 @@ case class Work(
   subjects: List[Concept] = List(),
   creators: List[Agent] = List(),
   genres: List[Concept] = List(),
-  thumbnail: Option[License] = None,
+  thumbnail: Option[Location] = None,
   items: List[Item] = List()
 ) {
   @JsonProperty("type") val ontologyType: String = "Work"
