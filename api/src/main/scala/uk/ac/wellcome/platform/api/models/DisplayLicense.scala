65b448b2a3fa45c5f011fa26681c5b6a96f828e4
Alex Chan <a.chan@wellcome.ac.uk>
2017-08-21 11:38:35 +0100

    Plumb through DisplayLicense and DisplayLocation appropriately

--- api/src/main/scala/uk/ac/wellcome/platform/api/models/DisplayLicense.scala
+++ api/src/main/scala/uk/ac/wellcome/platform/api/models/DisplayLicense.scala
@@ -2,6 +2,7 @@ package uk.ac.wellcome.platform.api.models
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.annotations.{ApiModel, ApiModelProperty}
+import uk.ac.wellcome.models.LicenseTrait
 
 @ApiModel(
   value = "License",
@@ -23,4 +24,13 @@ case class DisplayLicense(
 ) {
   @ApiModelProperty(readOnly = true, value = "A type of thing")
   @JsonProperty("type") val ontologyType: String = "License"
+}
+
+case object DisplayLicense {
+  def apply(license: LicenseTrait): DisplayLicense =
+    DisplayLicense(
+      licenseType = license.licenseType,
+      label = license.label,
+      url = license.url
+    )
 }
