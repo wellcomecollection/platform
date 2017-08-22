58239f2fac311f787d85408165f93ac7112b3aae
Alex Chan <a.chan@wellcome.ac.uk>
2017-08-22 17:35:05 +0100

    Build the basic Docker image

---
+++ docker/elasticdump/run_elasticdump.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o nounset
+
+aws s3 cp s3://platform-infra/config/prod/ingestor.ini ingestor_config.ini
+
+# Create the HTTP auth file required by elasticdump
+username=$(awk -F "=" '/es.username/ {print $2}' ingestor_config.ini)
+password=$(awk -F "=" '/es.password/ {print $2}' ingestor_config.ini)
+echo "user=$username"      > auth.ini
+echo "password=$password" >> auth.ini
+
+# Now run the elasticdump tool and pull it down
+hostname=$(awk -F "=" '/es.host/ {print $2}' ingestor_config.ini)
+outfile="dump_$(date)_$INDEX.txt"
+elasticdump --input="https://$hostname/$INDEX" --output="$outfile" --httpAuthFile=auth.ini
+
+# And copy it back up to S3
+aws s3 cp "$outfile" "s3://platform-infra/esdump/$outfile"
