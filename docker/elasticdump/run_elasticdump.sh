060da99654dd651e6ab6020a4e7612850a23739d
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-08-22 17:58:13 +0100

    Pull in a few extra variables

--- docker/elasticdump/run_elasticdump.sh
+++ docker/elasticdump/run_elasticdump.sh
@@ -3,18 +3,18 @@
 set -o errexit
 set -o nounset
 
-aws s3 cp s3://platform-infra/config/prod/ingestor.ini ingestor_config.ini
+aws s3 cp s3://$BUCKET/$CONFIG_KEY config.ini
 
 # Create the HTTP auth file required by elasticdump
-username=$(awk -F "=" '/es.username/ {print $2}' ingestor_config.ini)
-password=$(awk -F "=" '/es.password/ {print $2}' ingestor_config.ini)
+username=$(awk -F "=" '/es.username/ {print $2}' config.ini)
+password=$(awk -F "=" '/es.password/ {print $2}' config.ini)
 echo "user=$username"      > auth.ini
 echo "password=$password" >> auth.ini
 
 # Now run the elasticdump tool and pull it down
-hostname=$(awk -F "=" '/es.host/ {print $2}' ingestor_config.ini)
+hostname=$(awk -F "=" '/es.host/ {print $2}' config.ini)
 outfile="dump_$(date +"%Y-%m-%d_%H-%M-%S")_$INDEX.txt"
 elasticdump --input="https://$hostname/$INDEX" --output="$outfile" --httpAuthFile=auth.ini
 
 # And copy it back up to S3
-aws s3 cp "$outfile" "s3://platform-infra/elasticdump/$outfile"
+aws s3 cp "$outfile" "s3://$BUCKET/elasticdump/$outfile"
