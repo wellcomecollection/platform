3300dc8bd290e92893d987cd2f8abeafc0c69ae5
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-08-22 10:28:22 +0100

    Point the Miro adapter at the right script

--- miro_adapter/run.sh
+++ miro_adapter/run.sh
@@ -3,4 +3,4 @@
 set -o errexit
 set -o nounset
 
-python3 /miro_adapter.py --table="$TABLE" --collection="$COLLECTION" --bucket="$BUCKET" --key="$KEY"
+python3 /miro_adapter/miro_adapter.py --table="$TABLE" --collection="$COLLECTION" --bucket="$BUCKET" --key="$KEY"
