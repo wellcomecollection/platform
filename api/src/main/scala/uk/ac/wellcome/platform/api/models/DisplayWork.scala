07b0647f3dae6e52a3d227acf3c11a121ac91162
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-08-22 19:15:22 +0100

    scalafmt

--- api/src/main/scala/uk/ac/wellcome/platform/api/models/DisplayWork.scala
+++ api/src/main/scala/uk/ac/wellcome/platform/api/models/DisplayWork.scala
@@ -61,8 +61,7 @@ case class DisplayWork(
 
 case object DisplayWork {
 
-  def apply(work: Work,
-            includes: WorksIncludes): DisplayWork = {
+  def apply(work: Work, includes: WorksIncludes): DisplayWork = {
     DisplayWork(
       id = work.id,
       title = work.title,
