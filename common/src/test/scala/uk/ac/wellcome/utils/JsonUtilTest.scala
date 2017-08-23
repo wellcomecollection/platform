2c5e34441af389e3fd52778286d795e0f94824e1
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-08-23 15:20:30 +0100

    Move some of the tests into more appropriate files

--- common/src/test/scala/uk/ac/wellcome/utils/JsonUtilTest.scala
+++ common/src/test/scala/uk/ac/wellcome/utils/JsonUtilTest.scala
@@ -5,18 +5,6 @@ import org.scalatest.{FunSpec, Matchers}
 import uk.ac.wellcome.models.Work
 import uk.ac.wellcome.utils.JsonUtil
 
-class WorkTest extends FunSpec with Matchers {
-  it("should have an LD type 'Work' when serialised to JSON") {
-    val work = Work(
-      identifiers = List(),
-      title = "A book about a blue whale"
-    )
-    val jsonString = JsonUtil.toJson(work).get
-
-    jsonString.contains("""type":"Work"""") should be(true)
-  }
-}
-
 class JsonUtilTest extends FunSpec with Matchers {
   it("should not include fields where the value is empty or None") {
     val work = Work(
