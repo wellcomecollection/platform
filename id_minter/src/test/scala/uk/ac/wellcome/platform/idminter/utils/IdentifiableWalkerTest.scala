892c102e2c65bd74c2fd821ab5efde5bffc6dda9
Alex Chan <a.chan@wellcome.ac.uk>
2017-08-24 09:25:18 +0100

    Test the IdentifiableWalker adds some identifiers

--- id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/utils/IdentifiableWalkerTest.scala
+++ id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/utils/IdentifiableWalkerTest.scala
@@ -73,9 +73,142 @@ class IdentifiableWalkerTest
       .mkString("==")
   }
 
-  private def assertWalkerDoesNothing(jsonString: String) = {
-    assertJsonStringsAreEqual(jsonString, walker.identifyDocument(jsonString))
+  describe("Documents with Identifiable structures should be updated correctly") {
+    it("identify a document that is itself Identifiable") {
+      val sourceIdentifiers = List(SourceIdentifier("australia", "sydney"))
+      val ontologyType = "false capitals"
+
+      assertWalkerAddsCanonicalIdCorrectly(s"""
+      {
+        "canonicalId": "${generateCanonicalId(sourceIdentifiers, ontologyType)}",
+        "identifiers": [{
+          "identifierScheme": "${sourceIdentifiers(0).identifierScheme}",
+          "value": "${sourceIdentifiers(0).value}"
+        }],
+        "type": "${ontologyType}"
+      }
+      """)
+    }
+
+    it("identify a document with a key that is identifiable") {
+      val sourceIdentifiers = List(SourceIdentifier("westeros", "king's landing"))
+      val ontologyType = "fictional cities"
+
+      assertWalkerAddsCanonicalIdCorrectly(s"""
+      {
+        "ke": null,
+        "ki": "kiev",
+        "item": {
+          "canonicalId": "${generateCanonicalId(sourceIdentifiers, ontologyType)}",
+          "identifiers": [{
+            "identifierScheme": "${sourceIdentifiers(0).identifierScheme}",
+            "value": "${sourceIdentifiers(0).value}"
+          }],
+          "type": "${ontologyType}"
+        }
+      }
+      """)
+    }
+
+    it("should pick up all the source identifiers that are available") {
+      val sourceIdentifiersA = List(
+        SourceIdentifier("antarctica", "husvik"),
+        SourceIdentifier("greece", "spinalonga"),
+        SourceIdentifier("ireland", "waterfoot")
+      )
+      val ontologyTypeA = "ghost towns"
+
+      val sourceIdentifiersB = List(
+        SourceIdentifier("ocean", "atlantis"),
+        SourceIdentifier("regal", "camelot")
+      )
+      val ontologyTypeB = "mythological places"
+
+      val sourceIdentifiersC = List(
+        SourceIdentifier("england", "lundenwic"),
+        SourceIdentifier("scotland", "dunedin")
+      )
+      val ontologyTypeC = "cities that were renamed"
+
+      assertWalkerAddsCanonicalIdCorrectly(s"""
+      {
+        "items": [
+          {
+            "canonicalId": "${generateCanonicalId(sourceIdentifiersA, ontologyTypeA)}",
+            "identifiers": [
+              {
+                "identifierScheme": "${sourceIdentifiersA(0).identifierScheme}",
+                "value": "${sourceIdentifiersA(0).value}"
+              },
+              {
+                "identifierScheme": "${sourceIdentifiersA(1).identifierScheme}",
+                "value": "${sourceIdentifiersA(1).value}"
+              },
+              {
+                "identifierScheme": "${sourceIdentifiersA(2).identifierScheme}",
+                "value": "${sourceIdentifiersA(2).value}"
+              }
+            ],
+            "type": "${ontologyTypeA}"
+          },
+          {
+            "canonicalId": "${generateCanonicalId(sourceIdentifiersB, ontologyTypeB)}",
+            "identifiers": [
+              {
+                "identifierScheme": "${sourceIdentifiersB(0).identifierScheme}",
+                "value": "${sourceIdentifiersB(0).value}"
+              },
+              {
+                "identifierScheme": "${sourceIdentifiersB(1).identifierScheme}",
+                "value": "${sourceIdentifiersB(1).value}"
+              }
+            ],
+            "type": "${ontologyTypeB}"
+          }
+        ],
+        "title": "A letter about loss",
+        "lettering": "things we lose have a way of coming back to us in the end, if not always in the way we expect",
+        "subjects": [
+          {
+            "label": "Loss and grief",
+            "type": "Concept"
+          },
+          {
+            "label": "Resurrection and renaming",
+            "canonicalId": "${generateCanonicalId(sourceIdentifiersC, ontologyTypeC)}",
+            "identifiers": [
+              {
+                "identifierScheme": "${sourceIdentifiersC(0).identifierScheme}",
+                "value": "${sourceIdentifiersC(0).value}"
+              },
+              {
+                "identifierScheme": "${sourceIdentifiersC(1).identifierScheme}",
+                "value": "${sourceIdentifiersC(1).value}"
+              }
+            ],
+            "type": "${ontologyTypeC}"
+          }
+        ]
+      }
+      """)
+    }
   }
+
+  // Strip the canonical ID from a JSON string, then run it through the walker
+  // and check it's reinserted correctly.
+  private def assertWalkerAddsCanonicalIdCorrectly(jsonString: String) = {
+    val unidentifiedString = jsonString
+      .lines
+      .filterNot { _.contains("canonicalId") }
+      .mkString("\n")
+    unidentifiedString shouldNot be(jsonString)
+    assertJsonStringsAreEqual(
+      walker.identifyDocument(unidentifiedString),
+      jsonString)
+  }
+
+  private def assertWalkerDoesNothing(jsonString: String) =
+    assertJsonStringsAreEqual(jsonString, walker.identifyDocument(jsonString))
 
   private def assertJsonStringsAreEqual(jsonString1: String, jsonString2: String) = {
     val mapper = new ObjectMapper()
