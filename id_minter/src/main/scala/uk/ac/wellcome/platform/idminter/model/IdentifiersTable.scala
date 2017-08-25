aee144719d3a7b004729a7ef1cfb2d25fe84b83e
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-08-25 13:22:32 +0100

    Do the AltRefNo properly

--- id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/model/IdentifiersTable.scala
+++ id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/model/IdentifiersTable.scala
@@ -6,17 +6,19 @@ import scalikejdbc._
 
 /** Represents a set of identifiers as stored in MySQL */
 case class Identifier(
+  ontologyType: String = "Work",
   CanonicalID: String,
   MiroID: String,
-  ontologyType: String = "Work"
+  CalmAltRefNo: String
 )
 
 object Identifier {
   def apply(p: SyntaxProvider[Identifier])(rs: WrappedResultSet): Identifier =
     Identifier(
+      ontologyType = rs.string(p.resultName.ontologyType)
       CanonicalID = rs.string(p.resultName.CanonicalID),
       MiroID = rs.string(p.resultName.MiroID),
-      ontologyType = rs.string(p.resultName.ontologyType)
+      CalmAltRefNo = rs.string(p.resultName.CalmAltRefNo)
     )
 }
 
