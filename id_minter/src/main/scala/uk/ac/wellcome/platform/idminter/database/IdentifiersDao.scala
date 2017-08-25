c258e493158831b7ec5f9119fbbadfbc4b46c49d
Alex Chan <a.chan@wellcome.ac.uk>
2017-08-25 11:43:49 +0100

    Start building more generic lookups

--- id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/database/IdentifiersDao.scala
+++ id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/database/IdentifiersDao.scala
@@ -5,6 +5,8 @@ import javax.inject.Singleton
 import com.google.inject.Inject
 import com.twitter.inject.Logging
 import scalikejdbc._
+
+import uk.ac.wellcome.models.SourceIdentifier
 import uk.ac.wellcome.platform.idminter.model.{Identifier, IdentifiersTable}
 import uk.ac.wellcome.utils.GlobalExecutionContext.context
 
@@ -15,6 +17,38 @@ class IdentifiersDao @Inject()(db: DB, identifiers: IdentifiersTable)
     extends Logging {
 
   implicit val session = AutoSession(db.settingsProvider)
+
+  def lookupID(sourceIdentifiers: List[SourceIdentifier],
+               ontologyType: String): Future[Option[Identifier]] =
+    Future {
+      blocking {
+        info(s"About to search for existing ID matching $identifiers and $ontologyType")
+        val i = identifiers.i
+        withSQL {
+          select
+            .from(identifiers as i)
+            .where
+
+            // We always want to match the ontology type, and this field
+            // in SQL is never null.
+            .eq(i.ontologyType, ontologyType)
+
+            // This seems awfully repetitive, but I haven't been able to pin
+            // down a type parameter for `sql` that lets me put this in a
+            // function.
+            .map { sql: ConditionSQLBuilder[String] =>
+              val miroID = sourceIdentifiers.filter {
+                _.identifierScheme == "miro-image-number"
+              }
+              if (miroID.isEmpty) {
+                sql
+              } else {
+                sql.and.eq(i.MiroID, miroID.head.value).or.isNull(i.MiroID)
+              }
+            }
+        }.map(Identifier(i)).single.apply()
+      }
+    }
 
   def lookupMiroID(miroID: String,
                    ontologyType: String = "Work"): Future[Option[Identifier]] =
