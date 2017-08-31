c5e27d632aff72b6d8819edcf19ddd2f524eb140
Alex Chan <a.chan@wellcome.ac.uk>
2017-08-31 10:52:22 +0100

    Add a JSON intermediary step to the Miro adapter

---
+++ miro_adapter/build_json_derivatives.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+Parse image records from a Miro export and push JSON derivatives to S3.
+
+The output is a file stored in S3 which encodes Miro records as JSON objects,
+one object per line.  This makes it easier for us to run one-off scripts
+against an entire Miro record set, or find all the source data about a
+particular record.
+
+Usage:
+  build_json_derivatives.py --bucket=<BUCKET> --src=<SRC> --dst=<DST>
+  build_json_derivatives.py -h | --help
+
+Options:
+  -h --help                 Show this screen.
+  --bucket=<BUCKET>         S3 bucket containing the Miro XML dumps.
+  --src=<SRC>               Key of the Miro XML dump in the S3 bucket.
+  --dst=<DST>               Key of the JSON derivatives in the S3 bucket.
+
+"""
+
+import json
+import os
+import tempfile
+
+import boto3
+import docopt
+
+from utils import generate_images
+
+
+if __name__ == '__main__':
+    args = docopt.docopt(__doc__)
+    image_data = generate_images(bucket=args['--bucket'], key=args['--src'])
+
+    tmp_json = tempfile.mktemp()
+    os.makedirs(os.path.dirname(tmp_json), exist_ok=True)
+    with open(tmp_json, 'w') as f:
+        for img in image_data:
+
+            # Adding the separators omits unneeded whitespace in the JSON,
+            # giving us smaller files.
+            f.write(json.dumps(img, separators=(',', ':')) + '\n')
+
+    s3 = boto3.client('s3')
+    s3.upload_file(
+        Bucket=args['--bucket'],
+        Key=args['--dst'],
+        Filename=tmp_json
+    )
+
+    os.unlink(tmp_json)
--- miro_adapter/utils.py
+++ miro_adapter/utils.py
@@ -66,23 +66,45 @@ def fix_miro_xml_entities(xml_string):
     return xml_string
 
 
-def read_image_chunks_from_s3(bucket, key):
+def chunked_s3_reader(bucket, key, delimiter):
     """
-    Loading an entire XML file at once would be prohibitively expensive,
-    but we only need one <image> ... </image> block at a time.
+    Read a file from S3, generating everything up to ``delimiter`` in each
+    iteration.  This avoids loading the whole file into memory.
     """
     client = boto3.client('s3')
     obj = client.get_object(Bucket=bucket, Key=key)
     running = b''
     while True:
         new_data = obj['Body'].read(1024)
+
+        # If a read from S3 doesn't return any new data, then we're at the
+        # end of the file.  Give up anything else we've got, then return.
         if not new_data:
+            yield running
             break
+
         running += new_data
-        if b'</image>' in running:
-            curr, running = running.split(b'</image>')
-            curr = curr.split(b'<image>')[-1]
-            yield (b'<image>' + curr + b'</image>')
+        if delimiter in running:
+            curr, running = running.split(delimiter)
+            yield curr + delimiter
+
+
+def read_image_chunks_from_s3(bucket, key):
+    """
+    Loading an entire XML file at once would be prohibitively expensive,
+    but we only need one <image> ... </image> block at a time.
+    """
+    for chunk in chunked_s3_reader(bucket=bucket, key=key):
+
+        # The last chunk of data from the file will be some XML closing matter,
+        # which is uninteresting.
+        if b'<image>' not in curr:
+            continue
+
+        # We want to return complete XML blocks, so we split looking for the
+        # opening <image> tag, and include it on the result.
+        curr = curr.split(b'<image>')[-1]
+        yield b'<image>' + curr
 
 
 def generate_images(bucket, key):
