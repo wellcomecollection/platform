0a34720d8bd143927ecaabe48de239f5f4cc8b08
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-09-05 11:51:19 +0100

    Use the correct AWS credentials for publishing services

--- builds/publish_service.sh
+++ builds/publish_service.sh
@@ -3,8 +3,9 @@
 set -o errexit
 set -o nounset
 
-docker run --tty \
+ROOT=$(git rev-parse --show-toplevel)
+
+$ROOT/scripts/run_docker_with_aws_credentials.sh --tty \
 	--volume /var/run/docker.sock:/var/run/docker.sock \
 	--volume $(pwd):/repo \
-	--volume ~/.aws:/root/.aws \
 	publish_service_to_aws --project="$PROJECT" --infra-bucket="$INFRA_BUCKET"
