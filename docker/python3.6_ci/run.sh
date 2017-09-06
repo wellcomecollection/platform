8ca4310f8ce072ba3da11ee98aeb1304000002ef
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-09-06 12:39:35 +0100

    move up to date check into python container

--- docker/python3.6_ci/run.sh
+++ docker/python3.6_ci/run.sh
@@ -2,27 +2,38 @@
 
 set -o errexit
 set -o nounset
-set -o verbose
-
-cd /data
 
 if [[ "$OP" == "lint" ]]
 then
-  echo "Linting Python"
+  echo "Linting ..."
+
   flake8 --exclude six.py,six-*,structlog*,simplejson* **/*.py --ignore=E501
+
+  echo "Done."
 elif [[ "$OP" == "test" ]]
 then
-  echo "Testing Lambdas"
+  echo "Testing ..."
 
-  ./install_lambda_deps.sh
+  ./install_deps.sh
   ./test_lambdas.sh
 
+  echo "Done."
 elif [[ "$OP" == "install-deps" ]]
 then
-  echo "Installing Lambda dependencies"
-  ./install_lambda_deps.sh
+  echo "Installing dependencies ..."
 
+  ./install_deps.sh
+
+  echo "Done."
+elif [[ "$OP" == "is-master-head" ]]
+then
+  echo "Checking up to date with master ..."
+
+  /app/is_up_to_date_with_master.py
+
+  echo "Done."
 else
   echo "Unrecognised operation: $OP! Stopping."
+
   exit 1
 fi
