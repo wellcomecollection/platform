e13c88c0164a71a8114254bda476bff880676891
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-09-06 14:09:25 +0100

    ID is now a mandatory field on DisplayItems in the API (#892)
    Now we've merged the ID minter changes to support putting IDs on
    items, we can remove this temporary code.  If an unidentified item
    appears in our Elasticsearch index, it becomes an error.

--- api/src/main/scala/uk/ac/wellcome/platform/api/models/DisplayItem.scala
+++ api/src/main/scala/uk/ac/wellcome/platform/api/models/DisplayItem.scala
@@ -12,7 +12,7 @@ case class DisplayItem(
   @ApiModelProperty(
     dataType = "String",
     readOnly = true,
-    value = "The canonical identifier given to a thing.") id: Option[String],
+    value = "The canonical identifier given to a thing.") id: String,
   @ApiModelProperty(
     dataType = "List[uk.ac.wellcome.platform.api.models.DisplayIdentifier]",
     value =
@@ -30,7 +30,7 @@ case class DisplayItem(
 object DisplayItem {
   def apply(item: Item, includesIdentifiers: Boolean): DisplayItem =
     DisplayItem(
-      id = item.canonicalId,
+      id = item.id,
       identifiers =
         if (includesIdentifiers)
           Some(item.identifiers.map(DisplayIdentifier(_)))
