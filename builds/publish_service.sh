c30c0a77b92a7bb8ed902722fa8494c2c99ab58d
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-09-06 15:51:01 +0100

    Push the make tasks down into loris/Makefile

--- builds/publish_service.sh
+++ builds/publish_service.sh
@@ -7,5 +7,5 @@ ROOT=$(git rev-parse --show-toplevel)
 
 $ROOT/scripts/run_docker_with_aws_credentials.sh --tty \
 	--volume /var/run/docker.sock:/var/run/docker.sock \
-	--volume $(pwd):/repo \
+	--volume $ROOT:/repo \
 	publish_service_to_aws --project="$PROJECT" --infra-bucket="$INFRA_BUCKET"
