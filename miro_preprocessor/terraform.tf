19c8dce49014f57c1e064539749656b66977d595
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-09-07 10:55:22 +0100

    add skeleton project

---
+++ miro_preprocessor/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 0.9"
+
+  backend "s3" {
+    bucket     = "platform-infra"
+    key        = "platform-miro_preprocessor.tfstate"
+    lock_table = "terraform-locktable"
+    region     = "eu-west-1"
+  }
+}
+
+data "terraform_remote_state" "platform" {
+  backend = "s3"
+
+  config {
+    bucket = "platform-infra"
+    key    = "platform.tfstate"
+    region = "eu-west-1"
+  }
+}
