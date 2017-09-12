7276ff2901fc4419a0cbb029a2d59b059d0a936b
Alex Chan <a.chan@wellcome.ac.uk>
2017-09-12 13:59:03 +0100

    Make the .releases directory a variable

--- miro_preprocessor/generate_tfvars.sh
+++ miro_preprocessor/generate_tfvars.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
+ROOT="$(git rev-parse --show-toplevel)"
+RELEASE_DIR="$ROOT/.releases"
+
 # Name of tfvars file
 TF_VARS=terraform.tfvars
 RELEASE_IDS_FILE="release_ids.tfvars"
 
 # Ensure we don't have stale variables from a previous run
 rm -f $TF_VARS
-rm -rf releases
+rm -rf "$RELEASE_DIR"
 
 # Download releases from S3
-mkdir -p releases
-aws s3 cp s3://platform-infra/releases releases --recursive
+mkdir -p "$RELEASE_DIR"
+aws s3 cp s3://platform-infra/releases "$RELEASE_DIR" --recursive
 
 # Build a tfvars file containing the release ids
 echo "release_ids = {" >> "$TF_VARS"
-for f in releases/*;
+for f in "$RELEASE_DIR"/*;
 do
   echo "Processing $f: $(cat $f)"
   echo "  $(basename $f) = \"$(cat $f)\"" >> "$TF_VARS"
