a6a6cf9854e5b6b6598efecc968a1351f96ab517
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-09-13 11:08:24 +0100

    Create HTTP 404 errors

--- common/src/main/scala/uk/ac/wellcome/models/Error.scala
+++ common/src/main/scala/uk/ac/wellcome/models/Error.scala
@@ -6,8 +6,22 @@ case class Error(
   errorType: String,
   httpStatus: Option[Int],
   label: String,
-  description: Option[String],
-  reference: Option[String]
+  description: Option[String]
 ) {
   @JsonProperty("type") val ontologyType: String = "Error"
+}
+
+case object Error {
+  def apply(variant: String, description: String): Error = {
+    variant match {
+      case "http-404" => Error(
+        errorType = "http",
+        httpStatus = Some(404),
+        label = "Not Found",
+        description = Some(description)
+      )
+      case unknownVariant =>
+        throw new Exception(s"$unknownVariant is not a valid error variant")
+    }
+  }
 }
