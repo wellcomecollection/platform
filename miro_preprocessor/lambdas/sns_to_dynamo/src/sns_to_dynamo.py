759c3de6fa8aa76badf280c3de6dc714e5e0bbc2
caylaa <alicefuzier@gmail.com>
2017-09-13 12:25:33 +0100

    Make it actually put stuff into the dynamo table

--- miro_preprocessor/lambdas/sns_to_dynamo/src/sns_to_dynamo.py
+++ miro_preprocessor/lambdas/sns_to_dynamo/src/sns_to_dynamo.py
@@ -1,2 +1,35 @@
-def main():
-    pass
+import json
+import os
+import boto3
+
+
+def main(event, _):
+    print(f'Received event:\n{event}')
+
+    client = boto3.client('dynamodb')
+    table_name = os.environ["TABLE_NAME"]
+    image_info = json.loads(event['Records'][0]['Sns']['Message'])
+    image_data = image_info['image_data']
+    collection = image_info['collection']
+
+    print('Pushing image with ID %s' % (image_data['image_no_calc']))
+    client.put_item(
+        TableName=table_name,
+        Item={
+            'MiroID': {
+                'S': image_data['image_no_calc']
+            },
+            'MiroCollection': {
+                'S': collection
+            },
+            'ReindexShard': {
+                'S': 'default'
+            },
+            'ReindexVersion': {
+                'N': str(1)
+            },
+            'data': {
+                'S': json.dumps(image_data, separators=(',', ':'))
+            }
+        }
+    )
--- miro_preprocessor/lambdas/sns_to_dynamo/src/test_sns_to_dynamo.py
+++ miro_preprocessor/lambdas/sns_to_dynamo/src/test_sns_to_dynamo.py
@@ -9,10 +9,11 @@ import sns_to_dynamo
 
 @mock_dynamodb2
 def test_should_insert_the_json_into_dynamo():
-    dynamodb_client = boto3.client('dynamodb')
+    dynamodb = boto3.resource('dynamodb')
 
     table_name = "TestMiroData"
-    dynamodb_client.create_table(
+
+    table = dynamodb.create_table(
         TableName=table_name,
         KeySchema=[
             {
@@ -74,14 +75,21 @@ def test_should_insert_the_json_into_dynamo():
     )
 
     miro_id = "A0000002"
-    image_json = f"""{{
-        "image_no_calc": "{miro_id}",
-        "image_int_default":null,
-        "image_artwork_date_from":"01/02/2000",
-        "image_artwork_date_to":"13/12/2000",
-        "image_barcode":"10000000",
-        "image_creator":["Caspar Bauhin"]
-    }}"""
+
+    collection = "Images-C"
+    image_data = {
+        'image_no_calc': miro_id,
+        'image_int_default': None,
+        'image_artwork_date_from': "01/02/2000",
+        'image_artwork_date_to': "13/12/2000",
+        'image_barcode': "10000000",
+        'image_creator': ["Caspar Bauhin"]
+    }
+
+    image_json = json.dumps({
+        'collection': collection,
+        'image_data': image_data
+    })
 
     os.environ = {
         "TABLE_NAME": table_name
@@ -92,15 +100,14 @@ def test_should_insert_the_json_into_dynamo():
             'EventSource': 'aws:sns',
             'EventVersion': '1.0',
             'EventSubscriptionArn':
-                'arn:aws:sns:region:account_id:alb_server_error_alarm:stuff',
+                'arn:aws:sns:region:account_id:sns:stuff',
             'Sns': {
                 'Type': 'Notification',
                 'MessageId': 'b20eb72b-ffc7-5d09-9636-e6f65d67d10f',
                 'TopicArn':
-                    'arn:aws:sns:region:account_id:alb_server_error_alarm',
-                'Subject':
-                    'ALARM: "api-alb-target-500-errors" in EU - Ireland',
-                'Message': json.dumps(image_json),
+                    'arn:aws:sns:region:account_id:sns',
+                'Subject': None,
+                'Message': image_json,
                 'Timestamp': '2017-07-10T15:42:24.307Z',
                 'SignatureVersion': '1',
                 'Signature': 'signature',
@@ -112,15 +119,9 @@ def test_should_insert_the_json_into_dynamo():
 
     sns_to_dynamo.main(event, None)
 
-    dynamodb_response = dynamodb_client.get_item(
-        TableName=table_name,
-        Key={
-            'MiroID': {
-                'S': miro_id
-            }
-        })
+    dynamodb_response = table.get_item(Key={'MiroID': miro_id, 'MiroCollection': collection})
 
-    assert dynamodb_response['Item']['data'] == image_json
-    assert dynamodb_response['Item']['MiroCollection'] == "bu"
+    assert json.loads(dynamodb_response['Item']['data']) == image_data
+    assert dynamodb_response['Item']['MiroCollection'] == collection
     assert dynamodb_response['Item']['ReindexShard'] == "default"
-    assert dynamodb_response['Item']['ReindexVersion'] == 0
+    assert dynamodb_response['Item']['ReindexVersion'] == 1
