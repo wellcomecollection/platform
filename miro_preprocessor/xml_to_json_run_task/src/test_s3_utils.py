c0cac8319dadae29f11164a38fd70af19e8c9ce5
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-09-13 10:26:40 +0100

    add first draft of lambda

---
+++ miro_preprocessor/xml_to_json_run_task/src/test_s3_utils.py
@@ -0,0 +1,65 @@
+from utils import s3_utils
+
+import dateutil.parser
+
+
+def s3_event():
+    return {
+        "Records": [
+            {
+                "eventVersion": "2.0",
+                "eventSource": "aws:s3",
+                "awsRegion": "us-east-1",
+                "eventTime": "1970-01-01T00:00:00.000Z",
+                "eventName": "event-type",
+                "userIdentity": {
+                    "principalId": "Amazon-customer-ID-of-the-user-who-caused-the-event"
+                },
+                "requestParameters": {
+                    "sourceIPAddress": "ip-address-where-request-came-from"
+                },
+                "responseElements": {
+                    "x-amz-request-id": "Amazon S3 generated request ID",
+                    "x-amz-id-2": "Amazon S3 host that processed the request"
+                },
+                "s3": {
+                    "s3SchemaVersion": "1.0",
+                    "configurationId": "ID found in the bucket notification configuration",
+                    "bucket": {
+                        "name": "bucket-name",
+                        "ownerIdentity": {
+                            "principalId": "Amazon-customer-ID-of-the-bucket-owner"
+                        },
+                        "arn": "bucket-ARN"
+                    },
+                    "object": {
+                        "key": "bucket-name",
+                        "size": 1234,
+                        "eTag": "object eTag",
+                        "versionId": "v2",
+                        "sequencer": "foo"
+                    }
+                }
+            },
+            {
+            }
+        ]
+    }
+
+
+def test_parse_s3_event():
+    e = s3_event()
+    parsed_events = s3_utils.parse_s3_record(e)
+    expected_datetime = dateutil.parser.parse("1970-01-01T00:00:00.000Z")
+
+    expected_events = [{
+        "event_name": "event-type",
+        "event_time": expected_datetime,
+        "bucket_name": "bucket-name",
+        "object_key": "bucket-name",
+        "size": 1234,
+        "versionId": "v2"
+    }]
+
+    assert parsed_events == expected_events
+
---
+++ miro_preprocessor/xml_to_json_run_task/src/xml_to_json_run_task.py
@@ -0,0 +1,58 @@
+# -*- encoding: utf-8 -*-
+
+"""
+Foo
+"""
+
+import os
+
+import boto3
+
+import s3_utils
+import sns_utils
+
+def command_for_xml_to_json_task(event, cluster_name, container_name, task_definition):
+    bucket_name = event["bucket_name"]
+    object_src = event["object_src"]
+    object_dst = f'{object_src}.txt'
+
+    cmd = [
+        f'--bucket={bucket_name}',
+        f'--src={object_src}',
+        f'--src={object_dst}'
+    ]
+
+    return {
+        "cluster_name": cluster_name,
+        "container_name": container_name,
+        "task_definition": task_definition,
+        "started_by": "xml_to_json_run_task",
+        "command": cmd,
+    }
+
+def post_to_sns(sns_client, topic_arn, cmd):
+    return sns_utils.publish_sns_message(
+        sns_client,
+        topic_arn,
+        cmd
+    )
+
+def main(event, _):
+    print(f'event = {event!r}')
+
+    topic_arn = os.environ["TOPIC_ARN"]
+    cluster_name = os.environ["CLUSTER_NAME"]
+    container_name = os.environ["CONTAINER_NAME"]
+    task_definition = os.environ["TASK_DEFINITION_ARN"]
+
+    sns_client = boto3.client('sns')
+
+    s3_events =  s3_utils.parse_s3_record(event=event)
+    task_commands = [command_for_xml_to_json_task(
+        event,
+        cluster_name,
+        container_name,
+        task_definition
+    ) for event in s3_events]
+
+    [post_to_sns(sns_client, topic_arn, cmd) for cmd in task_commands]
