613349485318edd6e05348b165d4411073da30a9
Alex Chan <a.chan@wellcome.ac.uk>
2017-09-14 14:38:12 +0100

    Further sharding of Gatling results

--- docker/gatling/run.sh
+++ docker/gatling/run.sh
@@ -10,7 +10,7 @@ $GATLING_HOME/bin/gatling.sh --simulation $SIMULATION --run-description="$DESCRI
 GATLING_STATUS=$?
 
 LAST_RESULT="$(find /opt/gatling/results -maxdepth 1 -type d | sort | tail -n 1)"
-S3_LOCATION="s3://$S3_BUCKET/gatling/$(date +"%Y/%m")/$(basename $LAST_RESULT)"
+S3_LOCATION="s3://$S3_BUCKET/gatling/$(date +"%Y/%m/%d/%H")/%M_%S_$(basename $LAST_RESULT)"
 
 aws s3 cp \
     --acl=public-read \
