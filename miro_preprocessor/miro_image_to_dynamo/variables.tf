41c0d82b389cc8ef42f504330cab80949c9fc8d4
caylaa <alicefuzier@gmail.com>
2017-09-14 11:34:21 +0100

    Initial terraform configuration for miro_image_to_dynamo lambda

---
+++ miro_preprocessor/miro_image_to_dynamo/variables.tf
@@ -0,0 +1,15 @@
+variable "lambda_error_alarm_arn" {
+}
+
+variable "miro_image_to_dynamo_topic_arn" {
+}
+
+variable "miro_data_table_arn" {
+}
+
+variable "s3_bucket_arn" {
+}
+variable "s3_bucket_name" {
+}
+variable "miro_data_table_name" {
+}
