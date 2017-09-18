406a0ec77212e68a5ef9fce24f621a7b170bb9f6
Alex Chan <a.chan@wellcome.ac.uk>
2017-09-18 12:05:19 +0100

    dynamo*db* table

--- miro_preprocessor/terraform.tf
+++ miro_preprocessor/terraform.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = ">= 0.9"
 
   backend "s3" {
-    bucket       = "platform-infra"
-    key          = "platform-miro_preprocessor.tfstate"
-    dynamo_table = "terraform-locktable"
-    region       = "eu-west-1"
+    bucket         = "platform-infra"
+    key            = "platform-miro_preprocessor.tfstate"
+    dynamodb_table = "terraform-locktable"
+    region         = "eu-west-1"
   }
 }
 
