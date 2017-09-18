4c92bcdefab7a1fc36a7ac24843a4bc1fe3316e2
caylaa <alicefuzier@gmail.com>
2017-09-18 17:09:55 +0100

    Avoid resending the image if the checksum matches

--- miro_preprocessor/miro_copy_s3_asset/src/miro_copy_s3_asset.py
+++ miro_preprocessor/miro_copy_s3_asset/src/miro_copy_s3_asset.py
@@ -8,6 +8,20 @@ from botocore.exceptions import ClientError
 import sns_utils
 
 
+def get_content_md5(source_head_response):
+    return source_head_response['ResponseMetadata']['HTTPHeaders']['Content-MD5']
+
+
+def copy_image_asset(s3_client, source_bucket_name, source_key, destination_bucket_name, destination_key):
+    s3_client.copy_object(
+        CopySource={
+            'Bucket': source_bucket_name,
+            'Key': source_key
+        },
+        Bucket=destination_bucket_name,
+        Key=destination_key)
+
+
 def main(event, _):
     sns_client = boto3.client("sns")
     s3_client = boto3.client("s3")
@@ -23,7 +37,7 @@ def main(event, _):
     key = f"fullsize/{shard}/{miro_id}.jpg"
 
     try:
-        s3_client.head_object(Bucket=source_bucket_name, Key=key)
+        source_head_response = s3_client.head_object(Bucket=source_bucket_name, Key=key)
     except ClientError as client_error:
         if client_error.response['Error']['Code'] == '404':
             print(f"No image found for MiroId {miro_id}: skipping")
@@ -32,17 +46,21 @@ def main(event, _):
             raise
     else:
         destination_key = f"{shard}/{miro_id}.jpg"
-
-        s3_client.copy(
-            CopySource={
-                'Bucket': source_bucket_name,
-                'Key': key
-            },
-            Bucket=destination_bucket_name,
-            Key=destination_key)
+        try:
+            destination_head_response = s3_client.head_object(Bucket=destination_bucket_name, Key=destination_key)
+        except ClientError as client_error:
+            if client_error.response['Error']['Code'] == '404':
+                print(f"Destination bucket has no image: copying from {source_bucket_name}/{key} to {destination_bucket_name}/{destination_key}")
+                copy_image_asset(s3_client, source_bucket_name, key, destination_bucket_name, destination_key)
+                pass
+            else:
+                raise
+        else:
+            if not get_content_md5(source_head_response) == get_content_md5(destination_head_response):
+                print(f"Destination bucket has image with different hash: copying from {source_bucket_name}/{key} to {destination_bucket_name}/{destination_key}")
+                copy_image_asset(s3_client, source_bucket_name, key, destination_bucket_name, destination_key)
 
         sns_utils.publish_sns_message(
             sns_client,
             topic_arn,
-            image_info
-        )
+            image_info)
