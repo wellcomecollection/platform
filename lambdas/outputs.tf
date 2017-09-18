9fe2295ed811c2c08ab0643fe1c4af1e35954a41
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-09-18 10:37:39 +0100

    Add permissions for SNS run task (#933)
    * Add permissions for SNS run task
    
    * add perms to actually run a task

--- lambdas/outputs.tf
+++ lambdas/outputs.tf
@@ -5,3 +5,7 @@ output "lambda_error_alarm_arn" {
 output "run_ecs_task_topic_arn" {
   value = "${module.run_ecs_task.arn}"
 }
+
+output "run_ecs_task_topic_publish_policy" {
+  value = "${module.run_ecs_task.publish_policy}"
+}
