66b6779e4fbbf7cb1c1ef6f4f2e80866049f8fa1
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-09-18 15:02:42 +0100

    Get a minimal Loris 'terraform plan' working

---
+++ loris/terraform/cloudfront.tf
@@ -0,0 +1,60 @@
+resource "aws_cloudfront_distribution" "loris" {
+  origin {
+    domain_name = "iiif-origin.wellcomecollection.org"
+    origin_id   = "loris"
+
+    custom_origin_config {
+      https_port             = 443
+      http_port              = 80
+      origin_protocol_policy = "match-viewer"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+
+  aliases = ["iiif.wellcomecollection.org"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "loris"
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+
+      headers = ["User-Agent"]
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    min_ttl     = 0
+    default_ttl = 315576000
+    max_ttl     = 315576000
+  }
+
+  price_class = "PriceClass_100"
+
+  viewer_certificate {
+    acm_certificate_arn      = "${var.iiif_acm_cert_arn}"
+    minimum_protocol_version = "TLSv1"
+    ssl_support_method       = "sni-only"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  logging_config {
+    include_cookies = false
+    bucket          = "${data.terraform_remote_state.platform.cloudfront_logs_domain_name}"
+    prefix          = "loris"
+  }
+}
--- loris/terraform/terraform.tf
+++ loris/terraform/terraform.tf
@@ -8,3 +8,13 @@ terraform {
     region         = "eu-west-1"
   }
 }
+
+data "terraform_remote_state" "platform" {
+  backend = "s3"
+
+  config {
+    bucket = "platform-infra"
+    key    = "platform.tfstate"
+    region = "eu-west-1"
+  }
+}
