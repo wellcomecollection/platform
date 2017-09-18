56e67c7d6907ad70bc099194974a5aad74d7fb9f
Alex Chan <a.chan@wellcome.ac.uk>
2017-09-18 10:05:49 +0100

    Add a skeleton for the sorter logic

---
+++ miro_preprocessor/image_sorter/src/sorter_logic.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+"""
+This file contains the logic for sorting images into their final destination.
+
+It only has a single public function: ``sort_image``, which takes a Python
+dictionary and returns an instance of ``Decision``.
+"""
+
+import enum
+
+
+class Decision(enum.Enum):
+    cold_store = 'cold_store'
+    tandem_vault = 'tandem_vault'
+    digital_library = 'digital_library'
+
+
+class Undecidable(Exception):
+    pass
+
+
+def sort_image(metadata):
+    raise Undecidable(f'Reached the default fallthrough for {metadata!r}')
