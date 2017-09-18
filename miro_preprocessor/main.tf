cacd48db926ccbe50a3d592c3ca736983932f0cf
Alex Chan <a.chan@wellcome.ac.uk>
2017-09-18 10:49:19 +0100

    That was a mistake

--- miro_preprocessor/main.tf
+++ miro_preprocessor/main.tf
@@ -4,8 +4,8 @@ module "xml_to_json_converter" {
   bucket_miro_data_id = "${data.terraform_remote_state.platform.bucket_miro_data_id}"
   release_ids         = "${var.release_ids}"
 
-  s3_read_miro_data_json  = "${data.aws_iam_publish_policy_document.s3_read_miro_data.json}"
-  s3_write_miro_data_json = "${data.aws_iam_publish_policy_document.s3_write_miro_data.json}"
+  s3_read_miro_data_json  = "${data.aws_iam_policy_document.s3_read_miro_data.json}"
+  s3_write_miro_data_json = "${data.aws_iam_policy_document.s3_write_miro_data.json}"
 }
 
 module "xml_to_json_run_task" {
@@ -15,7 +15,7 @@ module "xml_to_json_run_task" {
   bucket_miro_data_arn   = "${data.terraform_remote_state.platform.bucket_miro_data_arn}"
   lambda_error_alarm_arn = "${data.terraform_remote_state.lambda.lambda_error_alarm_arn}"
 
-  s3_read_miro_data_json = "${data.aws_iam_publish_policy_document.s3_read_miro_data.json}"
+  s3_read_miro_data_json = "${data.aws_iam_policy_document.s3_read_miro_data.json}"
 
   container_name      = "${module.xml_to_json_converter.container_name}"
   topic_arn           = "${data.terraform_remote_state.lambda.run_ecs_task_topic_arn}"
