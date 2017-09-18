b5c251b5d1a90ec977e1fc2df0aa797d6b38c6dd
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-09-18 13:59:00 +0100

    Shorten CloudWatch URLs with bit.ly

--- lambdas/post_to_slack/variables.tf
+++ lambdas/post_to_slack/variables.tf
@@ -4,3 +4,4 @@ variable "terminal_failure_alarm_arn" {}
 variable "alb_server_error_alarm_arn" {}
 variable "dlq_alarm_arn" {}
 variable "ec2_instance_terminating_for_too_long_alarm_arn" {}
+variable "bitly_access_token" {}
