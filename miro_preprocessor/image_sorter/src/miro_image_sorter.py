d893ccbacdffa1ae593174365814402c17179372
Alex Chan <a.chan@wellcome.ac.uk>
2017-09-18 11:28:42 +0100

    Actually push to SNS

--- miro_preprocessor/image_sorter/src/miro_image_sorter.py
+++ miro_preprocessor/image_sorter/src/miro_image_sorter.py
@@ -5,6 +5,9 @@ import os
 
 import boto3
 
+from sorter_logic import Decision, sort_image
+from sns_utils import publish_sns_message
+
 
 def parse_s3_event(event):
     """
@@ -29,10 +32,21 @@ def main(event, _):
     # Parse environment config
     topic_cold_store = os.environ['TOPIC_COLD_STORE']
     topic_tandem_vault = os.environ['TOPIC_TANDEM_VAULT']
-    topic_digital_library = os.environ['TOPIC_DIGITAL_LIBRARY']
+    topic_catalogue_api = os.environ['TOPIC_DIGITAL_LIBRARY']
 
     s3_bucket = os.environ['S3_MIRODATA_ID']
     s3_key = parse_s3_event(event)
 
     # Fetch the metadata from S3
     metadata = fetch_s3_metadata(bucket=s3_bucket, key=s3_key)
+
+    # Decide where to put it, then send the metadata to SNS
+    decision = sort_image(metadata)
+
+    topic_arns = {
+        Decision.cold_store: topic_cold_store,
+        Decision.tandem_vault: topic_tandem_vault,
+        Decision.catalogue_api: topic_catalogue_api
+    }
+
+    publish_sns_message(topic_arn=topic_arns[decision], message=metadata)
