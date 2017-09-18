2e895f846d8c94349a959b714199d3e461a827bb
Alex Chan <a.chan@wellcome.ac.uk>
2017-09-18 11:00:05 +0100

    You actually do want ARNs there

--- miro_preprocessor/image_sorter/main.tf
+++ miro_preprocessor/image_sorter/main.tf
@@ -6,8 +6,8 @@ module "image_sorter_lambda" {
   alarm_topic_arn = "${var.lambda_error_alarm_arn}"
 
   environment_variables = {
-    TOPIC_COLD_STORE    = "${var.topic_cold_store_publish_policy}"
-    TOPIC_TANDEM_VAULT  = "${var.topic_tandem_vault_publish_policy}"
-    TOPIC_CATALOGUE_API = "${var.topic_catalogue_api_publish_policy}"
+    TOPIC_COLD_STORE    = "${var.topic_cold_store_publish_arn}"
+    TOPIC_TANDEM_VAULT  = "${var.topic_tandem_vault_publish_arn}"
+    TOPIC_CATALOGUE_API = "${var.topic_catalogue_api_publish_arn}"
   }
 }
