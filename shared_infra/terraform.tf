8abe9827a4b1112828599e71c5f7375ab0e60d20
Alex Chan <a.chan@wellcome.ac.uk>
2017-09-18 10:55:39 +0100

    Replace deprecated lock_table with dynamodb_table

--- shared_infra/terraform.tf
+++ shared_infra/terraform.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 0.9"
 
   backend "s3" {
-    bucket     = "platform-infra"
-    key        = "platform.tfstate"
-    lock_table = "terraform-locktable"
-    region     = "eu-west-1"
+    bucket       = "platform-infra"
+    key          = "platform.tfstate"
+    dynamo_table = "terraform-locktable"
+    region       = "eu-west-1"
   }
 }
