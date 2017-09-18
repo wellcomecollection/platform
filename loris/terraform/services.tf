647022c66d816d1e6f43571d32596b2f06fcd1b1
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-09-18 15:28:05 +0100

    Get a version of Loris terraform that plans correctly

---
+++ loris/terraform/services.tf
@@ -0,0 +1,33 @@
+module "loris" {
+  source             = "../terraform/services"
+  name               = "loris-new"
+  cluster_id         = "${data.terraform_remote_state.platform.ecs_cluster_api_id}"
+  task_role_arn      = "${module.ecs_loris_iam.task_role_arn}"
+  vpc_id             = "${data.terraform_remote_state.platform.vpc_api_id}"
+  app_uri            = "${module.ecr_loris.repository_url}:${var.release_ids["loris"]}"
+  nginx_uri          = "${module.ecr_nginx_loris.repository_url}:${var.release_ids["nginx_loris"]}"
+  listener_https_arn = "${data.terraform_remote_state.platform.api_alb_listener_https_arn}"
+  listener_http_arn  = "${data.terraform_remote_state.platform.api_alb_listener_http_arn}"
+  infra_bucket       = "${var.infra_bucket}"
+  config_key         = "config/${var.build_env}/loris.ini"
+  path_pattern       = "/image*"
+  healthcheck_path   = "/image/"
+  alb_priority       = "109"
+  host_name          = "iiif-origin.wellcomecollection.org"
+
+  cpu    = 1792
+  memory = 4096
+
+  desired_count = 1
+
+  deployment_minimum_healthy_percent = "50"
+  deployment_maximum_percent         = "200"
+
+  volume_name      = "loris"
+  volume_host_path = "${data.terraform_remote_state.platform.api_userdata_efs_mount_directory}/loris"
+  container_path   = "/mnt/loris"
+
+  loadbalancer_cloudwatch_id   = "${data.terraform_remote_state.platform.api_alb_cloudwatch_id}"
+  server_error_alarm_topic_arn = "${data.terraform_remote_state.platform.alb_server_error_alarm_arn}"
+  client_error_alarm_topic_arn = "${data.terraform_remote_state.platform.alb_client_error_alarm_arn}"
+}
--- loris/terraform/variables.tf
+++ loris/terraform/variables.tf
@@ -6,3 +6,18 @@ variable "aws_region" {
 variable "iiif_acm_cert_arn" {
   description = "ARN of ACM cert for iiif API (in us-east-1) for CloudFront"
 }
+
+variable "release_ids" {
+  description = "Release tags for platform apps"
+  type        = "map"
+}
+
+variable "build_env" {
+  description = "Build environment (prod, dev, stage, ...)"
+  default     = "prod"
+}
+
+variable "infra_bucket" {
+  description = "S3 bucket storing our configuration"
+  default     = "platform-infra"
+}
