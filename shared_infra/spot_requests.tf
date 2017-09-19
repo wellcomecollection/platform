295cf0d4a80b3ab1829709b5cfefaf36b0c8d45a
Alex Chan <a.chan@wellcome.ac.uk>
2017-09-19 16:19:51 +0100

    availability_zone != aws_region

--- shared_infra/spot_requests.tf
+++ shared_infra/spot_requests.tf
@@ -5,6 +5,6 @@ module "services_fleet" {
   instance_type     = "m4.xlarge"
   image_id          = "${data.aws_ami.stable_coreos.id}"
   key_name          = "${var.key_name}"
-  availability_zone = "${var.aws_region}"
+  availability_zone = "eu-west-1a"
   user_data         = "${module.services_userdata.rendered}"
 }
