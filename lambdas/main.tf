6805124df53dcfca0c4de4d36b75665dd0b690e3
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-09-20 10:10:41 +0100

    Remove S3 bucket trigger from lambdas

--- lambdas/main.tf
+++ lambdas/main.tf
@@ -148,3 +148,14 @@ module "update_task_for_config_change" {
 
   lambda_error_alarm_arn = "${module.lambda_error_alarm.arn}"
 }
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = "${data.terraform_remote_state.platform.bucket_infra_id}"
+
+  lambda_function {
+    lambda_function_arn = "${module.update_task_for_config_change.lambda_arn}"
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "config/prod/"
+    filter_suffix       = ".ini"
+  }
+}
