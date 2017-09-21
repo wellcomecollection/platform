b5046315424823ef22cb4237458eacfc11c21de7
caylaa <alicefuzier@gmail.com>
2017-09-21 16:09:33 +0100

    Address review comments

--- miro_preprocessor/image_sorter/src/sorter_logic.py
+++ miro_preprocessor/image_sorter/src/sorter_logic.py
@@ -18,7 +18,7 @@ class Rules:
     @staticmethod
     def _normalise_string(s):
         if s is not None:
-            s = s.lower()
+            s = s.lower().strip()
         if s == "":
             s = None
         return s
@@ -30,7 +30,7 @@ class Rules:
         return self._get(key) == self._normalise_string(value)
 
     def _is_collection(self, collection_name):
-        return self._normalise_string(self.collection) == f"source/images-{collection_name}"
+        return self._normalise_string(self.collection) == self._normalise_string(f"source/images-{collection_name}")
 
     def _search(self, regex, key):
         return re.search(regex, self._get(key))
@@ -40,11 +40,11 @@ class Rules:
 
     @property
     def is_f_collection(self):
-        return self._is_collection("f")
+        return self._is_collection("F")
 
     @property
     def is_l_or_m_or_v_collection(self):
-        return self._is_collection("l") or self._is_collection("v") or self._is_collection("m")
+        return self._is_collection("L") or self._is_collection("V") or self._is_collection("M")
 
     @property
     def image_library_dept_is_Archives_and_Manuscripts(self):
@@ -56,7 +56,7 @@ class Rules:
 
     @property
     def is_innopac_id_8_digits(self):
-        return self._search("[0-9]{8}", "image_innopac_id") is not None
+        return self._search(r"[0-9]{8}", "image_innopac_id") is not None
 
     @property
     def is_title_blank(self):
