73112caad771ddc52bcd44b2744dda869e8997ae
Alex Chan <alex@alexwlchan.net>
2017-09-21 09:22:13 +0100

    Return a 404 error if you look up a non-existent index (#968)
    * Add another test for 500 Internal Server error
    
    * Put the Not Found JSON body in a method
    
    * Prepare to ask more interesting questions about ES exceptions
    
    * Looking up a missing index is a 404
    
    * That should actually compile
    
    * More trimming
    
    * Brace yourselves!
    
    * More plumbing to get 404s out of the ESExceptionMapper
    
    * Tidy up the ES Exception Mapper
    
    * A handful of review markups

--- api/src/main/scala/uk/ac/wellcome/platform/api/finatra/exceptions/ElasticsearchResponseExceptionMapper.scala
+++ api/src/main/scala/uk/ac/wellcome/platform/api/finatra/exceptions/ElasticsearchResponseExceptionMapper.scala
@@ -32,6 +32,13 @@ class ElasticsearchResponseExceptionMapper @Inject()(
     DisplayError(Error(variant = "http-400", description = Some(message)))
   }
 
+  private def notFound(message: String, exception: Exception): DisplayError = {
+    error(
+      s"Sending HTTP 404 from ElasticsearchResponseExceptionMapper ($message)",
+      exception)
+    DisplayError(Error(variant = "http-404", description = Some(message)))
+  }
+
   private def serverError(message: String,
                           exception: Exception): DisplayError = {
     error(
@@ -40,41 +47,74 @@ class ElasticsearchResponseExceptionMapper @Inject()(
     DisplayError(Error(variant = "http-500", description = None))
   }
 
-  // One possible error is of the form:
+  // This error is of the form:
   //
   //     Result window is too large, from + size must be less than or equal
   //     to: [10000] but was [100000]. See the scroll api for a more
   //     efficient way to request large data sets. This limit can be set by
   //     changing the [index.max_result_window] index level setting.
   //
-  // In this case, we return a 400 Bad Request error, but we rephrase it
-  // to be shorter and to avoid talking about Elasticsearch concepts.
+  // When returning a 400 to the user, we wrap this error to avoid talking
+  // about internal Elasticsearch concepts.
   val resultSizePattern =
     """Result window is too large, from \+ size must be less than or equal to: \[([0-9]+)\]""".r.unanchored
 
-  /* Elasticsearch errors have a "root_cause" with a reason -- given such
-   * a reason, return an appropriate DisplayError.
-   */
-  private def reasonToError(reason: String,
-                            exception: Exception): DisplayError =
-    reason match {
-      case resultSizePattern(size) =>
-        userError(s"Only the first $size works are available in the API.",
-                  exception)
-      case _ =>
-        serverError(
-          s"Unknown reason for error in Elasticsearch response: $reason",
-          exception)
+  private def jsonToError(jsonDocument: String,
+                          exception: Exception): DisplayError = {
+    val mapper = new ObjectMapper()
+    val exceptionData = mapper.readTree(jsonDocument)
+
+    val esError = exceptionData.get("error")
+    val esErrorType = esError
+      .get("type")
+      .asText
+
+    esErrorType match {
+      // This occurs if the user requests a non-existent index as ?_index=foo.
+      // We return this as a 404 error to the user.
+      case "index_not_found_exception" => {
+        val index = esError
+          .get("index")
+          .asText
+        notFound(s"There is no index $index", exception)
+      }
+
+      // This may occur if the user requests an overly large page of results.
+      // We return this as a 400 error to the user.
+      case "search_phase_execution_exception" => {
+        val reason = esError
+          .get("root_cause")
+          .get(0)
+          .get("reason")
+          .asText
+
+        resultSizePattern.findFirstMatchIn(reason) match {
+          case Some(s) => {
+            val size = s.group(1)
+            userError(s"Only the first $size works are available in the API.",
+                      exception)
+          }
+          case _ => {
+            serverError(s"Unknown error in search phase execution: $reason",
+                        exception)
+          }
+        }
+      }
+
+      // Anything else should bubble up as a 500, as it's at least somewhat
+      // unexpected and worthy of further investigation.
+      case _ => {
+        serverError("Unknown error", exception)
+      }
     }
+  }
 
   private def toError(request: Request,
                       exception: ResponseException): DisplayError = {
-
     // Elasticsearch returns errors as JSON documents, which are stored in the
     // `message` attribute of ElasticsearchException.  Try to read it as JSON,
     // so we can check if this was a user error -- but if parsing fails, it's
     // enough to return a 500 error.
-
     try {
       // Annoyingly, the exact format of message is
       //
@@ -85,25 +125,13 @@ class ElasticsearchResponseExceptionMapper @Inject()(
       //
       // Except when it isn't!  Sometimes these exceptions don't include
       // a JSON response, so there's only a single-line message -- then `.head`
-      // fails: "NoSuchElementException: next on empty iterator".
-      val mapper = new ObjectMapper()
+      // fails: "NoSuchElementException: next on empty iterator".  I think
+      // this happens when the cluster doesn't reply, but I'm not sure.
       val jsonDocument = exception.getMessage
         .split("\n")
         .tail
         .head
-
-      val exceptionData = mapper.readTree(jsonDocument)
-      val reason = exceptionData
-        .get("error")
-        .get("root_cause")
-        .get(0)
-        .get("reason")
-      reason match {
-        case s: JsonNode => reasonToError(s.asText, exception)
-        case _ =>
-          serverError("Unable to find error reason in Elasticsearch response",
-                      exception)
-      }
+      jsonToError(jsonDocument = jsonDocument, exception = exception)
     } catch {
       case e: Exception =>
         serverError(s"Error ($e) parsing Elasticsearch response", exception)
@@ -116,6 +144,7 @@ class ElasticsearchResponseExceptionMapper @Inject()(
     val errorResponse = ResultResponse(context = contextUri, result = result)
     result.httpStatus.get match {
       case 500 => response.internalServerError.json(errorResponse)
+      case 404 => response.notFound.json(errorResponse)
       case 400 => response.badRequest.json(errorResponse)
     }
   }
