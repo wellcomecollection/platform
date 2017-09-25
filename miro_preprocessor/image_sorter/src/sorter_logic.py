788b22539f3b156a6690f9647c0c17944cd9a50f
Robert Kenny <kenoir@users.noreply.github.com>
2017-09-25 13:01:33 +0100

    Sorter rules (#948)
    * Deciding on a layout for rules
    
    * Adding first couple of rules for Cold storage
    
    * wip migrating to class instead of dict
    
    * Another rule
    
    * Making the tests a bit more sane (thanks Alex!)
    
    * Address review comments
    
    * Fix regex
    
    * There is no undecidable
    
    * Avoid nested dict
    
    * Some more rules around images with no information
    
    * All rules for cold storage
    
    * Some more TV rules
    
    * All TV rules
    
    * Some refactoring and return a list of decisions to allow for overlapping sets
    
    * Adding skeleton tests for extra possible cases
    
    * Added API rules to be reviewed later
    
    * Adding some extra tests to figure out what ends up in which categories
    
    * Raise exception if collection is not one of L,V,M or F
    
    * Adds logic to perform multiple decisions
    
    * Add ability to sort to multiple topics
    
    Integration tests for image_sorter
    
    * remove image_access_restrictions as rule, accept string None as image_use_restriction
    
    * address review comments

--- miro_preprocessor/image_sorter/src/sorter_logic.py
+++ miro_preprocessor/image_sorter/src/sorter_logic.py
@@ -6,23 +6,213 @@ It only has a single public function: ``sort_image``, which takes a Python
 dictionary and returns an instance of ``Decision``.
 """
 
+import dateutil.parser
 import enum
+import re
+
+
+class Rules:
+    def __init__(self, collection, image_data):
+        self.collection = collection
+        self.image_data = image_data
+
+    _empty_title_strings = [
+        "-",
+        "--",
+        "Awaiting description",
+        "Awaiting removal from MIRO as duplicate",
+        "Awaiting captions and consent form",
+        "Awaiting catalogue details",
+        "Awaiting caption info",
+        "No info available about this object",
+        "No neg"
+    ]
+
+    _cc_accesses = [
+        "CC-0",
+        "CC-BY",
+        "CC-BY-NC",
+        "CC-BY-NC-ND",
+        "None"
+    ]
+
+    @staticmethod
+    def _normalise_string(s):
+        if s is not None:
+            s = s.lower().strip()
+        if s == "":
+            s = None
+        return s
+
+    def _get(self, key):
+        return self.image_data.get(key)
+
+    def _get_normalised(self, key):
+        return self._normalise_string(self._get(key))
+
+    def _compare(self, key, value):
+        return self._get_normalised(key) == self._normalise_string(value)
+
+    def _is_collection(self, collection_name):
+        return self._normalise_string(self.collection) == self._normalise_string(f"images-{collection_name}")
+
+    def _search(self, regex, key):
+        return re.search(regex, self._get_normalised(key))
+
+    def _is_blank(self, key):
+        return self._get_normalised(key) is None
+
+    def _key_matches(self, key, string_list):
+        return any([self._compare(key, string) for string in string_list])
+
+    def _parse_date(self, date_string):
+        return dateutil.parser.parse(date_string, dayfirst=True)
+
+    def is_collection(self, *args):
+        return any(self._is_collection(collection) for collection in args)
+
+    @property
+    def image_library_dept_is_Archives_and_Manuscripts(self):
+        return self._compare("image_library_dept", "Archives and Manuscripts")
+
+    @property
+    def image_library_dept_is_Public_programmes(self):
+        return self._compare("image_library_dept", "Public programmes")
+
+    @property
+    def image_tech_captured_mode_is_videodisc(self):
+        return self._compare("image_tech_captured_mode", "videodisc")
+
+    @property
+    def is_innopac_id_8_digits(self):
+        return self._search(r"[0-9]{7}[0-9xX]{1}", "image_innopac_id") is not None
+
+    @property
+    def is_title_blank(self):
+        return self._is_blank("image_title")
+
+    @property
+    def is_title_empty(self):
+        return self.is_title_blank or self._key_matches('image_title', self._empty_title_strings)
+
+    @property
+    def is_image_pub_title_blank(self):
+        return self._is_blank("image_pub_title")
+
+    @property
+    def is_image_pub_periodical_blank(self):
+        return self._is_blank("image_pub_periodical")
+
+    @property
+    def is_after_first_march_2016(self):
+        scanned_date = self._get("image_tech_scanned_date")
+        if scanned_date is not None:
+            return self._parse_date(scanned_date) > self._parse_date("1 March 2016")
+        else:
+            return False
+
+    @property
+    def is_copyright_cleared(self):
+        return self._get('image_copyright_cleared') == 'Y'
+
+    @property
+    def is_cleared(self):
+        return self._get('image_cleared') == 'Y'
+
+    @property
+    def is_not_general_use(self):
+        return self._get('image_general_use') == 'N'
+
+    @property
+    def has_use_restrictions(self):
+        return not self._key_matches("image_use_restrictions", self._cc_accesses)
+
+    @property
+    def is_not_for_public_access(self):
+        return (not self.is_copyright_cleared) or self.is_not_general_use or self.has_use_restrictions
+
+    @property
+    def is_for_public_access(self):
+        return not self.is_not_for_public_access
+
+    @property
+    def use_restrictions_are_none(self):
+        return self._get_normalised("image_use_restrictions") == "none"
+
+    @property
+    def satisfies_api_filters(self):
+        return \
+            self.is_copyright_cleared and \
+            self.is_cleared and \
+            (not self.has_use_restrictions) and \
+            (not self.use_restrictions_are_none)
+
+    @property
+    def is_cold_store(self):
+        return self.is_collection("F") or \
+            (self.is_collection("L", "M", "V") and self.image_library_dept_is_Archives_and_Manuscripts) or \
+            (self.is_collection("L", "M", "V") and self.image_tech_captured_mode_is_videodisc) or \
+            (self.is_collection("L", "M", "V") and
+             not self.is_innopac_id_8_digits and
+             self.is_title_empty and
+             self.is_image_pub_title_blank and
+             self.is_image_pub_periodical_blank)
+
+    @property
+    def is_tandem_vault(self):
+        return self.image_library_dept_is_Public_programmes or \
+            self.is_collection("L") and self.is_after_first_march_2016 or \
+            self.is_collection("L", "M", "V") and self.is_not_for_public_access
+
+    @property
+    def is_digital_library(self):
+        return not self.image_library_dept_is_Public_programmes \
+            and self.is_for_public_access and self.is_innopac_id_8_digits
+
+    @property
+    def is_catalogue_api(self):
+        return (not self.is_tandem_vault or self.is_digital_library) and self.satisfies_api_filters
 
 
 class Decision(enum.Enum):
     cold_store = 'cold_store'
     tandem_vault = 'tandem_vault'
+    digital_library = 'digital_library'
     catalogue_api = 'catalogue_api'
+    none = 'none'
 
 
-class Undecidable(Exception):
+class InvalidCollectionException(Exception):
     pass
 
 
 def sort_image(collection, image_data):
-    # For now reproduce the behaviour of the miro_adapter and send all images to the catalogue API.
-    return Decision.catalogue_api
+    print(collection)
+    print(image_data)
+    r = Rules(collection, image_data)
 
-    raise Undecidable(
-        f'No decision for collection={collection!r}, image_data={image_data!r}'
-    )
+    decisions = []
+
+    if not r.is_collection("F", "L", "V", "M"):
+        raise InvalidCollectionException({
+            "collection": collection,
+            "image_data": image_data
+        })
+
+    if r.is_cold_store:
+        return [Decision.cold_store]
+
+    if r.is_tandem_vault:
+        decisions.append(Decision.tandem_vault)
+
+    if r.is_digital_library:
+        decisions.append(Decision.digital_library)
+
+    if r.is_catalogue_api:
+        decisions.append(Decision.catalogue_api)
+
+    if not r.is_catalogue_api and not r.is_digital_library and not r.is_tandem_vault:
+        decisions.append(Decision.none)
+
+    print(decisions)
+    return decisions
--- miro_preprocessor/image_sorter/src/test_sorter_logic.py
+++ miro_preprocessor/image_sorter/src/test_sorter_logic.py
@@ -2,36 +2,191 @@
 
 import pytest
 
-from sorter_logic import Decision, Undecidable, sort_image
+from sorter_logic import Decision, InvalidCollectionException, sort_image
+
+
+def collection_image_data(**kwargs):
+    image_data = {
+        "image_title": "Image Title",
+        "image_pub_title": "Image Pub Title",
+        "image_pub_periodical": "Lost socks monthly",
+        "image_library_dept": "Paperclips and hairnets",
+        "image_tech_captured_mode": "Frog retina",
+        "image_copyright_cleared": "Y",
+        "image_use_restrictions": "CC-BY",
+        "image_general_use": "Y",
+        "image_innopac_id": "12345678",
+        "image_cleared": "Y"
+    }
+    collection = 'images-M'
+    if 'collection' in kwargs.keys():
+        collection = kwargs.pop('collection')
+    image_data.update(kwargs)
+
+    return collection, image_data
+
+
+def image_with_no_info(collection, image_title):
+    return collection_image_data(collection=collection,
+                                 image_title=image_title,
+                                 image_pub_title=None,
+                                 image_pub_periodical=None,
+                                 image_innopac_id="1234567")
 
 
 @pytest.mark.parametrize('collection, image_data', [
-])
-def test_is_undecidable(collection, image_data):
-    """These examples are undecidable."""
-    with pytest.raises(Undecidable):
-        sort_image(image_data)
-
-
-@pytest.mark.parametrize('collection, image_data', [
-    # TODO: Write some examples...
+    collection_image_data(collection='images-F'),
+    collection_image_data(collection='images-L', image_library_dept="Archives and Manuscripts"),
+    collection_image_data(collection='images-V', image_library_dept="Archives and Manuscripts"),
+    collection_image_data(collection='images-M', image_library_dept="Archives and Manuscripts"),
+    collection_image_data(collection='images-L', image_tech_captured_mode="videodisc"),
+    collection_image_data(collection='images-V', image_tech_captured_mode="videodisc"),
+    collection_image_data(collection='images-M', image_tech_captured_mode="videodisc"),
+    image_with_no_info(collection='images-L', image_title=None),
+    image_with_no_info(collection='images-V', image_title=None),
+    image_with_no_info(collection='images-M', image_title=None),
+    image_with_no_info(collection='images-L', image_title="-"),
+    image_with_no_info(collection='images-V', image_title="-"),
+    image_with_no_info(collection='images-M', image_title="-"),
+    image_with_no_info(collection='images-L', image_title="--"),
+    image_with_no_info(collection='images-V', image_title="--"),
+    image_with_no_info(collection='images-M', image_title="--"),
+    image_with_no_info(collection='images-L', image_title="Awaiting description"),
+    image_with_no_info(collection='images-V', image_title="Awaiting description"),
+    image_with_no_info(collection='images-M', image_title="Awaiting description"),
+    image_with_no_info(collection='images-L', image_title="Awaiting removal from MIRO as duplicate"),
+    image_with_no_info(collection='images-V', image_title="Awaiting removal from MIRO as duplicate"),
+    image_with_no_info(collection='images-M', image_title="Awaiting removal from MIRO as duplicate"),
+    image_with_no_info(collection='images-L', image_title="Awaiting captions and consent form"),
+    image_with_no_info(collection='images-V', image_title="Awaiting captions and consent form"),
+    image_with_no_info(collection='images-M', image_title="Awaiting captions and consent form"),
+    image_with_no_info(collection='images-L', image_title="Awaiting catalogue details"),
+    image_with_no_info(collection='images-V', image_title="Awaiting catalogue details"),
+    image_with_no_info(collection='images-M', image_title="Awaiting catalogue details"),
+    image_with_no_info(collection='images-L', image_title="Awaiting caption info"),
+    image_with_no_info(collection='images-V', image_title="Awaiting caption info"),
+    image_with_no_info(collection='images-M', image_title="Awaiting caption info"),
+    image_with_no_info(collection='images-L', image_title="No info available about this object"),
+    image_with_no_info(collection='images-V', image_title="No info available about this object"),
+    image_with_no_info(collection='images-M', image_title="No info available about this object"),
+    image_with_no_info(collection='images-L', image_title="No neg"),
+    image_with_no_info(collection='images-V', image_title="No neg"),
+    image_with_no_info(collection='images-M', image_title="No neg"),
 ])
 def test_is_cold_store(collection, image_data):
     """These examples all end up in cold store."""
-    assert sort_image(collection, image_data) == Decision.cold_store
+    assert sort_image(collection, image_data) == [Decision.cold_store]
 
 
 @pytest.mark.parametrize('collection, image_data', [
-    # TODO: Write some examples...
+    collection_image_data(collection='images-L', image_library_dept="Public programmes"),
+    collection_image_data(collection='images-V', image_library_dept="Public programmes"),
+    collection_image_data(collection='images-M', image_library_dept="Public programmes"),
+    collection_image_data(collection='images-L',
+                          image_tech_scanned_date="02/03/2016",
+                          image_use_restrictions="Restricted"),
+    collection_image_data(collection='images-L', image_use_restrictions=None),
+    collection_image_data(collection='images-V', image_use_restrictions=None),
+    collection_image_data(collection='images-M', image_use_restrictions=None),
+    collection_image_data(collection='images-L', image_copyright_cleared="N"),
+    collection_image_data(collection='images-V', image_copyright_cleared="N"),
+    collection_image_data(collection='images-M', image_copyright_cleared="N"),
+    collection_image_data(collection='images-L', image_general_use="N"),
+    collection_image_data(collection='images-V', image_general_use="N"),
+    collection_image_data(collection='images-M', image_general_use="N"),
+    collection_image_data(collection='images-L', image_use_restrictions="Top-secret"),
+    collection_image_data(collection='images-V', image_use_restrictions="Top-secret"),
+    collection_image_data(collection='images-M', image_use_restrictions="Top-secret")
 ])
 def test_is_tandem_vault(collection, image_data):
     """These examples all end up in Tandem Vault."""
-    assert sort_image(collection, image_data) == Decision.tandem_vault
+    assert sort_image(collection, image_data) == [Decision.tandem_vault]
 
 
 @pytest.mark.parametrize('collection, image_data', [
-    # TODO: Write some examples...
+    collection_image_data(collection='images-L', image_use_restrictions="None"),
+    collection_image_data(collection='images-V', image_use_restrictions="None"),
+    collection_image_data(collection='images-M', image_use_restrictions="None"),
+    collection_image_data(collection='images-L', image_cleared="N"),
 ])
 def test_is_digital_library(collection, image_data):
     """These examples all end up in the Digital Library."""
-    assert sort_image(collection, image_data) == Decision.digital_library
+    assert sort_image(collection, image_data) == [Decision.digital_library]
+
+
+@pytest.mark.parametrize('collection, image_data', [
+    collection_image_data(collection='images-M', image_use_restrictions="CC-BY-NC-ND", image_innopac_id="blahbluh"),
+    collection_image_data(collection='images-L', image_use_restrictions="CC-BY-NC-ND", image_innopac_id="blahbluh"),
+    collection_image_data(collection='images-V', image_use_restrictions="CC-BY-NC-ND", image_innopac_id="blahbluh"),
+])
+def test_is_catalogue_api(collection, image_data):
+    """These examples all end up in the Digital Library."""
+    assert sort_image(collection, image_data) == [Decision.catalogue_api]
+
+
+@pytest.mark.parametrize('collection, image_data', [
+    collection_image_data(collection='images-M', image_use_restrictions="CC-BY-NC-ND", image_innopac_id="blahbluh",
+                          image_cleared="N"),
+    collection_image_data(collection='images-L', image_use_restrictions="CC-BY-NC-ND", image_innopac_id="blahbluh",
+                          image_cleared=None),
+])
+def test_is_no_decision(collection, image_data):
+    """These examples all end up in the Digital Library."""
+    assert sort_image(collection, image_data) == [Decision.none]
+
+
+@pytest.mark.parametrize('collection, image_data', [
+    collection_image_data(collection='images-L', image_tech_scanned_date="02/03/2016", image_cleared="N"),
+    collection_image_data(collection='images-L', image_tech_scanned_date="02/03/2016", image_cleared=None),
+    collection_image_data(collection='images-L', image_tech_scanned_date="02/03/2016", image_use_restrictions="None"),
+])
+def test_is_digital_library_and_tandem_vault(collection, image_data):
+    """These examples all end up in the Digital Library."""
+    assert sort_image(collection, image_data) == [Decision.tandem_vault, Decision.digital_library]
+
+
+@pytest.mark.parametrize('collection, image_data', [
+    collection_image_data(),
+    collection_image_data(image_innopac_id="1234567x"),
+    collection_image_data(image_title=None,
+                          image_pub_title=None,
+                          image_pub_periodical=None,
+                          image_innopac_id="1234567x"),
+    collection_image_data(image_title=None,
+                          image_pub_title=None,
+                          image_pub_periodical=None,
+                          image_innopac_id="12345678"),
+    collection_image_data(collection='images-L', image_tech_scanned_date="01/03/2016"),
+    collection_image_data(collection='images-L', image_tech_scanned_date="29/02/2016"),
+    collection_image_data(collection='images-L', image_use_restrictions="CC-0"),
+    collection_image_data(collection='images-V', image_use_restrictions="CC-0"),
+    collection_image_data(collection='images-M', image_use_restrictions="CC-0"),
+    collection_image_data(collection='images-L', image_use_restrictions="CC-BY"),
+    collection_image_data(collection='images-V', image_use_restrictions="CC-BY"),
+    collection_image_data(collection='images-M', image_use_restrictions="CC-BY"),
+    collection_image_data(collection='images-L', image_use_restrictions="CC-BY-NC"),
+    collection_image_data(collection='images-V', image_use_restrictions="CC-BY-NC"),
+    collection_image_data(collection='images-M', image_use_restrictions="CC-BY-NC"),
+    collection_image_data(collection='images-L', image_use_restrictions="CC-BY-NC-ND"),
+    collection_image_data(collection='images-V', image_use_restrictions="CC-BY-NC-ND"),
+    collection_image_data(collection='images-M', image_use_restrictions="CC-BY-NC-ND"),
+])
+def test_is_digital_library_and_catalogue_api(collection, image_data):
+    """These examples all end up in the Digital Library."""
+    assert sort_image(collection, image_data) == [Decision.digital_library, Decision.catalogue_api]
+
+
+@pytest.mark.parametrize('collection, image_data', [
+    collection_image_data(collection='images-L', image_tech_scanned_date="02/03/2016"),
+    collection_image_data(collection='images-L', image_tech_scanned_date="30/06/2018"),
+])
+def test_is_tandem_vault_and_digital_library_and_catalogue_api(collection, image_data):
+    """These examples all end up in the Digital Library."""
+    assert sort_image(collection, image_data) == [Decision.tandem_vault, Decision.digital_library,
+                                                  Decision.catalogue_api]
+
+
+def test_raise_exception_if_collection_is_not_flvm():
+    collection, image_data = collection_image_data(collection="images-A")
+    with pytest.raises(InvalidCollectionException):
+        sort_image(collection, image_data)
