bfd2cb0d084fd261bff7e94e54814f500aa7784d
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-09-27 12:27:16 +0100

    Removing efs mount from the API ASG

--- shared_infra/userdata.tf
+++ shared_infra/userdata.tf
@@ -12,5 +12,4 @@ module "monitoring_userdata" {
 module "api_userdata" {
   source            = "../terraform/userdata"
   cluster_name      = "${aws_ecs_cluster.api.name}"
-  efs_filesystem_id = "${module.loris_efs.efs_id}"
 }
