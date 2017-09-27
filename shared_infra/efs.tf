3a3247d20adc42b78030abf8d58b9a817daecb6b
caylaa <alicefuzier@gmail.com>
2017-09-27 12:40:23 +0100

    delete loris efs mount

--- shared_infra/efs.tf
+++ shared_infra/efs.tf
@@ -5,12 +5,3 @@ module "grafana_efs" {
   subnets                      = "${module.vpc_monitoring.subnets}"
   efs_access_security_group_id = "${module.monitoring_cluster_asg.instance_sg_id}"
 }
-
-module "loris_efs" {
-  name                         = "loris_cache"
-  source                       = "../terraform/efs"
-  vpc_id                       = "${module.vpc_api.vpc_id}"
-  subnets                      = "${module.vpc_api.subnets}"
-  efs_access_security_group_id = "${module.api_cluster_asg.instance_sg_id}"
-  performance_mode             = "maxIO"
-}
