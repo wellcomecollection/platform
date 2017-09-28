3439dcc2bdf009b4e5c9e3a22257880b34c22b6f
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-09-28 17:03:13 +0100

    fix broken schedule_reindexer

--- lambdas/schedule_reindexer/src/schedule_reindexer.py
+++ lambdas/schedule_reindexer/src/schedule_reindexer.py
@@ -8,7 +8,7 @@ This script is triggered by updates to the reindexer DynamoDB table.
 
 import os
 
-from utils.dynamo_utils import DynamoEvent
+from utils.dynamo_utils import DynamoImageFactory
 from utils.sns_utils import publish_sns_message
 
 
@@ -18,35 +18,70 @@ def get_service_name(reindexers, table_name):
     return table_service_dict[table_name]
 
 
-def main(event, _):
-    print(f'event = {event!r}')
-
-    dynamo_event = DynamoEvent(event)
-    table_name = dynamo_event.new_image["TableName"]["S"]
-    current_version = dynamo_event.new_image["CurrentVersion"]["N"]
-    requested_version = dynamo_event.new_image["RequestedVersion"]["N"]
+def _determine_count_and_capacity(record, desired_capacity):
+    current_version = record.new_image["CurrentVersion"]["N"]
+    requested_version = record.new_image["RequestedVersion"]["N"]
 
     if current_version == requested_version:
         desired_count = 0
         desired_capacity = 1
     else:
         desired_count = 1
-        desired_capacity = int(os.environ["DYNAMO_DESIRED_CAPACITY"])
 
-    scheduler_topic_arn = os.environ["SCHEDULER_TOPIC_ARN"]
-    cluster = os.environ["CLUSTER_NAME"]
-    service = get_service_name(os.environ["REINDEXERS"], table_name)
-    message = {'cluster': cluster, 'service': service,
-               'desired_count': desired_count}
+    return {
+        "count": desired_count,
+        "capacity": desired_capacity
+    }
+
+
+def _request_task_run(scheduler_topic_arn, cluster_name, service, desired_count):
+    message = {
+        'cluster': cluster_name,
+        'service': service,
+        'desired_count': desired_count
+    }
 
     print(f'message = {message!r}')
     print(f'scheduler_topic_arn = {scheduler_topic_arn!r}')
+
     publish_sns_message(topic_arn=scheduler_topic_arn, message=message)
 
-    dynamo_topic_arn = os.environ["DYNAMO_TOPIC_ARN"]
-    dynamo_table_name = os.environ["DYNAMO_TABLE_NAME"]
-    message = {'dynamo_table_name': dynamo_table_name,
-               'desired_capacity': desired_capacity}
+
+def _request_dynamo_provision(dynamo_topic_arn, dynamo_table_name, desired_capacity):
+    message = {
+        'dynamo_table_name': dynamo_table_name,
+        'desired_capacity': desired_capacity
+    }
 
     print(f'message = {message!r}')
+    print(f'dynamo_topic_arn = {dynamo_topic_arn!r}')
+
     publish_sns_message(topic_arn=dynamo_topic_arn, message=message)
+
+
+def main(event, _):
+    print(f'event = {event!r}')
+
+    desired_capacity = int(os.environ["DYNAMO_DESIRED_CAPACITY"])
+
+    for record in DynamoImageFactory.create(event):
+        table_name = record.new_image["TableName"]["S"]
+
+        desired = _determine_count_and_capacity(record, desired_capacity)
+
+        print(f"Count and capacity required: {desired}")
+
+        _request_task_run(
+            os.environ["SCHEDULER_TOPIC_ARN"],
+            os.environ["CLUSTER_NAME"],
+            get_service_name(os.environ["REINDEXERS"], table_name),
+            desired["count"]
+        )
+
+        _request_dynamo_provision(
+            os.environ["DYNAMO_TOPIC_ARN"],
+            os.environ["DYNAMO_TABLE_NAME"],
+            desired["capacity"]
+        )
+
+        print("Done.")
