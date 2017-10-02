ad2600fb44e0848aa30d77300413400ace8420f9
alicefuzier <alicefuzier@gmail.com>
2017-10-02 09:07:51 +0100

    Use EBS for Loris cache (#996)
    * Create another Loris cluster to play around with ebs
    
    * Add EBS volume to loris asg
    
    * Format ebs volume at startup
    
    * Run the cache cleaner locally on the instance
    
    * Make number of ec2 instances and tasks same as the efs loris
    
    * Formatting
    
    * Use local variable for efs mount directory
    
    * Remove loris-cache-cleaner container if it exists

--- terraform/ecs_asg/ecs_asg_launch_config/main.tf
+++ terraform/ecs_asg/ecs_asg_launch_config/main.tf
@@ -1,5 +1,5 @@
 resource "aws_launch_configuration" "ondemand_launch_config" {
-  count = "${1 - var.use_spot}"
+  count = "${var.use_spot == true ? 0 : "${var.ebs_device_name == "" ? 1 : 0}" }"
 
   security_groups = [
     "${aws_security_group.instance_sg.id}",
@@ -34,5 +34,30 @@ resource "aws_launch_configuration" "spot_launch_config" {
 
   lifecycle {
     create_before_destroy = true
+  }
+}
+
+resource "aws_launch_configuration" "ebs_launch_config" {
+  count = "${var.ebs_device_name == "" ? 0 : 1}"
+
+  security_groups = [
+    "${aws_security_group.instance_sg.id}",
+  ]
+
+  key_name                    = "${var.key_name}"
+  image_id                    = "${var.image_id}"
+  instance_type               = "${var.instance_type}"
+  iam_instance_profile        = "${var.instance_profile_name}"
+  user_data                   = "${var.user_data}"
+  associate_public_ip_address = "${var.public_ip}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  ebs_block_device {
+    volume_size = "${var.ebs_size}"
+    device_name = "${var.ebs_device_name}"
+    volume_type = "${var.ebs_volume_type}"
   }
 }
--- terraform/userdata/data.tf
+++ terraform/userdata/data.tf
@@ -1,13 +1,23 @@
 data "template_file" "template" {
-  template = "${file("${path.module}/templates/ecs-agent${var.efs_filesystem_id == "no_name_set" ? "" : "-with-efs"}.yml.template")}"
+  template = "${file("${path.module}/templates/${local.template_name}.yml.template")}"
 
   vars {
-    aws_region          = "${var.aws_region}"
-    ecs_cluster_name    = "${var.cluster_name}"
-    ecs_log_level       = "info"
-    ecs_agent_version   = "latest"
-    ecs_log_group_name  = "${aws_cloudwatch_log_group.ecs_agent.name}"
-    efs_filesystem_id   = "${var.efs_filesystem_id}"
-    efs_mount_directory = "/mnt/efs"
+    aws_region                   = "${var.aws_region}"
+    ecs_cluster_name             = "${var.cluster_name}"
+    ecs_log_level                = "info"
+    ecs_agent_version            = "latest"
+    ecs_log_group_name           = "${aws_cloudwatch_log_group.ecs_agent.name}"
+    efs_filesystem_id            = "${var.efs_filesystem_id}"
+    efs_mount_directory          = "${local.mount_directory}"
+    ebs_block_device             = "${var.ebs_block_device}"
+    ebs_cache_cleaner_group_name = "${var.cache_cleaner_cloudwatch_log_group}"
   }
+}
+
+locals {
+  has_efs_mount   = "${var.efs_filesystem_id == "no_name_set" ? false :true}"
+  has_ebs_mount   = "${var.ebs_block_device == "no_name_set" ? false :true}"
+  has_no_mount    = "${local.has_efs_mount == false ? "${local.has_ebs_mount == false ? true: false}" :false}"
+  template_name   = "ecs-agent${local.has_no_mount == true ? "" : "${local.has_efs_mount == true ? "-with-efs": "-with-ebs"}"}"
+  mount_directory = "${local.has_no_mount == true ? "" : "${local.has_efs_mount == true ? "/mnt/efs": "/mnt/ebs"}"}"
 }
