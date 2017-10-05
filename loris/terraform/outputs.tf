0df499f1f3b1a64cff547241f3e752aa39986c43
caylaa <alicefuzier@gmail.com>
2017-10-05 13:18:27 +0100

    miro preprocessor exceptions terraform

---
+++ loris/terraform/outputs.tf
@@ -0,0 +1,7 @@
+output "bucket_wellcomecollectio_miro_images_public_arn" {
+  value = "${aws_s3_bucket.wellcomecollection-miro-images-public.arn}"
+}
+
+output "bucket_wellcomecollectio_miro_images_public_id" {
+  value = "${aws_s3_bucket.wellcomecollection-miro-images-public.id}"
+}
