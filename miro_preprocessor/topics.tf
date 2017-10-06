ebdf8078806bb49b9beb69b8178b83a5e2222b2e
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-10-06 10:39:32 +0100

    fix miro preproc tf references

--- miro_preprocessor/topics.tf
+++ miro_preprocessor/topics.tf
@@ -1,29 +1,29 @@
 module "topic_miro_image_to_dynamo" {
-  source = "../terraform/sns"
+  source = "git::https://github.com/wellcometrust/terraform.git//sns"
   name   = "miro_image_to_dynamo"
 }
 
 module "cold_store_topic" {
-  source = "../terraform/sns"
+  source = "git::https://github.com/wellcometrust/terraform.git//sns"
   name   = "miro_cold_store"
 }
 
 module "tandem_vault_topic" {
-  source = "../terraform/sns"
+  source = "git::https://github.com/wellcometrust/terraform.git//sns"
   name   = "miro_tandem_vault"
 }
 
 module "catalogue_api_topic" {
-  source = "../terraform/sns"
+  source = "git::https://github.com/wellcometrust/terraform.git//sns"
   name   = "miro_catalogue_api"
 }
 
 module "digital_library_topic" {
-  source = "../terraform/sns"
+  source = "git::https://github.com/wellcometrust/terraform.git//sns"
   name   = "miro_digital_library"
 }
 
 module "none_topic" {
-  source = "../terraform/sns"
+  source = "git::https://github.com/wellcometrust/terraform.git//sns"
   name   = "miro_none"
 }
