3b4b32168d882c87dc8dd1e7e3ef00ab49aee7a2
caylaa <alicefuzier@gmail.com>
2017-10-09 11:21:01 +0100

    Change instance for EBS cluster. Reduce number of processes in wusgi

--- loris/terraform/asg.tf
+++ loris/terraform/asg.tf
@@ -31,7 +31,7 @@ module "loris_cluster_asg_ebs" {
   asg_max     = "4"
 
   image_id      = "${data.terraform_remote_state.platform.ecs_ami_id}"
-  instance_type = "t2.xlarge"
+  instance_type = "c4.xlarge"
 
   sns_topic_arn         = "${data.terraform_remote_state.platform.ec2_terminating_topic_arn}"
   publish_to_sns_policy = "${data.terraform_remote_state.platform.ec2_terminating_topic_publish_policy}"
