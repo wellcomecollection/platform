32fe94d67d4391915b4cc5b39c2cbe36b8029293
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-10-10 16:38:19 +0100

    Fix loris stack references (#1045)
    * Fix loris stack references
    
    * Fix remote state reference
    
    * fmt

--- loris/terraform/alb.tf
+++ loris/terraform/alb.tf
@@ -1,7 +1,7 @@
 module "loris_alb" {
   source  = "git::https://github.com/wellcometrust/terraform.git//ecs_alb?ref=v1.0.0"
   name    = "loris"
-  subnets = ["${data.terraform_remote_state.platform.vpc_api_subnets}"]
+  subnets = ["${local.vpc_api_subnets}"]
 
   loadbalancer_security_groups = [
     "${module.loris_cluster_asg.loadbalancer_sg_https_id}",
@@ -9,15 +9,15 @@ module "loris_alb" {
   ]
 
   certificate_domain = "api.wellcomecollection.org"
-  vpc_id             = "${data.terraform_remote_state.platform.vpc_api_id}"
+  vpc_id             = "${local.vpc_api_id}"
 
-  alb_access_log_bucket = "${data.terraform_remote_state.platform.bucket_alb_logs_id}"
+  alb_access_log_bucket = "${local.bucket_alb_logs_id}"
 }
 
 module "loris_alb_ebs" {
   source  = "git::https://github.com/wellcometrust/terraform.git//ecs_alb?ref=v1.0.0"
   name    = "loris-ebs"
-  subnets = ["${data.terraform_remote_state.platform.vpc_api_subnets}"]
+  subnets = ["${local.vpc_api_subnets}"]
 
   loadbalancer_security_groups = [
     "${module.loris_cluster_asg_ebs.loadbalancer_sg_https_id}",
@@ -25,7 +25,7 @@ module "loris_alb_ebs" {
   ]
 
   certificate_domain = "api.wellcomecollection.org"
-  vpc_id             = "${data.terraform_remote_state.platform.vpc_api_id}"
+  vpc_id             = "${local.vpc_api_id}"
 
-  alb_access_log_bucket = "${data.terraform_remote_state.platform.bucket_alb_logs_id}"
+  alb_access_log_bucket = "${local.bucket_alb_logs_id}"
 }
