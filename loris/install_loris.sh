f825eb4fa9a9338e753821a3e3a4f6fd1b756e43
Alex Chan <a.chan@wellcome.ac.uk>
2017-10-11 17:16:56 +0100

    More tracing in install_loris.sh

--- loris/install_loris.sh
+++ loris/install_loris.sh
@@ -6,10 +6,12 @@ set -o nounset
 
 # Install dependencies.  We don't include Apache because we're running
 # Loris with UWSGI and nginx, not Apache.
+echo "*** Installing Ubuntu imaging dependencies"
 apt-get install -y libffi-dev libjpeg-turbo8-dev libfreetype6-dev zlib1g-dev \
     liblcms2-dev liblcms2-utils libssl-dev libtiff5-dev libwebp-dev
 
 # Download and install the Loris code itself
+echo "*** Downloading the Loris source code"
 apt-get install -y unzip wget
 wget "https://github.com/$LORIS_GITHUB_USER/loris/archive/$LORIS_COMMIT.zip"
 unzip "$LORIS_COMMIT.zip"
@@ -17,13 +19,18 @@ rm "$LORIS_COMMIT.zip"
 apt-get remove -y unzip wget
 
 # Required or setup.py complains
+echo "*** Creating Loris user"
 useradd -d /var/www/loris -s /sbin/false loris
 
 # Upgrading pip to ensure we get a recent version as Ubuntu gives us a very old one
 # Otherwise we run into issues where deps rely on more recent versions e.g.
 # https://github.com/pyca/cryptography/issues/3959
+echo "*** Upgrading pip"
 pip install --upgrade pip
 
-cd "loris-$LORIS_COMMIT"
+echo "*** Installing Loris dependencies"
 pip install -r /requirements.txt
+
+echo "*** Installing Loris itself"
+cd "loris-$LORIS_COMMIT"
 python setup.py install
