00ef6f6152ac1ae40a9cf72538801ec00679402d
Alex Chan <a.chan@wellcome.ac.uk>
2017-10-11 17:58:35 +0100

    Make the Loris config path a variable

--- loris/run_loris.sh
+++ loris/run_loris.sh
@@ -4,20 +4,18 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-CONF_FILE=/opt/loris/etc/loris2.conf
-
-if [[ -f "$CONF_FILE" ]]
+if [[ -f "$LORIS_CONF_FILE" ]]
 then
   echo "Config is already present; skipping fetch from AWS..."
 else
   echo "Fetching config from AWS $INFRA_BUCKET/$CONFIG_KEY ..."
 
   aws s3 ls s3://$INFRA_BUCKET/$CONFIG_KEY
-  aws s3 cp s3://$INFRA_BUCKET/$CONFIG_KEY "$CONF_FILE"
+  aws s3 cp s3://$INFRA_BUCKET/$CONFIG_KEY "$LORIS_CONF_FILE"
 fi
 
 echo "=== config ==="
-cat "$CONF_FILE"
+cat "$LORIS_CONF_FILE"
 echo "=============="
 
 echo "Starting uwsgi..."
