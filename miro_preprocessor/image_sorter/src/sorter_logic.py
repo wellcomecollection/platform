511483f9d7018080c7724cfff3f160b7114791e5
Robert Kenny <kenoir@users.noreply.github.com>
2017-10-13 11:57:40 +0100

    When performing normalisations identified by key, allow lists (#1072)
    * when performing normalisations identified by key, allow the value to be a lit
    
    * simplify list check

--- miro_preprocessor/image_sorter/src/sorter_logic.py
+++ miro_preprocessor/image_sorter/src/sorter_logic.py
@@ -49,8 +49,18 @@ class Rules:
     def _get_normalised(self, key):
         return self._normalise_string(self._get(key))
 
+    def _compare_normalised(self, a, b):
+        return self._normalise_string(a) == self._normalise_string(b)
+
     def _compare(self, key, value):
-        return self._get_normalised(key) == self._normalise_string(value)
+        key_values = self._get(key)
+
+        if not isinstance(key_values, list):
+            key_values = [key_values]
+
+        return any(
+            self._compare_normalised(key_value, value) for key_value in key_values
+        )
 
     def _is_collection(self, collection_name):
         return self._normalise_string(self.collection) == self._normalise_string(f"images-{collection_name}")
