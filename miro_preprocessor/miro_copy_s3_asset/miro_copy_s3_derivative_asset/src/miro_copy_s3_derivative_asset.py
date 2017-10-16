1b46fea9ca1a5de56417f3bd239035b7dc6f63f6
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-10-16 11:09:50 +0100

    Trying to further eliminate duplication

--- miro_preprocessor/miro_copy_s3_asset/miro_copy_s3_derivative_asset/src/miro_copy_s3_derivative_asset.py
+++ miro_preprocessor/miro_copy_s3_asset/miro_copy_s3_derivative_asset/src/miro_copy_s3_derivative_asset.py
@@ -1,25 +1,26 @@
+from functools import partial
 import json
 import os
 
 import boto3
-from botocore.exceptions import ClientError
 
 from miro_utils import MiroImage
 import sns_utils
+from s3_utils import S3_Identifier
 import s3_utils
 
 
 def copy_and_forward_message(s3_client,
                              sns_client,
                              image_info,
-                             source_bucket_name,
-                             source_key,
-                             destination_bucket_name,
-                             destination_key,
-                             source_hash,
-                             topic_arn):
-    s3_utils.copy_asset_if_not_exists(s3_client, source_hash, destination_bucket_name, destination_key,
-                             source_bucket_name, source_key)
+                             topic_arn,
+                             source_identifier,
+                             destination_identifier,
+                             source_head_response):
+    s3_utils.copy_asset_if_not_exists(s3_client=s3_client,
+                                      source_head_response=source_head_response,
+                                      source_identifier=source_identifier,
+                                      destination_identifier=destination_identifier)
     sns_utils.publish_sns_message(
         sns_client,
         topic_arn,
@@ -37,23 +38,13 @@ def main(event, _):
     image_info = json.loads(event['Records'][0]['Sns']['Message'])
     miro_image = MiroImage(image_info)
     source_key = f"fullsize/{miro_image.image_path}.jpg"
-
-    try:
-        source_head_response = s3_client.head_object(Bucket=source_bucket_name, Key=source_key)
-    except ClientError as client_error:
-        if client_error.response['Error']['Code'] == '404':
-            print(f"No image found for MiroId {miro_image.miro_id}: skipping")
-            pass
-        else:
-            raise
-    else:
-        destination_key = f"{miro_image.image_path}.jpg"
-        copy_and_forward_message(s3_client,
-                                 sns_client,
-                                 image_info,
-                                 source_bucket_name,
-                                 source_key,
-                                 destination_bucket_name,
-                                 destination_key,
-                                 source_head_response['ETag'],
-                                 topic_arn)
+    destination_key = f"{miro_image.image_path}.jpg"
+    source_identifier = S3_Identifier(source_bucket_name, source_key)
+    destination_identifier = S3_Identifier(destination_bucket_name, destination_key)
+    s3_utils.exec_if_key_exists(s3_client, source_identifier=source_identifier,
+                            function=partial(
+                                copy_and_forward_message,
+                                sns_client=sns_client,
+                                image_info=image_info,
+                                topic_arn=topic_arn,
+                                destination_identifier=destination_identifier))
