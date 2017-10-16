4498e6394ed6a7f5772552888978868403439ba8
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-10-16 17:14:58 +0100

    Linting

--- shared_infra/lambdas/common/setup.py
+++ shared_infra/lambdas/common/setup.py
@@ -2,6 +2,7 @@ import os
 
 from setuptools import find_packages, setup
 
+
 def local_file(name):
     return os.path.relpath(os.path.join(os.path.dirname(__file__), name))
 
@@ -9,16 +10,16 @@ def local_file(name):
 SOURCE = local_file('src')
 
 setup(
-    name = 'wellcome_lambda_utils',
+    name='wellcome_lambda_utils',
     packages=find_packages(SOURCE),
     package_dir={'': SOURCE},
-    version = '0.0.0.4',
-    install_requires = ['boto'],
+    version='0.0.0.4',
+    install_requires=['boto'],
     python_requires='>=3',
-    description = 'Common lib for lambdas',
-    author = 'Wellcome digital platform',
-    author_email = 'wellcomedigitalplatform@wellcome.ac.uk',
-    url = 'https://github.com/wellcometrust/platform',
-    keywords = ['lambda','utils'],
-    classifiers = [],
+    description='Common lib for lambdas',
+    author='Wellcome digital platform',
+    author_email='wellcomedigitalplatform@wellcome.ac.uk',
+    url='https://github.com/wellcometrust/platform',
+    keywords=['lambda', 'utils'],
+    classifiers=[],
 )
