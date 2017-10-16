b8754dadb2539bc8d6581db9ce498cdece22cb5c
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-10-16 09:40:53 +0100

    PUT don't POST to include ID in inventory lambda

--- miro_preprocessor/miro_inventory/src/miro_inventory.py
+++ miro_preprocessor/miro_inventory/src/miro_inventory.py
@@ -10,7 +10,7 @@ from botocore.vendored import requests
 
 
 # Super naive jsonpath!
-def _extract_id_field(o, id_field):
+def _extract_id(o, id_field):
     path = id_field.split(".")
 
     for node in path:
@@ -19,24 +19,32 @@ def _extract_id_field(o, id_field):
     return o
 
 
-def _generate_indexable_json(event, id_field):
-    subject = event['Records'][0]['Sns']['Subject']
-    message = json.loads(event['Records'][0]['Sns']['Message'])
-    id = _extract_id_field(message, id_field)
+def _extract_subject(event):
+    return event['Records'][0]['Sns']['Subject']
 
-    return json.dumps({
+
+def _extract_message(event):
+    return json.loads(event['Records'][0]['Sns']['Message'])
+
+
+def _generate_indexable_object(event, id_field):
+    message = _extract_message(event)
+    subject = _extract_subject(event)
+    id = _extract_id(message, id_field)
+
+    return {
         'id': id,
         'subject': subject,
         'message': message
-    })
+    }
 
 
-def _post_to_es(es_cluster_url, es_index, es_type, es_username, es_password, payload):
-    post_url = f'{es_cluster_url}/{es_index}/{es_type}'
-    print(f'POSTing to {post_url}')
+def _put_to_es(id, es_cluster_url, es_index, es_type, es_username, es_password, payload):
+    put_url = f'{es_cluster_url}/{es_index}/{es_type}/{id}'
+    print(f'PUTing to {put_url}')
 
-    return requests.post(
-        post_url,
+    return requests.put(
+        put_url,
         data=payload,
         auth=(es_username, es_password)
     )
@@ -53,17 +61,18 @@ def main(event, _):
     es_password = os.environ["ES_PASSWORD"]
 
     id_field = os.environ["ID_FIELD"]
+    indexable = _generate_indexable_object(event, id_field)
 
-    indexable_json = _generate_indexable_json(event, id_field)
-    print(f'indexable_json: {indexable_json}')
+    print(f'indexable_json: {indexable}')
 
-    response = _post_to_es(
+    response = _put_to_es(
+        indexable['id'],
         es_cluster_url,
         es_index,
         es_type,
         es_username,
         es_password,
-        indexable_json
+        json.dumps(indexable)
     )
-    print(f'_post_to_es response: {response}')
+    print(f'_put_to_es response: {response}')
     response.raise_for_status()
