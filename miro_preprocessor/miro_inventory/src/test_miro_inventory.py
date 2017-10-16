61ae5b2c62b265a2ac12d606eb4731f4c89e1161
Robert Kenny <kenoir@users.noreply.github.com>
2017-10-16 10:16:13 +0100

    PUT don't POST to include ID in inventory lambda (#1075)

--- miro_preprocessor/miro_inventory/src/test_miro_inventory.py
+++ miro_preprocessor/miro_inventory/src/test_miro_inventory.py
@@ -9,8 +9,13 @@ from unittest import mock
 import miro_inventory
 
 miro_id = "A0000002"
-
+index = 'index'
+type = 'type'
 collection = "Images-C"
+cluster_url = 'https://example.com'
+
+put_url = f'{cluster_url}/{index}/{type}/{miro_id}'
+
 image_data = {
     'image_no_calc': miro_id,
     'image_int_default': None,
@@ -26,9 +31,9 @@ image_json = json.dumps({
 })
 
 os.environ = {
-    "ES_CLUSTER_URL": 'https://example.com',
-    "ES_INDEX": 'index',
-    "ES_TYPE": 'type',
+    "ES_CLUSTER_URL": cluster_url,
+    "ES_INDEX": index,
+    "ES_TYPE": type,
     "ES_USERNAME": 'foo',
     "ES_PASSWORD": 'foo',
     "ID_FIELD": 'image_data.image_no_calc',
@@ -57,31 +62,31 @@ event = {
 }
 
 
-# This method will be used by the mock to replace requests.post
-def mocked_requests_post(*args, **kwargs):
+# This method will be used by the mock to replace requests.put
+def mocked_requests_put(*args, **kwargs):
     class MockResponse(requests.Response):
         def __init__(self, status_code):
             self.status_code = status_code
             self.reason = "I am a mock object, don't ask me"
 
-    if args[0] == 'https://example.com/index/type':
+    if args[0] == put_url:
         return MockResponse(200)
 
     return MockResponse(500)
 
 
-@mock.patch('miro_inventory.requests.post', side_effect=mocked_requests_post)
+@mock.patch('miro_inventory.requests.put', side_effect=mocked_requests_put)
 def test_miro_inventory(mock_get):
     miro_inventory.main(event, None)
 
     mock_get.assert_called_with(
-        'https://example.com/index/type',
+        put_url,
         data='{"id": "A0000002", "subject": "catalogue_api", "message": {"collection": "Images-C", "image_data": {"image_no_calc": "A0000002", "image_int_default": null, "image_artwork_date_from": "01/02/2000", "image_artwork_date_to": "13/12/2000", "image_barcode": "10000000", "image_creator": ["Caspar Bauhin"]}}}',
         auth=('foo', 'foo')
     )
 
 
-@mock.patch('miro_inventory.requests.post', side_effect=mocked_requests_post)
+@mock.patch('miro_inventory.requests.put', side_effect=mocked_requests_put)
 def test_miro_inventory_raises_error_on_500(mock_get):
     with pytest.raises(HTTPError):
         os.environ["ES_INDEX"] = "nope"
