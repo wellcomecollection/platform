efa10c4a6d78d6a87cee3d40f1dcbf18911d2ef6
Alex Chan <a.chan@wellcome.ac.uk>
2017-10-18 11:55:35 +0100

    These functions aren't used anywhere???

--- lambda_utils/src/wellcome_lambda_utils/s3_utils.py
+++ lambda_utils/src/wellcome_lambda_utils/s3_utils.py
@@ -1,12 +1,7 @@
-import collections
+# -*- encoding: utf-8 -*-
 
 import boto3
 from botocore.exceptions import ClientError
-
-S3_Identifier = collections.namedtuple(
-    'S3_Identifier',
-    'bucket_name key'
-)
 
 
 def is_object(bucket, key):
@@ -58,45 +53,3 @@ def copy_object(src_bucket, src_key, dst_bucket, dst_key, lazy=False):
             Bucket=dst_bucket,
             Key=dst_key
         )
-
-
-def _copy_image_asset(s3_client, source_identifier, destination_identifier):
-    print(f"copying from {source_identifier.bucket_name}/{source_identifier.key} to {destination_identifier.bucket_name}/{destination_identifier.key}")
-    s3_client.copy_object(
-        CopySource={
-            'Bucket': source_identifier.bucket_name,
-            'Key': source_identifier.key
-        },
-        Bucket=destination_identifier.bucket_name,
-        Key=destination_identifier.key)
-
-
-def _is_same_etag(destination_head_response, source_head_response):
-    return source_head_response['ETag'] == destination_head_response['ETag']
-
-
-def copy_asset_if_not_exists(s3_client, source_head_response, source_identifier, destination_identifier):
-    try:
-        destination_head_response = s3_client.head_object(Bucket=destination_identifier.bucket_name,
-                                                          Key=destination_identifier.key)
-    except ClientError as client_error:
-        if client_error.response['Error']['Code'] == '404':
-            print(f"Destination bucket has no image")
-            pass
-        else:
-            raise
-    if not destination_head_response or not _is_same_etag(destination_head_response, source_head_response):
-        _copy_image_asset(s3_client, source_identifier, destination_identifier)
-
-
-def exec_if_key_exists(s3_client, source_identifier, function):
-    try:
-        source_head_response = s3_client.head_object(Bucket=source_identifier.bucket_name, Key=source_identifier.key)
-    except ClientError as client_error:
-        if client_error.response['Error']['Code'] == '404':
-            print(f"No image found for key {source_identifier.key}: skipping")
-            pass
-        else:
-            raise
-    else:
-        function(s3_client=s3_client, source_identifier=source_identifier, source_head_response=source_head_response)
