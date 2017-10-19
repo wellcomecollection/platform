8a0a4cd2c16f9bffe8f4d6e374c2a312fd58ad7c
Alex Chan <a.chan@wellcome.ac.uk>
2017-10-19 15:56:39 +0100

    Create all the upload sets

--- lambda_utils/src/wellcome_lambda_utils/tandem_vault.py
+++ lambda_utils/src/wellcome_lambda_utils/tandem_vault.py
@@ -3,10 +3,30 @@
 Utilities for interacting with the Tandem Vault API.
 """
 
+import collections
+
 import requests
 
 
 API_URL = 'https://wellcome.tandemvault.com/api/v1'
+
+
+Miro = collections.namedtuple('Miro', ['upload_set'])
+
+upload_sets = {
+    'A: Animal images': Miro(47806),
+    'AS: Family life': Miro(47807),
+    'B: External biomedical': Miro(47808),
+    'D: Footage': Miro(47809),
+    'F: Microfilm': Miro(47810),
+    'FP: Family life': Miro(47811),
+    'L: Library (historical)': Miro(47812),
+    'M: Library (historical)': Miro(47813),
+    'N: Clinical': Miro(47814),
+    'S: Slide collection': Miro(47815),
+    'V: Iconographic works': Miro(47816),
+    'W: Publishing group Intl health': Miro(47817),
+}
 
 
 class TandemVaultAPI(object):
@@ -33,5 +53,22 @@ class TandemVaultAPI(object):
         return resp.json()['id']
 
 
+titles = [
+    'A: Animal images',
+    'AS: Family life',
+    'B: External biomedical',
+    'D: Footage',
+    'F: Microfilm',
+    'FP: Family life',
+    'L: Library (historical)',
+    'M: Library (historical)',
+    'N: Clinical',
+    'S: Slide collection',
+    'V: Iconographic works',
+    'W: Publishing group Intl health',
+]
+
+
 api = TandemVaultAPI(api_key=API_KEY)
-api.create_upload_set(title='Alex test upload set')
+for t in titles:
+    print(t, api.create_upload_set(title=t))
