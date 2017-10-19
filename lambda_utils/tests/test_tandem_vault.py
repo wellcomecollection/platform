73422302d635cf3fb7af3ea3dec0a38259288aa3
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-10-19 15:49:27 +0100

    Revert "Also add a way to list upload sets"
    f0f7dfc467646cf830d7731ef6ac30401055f03c

--- lambda_utils/tests/test_tandem_vault.py
+++ lambda_utils/tests/test_tandem_vault.py
@@ -15,26 +15,15 @@ with Betamax.configure() as config:
     config.cassette_library_dir = 'cassettes'
     config.define_cassette_placeholder('<API_KEY>', api_key)
 
-sess = requests.Session()
-with Betamax(sess) as vcr:
-    vcr.use_cassette('tandem_vault')
-    api = tandem_vault.TandemVaultAPI(api_key, sess=sess)
+
+@pytest.fixture
+def api():
+    sess = requests.Session()
+    with Betamax(sess) as vcr:
+        vcr.use_cassette('tandem_vault')
+        yield tandem_vault.TandemVaultAPI(api_key, sess=sess)
 
 
-def test_can_create_upload_set():
+def test_can_create_upload_set(api):
     set_id = api.create_upload_set(title='Alex test upload set')
     assert isinstance(set_id, int)
-
-
-def test_can_get_upload_set():
-    upload_sets = api.get_upload_sets()
-
-    # This is a generator object, we can read some upload sets
-    for _ in range(3):
-        next(upload_sets)
-
-    # And make some quick assertions about the fourth upload set
-    upload_set = next(upload_sets)
-    assert isinstance(upload_set, dict)
-    assert 'id' in upload_set
-    assert 'title' in upload_set
