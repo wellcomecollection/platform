2f2216550105993c66486cfff89c5d74a4a2584b
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-10-19 12:41:43 +0100

    Add a skeleton stack for the Sierra adapter (#1092)
    * Add a Makefile for the Sierra adapter
    
    * Add a README for the Sierra adapter
    
    * Add a skeleton Terraform stack
    
    * Add some Make tasks for the Sierra terraform stack
    
    * Add an ECR repo for the Sierra adapter
    
    * Copy across the old Sierra OAuth script
    
    * Add tasks for building/publishing the Sierra adapter
    
    * Add this stack to Travis
    
    * Add a way to run tests on the Sierra adapter
    
    * Reduce the levels of nesting
    
    * Add the rest of the Terraform stack
    
    * Tweak the Travis config
    
    * Change maintainer email address
    
    * We don't have remote tfvars yet
    
    * pylint

---
+++ sierra_adapter/src/run.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+"""
+Python script for doing basic requests against Sierra.
+
+To configure:
+
+*   Fill in the API key and API secret you got from Sierra
+*   Change the URL to point to the resource you want
+
+"""
+
+from pprint import pprint
+
+import requests
+
+oauthkey = '<API_KEY>'
+oauthsec = '<API_SECRET>'
+
+BASE = 'https://libsys.wellcomelibrary.org/iii'
+API_VERSION = 'sierra-api/v3'
+
+# Change this URL to whatever resource you want to retrieve
+URL = f'{BASE}/{API_VERSION}/branches'
+
+# Get an access token
+# https://sandbox.iii.com/docs/Content/zReference/authClient.htm
+resp = requests.post(f'{BASE}/{API_VERSION}/token', auth=(oauthkey, oauthsec))
+resp.raise_for_status()
+access_token = resp.json()['access_token']
+
+# https://sandbox.iii.com/docs/Content/zReference/authClient.htm
+# https://sandbox.iii.com/docs/Content/zReference/operations.htm#XML
+headers = {
+    'Authorization': f'Bearer {access_token}',
+    'Accept': 'application/json',
+}
+
+print('*** Fetching contents of %s' % URL)
+resp = requests.get(URL, headers=headers)
+resp.raise_for_status()
+
+pprint(resp.json())
