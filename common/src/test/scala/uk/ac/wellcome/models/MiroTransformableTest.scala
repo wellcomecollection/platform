92c65c391594dd61c6cc93d37ae6e9f49c619f7f
Alex Chan <a.chan@wellcome.ac.uk>
2017-10-19 11:50:55 +0100

    Add a test that genres are de-duplicated in Miro

--- common/src/test/scala/uk/ac/wellcome/models/MiroTransformableTest.scala
+++ common/src/test/scala/uk/ac/wellcome/models/MiroTransformableTest.scala
@@ -503,6 +503,19 @@ class MiroTransformableGenresTest
     )
   }
 
+  it("should deduplicate entries in the genre field if necessary") {
+    transformRecordAndCheckGenres(
+      data = s"""
+        "image_title": "A duality of dancing dodos",
+        "image_phys_format": "oil painting",
+        "image_lc_genre": "oil painting"
+      """,
+      expectedGenres = List(
+        Concept("oil painting")
+      )
+    )
+  }
+
   private def transformRecordAndCheckGenres(
     data: String,
     expectedGenres: List[Concept] = List()
