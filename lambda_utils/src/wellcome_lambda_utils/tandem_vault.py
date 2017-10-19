f4079001167ea950ce5c67f017910f6607606da0
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-10-19 14:54:35 +0100

    Okay, but now in a class

--- lambda_utils/src/wellcome_lambda_utils/tandem_vault.py
+++ lambda_utils/src/wellcome_lambda_utils/tandem_vault.py
@@ -3,25 +3,35 @@
 Utilities for interacting with the Tandem Vault API.
 """
 
-# from wellcome_lambda_utils.http_utils import http_post
-from http_utils import http_post
+import requests
+
 
 API_URL = 'https://wellcome.tandemvault.com/api/v1'
 
 
-def create_upload_set(api_key, title):
-    """
-    Create an upload set in Tandem Vault, and return the upload
-    set identifier.
-    """
-    resp = http_post(
-        f'{API_URL}/upload_sets',
-        params={
-            'api_key': api_key,
-            'upload_set[title]': title
-        }
-    )
-    return resp.json()['id']
+class TandemVaultAPI(object):
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.sess = requests.Session()
+
+    def create_upload_set(self, title):
+        """
+        Create an upload set in TV, and return the upload set identifier.
+
+        Note: Upload set titles are not unique in TV, so calling this method
+        more than once will create multiple upload sets.
+        """
+        resp = self.sess.post(
+            f'{API_URL}/upload_sets',
+            params={
+                'api_key': self.api_key,
+                'upload_set[title]': title,
+            }
+        )
+        resp.raise_for_status()
+        return resp.json()['id']
 
 
-create_upload_set(API_KEY, title='Alex test upload set')
+api = TandemVaultAPI(api_key=API_KEY)
+api.create_upload_set(title='Alex test upload set')
