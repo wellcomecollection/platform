a7de452ca4319c978cce761b09bde0f13f1d4c1f
Alex Chan <a.chan@wellcome.ac.uk>
2017-10-20 10:26:14 +0100

    Upload assets to Tandem Vault

--- lambda_utils/src/wellcome_lambda_utils/tandem_vault.py
+++ lambda_utils/src/wellcome_lambda_utils/tandem_vault.py
@@ -4,15 +4,18 @@ Utilities for interacting with the Tandem Vault API.
 """
 
 import collections
+import os
 from pprint import pprint
 
+import boto3
+import dateutil.parser
 import requests
 
 
 API_URL = 'https://wellcome.tandemvault.com/api/v1'
 
 
-Miro = collections.namedtuple('Miro', ['upload_set', 'collection_id'])
+Miro = collections.namedtuple('Miro', ['upload_set_id', 'collection_id'])
 
 miro_collections = {
     'A': Miro(47806, 111597),
@@ -28,6 +31,10 @@ miro_collections = {
     'V': Miro(47816, 111609),
     'W': Miro(47817, 1116011),
 }
+
+
+def seconds_since_epoch(dt_string):
+    return dateutil.parser.parse(dt_string).epoch
 
 
 class TandemVaultAPI(object):
@@ -66,3 +73,57 @@ class TandemVaultAPI(object):
         )
         resp.raise_for_status()
         pprint(resp.json())
+
+    def upload_image_to_tv(self, src_bucket, src_key):
+        s3 = boto3.client('s3')
+        body = s3.get_object(Bucket=src_bucket, Key=src_key)['Body']
+
+        # Retrieve permissions for uploading to S3
+        resp = self.sess.post(
+            f'{API_URL}/assets/get_upload_signature',
+            params={
+                'api_key': self.api_key,
+                'filename': os.path.basename(src_key),
+                'content_type': 'image/jpeg',
+            }
+        )
+        resp.raise_for_status()
+        upload_data = resp.json()
+        pprint(upload_data)
+
+        # Use the response from the previous request to upload the
+        # asset to S3
+        resp = self.sess.post(
+            'http://uploads.tandemstock.com.s3.amazonaws.com/',
+            files={
+                'Content-Type': 'image/jpeg',
+                'Content-Disposition': 'attachment',
+                'AWSAccessKeyId': upload_data['access_key_id'],
+                'Signature': upload_data['signature'],
+                'policy': upload_data['policy'],
+                'acl': 'private',
+                'key': upload_data['key'],
+                'filename': os.path.basename(src_key),
+                'success_action_status': '201',
+                'file': body,
+            }
+        )
+        resp.raise_for_status()
+        s3_data = resp.text
+        pprint(s3_data)
+
+        # Now add the asset to Tandem Vault
+        resp = self.sess.post(
+            f'{API_URL}/assets',
+            params={
+                'api_key': self.api_key,
+                'upload_set_id': miro_collections['B'].upload_set_id,
+                'asset[filename]': os.path.basename(src_key),
+            }
+        )
+        resp.raise_for_status()
+        asset_data = resp.json()
+        pprint(asset_data)
+
+tv = TandemVaultAPI(API_KEY)
+tv.upload_image_to_tv('miro-images-sync', 'fullsize/B0008000/B0008752.jpg')
