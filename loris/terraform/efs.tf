11ad99079bcc40dc039565a5f131fac4cb6d276b
caylaa <alicefuzier@gmail.com>
2017-10-20 09:26:03 +0100

    Migrating main loris cluster to c4.xlarge with EBS IO1 volume. Switching second loris cluster to EBS GP2 to see if we can reduce costs

--- loris/terraform/efs.tf
+++
@@ -1,8 +0,0 @@
-module "loris_efs" {
-  name                         = "loris_cache"
-  source                       = "git::https://github.com/wellcometrust/terraform.git//efs?ref=v1.0.0"
-  vpc_id                       = "${local.vpc_api_id}"
-  subnets                      = "${local.vpc_api_subnets}"
-  efs_access_security_group_id = "${module.loris_cluster_asg.instance_sg_id}"
-  performance_mode             = "maxIO"
-}
