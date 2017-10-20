07146b998477cbb08b3f989615efc99c69808387
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-10-20 10:46:22 +0100

    Formatting

--- loris/terraform/userdata.tf
+++ loris/terraform/userdata.tf
@@ -1,6 +1,6 @@
 module "loris_userdata" {
-  source            = "git::https://github.com/wellcometrust/terraform.git//userdata?ref=v1.0.0"
-  cluster_name      = "${aws_ecs_cluster.loris.name}"
+  source                             = "git::https://github.com/wellcometrust/terraform.git//userdata?ref=v1.0.0"
+  cluster_name                       = "${aws_ecs_cluster.loris.name}"
   ebs_block_device                   = "/dev/xvdb"
   cache_cleaner_cloudwatch_log_group = "${aws_cloudwatch_log_group.cache_cleaner_log_group.name}"
   ebs_cache_max_age_days             = "30"
