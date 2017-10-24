f29d00ad4bca561cb64ad81af44acb10f765f625
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-10-24 11:19:17 +0100

    Add more permissions

--- shared_infra/iam_policy_document.tf
+++ shared_infra/iam_policy_document.tf
@@ -89,3 +89,15 @@ data "aws_iam_policy_document" "sqs_readwrite" {
     ]
   }
 }
+
+data "aws_iam_policy_document" "upload_sqs_freeze" {
+  statement {
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.infra.arn}/sqs/*",
+    ]
+  }
+}
