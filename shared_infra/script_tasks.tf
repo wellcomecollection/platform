4f80d1f5084fae97e779fe6922d425c80d68262e
Alex Chan <a.chan@wellcome.ac.uk>
2017-10-24 11:27:55 +0100

    Fix names, variables

--- shared_infra/script_tasks.tf
+++ shared_infra/script_tasks.tf
@@ -2,7 +2,7 @@ module "sqs_freezeray" {
   source        = "git::https://github.com/wellcometrust/terraform.git//ecs_script_task?ref=v1.0.0"
   task_name     = "sqs_freezeray"
   app_uri       = "${module.ecr_repository_sqs_freezeray.repository_url}:${var.release_ids["sqs_freezeray"]}"
-  task_role_arn = "${module.ecs_elasticdump_iam.task_role_arn}"
+  task_role_arn = "${module.ecs_sqs_freezeray_iam.task_role_arn}"
 
   cpu    = 1024
   memory = 1024
