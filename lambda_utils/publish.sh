ec79b253917045863d9dbef9f7f444a53d871c66
Alex Chan <a.chan@wellcome.ac.uk>
2017-10-24 09:23:08 +0100

    Be less bad about variables

--- lambda_utils/publish.sh
+++ lambda_utils/publish.sh
@@ -3,15 +3,15 @@
 set -o errexit
 set -o nounset
 
-if [ "$(PUBLISH_VERSION)" = "$(SETUP_VERSION)" ]
+if [ "$PUBLISH_VERSION" = "$SETUP_VERSION" ]
 then
 	echo "*** Uploaded package is up-to-date, nothing to do"
 else
 	echo "*** Newer version available, uploading new package"
 	docker run                                                                  \
-		--volume $(LAMBDA_UTILS):/src                                             \
+		--volume $LAMBDA_UTILS:/src                                               \
 		--workdir /src                                                            \
-		--env TWINE_USERNAME="$(PYPI_USERNAME)"                                   \
-		--env TWINE_PASSWORD="$(PYPI_PASSWORD)"                                   \
+		--env TWINE_USERNAME="$PYPI_USERNAME"                                     \
+		--env TWINE_PASSWORD="$PYPI_PASSWORD"                                     \
 		greengloves/twine:1.9.1 upload dist/*.tar.gz
 fi
