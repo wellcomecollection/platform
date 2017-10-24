745be1e671f47ea335112bde4a89f079fb1adbdb
alicefuzier <alicefuzier@gmail.com>
2017-10-24 10:48:59 +0100

    End loris experiments (#1111)
    * Delete second Loris cluster. Keep c4.xlarge with GP2 EBS config
    
    * Delete ebs alb
    
    * Delete cluster as well

--- loris/terraform/ecs.tf
+++ loris/terraform/ecs.tf
@@ -1,7 +1,3 @@
 resource "aws_ecs_cluster" "loris" {
   name = "loris_cluster"
 }
-
-resource "aws_ecs_cluster" "loris_ebs" {
-  name = "loris_cluster_ebs"
-}
--- loris/terraform/services.tf
+++ loris/terraform/services.tf
@@ -30,36 +30,3 @@ module "loris" {
   server_error_alarm_topic_arn = "${local.alb_server_error_alarm_arn}"
   client_error_alarm_topic_arn = "${local.alb_client_error_alarm_arn}"
 }
-
-module "loris_ebs" {
-  source             = "git::https://github.com/wellcometrust/terraform.git//services?ref=v1.0.0"
-  name               = "loris-ebs"
-  cluster_id         = "${aws_ecs_cluster.loris_ebs.id}"
-  task_role_arn      = "${module.ecs_loris_iam.task_role_arn}"
-  vpc_id             = "${local.vpc_api_id}"
-  app_uri            = "${module.ecr_loris.repository_url}:${var.release_ids["loris"]}"
-  nginx_uri          = "${module.ecr_nginx_loris.repository_url}:${var.release_ids["nginx_loris"]}"
-  listener_https_arn = "${module.loris_alb_ebs.listener_https_arn}"
-  listener_http_arn  = "${module.loris_alb_ebs.listener_http_arn}"
-  infra_bucket       = "${var.infra_bucket}"
-  config_key         = "config/${var.build_env}/loris-ebs.ini"
-  path_pattern       = "/image*"
-  healthcheck_path   = "/image/"
-  alb_priority       = "101"
-
-  cpu    = 3960
-  memory = 7350
-
-  desired_count = 4
-
-  deployment_minimum_healthy_percent = "50"
-  deployment_maximum_percent         = "200"
-
-  volume_name      = "loris"
-  volume_host_path = "${module.loris_userdata_ebs.efs_mount_directory}/loris"
-  container_path   = "/mnt/loris"
-
-  loadbalancer_cloudwatch_id   = "${module.loris_alb.cloudwatch_id}"
-  server_error_alarm_topic_arn = "${local.alb_server_error_alarm_arn}"
-  client_error_alarm_topic_arn = "${local.alb_client_error_alarm_arn}"
-}
