e9882fe9cba4a971121c99609006da7930bde9a5
Alex Chan <a.chan@wellcome.ac.uk>
2017-10-24 10:34:07 +0100

    More functions!

--- shared_infra/sqs_freezeray/run.py
+++ shared_infra/sqs_freezeray/run.py
@@ -6,9 +6,9 @@ import json
 import boto3
 
 
-def get_messages(queue_url, delete_messages=False, batch_size=10):
+def get_messages(queue_url, delete=False, batch_size=10):
     """
-    Gets messages from an SQS queue.  If ``delete_messages`` is True, the
+    Gets messages from an SQS queue.  If ``delete`` is True, the
     messages are also deleted after they've been read.
     """
     client = boto3.client('sqs')
@@ -27,7 +27,7 @@ def get_messages(queue_url, delete_messages=False, batch_size=10):
         # If we're deleting the messages ourselves, we don't need to send
         # the ReceiptHandle to the caller (it's only used for deleting).
         # If not, we send the entire response.
-        if delete_messages:
+        if delete:
             for m in resp['Messages']:
                 yield {k: v for k, v in m.items() if k != 'ReceiptHandle'}
         else:
@@ -35,7 +35,7 @@ def get_messages(queue_url, delete_messages=False, batch_size=10):
 
         # Now delete the messages from the queue, so they won't be read
         # on the next GET call.
-        if delete_messages:
+        if delete:
             client.delete_message_batch(
                 QueueUrl=queue_url,
                 Entries=[
@@ -55,12 +55,32 @@ def write_to_s3(bucket, key, messages):
 
 
 
-messages = []
+def write_all_messages_to_s3(bucket, key, queue_url):
+    """
+    Write all the messages from a queue to an S3 bucket.
+    """
+    messages = []
 
-for m in get_messages('https://sqs.eu-west-1.amazonaws.com/760097843905/alex-test-queue', delete_messages=False):
-    messages.append(m)
+    def write():
+        write_to_s3(bucket=bucket, key=key, messages=messages)
+
+    generator = get_messages(queue_url=queue_url, delete=True, batch_size=10)
+    for i, message in enumerate(generator):
+
+        # Because messages are deleted after they're processed, and received
+        # in batches of 10, we write to S3 after every 10 messages.  If the
+        # task exits halfway through, we haven't lost data.
+        if i % 10 == 0:
+            write()
+
+    write()
 
 
-write_to_s3(bucket='platform-infra', key='sqs/alex-test-queue.txt', messages=messages)
 
-print(data)
+
+if __name__ == '__main__':
+    bucket = 'platform-infra'
+    key = 'sqs/alex-test-queue.txt'
+    queue_url = 'https://sqs.eu-west-1.amazonaws.com/760097843905/alex-test-queue'
+
+    write_all_messages_to_s3(bucket=bucket, key=key, queue_url=queue_url)
