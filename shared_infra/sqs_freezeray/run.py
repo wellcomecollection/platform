7d6382c7b2ed247957683318af04d56990e1d038
Alex Chan <a.chan@wellcome.ac.uk>
2017-10-24 10:27:08 +0100

    Push the SQS reader into a generator

--- shared_infra/sqs_freezeray/run.py
+++ shared_infra/sqs_freezeray/run.py
@@ -5,16 +5,50 @@ import json
 
 import boto3
 
-sqs_client = boto3.client('sqs')
+
+def get_messages(queue_url, delete_messages=False, batch_size=10):
+    """
+    Gets messages from an SQS queue.  If ``delete_messages`` is True, the
+    messages are also deleted after they've been read.
+    """
+    client = boto3.client('sqs')
+    while True:
+        # We batch message responses to reduce load on the SQS API.
+        # Note: 10 is currently the most messages you can read at once.
+        resp = client.receive_message(
+            QueueUrl=queue_url,
+            MaxNumberOfMessages=batch_size
+        )
+
+        # If there's nothing available, the queue is empty.  Abort!
+        if 'Messages' not in resp:
+            break
+
+        # If we're deleting the messages ourselves, we don't need to send
+        # the ReceiptHandle to the caller (it's only used for deleting).
+        # If not, we send the entire response.
+        if delete_messages:
+            for m in resp['Messages']:
+                yield {k: v for k, v in m.items() if k != 'ReceiptHandle'}
+        else:
+            yield from resp['Messages']
+
+        # Now delete the messages from the queue, so they won't be read
+        # on the next GET call.
+        if delete_messages:
+            client.delete_message_batch(
+                QueueUrl=queue_url,
+                Entries=[
+                    {'Id': m['MessageId'], 'ReceiptHandle': m['ReceiptHandle']}
+                    for m in messages
+                ]
+            )
+
 
 messages = []
 
-resp = sqs_client.receive_message(
-    QueueUrl='https://sqs.eu-west-1.amazonaws.com/760097843905/alex-test-queue',
-    MaxNumberOfMessages=10,
-)
-
-messages.extend([json.dumps(m).encode('ascii') for m in resp['Messages']])
+for m in get_messages('https://sqs.eu-west-1.amazonaws.com/760097843905/alex-test-queue', delete_messages=False):
+    messages.append(json.dumps(m).encode('ascii'))
 
 s3_client = boto3.client('s3')
 
