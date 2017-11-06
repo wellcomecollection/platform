4cb8d75fe7d5f9f002f72ab625ab68753107f3b7
Robert Kenny <kenoir@users.noreply.github.com>
2017-11-06 09:41:46 +0000

    Miro image to tandem vault fixes (#1141)
    * Adds miro_image_to_tandem_vault lambda
    
    * cascade Makefile refs
    
    * seperate concerns for tandem vault, miro
    
    * drafting structure of lambda
    
    * cleaning up
    
    * add metadata
    
    * Infrastructure for tandem_vault_uploader and tandem_vault_enricher tasks
    
    * Remove miro_image_to_tandem_vault lambda and create make tasks for tandem_vault_uploader and tandem_vault_enricher
    
    * Formatting
    
    * adds app code
    
    * Enricher -> Enrichment
    
    * pylint
    
    * Some terraform fixes
    
    * contrib substit fun time
    
    * Add execute permission to scripts
    
    * Add entrypoint to start scripts
    
    * Always build before publishing
    
    * Ad the stupid shebang
    
    * Add region to environment variables and fix queue url for uploader
    
    * pylint
    
    * Adding logging
    
    * clean up data from miro a bit more
    
    * lint
    
    * Some logging improvements
    
    * really really really check that the  field is correct
    
    * Fix errors in extracting image_award_date
    
    * Updated upload set ids
    
    * Don't make images accepted by default
    
    * Prepend "Related images: " to the list of related ids
    
    * slide rule
    
    * Add newline after title
    
    * prepend b number
    
    * Add space
    
    * Linting

--- miro_preprocessor/miro_image_to_tandem_vault/src/tandem_vault_enrichment.py
+++ miro_preprocessor/miro_image_to_tandem_vault/src/tandem_vault_enrichment.py
@@ -5,9 +5,11 @@ Lambda for loading images and data into Tandem Vault
 """
 
 import json
+import logging
 import os
 
 import boto3
+import daiquiri
 
 from wellcome_lambda_utils.miro_utils import MiroImage
 
@@ -23,10 +25,17 @@ import tandem_vault_api
 import sqs_utils
 
 
+daiquiri.setup(level=logging.INFO)
+logger = daiquiri.getLogger(__name__)
+
+
 def is_wia_award_winner(miro_image):
+    if "image_award" not in miro_image.image_data:
+        return False
+    if not miro_image.image_data['image_award']:
+        return False
     if "Biomedical Image Awards" in miro_image.image_data['image_award']:
         return True
-
     if "Wellcome Image Awards" in miro_image.image_data['image_award']:
         return True
 
@@ -34,10 +43,24 @@ def is_wia_award_winner(miro_image):
 
 
 def determine_wia_collection(image_data):
-    assert len(image_data['image_award_date']) == 1
+    if "image_award_date" not in image_data:
+        raise InvalidWIAYear(image_data)
+    if not image_data['image_award_date']:
+        raise InvalidWIAYear(image_data)
 
-    if image_data['image_award_date'][0] in wia_year:
-        return wia_year[image_data['image_award_date'][0]]
+    if not isinstance(image_data['image_award_date'], list):
+        award_years = [image_data['image_award_date']]
+    else:
+        award_years = image_data['image_award_date']
+
+    award_years = list(set(award_years))
+    award_years = [award_year for award_year in award_years if award_year]
+
+    if not len(award_years) == 1:
+        raise InvalidWIAYear(image_data)
+
+    if award_years[0] in wia_year:
+        return wia_year[award_years[0]]
 
     raise InvalidWIAYear(image_data)
 
@@ -58,30 +81,36 @@ def main():
     sqs_reader = sqs_utils.SQSReader(sqs_client, queue_url)
 
     for message in sqs_reader:
-        outer_message = json.loads(message['Body'])
-        tandem_vault_upload_info = json.loads(outer_message['Message'])
+        try:
+            outer_message = json.loads(message['Body'])
+            tandem_vault_upload_info = json.loads(outer_message['Message'])
 
-        miro_image = MiroImage(tandem_vault_upload_info['image_info'])
-        asset_id = tandem_vault_upload_info['asset_id']
+            miro_image = MiroImage(tandem_vault_upload_info['image_info'])
+            asset_id = tandem_vault_upload_info['asset_id']
 
-        # Add to miro collection
-        miro_collection_id = miro_collections[miro_image.collection].collection_id
-        api.add_image_to_collection(asset_id, miro_collection_id)
+            logger.info(f"Adding metadata to {miro_image.miro_id}")
+            # Add to miro collection
+            miro_collection_id = miro_collections[miro_image.collection].collection_id
+            api.add_image_to_collection(asset_id, miro_collection_id)
 
-        # Add to wia collection
-        if is_wia_award_winner(miro_image):
-            wia_collection_id = determine_wia_collection(miro_image.image_data)
-            api.add_image_to_collection(asset_id, wia_collection_id)
+            # Add metadata
+            metadata = create_metadata(miro_image.image_data)
+            api.add_image_metadata(asset_id, metadata)
 
-        # Add metadata
-        metadata = create_metadata(miro_image.image_data)
-        api.add_image_metadata(asset_id, metadata)
+            # Add tags
+            tags = create_tags(miro_image.image_data)
+            api.add_image_tags(asset_id, tags)
 
-        # Add tags
-        tags = create_tags(miro_image.image_data)
-        api.add_image_tags(asset_id, tags)
+            # Add to wia collection
+            if is_wia_award_winner(miro_image):
+                wia_collection_id = determine_wia_collection(miro_image.image_data)
+                api.add_image_to_collection(asset_id, wia_collection_id)
 
-        sqs_reader.delete_current()
+            logger.info(f"Successfully added metadata for {miro_image.miro_id}")
+        except Exception:
+            logger.exception(f"Failed adding metadata for {miro_image}")
+        else:
+            sqs_reader.delete_current()
 
 
 if __name__ == '__main__':
