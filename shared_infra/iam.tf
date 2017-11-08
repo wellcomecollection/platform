570867a63ceb7d705a4faddb1f812f3d87795edc
caylaa <alicefuzier@gmail.com>
2017-11-08 09:43:55 +0000

    Dockerise end taskicise sqs_redrive

--- shared_infra/iam.tf
+++ shared_infra/iam.tf
@@ -2,3 +2,8 @@ module "ecs_sqs_freezeray_iam" {
   source = "git::https://github.com/wellcometrust/terraform.git//ecs_iam?ref=v1.0.0"
   name   = "sqs_freezeray"
 }
+
+module "ecs_sqs_redrive_iam" {
+  source = "git::https://github.com/wellcometrust/terraform.git//ecs_iam?ref=v1.0.0"
+  name   = "sqs_redrive"
+}
