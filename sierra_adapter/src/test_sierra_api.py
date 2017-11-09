fd724e3c79c9baa7a8af21bea6d92f2dced2d4ef
Alex Chan <a.chan@wellcome.ac.uk>
2017-11-09 11:10:00 +0000

    Push the Betamax stuff into a fixture

--- sierra_adapter/src/test_sierra_api.py
+++ sierra_adapter/src/test_sierra_api.py
@@ -4,59 +4,62 @@ import os
 
 import betamax
 from betamax import Betamax
+import pytest
 
 import sierra_api
 
 
-oauthkey = os.environ.get("SIERRA_OAUTH_KEY", "sierra_key")
-oauthsec = os.environ.get("SIERRA_OAUTH_SECRET", "sierra_secret")
+@pytest.fixture
+def api():
+    """Returns an instance of the API with Betamax enabled."""
+    # Read environment config.
+    oauthkey = os.environ.get("SIERRA_OAUTH_KEY", "sierra_key")
+    oauthsec = os.environ.get("SIERRA_OAUTH_SECRET", "sierra_secret")
 
-api_url = 'https://libsys.wellcomelibrary.org/iii/sierra-api/v3'
+    # The OAuth key and secret are initially passed with HTTP Basic Auth,
+    # so make sure we stub this out in recorded responses.
+    config = Betamax.configure()
+    config.define_cassette_placeholder(
+        '<SIERRA_OAUTH>',
+        base64.b64encode(
+            f'{oauthkey}:{oauthsec}'.encode('utf-8')
+        ).decode('utf-8')
+    )
 
-config = Betamax.configure()
-config.define_cassette_placeholder(
-    '<SIERRA_OAUTH>',
-    base64.b64encode(
-        f'{oauthkey}:{oauthsec}'.encode('utf-8')
-    ).decode('utf-8')
-)
+    # Now set up a recorder.
+    session = requests.Session()
+    recorder = Betamax(session, cassette_library_dir='/cassettes')
 
-session = requests.Session()
-recorder = betamax.Betamax(
-    session,
-    cassette_library_dir='/cassettes'
-)
-
-
-def test_can_get_bibs_from_api():
-    with recorder.use_cassette('sierra_bibs'):
-        expected_length = 29
-
-        api = sierra_api.SierraAPI(api_url, oauthkey, oauthsec, session)
-
-        bibs = api.get_objects("/bibs", {
-            'updatedDate': "[2013-12-10T17:16:35Z,2013-12-13T21:34:35Z]"
-        })
-
-        actual_length = len(bibs)
-        assert actual_length == expected_length
-
-        results = list(bibs)
-        assert len(results) == expected_length
+    # Then create an API session in the recorder, and pass it up to the
+    # test session.
+    with recorder.use_cassette('sierra_api'):
+        api_url = 'https://libsys.wellcomelibrary.org/iii/sierra-api/v3'
+        yield sierra_api.SierraAPI(api_url, oauthkey, oauthsec, session)
 
 
-def test_can_get_items_from_api():
-    with recorder.use_cassette('sierra_items'):
-        expected_length = 50
+def test_can_get_bibs_from_api(api):
+    expected_length = 29
 
-        api = sierra_api.SierraAPI(api_url, oauthkey, oauthsec, session)
+    bibs = api.get_objects("/bibs", params={
+        'updatedDate': "[2013-12-10T17:16:35Z,2013-12-13T21:34:35Z]"
+    })
 
-        bibs = api.get_objects("/items", {
-            'updatedDate': "[2013-12-10T17:16:35Z,2013-12-13T21:34:35Z]"
-        })
+    actual_length = len(bibs)
+    assert actual_length == expected_length
 
-        actual_length = len(bibs)
-        assert actual_length == expected_length
+    results = list(bibs)
+    assert len(results) == expected_length
 
-        results = list(bibs)
-        assert len(results) == expected_length
+
+def test_can_get_items_from_api(api):
+    expected_length = 50
+
+    bibs = api.get_objects("/items", {
+        'updatedDate': "[2013-12-10T17:16:35Z,2013-12-13T21:34:35Z]"
+    })
+
+    actual_length = len(bibs)
+    assert actual_length == expected_length
+
+    results = list(bibs)
+    assert len(results) == expected_length
