b5ace305e28a79b5e5e12491565fb58ea378dac0
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-11-09 10:15:20 +0000

    format

--- sierra_adapter/src/test_sierra_api.py
+++ sierra_adapter/src/test_sierra_api.py
@@ -11,8 +11,10 @@ import sierra_api
 def test_can_get_bibs_from_api():
     oauthkey = os.environ.get("SIERRA_OAUTH_KEY", "sierra_key")
     oauthsec = os.environ.get("SIERRA_OAUTH_SECRET", "sierra_secret")
+
     session = requests.Session()
     recorder = betamax.Betamax(session, cassette_library_dir='/cassettes')
+
     config = Betamax.configure()
     config.define_cassette_placeholder(
         '<SIERRA_OAUTH>',
@@ -20,13 +22,16 @@ def test_can_get_bibs_from_api():
             f'{oauthkey}:{oauthsec}'.encode('utf-8')
         ).decode('utf-8')
     )
+
     with recorder.use_cassette('sierra_bibs'):
         expected_length = 29
 
         api_url = 'https://libsys.wellcomelibrary.org/iii/sierra-api/v3'
         api = sierra_api.SierraAPI(api_url, oauthkey, oauthsec, session)
 
-        bibs = api.get_objects("/bibs", {'updatedDate': "[2013-12-10T17:16:35Z,2013-12-13T21:34:35Z]"})
+        bibs = api.get_objects("/bibs", {
+            'updatedDate': "[2013-12-10T17:16:35Z,2013-12-13T21:34:35Z]"
+        })
 
         actual_length = len(bibs)
         assert actual_length == expected_length
