80f07c5864536824ffa8af9314907634058287e5
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-11-09 16:56:15 +0000

    test_write_objects_to_s3

--- sierra_adapter/sierra_objects_to_s3/src/test_sierra_objects_to_s3.py
+++ sierra_adapter/sierra_objects_to_s3/src/test_sierra_objects_to_s3.py
@@ -1,6 +1,27 @@
 # -*- encoding: utf-8 -*-
 
+import json
+from operator import itemgetter
+
+import boto3
+from moto import mock_s3
+import pytest
+
 import sierra_objects_to_s3
+
+test_bucket_name = "bukkit"
+
+
+@pytest.yield_fixture(scope="function")
+def mock_s3_client():
+    mock_s3().start()
+
+    s3_client = boto3.client("s3")
+    s3_client.create_bucket(Bucket=test_bucket_name, ACL="private")
+
+    yield s3_client
+
+    mock_s3().stop()
 
 
 def test_build_from_to_params():
@@ -20,5 +41,42 @@ def test_build_from_to_params():
     ) == {'updatedDate': f'[,1982-01-04T00:00:00Z]'}
 
 
-def test_write_objects_to_s3():
-    assert True == True
+def test_write_objects_to_s3(mock_s3_client):
+    objects = sorted([
+        {
+            'id': '1',
+            'value': 'foo'
+        },
+        {
+            'id': '2',
+            'value': 'bar'
+        }
+    ], key=itemgetter('id'))
+
+    sierra_objects_to_s3.write_objects_to_s3(
+        mock_s3_client,
+        test_bucket_name,
+        'example',
+        objects
+    )
+
+    response = mock_s3_client.list_objects(
+        Bucket=test_bucket_name
+    )
+
+    assert len(response['Contents']) == len(objects)
+
+    received_objects = []
+
+    for object in response['Contents']:
+        print(object['Key'])
+        response = mock_s3_client.get_object(
+            Bucket=test_bucket_name,
+            Key=object['Key']
+        )
+
+        parsed_body = json.loads(response['Body'].read())
+
+        received_objects.append(parsed_body)
+
+    assert objects == sorted(received_objects, key=itemgetter('id'))
