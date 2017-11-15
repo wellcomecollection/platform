2416104f163a40c58476fec4bbf2028f80c2cffe
Alex Chan <a.chan@wellcome.ac.uk>
2017-11-15 15:33:44 +0000

    Delete an unnecessary script

--- builds/publish_service.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-set -o nounset
-
-ROOT=$(git rev-parse --show-toplevel)
-
-$ROOT/builds/docker_run.py --aws --dind -- \
-	publish_service_to_aws --project="$PROJECT" --infra-bucket="$INFRA_BUCKET"
