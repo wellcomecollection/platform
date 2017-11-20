4e5736846a95536237497e28dcaf9d61236483cb
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-11-20 13:47:44 +0000

    Tweak the location of Lambda ZIPs in S3

--- monitoring/deployment_tracking/service_deployment_status/main.tf
+++ monitoring/deployment_tracking/service_deployment_status/main.tf
@@ -12,7 +12,7 @@ module "lambda_service_deployment_status" {
   }
 
   alarm_topic_arn = "${var.lambda_error_alarm_arn}"
-  s3_key          = "lambdas/deployment_tracking/service_deployment_status.zip"
+  s3_key          = "lambdas/monitoring/deployment_tracking/service_deployment_status.zip"
 }
 
 module "trigger_service_deployment_status" {
