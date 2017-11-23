61e8e94d9e1d4baaf1ac9cbe789e9112856f76a5
Robert Kenny <kenoir@users.noreply.github.com>
2017-11-23 09:13:14 +0000

    Remove containers published by wellcometrust/dockerfiles (#1189)
    * remove terraform container
    
    * python and json linting wellcome containers
    
    * remove leftover jslint container
    
    * replace image_builder
    
    * remove publish_lambda_zip in favour or wellcome/publish_lambda_zip
    
    * remove fake-sns in favour of wellcome/fake-sns
    
    * remove elasticdump as moved to wellcome/dockerfiles
    
    * some makefile rearranging
    
    * remove reference to jslint_ci
    
    * refer to container image that actually exists
    
    * moves nginx_build_image make function into nginx project

--- docker/jslint_ci/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-set -o errexit
-set -o nounset
-set -o verbose
-
-find /data/ -not -path '*WIP*' -name *.js | xargs jshint
-find /data/ -not -path '*WIP*' -name *.json | xargs jshint
