f51cad414cc6370fa59995a6da0305d4827f8f02
Alex Chan <a.chan@wellcome.ac.uk>
2017-11-27 08:59:32 +0000

    Read from SQS and build a source

--- sierra_adapter/sierra_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_to_dynamo/services/SierraToDynamoWorkerService.scala
+++ sierra_adapter/sierra_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_to_dynamo/services/SierraToDynamoWorkerService.scala
@@ -1,23 +1,34 @@
 package uk.ac.wellcome.platform.sierra_to_dynamo.services
 
+import scala.concurrent.duration._
+import scala.concurrent.Future
+
 import akka.actor.ActorSystem
 import com.google.inject.Inject
-import uk.ac.wellcome.metrics.MetricsSender
+
 import uk.ac.wellcome.models.aws.SQSMessage
+import uk.ac.wellcome.platform.sierra_to_dynamo.models.AdapterRequest
+import uk.ac.wellcome.sierra.{SierraSource, ThrottleRate}
 import uk.ac.wellcome.sqs.{SQSReader, SQSWorker}
 import uk.ac.wellcome.utils.GlobalExecutionContext.context
 
-import scala.concurrent.Future
-
 class SierraToDynamoWorkerService @Inject()(
   reader: SQSReader,
-  system: ActorSystem,
-  metrics: MetricsSender
+  system: ActorSystem
 ) extends SQSWorker(reader, system, metrics) {
 
-  override def processMessage(message: SQSMessage): Future[Unit] = {
-    println(message)
-    Future.successful(())
-  }
+  override def processMessage(message: SQSMessage): Future[Unit] =
+    for {
+      request <- Future.fromTry(JsonUtil.fromJson[AdapterRequest](message.body))
+      source <- SierraSource(
+        apiUrl = "https://sierra.example.com/api/",
+        oauthKey = oauthKey,
+        oauthSecret = oauthSecret,
+        throttleRate = ThrottleRate(4, 1.second)
+      )(
+        resourceType = "items",
+        params = request.params
+      )
+    } yield ()
 
 }
