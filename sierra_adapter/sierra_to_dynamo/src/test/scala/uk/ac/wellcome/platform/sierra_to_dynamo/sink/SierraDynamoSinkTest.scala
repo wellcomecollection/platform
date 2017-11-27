97fe1f36de644030a6d6c82a209f787cd4aace66
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-11-27 13:59:48 +0000

    Finish the sink; add remaining tests

--- sierra_adapter/sierra_to_dynamo/src/test/scala/uk/ac/wellcome/platform/sierra_to_dynamo/sink/SierraDynamoSinkTest.scala
+++ sierra_adapter/sierra_to_dynamo/src/test/scala/uk/ac/wellcome/platform/sierra_to_dynamo/sink/SierraDynamoSinkTest.scala
@@ -2,26 +2,18 @@ package uk.ac.wellcome.platform.sierra_to_dynamo.sink
 
 import java.time.Instant
 
-import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, Graph, SinkShape}
-import akka.stream.scaladsl.{Sink, Source}
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
-import com.gu.scanamo.{DynamoFormat, Scanamo}
-import io.circe.Json
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+import com.gu.scanamo.Scanamo
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{FunSpec, FunSuite, Matchers}
+import org.scalatest.{FunSpec, Matchers}
 import com.gu.scanamo.syntax._
-import io.circe.optics.JsonPath
 import io.circe.parser._
 import uk.ac.wellcome.platform.sierra_to_dynamo.locals.SierraDynamoDBLocal
 import uk.ac.wellcome.platform.sierra_to_dynamo.models.SierraRecord
 import uk.ac.wellcome.platform.sierra_to_dynamo.models.SierraRecord._
 import uk.ac.wellcome.test.utils.ExtendedPatience
-
-import scala.concurrent.Future
-
-
 
 class SierraDynamoSinkTest extends FunSpec with ScalaFutures with SierraDynamoDBLocal with Matchers with ExtendedPatience {
   implicit val system = ActorSystem()
@@ -46,15 +38,64 @@ class SierraDynamoSinkTest extends FunSpec with ScalaFutures with SierraDynamoDB
     }
   }
 
-}
+  it("should not overwrite new data with old data") {
+    val id = "200002"
+    val oldUpdatedDate = "2001-01-01T00:00:01Z"
+    val newUpdatedDate = "2017-12-12T23:59:59Z"
 
-object SierraDynamoSink {
-  def apply(client: AmazonDynamoDB, tableName: String): Sink[Json, Future[Done]] = Sink.foreach(json => {
-    val record = SierraRecord(
-      id = JsonPath.root.id.string.getOption(json).get,
-      data = json.noSpaces,
-      updatedDate = Instant.parse(JsonPath.root.updatedDate.string.getOption(json).get)
+    val newRecord = SierraRecord(
+      id = id,
+      updatedDate = Instant.parse(newUpdatedDate),
+      data = s"""{"id": "$id", "updatedDate": "$newUpdatedDate", "comment": "I am a shiny new record"}"""
     )
-    Scanamo.put(client)(tableName)(record)
-  })
+    Scanamo.put(dynamoDbClient)(tableName)(newRecord)
+
+    val oldJson = parse(
+      s"""
+         |{
+         |  "id": "$id",
+         |  "updatedDate": "$oldUpdatedDate",
+         |  "comment": "I am an old record"
+         |}
+       """.stripMargin).right.get
+
+    val sink = SierraDynamoSink(client = dynamoDbClient, tableName = tableName)
+    val futureUnit = Source.single(oldJson).runWith(sink)
+    whenReady(futureUnit) { _ =>
+      Scanamo.get[SierraRecord](dynamoDbClient)(tableName)('id -> id) shouldBe Some(Right(newRecord))
+    }
+  }
+
+  it("should overwrite old data with new data") {
+    val id = "300003"
+    val oldUpdatedDate = "2001-01-01T01:01:01Z"
+    val newUpdatedDate = "2011-11-11T11:11:11Z"
+
+    val oldRecord = SierraRecord(
+      id = id,
+      updatedDate = Instant.parse(oldUpdatedDate),
+      data = s"""{"id": "$id", "updatedDate": "$oldUpdatedDate", "comment": "Legacy line of lamentable leopards"}"""
+    )
+    Scanamo.put(dynamoDbClient)(tableName)(oldRecord)
+
+    val newJson = parse(
+      s"""
+         |{
+         |  "id": "$id",
+         |  "updatedDate": "$newUpdatedDate",
+         |  "comment": "Nice! New notes about narwhals in November"
+         |}
+       """.stripMargin).right.get
+
+    val sink = SierraDynamoSink(client = dynamoDbClient, tableName = tableName)
+    val futureUnit = Source.single(newJson).runWith(sink)
+    val newRecord = SierraRecord(
+      id = id,
+      updatedDate = Instant.parse(newUpdatedDate),
+      data = s"""{"id":"$id","updatedDate":"$newUpdatedDate","comment":"Nice! New notes about narwhals in November"}"""
+    )
+    whenReady(futureUnit) { _ =>
+      Scanamo.get[SierraRecord](dynamoDbClient)(tableName)('id -> id) shouldBe Some(Right(newRecord))
+    }
+  }
 }
