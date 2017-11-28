e340458f368e621ea7f79b9d7576f7fa561a4379
caylaa <alicefuzier@gmail.com>
2017-11-28 13:08:25 +0000

    SierraToDynamoWorkerService inserts records from sierra into dynamo db!

--- sierra_adapter/sierra_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_to_dynamo/services/SierraToDynamoWorkerService.scala
+++ sierra_adapter/sierra_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_to_dynamo/services/SierraToDynamoWorkerService.scala
@@ -1,34 +1,55 @@
 package uk.ac.wellcome.platform.sierra_to_dynamo.services
 
-import scala.concurrent.duration._
-import scala.concurrent.Future
+import akka.Done
 import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Keep
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.google.inject.Inject
+import io.circe.optics.JsonPath.root
+import io.circe.parser._
 import uk.ac.wellcome.metrics.MetricsSender
 import uk.ac.wellcome.models.aws.SQSMessage
-import uk.ac.wellcome.platform.sierra_to_dynamo.models.AdapterRequest
-import uk.ac.wellcome.sierra.{SierraSource, ThrottleRate}
+import uk.ac.wellcome.platform.sierra_to_dynamo.sink.SierraDynamoSink
+import uk.ac.wellcome.sierra.SierraSource
 import uk.ac.wellcome.sqs.{SQSReader, SQSWorker}
 import uk.ac.wellcome.utils.GlobalExecutionContext.context
+
+import scala.concurrent.Future
+import scala.util.Try
 
 class SierraToDynamoWorkerService @Inject()(
   reader: SQSReader,
   system: ActorSystem,
-  metrics: MetricsSender
+  metrics: MetricsSender,
+  dynamoDbClient: AmazonDynamoDB,
+  apiUrl: String,
+  sierraOauthKey: String,
+  sierraOauthSecret: String,
+  resourceType: String,
+  dynamoTableName: String
 ) extends SQSWorker(reader, system, metrics) {
+  implicit val actorSystem = system
+  implicit val materialiser = ActorMaterializer()
 
-  override def processMessage(message: SQSMessage): Future[Unit] = ???
-//    for {
-//      request <- Future.fromTry(JsonUtil.fromJson[AdapterRequest](message.body))
-//      source <- SierraSource(
-//        apiUrl = "https://sierra.example.com/api/",
-//        oauthKey = oauthKey,
-//        oauthSecret = oauthSecret,
-//        throttleRate = ThrottleRate(4, 1.second)
-//      )(
-//        resourceType = "items",
-//        params = request.params
-//      )
-//    } yield ()
+  override def processMessage(message: SQSMessage): Future[Unit] =
+    for {
+      params <- extractUpdatedDateWindow(message)
+      _ <- runSierraStream(params)
+    } yield ()
 
+  private def runSierraStream(params: Map[String, String]): Future[Done] = {
+    val graph = SierraSource(apiUrl, sierraOauthKey, sierraOauthSecret)(
+      resourceType,
+      params).toMat(SierraDynamoSink(dynamoDbClient, dynamoTableName))(Keep.right)
+    graph.run()
+  }
+
+  private def extractUpdatedDateWindow(message: SQSMessage) =
+    Future.fromTry(Try(parse(message.body).right.get)).map { json =>
+      val start = root.start.string.getOption(json).get
+      val end = root.end.string.getOption(json).get
+
+      Map("updatedDate" -> s"[$start,$end]")
+    }
 }
--- sierra_adapter/sierra_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_to_dynamo/sink/SierraDynamoSink.scala
+++ sierra_adapter/sierra_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_to_dynamo/sink/SierraDynamoSink.scala
@@ -1,34 +1,61 @@
 package uk.ac.wellcome.platform.sierra_to_dynamo.sink
 
+import java.time.format.DateTimeFormatter
+import java.time.{LocalDate, ZoneOffset}
+
 import akka.Done
 import akka.stream.scaladsl.Sink
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
-import com.gu.scanamo.{Scanamo, Table}
 import com.gu.scanamo.syntax._
+import com.gu.scanamo.{Scanamo, Table}
+import com.twitter.inject.Logging
 import io.circe.Json
-import io.circe.optics.JsonPath
+import io.circe.optics.JsonPath.root
 import uk.ac.wellcome.platform.sierra_to_dynamo.models.SierraRecord
 import uk.ac.wellcome.platform.sierra_to_dynamo.models.SierraRecord._
 
 import scala.concurrent.Future
 
-object SierraDynamoSink {
+object SierraDynamoSink extends Logging {
   def apply(client: AmazonDynamoDB,
             tableName: String): Sink[Json, Future[Done]] =
     Sink.foreach(json => {
-      val record = SierraRecord(
-        id = JsonPath.root.id.string.getOption(json).get,
-        data = json.noSpaces,
-        updatedDate = JsonPath.root.updatedDate.string.getOption(json).get
-      )
+      logger.debug(s"Inserting ${json.spaces4} in dynamo Db")
+      val maybeUpdatedDate = root.updatedDate.string.getOption(json)
+      val record = maybeUpdatedDate match {
+        case Some(updatedDate) =>
+          SierraRecord(
+            id = getId(json),
+            data = json.noSpaces,
+            modifiedDate = updatedDate
+          )
+        case None =>
+          SierraRecord(
+            id = getId(json),
+            data = json.noSpaces,
+            modifiedDate = getDeletedDateTimeAtStartOfDay(json)
+          )
+      }
 
       val table = Table[SierraRecord](tableName)
       val ops = table
         .given(
           not(attributeExists('id)) or
-            (attributeExists('id) and 'updatedDate < record.updatedDate.getEpochSecond)
+            (attributeExists('id) and 'updatedDate < record.modifiedDate.getEpochSecond)
         )
         .put(record)
-      Scanamo.exec(client)(ops)
+      Scanamo.exec(client)(ops) match {
+        case Right(_) => logger.info(s"${json.spaces4} saved successfully to dynamo Db")
+        case Left(error) => logger.warn(s"Failed saving ${json.spaces4} into DynamoDB", error)
+      }
     })
+
+  private def getDeletedDateTimeAtStartOfDay(json: Json) = {
+    val formatter = DateTimeFormatter.ISO_DATE
+    LocalDate.parse(root.deletedDate.string.getOption(json).get, formatter).atStartOfDay().toInstant(ZoneOffset.UTC)
+  }
+
+  private def getId(json: Json) = {
+    root.id.string.getOption(json).get
+  }
 }
