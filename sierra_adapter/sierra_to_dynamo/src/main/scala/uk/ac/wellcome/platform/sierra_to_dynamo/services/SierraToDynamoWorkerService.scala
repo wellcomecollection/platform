eb71aaf0a44dd6e8716184380a5900559d978e0e
caylaa <alicefuzier@gmail.com>
2017-11-28 09:57:15 +0000

    FIrst test for the SierraToDynamoWorkerService

--- sierra_adapter/sierra_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_to_dynamo/services/SierraToDynamoWorkerService.scala
+++ sierra_adapter/sierra_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_to_dynamo/services/SierraToDynamoWorkerService.scala
@@ -2,10 +2,9 @@ package uk.ac.wellcome.platform.sierra_to_dynamo.services
 
 import scala.concurrent.duration._
 import scala.concurrent.Future
-
 import akka.actor.ActorSystem
 import com.google.inject.Inject
-
+import uk.ac.wellcome.metrics.MetricsSender
 import uk.ac.wellcome.models.aws.SQSMessage
 import uk.ac.wellcome.platform.sierra_to_dynamo.models.AdapterRequest
 import uk.ac.wellcome.sierra.{SierraSource, ThrottleRate}
@@ -14,21 +13,22 @@ import uk.ac.wellcome.utils.GlobalExecutionContext.context
 
 class SierraToDynamoWorkerService @Inject()(
   reader: SQSReader,
-  system: ActorSystem
+  system: ActorSystem,
+  metrics: MetricsSender
 ) extends SQSWorker(reader, system, metrics) {
 
-  override def processMessage(message: SQSMessage): Future[Unit] =
-    for {
-      request <- Future.fromTry(JsonUtil.fromJson[AdapterRequest](message.body))
-      source <- SierraSource(
-        apiUrl = "https://sierra.example.com/api/",
-        oauthKey = oauthKey,
-        oauthSecret = oauthSecret,
-        throttleRate = ThrottleRate(4, 1.second)
-      )(
-        resourceType = "items",
-        params = request.params
-      )
-    } yield ()
+  override def processMessage(message: SQSMessage): Future[Unit] = ???
+//    for {
+//      request <- Future.fromTry(JsonUtil.fromJson[AdapterRequest](message.body))
+//      source <- SierraSource(
+//        apiUrl = "https://sierra.example.com/api/",
+//        oauthKey = oauthKey,
+//        oauthSecret = oauthSecret,
+//        throttleRate = ThrottleRate(4, 1.second)
+//      )(
+//        resourceType = "items",
+//        params = request.params
+//      )
+//    } yield ()
 
 }
