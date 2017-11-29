218f18747af9b33769bdac45fdd8a8d836a31fdc
Alex Chan <a.chan@wellcome.ac.uk>
2017-11-29 10:39:37 +0000

    Remove the stream ARN from DynamoConfig

--- catalogue_pipeline/reindexer/src/test/scala/uk/ac/wellcome/platform/reindexer/services/ReindexTrackerServiceTest.scala
+++ catalogue_pipeline/reindexer/src/test/scala/uk/ac/wellcome/platform/reindexer/services/ReindexTrackerServiceTest.scala
@@ -16,8 +16,8 @@ class ReindexTrackerServiceTest
     with ExtendedPatience {
 
   val dynamoConfigs = Map(
-    "reindex" -> DynamoConfig("streamArn", reindexTableName),
-    "calm" -> DynamoConfig("streamArn", calmDataTableName))
+    "reindex" -> DynamoConfig(table = reindexTableName),
+    "calm" -> DynamoConfig(table = calmDataTableName))
 
   it(
     "should return the index and shard in need of reindexing"
--- common/src/main/scala/uk/ac/wellcome/finatra/modules/DynamoConfigModule.scala
+++ common/src/main/scala/uk/ac/wellcome/finatra/modules/DynamoConfigModule.scala
@@ -7,30 +7,27 @@ import com.twitter.inject.TwitterModule
 import uk.ac.wellcome.models.aws.DynamoConfig
 
 trait DynamoConfigModule extends TwitterModule {
-  val arnTpl: String = "aws.dynamo.%s.streams.arn"
   val tableTpl: String = "aws.dynamo.%s.tableName"
 
-  def flags(tableName: String) = (
-    flag[String](arnTpl.format(tableName), "", "ARN of the DynamoDB stream"),
+  def flags(tableName: String) =
     flag[String](tableTpl.format(tableName), "", "Name of the DynamoDB table")
-  )
 }
 
 object PlatformDynamoConfigModule extends DynamoConfigModule {
 
-  val (miroArn, miroTable) = flags("miroData")
-  val (identArn, identTable) = flags("identifiers")
-  val (calmArn, calmTable) = flags("calmData")
-  val (reindexArn, reindexTable) = flags("reindexTracker")
+  val miroTable = flags("miroData")
+  val identTable = flags("identifiers")
+  val calmTable = flags("calmData")
+  val reindexTable = flags("reindexTracker")
 
   @Singleton
   @Provides
   def providesDynamoConfig(): Map[String, DynamoConfig] =
     Map(
-      "calm" -> DynamoConfig(calmArn(), calmTable()),
-      "identifiers" -> DynamoConfig(identArn(), identTable()),
-      "miro" -> DynamoConfig(miroArn(), miroTable()),
-      "reindex" -> DynamoConfig(reindexArn(), reindexTable())
+      "calm" -> DynamoConfig(calmTable()),
+      "identifiers" -> DynamoConfig(identTable()),
+      "miro" -> DynamoConfig(miroTable()),
+      "reindex" -> DynamoConfig(reindexTable())
     ).filterNot {
       case (_, v) => v.table.isEmpty
     }
