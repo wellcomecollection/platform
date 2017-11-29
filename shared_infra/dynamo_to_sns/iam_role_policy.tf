3069090981f15f1bc65a95050eb09a826d90a934
Alex Chan <a.chan@wellcome.ac.uk>
2017-11-29 15:29:24 +0000

    Fix the name of a variable

--- shared_infra/dynamo_to_sns/iam_role_policy.tf
+++ shared_infra/dynamo_to_sns/iam_role_policy.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role_policy" "dynamo_to_miro_sns" {
   role   = "${module.lambda_dynamo_to_sns.role_name}"
-  policy = "${data.aws_iam_role_policy.publish_to_topic.json}"
+  policy = "${data.aws_iam_policy_document.publish_to_topic.json}"
 }
 
 data "aws_iam_policy_document" "publish_to_topic" {
