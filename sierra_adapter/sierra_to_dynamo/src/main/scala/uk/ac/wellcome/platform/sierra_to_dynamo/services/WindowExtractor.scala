f6c34e9809d6abed3c4f7ce7464f963aeaa91449
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-11-30 12:36:39 +0000

    Formatting and imports

--- sierra_adapter/sierra_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_to_dynamo/services/WindowExtractor.scala
+++ sierra_adapter/sierra_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_to_dynamo/services/WindowExtractor.scala
@@ -11,22 +11,26 @@ import uk.ac.wellcome.sqs.SQSReaderGracefulException
 
 import scala.util.Try
 
-object WindowExtractor extends Logging{
-  def extractWindow(jsonString: String): Try[String] = Try(parse(jsonString).right.get).map { json =>
+object WindowExtractor extends Logging {
+  def extractWindow(jsonString: String): Try[String] =
+    Try(parse(jsonString).right.get)
+      .map { json =>
+        val start = extractField("start", json)
+        val end = extractField("end", json)
 
-    val start = extractField("start", json)
-    val end = extractField("end", json)
+        val startDateTime = parseStringToDateTime(start)
+        val endDateTime = parseStringToDateTime(end)
+        if (startDateTime.isAfter(endDateTime) || startDateTime.isEqual(
+              endDateTime))
+          throw new Exception(s"$start is after $end")
 
-    val startDateTime = parseStringToDateTime(start)
-    val endDateTime = parseStringToDateTime(end)
-    if (startDateTime.isAfter(endDateTime) || startDateTime.isEqual(endDateTime))
-      throw new Exception(s"$start is after $end")
-
-    s"[$start,$end]"
-  }.recover {
-    case e: Exception => warn(s"Error parsing $jsonString", e)
-      throw SQSReaderGracefulException(e)
-  }
+        s"[$start,$end]"
+      }
+      .recover {
+        case e: Exception =>
+          warn(s"Error parsing $jsonString", e)
+          throw SQSReaderGracefulException(e)
+      }
 
   private def extractField(field: String, json: Json) = {
     root.selectDynamic(field).string.getOption(json).get
