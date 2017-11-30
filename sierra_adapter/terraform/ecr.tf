e45b9852a1f35c2313923a64cf1af08cbe0eaa40
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-11-30 14:39:30 +0000

    add sierra_bib_merger ecr repo

--- sierra_adapter/terraform/ecr.tf
+++ sierra_adapter/terraform/ecr.tf
@@ -2,3 +2,8 @@ module "ecr_repository_sierra_to_dynamo" {
   source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
   name   = "sierra_to_dynamo"
 }
+
+module "ecr_repository_sierra_bib_merger" {
+  source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
+  name   = "sierra_bib_merger"
+}
