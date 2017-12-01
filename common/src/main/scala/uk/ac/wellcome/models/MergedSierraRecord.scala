7f626f9d10521b3c2776143c36410167e4b0c8f7
Alex Chan <a.chan@wellcome.ac.uk>
2017-12-01 16:11:31 +0000

    Add some brief descriptions
    [skip ci]

--- common/src/main/scala/uk/ac/wellcome/models/MergedSierraRecord.scala
+++ common/src/main/scala/uk/ac/wellcome/models/MergedSierraRecord.scala
@@ -2,11 +2,21 @@ package uk.ac.wellcome.models
 
 import uk.ac.wellcome.utils.JsonUtil
 
+/** Represents a row in the DynamoDB database of "merged" Sierra records;
+  * that is, records that contain data for both bibs and
+  * their associated items.
+  */
 case class MergedSierraRecord(
   id: String,
   bibData: Option[SierraBibRecord] = None,
   version: Int = 1
 ) {
+
+  /** Given a new bib record, construct the new merged row that we should
+    * insert into the merged database.
+    *
+    * Returns None if there's nothing to do.
+    */
   def mergeBibRecord(record: SierraBibRecord): Option[MergedSierraRecord] = {
     if (record.id != this.id) {
       throw new RuntimeException(
