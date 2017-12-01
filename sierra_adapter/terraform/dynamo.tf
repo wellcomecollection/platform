8781eb3287ffeb03504f5635af7c3060925b2a4b
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-12-01 09:22:39 +0000

    This doesn't need to be a range key

--- sierra_adapter/terraform/dynamo.tf
+++ sierra_adapter/terraform/dynamo.tf
@@ -3,18 +3,12 @@ resource "aws_dynamodb_table" "sierradata_table" {
   read_capacity    = 1
   write_capacity   = 1
   hash_key         = "id"
-  range_key        = "version"
   stream_enabled   = true
   stream_view_type = "NEW_AND_OLD_IMAGES"
 
   attribute {
     name = "id"
     type = "S"
-  }
-
-  attribute {
-    name = "version"
-    type = "N"
   }
 
   lifecycle {
