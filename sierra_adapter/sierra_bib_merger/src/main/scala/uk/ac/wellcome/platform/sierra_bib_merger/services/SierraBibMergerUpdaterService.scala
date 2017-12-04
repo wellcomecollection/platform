515e915bfd2ba243595f3fb36602543c56826d50
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-12-04 12:27:35 +0000

    local test

--- sierra_adapter/sierra_bib_merger/src/main/scala/uk/ac/wellcome/platform/sierra_bib_merger/services/SierraBibMergerUpdaterService.scala
+++ sierra_adapter/sierra_bib_merger/src/main/scala/uk/ac/wellcome/platform/sierra_bib_merger/services/SierraBibMergerUpdaterService.scala
@@ -1,20 +1,18 @@
 package uk.ac.wellcome.platform.sierra_bib_merger.services
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
-import com.amazonaws.services.dynamodbv2.model.PutItemResult
 import com.google.inject.Inject
-import com.gu.scanamo.{Scanamo, Table}
 import com.gu.scanamo.syntax._
-import com.twitter.inject.annotations.Flag
+import com.gu.scanamo.{Scanamo, Table}
 import com.twitter.inject.Logging
 import uk.ac.wellcome.metrics.MetricsSender
+import uk.ac.wellcome.models.SierraBibRecord._
 import uk.ac.wellcome.models.aws.DynamoConfig
 import uk.ac.wellcome.models.{MergedSierraRecord, SierraBibRecord}
-import uk.ac.wellcome.models.SierraBibRecord._
-
-import scala.concurrent.ExecutionContext.Implicits.global
 
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 
 class SierraBibMergerUpdaterService @Inject()(
   dynamoDBClient: AmazonDynamoDB,
@@ -22,41 +20,43 @@ class SierraBibMergerUpdaterService @Inject()(
   dynamoConfig: DynamoConfig
 ) extends Logging {
 
-  def update(bibRecord: SierraBibRecord): Unit = {
+  private val table = Table[MergedSierraRecord](dynamoConfig.table)
 
-    // First we read the existing record from the table
-    val existingRecord: MergedSierraRecord = Scanamo.get[MergedSierraRecord](
-      dynamoDBClient)(dynamoConfig.table)('id -> bibRecord.id) match {
+  private def putRecord(record: MergedSierraRecord) = table
+    .given(
+      not(attributeExists('id)) or
+        (attributeExists('id) and 'version < record.version)
+    )
+    .put(record)
 
-      // TODO: Handle this properly!
-      case Some(record) => record.right.get
-      case None => MergedSierraRecord(id = bibRecord.id)
+  def update(bibRecord: SierraBibRecord): Future[Unit] = Future {
+    val existingRecord = Scanamo.exec(dynamoDBClient)(
+      table.get('id -> bibRecord.id)
+    )
+
+    val newRecord = existingRecord.map {
+      case Left(error) => Left(
+        new RuntimeException(error.toString)
+      )
+      case Right(record) => record.mergeBibRecord(bibRecord).toRight(
+        new RuntimeException("Unable to merge record!")
+      )
+    }.getOrElse(
+      Right(MergedSierraRecord(id = bibRecord.id))
+    )
+
+    val putOperation = newRecord match {
+      case Right(record) => Scanamo.exec(dynamoDBClient)(
+        putRecord(record)
+      ).left.map(e => new RuntimeException(e.toString))
+      case Left(e) => Left(e)
     }
 
-    // Then we add the record we've received from the Sierra API, and
-    // optionally send that to DynamoDB.
-    val newRecord: Option[MergedSierraRecord] =
-      existingRecord.mergeBibRecord(bibRecord)
-
-    logger.info(s"Saving $newRecord to DynamoDB")
-
-    newRecord match {
-      case Some(record) => {
-        val table = Table[MergedSierraRecord](dynamoConfig.table)
-        val ops = table
-          .given(
-            not(attributeExists('id)) or
-              (attributeExists('id) and 'version < record.version)
-          )
-          .put(record)
-        val x = Scanamo.exec(dynamoDBClient)(ops) match {
-          case Right(_) =>
-            logger.info(s"$record saved successfully to DynamoDB")
-          case Left(error) =>
-            logger.warn(s"Failed saving $record to DynamoDB", error)
-        }
-      }
-      case None => ()
+    putOperation match {
+      case Right(_) =>
+        logger.info(s"${bibRecord.id} saved successfully to DynamoDB")
+      case Left(error) =>
+        logger.warn(s"Failed processing ${bibRecord.id}", error)
     }
   }
 }
--- sierra_adapter/sierra_bib_merger/src/main/scala/uk/ac/wellcome/platform/sierra_bib_merger/services/SierraBibMergerWorkerService.scala
+++ sierra_adapter/sierra_bib_merger/src/main/scala/uk/ac/wellcome/platform/sierra_bib_merger/services/SierraBibMergerWorkerService.scala
@@ -2,14 +2,15 @@ package uk.ac.wellcome.platform.sierra_bib_merger.services
 
 import akka.actor.ActorSystem
 import com.google.inject.Inject
+import grizzled.slf4j.Logging
 import uk.ac.wellcome.metrics.MetricsSender
-import uk.ac.wellcome.models.aws.SQSMessage
 import uk.ac.wellcome.models.SierraBibRecord
+import uk.ac.wellcome.models.aws.SQSMessage
 import uk.ac.wellcome.sqs.{SQSReader, SQSWorker}
 import uk.ac.wellcome.utils.JsonUtil
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
 
 class SierraBibMergerWorkerService @Inject()(
@@ -17,18 +18,11 @@ class SierraBibMergerWorkerService @Inject()(
   system: ActorSystem,
   metrics: MetricsSender,
   sierraBibMergerUpdaterService: SierraBibMergerUpdaterService
-) extends SQSWorker(reader, system, metrics) {
+) extends SQSWorker(reader, system, metrics) with Logging {
 
   override def processMessage(message: SQSMessage): Future[Unit] =
     JsonUtil.fromJson[SierraBibRecord](message.body) match {
-      case Success(record) => {
-        info(s"Parsed message as bib record $record")
-        sierraBibMergerUpdaterService.update(record)
-        Future.successful(Unit)
-      }
-      case Failure(e) => {
-        warn(s"Failed parsing message as bib record: $e")
-        Future.successful(e)
-      }
+      case Success(record) => sierraBibMergerUpdaterService.update(record)
+      case Failure(e) => Future(logger.warn(s"Failed processing $message", e))
     }
 }
