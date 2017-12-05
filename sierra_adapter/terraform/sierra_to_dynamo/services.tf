166e3046d3aec9d3ffc0d5cf188c63f322da775e
Alex Chan <a.chan@wellcome.ac.uk>
2017-12-05 09:26:24 +0000

    Use default service healthcheck paths

--- sierra_adapter/terraform/sierra_to_dynamo/services.tf
+++ sierra_adapter/terraform/sierra_to_dynamo/services.tf
@@ -17,7 +17,7 @@ module "sqs_autoscaling_alarms" {
 }
 
 module "sierra_to_dynamo_service" {
-  source             = "git::https://github.com/wellcometrust/terraform.git//services?ref=v1.3.0"
+  source             = "git::https://github.com/wellcometrust/terraform.git//services?ref=v1.3.1"
   name               = "sierra_to_dynamo_${var.resource_type}"
   cluster_id         = "${var.cluster_id}"
   task_role_arn      = "${module.ecs_sierra_to_dynamo_iam.task_role_arn}"
@@ -27,7 +27,6 @@ module "sierra_to_dynamo_service" {
   listener_http_arn  = "${var.alb_listener_http_arn}"
   path_pattern       = "/sierra_to_dynamo_${var.resource_type}/*"
   alb_priority       = "${var.alb_priority}"
-  healthcheck_path   = "/sierra_to_dynamo_${var.resource_type}/management/healthcheck"
   infra_bucket       = "${var.infra_bucket}"
   https_domain       = "services.wellcomecollection.ac.uk"
 
