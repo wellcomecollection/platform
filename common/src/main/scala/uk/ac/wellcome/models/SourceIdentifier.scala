6171fc8d6384c20815982a13724d0ad08f50d9c9
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-12-06 15:21:14 +0000

    Removing useless import

--- common/src/main/scala/uk/ac/wellcome/models/SourceIdentifier.scala
+++ common/src/main/scala/uk/ac/wellcome/models/SourceIdentifier.scala
@@ -1,7 +1,5 @@
 package uk.ac.wellcome.models
 
-import com.fasterxml.jackson.module.scala.JsonScalaEnumeration
-
 /** An identifier received from one of the original sources */
 case class SourceIdentifier(
   identifierScheme: IdentifierSchemes.IdentifierScheme,
