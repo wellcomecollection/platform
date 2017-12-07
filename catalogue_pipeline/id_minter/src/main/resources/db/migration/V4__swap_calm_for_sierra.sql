9d4d0724f11f8da61b966dfba51b913a35964c37
Alex Chan <a.chan@wellcome.ac.uk>
2017-12-07 15:21:58 +0000

    Actually we do want the CalmAltRefNo column

--- catalogue_pipeline/id_minter/src/main/resources/db/migration/V4__swap_calm_for_sierra.sql
+++ catalogue_pipeline/id_minter/src/main/resources/db/migration/V4__swap_calm_for_sierra.sql
@@ -1,14 +1,10 @@
 -- Now we're ingesting Sierra records, we're actually using Sierra IDs in
--- the ID minter.  Since we're not doing Calm yet, we'll drop this column
--- for now.
+-- the ID minter.
 
 USE ${database};
 
 ALTER TABLE ${tableName}
-DROP COLUMN CalmAltRefNo;
-
-ALTER TABLE ${tableName}
-ADD COLUMN SierraSystemNumber varchar(255) AFTER MiroID;
+ADD COLUMN SierraSystemNumber varchar(255) AFTER CalmAltRefNo;
 
 ALTER TABLE ${tableName}
 ADD UNIQUE (ontologyType, SierraSystemNumber);
