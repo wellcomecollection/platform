021268fb9b61a473feca241f9b3408e18ed876cf
Alex Chan <alex@alexwlchan.net>
2017-12-07 09:57:14 +0000

    Add a script for Travis to autoformat in CI

---
+++ .travis/run_autoformat.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8
+"""
+Wrapper script for doing auto-formatting in Travis.
+
+In particular, this script will autoformat Terraform and Scala, then commit
+and push the results.  It also lints Python and JSON, but those aren't
+auto-formatted (yet).
+"""
+
+from tooling import changed_files, git, make
+
+
+if __name__ == '__main__':
+    make('format')
+
+    if changed_files():
+        print('*** There were changes from formatting, creating a commit')
+
+        git('config', 'user.name', 'Travis CI on behalf of Wellcome')
+        git('config', 'user.email', 'wellcomedigitalplatform@wellcome.ac.uk')
+        git('config', 'core.sshCommand', 'ssh -i deploy_key')
+        git(
+            'remote', 'add', 'ssh-origin',
+            'git@github.com:wellcometrust/platform.git'
+        )
+
+        git('add', '--verbose', '--all')
+        git('commit', '-m', 'Apply auto-formatting rules')
+        git('push', 'ssh-origin', 'autoformat-in-ci')
+    else:
+        print('*** There were no changes from auto-formatting')
+
+    make('check-format')
