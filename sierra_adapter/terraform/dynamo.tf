6886062ba5df1a58b42923ef364f63da538b8bf3
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-12-07 09:57:58 +0000

    Add some format errors for Travis to whinge about

--- sierra_adapter/terraform/dynamo.tf
+++ sierra_adapter/terraform/dynamo.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "sierradata_table" {
   name             = "SierraData_Merged"
   read_capacity    = 1
-  write_capacity   = 1
-  hash_key         = "id"
+  write_capacity = 1
+  hash_key   = "id"
   stream_enabled   = true
   stream_view_type = "NEW_AND_OLD_IMAGES"
 
