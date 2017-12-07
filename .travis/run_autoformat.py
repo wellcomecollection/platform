7f6cc8cece58027cf8cd8decb102e1a175fd3660
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-12-07 13:10:22 +0000

    More flush=True

--- .travis/run_autoformat.py
+++ .travis/run_autoformat.py
@@ -15,7 +15,9 @@ if __name__ == '__main__':
     make('format')
 
     if changed_files():
-        print('*** There were changes from formatting, creating a commit')
+        print(
+            '*** There were changes from formatting, creating a commit',
+            flush=True)
 
         git('config', 'user.name', 'Travis CI on behalf of Wellcome')
         git('config', 'user.email', 'wellcomedigitalplatform@wellcome.ac.uk')
@@ -29,6 +31,6 @@ if __name__ == '__main__':
         git('commit', '-m', 'Apply auto-formatting rules')
         git('push', 'ssh-origin', 'autoformat-in-ci')
     else:
-        print('*** There were no changes from auto-formatting')
+        print('*** There were no changes from auto-formatting', flush=True)
 
     make('check-format')
