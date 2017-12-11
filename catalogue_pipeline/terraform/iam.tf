eaf8d6aae5268069771573c080ae7538aec15d47
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-12-11 10:24:06 +0000

    remove unused ecs_miro_reindexer_iam

--- catalogue_pipeline/terraform/iam.tf
+++ catalogue_pipeline/terraform/iam.tf
@@ -1,8 +1,3 @@
-module "ecs_miro_reindexer_iam" {
-  source = "git::https://github.com/wellcometrust/terraform.git//ecs_iam?ref=v1.0.0"
-  name   = "miro_reindexer"
-}
-
 module "ecs_services_iam" {
   source = "git::https://github.com/wellcometrust/terraform.git//ecs_iam?ref=v1.0.0"
   name   = "services"
