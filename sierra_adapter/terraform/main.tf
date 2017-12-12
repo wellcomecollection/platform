e332cf49f5f373f6ce756520603ff3fd7f7fda42
Alex Chan <a.chan@wellcome.ac.uk>
2017-12-12 17:35:11 +0000

    Add Terraform for the SierraItemsToDynamo app

--- sierra_adapter/terraform/main.tf
+++ sierra_adapter/terraform/main.tf
@@ -6,6 +6,14 @@ module "sierra_window_generator_bibs" {
   resource_type          = "bibs"
 }
 
+module "sierra_window_generator_items" {
+  source                 = "sierra_window_generator"
+  window_length_minutes  = "30"
+  lambda_trigger_minutes = "15"
+  lambda_error_alarm_arn = "${local.lambda_error_alarm_arn}"
+  resource_type          = "items"
+}
+
 module "sierra_to_dynamo_bibs" {
   source             = "sierra_to_dynamo"
   resource_type      = "bibs"
@@ -27,6 +35,35 @@ module "sierra_to_dynamo_bibs" {
   sierra_oauth_key    = "${var.sierra_oauth_key}"
   sierra_oauth_secret = "${var.sierra_oauth_secret}"
   sierra_fields       = "${var.sierra_bibs_fields}"
+
+  vpc_id = "${local.services_vpc_id}"
+
+  account_id = "${data.aws_caller_identity.current.account_id}"
+
+  lambda_error_alarm_arn = "${local.lambda_error_alarm_arn}"
+}
+
+module "sierra_to_dynamo_items" {
+  source             = "sierra_to_dynamo"
+  resource_type      = "items"
+  windows_topic_name = "${module.sierra_window_generator_items.topic_name}"
+
+  dlq_alarm_arn = "${data.terraform_remote_state.shared_infra.dlq_alarm_arn}"
+  cluster_name  = "${data.terraform_remote_state.catalogue_pipeline.ecs_services_cluster_name}"
+
+  alb_priority               = 109
+  alb_server_error_alarm_arn = "${local.services_alb_server_error_alarm_arn}"
+  alb_client_error_alarm_arn = "${local.services_alb_client_error_alarm_arn}"
+  alb_cloudwatch_id          = "${local.services_alb_cloudwatch_id}"
+  alb_listener_http_arn      = "${local.services_alb_listener_http_arn}"
+  alb_listener_https_arn     = "${local.services_alb_listener_https_arn}"
+
+  release_id = "${var.release_ids["sierra_items_to_dynamo"]}"
+
+  sierra_api_url      = "${var.sierra_api_url}"
+  sierra_oauth_key    = "${var.sierra_oauth_key}"
+  sierra_oauth_secret = "${var.sierra_oauth_secret}"
+  sierra_fields       = "${var.sierra_items_fields}"
 
   vpc_id = "${local.services_vpc_id}"
 
