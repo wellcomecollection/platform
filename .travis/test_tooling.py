ca64d335756287dceb4104e80cf97344766fce8c
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-12-13 10:37:47 +0000

    Add that common lib to the Travis build matrix

--- .travis/test_tooling.py
+++ .travis/test_tooling.py
@@ -6,8 +6,16 @@ import tooling
 
 
 @pytest.mark.parametrize('path, task, expected_result', [
-    ('/sierra_adapter/terraform/main.tf', 'sierra_adapter-test', False),
-    ('/sierra_adapter/terraform/main.tf', 'loris-test', False),
+    ('sierra_adapter/terraform/main.tf', 'sierra_adapter-test', False),
+    ('sierra_adapter/terraform/main.tf', 'loris-test', False),
+
+    ('sierra_adapter/Makefile', 'sierra_adapter-test', True),
+    ('sierra_adapter/Makefile', 'loris-test', False),
+    ('sierra_adapter/Makefile', 'unknown-task', True),
+
+    ('sierra_adapter/common/main.scala', 'sierra_adapter-test', True),
+    ('sierra_adapter/common/main.scala', 'loris-test', False),
+    ('sierra_adapter/common/main.scala', 'unknown-task', True),
 ])
 def test_affects_test(path, task, expected_result):
     assert tooling.affects_tests(path=path, task=task) == expected_result
