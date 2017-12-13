8db867c2c38217f0213f951adf83c27dfb108252
caylaa <alicefuzier@gmail.com>
2017-12-13 15:19:46 +0000

    Extracted the logic for interacting with DynamoDb to the MergedSierraRecordDao

---
+++ sierra_adapter/sierra_item_merger/src/main/scala/uk/ac/wellcome/platform/sierra_item_merger/dynamo/MergedSierraRecordDao.scala
@@ -0,0 +1,44 @@
+package uk.ac.wellcome.platform.sierra_item_merger.dynamo
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.gu.scanamo.{Scanamo, Table}
+import com.gu.scanamo.ops.ScanamoOps
+import com.gu.scanamo.syntax._
+import com.twitter.inject.Logging
+import uk.ac.wellcome.models.MergedSierraRecord
+import uk.ac.wellcome.dynamo._
+import uk.ac.wellcome.models.aws.DynamoConfig
+
+import scala.concurrent.Future
+
+import uk.ac.wellcome.utils.GlobalExecutionContext.context
+
+class MergedSierraRecordDao(dynamoDbClient: AmazonDynamoDB, dynamoConfig: DynamoConfig) extends Logging {
+  private val table = Table[MergedSierraRecord](dynamoConfig.table)
+
+  private def scanamoExec[T](op: ScanamoOps[T]) =
+    Scanamo.exec(dynamoDbClient)(op)
+
+  private def putRecord(record: MergedSierraRecord) =
+    table
+      .given(
+        not(attributeExists('id)) or
+          (attributeExists('id) and 'version < record.version)
+      )
+      .put(record)
+
+  def updateRecord(record: MergedSierraRecord): Future[Unit] = Future {
+    scanamoExec(putRecord(record))
+  }
+
+  def getRecord(id: String): Future[Option[MergedSierraRecord]] = Future {
+    scanamoExec(table.get('id -> id)) match {
+      case Some(Right(record)) => Some(record)
+      case Some(Left(scanamoError)) =>
+        val exception = new RuntimeException(scanamoError.toString)
+        error(s"An error occurred while retrieving $id from DynamoDB", exception)
+        throw exception
+      case None => None
+    }
+  }
+}
---
+++ sierra_adapter/sierra_item_merger/src/test/scala/uk/ac/wellcome/platform/sierra_item_merger/dynamo/MergedSierraRecordDaoTest.scala
@@ -0,0 +1,110 @@
+package uk.ac.wellcome.platform.sierra_item_merger.dynamo
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.model.{GetItemRequest, PutItemRequest}
+import com.gu.scanamo.ops.ScanamoOps
+import com.gu.scanamo.{Scanamo, ScanamoAsync, Table}
+import com.gu.scanamo.syntax._
+import com.twitter.inject.Logging
+import org.mockito.Matchers.any
+import org.mockito.Mockito.when
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.models.MergedSierraRecord
+import uk.ac.wellcome.models.aws.DynamoConfig
+import uk.ac.wellcome.platform.sierra_item_merger.locals.DynamoDBLocal
+import uk.ac.wellcome.dynamo._
+import uk.ac.wellcome.test.utils.ExtendedPatience
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class MergedSierraRecordDaoTest
+  extends FunSpec
+    with Matchers
+    with DynamoDBLocal
+    with ScalaFutures
+    with MockitoSugar
+    with ExtendedPatience {
+
+  val mergedSierraRecordDao = new MergedSierraRecordDao(dynamoDbClient, DynamoConfig(tableName))
+
+  describe("get a merged sierra record") {
+    it("should return a future of merged sierra record if it exists in  DynamoDB") {
+      val mergedSierraRecord = MergedSierraRecord(id = "b1111", maybeBibData = None, itemData = Map(), version = 1)
+
+      Scanamo.put(dynamoDbClient)(tableName)(mergedSierraRecord)
+
+      whenReady(mergedSierraRecordDao.getRecord(mergedSierraRecord.id)) {record =>
+        record shouldBe Some(mergedSierraRecord)
+      }
+    }
+
+    it("should return a future of None if the record does not exist in DynamoDB") {
+      val mergedSierraRecord = MergedSierraRecord(id = "b1111", maybeBibData = None, itemData = Map(), version = 1)
+
+      Scanamo.put(dynamoDbClient)(tableName)(mergedSierraRecord)
+
+      whenReady(mergedSierraRecordDao.getRecord("b88888")) {record =>
+        record shouldBe None
+      }
+    }
+
+    it("returns a failed future with the underlying exception if the DynamoDB read fails") {
+      val dynamoDbClient = mock[AmazonDynamoDB]
+      val expectedException = new RuntimeException("AAAAAARGH!")
+      when(dynamoDbClient.getItem(any[GetItemRequest]))
+        .thenThrow(expectedException)
+
+      val mergedSierraRecordDaoMockedDynamoClient = new MergedSierraRecordDao(dynamoDbClient, DynamoConfig(tableName))
+
+      val futureUnit = mergedSierraRecordDaoMockedDynamoClient.getRecord("b88888")
+
+      whenReady(futureUnit.failed) { ex =>
+        ex shouldBe expectedException
+      }
+    }
+  }
+
+  describe("update a merged sierra record") {
+    it("should insert a new merged sierra record if it doesn't already exist in DynamoDB") {
+      val id = "b1111"
+      val mergedSierraRecord = MergedSierraRecord(id = id, maybeBibData = None, itemData = Map(), version = 1)
+
+      whenReady(mergedSierraRecordDao.updateRecord(mergedSierraRecord)) {_ =>
+        Scanamo.get[MergedSierraRecord](dynamoDbClient)(tableName)('id -> id).get shouldBe Right(
+          mergedSierraRecord
+        )
+      }
+    }
+
+    it("should update a merged sierra record if it already exists and has a higher version") {
+      val id = "b1111"
+      val mergedSierraRecord = MergedSierraRecord(id = id, maybeBibData = None, itemData = Map(), version = 1)
+      val newerMergedSierraRecord = MergedSierraRecord(id = id, maybeBibData = None, itemData = Map(), version = 2)
+
+      Scanamo.put(dynamoDbClient)(tableName)(mergedSierraRecord)
+
+      whenReady(mergedSierraRecordDao.updateRecord(newerMergedSierraRecord)) {_ =>
+        Scanamo.get[MergedSierraRecord](dynamoDbClient)(tableName)('id -> id).get shouldBe Right(
+          newerMergedSierraRecord
+        )
+      }
+    }
+
+    it("should not update a merged sierra record if it already exists and has a lower version") {
+      val id = "b1111"
+      val mergedSierraRecord = MergedSierraRecord(id = id, maybeBibData = None, itemData = Map(), version = 1)
+      val newerMergedSierraRecord = MergedSierraRecord(id = id, maybeBibData = None, itemData = Map(), version = 2)
+
+      Scanamo.put(dynamoDbClient)(tableName)(newerMergedSierraRecord)
+
+      whenReady(mergedSierraRecordDao.updateRecord(mergedSierraRecord)) {_ =>
+        Scanamo.get[MergedSierraRecord](dynamoDbClient)(tableName)('id -> id).get shouldBe Right(
+          newerMergedSierraRecord
+        )
+      }
+    }
+  }
+}
