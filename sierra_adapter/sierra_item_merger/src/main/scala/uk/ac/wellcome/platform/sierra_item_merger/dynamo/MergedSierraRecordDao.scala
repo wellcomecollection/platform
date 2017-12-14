f1f0b903f84eb92326a63ed9804d41820d4d6234
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2017-12-14 10:19:10 +0000

    Apply auto-formatting rules

--- sierra_adapter/sierra_item_merger/src/main/scala/uk/ac/wellcome/platform/sierra_item_merger/dynamo/MergedSierraRecordDao.scala
+++ sierra_adapter/sierra_item_merger/src/main/scala/uk/ac/wellcome/platform/sierra_item_merger/dynamo/MergedSierraRecordDao.scala
@@ -14,7 +14,7 @@ import scala.concurrent.Future
 import uk.ac.wellcome.utils.GlobalExecutionContext.context
 
 class MergedSierraRecordDao @Inject()(dynamoDbClient: AmazonDynamoDB,
-                            dynamoConfig: DynamoConfig)
+                                      dynamoConfig: DynamoConfig)
     extends Logging {
   private val table = Table[MergedSierraRecord](dynamoConfig.table)
 
