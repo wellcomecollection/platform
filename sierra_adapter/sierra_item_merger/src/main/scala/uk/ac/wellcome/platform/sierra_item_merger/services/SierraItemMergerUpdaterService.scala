4d055547d09eb2036f75908d5619255f3d168b47
Robert Kenny <kenoir@users.noreply.github.com>
2017-12-18 17:03:58 +0000

    Adds unlinking to SierraItemMergerUpdater (#1319)
    * Adds unlinking to SierraItemMergerUpdater
    
    * Apply auto-formatting rules

--- sierra_adapter/sierra_item_merger/src/main/scala/uk/ac/wellcome/platform/sierra_item_merger/services/SierraItemMergerUpdaterService.scala
+++ sierra_adapter/sierra_item_merger/src/main/scala/uk/ac/wellcome/platform/sierra_item_merger/services/SierraItemMergerUpdaterService.scala
@@ -5,6 +5,7 @@ import com.twitter.inject.Logging
 import uk.ac.wellcome.metrics.MetricsSender
 import uk.ac.wellcome.models.{MergedSierraRecord, SierraItemRecord}
 import uk.ac.wellcome.platform.sierra_adapter.dynamo.MergedSierraRecordDao
+import uk.ac.wellcome.sqs.SQSReaderGracefulException
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -16,7 +17,7 @@ class SierraItemMergerUpdaterService @Inject()(
 
   def update(itemRecord: SierraItemRecord): Future[Unit] = {
 
-    val updateFutures = itemRecord.bibIds.map { bibId =>
+    val mergeUpdateFutures = itemRecord.bibIds.map { bibId =>
       mergedSierraRecordDao
         .getRecord(bibId)
         .flatMap {
@@ -32,7 +33,27 @@ class SierraItemMergerUpdaterService @Inject()(
         }
     }
 
-    Future.sequence(updateFutures).map(_ => ())
+    val unlinkUpdateFutures = itemRecord.unlinkedBibIds.map { unlinkedBibId =>
+      mergedSierraRecordDao
+        .getRecord(unlinkedBibId)
+        .flatMap {
+          case Some(record) =>
+            val mergedRecord = record.unlinkItemRecord(itemRecord)
+            if (mergedRecord != record)
+              mergedSierraRecordDao.updateRecord(mergedRecord)
+            else Future.successful(())
+          // In the case we cannot find the bib record
+          // assume we're too early and put the message back
+          case None =>
+            Future.failed(
+              SQSReaderGracefulException(
+                new RuntimeException(
+                  "Missing Bib record to unlink: $unlinkedBibId.")
+              ))
+        }
+    }
+
+    Future.sequence(mergeUpdateFutures ++ unlinkUpdateFutures).map(_ => ())
   }
 
 }
