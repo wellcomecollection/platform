ab1a7ab2d9d7a8e77b48cdd3be4265c06ca65922
Alex Chan <a.chan@wellcome.ac.uk>
2017-12-18 16:11:44 +0000

    Remove a couple of unused locals

--- sierra_adapter/terraform/locals.tf
+++ sierra_adapter/terraform/locals.tf
@@ -1,8 +1,4 @@
 locals {
-  alb_cloudwatch_id      = "${module.sierra_adapter_cluster.alb_cloudwatch_id}"
-  alb_listener_http_arn  = "${module.sierra_adapter_cluster.alb_listener_http_arn}"
-  alb_listener_https_arn = "${module.sierra_adapter_cluster.alb_listener_https_arn}"
-
   alb_server_error_alarm_arn = "${data.terraform_remote_state.shared_infra.alb_server_error_alarm_arn}"
   alb_client_error_alarm_arn = "${data.terraform_remote_state.shared_infra.alb_client_error_alarm_arn}"
   lambda_error_alarm_arn     = "${data.terraform_remote_state.shared_infra.lambda_error_alarm_arn}"
