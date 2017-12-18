c9db63e20b57e5a034445417959278fd101dec19
caylaa <alicefuzier@gmail.com>
2017-12-18 11:06:57 +0000

    Swapping jackson for cice in the bib merger as well as Jackson sends null when the json contains the value "null"

---
+++ sierra_adapter/common/src/main/scala/uk/ac/wellcome/sierra_adapter/circe/package.scala
@@ -0,0 +1,21 @@
+package uk.ac.wellcome.sierra_adapter
+
+import java.time.Instant
+
+import io.circe.{Decoder, HCursor}
+import cats.syntax.either._
+import io.circe.generic.extras.Configuration
+
+package object circe {
+  implicit val decodeInstant: Decoder[Instant] = new Decoder[Instant] {
+    final def apply(c: HCursor): Decoder.Result[Instant] =
+      for {
+        epochSeconds <- c.as[Long]
+      } yield {
+        Instant.ofEpochSecond(epochSeconds)
+      }
+  }
+
+  implicit val customConfig: Configuration =
+    Configuration.default.withDefaults.withDiscriminator("type")
+}
