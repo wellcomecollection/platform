903b71f2a13c42e086ef7f3c55140b7d5007beab
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-12-18 11:28:05 +0000

    Apply auto-formatting rules

--- sierra_adapter/sierra_bib_merger/src/test/scala/uk/ac/wellcome/platform/sierra_bib_merger/services/SierraBibMergerWorkerServiceTest.scala
+++ sierra_adapter/sierra_bib_merger/src/test/scala/uk/ac/wellcome/platform/sierra_bib_merger/services/SierraBibMergerWorkerServiceTest.scala
@@ -10,19 +10,35 @@ import uk.ac.wellcome.platform.sierra_adapter.dynamo.MergedSierraRecordDao
 import uk.ac.wellcome.sqs.{SQSReader, SQSReaderGracefulException}
 import uk.ac.wellcome.test.utils.ExtendedPatience
 
-class SierraBibMergerWorkerServiceTest extends FunSpec with MockitoSugar with ScalaFutures with Matchers with ExtendedPatience{
+class SierraBibMergerWorkerServiceTest
+    extends FunSpec
+    with MockitoSugar
+    with ScalaFutures
+    with Matchers
+    with ExtendedPatience {
 
-  it("should not throw a NullPointerException when receiving a message with null body") {
+  it(
+    "should not throw a NullPointerException when receiving a message with null body") {
 
     val sqsReader = mock[SQSReader]
     val metricsSender = mock[MetricsSender]
-    val mergerUpdaterService = new SierraBibMergerUpdaterService(mock[MergedSierraRecordDao], metricsSender)
-    val worker = new SierraBibMergerWorkerService(sqsReader, ActorSystem(), metricsSender, mergerUpdaterService)
+    val mergerUpdaterService =
+      new SierraBibMergerUpdaterService(mock[MergedSierraRecordDao],
+                                        metricsSender)
+    val worker = new SierraBibMergerWorkerService(sqsReader,
+                                                  ActorSystem(),
+                                                  metricsSender,
+                                                  mergerUpdaterService)
 
-    val future = worker.processMessage(SQSMessage(subject = Some("default-subject"), body = "null", topic = "", messageType = "", timestamp = ""))
+    val future = worker.processMessage(
+      SQSMessage(subject = Some("default-subject"),
+                 body = "null",
+                 topic = "",
+                 messageType = "",
+                 timestamp = ""))
 
-    whenReady(future.failed) {ex =>
-      ex shouldBe a [SQSReaderGracefulException]
+    whenReady(future.failed) { ex =>
+      ex shouldBe a[SQSReaderGracefulException]
     }
 
   }
