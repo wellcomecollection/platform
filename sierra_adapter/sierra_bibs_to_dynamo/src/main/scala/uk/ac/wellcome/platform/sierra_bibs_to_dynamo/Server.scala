80cd925ef551f9ebaea495859895fa2481733a72
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-12-19 10:30:11 +0000

    Consolidate DynamoConfig for sierra-x-to-dynamo services

--- sierra_adapter/sierra_bibs_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_bibs_to_dynamo/Server.scala
+++ sierra_adapter/sierra_bibs_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_bibs_to_dynamo/Server.scala
@@ -2,18 +2,12 @@ package uk.ac.wellcome.platform.sierra_bibs_to_dynamo
 
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.HttpServer
-import com.twitter.finatra.http.filters.{
-  CommonFilters,
-  LoggingMDCFilter,
-  TraceIdMDCFilter
-}
+import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
 import com.twitter.finatra.http.routing.HttpRouter
 import uk.ac.wellcome.finatra.modules._
 import uk.ac.wellcome.finatra.controllers.ManagementController
-import uk.ac.wellcome.platform.sierra_bibs_to_dynamo.modules.{
-  SierraBibsToDynamoModule,
-  SierraDynamoConfigModule
-}
+import uk.ac.wellcome.platform.sierra_adapter.modules.SierraDynamoConfigModule
+import uk.ac.wellcome.platform.sierra_bibs_to_dynamo.modules.SierraBibsToDynamoModule
 
 object ServerMain extends Server
 
