bfc3b5d9f2308303a98f590548a1fa0333cf8c0e
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-12-19 13:27:50 +0000

    Fix namespacing in sierra_adapter lib

--- sierra_adapter/sierra_items_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_items_to_dynamo/Server.scala
+++ sierra_adapter/sierra_items_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_items_to_dynamo/Server.scala
@@ -9,7 +9,7 @@ import com.twitter.finatra.http.filters.{
 }
 import com.twitter.finatra.http.routing.HttpRouter
 import uk.ac.wellcome.finatra.modules._
-import uk.ac.wellcome.platform.sierra_adapter.modules.SierraDynamoConfigModule
+import uk.ac.wellcome.sierra_adapter.modules.SierraDynamoConfigModule
 import uk.ac.wellcome.platform.sierra_items_to_dynamo.controllers.ManagementController
 import uk.ac.wellcome.platform.sierra_items_to_dynamo.modules.SierraItemsToDynamoModule
 
