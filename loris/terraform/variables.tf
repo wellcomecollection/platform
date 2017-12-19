c3362b1a0dacc16e94a9936cec488fbfeb185d92
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-12-19 11:13:10 +0000

    Add an essay comment about why CloudFront/ACM regions are terrible

--- loris/terraform/variables.tf
+++ loris/terraform/variables.tf
@@ -3,6 +3,18 @@ variable "aws_region" {
   default     = "eu-west-1"
 }
 
+# We can't look up the ARN of our IIIF ACM certificate using the Terraform
+# data providers because most of our infrastructure runs in eu-west-1, but
+# the certificate for CloudFront has to live in us-east-1.
+#
+# Quoting http://docs.aws.amazon.com/acm/latest/userguide/acm-regions.html:
+#
+#     To use an ACM Certificate with Amazon CloudFront, you must request or
+#     import the certificate in the US East (N. Virginia) region.
+#     ACM Certificates in this region that are associated with a CloudFront
+#     distribution are distributed to all the geographic locations configured
+#     for that distribution.
+#
 variable "iiif_acm_cert_arn" {
   description = "ARN of ACM cert for iiif API (in us-east-1) for CloudFront"
 }
