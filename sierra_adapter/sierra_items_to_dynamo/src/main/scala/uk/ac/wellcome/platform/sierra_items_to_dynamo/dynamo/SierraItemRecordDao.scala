8813792cbee2327782b618c4151d19a8e9a0ce07
caylaa <alicefuzier@gmail.com>
2017-12-20 15:44:04 +0000

    Creating SierraItemRecordDao

---
+++ sierra_adapter/sierra_items_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_items_to_dynamo/dynamo/SierraItemRecordDao.scala
@@ -0,0 +1,38 @@
+package uk.ac.wellcome.platform.sierra_items_to_dynamo.dynamo
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.gu.scanamo.syntax._
+import com.gu.scanamo.{Scanamo, Table}
+import com.twitter.inject.Logging
+import uk.ac.wellcome.models.SierraItemRecord
+import uk.ac.wellcome.dynamo._
+import uk.ac.wellcome.utils.GlobalExecutionContext.context
+
+import scala.concurrent.Future
+
+class SierraItemRecordDao(dynamoDbClient: AmazonDynamoDB, tableName: String) extends Logging {
+
+  val table = Table[SierraItemRecord](tableName)
+
+  def updateItem(sierraItemRecord: SierraItemRecord): Future[Unit] = Future {
+    Scanamo.exec(dynamoDbClient)(table
+      .given(
+        not(attributeExists('id)) or
+          (attributeExists('id) and 'modifiedDate < sierraItemRecord.modifiedDate.getEpochSecond)
+      )
+      .put(sierraItemRecord))
+  }
+
+
+  def getItem(id: String): Future[Option[SierraItemRecord]] = Future {
+    Scanamo.get[SierraItemRecord](dynamoDbClient)(tableName)('id -> id) match {
+      case Some(Right(item)) => Some(item)
+      case None => None
+      case Some(Left(readError)) =>
+        val exception = new RuntimeException(s"An error occurred while retrieving item $id: $readError")
+        error(s"An error occurred while retrieving item $id: $readError", exception)
+        throw exception
+    }
+  }
+
+}
---
+++ sierra_adapter/sierra_items_to_dynamo/src/test/scala/uk/ac/wellcome/platform/sierra_items_to_dynamo/dynamo/SierraItemRecordDaoTest.scala
@@ -0,0 +1,114 @@
+package uk.ac.wellcome.platform.sierra_items_to_dynamo.dynamo
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.model.{GetItemRequest, PutItemRequest}
+import com.gu.scanamo.Scanamo
+import com.gu.scanamo.syntax._
+import org.mockito.Matchers.any
+import org.mockito.Mockito._
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.models.SierraItemRecord
+import uk.ac.wellcome.platform.sierra_items_to_dynamo.locals.SierraItemsToDynamoDBLocal
+import uk.ac.wellcome.test.utils.ExtendedPatience
+import uk.ac.wellcome.dynamo._
+
+class SierraItemRecordDaoTest extends FunSpec with Matchers
+with SierraItemsToDynamoDBLocal with ScalaFutures with MockitoSugar with ExtendedPatience{
+
+  val sierraItemRecordDao = new SierraItemRecordDao(dynamoDbClient, tableName)
+
+  describe("get item") {
+
+    it("should return an item record if it exists in the database") {
+      val id = "i111"
+      val sierraItemRecord = SierraItemRecord(id = id, data = "{}", modifiedDate = "2005-01-01T00:00:00Z", bibIds = List())
+      Scanamo.put(dynamoDbClient)(tableName)(sierraItemRecord)
+
+      whenReady(sierraItemRecordDao.getItem(id = id)) { maybeItem =>
+        maybeItem shouldBe Some(sierraItemRecord)
+      }
+    }
+
+    it("should return None if the id does not exist in the database") {
+      whenReady(sierraItemRecordDao.getItem(id = "i111")) { maybeItem =>
+        maybeItem shouldBe None
+      }
+    }
+
+    it("should fail if an exception is thrown by dynamoDbClient") {
+      val dynamoDbClient = mock[AmazonDynamoDB]
+      val sierraItemRecordDao = new SierraItemRecordDao(dynamoDbClient, tableName)
+
+      val expectedException = new RuntimeException("BLERGH")
+      when(dynamoDbClient.getItem(any[GetItemRequest])).thenThrow(expectedException)
+
+      whenReady(sierraItemRecordDao.getItem(id = "i111").failed) { ex =>
+        ex shouldBe expectedException
+      }
+    }
+  }
+
+  describe("update item") {
+
+    it("should insert an item"){
+      val id = "i111"
+      val sierraItemRecord = SierraItemRecord(id = id, data = "{}", modifiedDate = "2005-01-01T00:00:00Z", bibIds = List())
+
+      whenReady(sierraItemRecordDao.updateItem(sierraItemRecord)) { _ =>
+        Scanamo.get[SierraItemRecord](dynamoDbClient)(tableName)('id -> id) shouldBe Some(Right(
+          sierraItemRecord
+        ))
+      }
+    }
+
+    it("should update an item if the existing one has an older modifiedDate") {
+      val id = "i111"
+      val oldSierraItemRecord = SierraItemRecord(id = id, data = "{}", modifiedDate = "2005-01-01T00:00:00Z", bibIds = List())
+      Scanamo.put(dynamoDbClient)(tableName)(oldSierraItemRecord)
+
+      val newSierraItemRecord = SierraItemRecord(id = id, data = "{}", modifiedDate = "2006-01-01T00:00:00Z", bibIds = List("b111"))
+
+      whenReady(sierraItemRecordDao.updateItem(newSierraItemRecord)) { _ =>
+        Scanamo.get[SierraItemRecord](dynamoDbClient)(tableName)('id -> id) shouldBe Some(Right(
+          newSierraItemRecord
+        ))
+      }
+
+    }
+
+    it("should not update an item if the existing one has an newer modifiedDate") {
+      val id = "i111"
+      val oldSierraItemRecord = SierraItemRecord(id = id, data = "{}", modifiedDate = "2005-01-01T00:00:00Z", bibIds = List())
+      val newSierraItemRecord = SierraItemRecord(id = id, data = "{}", modifiedDate = "2006-01-01T00:00:00Z", bibIds = List("b111"))
+      Scanamo.put(dynamoDbClient)(tableName)(newSierraItemRecord)
+
+
+      whenReady(sierraItemRecordDao.updateItem(oldSierraItemRecord)) { _ =>
+        Scanamo.get[SierraItemRecord](dynamoDbClient)(tableName)('id -> id) shouldBe Some(Right(
+          newSierraItemRecord
+        ))
+      }
+
+    }
+
+    it("should fail if an exception is thrown by dynamoDbClient") {
+      val sierraItemRecord = SierraItemRecord(id = "i111", data = "{}", modifiedDate = "2005-01-01T00:00:00Z", bibIds = List())
+
+      val dynamoDbClient = mock[AmazonDynamoDB]
+      val sierraItemRecordDao = new SierraItemRecordDao(dynamoDbClient, tableName)
+
+      val expectedException = new RuntimeException("BLERGH")
+      when(dynamoDbClient.putItem(any[PutItemRequest])).thenThrow(expectedException)
+
+      whenReady(sierraItemRecordDao.updateItem(sierraItemRecord).failed) { ex =>
+        ex shouldBe expectedException
+      }
+    }
+
+  }
+
+}
+
+
