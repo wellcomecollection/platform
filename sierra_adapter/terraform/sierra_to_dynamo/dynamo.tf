282a5787e7edb29afcb18966326a3cfede2dcf79
Alex Chan <a.chan@wellcome.ac.uk>
2017-12-20 11:34:09 +0000

    BEGONE, RANGE KEY, AND NEVER DARKEN OUR DOORSTEP AGAIN

--- sierra_adapter/terraform/sierra_to_dynamo/dynamo.tf
+++ sierra_adapter/terraform/sierra_to_dynamo/dynamo.tf
@@ -3,18 +3,12 @@ resource "aws_dynamodb_table" "sierra_table" {
   read_capacity    = 1
   write_capacity   = 1
   hash_key         = "id"
-  range_key        = "modifiedDate"
   stream_enabled   = true
   stream_view_type = "NEW_AND_OLD_IMAGES"
 
   attribute {
     name = "id"
     type = "S"
-  }
-
-  attribute {
-    name = "modifiedDate"
-    type = "N"
   }
 
   lifecycle {
