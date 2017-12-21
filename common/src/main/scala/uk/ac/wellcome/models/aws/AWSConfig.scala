1a3ff2b61236c794bc5ea3eea3fb6106b042a76a
Alex Chan <alex@alexwlchan.net>
2017-12-21 11:31:25 +0000

    Miscellaneous Sierra adapter fixes (#1326)
    * Turn on the window generators
    
    * Actually publish the item merger service
    
    * Extend the timeout on the Lambdas
    
    * Expose a helpful constructor on AWSConfig
    
    * Point the autoscaling services at the terrible fix
    
    * BEGONE, RANGE KEY, AND NEVER DARKEN OUR DOORSTEP AGAIN
    
    * Apply auto-formatting rules
    
    * use real tf release

--- common/src/main/scala/uk/ac/wellcome/models/aws/AWSConfig.scala
+++ common/src/main/scala/uk/ac/wellcome/models/aws/AWSConfig.scala
@@ -1,5 +1,16 @@
 package uk.ac.wellcome.models.aws
 
-case class AWSConfig(region: String,
-                     accessKey: Option[String],
-                     secretKey: Option[String])
+case class AWSConfig(
+  region: String,
+  accessKey: Option[String],
+  secretKey: Option[String]
+)
+
+object AWSConfig {
+  def apply(region: String, accessKey: String, secretKey: String): AWSConfig =
+    AWSConfig(
+      region = region,
+      accessKey = Some(accessKey),
+      secretKey = Some(secretKey)
+    )
+}
