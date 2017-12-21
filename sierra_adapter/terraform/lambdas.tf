b3dd9317c130e260e5e8b774131fd4f5cb28419a
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-12-21 12:34:29 +0000

    use filter to allow sierra_items_merger_events_topic to be triggered

--- sierra_adapter/terraform/lambdas.tf
+++ sierra_adapter/terraform/lambdas.tf
@@ -21,3 +21,27 @@ module "trigger_sierra_bibs_merger_filter" {
   lambda_function_arn  = "${module.lambda_sierra_bibs_merger_filter.arn}"
   lambda_function_name = "${module.lambda_sierra_bibs_merger_filter.role_name}"
 }
+
+module "lambda_sierra_items_merger_filter" {
+  source = "git::https://github.com/wellcometrust/terraform.git//lambda?ref=v1.2.0"
+
+  name        = "sierra_items_merger_filter_filter"
+  module_name = "transformer_sns_filter"
+  description = "Filters DynamoDB events for the Sierra items merger"
+  timeout     = 6
+
+  environment_variables = {
+    TOPIC_ARN = "${module.sierra_items_merger_events_topic.arn}"
+  }
+
+  alarm_topic_arn = "${local.lambda_error_alarm_arn}"
+  s3_key          = "lambdas/catalogue_pipeline/transformer_sns_filter.zip"
+}
+
+module "trigger_sierra_items_merger_filter" {
+  source = "git::https://github.com/wellcometrust/terraform.git//lambda/trigger_sns?ref=v1.0.0"
+
+  sns_trigger_arn      = "${module.sierra_to_dynamo_items.topic_arn}"
+  lambda_function_arn  = "${module.lambda_sierra_items_merger_filter.arn}"
+  lambda_function_name = "${module.lambda_sierra_items_merger_filter.role_name}"
+}
