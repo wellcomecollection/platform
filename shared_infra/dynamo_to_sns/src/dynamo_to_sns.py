907a063184e20e4fab4e81efaaad797f68293071
Alex Chan <a.chan@wellcome.ac.uk>
2018-01-02 13:27:27 +0000

    Now we allow having different stream types

--- shared_infra/dynamo_to_sns/src/dynamo_to_sns.py
+++ shared_infra/dynamo_to_sns/src/dynamo_to_sns.py
@@ -11,24 +11,45 @@ from wellcome_aws_utils.dynamo_event import create_dynamo_events
 from wellcome_aws_utils.sns_utils import publish_sns_message
 
 
-def get_sns_messages(dynamo_event):
-    """Given a DynamoDB Stream event, generate messages for sending to SNS."""
-    for dynamo_event in create_dynamo_events(event):
-        yield {
-            'event_type': dynamo_event.event_type.name,
-            'old_image': dynamo_event.old_image(deserialize_values=True),
-            'new_image': dynamo_event.new_image(deserialize_values=True),
-        }
+def get_sns_messages(trigger_event, stream_view_type):
+    """Given a DynamoDB Stream event, generate messages for sending to SNS.
+
+    ``stream_view_type`` must be one of the following values:
+
+    -   FULL_EVENT, which has "event_type", "old_image" and "new_image" keys
+    -   NEW_IMAGE_ONLY or OLD_IMAGE_ONLY, which post the corresponding image
+        directly into SNS.
+
+    """
+    for dynamo_event in create_dynamo_events(trigger_event):
+        if stream_view_type == 'FULL_EVENT':
+            yield {
+                'event_type': dynamo_event.event_type.name,
+                'old_image': dynamo_event.old_image(deserialize_values=True),
+                'new_image': dynamo_event.new_image(deserialize_values=True),
+            }
+        elif stream_view_type == 'OLD_IMAGE_ONLY':
+            yield dynamo_event.old_image(deserialize_values=True)
+        elif stream_view_type == 'NEW_IMAGE_ONLY':
+            yield dynamo_event.new_image(deserialize_values=True)
+        else:
+            raise ValueError(
+                f"Unrecognised stream view type: {stream_view_type!r}"
+            )
 
 
 def main(event, _):
-    print(f'Received event:\n{event}')
+    print(f'Received event: {event!r}')
 
     topic_arn = os.environ['TOPIC_ARN']
+    stream_view_type = os.environ.get('STREAM_VIEW_TYPE', 'FULL_EVENT')
 
     sns_client = boto3.client('sns')
 
-    for message in get_sns_messages(dynamo_event=event):
+    for message in get_sns_messages(
+        trigger_event=event,
+        stream_view_type=stream_view_type
+    ):
         publish_sns_message(
             sns_client=sns_client,
             topic_arn=topic_arn,
