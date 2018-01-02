e0cb844004357ba0591dfee7a76d16848d216942
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-01-02 13:23:57 +0000

    Do some refactoring in dynamo_to_sns.py

--- shared_infra/dynamo_to_sns/src/dynamo_to_sns.py
+++ shared_infra/dynamo_to_sns/src/dynamo_to_sns.py
@@ -1,7 +1,6 @@
 # -*- encoding: utf-8 -*-
 """
-Receives DynamoDB events and publishes the new image from
-the event to an SNS topic
+Receives DynamoDB events and publishes the event to an SNS topic.
 """
 
 import os
@@ -12,21 +11,26 @@ from wellcome_aws_utils.dynamo_event import create_dynamo_events
 from wellcome_aws_utils.sns_utils import publish_sns_message
 
 
-def main(event, _):
-    print(f'Received event:\n{event}')
-
-    sns_client = boto3.client('sns')
-    topic_arn = os.environ['TOPIC_ARN']
-
+def get_sns_messages(dynamo_event):
+    """Given a DynamoDB Stream event, generate messages for sending to SNS."""
     for dynamo_event in create_dynamo_events(event):
-        print(dynamo_event)
-        parsed_event = {
+        yield {
             'event_type': dynamo_event.event_type.name,
             'old_image': dynamo_event.old_image(deserialize_values=True),
             'new_image': dynamo_event.new_image(deserialize_values=True),
         }
+
+
+def main(event, _):
+    print(f'Received event:\n{event}')
+
+    topic_arn = os.environ['TOPIC_ARN']
+
+    sns_client = boto3.client('sns')
+
+    for message in get_sns_messages(dynamo_event=event):
         publish_sns_message(
+            sns_client=sns_client,
             topic_arn=topic_arn,
-            message=parsed_event,
-            sns_client=sns_client
+            message=message
         )
