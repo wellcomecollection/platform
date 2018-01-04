766b07063d723b0838e0ca8e98d8bad86db6f362
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-01-04 16:30:36 +0000

    Get the stream type correct

--- sierra_adapter/terraform/pipeline/lambdas.tf
+++ sierra_adapter/terraform/pipeline/lambdas.tf
@@ -27,9 +27,11 @@ module "trigger_sierra_window_generator_lambda" {
 module "dynamo_to_sns" {
   source = "git::https://github.com/wellcometrust/platform.git//shared_infra/dynamo_to_sns"
 
-  name           = "sierra_dynamo_to_sns_${var.resource_type}"
+  name           = "sierra_${var.resource_type}_updates"
   src_stream_arn = "${aws_dynamodb_table.sierra_table.stream_arn}"
   dst_topic_arn  = "${module.sierra_to_dynamo_updates_topic.arn}"
+
+  stream_view_type = "NEW_IMAGE_ONLY"
 
   lambda_error_alarm_arn = "${var.lambda_error_alarm_arn}"
 }
