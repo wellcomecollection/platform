46e3513cebc8e014a507a5541b7b369a4236ecf2
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-01-04 15:35:00 +0000

    Strip out the transformer filter in the catalogue pipeline stack

--- catalogue_pipeline/terraform/service_sierra_transformer.tf
+++ catalogue_pipeline/terraform/service_sierra_transformer.tf
@@ -27,10 +27,14 @@ module "sierra_transformer" {
   alb_client_error_alarm_arn = "${local.alb_client_error_alarm_arn}"
 }
 
-module "dynamo_to_sns_sierra_transformer" {
-  source                 = "transformer_filter"
-  name                   = "sierra"
-  src_stream_arn         = "${local.sierradata_table_stream_arn}"
-  dst_topic_arn          = "${module.sierra_transformer_topic.arn}"
+module "sierra_transformer_dynamo_to_sns" {
+  source = "git::https://github.com/wellcometrust/platform.git//shared_infra/dynamo_to_sns"
+
+  name           = "sierra"
+  src_stream_arn = "${local.sierradata_table_stream_arn}"
+  dst_topic_arn  = "${module.sierra_transformer_topic.arn}"
+
+  stream_view_type = "NEW_IMAGE_ONLY"
+
   lambda_error_alarm_arn = "${local.lambda_error_alarm_arn}"
 }
