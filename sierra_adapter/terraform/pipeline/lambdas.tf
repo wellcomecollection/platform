368b48607f802ac5ab591a815dc87b7e5af6b6d7
Alex Chan <a.chan@wellcome.ac.uk>
2018-01-04 15:35:38 +0000

    Reduce repetition. Reduce repetition.

--- sierra_adapter/terraform/pipeline/lambdas.tf
+++ sierra_adapter/terraform/pipeline/lambdas.tf
@@ -1,7 +1,7 @@
 module "window_generator_lambda" {
   source = "git::https://github.com/wellcometrust/terraform.git//lambda?ref=v1.0.5"
 
-  name = "sierra_${var.resource_type}_window_generator_${var.resource_type}"
+  name = "sierra_${var.resource_type}_window_generator"
 
   s3_key      = "lambdas/sierra_adapter/sierra_window_generator.zip"
   module_name = "sierra_window_generator"
