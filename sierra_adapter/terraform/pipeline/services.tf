512119c646ea617a2f37a9ef1f753cb2a2042f66
Alex Chan <a.chan@wellcome.ac.uk>
2018-01-05 11:07:12 +0000

    sierra_to_dynamo services are no longer autoscaling

--- sierra_adapter/terraform/pipeline/services.tf
+++ sierra_adapter/terraform/pipeline/services.tf
@@ -1,11 +1,16 @@
-module "sierra_to_dynamo_service" {
-  source = "git::https://github.com/wellcometrust/terraform-modules.git//sqs_autoscaling_service?ref=v5.3.0"
-  name   = "sierra_${var.resource_type}_to_dynamo"
+locals {
+  sierra_to_dynamo_name = "sierra_${var.resource_type}_to_dynamo"
+}
 
-  source_queue_name  = "${module.windows_queue.name}"
-  source_queue_arn   = "${module.windows_queue.arn}"
-  ecr_repository_url = "${module.ecr_repository_sierra_to_dynamo.repository_url}"
-  release_id         = "${var.sierra_to_dynamo_release_id}"
+data "aws_ecs_cluster" "cluster" {
+  cluster_name = "${var.cluster_name}"
+}
+
+module "sierra_to_dynamo_service" {
+  source = "git::https://github.com/wellcometrust/terraform-modules.git//ecs/service?ref=v5.3.0"
+  name   = "${local.sierra_to_dynamo_name}"
+
+  app_uri = "${module.ecr_repository_sierra_to_dynamo.repository_url}:${var.sierra_to_dynamo_release_id}"
 
   env_vars = {
     windows_queue_url = "${module.windows_queue.id}"
@@ -19,15 +24,25 @@ module "sierra_to_dynamo_service" {
     sierra_fields        = "${var.sierra_fields}"
   }
 
+  path_pattern = "/${local.sierra_to_dynamo_name}/*"
+
+  cpu    = 256
+  memory = 1024
+
+  deployment_minimum_healthy_percent = 0
+  deployment_maximum_percent         = 200
+
   env_vars_length = 7
 
-  cluster_name               = "${var.cluster_name}"
-  vpc_id                     = "${var.vpc_id}"
-  alb_cloudwatch_id          = "${var.alb_cloudwatch_id}"
-  alb_listener_https_arn     = "${var.alb_listener_https_arn}"
-  alb_listener_http_arn      = "${var.alb_listener_http_arn}"
-  alb_server_error_alarm_arn = "${var.alb_server_error_alarm_arn}"
-  alb_client_error_alarm_arn = "${var.alb_client_error_alarm_arn}"
+  cluster_id                   = "${data.aws_ecs_cluster.cluster.arn}"
+  vpc_id                       = "${var.vpc_id}"
+  loadbalancer_cloudwatch_id   = "${var.alb_cloudwatch_id}"
+  listener_https_arn           = "${var.alb_listener_https_arn}"
+  listener_http_arn            = "${var.alb_listener_http_arn}"
+  client_error_alarm_topic_arn = "${var.alb_server_error_alarm_arn}"
+  server_error_alarm_topic_arn = "${var.alb_client_error_alarm_arn}"
+
+  https_domain = "services.wellcomecollection.org"
 }
 
 module "sierra_merger_service" {
