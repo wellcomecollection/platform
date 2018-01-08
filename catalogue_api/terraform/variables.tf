ae5870e368f18affe4d24f8b2f2b23ba155e4e1a
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-01-08 16:16:59 +0000

    Remove a few now-unused variables

--- catalogue_api/terraform/variables.tf
+++ catalogue_api/terraform/variables.tf
@@ -54,37 +54,22 @@ variable "pinned_remus_api_nginx" {
   default     = ""
 }
 
-variable "api_task_count_stage" {
-  description = "How many tasks to run in the staging API"
-  default     = 1
-}
-
 # These variables change less frequently -- the service blocks in services.tf
 # will choose which variable to use based on the value of `production_api`.
-
-variable "api_task_count" {
-  description = "How many tasks to run in the production API"
-  default     = 3
-}
 
 variable "api_host" {
   description = "Hostname to use for the production API"
   default     = "api.wellcomecollection.org"
 }
 
-variable "api_host_stage" {
-  description = "Hostname to use for the staging version of the API"
-  default     = "api-stage.wellcomecollection.org"
-}
-
 variable "es_config_romulus" {
-  description = "ElasticCloud config for romulus"
+  description = "Elasticcloud config for romulus"
   type        = "map"
   default     = {}
 }
 
 variable "es_config_remus" {
-  description = "ElasticCloud config for remus"
+  description = "Elasticcloud config for remus"
   type        = "map"
   default     = {}
 }
