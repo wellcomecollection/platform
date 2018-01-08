a5929a7882c0e0dad64fcd82798c2410c6af315b
Alex Chan <a.chan@wellcome.ac.uk>
2018-01-08 15:50:38 +0000

    Remember to quote your strings!

--- catalogue_api/terraform/api_service/locals.tf
+++ catalogue_api/terraform/api_service/locals.tf
@@ -9,5 +9,5 @@ locals {
   api_release_id   = "${local.is_prod_api ? var.prod_api_release_id : var.release_ids["api"]}"
   nginx_release_id = "${local.is_prod_api ? var.prod_api_nginx_release_id : var.release_ids["nginx_api"]}"
 
-  host_name = "${local.is_prod_api ? api.wellcomecollection.org : api-stage.wellcomecollection.org}"
+  host_name = "${local.is_prod_api ? "api.wellcomecollection.org" : "api-stage.wellcomecollection.org"}"
 }
