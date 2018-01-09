9be59acc58f99a36317c448ea4d7bfc09d2111f0
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-01-09 17:29:39 +0000

    Use the S3 (Sequential S3 Sink, not S3) to push JSON to S3

--- sierra_adapter/sierra_reader/src/main/scala/uk/ac/wellcome/platform/sierra_reader/services/SierraReaderWorkerService.scala
+++ sierra_adapter/sierra_reader/src/main/scala/uk/ac/wellcome/platform/sierra_reader/services/SierraReaderWorkerService.scala
@@ -3,25 +3,29 @@ package uk.ac.wellcome.platform.sierra_reader.services
 import akka.Done
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Sink
+import com.amazonaws.services.s3.AmazonS3
 import com.google.inject.Inject
 import com.twitter.inject.annotations.Flag
 import uk.ac.wellcome.metrics.MetricsSender
 import uk.ac.wellcome.models.aws.SQSMessage
-import uk.ac.wellcome.platform.sierra_reader.sink.{SierraRecordWrapperFlow, SierraResourceTypes}
+import uk.ac.wellcome.platform.sierra_reader.flow.{SierraRecordWrapperFlow, SierraResourceTypes}
 import uk.ac.wellcome.sierra.{SierraSource, ThrottleRate}
-import uk.ac.wellcome.sns.SNSWriter
 import uk.ac.wellcome.sqs.{SQSReader, SQSWorker}
 import uk.ac.wellcome.sierra_adapter.services.WindowExtractor
+import io.circe.syntax._
+import io.circe.generic.auto._
+import uk.ac.wellcome.circe._
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
+import uk.ac.wellcome.platform.sierra_reader.sink.SequentialS3Sink
 
 class SierraReaderWorkerService @Inject()(
   reader: SQSReader,
-  writer: SNSWriter,
+  s3client: AmazonS3,
   system: ActorSystem,
   metrics: MetricsSender,
+  @Flag("aws.s3.bucketName") bucketName: String,
   @Flag("sierra.apiUrl") apiUrl: String,
   @Flag("sierra.oauthKey") sierraOauthKey: String,
   @Flag("sierra.oauthSecret") sierraOauthSecret: String,
@@ -31,6 +35,8 @@ class SierraReaderWorkerService @Inject()(
   implicit val actorSystem = system
   implicit val materialiser = ActorMaterializer()
   implicit val executionContext = system.dispatcher
+
+  val s3sink = SequentialS3Sink(client = s3client, bucketName = bucketName)
 
   val throttleRate = ThrottleRate(3, 1.second)
 
@@ -42,8 +48,10 @@ class SierraReaderWorkerService @Inject()(
     } yield ()
 
   private def runSierraStream(params: Map[String, String]): Future[Done] = {
-    SierraSource(apiUrl, sierraOauthKey, sierraOauthSecret, throttleRate)(
-      resourceType = "bibs",
-      params).via(SierraRecordWrapperFlow(resourceType = SierraResourceTypes.bibs)).runWith(Sink.ignore)
+    SierraSource(apiUrl, sierraOauthKey, sierraOauthSecret, throttleRate)(resourceType = "bibs", params)
+      .via(SierraRecordWrapperFlow(resourceType = SierraResourceTypes.bibs))
+      .map(record => record.asJson)
+      .zipWithIndex
+      .runWith(s3sink)
   }
 }
