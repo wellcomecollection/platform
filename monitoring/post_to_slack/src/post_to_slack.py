fb19d7596c108f0f97c02d6196c40b09bef4ce7e
Alex Chan <a.chan@wellcome.ac.uk>
2018-01-09 10:22:14 +0000

    Give a slightly nicer message in that case

--- monitoring/post_to_slack/src/post_to_slack.py
+++ monitoring/post_to_slack/src/post_to_slack.py
@@ -67,6 +67,13 @@ class Alarm:
         """
         Try to return a more human-readable explanation for the alarm.
         """
+        if self.name.endswith('-alb-not-enough-healthy-hosts'):
+            if self.state_reason == (
+                'Threshold Crossed: no datapoints were received for 1 period '
+                'and 1 missing datapoint was treated as [Breaching].'
+            ):
+                return 'There are no healthy hosts in the ALB target group.'
+
         match = DATAPOINT_RE.search(self.state_reason)
         if match is None:
             return
