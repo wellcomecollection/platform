40d5c637970e3d904f6ec542f51b4c2e6ea2e4e0
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-01-11 12:21:31 +0000

    Write some Terraform for the Sierra reader (#1386)
    * Split the window_generator into its own TF module
    
    * Set up the sierra_reader in TF
    
    * Fix the binary name
    
    * Fix the application config template
    
    * Fix some IAM permissions
    
    * Apply auto-formatting rules

--- sierra_adapter/terraform/pipeline/iam_role_policy.tf
+++ sierra_adapter/terraform/pipeline/iam_role_policy.tf
@@ -1,8 +1,3 @@
-resource "aws_iam_role_policy" "sierra_window_generator_sns_publish" {
-  role   = "${module.window_generator_lambda.role_name}"
-  policy = "${module.windows_topic.publish_policy}"
-}
-
 resource "aws_iam_role_policy" "allow_dynamo_access" {
   role   = "${module.sierra_to_dynamo_service.task_role_name}"
   policy = "${data.aws_iam_policy_document.sierra_table_permissions.json}"
