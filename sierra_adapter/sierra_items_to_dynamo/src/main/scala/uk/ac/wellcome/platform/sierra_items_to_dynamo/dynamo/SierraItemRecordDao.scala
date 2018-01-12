25fafeb5de91870f9d50cb632cd6be2a54e0d0aa
caylaa <alicefuzier@gmail.com>
2018-01-12 13:38:51 +0000

    Cleanup

--- sierra_adapter/sierra_items_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_items_to_dynamo/dynamo/SierraItemRecordDao.scala
+++ sierra_adapter/sierra_items_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_items_to_dynamo/dynamo/SierraItemRecordDao.scala
@@ -1,10 +1,6 @@
 package uk.ac.wellcome.platform.sierra_items_to_dynamo.dynamo
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
-import com.amazonaws.services.dynamodbv2.model.{
-  ConditionalCheckFailedException,
-  PutItemResult
-}
 import com.google.inject.Inject
 import com.gu.scanamo.ops.ScanamoOps
 import com.gu.scanamo.syntax._
@@ -13,7 +9,6 @@ import com.twitter.inject.Logging
 import uk.ac.wellcome.dynamo._
 import uk.ac.wellcome.models.aws.DynamoConfig
 import uk.ac.wellcome.models.transformable.sierra.SierraItemRecord
-import uk.ac.wellcome.platform.sierra_items_to_dynamo.sink.SierraItemsDynamoSink.logger
 import uk.ac.wellcome.utils.GlobalExecutionContext.context
 
 import scala.concurrent.Future
--- sierra_adapter/sierra_items_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_items_to_dynamo/modules/SierraItemsToDynamoModule.scala
+++ sierra_adapter/sierra_items_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_items_to_dynamo/modules/SierraItemsToDynamoModule.scala
@@ -5,13 +5,6 @@ import com.twitter.inject.{Injector, TwitterModule}
 import uk.ac.wellcome.platform.sierra_items_to_dynamo.services.SierraItemsToDynamoWorkerService
 
 object SierraItemsToDynamoModule extends TwitterModule {
-  flag[String]("sierra.apiUrl", "", "Sierra API url")
-  flag[String]("sierra.oauthKey", "", "Sierra API oauth key")
-  flag[String]("sierra.oauthSecret", "", "Sierra API oauth secret")
-  flag[String]("sierra.fields",
-               "",
-               "List of fields to include in the Sierra API response")
-
   override def singletonStartup(injector: Injector) {
     val workerService = injector.instance[SierraItemsToDynamoWorkerService]
 
