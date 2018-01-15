fdde73245d7c008a6b03c2eb9df0935fbba91942
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-01-15 16:34:39 +0000

    clean up ElasticSearchIndex a bit

--- common/src/main/scala/uk/ac/wellcome/elasticsearch/ElasticSearchIndex.scala
+++ common/src/main/scala/uk/ac/wellcome/elasticsearch/ElasticSearchIndex.scala
@@ -2,13 +2,11 @@ package uk.ac.wellcome.elasticsearch
 
 import com.sksamuel.elastic4s.http.ElasticDsl.{createIndex, _}
 import com.sksamuel.elastic4s.http.HttpClient
+import com.sksamuel.elastic4s.http.index.mappings.PutMappingResponse
 import com.sksamuel.elastic4s.mappings.MappingDefinition
 import com.sksamuel.elastic4s.mappings.dynamictemplate.DynamicMapping
-import com.sksamuel.elastic4s.mappings.dynamictemplate.DynamicMapping.Dynamic
 import com.twitter.inject.Logging
-import org.elasticsearch.ResourceAlreadyExistsException
 import org.elasticsearch.client.ResponseException
-import org.elasticsearch.transport.RemoteTransportException
 import uk.ac.wellcome.utils.GlobalExecutionContext.context
 
 import scala.concurrent.Future
@@ -18,34 +16,32 @@ trait ElasticSearchIndex extends Logging {
   val httpClient: HttpClient
   val mappingDefinition: MappingDefinition
 
-  def create: Future[Unit] =
-    httpClient
+  def update(): Future[PutMappingResponse] = httpClient
+    .execute {
+      putMapping(indexName / mappingDefinition.`type`)
+        .dynamic(
+          mappingDefinition.dynamic.getOrElse(
+            DynamicMapping.Strict))
+        .as(mappingDefinition.fields)
+    }
+    .recover {
+      case e: Throwable =>
+        error(s"Failed updating index $indexName", e)
+        throw e
+    }
+
+  def create: Future[Unit] = httpClient
       .execute(createIndex(indexName).mappings {
         mappingDefinition
       })
-      .recover {
-        case e: ResponseException =>
-          if(e.getCause.isInstanceOf[ResourceAlreadyExistsException]) {
+      .recoverWith {
+        case e: ResponseException
+          if e.getMessage().contains("index_already_exists_exception") =>
             info(s"Index $indexName already exists")
-          }
+            update()
         case e: Throwable =>
           error(s"Failed creating index $indexName", e)
-          throw e
-      }
-      .flatMap { _ =>
-        httpClient
-          .execute {
-            putMapping(indexName / mappingDefinition.`type`)
-              .dynamic(
-                mappingDefinition.dynamic.getOrElse(
-                  DynamicMapping.Strict))
-              .as(mappingDefinition.fields)
-          }
-          .recover {
-            case e: Throwable =>
-              error(s"Failed updating index $indexName", e)
-              throw e
-          }
+          Future.failed(e)
       }
       .map { _ =>
         info("Index updated successfully")
