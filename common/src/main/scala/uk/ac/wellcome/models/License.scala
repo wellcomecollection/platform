3b546bc5b0ddc462e61b9754489844fa15dd4daa
caylaa <alicefuzier@gmail.com>
2018-01-16 20:56:26 +0000

    re-add case

--- common/src/main/scala/uk/ac/wellcome/models/License.scala
+++ common/src/main/scala/uk/ac/wellcome/models/License.scala
@@ -60,25 +60,25 @@ case object License_CCBY extends License {
   val url = "http://creativecommons.org/licenses/by/4.0/"
 }
 
-object License_CCBYNC extends License{
+case object License_CCBYNC extends License{
   val licenseType = "CC-BY-NC"
   val label = "Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)"
   val url = "https://creativecommons.org/licenses/by-nc/4.0/"
 }
 
-object License_CCBYNCND extends License {
+case object License_CCBYNCND extends License {
   val licenseType = "CC-BY-NC-ND"
   val label = "Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)"
   val url = "https://creativecommons.org/licenses/by-nc-nd/4.0/"
 }
 
-object License_CC0 extends License {
+case object License_CC0 extends License {
   val licenseType = "CC-0"
   val label = "CC0 1.0 Universal"
   val url = "https://creativecommons.org/publicdomain/zero/1.0/legalcode"
 }
 
-object License_PDM extends License {
+case object License_PDM extends License {
   val licenseType = "PDM"
   val label = "Public Domain Mark"
   val url = "https://creativecommons.org/share-your-work/public-domain/pdm/"
