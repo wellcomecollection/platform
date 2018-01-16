4d458aa5b20ed1b91177850621eec1904af439c9
caylaa <alicefuzier@gmail.com>
2018-01-16 20:50:27 +0000

    Remove JsonProperty annotation for types not serialised with jackson

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/source/MiroTransformableData.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/source/MiroTransformableData.scala
@@ -1,6 +1,5 @@
 package uk.ac.wellcome.transformer.source
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import io.circe.generic.extras.JsonKey
 import org.apache.commons.lang.StringEscapeUtils
 import uk.ac.wellcome.circe.jsonUtil._
@@ -8,28 +7,28 @@ import uk.ac.wellcome.circe.jsonUtil._
 import scala.util.{Failure, Success, Try}
 
 case class MiroTransformableData(
-  @JsonKey("image_title") @JsonProperty("image_title") title: Option[String],
-  @JsonKey("image_creator") @JsonProperty("image_creator") creator: Option[List[String]],
-  @JsonKey("image_image_desc") @JsonProperty("image_image_desc") description: Option[String],
-  @JsonKey("image_image_desc_academic") @JsonProperty("image_image_desc_academic") academicDescription: Option[String],
-  @JsonKey("image_secondary_creator") @JsonProperty("image_secondary_creator") secondaryCreator: Option[List[String]],
-  @JsonKey("image_artwork_date") @JsonProperty("image_artwork_date") artworkDate: Option[String],
-  @JsonKey("image_cleared") @JsonProperty("image_cleared") cleared: Option[String],
-  @JsonKey("image_copyright_cleared") @JsonProperty("image_copyright_cleared") copyrightCleared: Option[String],
-  @JsonKey("image_keywords") @JsonProperty("image_keywords") keywords: Option[List[String]],
-  @JsonKey("image_keywords_unauth") @JsonProperty("image_keywords_unauth") keywordsUnauth: Option[List[String]],
-  @JsonKey("image_phys_format") @JsonProperty("image_phys_format") physFormat: Option[String],
-  @JsonKey("image_lc_genre") @JsonProperty("image_lc_genre") lcGenre: Option[String],
-  @JsonKey("image_tech_file_size") @JsonProperty("image_tech_file_size") techFileSize: Option[List[String]],
-  @JsonKey("image_use_restrictions") @JsonProperty("image_use_restrictions") useRestrictions: Option[String],
-  @JsonKey("image_supp_lettering") @JsonProperty("image_supp_lettering") suppLettering: Option[String],
-  @JsonKey("image_innopac_id") @JsonProperty("image_innopac_id") innopacID: Option[String],
-  @JsonKey("image_credit_line") @JsonProperty("image_credit_line") creditLine: Option[String],
-  @JsonKey("image_source_code") @JsonProperty("image_source_code") sourceCode: Option[String],
-  @JsonKey("image_library_ref_department") @JsonProperty("image_library_ref_department") libraryRefDepartment: Option[List[String]],
-  @JsonKey("image_library_ref_id") @JsonProperty("image_library_ref_id") libraryRefId: Option[List[String]],
-  @JsonKey("image_award") @JsonProperty("image_award") award: Option[List[String]],
-  @JsonKey("image_award_date") @JsonProperty("image_award_date") awardDate: Option[List[String]]
+  @JsonKey("image_title") title: Option[String],
+  @JsonKey("image_creator") creator: Option[List[String]],
+  @JsonKey("image_image_desc") description: Option[String],
+  @JsonKey("image_image_desc_academic") academicDescription: Option[String],
+  @JsonKey("image_secondary_creator") secondaryCreator: Option[List[String]],
+  @JsonKey("image_artwork_date") artworkDate: Option[String],
+  @JsonKey("image_cleared") cleared: Option[String],
+  @JsonKey("image_copyright_cleared") copyrightCleared: Option[String],
+  @JsonKey("image_keywords") keywords: Option[List[String]],
+  @JsonKey("image_keywords_unauth") keywordsUnauth: Option[List[String]],
+  @JsonKey("image_phys_format") physFormat: Option[String],
+  @JsonKey("image_lc_genre") lcGenre: Option[String],
+  @JsonKey("image_tech_file_size") techFileSize: Option[List[String]],
+  @JsonKey("image_use_restrictions") useRestrictions: Option[String],
+  @JsonKey("image_supp_lettering") suppLettering: Option[String],
+  @JsonKey("image_innopac_id") innopacID: Option[String],
+  @JsonKey("image_credit_line") creditLine: Option[String],
+  @JsonKey("image_source_code") sourceCode: Option[String],
+  @JsonKey("image_library_ref_department") libraryRefDepartment: Option[List[String]],
+  @JsonKey("image_library_ref_id") libraryRefId: Option[List[String]],
+  @JsonKey("image_award") award: Option[List[String]],
+  @JsonKey("image_award_date") awardDate: Option[List[String]]
 )
 
 case object MiroTransformableData {
--- common/src/main/scala/uk/ac/wellcome/models/aws/SQSMessage.scala
+++ common/src/main/scala/uk/ac/wellcome/models/aws/SQSMessage.scala
@@ -1,12 +1,11 @@
 package uk.ac.wellcome.models.aws
 
-import com.fasterxml.jackson.annotation._
 import io.circe.generic.extras.JsonKey
 
 case class SQSMessage(
-  @JsonKey("Subject") @JsonProperty("Subject") subject: Option[String],
-  @JsonKey("Message") @JsonProperty("Message") body: String,
-  @JsonKey("TopicArn") @JsonProperty("TopicArn") topic: String,
-  @JsonKey("Type") @JsonProperty("Type") messageType: String,
-  @JsonKey("Timestamp") @JsonProperty("Timestamp") timestamp: String
+  @JsonKey("Subject") subject: Option[String],
+  @JsonKey("Message") body: String,
+  @JsonKey("TopicArn") topic: String,
+  @JsonKey("Type") messageType: String,
+  @JsonKey("Timestamp") timestamp: String
 )
