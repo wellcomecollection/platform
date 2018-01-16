02f787d75c3346f5b870aebdf8c568bcb23b172f
Alex Chan <a.chan@wellcome.ac.uk>
2018-01-16 17:34:24 +0000

    Add a test that invisible works don't show up in API results

--- catalogue_api/api/src/test/scala/uk/ac/wellcome/platform/api/ApiWorksTestInvisible.scala
+++ catalogue_api/api/src/test/scala/uk/ac/wellcome/platform/api/ApiWorksTestInvisible.scala
@@ -22,4 +22,54 @@ class ApiWorksTestInvisible extends ApiWorksTestBase {
     }
   }
 
+  it("excludes works with visible=false from list results") {
+    // Start by indexing a work with visible=false.
+    val work = workWith(
+      canonicalId = "gze7bc24",
+      title = "This work has been deleted",
+      visible = false
+    )
+
+    // Then we index two ordinary works into Elasticsearch.
+    val works = createWorks(2)
+    insertIntoElasticSearch(works: _*)
+
+    eventually {
+      server.httpGet(
+        path = s"/$apiPrefix/works",
+        andExpect = Status.Ok,
+        withJsonBody = s"""
+          |{
+          |  ${resultList(totalResults = 3)},
+          |  "results": [
+          |   {
+          |     "type": "Work",
+          |     "id": "${works(0).id}",
+          |     "title": "${works(0).title}",
+          |     "description": "${works(0).description.get}",
+          |     "lettering": "${works(0).lettering.get}",
+          |     "createdDate": ${period(
+            works(0).createdDate.get)},
+          |     "creators": [ ${agent(works(0).creators(0))} ],
+          |     "subjects": [ ],
+          |     "genres": [ ]
+          |   },
+          |   {
+          |     "type": "Work",
+          |     "id": "${works(1).id}",
+          |     "title": "${works(1).title}",
+          |     "description": "${works(1).description.get}",
+          |     "lettering": "${works(1).lettering.get}",
+          |     "createdDate": ${period(
+            works(1).createdDate.get)},
+          |     "creators": [ ${agent(works(1).creators(0))} ],
+          |     "subjects": [ ],
+          |     "genres": [ ]
+          |   }
+          |  ]
+          |}
+          """.stripMargin
+      )
+    }
+  }
 }
