65e1f8fc5daf41935fcfb0b1532dfb8af22836fe
Alex Chan <alex@alexwlchan.net>
2018-01-17 16:23:43 +0000

    Teach the API to ignore works with visible=false (#1431)
    * Add a test for looking up an individual invisible work
    
    * Create a separate base class for ApiWorksTest
    
    * Create a separate test class for 410 Gone tests
    
    * Add a test that invisible works don't show up in API results
    
    * 2 != 3
    
    * Actually index the deleted work!
    
    * Tell Swagger that we can return 410 errors
    
    * Correctly 410 if looking up a missing work
    
    * Add a test that invisible works are excluded from search
    
    * Tweak the search so it's useful
    
    * Tighten up this test
    
    * Correctly filter results on visible=true
    
    * Correctly ignore the "visible" parameter
    
    * Add a test case in ElasticsearchServiceTest
    
    * Apply auto-formatting rules
    
    * Fix the ElasticsearchServiceTest
    
    * Apply auto-formatting rules

---
+++ catalogue_api/api/src/test/scala/uk/ac/wellcome/platform/api/ApiWorksTestInvisible.scala
@@ -0,0 +1,110 @@
+package uk.ac.wellcome.platform.api
+
+import com.twitter.finagle.http.Status
+import uk.ac.wellcome.models.Work
+
+class ApiWorksTestInvisible extends ApiWorksTestBase {
+
+  it("returns an HTTP 410 Gone if looking up a work with visible = false") {
+    val work = workWith(
+      canonicalId = "g9dtcj2e",
+      title = "This work has been deleted",
+      visible = false
+    )
+
+    insertIntoElasticSearch(work)
+
+    eventually {
+      server.httpGet(
+        path = s"/$apiPrefix/works/${work.canonicalId.get}",
+        andExpect = Status.Gone,
+        withJsonBody = gone(work.title)
+      )
+    }
+  }
+
+  it("excludes works with visible=false from list results") {
+    // Start by indexing a work with visible=false.
+    val deletedWork = workWith(
+      canonicalId = "gze7bc24",
+      title = "This work has been deleted",
+      visible = false
+    )
+
+    // Then we index two ordinary works into Elasticsearch.
+    val works = createWorks(2)
+
+    val worksToIndex: Seq[Work] = Seq[Work](deletedWork) ++ works
+    insertIntoElasticSearch(worksToIndex: _*)
+
+    eventually {
+      server.httpGet(
+        path = s"/$apiPrefix/works",
+        andExpect = Status.Ok,
+        withJsonBody = s"""
+          |{
+          |  ${resultList(totalResults = 2)},
+          |  "results": [
+          |   {
+          |     "type": "Work",
+          |     "id": "${works(0).id}",
+          |     "title": "${works(0).title}",
+          |     "description": "${works(0).description.get}",
+          |     "lettering": "${works(0).lettering.get}",
+          |     "createdDate": ${period(works(0).createdDate.get)},
+          |     "creators": [ ${agent(works(0).creators(0))} ],
+          |     "subjects": [ ],
+          |     "genres": [ ]
+          |   },
+          |   {
+          |     "type": "Work",
+          |     "id": "${works(1).id}",
+          |     "title": "${works(1).title}",
+          |     "description": "${works(1).description.get}",
+          |     "lettering": "${works(1).lettering.get}",
+          |     "createdDate": ${period(works(1).createdDate.get)},
+          |     "creators": [ ${agent(works(1).creators(0))} ],
+          |     "subjects": [ ],
+          |     "genres": [ ]
+          |   }
+          |  ]
+          |}
+          """.stripMargin
+      )
+    }
+  }
+
+  it("excludes works with visible=false from search results") {
+    val work = workWith(
+      canonicalId = "r8dx6std",
+      title = "A deleted dodo"
+    )
+    val deletedWork = workWith(
+      canonicalId = "e7rxkty8",
+      title = "This work has been deleted",
+      visible = false
+    )
+    insertIntoElasticSearch(work, deletedWork)
+
+    eventually {
+      server.httpGet(
+        path = s"/$apiPrefix/works?query=deleted",
+        andExpect = Status.Ok,
+        withJsonBody = s"""
+          |{
+          |  ${resultList()},
+          |  "results": [
+          |   {
+          |     "type": "Work",
+          |     "id": "${work.id}",
+          |     "title": "${work.title}",
+          |     "creators": [],
+          |     "subjects": [ ],
+          |     "genres": [ ]
+          |   }
+          |  ]
+          |}""".stripMargin
+      )
+    }
+  }
+}
