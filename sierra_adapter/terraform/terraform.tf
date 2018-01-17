b6f8b974d8f796fb44758f1e8180713058573e87
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-01-17 12:43:44 +0000

    Apply auto-formatting rules

--- sierra_adapter/terraform/terraform.tf
+++ sierra_adapter/terraform/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.9"
 
   backend "s3" {
-    bucket = "platform-infra"
+    bucket         = "platform-infra"
     key            = "terraform/sierra_adapter.tfstate"
     dynamodb_table = "terraform-locktable"
     region         = "eu-west-1"
