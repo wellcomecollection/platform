6e6d631cd69dfdf9c51ffa6d3ab63a4f5f617172
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-01-17 11:13:46 +0000

    Add permissions to write to CloudWatch

--- sierra_adapter/terraform/merger/iam_role_policy.tf
+++ sierra_adapter/terraform/merger/iam_role_policy.tf
@@ -2,3 +2,8 @@ resource "aws_iam_role_policy" "allow_merged_dynamo_access" {
   role   = "${module.sierra_merger_service.task_role_name}"
   policy = "${data.aws_iam_policy_document.sierra_merged_table_permissions.json}"
 }
+
+resource "aws_iam_role_policy" "push_cloudwatch_metric" {
+  role   = "${module.sierra_merger_service.task_role_name}"
+  policy = "${data.aws_iam_policy_document.allow_cloudwatch_push_metrics.json}"
+}
