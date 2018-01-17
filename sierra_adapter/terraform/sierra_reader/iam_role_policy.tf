84100178a6911d2311b7730ef82628e5d5832ab6
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-01-17 11:37:41 +0000

    Add permissions to write to CloudWatch (#1433)

--- sierra_adapter/terraform/sierra_reader/iam_role_policy.tf
+++ sierra_adapter/terraform/sierra_reader/iam_role_policy.tf
@@ -7,3 +7,8 @@ resource "aws_iam_role_policy" "allow_read_from_windows_q" {
   role   = "${module.sierra_reader_service.task_role_name}"
   policy = "${data.aws_iam_policy_document.read_from_windows_q.json}"
 }
+
+resource "aws_iam_role_policy" "push_cloudwatch_metric" {
+  role   = "${module.sierra_reader_service.task_role_name}"
+  policy = "${data.aws_iam_policy_document.allow_cloudwatch_push_metrics.json}"
+}
