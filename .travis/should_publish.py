c85afaa0446a3827cb066deef2b4e03da0c65ddd
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-01-18 19:27:35 +0000

    Fix the test logic again

--- .travis/should_publish.py
+++ .travis/should_publish.py
@@ -15,8 +15,8 @@ def should_publish(task, travis_event_type):
     We skip doing the publish/deploy step when running a build on master that
     doesn't have any relevant changes since the last deploy.
     """
-    if travis_event_type == 'pull_request':
-        print('*** We never publish from pull requests!')
+    if travis_event_type in ('cron', 'pull_request'):
+        print('*** We never publish from cron or pull requests!')
         return False
 
     assert travis_event_type == 'push'
