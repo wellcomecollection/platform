5ce14a10c8321b816d01a4eefb21103d1c855988
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-01-28 23:49:21 +0000

    Run 'terraform fmt'

--- sierra_adapter/terraform/s3.tf
+++ sierra_adapter/terraform/s3.tf
@@ -1,25 +1,25 @@
 resource "aws_s3_bucket" "sierra_data" {
   bucket = "wellcomecollection-sierra-adapter-data"
 
-#  lifecycle_rule {
-#    id      = "records_bibs"
-#    prefix  = "records_bibs/"
-#    enabled = true
-#
-#    expiration {
-#      days = 7
-#    }
-#  }
-#
-#  lifecycle_rule {
-#    id      = "records_items"
-#    prefix  = "records_items/"
-#    enabled = true
-#
-#    expiration {
-#      days = 7
-#    }
-#  }
+  #  lifecycle_rule {
+  #    id      = "records_bibs"
+  #    prefix  = "records_bibs/"
+  #    enabled = true
+  #
+  #    expiration {
+  #      days = 7
+  #    }
+  #  }
+  #
+  #  lifecycle_rule {
+  #    id      = "records_items"
+  #    prefix  = "records_items/"
+  #    enabled = true
+  #
+  #    expiration {
+  #      days = 7
+  #    }
+  #  }
 
   lifecycle {
     prevent_destroy = true
