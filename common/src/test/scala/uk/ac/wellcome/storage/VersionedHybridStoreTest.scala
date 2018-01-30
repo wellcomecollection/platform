79bc4814a4ff8de54738643d10c2c3e8ce78f334
Alex Chan <a.chan@wellcome.ac.uk>
2018-01-30 15:54:09 +0000

    Get first working version of the HybridStore

--- common/src/test/scala/uk/ac/wellcome/storage/VersionedHybridStoreTest.scala
+++ common/src/test/scala/uk/ac/wellcome/storage/VersionedHybridStoreTest.scala
@@ -4,9 +4,12 @@ import com.gu.scanamo.{DynamoFormat, Scanamo}
 import com.gu.scanamo.syntax._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.dynamo.VersionedDao
 import uk.ac.wellcome.locals.DynamoDBLocal
-import uk.ac.wellcome.models.Versioned
-import uk.ac.wellcome.test.utils.{JsonTestUtil, S3Local}
+import uk.ac.wellcome.models.aws.DynamoConfig
+import uk.ac.wellcome.models.{VersionUpdater, Versioned}
+import uk.ac.wellcome.s3.VersionedObjectStore
+import uk.ac.wellcome.test.utils.{ExtendedPatience, JsonTestUtil, S3Local}
 import uk.ac.wellcome.utils.JsonUtil._
 
 
@@ -18,14 +21,25 @@ case class ExampleRecord(
 ) extends Versioned
 
 
-class VersionedHybridStoreTest extends FunSpec with Matchers with S3Local with ScalaFutures with DynamoDBLocal[ExampleRecord] with JsonTestUtil {
+class VersionedHybridStoreTest extends FunSpec with Matchers with S3Local with ScalaFutures with DynamoDBLocal[ExampleRecord] with JsonTestUtil with ExtendedPatience {
+
+  implicit val testVersionUpdater = new VersionUpdater[ExampleRecord]{
+    override def updateVersion(testVersioned: ExampleRecord, newVersion: Int): ExampleRecord = {
+      testVersioned.copy(version = newVersion)
+    }
+  }
 
   override lazy val evidence: DynamoFormat[ExampleRecord] = DynamoFormat[ExampleRecord]
 
   override lazy val tableName: String = "versioned-hybrid-store-test"
   val bucketName = createBucketAndReturnName("versioned-hybrid-store-test")
 
-  val hybridStore = new VersionedHybridStore()
+  val hybridStore = new VersionedHybridStore(
+    versionedObjectStore = new VersionedObjectStore(s3Client = s3Client, bucketName = bucketName),
+    versionedDao = new VersionedDao(dynamoDbClient = dynamoDbClient, dynamoConfig = DynamoConfig(
+      table = tableName
+    ))
+  )
 
   it("stores a versioned record if it has never been seen before") {
     val record = ExampleRecord(
@@ -51,7 +65,6 @@ class VersionedHybridStoreTest extends FunSpec with Matchers with S3Local with S
       val retrievedJson = getJsonFromS3(bucketName = bucketName, key = s3key).noSpaces
       assertJsonStringsAreEqual(retrievedJson, toJson(record.copy(version = 2)).get)
     }
-
   }
 
 }
