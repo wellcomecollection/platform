c380d9a4f444d2bde5f00bfed1e6d38b9bb26e60
caylaa <alicefuzier@gmail.com>
2018-02-01 16:28:33 +0000

    Use the correct id field

--- catalogue_pipeline/terraform/vhs/dynamo.tf
+++ catalogue_pipeline/terraform/vhs/dynamo.tf
@@ -2,7 +2,7 @@ resource "aws_dynamodb_table" "table" {
   name             = "${var.table_name}"
   read_capacity    = 1
   write_capacity   = 1
-  hash_key         = "sourceId"
+  hash_key         = "id"
   stream_enabled   = true
   stream_view_type = "NEW_AND_OLD_IMAGES"
 
