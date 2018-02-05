a27abc01a3614819e802931e208f18d6c07bbcb6
Alex Chan <a.chan@wellcome.ac.uk>
2018-02-05 11:54:12 +0000

    Get the first test case passing

--- catalogue_pipeline/reindex_job_creator/src/reindex_job_creator.py
+++ catalogue_pipeline/reindex_job_creator/src/reindex_job_creator.py
@@ -1,4 +1,30 @@
 # -*- encoding: utf-8 -*-
 
+import os
+
+import boto3
+
+from wellcome_aws_utils.dynamo_event import DynamoEvent
+from wellcome_aws_utils.sns_utils import publish_sns_message
+
+
 def main(event, _ctxt):
-    pass
+    print(f'event={event!r}')
+
+    sns_client = boto3.client('sns')
+    topic_arn = os.environ['TOPIC_ARN']
+
+    for record in event['Records']:
+        image = record['dynamodb']['NewImage']
+
+        message = {
+            'shardId': image['shardId'],
+            'desiredVersion': image['desiredVersion'],
+        }
+
+        publish_sns_message(
+            sns_client=sns_client,
+            topic_arn=topic_arn,
+            message=message,
+            subject='Reindex job from reindex_job_creator'
+        )
