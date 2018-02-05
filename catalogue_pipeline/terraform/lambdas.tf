25c6497e3f920233ced31174e787c8cfa552ee4b
Alex Chan <a.chan@wellcome.ac.uk>
2018-02-05 11:31:52 +0000

    Add DynamoToSNS for the ReindexShardTracker

--- catalogue_pipeline/terraform/lambdas.tf
+++ catalogue_pipeline/terraform/lambdas.tf
@@ -22,3 +22,15 @@ module "trigger_reindexer_lambda" {
   function_arn  = "${module.lambda_schedule_reindexer.arn}"
   function_role = "${module.lambda_schedule_reindexer.role_name}"
 }
+
+module "reindex_shard_tracker_dynamo_to_sns" {
+  source = "git::https://github.com/wellcometrust/platform.git//shared_infra/dynamo_to_sns"
+
+  name           = "reindex_shard_tracker_updates"
+  src_stream_arn = "${aws_dynamodb_table.reindex_shard_tracker.stream_arn}"
+  dst_topic_arn  = "${module.reindex_shard_tracker_topic.arn}"
+
+  stream_view_type = "NEW_IMAGE_ONLY"
+
+  lambda_error_alarm_arn = "${local.lambda_error_alarm_arn}"
+}
