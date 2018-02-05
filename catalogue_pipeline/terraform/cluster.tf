0e6fdddbc5d168d03507f2d6b78095b1ef1b3911
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-02-05 11:31:43 +0000

    Update the catalogue-pipeline cluster to use the newest version of the ecs autoscaling module

---
+++ catalogue_pipeline/terraform/cluster.tf
@@ -0,0 +1,17 @@
+module "catalogue_pipeline_cluster" {
+  source = "git::https://github.com/wellcometrust/terraform.git//ecs/cluster?ref=asg_sg"
+  name   = "catalogue-pipeline"
+
+  vpc_subnets = ["${module.vpc_services.subnets}"]
+  vpc_id      = "${module.vpc_services.vpc_id}"
+
+  alb_certificate_domain = "catalogue-pipeline.wellcomecollection.org"
+
+  key_name = "${var.key_name}"
+
+  ec2_terminating_topic_arn                       = "${local.ec2_terminating_topic_arn}"
+  ec2_terminating_topic_publish_policy            = "${local.ec2_terminating_topic_publish_policy}"
+  ec2_instance_terminating_for_too_long_alarm_arn = "${local.ec2_instance_terminating_for_too_long_alarm_arn}"
+
+  alb_log_bucket_id = "${local.bucket_alb_logs_id}"
+}
--- catalogue_pipeline/terraform/userdata.tf
+++
@@ -1,4 +0,0 @@
-module "services_userdata" {
-  source       = "git::https://github.com/wellcometrust/terraform.git//userdata?ref=v1.0.0"
-  cluster_name = "${aws_ecs_cluster.services.name}"
-}
