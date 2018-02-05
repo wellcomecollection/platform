992443911e46996f469efe0731870ea74147c7fc
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-02-05 06:56:59 +0000

    Write something for parsing threshold messages

--- monitoring/post_to_slack/src/alarms.py
+++ monitoring/post_to_slack/src/alarms.py
@@ -3,27 +3,46 @@
 Utilities for working with CloudWatch alarms.
 """
 
-import json
+from datetime import datetime
+import re
+
+import attr
 
 
-class Alarm:
-    def __init__(self, message):
-        self.message = message
+# An alarm message is typically of the form:
+#
+#   Threshold Crossed: 1 datapoint [2.0 (05/02/18 06:35:00)] was greater than
+#   or equal to the threshold (1.0).
+#
+THRESHOLD_RE = re.compile(
+    r'^Threshold Crossed: '
+    r'1 datapoint '
+    r'\[(?P<actual_value>\d+)\.0 \((?P<date>\d{2}/\d{2}/\d{2} \d{2}:\d{2}:\d{2})\)\] '
+    r'was '
+    r'(?P<operator>(?:greater|less) than(?: or equal to)?) '
+    r'the threshold \((?P<desired_value>\d+)\.0\).'
+)
 
-    @property
-    def name(self):
-        """Returns the name of the alarm."""
-        return self.message['AlarmName']
 
-    @property
-    def source(self):
-        """Returns the name of the alarming service or queue, if possible."""
-        # e.g. ingestor-alb-unhealthy-hosts, loris-alb-not-enough-healthy-hosts
-        if '-alb-' in self.name:
-            return self.name.split('-alb-')[0]
+@attr.s
+class Threshold:
+    """Holds information about a "threshold crossed" error."""
+    actual_value = attr.ib(converter=int)
+    desired_value = attr.ib(converter=int)
 
-        # e.g. transformer_queue_dlq_not_empty, s3_demultiplexer_dlq_not_empty
-        if self.name.endswith('_dlq_not_empty'):
-            return self.name[:-len('_dlq_not_empty')]
+    # Datetime strings in alarms are of the form DD/MM/YY HH:MM:SS.
+    # For example: "03/02/18 16:13:00" or "05/02/18 06:38:00".
+    date = attr.ib(
+        converter=lambda dt: datetime.strptime(dt, '%d/%m/%y %H:%M:%S')
+    )
 
-        raise ValueError(f'Unable to identify source in {self.name!r}')
+    # e.g. 'greater than or equal to', 'less than'
+    operator = attr.ib()
+
+    @classmethod
+    def from_message(cls, message):
+        match = THRESHOLD_RE.match(message)
+        if match is None:
+            raise ValueError(f'Unable to parse message {message!r}')
+
+        return cls(**match.groupdict())
