0695dd19b6876af19d0631619af4e9e22fbed665
Alex Chan <alex@alexwlchan.net>
2018-02-05 06:56:59 +0000

    Write something for parsing threshold messages

--- monitoring/post_to_slack/src/test_alarms.py
+++ monitoring/post_to_slack/src/test_alarms.py
@@ -1,16 +1,54 @@
 # -*- encoding: utf-8
 
+import datetime as dt
+
 import pytest
 
-from alarms import Alarm
+from alarms import Threshold
 
 
-@pytest.mark.parametrize('alarm_name, expected_source', [
-    ('ingestor-alb-unhealthy-hosts', 'ingestor'),
-    ('loris-alb-not-enough-healthy-hosts', 'loris'),
-    ('transformer_queue_dlq_not_empty', 'transformer_queue'),
-    ('sierra_items_windows_dlq_not_empty', 'sierra_items_windows'),
-])
-def test_source(alarm_name, expected_source):
-    alarm = Alarm(message={'AlarmName': alarm_name})
-    assert alarm.source == expected_source
+class TestThreshold:
+
+    @pytest.mark.parametrize('message, actual_value', [
+        ('Threshold Crossed: 1 datapoint [1.0 (05/02/18 06:28:00)] was greater than the threshold (1.0).', 1),
+        ('Threshold Crossed: 1 datapoint [12.0 (05/02/18 06:28:00)] was greater than the threshold (1.0).', 12),
+    ])
+    def test_actual_value(self, message, actual_value):
+        t = Threshold.from_message(message)
+        assert t.actual_value == actual_value
+
+    @pytest.mark.parametrize('message, desired_value', [
+        ('Threshold Crossed: 1 datapoint [1.0 (05/02/18 06:28:00)] was greater than the threshold (1.0).', 1),
+        ('Threshold Crossed: 1 datapoint [1.0 (05/02/18 06:28:00)] was greater than the threshold (12.0).', 12),
+        ('Threshold Crossed: 1 datapoint [1.0 (05/02/18 06:28:00)] was greater than the threshold (0.0).', 0),
+    ])
+    def test_desired_value(self, message, desired_value):
+        t = Threshold.from_message(message)
+        assert t.desired_value == desired_value
+
+    @pytest.mark.parametrize('message, expected_date', [
+        ('Threshold Crossed: 1 datapoint [1.0 (05/02/18 06:28:00)] was greater than the threshold (1.0).',
+         dt.datetime(2018, 2, 5, 6, 28, 0)),
+        ('Threshold Crossed: 1 datapoint [1.0 (11/08/18 10:55:00)] was greater than the threshold (1.0).',
+         dt.datetime(2018, 8, 11, 10, 55, 0)),
+        ('Threshold Crossed: 1 datapoint [1.0 (09/01/18 10:36:31)] was greater than the threshold (1.0).',
+         dt.datetime(2018, 1, 9, 10, 36, 31)),
+    ])
+    def test_date(self, message, expected_date):
+        t = Threshold.from_message(message)
+        assert t.date == expected_date
+
+    @pytest.mark.parametrize('message, expected_operator', [
+        ('Threshold Crossed: 1 datapoint [1.0 (05/02/18 06:28:00)] was greater than the threshold (1.0).', 'greater than'),
+        ('Threshold Crossed: 1 datapoint [1.0 (05/02/18 06:28:00)] was greater than or equal to the threshold (1.0).', 'greater than or equal to'),
+        ('Threshold Crossed: 1 datapoint [1.0 (05/02/18 06:28:00)] was less than the threshold (1.0).', 'less than'),
+        ('Threshold Crossed: 1 datapoint [1.0 (05/02/18 06:28:00)] was less than or equal to the threshold (1.0).', 'less than or equal to'),
+    ])
+    def test_operator(self, message, expected_operator):
+        t = Threshold.from_message(message)
+        assert t.operator == expected_operator
+
+    @pytest.mark.parametrize('bad_message', ['foo', 'not a real message'])
+    def test_unexpected_message_is_valueerror(self, bad_message):
+        with pytest.raises(ValueError):
+            Threshold.from_message(bad_message)
