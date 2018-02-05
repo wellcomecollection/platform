92123dfd9bab107e1f5de01a471b21e6f972c184
Alex Chan <a.chan@wellcome.ac.uk>
2018-02-05 12:11:22 +0000

    Write Terraform for the new Lambda

--- catalogue_pipeline/terraform/lambdas.tf
+++ catalogue_pipeline/terraform/lambdas.tf
@@ -9,3 +9,27 @@ module "reindex_shard_tracker_dynamo_to_sns" {
 
   lambda_error_alarm_arn = "${local.lambda_error_alarm_arn}"
 }
+
+module "reindex_job_creator_lambda" {
+  source = "git::https://github.com/wellcometrust/terraform.git//lambda?ref=v1.0.5"
+
+  name   = "reindex_job_creator"
+  s3_key = "lambdas/catalogue_pipeline/reindex_job_creator.zip"
+
+  description = "Generate jobs for the reindexer from the ${aws_dynamodb_table.reindex_shard_tracker.id} table"
+
+  environment_variables = {
+    TOPIC_ARN = "${module.reindex_jobs_topic.arn}"
+  }
+
+  lambda_error_alarm_arn = "${local.lambda_error_alarm_arn}"
+}
+
+module "trigger_reindex_job_creator_lambda" {
+  source = "git::https://github.com/wellcometrust/terraform-modules.git//lambda/trigger_sns?ref=v1.0.0"
+
+  lambda_function_name = "${module.reindex_job_creator_lambda.function_name}"
+  lambda_function_arn  = "${module.reindex_job_creator_lambda.arn}"
+  sns_trigger_arn      = "${module.reindex_shard_tracker_topic.arn}"
+}
+
