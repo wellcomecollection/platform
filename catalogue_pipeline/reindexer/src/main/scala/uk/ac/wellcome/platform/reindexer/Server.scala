700ae95c3eb56d7a391f79447992b295829870d2
Alex Chan <a.chan@wellcome.ac.uk>
2018-02-06 11:20:18 +0000

    This flag is never even used!

--- catalogue_pipeline/reindexer/src/main/scala/uk/ac/wellcome/platform/reindexer/Server.scala
+++ catalogue_pipeline/reindexer/src/main/scala/uk/ac/wellcome/platform/reindexer/Server.scala
@@ -27,10 +27,6 @@ class Server extends HttpServer {
                              ReindexModule,
                              AkkaModule)
 
-  flag[Int]("reindex.maxAttempts",
-            3,
-            "Maximum number of times to retry a reindex operation")
-
   override def configureHttp(router: HttpRouter) {
     router
       .filter[CommonFilters]
