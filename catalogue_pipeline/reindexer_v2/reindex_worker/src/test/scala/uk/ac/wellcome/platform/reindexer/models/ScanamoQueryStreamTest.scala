de75c44001416b85aa9df70b1ac13516c765da31
Alex Chan <a.chan@wellcome.ac.uk>
2018-02-06 16:58:14 +0000

    Get the ScanamoQueryStreamTest compiling again

--- catalogue_pipeline/reindexer_v2/reindex_worker/src/test/scala/uk/ac/wellcome/platform/reindexer/models/ScanamoQueryStreamTest.scala
+++ catalogue_pipeline/reindexer_v2/reindex_worker/src/test/scala/uk/ac/wellcome/platform/reindexer/models/ScanamoQueryStreamTest.scala
@@ -1,45 +1,55 @@
 package uk.ac.wellcome.reindexer.models
 
-import com.gu.scanamo.Scanamo
+import com.gu.scanamo.{DynamoFormat, Scanamo}
 import com.gu.scanamo.error.DynamoReadError
 import com.gu.scanamo.query._
 import com.gu.scanamo.request.{ScanamoQueryOptions, ScanamoQueryRequest}
 import org.scalatest.concurrent.{Eventually, IntegrationPatience}
 import org.scalatest.{BeforeAndAfterEach, FunSpec, Matchers}
 import uk.ac.wellcome.models.transformable.MiroTransformable
-import uk.ac.wellcome.platform.reindexer.locals.DynamoDBLocal
 import uk.ac.wellcome.test.utils.DynamoConstants._
 
 import scala.collection.mutable.ListBuffer
-
 import com.gu.scanamo.syntax._
+import uk.ac.wellcome.locals.DynamoDBLocal
 
 class ScanamoQueryStreamTest
     extends FunSpec
     with BeforeAndAfterEach
     with Eventually
     with IntegrationPatience
-    with DynamoDBLocal
+    with DynamoDBLocal[MiroTransformable]
     with Matchers {
 
+  val tableName = "scanamo-query-stream-test-table"
   val bigString = "_" * maxDynamoItemSizeinKb
 
-  it("should run a given function for each batch from the query provided") {
+  override lazy val evidence: DynamoFormat[MiroTransformable] =
+    DynamoFormat[MiroTransformable]
 
+  it("should run a given function for each batch from the query provided") {
     type ResultGroup = List[Either[DynamoReadError, MiroTransformable]]
     val resultGroups: ListBuffer[ResultGroup] = new ListBuffer[ResultGroup]()
 
     val expectedBatchCount = 4
-    val itemsToPut = generateMiroTransformablesInBatches(expectedBatchCount)
 
-    itemsToPut.foreach(
-      Scanamo.put(dynamoDbClient)(miroDataTableName)
-    )
+    val itemsToPut = (1 to expectedBatchCount).map { i =>
+      MiroTransformable(
+        sourceId = s"Image$i",
+        MiroCollection = "CollectionV",
+        data = bigString,
+        reindexVersion = 1
+      )
+    }.toList
+
+    itemsToPut.foreach { item =>
+      Scanamo.put(dynamoDbClient)(tableName)(item)
+    }
 
     val expectedItemsStored = itemsToPut.map(Right(_))
 
     val scanamoQueryRequest = ScanamoQueryRequest(
-      miroDataTableName,
+      tableName,
       Some("ReindexTracker"),
       Query(
         AndQueryCondition(
@@ -49,10 +59,9 @@ class ScanamoQueryStreamTest
       ScanamoQueryOptions.default
     )
 
-    def updateVersion(resultGroup: ResultGroup): ResultGroup = {
-      resultGroups += resultGroup
-
-      resultGroup
+    def updateVersion(group: ResultGroup): ResultGroup = {
+      resultGroups += group
+      group
     }
 
     val ops = ScanamoQueryStream
@@ -67,6 +76,5 @@ class ScanamoQueryStreamTest
 
     resultGroups.length shouldBe expectedBatchCount
     actualItemsStored should contain theSameElementsAs expectedItemsStored
-
   }
 }
