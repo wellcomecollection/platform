ab01c287a9bf206be35cc721a7c07b6241afdba5
Alex Chan <a.chan@wellcome.ac.uk>
2018-02-07 13:30:57 +0000

    Store and compare the currency amounts

--- shared_infra/slack_budget_bot/src/slack_budget_bot.py
+++ shared_infra/slack_budget_bot/src/slack_budget_bot.py
@@ -2,20 +2,49 @@
 
 import attr
 import boto3
+import requests
 
 
 @attr.s
-class Budget(object):
+class CurrencyAmount:
+    unit = attr.ib()
+
+    # Turning strings like '233.205000000000012505552149377763271331787109375'
+    # into floats is a little cheaty, but a floating-point error here is
+    # insignificant, so we'll take the risk!
+    amount = attr.ib(converter=float)
+
+    def __gt__(self, other):
+        if self.unit != other.unit:
+            raise ValueError(
+                "Cannot compare {type(self).__name__} with different "
+                "currencies: {self.unit!r} != {other.unit!r}"
+            )
+        return self.amount > other.amount
+
+    def __lt__(self, other):
+        if self.unit != other.unit:
+            raise ValueError(
+                "Cannot compare {type(self).__name__} with different "
+                "currencies: {self.unit!r} != {other.unit!r}"
+            )
+        return self.amount < other.amount
+
+
+@attr.s
+class Budget:
     """Wrapper around an budget from the DescribeBudgets API."""
     data = attr.ib()
 
     @property
-    def actual_spend(self):
-        return float(self.data['CalculatedSpend']['ActualSpend']['Amount'])
+    def budget_limit(self):
+        spend = self.data['BudgetLimit']
+        return CurrencyAmount(unit=spend['Unit'], amount=spend['Amount'])
 
     @property
     def forecasted_spend(self):
-        return float(self.data['CalculatedSpend']['ForecastedSpend']['Amount'])
+        spend = self.data['CalculatedSpend']['ForecastedSpend']
+        return CurrencyAmount(unit=spend['Unit'], amount=spend['Amount'])
 
 
 def get_budgets(account_id):
@@ -33,6 +62,11 @@ def get_budgets(account_id):
 
 def main():
     budgets = get_budgets(account_id='760097843905')
+
+    bad_budgets = [b for b in budgets if b.budget_limit < b.forecasted_spend]
+
+    from pprint import pprint
+    pprint(bad_budgets)
 
 
 if __name__ == '__main__':
