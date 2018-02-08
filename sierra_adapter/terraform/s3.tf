10c735ff9cb6c0c2d3cd172617955b953615e98c
Alex Chan <alex@alexwlchan.net>
2018-02-08 17:35:57 +0000

    Rename the bucket that has the sierra_adapter data (#1522)

--- sierra_adapter/terraform/s3.tf
+++ sierra_adapter/terraform/s3.tf
@@ -1,25 +1,5 @@
-resource "aws_s3_bucket" "sierra_reader" {
-  bucket = "wellcomecollection-sierra-reader-data"
-
-  #  lifecycle_rule {
-  #    id      = "records_bibs"
-  #    prefix  = "records_bibs/"
-  #    enabled = true
-  #
-  #    expiration {
-  #      days = 7
-  #    }
-  #  }
-  #
-  #  lifecycle_rule {
-  #    id      = "records_items"
-  #    prefix  = "records_items/"
-  #    enabled = true
-  #
-  #    expiration {
-  #      days = 7
-  #    }
-  #  }
+resource "aws_s3_bucket" "sierra_adapter" {
+  bucket = "wellcomecollection-platform-adapters-sierra"
 
   lifecycle {
     prevent_destroy = true
