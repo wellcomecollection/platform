dfb2b2269a3a31979daa85b282936eeb1fecafed
Alex Chan <a.chan@wellcome.ac.uk>
2018-02-12 10:19:29 +0000

    Test fixture to create the DynamoDB table

---
+++ catalogue_pipeline/reindexer_v2/reindex_shard_generator/src/test_reindex_shard_generator.py
@@ -0,0 +1,67 @@
+# -*- encoding: utf-8 -*-
+
+import pytest
+
+from reindex_shard_generator import main
+
+
+@pytest.fixture
+def table_name():
+    return 'test_reindex_shard_generator-table'
+
+
+@pytest.fixture
+def source_data_table(dynamodb_client, table_name):
+    dynamodb_client.create_table(
+        TableName=table_name,
+        KeySchema=[
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH',
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'S',
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 1,
+            'WriteCapacityUnits': 1,
+        }
+    )
+
+    # Quoting from the boto3 docs:
+    #
+    #   CreateTable is an asynchronous operation. Upon receiving a CreateTable
+    #   request, DynamoDB immediately returns a response with a TableStatus
+    #   of CREATING . After the table is created, DynamoDB sets the
+    #   TableStatus to ACTIVE . You can perform read and write operations
+    #   only on an ACTIVE table.
+    #
+    #   You can use the DescribeTable action to check the table status.
+    #
+    # So we wait for the table to be created before yielding to the test.
+    #
+    # Link:  https://boto3.readthedocs.io/en/stable/reference/services/dynamodb.html#DynamoDB.Client.create_table
+    for i in range(10):
+        resp = dynamodb_client.describe_table(TableName=table_name)
+
+        if resp['Table']['TableStatus'] == 'ACTIVE':
+            break
+        else:
+            import time
+            time.sleep(1)
+
+    else:  # no break
+        raise RuntimeError(
+            f'Timed out waiting for table {table_name} to become ACTIVE'
+        )
+
+
+def test_reindex_shard_generator(dynamodb_client, source_data_table):
+    from pprint import pprint
+
+    pprint(dynamodb_client.list_tables())
+    assert False
