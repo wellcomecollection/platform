3000b3ece76cd3747783bd0136f5b792340149e0
caylaa <alicefuzier@gmail.com>
2018-02-13 14:22:35 +0000

    Remove unused imports and make shard_size a parameter of create_reindex_shard

--- catalogue_pipeline/reindexer_v2/reindex_shard_generator/src/test_shard_manager.py
+++ catalogue_pipeline/reindexer_v2/reindex_shard_generator/src/test_shard_manager.py
@@ -1,25 +1,23 @@
 # -*- encoding: utf-8
 
-from hypothesis import assume, given
-from hypothesis.strategies import integers
 import pytest
 
 from shard_manager import create_reindex_shard
 
 
 @pytest.mark.parametrize(
-    'source_name, source_id, source_size, expected_reindex_shard', [
-    ('sierra', 'b0000001', 5e6, 'sierra/2441'),
-    ('sierra', 'i0000001', 5e6, 'sierra/175'),
-    ('miro', 'V0000123', 2.5e5, 'miro/9'),
+    'source_name, source_id, source_size, shard_size, expected_reindex_shard', [
+    ('sierra', 'b0000001', 5e6, 1500, 'sierra/2441'),
+    ('sierra', 'i0000001', 5e6, 2000, 'sierra/2349'),
+    ('miro', 'V0000123', 2.5e5, 1000, 'miro/51'),
 ])
 def test_create_reindex_shard(
-    source_name, source_id, source_size, expected_reindex_shard
+    source_name, source_id, source_size, shard_size, expected_reindex_shard
 ):
     reindex_shard = create_reindex_shard(
         source_name=source_name,
         source_id=source_id,
         source_size=source_size,
-        shard_size=1500
+        shard_size=shard_size
     )
     assert reindex_shard == expected_reindex_shard
