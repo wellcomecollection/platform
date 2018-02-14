214c3245ba5cc5e26e50f5f4bcce9df03f9c5a22
Alex Chan <alex@alexwlchan.net>
2018-02-14 09:37:46 +0000

    Parse the first set of arguments

--- catalogue_pipeline/reindexer_v2/manage_reindex.py
+++ catalogue_pipeline/reindexer_v2/manage_reindex.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8
 """
-Create/update reindex shards in the ReindexShardTracker table.
+Create/update reindex shards in the reindex shard tracker table.
 
-Usage: manage_reindex.py create-shards --prefix=<PREFIX> --count=<COUNT>
+Usage: manage_reindex.py create-shards --prefix=<PREFIX> --count=<COUNT> [--table=<TABLE>]
 
 Actions:
-  create-shards         Create a new set of shards in the ReindexShardTracker
+  create-shards         Create a new set of shards in the reindex shard tracker
                         table with requestedVersion = currentVersion = 1
 
 Options:
   --prefix=<PREFIX>     Name of the reindex shard prefix, e.g. sierra, miro
   --count=<COUNT>       How many shards to create in the table
+  --table=<TABLE>       Name of the reindex shard tracker DynamoDB table
 
 """
+
+import docopt
+
+
+def create_shards(prefix, count, table_name):
+    """Create new shards in the table."""
+
+
+if __name__ == '__main__':
+    args = docopt.docopt(__doc__)
+
+    default_table_name = 'AlexTest'
+
+    if args['create-shards']:
+        prefix = args['--prefix']
+        count = int(args['--count'])
+        table_name = args['--table'] or default_table_name
+
+        create_shards(prefix=prefix, count=count, table_name=table_name)
