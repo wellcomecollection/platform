b6cdf49b10577ac6327a2aa67c8ce04914019009
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-02-14 09:42:22 +0000

    Fix complete reindex lambda s3 path

--- catalogue_pipeline/terraform/lambdas.tf
+++ catalogue_pipeline/terraform/lambdas.tf
@@ -14,7 +14,7 @@ module "reindex_job_creator_lambda" {
   source = "git::https://github.com/wellcometrust/terraform.git//lambda?ref=v1.0.5"
 
   name   = "reindex_job_creator"
-  s3_key = "lambdas/catalogue_pipeline/reindex_job_creator.zip"
+  s3_key = "lambdas/catalogue_pipeline/reindexer_v2/reindex_job_creator.zip"
 
   description = "Generate jobs for the reindexer from the ${aws_dynamodb_table.reindex_shard_tracker.id} table"
 
