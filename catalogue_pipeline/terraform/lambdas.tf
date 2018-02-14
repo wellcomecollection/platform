e9bb309442f86d5652a94d415911a8e6eef0e10f
Alex Chan <a.chan@wellcome.ac.uk>
2018-02-14 14:35:07 +0000

    Bump the timeout and batch size in the reindex_shard_generator

--- catalogue_pipeline/terraform/lambdas.tf
+++ catalogue_pipeline/terraform/lambdas.tf
@@ -66,6 +66,8 @@ module "shard_generator_lambda" {
 
   description = "Generate reindexShards for items in the ${module.versioned-hybrid-store.table_name} table"
 
+  timeout = 60
+
   environment_variables = {
     TABLE_NAME = "${module.versioned-hybrid-store.table_name}"
   }
@@ -79,4 +81,6 @@ module "trigger_shard_generator_lambda" {
   stream_arn    = "${module.versioned-hybrid-store.table_stream_arn}"
   function_arn  = "${module.shard_generator_lambda.arn}"
   function_role = "${module.shard_generator_lambda.role_name}"
+
+  batch_size = 50
 }
