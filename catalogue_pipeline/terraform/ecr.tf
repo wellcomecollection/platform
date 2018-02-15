830c54d4c78f64327f2743b7fd39208cb99bece1
Alex Chan <alex@alexwlchan.net>
2018-02-15 11:37:31 +0000

    Create an ECR repo for the reindex_worker

--- catalogue_pipeline/terraform/ecr.tf
+++ catalogue_pipeline/terraform/ecr.tf
@@ -23,6 +23,11 @@ module "ecr_repository_reindexer" {
   name   = "reindexer"
 }
 
+module "ecr_repository_reindex_worker" {
+  source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
+  name   = "reindex_worker"
+}
+
 module "ecr_repository_elasticdump" {
   source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
   name   = "elasticdump"
