63aba29f36d28af8d0ef77eb72406ad1de8144b4
Alex Chan <a.chan@wellcome.ac.uk>
2018-02-19 15:50:08 +0000

    Sourced: move the ID-specific logic onto the parent class
    Several of the methods/formatters defined on Versioned can also be
    defined on Sourced, and this makes them accessible to classes that
    extend S but not V.

---
+++ common/src/main/scala/uk/ac/wellcome/models/Sourced.scala
@@ -0,0 +1,30 @@
+package uk.ac.wellcome.models
+
+import com.amazonaws.services.dynamodbv2.model.AttributeValue
+import com.gu.scanamo.DynamoFormat
+import com.gu.scanamo.error.DynamoReadError
+
+trait Sourced {
+  val sourceId: String
+  val sourceName: String
+  val id: String = Sourced.id(sourceName, sourceId)
+}
+
+object Sourced {
+  implicit def toSourcedDynamoFormatWrapper[T <: Sourced](
+    implicit dynamoFormat: DynamoFormat[T]): SourcedDynamoFormatWrapper[T] =
+    new SourcedDynamoFormatWrapper[T](dynamoFormat)
+
+  def id(sourceName: String, sourceId: String) = s"$sourceName/$sourceId"
+}
+
+class SourcedDynamoFormatWrapper[T <: Sourced](
+  dynamoFormat: DynamoFormat[T]) {
+  val enrichedDynamoFormat = new DynamoFormat[T] {
+    override def read(av: AttributeValue): Either[DynamoReadError, T] =
+      dynamoFormat.read(av)
+
+    override def write(t: T): AttributeValue =
+      dynamoFormat.write(t).addMEntry("id", new AttributeValue(t.id))
+  }
+}
