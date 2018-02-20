206f18cdf048a05068fbc5ac429cdf6cb0aba633
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-02-20 16:33:07 +0000

    Apply auto-formatting rules

--- reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex_worker/services/ReindexService.scala
+++ reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex_worker/services/ReindexService.scala
@@ -13,7 +13,11 @@ import uk.ac.wellcome.dynamo.VersionedDao
 import uk.ac.wellcome.exceptions.GracefulFailureException
 import uk.ac.wellcome.metrics.MetricsSender
 import uk.ac.wellcome.models.aws.DynamoConfig
-import uk.ac.wellcome.models.{Sourced, SourcedDynamoFormatWrapper, VersionUpdater}
+import uk.ac.wellcome.models.{
+  Sourced,
+  SourcedDynamoFormatWrapper,
+  VersionUpdater
+}
 import uk.ac.wellcome.platform.reindex_worker.models.ReindexJob
 import uk.ac.wellcome.storage.HybridRecord
 import uk.ac.wellcome.utils.GlobalExecutionContext.context
@@ -37,7 +41,8 @@ class ReindexService @Inject()(dynamoDBClient: AmazonDynamoDB,
   }
 
   def runReindex(reindexJob: ReindexJob)(
-    implicit evidence: SourcedDynamoFormatWrapper[HybridRecord]): Future[List[Unit]] = {
+    implicit evidence: SourcedDynamoFormatWrapper[HybridRecord])
+    : Future[List[Unit]] = {
 
     info(s"ReindexTargetService running $reindexJob")
 
@@ -51,19 +56,20 @@ class ReindexService @Inject()(dynamoDBClient: AmazonDynamoDB,
     // large, this might cause out-of-memory errors -- in practice, we're
     // hoping that the shards/individual records are small enough for this
     // not to be a problem.
-    val queryResults: List[Either[DynamoReadError, HybridRecord]] = Scanamo.exec(dynamoDBClient)(
-      index.query(
-        'reindexShard -> reindexJob.shardId and
-        KeyIs('reindexVersion, LT, reindexJob.desiredVersion)
+    val queryResults: List[Either[DynamoReadError, HybridRecord]] =
+      Scanamo.exec(dynamoDBClient)(
+        index.query(
+          'reindexShard -> reindexJob.shardId and
+            KeyIs('reindexVersion, LT, reindexJob.desiredVersion)
+        )
       )
-    )
 
     val outdatedRecords = queryResults.map {
       case Left(err: DynamoReadError) => {
-                  warn(s"Failed to read Dynamo records for $reindexJob: $err")
-                  throw GracefulFailureException(
-                    new RuntimeException(s"Error in the DynamoDB query: $err")
-                  )
+        warn(s"Failed to read Dynamo records for $reindexJob: $err")
+        throw GracefulFailureException(
+          new RuntimeException(s"Error in the DynamoDB query: $err")
+        )
       }
       case Right(r: HybridRecord) => r
     }
@@ -71,8 +77,12 @@ class ReindexService @Inject()(dynamoDBClient: AmazonDynamoDB,
     // Then we PUT all the records.  It might be more efficient to do a
     // bulk update, but this will do for now.
     val updates: List[Future[Unit]] = outdatedRecords.map { hybridRecord =>
-      val updatedRecord = hybridRecord.copy(reindexVersion = reindexJob.desiredVersion)
-      versionedDao.updateRecord[HybridRecord](updatedRecord)(evidence = evidence, versionUpdater = versionUpdater)
+      val updatedRecord =
+        hybridRecord.copy(reindexVersion = reindexJob.desiredVersion)
+      versionedDao.updateRecord[HybridRecord](updatedRecord)(evidence =
+                                                               evidence,
+                                                             versionUpdater =
+                                                               versionUpdater)
     }
 
     Future.sequence(updates)
