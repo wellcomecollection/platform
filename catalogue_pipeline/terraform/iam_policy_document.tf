dd8970e2b5fb200e6a20246b9846c2c82bd78943
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-02-20 12:22:30 +0000

    reindexer: move the complete_reindex Lambda into the new TF stack

--- catalogue_pipeline/terraform/iam_policy_document.tf
+++ catalogue_pipeline/terraform/iam_policy_document.tf
@@ -1,15 +1,3 @@
-data "aws_iam_policy_document" "reindex_shard_tracker_table" {
-  statement {
-    actions = [
-      "dynamodb:*",
-    ]
-
-    resources = [
-      "${aws_dynamodb_table.reindex_shard_tracker.arn}",
-    ]
-  }
-}
-
 data "aws_iam_policy_document" "allow_cloudwatch_push_metrics" {
   statement {
     actions = [
---
+++ reindexer/terraform/iam_policy_document.tf
@@ -0,0 +1,11 @@
+data "aws_iam_policy_document" "reindex_shard_tracker_table_full_access" {
+  statement {
+    actions = [
+      "dynamodb:*",
+    ]
+
+    resources = [
+      "${aws_dynamodb_table.reindex_shard_tracker.arn}",
+    ]
+  }
+}
