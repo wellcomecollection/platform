7a966a345b250acb4ef09c002c1982c3f9a3c9c8
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-02-20 12:17:56 +0000

    reindexer: move the reindex_job_creator into the new TF stack

--- catalogue_pipeline/terraform/lambdas.tf
+++ catalogue_pipeline/terraform/lambdas.tf
@@ -1,29 +1,3 @@
-module "reindex_job_creator_lambda" {
-  source = "git::https://github.com/wellcometrust/terraform.git//lambda?ref=v1.0.5"
-
-  name   = "reindex_job_creator"
-  s3_key = "lambdas/reindexer/reindex_job_creator.zip"
-
-  description = "Generate jobs for the reindexer from the ${aws_dynamodb_table.reindex_shard_tracker.id} table"
-
-  timeout = 10
-
-  environment_variables = {
-    TOPIC_ARN = "${module.reindex_jobs_topic.arn}"
-  }
-
-  alarm_topic_arn = "${local.lambda_error_alarm_arn}"
-}
-
-module "trigger_reindex_job_creator_lambda" {
-  source = "git::https://github.com/wellcometrust/terraform-modules.git//lambda/trigger_sns?ref=v1.0.0"
-
-  lambda_function_name = "${module.reindex_job_creator_lambda.function_name}"
-  lambda_function_arn  = "${module.reindex_job_creator_lambda.arn}"
-
-  sns_trigger_arn = "${module.reindex_shard_tracker_topic.arn}"
-}
-
 module "complete_reindex_lambda" {
   source = "git::https://github.com/wellcometrust/terraform.git//lambda?ref=v1.0.5"
 
--- catalogue_pipeline/terraform/topics.tf
+++ catalogue_pipeline/terraform/topics.tf
@@ -23,11 +23,6 @@ module "id_minter_topic" {
   name   = "id_minter"
 }
 
-module "reindex_jobs_topic" {
-  source = "git::https://github.com/wellcometrust/terraform.git//sns?ref=v1.0.0"
-  name   = "reindex_jobs"
-}
-
 module "reindex_jobs_complete_topic" {
   source = "git::https://github.com/wellcometrust/terraform.git//sns?ref=v6.4.2"
   name   = "reindex_jobs_complete"
