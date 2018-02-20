cd5f5f3a3b6bc91d9f8355a3d752463db7ad0a7d
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-02-20 12:52:47 +0000

    reindexer: fix a typo in a local name

--- reindexer/terraform/locals.tf
+++ reindexer/terraform/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  vhs_dynamodb_full_access_policy = "${data.terraform_remote_state.catalogue_pipeline.vhs_dynamo_full_access_policy}"
+  vhs_dynamodb_full_access_policy = "${data.terraform_remote_state.catalogue_pipeline.vhs_dynamodb_full_access_policy}"
   vhs_dynamodb_update_policy      = "${data.terraform_remote_state.catalogue_pipeline.vhs_dynamodb_update_policy}"
   vhs_table_name                  = "${data.terraform_remote_state.catalogue_pipeline.vhs_table_name}"
   vhs_table_stream_arn            = "${data.terraform_remote_state.catalogue_pipeline.vhs_table_stream_arn}"
