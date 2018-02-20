47f49ae960525332a4710950dc2fdad4f8752bb3
caylaa <alicefuzier@gmail.com>
2018-02-20 12:46:10 +0000

    Make the id_minter compile

--- catalogue_pipeline/id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/IdMinterFeatureTest.scala
+++ catalogue_pipeline/id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/IdMinterFeatureTest.scala
@@ -36,6 +36,7 @@ class IdMinterFeatureTest
 
     val work = Work(title = Some(title),
                     sourceIdentifier = identifier,
+      version = 1,
                     identifiers = List(identifier))
 
     val sqsMessage = SQSMessage(
