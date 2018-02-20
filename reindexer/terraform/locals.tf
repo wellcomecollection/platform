2993a41cb3c677e13f248bad7cb993893659cec2
Alex Chan <a.chan@wellcome.ac.uk>
2018-02-20 12:44:13 +0000

    vhs: Better names for the exported Terraform policies

--- reindexer/terraform/locals.tf
+++ reindexer/terraform/locals.tf
@@ -1,7 +1,8 @@
 locals {
-  vhs_dynamo_update_policy = "${data.terraform_remote_state.catalogue_pipeline.vhs_dynamo_update_policy}"
-  vhs_table_name           = "${data.terraform_remote_state.catalogue_pipeline.vhs_table_name}"
-  vhs_table_stream_arn     = "${data.terraform_remote_state.catalogue_pipeline.vhs_table_stream_arn}"
+  vhs_dynamodb_full_access_policy = "${data.terraform_remote_state.catalogue_pipeline.vhs_dynamo_full_access_policy}"
+  vhs_dynamodb_update_policy      = "${data.terraform_remote_state.catalogue_pipeline.vhs_dynamodb_update_policy}"
+  vhs_table_name                  = "${data.terraform_remote_state.catalogue_pipeline.vhs_table_name}"
+  vhs_table_stream_arn            = "${data.terraform_remote_state.catalogue_pipeline.vhs_table_stream_arn}"
 
   lambda_error_alarm_arn = "${data.terraform_remote_state.shared_infra.lambda_error_alarm_arn}"
 }
