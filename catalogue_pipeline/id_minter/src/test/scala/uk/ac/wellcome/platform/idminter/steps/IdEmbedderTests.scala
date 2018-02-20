6210df53d7055936006e8fb619868b2cdb4b39c2
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-02-20 12:46:10 +0000

    Make the id_minter compile

--- catalogue_pipeline/id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/steps/IdEmbedderTests.scala
+++ catalogue_pipeline/id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/steps/IdEmbedderTests.scala
@@ -48,6 +48,7 @@ class IdEmbedderTests
 
     val originalWork = Work(title = Some("crap"),
                             sourceIdentifier = identifier,
+      version = 1,
                             canonicalId = None)
 
     val newCanonicalId = "5467"
@@ -82,6 +83,7 @@ class IdEmbedderTests
 
     val originalWork = Work(title = Some("crap"),
                             sourceIdentifier = identifier,
+      version = 1,
                             canonicalId = None)
 
     val expectedException = new Exception("Aaaaah something happened!")
@@ -125,6 +127,7 @@ class IdEmbedderTests
 
     val originalWork = Work(title = Some("crap"),
                             sourceIdentifier = identifier,
+      version =1,
                             canonicalId = None,
                             items = List(originalItem1, originalItem2))
 
