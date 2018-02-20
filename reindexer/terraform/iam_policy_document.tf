de980d97d411233c1959a97b2ed3214ff467d59c
Alex Chan <a.chan@wellcome.ac.uk>
2018-02-20 12:50:34 +0000

    aws_iam_policy_document: delete two unused policy documents

--- reindexer/terraform/iam_policy_document.tf
+++ reindexer/terraform/iam_policy_document.tf
@@ -21,20 +21,3 @@ data "aws_iam_policy_document" "allow_cloudwatch_push_metrics" {
     ]
   }
 }
-
-data "aws_iam_policy_document" "allow_cloudwatch_read_metrics" {
-  statement {
-    actions = [
-      "cloudwatch:DescribeAlarmHistory",
-      "cloudwatch:DescribeAlarms",
-      "cloudwatch:DescribeAlarmsForMetric",
-      "cloudwatch:GetMetricData",
-      "cloudwatch:GetMetricStatistics",
-      "cloudwatch:ListMetrics",
-    ]
-
-    resources = [
-      "*",
-    ]
-  }
-}
