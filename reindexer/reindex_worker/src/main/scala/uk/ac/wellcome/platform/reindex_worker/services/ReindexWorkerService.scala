5fd09f85956bc7b86947f7815bd97938c4469492
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-02-21 16:01:24 +0000

    processMessage: use a for comprehension instead of explicit match
    JsonUtil already throws a GracefulFailureException if it fails to parse
    a message, so we don't need to rethrow one here.

--- reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex_worker/services/ReindexWorkerService.scala
+++ reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex_worker/services/ReindexWorkerService.scala
@@ -2,7 +2,6 @@ package uk.ac.wellcome.platform.reindex_worker.services
 
 import akka.actor.ActorSystem
 import com.google.inject.Inject
-import uk.ac.wellcome.exceptions.GracefulFailureException
 import uk.ac.wellcome.metrics.MetricsSender
 import uk.ac.wellcome.models.aws.SQSMessage
 import uk.ac.wellcome.platform.reindex_worker.models.{
@@ -15,7 +14,6 @@ import uk.ac.wellcome.utils.GlobalExecutionContext.context
 import uk.ac.wellcome.utils.JsonUtil._
 
 import scala.concurrent.Future
-import scala.util.{Failure, Success}
 
 class ReindexWorkerService @Inject()(
   targetService: ReindexService,
@@ -26,19 +24,13 @@ class ReindexWorkerService @Inject()(
 ) extends SQSWorker(reader, system, metrics) {
 
   override def processMessage(message: SQSMessage): Future[Unit] =
-    fromJson[ReindexJob](message.body) match {
-      case Success(reindexJob) => {
-        val result = for {
-          _ <- targetService.runReindex(reindexJob = reindexJob)
-          message <- Future.fromTry(toJson(CompletedReindexJob(reindexJob)))
-          result <- snsWriter.writeMessage(
-            subject = s"source: ${this.getClass.getSimpleName}.processMessage",
-            message = message
-          )
-        } yield ()
-        result.recover { case err => throw GracefulFailureException(err) }
-      }
-
-      case Failure(err) => Future.failed(new GracefulFailureException(err))
-    }
+    for {
+      reindexJob <- Future.fromTry(fromJson[ReindexJob](message.body))
+      _ <- targetService.runReindex(reindexJob = reindexJob)
+      message <- Future.fromTry(toJson(CompletedReindexJob(reindexJob)))
+      _ <- snsWriter.writeMessage(
+        subject = s"source: ${this.getClass.getSimpleName}.processMessage",
+        message = message
+      )
+    } yield ()
 }
