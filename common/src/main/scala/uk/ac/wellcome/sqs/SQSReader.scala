eff2a3fb78a1cab9048f178e612b38c9ac1df259
caylaa <alicefuzier@gmail.com>
2018-02-21 14:12:13 +0000

    Don't log so much

--- common/src/main/scala/uk/ac/wellcome/sqs/SQSReader.scala
+++ common/src/main/scala/uk/ac/wellcome/sqs/SQSReader.scala
@@ -67,7 +67,7 @@ class SQSReader @Inject()(sqsClient: AmazonSQS, sqsConfig: SQSConfig)
         .flatMap(_ => deleteMessage(message))
         .recover {
           case e: GracefulFailureException =>
-            warn(s"An error occurred while processing the message $message", e)
+            warn(s"An error occurred while processing the message ${message.getMessageId}", e)
             ()
           case e: Throwable =>
             error(s"Error processing message ${message.getMessageId}", e)
