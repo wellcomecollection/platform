28162defc0ba156217db45510d12c3433ca363f9
Alex Chan <a.chan@wellcome.ac.uk>
2018-02-21 13:58:46 +0000

    terraform: move all the tfstate files into the new bucket

--- reindexer/terraform/terraform.tf
+++ reindexer/terraform/terraform.tf
@@ -13,7 +13,7 @@ data "terraform_remote_state" "catalogue_pipeline" {
   backend = "s3"
 
   config {
-    bucket = "platform-infra"
+    bucket = "wellcomecollection-platform-infra"
     key    = "terraform/catalogue_pipeline.tfstate"
     region = "eu-west-1"
   }
@@ -23,7 +23,7 @@ data "terraform_remote_state" "shared_infra" {
   backend = "s3"
 
   config {
-    bucket = "platform-infra"
+    bucket = "wellcomecollection-platform-infra"
     key    = "terraform/shared_infra.tfstate"
     region = "eu-west-1"
   }
