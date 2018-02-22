cf464811be4becfbe58ddbd2f6f13a901b1697fa
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-02-22 17:03:19 +0000

    gatling_to_cloudwatch: fix TF parameter name

--- monitoring/load_test/gatling_to_cloudwatch/main.tf
+++ monitoring/load_test/gatling_to_cloudwatch/main.tf
@@ -1,14 +1,14 @@
 module "lambda_gatling_to_cloudwatch" {
   source = "git::https://github.com/wellcometrust/terraform.git//lambda?ref=v1.0.0"
-  s3_key = "lambdas/monitoring/load_test/gatling_to_cloudwatch.zip"
+
+  s3_bucket = "${var.infra_bucket}"
+  s3_key    = "lambdas/monitoring/load_test/gatling_to_cloudwatch.zip"
 
   name        = "gatling_to_cloudwatch"
   description = "Record gatling results as CloudWatch metrics"
   timeout     = 5
 
   alarm_topic_arn = "${var.lambda_error_alarm_arn}"
-
-  infra_bucket = "${var.infra_bucket}"
 }
 
 module "trigger_gatling_to_cloudwatch" {
