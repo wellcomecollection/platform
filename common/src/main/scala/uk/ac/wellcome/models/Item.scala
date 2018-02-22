7e73cb044236339a66bcbf237c182177dacee174
caylaa <alicefuzier@gmail.com>
2018-02-22 11:38:38 +0000

    Splitting Item into IdentifiedItem and UnidentifiedItem

--- common/src/main/scala/uk/ac/wellcome/models/Item.scala
+++ common/src/main/scala/uk/ac/wellcome/models/Item.scala
@@ -1,10 +1,26 @@
 package uk.ac.wellcome.models
 
-case class Item(
-  canonicalId: Option[String] = None,
+sealed trait Item extends Identifiable{
+val  sourceIdentifier: SourceIdentifier
+val  identifiers: List[SourceIdentifier]
+val  locations: List[Location]
+val  visible: Boolean
+val  ontologyType: String
+}
+
+case class UnidentifiedItem(
   sourceIdentifier: SourceIdentifier,
   identifiers: List[SourceIdentifier] = Nil,
   locations: List[Location] = List(),
   visible: Boolean = true,
   ontologyType: String = "Item"
-) extends Identifiable
+) extends Identifiable with Item
+
+case class IdentifiedItem(
+  canonicalId: String,
+  sourceIdentifier: SourceIdentifier,
+  identifiers: List[SourceIdentifier] = Nil,
+  locations: List[Location] = List(),
+  visible: Boolean = true,
+  ontologyType: String = "Item"
+) extends Identifiable with Item
