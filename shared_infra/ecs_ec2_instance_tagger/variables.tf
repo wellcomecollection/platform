e18a3bd82394b57a36c0a0ca7c1eea5cf7b86b66
Alex Chan <a.chan@wellcome.ac.uk>
2018-02-22 16:46:31 +0000

    shared_infra: get Lambda bundles from the new bucket

--- shared_infra/ecs_ec2_instance_tagger/variables.tf
+++ shared_infra/ecs_ec2_instance_tagger/variables.tf
@@ -3,3 +3,5 @@ variable "lambda_error_alarm_arn" {}
 variable "ecs_container_instance_state_change_arn" {}
 variable "ecs_container_instance_state_change_name" {}
 variable "bucket_infra_arn" {}
+
+variable "infra_bucket" {}
--- shared_infra/update_ecs_service_size/variables.tf
+++ shared_infra/update_ecs_service_size/variables.tf
@@ -1,2 +1,4 @@
 variable "lambda_error_alarm_arn" {}
 variable "service_scheduler_topic_arn" {}
+
+variable "infra_bucket" {}
