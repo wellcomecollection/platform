273d21123c733cf4493954808b6bfa88938476a5
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-02-22 11:25:56 +0000

    Split Work into Unidentifiedwork and IdentifiedWork

--- common/src/main/scala/uk/ac/wellcome/models/Identifiable.scala
+++ common/src/main/scala/uk/ac/wellcome/models/Identifiable.scala
@@ -1,13 +1,6 @@
 package uk.ac.wellcome.models
 
-case class UnidentifiableException()
-    extends Exception("Can't find canonicalId for this Identifiable")
-
 trait Identifiable {
-  val canonicalId: Option[String]
-  def id: String = canonicalId.getOrElse(
-    throw UnidentifiableException()
-  )
   val sourceIdentifier: SourceIdentifier
   val ontologyType: String
 }
---
+++ common/src/test/scala/uk/ac/wellcome/models/IdentifiedWorkTest.scala
@@ -0,0 +1,160 @@
+package uk.ac.wellcome.models
+
+import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.utils.JsonUtil._
+import uk.ac.wellcome.test.utils.JsonTestUtil
+
+class IdentifiedWorkTest extends FunSpec with Matchers with JsonTestUtil {
+
+  private val license_CCBYJson =
+    s"""{
+            "licenseType": "${License_CCBY.licenseType}",
+            "label": "${License_CCBY.label}",
+            "url": "${License_CCBY.url}",
+            "type": "License"
+          }"""
+
+  val identifiedWorkJson: String =
+    s"""
+      |{
+      |  "title": "title",
+      |  "sourceIdentifier": {
+      |    "identifierScheme": "${IdentifierSchemes.miroImageNumber.toString}",
+      |    "value": "value"
+      |  },
+      |  "version": 1,
+      |  "identifiers": [
+      |    {
+      |      "identifierScheme": "${IdentifierSchemes.miroImageNumber.toString}",
+      |      "value": "value"
+      |    }
+      |  ],
+      |  "canonicalId": "canonicalId",
+      |  "description": "description",
+      |  "lettering": "lettering",
+      |  "createdDate": {
+      |    "label": "period",
+      |    "type": "Period"
+      |  },
+      |  "subjects": [
+      |    {
+      |      "label": "subject",
+      |      "type": "Concept"
+      |    }
+      |  ],
+      |  "creators": [
+      |    {
+      |      "label": "47",
+      |      "type": "Agent"
+      |    }
+      |  ],
+      |  "genres": [
+      |    {
+      |      "label": "genre",
+      |      "type": "Concept"
+      |    }
+      |  ],
+      |  "thumbnail": {
+      |    "locationType": "location",
+      |    "url" : "",
+      |    "credit" : null,
+      |    "license": $license_CCBYJson,
+      |    "type": "DigitalLocation"
+      |  },
+      |  "items": [
+      |    {
+      |      "canonicalId": "canonicalId",
+      |      "sourceIdentifier": {
+      |        "identifierScheme": "${IdentifierSchemes.miroImageNumber.toString}",
+      |        "value": "value"
+      |      },
+      |      "identifiers": [
+      |        {
+      |          "identifierScheme": "${IdentifierSchemes.miroImageNumber.toString}",
+      |          "value": "value"
+      |        }
+      |      ],
+      |      "locations": [
+      |        {
+      |          "locationType": "location",
+      |          "url" : "",
+      |          "credit" : null,
+      |          "license": $license_CCBYJson,
+      |          "type": "DigitalLocation"
+      |        }
+      |      ],
+      |      "visible":true,
+      |      "type": "Item"
+      |    }
+      |  ],
+      |  "publishers": [
+      |    {
+      |      "label": "MIT Press",
+      |      "type": "Organisation"
+      |    }
+      |  ],
+      |  "visible":true,
+      |  "type": "Work"
+      |}
+    """.stripMargin
+
+  val location = DigitalLocation(
+    locationType = "location",
+    url = "",
+    license = License_CCBY
+  )
+
+  val identifier = SourceIdentifier(
+    identifierScheme = IdentifierSchemes.miroImageNumber,
+    value = "value"
+  )
+
+  val item = Item(
+    canonicalId = Some("canonicalId"),
+    sourceIdentifier = identifier,
+    identifiers = List(identifier),
+    locations = List(location)
+  )
+
+  val publisher = Organisation(
+    label = "MIT Press"
+  )
+
+  val publishers = List(publisher)
+  val identifiedWork = IdentifiedWork(
+    canonicalId = "canonicalId",
+    title = Some("title"),
+    sourceIdentifier = identifier,
+    version = 1,
+    identifiers = List(identifier),
+    description = Some("description"),
+    lettering = Some("lettering"),
+    createdDate = Some(Period("period")),
+    subjects = List(Concept("subject")),
+    creators = List(Agent("47")),
+    genres = List(Concept("genre")),
+    thumbnail = Some(location),
+    items = List(item),
+    publishers = publishers
+  )
+
+  it("should serialise an identified Item as Work") {
+    val result = toJson(identifiedWork)
+
+    result.isSuccess shouldBe true
+    assertJsonStringsAreEqual(result.get, identifiedWorkJson)
+  }
+
+  it("should deserialize a JSON string as a identified Item") {
+    val result = fromJson[IdentifiedWork](identifiedWorkJson)
+
+    result.isSuccess shouldBe true
+    result.get shouldBe identifiedWork
+  }
+
+  it("should have an ontology type 'Work' when serialised to JSON") {
+    val jsonString = toJson(identifiedWork).get
+
+    jsonString.contains("""type":"Work"""") should be(true)
+  }
+}
