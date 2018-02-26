3aaabf5c93cf2c5a7c10e06fee77f727ca9139f7
caylaa <alicefuzier@gmail.com>
2018-02-26 12:38:02 +0000

    Adding test to assert that annotated fields get copied across in dynamo

--- common/src/test/scala/uk/ac/wellcome/storage/VersionedHybridStoreLocal.scala
+++ common/src/test/scala/uk/ac/wellcome/storage/VersionedHybridStoreLocal.scala
@@ -1,40 +1,36 @@
 package uk.ac.wellcome.storage
 
+import com.gu.scanamo.Scanamo
 import com.gu.scanamo.syntax._
-import com.gu.scanamo.{DynamoFormat, Scanamo}
 import org.scalatest.Suite
 import uk.ac.wellcome.dynamo.VersionedDao
 import uk.ac.wellcome.locals.DynamoDBLocal
-import uk.ac.wellcome.models.{Id, Sourced}
+import uk.ac.wellcome.models.Id
 import uk.ac.wellcome.models.aws.DynamoConfig
 import uk.ac.wellcome.s3.{KeyPrefixGenerator, S3ObjectStore}
 import uk.ac.wellcome.test.utils.{ExtendedPatience, JsonTestUtil, S3Local}
 
-trait VersionedHybridStoreLocal[T <: Id]
+trait VersionedHybridStoreLocal
     extends DynamoDBLocal[HybridRecord]
     with S3Local
     with JsonTestUtil
     with ExtendedPatience { this: Suite =>
 
-  override lazy val evidence: DynamoFormat[HybridRecord] =
-    DynamoFormat[HybridRecord]
-
-  val keyPrefixGenerator: KeyPrefixGenerator[T]
-
-  val hybridStore = new VersionedHybridStore[T](
+  def createHybridStore[T <: Id] = new VersionedHybridStore[T](
     sourcedObjectStore = new S3ObjectStore(
       s3Client = s3Client,
       bucketName = bucketName,
-      keyPrefixGenerator = keyPrefixGenerator
+      keyPrefixGenerator = new KeyPrefixGenerator[T] {
+        override def generate(obj: T): String = "/"
+      }
     ),
-    versionedDao = new VersionedDao(
-      dynamoDbClient = dynamoDbClient,
+    versionedDao = new VersionedDao(dynamoDbClient = dynamoDbClient,
       dynamoConfig = DynamoConfig(
         table = tableName
       ))
   )
 
-  def assertHybridRecordIsStoredCorrectly(record: T, expectedJson: String) = {
+  def assertHybridRecordIsStoredCorrectly[T <: Id](record: T, expectedJson: String) = {
     val dynamoRecord = Scanamo
       .get[HybridRecord](dynamoDbClient)(tableName)('id -> record.id)
       .get
