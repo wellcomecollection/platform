ea5a96f0f29e1f20a942a6f189657de7731fddc5
Alex Chan <alex@alexwlchan.net>
2018-02-27 10:08:50 +0000

    SierraItems: move the JSON parsing onto the trait, test it

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraItems.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraItems.scala
@@ -1,3 +1,27 @@
 package uk.ac.wellcome.transformer.transformers.sierra
 
-trait SierraItems { }
+import com.twitter.inject.Logging
+import scala.util.{Failure, Success}
+import uk.ac.wellcome.models.transformable.SierraTransformable
+import uk.ac.wellcome.transformer.source.SierraItemData
+import uk.ac.wellcome.utils.JsonUtil._
+
+trait SierraItems extends Logging {
+  def extractItemData(sierraTransformable: SierraTransformable): List[SierraItemData] = {
+    sierraTransformable
+      .itemData
+      .values
+      .map { _.data }
+      .map { jsonString =>
+        fromJson[SierraItemData](jsonString) match {
+          case Success(d) => Some(d)
+          case Failure(e) => {
+            error(s"Failed to parse item!", e)
+            None
+          }
+        }
+      }
+      .toList
+      .flatten
+  }
+}
