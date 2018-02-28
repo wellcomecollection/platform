a6f0157bbf2ff5d1c03105c954cb9d342493e845
Alex Chan <alex@alexwlchan.net>
2018-02-28 15:35:25 +0000

    SierraPublicationDate: clarify notes, and add appropriate tests

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraPublicationDate.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraPublicationDate.scala
@@ -20,9 +20,23 @@ trait SierraPublicationDate {
   // the string directly -- we're not doing any parsing of the date yet.
   //
   // Notes:
-  //  - Both the MARC field 260 and subfield $c are repeatable.  For now
-  //    we just grab them all and join them with semicolons.
+  //
+  //  - MARC field 260 and subfield $c are both labelled "R" (repeatable).
+  //    However, the notes for subfield $c say:
+  //
+  //        Only one subfield $c may be recorded in multiple publishing
+  //        statements of a record.
+  //
+  //    Because it's unclear, for now we just take them all and join them
+  //    with semicolons.
+  //
   //    TODO: Decide a proper strategy for doing this!
+  //
+  //  - This field may contain punctuation based on surrounding MARC fields,
+  //    or which explains exactly what the date means.  For now, we treat
+  //    this as entirely opaque, and just pipe through the raw string.
+  //
+  //    TODO: Parse it properly!
   //
   // https://www.loc.gov/marc/bibliographic/bd260.html
   //
--- catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraPublicationDateTest.scala
+++ catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraPublicationDateTest.scala
@@ -13,7 +13,7 @@ class SierraPublicationDateTest extends FunSpec with Matchers with SierraData {
 
   val transformer = new SierraPublicationDate {}
 
-  it("extracts publication date from MARC field 260 with subfield c") {
+  it("extracts publication date from MARC field 260 with subfield $c") {
     val varFields = List(
       VarField(
         fieldTag = "?",
@@ -30,12 +30,80 @@ class SierraPublicationDateTest extends FunSpec with Matchers with SierraData {
     )
 
     val bibData = SierraBibData(
-      id = "b1234567",
-      title = Some("A xylophone full of xenon."),
+      id = "r1000001",
+      title = Some("A red rhinoceros riding a railroad"),
       varFields = varFields
     )
 
-    transformer.getPublicationDate(bibData) shouldBe Some(Period("1923."))
+    transformer.getPublicationDate(bibData).get shouldBe Period("1923.")
+  }
+
+  it("ignores other non-$c subfields") {
+    val varFields = List(
+      VarField(
+        fieldTag = "?",
+        marcTag = "260",
+        indicator1 = " ",
+        indicator2 = " ",
+        subfields = List(
+          MarcSubfield(tag = "a", content = "Paris ;"),
+          MarcSubfield(tag = "a", content = "New York :"),
+          MarcSubfield(tag = "b", content = "Vogue,"),
+          MarcSubfield(tag = "c", content = "1964-")
+        )
+      )
+    )
+
+    val bibData = SierraBibData(
+      id = "r2000002",
+      title = Some("Rude ribbets from riled reptiles"),
+      varFields = varFields
+    )
+
+    transformer.getPublicationDate(bibData).get shouldBe Period("1964-")
+  }
+
+  it("concatenates values from multiple 260 fields or multiple $c subfields") {
+    val varFields = List(
+      VarField(
+        fieldTag = "?",
+        marcTag = "260",
+        indicator1 = " ",
+        indicator2 = " ",
+        subfields = List(
+          MarcSubfield(tag = "a", content = "Victoria, B. C. :"),
+          MarcSubfield(tag = "c", content = "1898-1945.")
+        )
+      ),
+      VarField(
+        fieldTag = "?",
+        marcTag = "261",
+        indicator1 = " ",
+        indicator2 = " ",
+        subfields = List()
+      ),
+      VarField(
+        fieldTag = "?",
+        marcTag = "260",
+        indicator1 = " ",
+        indicator2 = " ",
+        subfields = List(
+          MarcSubfield(tag = "a", content = "[New York] :"),
+          MarcSubfield(tag = "b", content = "American Statistical Association,"),
+          MarcSubfield(tag = "c", content = "1975.")
+          MarcSubfield(tag = "c", content = "1976.")
+        )
+      )
+    )
+
+    val bibData = SierraBibData(
+      id = "r3000003",
+      title = Some("Running riot with rovers is rarely risible"),
+      varFields = varFields
+    )
+
+    transformer.getPublicationDate(bibData).get shouldBe Period(
+      "1898-1945.; 1975.; 1976."))
   }
 
 }
