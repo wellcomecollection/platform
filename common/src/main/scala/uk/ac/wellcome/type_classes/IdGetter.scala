f32ffeebe6ad43000c195b40367425857c57619f
caylaa <alicefuzier@gmail.com>
2018-02-28 15:59:07 +0000

    Comments

--- common/src/main/scala/uk/ac/wellcome/type_classes/IdGetter.scala
+++ common/src/main/scala/uk/ac/wellcome/type_classes/IdGetter.scala
@@ -5,14 +5,16 @@ import shapeless.labelled.FieldType
 import shapeless.ops.record.Selector
 import shapeless.record._
 
-
+// Type class that returns the id of an instances of T
 trait IdGetter[T] {
   def id(t: T): String
 }
 
 object IdGetter {
   val w = Witness(Symbol("id"))
-  type id = w.T
+  type IdKey = w.T
+
+  type IdField = FieldType[IdKey, String]
 
   def apply[A](implicit enc: IdGetter[A]): IdGetter[A] =
     enc
@@ -21,14 +23,17 @@ object IdGetter {
     def id(t: T) = f(t)
   }
 
-  implicit def elementIdGetter[L <: HList](implicit selector: Selector.Aux[FieldType[id, String] :: L, id, String]) = createIdGetter {t: (FieldType[id, String] :: L) =>
+  // Generates an IdGetter for an HList of arbitrary length with the id field as the first field
+  implicit def elementIdGetter[L <: HList](implicit selector: Selector.Aux[IdField :: L, IdKey, String]) = createIdGetter {t: IdField :: L =>
     t.get('id)
   }
 
+  // Generates an IdGetter for an HList returning the IdGetter for the tail of the HList
   implicit def hlistIdGetter[H, T <: HList](implicit idGetter: IdGetter[T]) = createIdGetter[H::T]{t: (H::T)=>
     idGetter.id(t.tail)
   }
 
+  // Generates an IdGetter for a case class using the IdGetter for its HLists representation
   implicit def productIdGetter[C, T](implicit labelledGeneric: LabelledGeneric.Aux[C,T],idGetter: IdGetter[T]) = createIdGetter[C]{t: C =>
     idGetter.id(labelledGeneric.to(t))
   }
