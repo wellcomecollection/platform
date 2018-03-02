b121e51419576fd76d28530883fe1863fdfb8de7
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-02 11:04:09 +0000

    SierraPhysicalDescription.scala: actually implement the MARC parsing

---
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraPhysicalDescription.scala
@@ -0,0 +1,42 @@
+package uk.ac.wellcome.transformer.transformers.sierra
+
+import uk.ac.wellcome.transformer.source.{MarcSubfield, SierraBibData}
+
+trait SierraPublicationDate {
+
+  // Populate wwork:physicalDescription.
+  //
+  // We use MARC field 300 and subfield $b.
+  //
+  // Notes:
+  //
+  //  - MARC field 300 and subfield $b are both labelled "R" (repeatable).
+  //    According to Branwen, this field does appear multiple times on some
+  //    of our records -- not usually on books, but on some of the moving image
+  //    & sound records.
+  //
+  //  - So far we don't do any stripping of punctuation, and if multiple
+  //    subfields are found on a record, I'm just joining them with newlines.
+  //
+  //    TODO: Decide a proper strategy for joining multiple physical
+  //    descriptions!
+  //
+  // https://www.loc.gov/marc/bibliographic/bd300.html
+  //
+  def getPhysicalDescription(bibData: SierraBibData): Option[String] = {
+    val matchingSubfields = bibData.varFields
+      .filter { _.marcTag == Some("300") }
+      .map { _.subfields }
+      .flatten
+      .filter { _.tag == "b" }
+
+    if (matchingSubfields.isEmpty) {
+      None
+    } else {
+      val label = matchingSubfields
+        .map { _.content }
+        .mkString("\n\n")
+      Some(label)
+    }
+  }
+}
