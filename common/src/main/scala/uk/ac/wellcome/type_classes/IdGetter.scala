aa3e8f532a3964ee01e5f00f3c9636e90fb352ac
caylaa <alicefuzier@gmail.com>
2018-03-06 15:34:29 +0000

    Simplify generation of IdGetter and VersionGetter

--- common/src/main/scala/uk/ac/wellcome/type_classes/IdGetter.scala
+++ common/src/main/scala/uk/ac/wellcome/type_classes/IdGetter.scala
@@ -14,8 +14,6 @@ object IdGetter {
   val w = Witness(Symbol("id"))
   type IdKey = w.T
 
-  type IdField = FieldType[IdKey, String]
-
   def apply[A](implicit enc: IdGetter[A]): IdGetter[A] =
     enc
 
@@ -23,17 +21,11 @@ object IdGetter {
     def id(t: T) = f(t)
   }
 
-  // Generates an IdGetter for an HList of arbitrary length with the id field as the first field
-  implicit def elementIdGetter[L <: HList](
-    implicit selector: Selector.Aux[IdField :: L, IdKey, String]) =
-    createIdGetter { t: IdField :: L =>
-      t.get('id)
-    }
-
   // Generates an IdGetter for an HList returning the IdGetter for the tail of the HList
-  implicit def hlistIdGetter[H, T <: HList](implicit idGetter: IdGetter[T]) =
-    createIdGetter[H :: T] { t: (H :: T) =>
-      idGetter.id(t.tail)
+  implicit def hlistIdGetter[L <: HList](
+    implicit selector: Selector.Aux[L, IdKey, String]) =
+    createIdGetter { t: L =>
+      selector(t)
     }
 
   // Generates an IdGetter for a case class using the IdGetter for its HLists representation
