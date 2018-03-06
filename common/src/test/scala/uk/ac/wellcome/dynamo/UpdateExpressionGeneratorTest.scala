9d9e2fbb98bec5b9709ef01425d76c6ccded550f
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-03-06 17:01:16 +0000

    Apply auto-formatting rules

--- common/src/test/scala/uk/ac/wellcome/dynamo/UpdateExpressionGeneratorTest.scala
+++ common/src/test/scala/uk/ac/wellcome/dynamo/UpdateExpressionGeneratorTest.scala
@@ -10,13 +10,16 @@ class UpdateExpressionGeneratorTest extends FunSpec with Matchers {
   case class Example(id: String, data: String, number: Int, date: Instant)
 
   it("generates an update expression for a case class") {
-    val example = Example(id = "111", data = "something", number = 1, date = Instant.now())
+    val example =
+      Example(id = "111", data = "something", number = 1, date = Instant.now())
 
-    val expectedUpdateExpression = set('data -> example.data) and set('number -> example.number) and set('date -> example.date)
+    val expectedUpdateExpression = set('data -> example.data) and set(
+      'number -> example.number) and set('date -> example.date)
 
     val updateExpressionGenerator = UpdateExpressionGenerator[Example]
 
-    updateExpressionGenerator.generateUpdateExpression(example) shouldBe Some(expectedUpdateExpression)
+    updateExpressionGenerator.generateUpdateExpression(example) shouldBe Some(
+      expectedUpdateExpression)
   }
 
   case class EmptyExample(id: String)
