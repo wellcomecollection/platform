53c8ecc9eae03b05d84ea1c716fe7a059f0a9af8
Alex Chan <a.chan@wellcome.ac.uk>
2018-03-07 17:11:42 +0000

    SierraReaderWorkerService.scala: fix a broken import

--- sierra_adapter/sierra_reader/src/main/scala/uk/ac/wellcome/platform/sierra_reader/services/SierraReaderWorkerService.scala
+++ sierra_adapter/sierra_reader/src/main/scala/uk/ac/wellcome/platform/sierra_reader/services/SierraReaderWorkerService.scala
@@ -8,10 +8,8 @@ import com.google.inject.Inject
 import com.twitter.inject.annotations.Flag
 import uk.ac.wellcome.metrics.MetricsSender
 import uk.ac.wellcome.models.aws.SQSMessage
-import uk.ac.wellcome.platform.sierra_reader.flow.{
-  SierraRecordWrapperFlow,
-  SierraResourceTypes
-}
+import uk.ac.wellcome.platform.sierra_reader.flow.SierraRecordWrapperFlow
+import uk.ac.wellcome.platform.sierra_reader.models.SierraResourceTypes
 import uk.ac.wellcome.sierra.{SierraSource, ThrottleRate}
 import uk.ac.wellcome.sqs.{SQSReader, SQSWorker}
 import uk.ac.wellcome.sierra_adapter.services.WindowExtractor
