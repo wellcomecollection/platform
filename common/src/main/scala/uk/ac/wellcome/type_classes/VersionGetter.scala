83dbd0c1b64bc47d7a366cc55a5a4dda6dbb21f7
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-07 13:37:23 +0000

    VersionGetter.scala: add some explanatory comments

--- common/src/main/scala/uk/ac/wellcome/type_classes/VersionGetter.scala
+++ common/src/main/scala/uk/ac/wellcome/type_classes/VersionGetter.scala
@@ -5,7 +5,13 @@ import shapeless.labelled.FieldType
 import shapeless.ops.record.Selector
 import shapeless.record._
 
-// Type class that returns the version of an instances of T
+// Type class with a method for getting the version of an instance of T.
+//
+// The type T must have a field "version".
+//
+// This isn't defined by the type hierarchy in the trait; instead it's checked
+// at compile-time by shapeless and the companion object.
+//
 trait VersionGetter[T] {
   def version(t: T): Int
 }
@@ -22,17 +28,31 @@ object VersionGetter {
       def version(t: T) = f(t)
     }
 
-  // Generates an VersionGetter for an HList returning the VersionGetter for the tail of the HList
+  // Generates an VersionGetter for an HList.
+  //
+  // Selector is a Shapeless type class that selects an individual field
+  // from an HList.  It takes three type parameters:
+  //
+  //  - the type of the input (L)
+  //  - the field to select (version)
+  //  - the type of the returned value (Int)
+  //
   implicit def hlistVersionGetter[L <: HList](
     implicit selector: Selector.Aux[L, version, Int]) = createVersionGetter {
     t: L =>
       selector(t)
   }
 
-  // Generates an VersionGetter for a case class using the VersionGetter for its HLists representation
-  implicit def productVersionGetter[C, T](
-    implicit labelledGeneric: LabelledGeneric.Aux[C, T],
-    versionGetter: VersionGetter[T]) = createVersionGetter[C] { t: C =>
-    versionGetter.version(labelledGeneric.to(t))
+  // Generates an VersionGetter for a case class.
+  //
+  // LabelledGeneric is a Shapeless type class that allows us to convert
+  // between case classes and their HList representation.
+  //
+  // labelledGeneric.to(c) converts the case class C to an HList L, and then we
+  // can use the constructor above.
+  implicit def productVersionGetter[C, L](
+    implicit labelledGeneric: LabelledGeneric.Aux[C, L],
+    versionGetter: VersionGetter[L]) = createVersionGetter[C] { c: C =>
+    versionGetter.version(labelledGeneric.to(c))
   }
 }
