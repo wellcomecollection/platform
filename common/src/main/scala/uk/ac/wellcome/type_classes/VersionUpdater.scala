5a533cac44aa26aa81d4fefbf087e783119b0e94
Alex Chan <a.chan@wellcome.ac.uk>
2018-03-07 13:28:22 +0000

    VersionUpdater.scala: add some notes on the Shapeless features

--- common/src/main/scala/uk/ac/wellcome/type_classes/VersionUpdater.scala
+++ common/src/main/scala/uk/ac/wellcome/type_classes/VersionUpdater.scala
@@ -5,7 +5,13 @@ import shapeless.labelled.FieldType
 import shapeless.ops.record.Updater
 import shapeless.record._
 
-// Type class that updates the version field on an instance of T
+// Type class with a method for updating the version on an instance of T.
+//
+// The type T must have a field "version".
+//
+// This isn't defined by the type hierarchy in the trait; instead it's checked
+// at compile-time by shapeless and the companion object.
+//
 trait VersionUpdater[T] {
   def updateVersion(versioned: T, newVersion: Int): T
 }
@@ -24,14 +30,27 @@ object VersionUpdater {
       def updateVersion(t: T, version: Int) = f(t, version)
     }
 
-  // Generates a VersionUpdater for an arbitrary HList
+  // Generates a VersionUpdater for an arbitrary HList.
+  //
+  // Updater is a Shapeless type class that can update fields on an HList
+  // (a "record" in Shapeless terminology).
+  //
+  // In this case, it takes L as an input, modifies versionField, and returns
+  // another instance of L.  This means we can only create a VersionUpdater
+  // for HLists that already have a "version" field,
   implicit def hlistVersionUpdater[L <: HList](
     implicit updater: Updater.Aux[L, versionField, L]) =
     createVersionUpdater[L] { (t: L, newVersion: Int) =>
       t.updated('version, newVersion)
     }
 
-  // Generates an VersionUpdater for a case class using the VersionUpdater for its HLists representation
+  // Generates an VersionUpdater for a case class.
+  //
+  // LabelledGeneric is a Shapeless type class that allows us to convert
+  // between case classes and their HList representation.
+  //
+  // gen.to(t) converts the case class to an HList, and gen.from(...) goes
+  // in the other direction.
   implicit def productVersionUpdater[C, L](
     implicit gen: LabelledGeneric.Aux[C, L],
     versionUpdater: VersionUpdater[L]) = createVersionUpdater[C] {
