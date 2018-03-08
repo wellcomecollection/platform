9cc31fb39a86f975ceed83a28661384e73547a6c
Alex Chan <a.chan@wellcome.ac.uk>
2018-03-08 10:11:38 +0000

    sierra_bib_merger: add AWS credential flags for ServerTest

--- sierra_adapter/sierra_bib_merger/src/test/scala/uk/ac/wellcome/platform/sierra_bib_merger/ServerTest.scala
+++ sierra_adapter/sierra_bib_merger/src/test/scala/uk/ac/wellcome/platform/sierra_bib_merger/ServerTest.scala
@@ -3,14 +3,15 @@ package uk.ac.wellcome.platform.sierra_bib_merger
 import com.twitter.finagle.http.Status._
 import com.twitter.finatra.http.EmbeddedHttpServer
 import com.twitter.inject.server.FeatureTest
+import uk.ac.wellcome.test.utils.{AmazonCloudWatchFlag, SQSLocal}
 
-class ServerTest extends FeatureTest {
+class ServerTest extends FeatureTest with AmazonCloudWatchFlag with SQSLocal {
 
   val server = new EmbeddedHttpServer(
     new Server(),
     flags = Map(
       "aws.dynamo.tableName" -> "serverTestTable"
-    )
+    ) ++ sqsLocalFlags ++ cloudWatchLocalEndpointFlag
   )
 
   test("it shows the healthcheck message") {
