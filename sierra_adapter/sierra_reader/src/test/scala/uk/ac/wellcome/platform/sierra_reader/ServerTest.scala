8ce96a415dbfa6f75d82663b9ae94d28eca5138a
Alex Chan <a.chan@wellcome.ac.uk>
2018-03-08 13:33:44 +0000

    sierra_reader: include AWS credentials in ServerTest flags

--- sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/ServerTest.scala
+++ sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/ServerTest.scala
@@ -3,14 +3,21 @@ package uk.ac.wellcome.platform.sierra_reader
 import com.twitter.finagle.http.Status._
 import com.twitter.finatra.http.EmbeddedHttpServer
 import com.twitter.inject.server.FeatureTest
+import uk.ac.wellcome.test.utils.{AmazonCloudWatchFlag, S3Local, SQSLocal}
 
-class ServerTest extends FeatureTest {
+class ServerTest extends FeatureTest
+  with AmazonCloudWatchFlag
+  with S3Local
+  with SQSLocal {
+
+  override lazy val bucketName = "sierra-reader-servertest-bucket"
 
   val server = new EmbeddedHttpServer(
     new Server(),
     Map(
       "reader.resourceType" -> "bibs"
-    ))
+    ) ++ s3LocalFlags ++ sqsLocalFlags ++ cloudWatchLocalEndpointFlag
+  )
 
   test("it shows the healthcheck message") {
     server.httpGet(
