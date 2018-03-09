afe76f5c9a096b28ea3f067aca2cd4dc8f7c8247
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-03-09 15:19:07 +0000

    Apply auto-formatting rules

--- catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/transformer/transformers/SierraTransformableTransformerTest.scala
+++ catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/transformer/transformers/SierraTransformableTransformerTest.scala
@@ -468,7 +468,8 @@ class SierraTransformableTransformerTest
       transformer.transform(sierraTransformable, version = 1)
     transformedSierraRecord.isSuccess shouldBe true
 
-    transformedSierraRecord.get.get.placesOfPublication should contain only Place(label = place)
+    transformedSierraRecord.get.get.placesOfPublication should contain only Place(
+      label = place)
 
   }
 }
