f50555f9eb16530c4067d145d002725aa140e358
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-09 12:58:54 +0000

    Ignore country field for now and read just 260 a

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraPlaceOfPublication.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraPlaceOfPublication.scala
@@ -4,25 +4,14 @@ import uk.ac.wellcome.models._
 import uk.ac.wellcome.transformer.source.{MarcSubfield, SierraBibData, VarField}
 
 trait SierraPlaceOfPublication {
-  def getPlaceOfPublication(
-    bibData: SierraBibData): List[UnidentifiedOrUnidentifieablePlaceOfPublication] = {
-    val unidentifiablePlaceOfPublications = bibData.varFields.collect {
+  def getPlacesOfPublication(
+    bibData: SierraBibData): List[PlaceOfPublication] = {
+    bibData.varFields.collect {
       case VarField(_, None, Some("260"), _, _, subfields) =>
         subfields.collect {
           case MarcSubfield("a", content) =>
-            UnidentifiablePlaceOfPublication(label = content)
+            PlaceOfPublication(label = content)
         }
     }.flatten
-
-    val maybeIdentifiablePlaceOfPublication = bibData.country.map(country => {
-      val sourceIdentifier =
-        SourceIdentifier(IdentifierSchemes.marcCountries, country.code)
-      UnidentifiedPlaceOfPublication(
-        country.name,
-        sourceIdentifier = sourceIdentifier,
-        identifiers = List(sourceIdentifier))
-    })
-
-    unidentifiablePlaceOfPublications ++ maybeIdentifiablePlaceOfPublication.toList
   }
 }
