da42b6dc8bf9721810341f7fe1e02678a33dd9cc
Alex Chan <alex@alexwlchan.net>
2018-03-12 07:33:36 +0000

    sierra_bib_merger: add some more AWS credentials to ServerTest

--- sierra_adapter/sierra_bib_merger/src/test/scala/uk/ac/wellcome/platform/sierra_bib_merger/ServerTest.scala
+++ sierra_adapter/sierra_bib_merger/src/test/scala/uk/ac/wellcome/platform/sierra_bib_merger/ServerTest.scala
@@ -3,15 +3,29 @@ package uk.ac.wellcome.platform.sierra_bib_merger
 import com.twitter.finagle.http.Status._
 import com.twitter.finatra.http.EmbeddedHttpServer
 import com.twitter.inject.server.FeatureTest
-import uk.ac.wellcome.test.utils.{AmazonCloudWatchFlag, SQSLocal}
+import uk.ac.wellcome.test.utils.{
+  AmazonCloudWatchFlag,
+  DynamoDBLocalClients,
+  S3Local,
+  SQSLocal
+}
 
-class ServerTest extends FeatureTest with AmazonCloudWatchFlag with SQSLocal {
+class ServerTest
+    extends FeatureTest
+    with AmazonCloudWatchFlag
+    with DynamoDBLocalClients
+    with S3Local
+    with SQSLocal {
+
+ override lazy val bucketName = "sierra-bib-merger-servertest-bucket"
+ val queueUrl = createQueueAndReturnUrl("sierra-bib-merger-servertest-q")
 
   val server = new EmbeddedHttpServer(
     new Server(),
     flags = Map(
-      "aws.dynamo.tableName" -> "serverTestTable"
-    ) ++ sqsLocalFlags ++ cloudWatchLocalEndpointFlag
+      "aws.dynamo.tableName" -> "serverTestTable",
+       "aws.sqs.queue.url" -> queueUrl
+    ) ++ s3LocalFlags ++ sqsLocalFlags ++ cloudWatchLocalEndpointFlag ++ dynamoDbLocalEndpointFlags
   )
 
   test("it shows the healthcheck message") {
