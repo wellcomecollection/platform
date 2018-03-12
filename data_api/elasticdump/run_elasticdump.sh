48698436e6c38c82737b873e0a04f365db0da72f
Alex Chan <a.chan@wellcome.ac.uk>
2018-03-12 15:16:11 +0000

    elasticdump: implement the main worker script

--- data_api/elasticdump/run_elasticdump.sh
+++ data_api/elasticdump/run_elasticdump.sh
@@ -3,3 +3,38 @@
 set -o errexit
 set -o nounset
 
+sqs_response=$(
+  aws sqs receive-message --queue-url=$sqs_queue_url --max-number-of-messages=1
+)
+
+# The structure of the SQS response is of the form
+#
+#     {
+#       "Messages": [
+#         {
+#           "Body": "{\"foo\": \"bar\"}",
+#           "ReceiptHandle": "AQEB...Xg==",
+#           "MD5OfBody": "9423...8fcf",
+#           "MessageId": "a82...b14"
+#         }
+#       ]
+#     }
+#
+receipt_handle=$(echo "$sqs_response" | jq -r '.Messages' | jq -r '.[0]' | jq -r '.ReceiptHandle')
+
+es_url="https://$es_username:$es_password@$es_name.$es_region.aws.found.io:$es_port/$es_index"
+
+elasticdump \
+  --input="$es_url" \
+  --output=index.json \
+  --type=data
+
+cat index.json | gzip > index.json.gz
+
+aws s3 cp index.json.gz s3://$infra_bucket/elasticdump/$(date +"%s")_$es_index.txt
+
+aws sqs delete-message \
+  --queue-url=$sqs_queue_url \
+  --receipt-handle=$receipt_handle
+
+exit 0
