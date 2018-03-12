b8ad4596b92285205003f7d913cd38415f729269
Alex Chan <a.chan@wellcome.ac.uk>
2018-03-12 11:05:31 +0000

    snapshot_scheduler: implement the mainline functionality

--- data_api/snapshot_scheduler/src/test_snapshot_scheduler.py
+++ data_api/snapshot_scheduler/src/test_snapshot_scheduler.py
@@ -4,6 +4,8 @@ import datetime as dt
 
 import mock
 
+import snapshot_scheduler
+
 
 class patched_datetime(dt.datetime):
     @classmethod
@@ -13,10 +15,10 @@ class patched_datetime(dt.datetime):
 
 @mock.patch('datetime.datetime', patched_datetime)
 def test_writes_message_to_sqs(sns_client, topic_arn):
+    snapshot_scheduler.main(sns_client=sns_client)
+
     messages = sns_client.list_messages()
     assert len(messages) == 1
     assert messages[0][':message'] == {
-        'time': '2011-06-21T00:00:00+00:00',
+        'time': '2011-06-21T00:00:00',
     }
-
-    assert False
