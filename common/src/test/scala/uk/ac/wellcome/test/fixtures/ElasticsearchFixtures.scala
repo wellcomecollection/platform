d56be83b4ab23d9b6ee896112940ca78b1ae5675
Alex Chan <a.chan@wellcome.ac.uk>
2018-03-12 16:43:45 +0000

    ElasticsearchFixtures.scala: naming and tweaks

--- common/src/test/scala/uk/ac/wellcome/test/fixtures/ElasticsearchFixtures.scala
+++ common/src/test/scala/uk/ac/wellcome/test/fixtures/ElasticsearchFixtures.scala
@@ -1,4 +1,4 @@
-package uk.ac.wellcome.platform.ingestor.fixtures
+package uk.ac.wellcome.test.fixtures
 
 import com.sksamuel.elastic4s.http.ElasticDsl._
 import com.sksamuel.elastic4s.http.HttpClient
@@ -8,17 +8,19 @@ import org.scalatest.{Matchers, Suite}
 import org.scalatest.concurrent.{Eventually, ScalaFutures}
 import uk.ac.wellcome.elasticsearch.WorksIndex
 import uk.ac.wellcome.finatra.modules.ElasticCredentials
-import uk.ac.wellcome.test.fixtures.TestWith
-import uk.ac.wellcome.test.utils.ExtendedPatience
+import uk.ac.wellcome.models.IdentifiedWork
+import uk.ac.wellcome.test.utils.{ExtendedPatience, JsonTestUtil}
 import uk.ac.wellcome.utils.GlobalExecutionContext.context
+import uk.ac.wellcome.utils.JsonUtil._
 
 import scala.util.{Random, Try}
 
-trait ElasticSearchIndex
+trait ElasticsearchFixtures
     extends Eventually
     with ExtendedPatience
     with ScalaFutures
-    with Matchers { this: Suite =>
+    with Matchers
+    with JsonTestUtil { this: Suite =>
 
   private val esHost = "localhost"
   private val esPort = "9200"
@@ -42,9 +44,7 @@ trait ElasticSearchIndex
     elasticClient.execute(clusterHealth()).await.numberOfNodes shouldBe 1
   }
 
-  def withLocalElasticIndex[R](testWith: TestWith[String, R]) = {
-    val indexName = Random.alphanumeric take 10 mkString
-    val itemType = "works"
+  def withLocalElasticsearchIndex[R](indexName: String, itemType: String)(testWith: TestWith[String, R]) = {
     val index = new WorksIndex(
       client = elasticClient,
       name = indexName,
@@ -59,6 +59,21 @@ trait ElasticSearchIndex
       testWith(indexName)
     } finally {
       Try { elasticClient.execute(deleteIndex(indexName)) }
+    }
+  }
+
+  def assertElasticsearchEventuallyHasWork(work: IdentifiedWork, indexName: String, itemType: String) = {
+    val workJson = toJson(work).get
+
+    eventually {
+      val hits = elasticClient
+        .execute(search(s"$indexName/$itemType").matchAllQuery().limit(100))
+        .map { _.hits.hits }
+        .await
+
+      hits should have size 1
+
+      assertJsonStringsAreEqual(hits.head.sourceAsString, workJson)
     }
   }
 }
