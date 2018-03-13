946b456a9ebc17e140b1b1f2ac3fa951495ea8b9
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-13 12:04:39 +0000

    Adds snapshot_convertor skeleton project

---
+++ data_api/snapshot_convertor/src/main/scala/uk/ac/wellcome/platform/snapshot_convertor/modules/SnapshotConvertorWorkerModule.scala
@@ -0,0 +1,25 @@
+package uk.ac.wellcome.platform.snapshot_convertor.modules
+
+import akka.actor.ActorSystem
+import com.twitter.inject.{Injector, TwitterModule}
+import uk.ac.wellcome.platform.snapshot_convertor.services.SnapshotConvertorWorkerService
+
+object SnapshotConvertorWorkerModule extends TwitterModule {
+
+  // eagerly load worker service
+  override def singletonStartup(injector: Injector) {
+    super.singletonStartup(injector)
+    injector.instance[SnapshotConvertorWorkerService]
+  }
+
+  override def singletonShutdown(injector: Injector) {
+    info("Terminating SQS worker")
+
+    val system = injector.instance[ActorSystem]
+    val workerService = injector.instance[ReindexWorkerService]
+
+    workerService.stop()
+    system.terminate()
+  }
+
+}
---
+++ data_api/snapshot_convertor/src/test/scala/uk/ac/wellcome/platform/snapshot_convertor/SnapshotConvertorFeatureTest.scala
@@ -0,0 +1,56 @@
+package uk.ac.wellcome.platform.snapshot_convertor
+
+import com.gu.scanamo.{DynamoFormat, Scanamo}
+import com.twitter.finatra.http.EmbeddedHttpServer
+import org.scalatest.concurrent.{Eventually, ScalaFutures}
+import org.scalatest.Matchers
+import org.scalatest.FunSpec
+import uk.ac.wellcome.test.fixtures._
+import scala.collection.JavaConversions._
+
+class SnapshotConvertorFeatureTest
+    extends FunSpec
+    with Matchers
+    with Eventually
+    with AmazonCloudWatchFlag
+    with SqsFixtures
+    with SnsFixtures
+    with S3
+    with ScalaFutures {
+
+  def withServer[R](queueUrl: String, topicArn: String, bucketName: String)(
+    testWith: TestWith[EmbeddedHttpServer, R]) = {
+    val server: EmbeddedHttpServer =
+      new EmbeddedHttpServer(
+        new Server(),
+        flags = Map(
+          "aws.s3.bucketName" -> bucketName,
+          "aws.region" -> "eu-west-1",
+          "aws.sns.topic.arn" -> topicArn,
+          "aws.sqs.queue.url" -> queueUrl
+        ) ++ snsLocalFlags ++ cloudWatchLocalEndpointFlag ++ sqsLocalFlags ++ s3LocalFlags
+      )
+
+    server.start()
+
+    try {
+      testWith(server)
+    } finally {
+      server.close()
+    }
+  }
+
+  it("does not fail") {
+    withLocalSqsQueue { queueUrl =>
+      withLocalSnsTopic { topicArn =>
+        withLocalS3Bucket { bucketname =>
+          withServer(queueUrl, topicArn, bucketName: String) { server =>
+
+          true shouldBe false
+
+          }
+        }
+      }
+    }
+  }
+}
