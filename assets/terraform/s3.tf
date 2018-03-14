ed8fb666743f406930b4976b455e01c64c8eb0c9
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-14 16:09:26 +0000

    assets/s3.tf: add a lifecycle transition to Standard IA (#1681)
    * assets/s3.tf: add a lifecycle transition to Standard IA
    
    * Apply auto-formatting rules

--- assets/terraform/s3.tf
+++ assets/terraform/s3.tf
@@ -5,4 +5,15 @@ resource "aws_s3_bucket" "working_storage" {
   lifecycle {
     prevent_destroy = true
   }
+
+  lifecycle_rule {
+    id = "transition_all_to_standard_ia"
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    enabled = true
+  }
 }
