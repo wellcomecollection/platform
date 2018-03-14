b248818b34868d1ec799144b9349158fe13d8d21
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-14 09:35:19 +0000

    build_windows.py: attempt to explain this script better (#1669)

--- sierra_adapter/build_windows.py
+++ sierra_adapter/build_windows.py
@@ -1,8 +1,37 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 """
-Usage: build_windows.py --start=<START> --end=<END> [--interval=<INTERVAL>] --resource=<RESOURCE>
-       build_windows.py -h | --help
+Build 'update windows' for the Sierra adapter pipeline.
+
+Usage:
+    build_windows.py --start=<START> --end=<END> [--window_length=<WINDOW_LENGTH>] --resource=<RESOURCE>
+    build_windows.py -h | --help
+
+Options:
+    --start=<START>         When to start polling
+    --end=<END>             The earliest point to stop polling
+    --window_length=<WINDOW_LENGTH>
+                            The number of minutes per window
+    --resource=<RESOURCE>   Should the pipeline fetch bibs or items?
+
+
+This script generates windows to poll for updates from Sierra, and sends
+them to the SNS topic that triggers our pipeline.  You pass it an interval
+(start, end), and then it generates windows of length ``window_length`` that
+cover the interval.
+
+For example, calling the script with arguments
+
+    --start='10:00' --end='10:59' --window_length=15 --resource=bibs
+
+which generate four windows:
+
+    ( 10:00 ------------------------------------------------------ 11:00 )
+    | 10:00 -- 10:15 |
+                     | 10:15 -- 10:30 |
+                                      | 10:30 -- 10:45 |
+                                                       | 10:45 --- 11:00 |
+
 """
 
 import datetime as dt
@@ -18,7 +47,7 @@ args = docopt.docopt(__doc__)
 
 start = maya.parse(args['--start']).datetime()
 end = maya.parse(args['--end']).datetime()
-minutes = int(args['--interval'] or 30)
+minutes = int(args['--window_length'] or 30)
 resource = args['--resource']
 
 assert resource in ('bibs', 'items')
