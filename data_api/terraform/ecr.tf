8adc07a02973b1cdcfe061710ebf507701f11ae4
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-14 12:33:06 +0000

    data_api: put the elasticdump service in the data_api cluster (#1674)
    * data_api: put the elasticdump service in the data_api cluster
    
    * elasticdump: more Terraform fixes
    
    * iam_role_policy.tf: fix name of policy document

--- data_api/terraform/ecr.tf
+++ data_api/terraform/ecr.tf
@@ -2,3 +2,8 @@ module "ecr_repository_snapshot_convertor" {
   source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
   name   = "snapshot_convertor"
 }
+
+module "ecr_repository_elasticdump" {
+  source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
+  name   = "elasticdump"
+}
