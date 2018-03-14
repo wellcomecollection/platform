239c253d4303c74d013d634c43bc457eb992b50e
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-14 10:58:29 +0000

    SierraPlaceOfPublication.scala: add a minimal comment

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraPlaceOfPublication.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraPlaceOfPublication.scala
@@ -8,6 +8,15 @@ import uk.ac.wellcome.transformer.source.{
 }
 
 trait SierraPlaceOfPublication {
+
+  // Populate wwork:placeOfPublication.
+  //
+  // We use MARC field 260 subfield $a.
+  //
+  // Notes:
+  //  - This field is repeatable, so we need to cope with multiple instances
+  //    of this field.
+  //
   def getPlacesOfPublication(bibData: SierraBibData): List[Place] = {
     bibData.varFields.collect {
       case VarField(_, None, Some("260"), _, _, subfields) =>
