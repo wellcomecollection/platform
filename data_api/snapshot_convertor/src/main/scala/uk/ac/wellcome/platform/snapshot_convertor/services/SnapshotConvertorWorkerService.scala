e5974e9322a694db07ca0c7bd882348e800aed6c
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-16 10:25:55 +0000

    missing import

--- data_api/snapshot_convertor/src/main/scala/uk/ac/wellcome/platform/snapshot_convertor/services/SnapshotConvertorWorkerService.scala
+++ data_api/snapshot_convertor/src/main/scala/uk/ac/wellcome/platform/snapshot_convertor/services/SnapshotConvertorWorkerService.scala
@@ -4,19 +4,13 @@ import akka.actor.ActorSystem
 import com.google.inject.Inject
 import uk.ac.wellcome.metrics.MetricsSender
 import uk.ac.wellcome.models.aws.SQSMessage
-import uk.ac.wellcome.platform.snapshot_convertor.models.ConversionJob
+import uk.ac.wellcome.platform.snapshot_convertor.models.{CompletedConversionJob, ConversionJob}
 import uk.ac.wellcome.sns.SNSWriter
 import uk.ac.wellcome.sqs.{SQSReader, SQSWorker}
 import uk.ac.wellcome.utils.GlobalExecutionContext.context
 import uk.ac.wellcome.utils.JsonUtil._
 
 import scala.concurrent.Future
-
-
-
-
-
-
 
 class SnapshotConvertorWorkerService @Inject()(
   convertorService: ConvertorService,
