37f40c18a297aabc549332bc75895dcfd15f3991
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-16 07:58:47 +0000

    Regarding your previous letter(ing) (#1686)
    * transformer: update the lettering tests for the new transformation
    
    * SierraLettering.scala: use the contents of 246 subfield $a
    
    Rather than a non-existent lettering field!
    
    * SierraBibData.scala: remove the non-existent 'lettering' field
    
    * Apply auto-formatting rules

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/source/SierraBibData.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/source/SierraBibData.scala
@@ -3,7 +3,6 @@ package uk.ac.wellcome.transformer.source
 case class SierraBibData(
   id: String,
   title: Option[String],
-  lettering: Option[String] = None,
   deleted: Boolean = false,
   suppressed: Boolean = false,
   country: Option[Country] = None,
