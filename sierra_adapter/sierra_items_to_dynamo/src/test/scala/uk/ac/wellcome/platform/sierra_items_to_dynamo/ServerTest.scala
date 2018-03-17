6e68ebe9741c28f2a1b7f944cf22c3d7f9f5d6a0
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-17 14:02:37 +0000

    sierra_items_to_dynamo test isolation work

--- sierra_adapter/sierra_items_to_dynamo/src/test/scala/uk/ac/wellcome/platform/sierra_items_to_dynamo/ServerTest.scala
+++ sierra_adapter/sierra_items_to_dynamo/src/test/scala/uk/ac/wellcome/platform/sierra_items_to_dynamo/ServerTest.scala
@@ -1,34 +1,34 @@
 package uk.ac.wellcome.platform.sierra_items_to_dynamo
 
+import com.gu.scanamo.DynamoFormat
 import com.twitter.finagle.http.Status._
-import com.twitter.finatra.http.EmbeddedHttpServer
-import com.twitter.inject.server.FeatureTest
-import uk.ac.wellcome.test.utils.{
-  AmazonCloudWatchFlag,
-  DynamoDBLocalClients,
-  SQSLocal
-}
+import org.scalatest.FunSpec
+import uk.ac.wellcome.models.transformable.sierra.SierraItemRecord
+import uk.ac.wellcome.test.fixtures.{LocalDynamoDb, SqsFixtures}
 
 class ServerTest
-    extends FeatureTest
-    with AmazonCloudWatchFlag
-    with DynamoDBLocalClients
-    with SQSLocal {
+    extends FunSpec
+      with LocalDynamoDb[SierraItemRecord]
+      with SqsFixtures
+      with fixtures.Server {
 
-  val queueUrl = createQueueAndReturnUrl("sierra-item-to-dynamo-servertest-q")
+  override lazy val evidence = DynamoFormat[SierraItemRecord]
 
-  val server = new EmbeddedHttpServer(
-    new Server(),
-    flags = Map(
-      "aws.sqs.queue.url" -> queueUrl
-    ) ++ sqsLocalFlags ++ cloudWatchLocalEndpointFlag ++ dynamoDbLocalEndpointFlags(
-      "serverTestTable")
-  )
+  it("shows the healthcheck message") {
+    withLocalDynamoDbTable { tableName =>
+      withLocalSqsQueue { queueUrl =>
 
-  test("it shows the healthcheck message") {
-    server.httpGet(
-      path = "/management/healthcheck",
-      andExpect = Ok,
-      withJsonBody = """{"message": "ok"}""")
+        val flags = sqsLocalFlags(queueUrl) ++ dynamoDbLocalEndpointFlags(tableName) ++ Map(
+          "aws.dynamo.tableName" -> tableName
+        )
+
+        withServer(flags) { server =>
+          server.httpGet(
+            path = "/management/healthcheck",
+            andExpect = Ok,
+            withJsonBody = """{"message": "ok"}""")
+        }
+      }
+    }
   }
 }
---
+++ sierra_adapter/sierra_items_to_dynamo/src/test/scala/uk/ac/wellcome/platform/sierra_items_to_dynamo/fixtures/Server.scala
@@ -0,0 +1,32 @@
+package uk.ac.wellcome.platform.sierra_items_to_dynamo.fixtures
+
+import com.twitter.finatra.http.EmbeddedHttpServer
+import org.scalatest.Suite
+import uk.ac.wellcome.test.fixtures.TestWith
+import uk.ac.wellcome.test.utils.AmazonCloudWatchFlag
+import uk.ac.wellcome.platform.sierra_items_to_dynamo.{Server => AppServer}
+
+trait Server extends AmazonCloudWatchFlag { this: Suite =>
+  def withServer[R](
+                     flags: Map[String, String],
+                     modifyServer: (EmbeddedHttpServer) => EmbeddedHttpServer = identity
+                   )(testWith: TestWith[EmbeddedHttpServer, R]) = {
+
+    val server: EmbeddedHttpServer = modifyServer(
+      new EmbeddedHttpServer(
+        new AppServer(),
+        flags = Map(
+          "aws.region" -> "localhost"
+        ) ++ flags ++ cloudWatchLocalEndpointFlag
+      )
+    )
+
+    server.start()
+
+    try {
+      testWith(server)
+    } finally {
+      server.close()
+    }
+  }
+}
