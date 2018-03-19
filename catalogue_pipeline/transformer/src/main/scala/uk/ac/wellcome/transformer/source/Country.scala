855865bcbe5a5b00b8d231eba9c63cd19244b42e
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-19 15:22:35 +0000

    Add 'lang' to the Sierra bib data model

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/source/Country.scala
+++
@@ -1,3 +0,0 @@
-package uk.ac.wellcome.transformer.source
-
-case class Country(code: String, name: String)
---
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/source/sierra/Language.scala
@@ -0,0 +1,3 @@
+package uk.ac.wellcome.transformer.source.sierra
+
+case class Country(code: String, name: String)
