92962df28241409bb47922aa6477db58e8c17f35
Alex Chan <a.chan@wellcome.ac.uk>
2018-03-19 11:23:34 +0000

    Use an implicit Indexable instead
    This is the recommendation from the elastic4s README, and reduces some
    of the naming difficulties:
    
    https://github.com/sksamuel/elastic4s#indexing-typeclass

--- sbt_common/elasticsearch/src/main/scala/uk/ac/wellcome/elasticsearch/models/IndexableIdentifiedWork.scala
+++ sbt_common/elasticsearch/src/main/scala/uk/ac/wellcome/elasticsearch/models/IndexableIdentifiedWork.scala
@@ -4,7 +4,7 @@ import com.sksamuel.elastic4s.Indexable
 import uk.ac.wellcome.models.IdentifiedWork
 import uk.ac.wellcome.utils.JsonUtil._
 
-case object IndexableIdentifiedWork extends Indexable[IdentifiedWork] {
+implicit object IdentifiedWorkIndexable extends Indexable[IdentifiedWork] {
   override def json(t: IdentifiedWork): String =
     toJson(t).get
 }
