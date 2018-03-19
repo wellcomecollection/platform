7a9301d42c29e85ee420c164a94220d1defcbb7f
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-03-19 16:10:48 +0000

    Apply auto-formatting rules

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraCreators.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraCreators.scala
@@ -7,15 +7,27 @@ trait SierraCreators extends MarcUtils {
 
   def getCreators(bibData: SierraBibData): List[AbstractAgent] = {
 
-    val persons = getMatchingSubfields(bibData, "100", List("a", "b", "c")).map { subfields =>
-      val name = subfields.collectFirst { case MarcSubfield("a", content) => content }
-      val numeration = subfields.collectFirst { case MarcSubfield("b", content) => content }
-      val prefixes = subfields.collect { case MarcSubfield("c", content) => content }
+    val persons =
+      getMatchingSubfields(bibData, "100", List("a", "b", "c")).map {
+        subfields =>
+          val name = subfields.collectFirst {
+            case MarcSubfield("a", content) => content
+          }
+          val numeration = subfields.collectFirst {
+            case MarcSubfield("b", content) => content
+          }
+          val prefixes = subfields.collect {
+            case MarcSubfield("c", content) => content
+          }
 
-      val prefixString = if(prefixes.isEmpty) None else Some(prefixes.mkString(" "))
+          val prefixString =
+            if (prefixes.isEmpty) None else Some(prefixes.mkString(" "))
 
-      Person(label = name.get, prefix = prefixString, numeration = numeration)
-    }
+          Person(
+            label = name.get,
+            prefix = prefixString,
+            numeration = numeration)
+      }
 
     val organisations = getMatchingSubfields(bibData, "110", "a").map {
       subfields =>
