78751fb3be7fcf2890df443936880d837e3a7e2a
Alex Chan <a.chan@wellcome.ac.uk>
2018-03-19 15:14:12 +0000

    Add a 'language' field to the display models

---
+++ sbt_common/display/src/main/scala/uk/ac/wellcome/display/models/DisplayLanguage.scala
@@ -0,0 +1,28 @@
+package uk.ac.wellcome.display.models
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.annotations.{ApiModel, ApiModelProperty}
+import uk.ac.wellcome.models.Language
+
+@ApiModel(
+  value = "Language",
+  description = "A language recognised as one of those in the ISO 639-2 language codes."
+)
+case class DisplayLanguage(
+  @ApiModelProperty(
+    value = "An ISO 639-2 language code."
+  ) id: String,
+  @ApiModelProperty(
+    value = "The name of a language"
+  ) label: String
+) {
+  @ApiModelProperty(readOnly = true, value = "A type of thing")
+  @JsonProperty("type") val ontologyType: String = "Language"
+}
+
+case object DisplayLanguage {
+  def apply(language: Language): DisplayLanguage = DisplayLanguage(
+    id = language.id,
+    label = language.label
+  )
+}
