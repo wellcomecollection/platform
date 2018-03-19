f7a91e1c465baeeb9550c35521428404bdba97fd
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-19 14:07:58 +0000

    Add test to account for more than one prefix

--- catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraCreatorsTest.scala
+++ catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraCreatorsTest.scala
@@ -57,27 +57,42 @@ class SierraCreatorsTest extends FunSpec with Matchers {
 
     val creators = transformer.getCreators(bibData)
 
-    creators should contain only Person(
-      name = name,
-      prefix = Some(prefix),
-      numeration = Some(numeration))
+    creators should contain only Person(name = name, prefixes = List(prefix), numeration = Some(numeration))
+  }
+
+  it("extracts multiple prefixes from marcTag 100 c"){
+    val name = "Samuel Vines"
+    val prefixes = List("Commander", "His Grace, The Duke of Ankh")
+
+    val prefixSubfields = prefixes.map( prefix => MarcSubfield(tag = "c", content = prefix))
+
+    val subfields =  prefixSubfields :+ MarcSubfield(tag = "a", content = name)
+    val bibData = SierraBibData(
+      id = "1234567",
+      title = None,
+      varFields = List(
+        VarField(
+          fieldTag = "p",
+          marcTag = "100",
+          indicator1 = "",
+          indicator2 = "",
+          subfields = subfields))
+    )
+
+    val transformer = new SierraCreators {}
+
+    val creators = transformer.getCreators(bibData)
+
+    creators should contain only Person(name = name, prefixes = prefixes)
   }
 
 }
 
 trait SierraCreators extends MarcUtils {
-  def getCreators(bibData: SierraBibData): List[AbstractAgent] =
-    getMatchingSubfields(bibData, "100", List("a", "b", "c")).map {
-      subfields =>
-        val name = subfields.collectFirst {
-          case MarcSubfield("a", content) => content
-        }
-        val numeration = subfields.collectFirst {
-          case MarcSubfield("b", content) => content
-        }
-        val prefix = subfields.collectFirst {
-          case MarcSubfield("c", content) => content
-        }
-        Person(name = name.get, prefix = prefix, numeration = numeration)
-    }
+  def getCreators(bibData: SierraBibData): List[AbstractAgent] = getMatchingSubfields(bibData, "100", List("a", "b", "c")).map{subfields =>
+    val name = subfields.collectFirst{ case MarcSubfield("a", content) => content}
+    val numeration = subfields.collectFirst { case MarcSubfield("b", content) => content }
+    val prefixes = subfields.collect { case MarcSubfield("c", content) => content }
+    Person(name = name.get, prefixes = prefixes, numeration = numeration)
+  }
 }
