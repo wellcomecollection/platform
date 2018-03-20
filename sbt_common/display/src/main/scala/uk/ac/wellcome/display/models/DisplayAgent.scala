994856bb9fd6f9c14c1802e43c604cd09ae87fe0
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-03-20 11:00:59 +0000

    Apply auto-formatting rules

--- sbt_common/display/src/main/scala/uk/ac/wellcome/display/models/DisplayAgent.scala
+++ sbt_common/display/src/main/scala/uk/ac/wellcome/display/models/DisplayAgent.scala
@@ -19,11 +19,13 @@ case class DisplayAgent(
 case object DisplayAbstractAgent {
   def apply(agent: AbstractAgent): DisplayAbstractAgent =
     agent match {
-      case a: Agent => DisplayAgent (
-    label = a.label,
-    ontologyType = a.ontologyType
-    )
-      case p: Person => DisplayPerson(p.label, p.prefix, p.numeration, p.ontologyType)
+      case a: Agent =>
+        DisplayAgent(
+          label = a.label,
+          ontologyType = a.ontologyType
+        )
+      case p: Person =>
+        DisplayPerson(p.label, p.prefix, p.numeration, p.ontologyType)
       case o: Organisation => DisplayOrganisation(o.label, o.ontologyType)
 
     }
@@ -32,25 +34,26 @@ case object DisplayAbstractAgent {
 @ApiModel(
   value = "Person"
 )
-case class DisplayPerson(
-                          @ApiModelProperty(
-  value = "The name of the person"
-) label: String,
-                          @ApiModelProperty(
-  dataType = "String",
-  value =
-    "The title of the person"
-)prefix: Option[String]= None,
-                          @ApiModelProperty(
-  dataType = "String",
-  value =
-    "The numerationof the person"
-)numeration: Option[String]= None,
-                          @JsonProperty("type") ontologyType: String = "Person") extends DisplayAbstractAgent
+case class DisplayPerson(@ApiModelProperty(
+                           value = "The name of the person"
+                         ) label: String,
+                         @ApiModelProperty(
+                           dataType = "String",
+                           value = "The title of the person"
+                         ) prefix: Option[String] = None,
+                         @ApiModelProperty(
+                           dataType = "String",
+                           value = "The numerationof the person"
+                         ) numeration: Option[String] = None,
+                         @JsonProperty("type") ontologyType: String = "Person")
+    extends DisplayAbstractAgent
 
 @ApiModel(
   value = "Organisation"
 )
 case class DisplayOrganisation(@ApiModelProperty(
-  value = "The name of the organisation"
-)label: String, @JsonProperty("type") ontologyType: String = "Organisation") extends DisplayAbstractAgent
+                                 value = "The name of the organisation"
+                               ) label: String,
+                               @JsonProperty("type") ontologyType: String =
+                                 "Organisation")
+    extends DisplayAbstractAgent
