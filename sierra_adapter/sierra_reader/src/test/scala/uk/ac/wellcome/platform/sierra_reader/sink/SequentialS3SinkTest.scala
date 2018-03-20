eccb272f05a014136466acd6bde2c6f76b0cbb5f
Alex Chan <a.chan@wellcome.ac.uk>
2018-03-20 15:47:16 +0000

    SequentialS3SinkTest.scala: also use the Akka fixtures

--- sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/sink/SequentialS3SinkTest.scala
+++ sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/sink/SequentialS3SinkTest.scala
@@ -6,7 +6,7 @@ import akka.stream.scaladsl.Source
 import io.circe.parser._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, FunSpec, Matchers}
-import uk.ac.wellcome.test.fixtures.S3
+import uk.ac.wellcome.test.fixtures.{AkkaFixtures, S3}
 import uk.ac.wellcome.test.utils.ExtendedPatience
 
 import scala.collection.JavaConversions._
@@ -15,98 +15,98 @@ class SequentialS3SinkTest
     extends FunSpec
     with Matchers
     with S3
+    with AkkaFixtures
     with BeforeAndAfterAll
     with ScalaFutures
     with ExtendedPatience {
 
-  implicit val system = ActorSystem()
-  implicit val materialiser = ActorMaterializer()
-  implicit val executionContext = system.dispatcher
-
-  override def afterAll(): Unit = {
-    system.terminate()
-    materialiser.shutdown()
-    super.afterAll()
-  }
-
   it("puts a single JSON in S3") {
-    withLocalS3Bucket { bucketName =>
-      val sink =
-        SequentialS3Sink(s3Client, bucketName = bucketName, keyPrefix = "testA_")
+    withActorSystem { system =>
+      implicit val executionContext = system.dispatcher
+      withLocalS3Bucket { bucketName =>
+        val sink =
+          SequentialS3Sink(s3Client, bucketName = bucketName, keyPrefix = "testA_")
 
-      val json = parse(s"""{"hello": "world"}""").right.get
+        val json = parse(s"""{"hello": "world"}""").right.get
 
-      val futureDone = Source
-        .single(json)
-        .zipWithIndex
-        .runWith(sink)
+        val futureDone = Source
+          .single(json)
+          .zipWithIndex
+          .runWith(sink)
 
-      whenReady(futureDone) { _ =>
-        val s3objects = s3Client.listObjects(bucketName).getObjectSummaries
-        s3objects should have size 1
-        s3objects.head.getKey() shouldBe "testA_0000.json"
+        whenReady(futureDone) { _ =>
+          val s3objects = s3Client.listObjects(bucketName).getObjectSummaries
+          s3objects should have size 1
+          s3objects.head.getKey() shouldBe "testA_0000.json"
 
-        getJsonFromS3(bucketName, "testA_0000.json") shouldBe json
+          getJsonFromS3(bucketName, "testA_0000.json") shouldBe json
+        }
       }
     }
   }
 
   it("puts multiple JSON bodies into S3") {
-    withLocalS3Bucket { bucketName =>
-      val sink =
-        SequentialS3Sink(s3Client, bucketName = bucketName, keyPrefix = "testB_")
+    withActorSystem { system =>
+      implicit val executionContext = system.dispatcher
+      withLocalS3Bucket { bucketName =>
+        val sink =
+          SequentialS3Sink(s3Client, bucketName = bucketName, keyPrefix = "testB_")
 
-      val json0 = parse(s"""{"red": "orange"}""").right.get
-      val json1 = parse(s"""{"orange": "yellow"}""").right.get
-      val json2 = parse(s"""{"yellow": "green"}""").right.get
+        val json0 = parse(s"""{"red": "orange"}""").right.get
+        val json1 = parse(s"""{"orange": "yellow"}""").right.get
+        val json2 = parse(s"""{"yellow": "green"}""").right.get
 
-      val futureDone = Source(List(json0, json1, json2)).zipWithIndex
-        .runWith(sink)
+        val futureDone = Source(List(json0, json1, json2)).zipWithIndex
+          .runWith(sink)
 
-      whenReady(futureDone) { _ =>
-        val s3objects = s3Client.listObjects(bucketName).getObjectSummaries
-        s3objects should have size 3
-        s3objects.map {
-          _.getKey()
-        } shouldBe List(
-          "testB_0000.json",
-          "testB_0001.json",
-          "testB_0002.json")
+        whenReady(futureDone) { _ =>
+          val s3objects = s3Client.listObjects(bucketName).getObjectSummaries
+          s3objects should have size 3
+          s3objects.map {
+            _.getKey()
+          } shouldBe List(
+            "testB_0000.json",
+            "testB_0001.json",
+            "testB_0002.json")
 
-        getJsonFromS3(bucketName, "testB_0000.json") shouldBe json0
-        getJsonFromS3(bucketName, "testB_0001.json") shouldBe json1
-        getJsonFromS3(bucketName, "testB_0002.json") shouldBe json2
+          getJsonFromS3(bucketName, "testB_0000.json") shouldBe json0
+          getJsonFromS3(bucketName, "testB_0001.json") shouldBe json1
+          getJsonFromS3(bucketName, "testB_0002.json") shouldBe json2
+        }
       }
     }
   }
 
   it("uses the offset if provided") {
-    withLocalS3Bucket { bucketName =>
-      val sink = SequentialS3Sink(
-        s3Client,
-        bucketName = bucketName,
-        keyPrefix = "testC_",
-        offset = 3)
+    withActorSystem { system =>
+      implicit val executionContext = system.dispatcher
+      withLocalS3Bucket { bucketName =>
+        val sink = SequentialS3Sink(
+          s3Client,
+          bucketName = bucketName,
+          keyPrefix = "testC_",
+          offset = 3)
 
-      val json0 = parse(s"""{"red": "orange"}""").right.get
-      val json1 = parse(s"""{"orange": "yellow"}""").right.get
-      val json2 = parse(s"""{"yellow": "green"}""").right.get
+        val json0 = parse(s"""{"red": "orange"}""").right.get
+        val json1 = parse(s"""{"orange": "yellow"}""").right.get
+        val json2 = parse(s"""{"yellow": "green"}""").right.get
 
-      val futureDone = Source(List(json0, json1, json2)).zipWithIndex
-        .runWith(sink)
+        val futureDone = Source(List(json0, json1, json2)).zipWithIndex
+          .runWith(sink)
 
-      whenReady(futureDone) { _ =>
-        val s3objects = s3Client.listObjects(bucketName).getObjectSummaries
-        s3objects.map {
-          _.getKey()
-        } shouldBe List(
-          "testC_0003.json",
-          "testC_0004.json",
-          "testC_0005.json")
+        whenReady(futureDone) { _ =>
+          val s3objects = s3Client.listObjects(bucketName).getObjectSummaries
+          s3objects.map {
+            _.getKey()
+          } shouldBe List(
+            "testC_0003.json",
+            "testC_0004.json",
+            "testC_0005.json")
 
-        getJsonFromS3(bucketName, "testC_0003.json") shouldBe json0
-        getJsonFromS3(bucketName, "testC_0004.json") shouldBe json1
-        getJsonFromS3(bucketName, "testC_0005.json") shouldBe json2
+          getJsonFromS3(bucketName, "testC_0003.json") shouldBe json0
+          getJsonFromS3(bucketName, "testC_0004.json") shouldBe json1
+          getJsonFromS3(bucketName, "testC_0005.json") shouldBe json2
+        }
       }
     }
   }
