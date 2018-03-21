7b507e26f3982b4b45073d1cf02ba955365bf65d
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-21 07:44:18 +0000

    ReindexWorkerServiceTest.scala: continue rearranging fixtures

--- reindexer/reindex_worker/src/test/scala/uk/ac/wellcome/platform/reindex_worker/services/ReindexWorkerServiceTest.scala
+++ reindexer/reindex_worker/src/test/scala/uk/ac/wellcome/platform/reindex_worker/services/ReindexWorkerServiceTest.scala
@@ -10,7 +10,6 @@ import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{FunSpec, Matchers}
 import uk.ac.wellcome.dynamo.VersionedDao
 import uk.ac.wellcome.exceptions.GracefulFailureException
-import uk.ac.wellcome.locals.DynamoDBLocal
 import uk.ac.wellcome.metrics.MetricsSender
 import uk.ac.wellcome.models.aws.{
   DynamoConfig,
@@ -22,7 +21,12 @@ import uk.ac.wellcome.platform.reindex_worker.TestRecord
 import uk.ac.wellcome.platform.reindex_worker.models.ReindexJob
 import uk.ac.wellcome.sns.SNSWriter
 import uk.ac.wellcome.sqs.SQSReader
-import uk.ac.wellcome.test.utils.{SNSLocal, SQSLocal}
+import uk.ac.wellcome.test.fixtures.{
+  AkkaFixtures,
+  LocalDynamoDb,
+  SnsFixtures,
+  SqsFixtures
+}
 import uk.ac.wellcome.utils.JsonUtil._
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -32,91 +36,82 @@ import scala.concurrent.duration._
 class ReindexWorkerServiceTest
     extends FunSpec
     with Matchers
-    with DynamoDBLocal[TestRecord]
     with MockitoSugar
-    with SNSLocal
-    with SQSLocal
+    with AkkaFixtures
+    with LocalDynamoDb
+    with SnsFixtures
+    with SqsFixtures
     with ScalaFutures {
-
-  val actorSystem = ActorSystem()
-
-  val metricsSender: MetricsSender =
-    new MetricsSender(
-      namespace = "reindexer-tests",
-      100 milliseconds,
-      mock[AmazonCloudWatch],
-      actorSystem)
-
-  override lazy val tableName = "table"
 
   override lazy val evidence: DynamoFormat[TestRecord] =
     DynamoFormat[TestRecord]
 
-  val queueUrl = createQueueAndReturnUrl("reindex-worker-service-test-q")
-  val topicArn = createTopicAndReturnArn("reindex-worker-service-test-topic")
-
   it("returns a successful Future if the reindex completes correctly") {
-    withReindexWorkerService { service =>
-      val reindexJob = ReindexJob(
-        shardId = "sierra/123",
-        desiredVersion = 6
-      )
-
-      val testRecord = TestRecord(
-        id = "id/111",
-        version = 1,
-        someData = "A dire daliance directly dancing due down.",
-        reindexShard = reindexJob.shardId,
-        reindexVersion = reindexJob.desiredVersion - 1
-      )
-
-      Scanamo.put(dynamoDbClient)(tableName)(testRecord)
-
-      val expectedRecords = List(
-        testRecord.copy(
-          version = testRecord.version + 1,
-          reindexVersion = reindexJob.desiredVersion
+    withLocalDynamoDbTable { tableName =>
+      withReindexWorkerService(tableName) { service =>
+        val reindexJob = ReindexJob(
+          shardId = "sierra/123",
+          desiredVersion = 6
         )
-      )
 
-      val sqsMessage = SQSMessage(
-        subject = None,
-        body = toJson(reindexJob).get,
-        topic = "topic",
-        messageType = "message",
-        timestamp = "now"
-      )
+        val testRecord = TestRecord(
+          id = "id/111",
+          version = 1,
+          someData = "A dire daliance directly dancing due down.",
+          reindexShard = reindexJob.shardId,
+          reindexVersion = reindexJob.desiredVersion - 1
+        )
 
-      val future = service.processMessage(message = sqsMessage)
+        Scanamo.put(dynamoDbClient)(tableName)(testRecord)
 
-      whenReady(future) { _ =>
-        val actualRecords: List[TestRecord] =
-          Scanamo.scan[TestRecord](dynamoDbClient)(tableName).map(_.right.get)
+        val expectedRecords = List(
+          testRecord.copy(
+            version = testRecord.version + 1,
+            reindexVersion = reindexJob.desiredVersion
+          )
+        )
 
-        actualRecords shouldBe expectedRecords
+        val sqsMessage = SQSMessage(
+          subject = None,
+          body = toJson(reindexJob).get,
+          topic = "topic",
+          messageType = "message",
+          timestamp = "now"
+        )
+
+        val future = service.processMessage(message = sqsMessage)
+
+        whenReady(future) { _ =>
+          val actualRecords: List[TestRecord] =
+            Scanamo.scan[TestRecord](dynamoDbClient)(tableName).map(_.right.get)
+
+          actualRecords shouldBe expectedRecords
+        }
       }
     }
   }
 
   it(
     "returns a failed Future if it cannot parse the SQS message as a ReindexJob") {
-    withReindexWorkerService { service =>
-      val sqsMessage = SQSMessage(
-        subject = None,
-        body = "<xml>What is JSON.</xl?>",
-        topic = "topic",
-        messageType = "message",
-        timestamp = "now"
-      )
+    withLocalDynamoDbTable { tableName =>
+      withReindexWorkerService(tableName) { service =>
+        val sqsMessage = SQSMessage(
+          subject = None,
+          body = "<xml>What is JSON.</xl?>",
+          topic = "topic",
+          messageType = "message",
+          timestamp = "now"
+        )
 
-      val service = reindexWorkerService()
+        val service = reindexWorkerService()
 
-      val future = service.processMessage(message = sqsMessage)
+        val future = service.processMessage(message = sqsMessage)
 
-      whenReady(future.failed) { result =>
-        result shouldBe a[GracefulFailureException]
-        result.getMessage should include(
-          "expected json value got < (line 1, column 1)")
+        whenReady(future.failed) { result =>
+          result shouldBe a[GracefulFailureException]
+          result.getMessage should include(
+            "expected json value got < (line 1, column 1)")
+        }
       }
     }
   }
@@ -166,6 +161,7 @@ class ReindexWorkerServiceTest
   }
 
   def withReindexWorkerService(
+      tableName: String,
       testWith: TestWith[ReindexWorkerService, Assertion]) = {
     withActorSystem { actorSystem =>
 
@@ -176,41 +172,39 @@ class ReindexWorkerServiceTest
         actorSystem = actorSystem
       )
 
-      withLocalDynamoDbTable { tableName =>
-        withLocalSqsQueue { queueUrl =>
-          withLocalTopicArn { topicArn =>
+      withLocalSqsQueue { queueUrl =>
+        withLocalTopicArn { topicArn =>
 
-            val workerService = new ReindexWorkerService(
-              targetService = new ReindexService(
-                dynamoDBClient = dynamoDbClient,
-                metricsSender = metricsSender,
-                versionedDao = new VersionedDao(
-                  dynamoDbClient = dynamoDbClient,
-                  dynamoConfig = DynamoConfig(table = dynamoTableName)
-                ),
+          val workerService = new ReindexWorkerService(
+            targetService = new ReindexService(
+              dynamoDBClient = dynamoDbClient,
+              metricsSender = metricsSender,
+              versionedDao = new VersionedDao(
+                dynamoDbClient = dynamoDbClient,
                 dynamoConfig = DynamoConfig(table = dynamoTableName)
               ),
-              reader = new SQSReader(
-                sqsClient = sqsClient,
-                sqsConfig = SQSConfig(
-                  queueUrl = queueUrl,
-                  waitTime = 1 second,
-                  maxMessages = 1
-                )
-              ),
-              snsWriter = new SNSWriter(
-                snsClient = snsClient,
-                snsConfig = SNSConfig(topicArn = topicArn)
-              ),
-              system = actorSystem,
-              metrics = metricsSender
-            )
+              dynamoConfig = DynamoConfig(table = dynamoTableName)
+            ),
+            reader = new SQSReader(
+              sqsClient = sqsClient,
+              sqsConfig = SQSConfig(
+                queueUrl = queueUrl,
+                waitTime = 1 second,
+                maxMessages = 1
+              )
+            ),
+            snsWriter = new SNSWriter(
+              snsClient = snsClient,
+              snsConfig = SNSConfig(topicArn = topicArn)
+            ),
+            system = actorSystem,
+            metrics = metricsSender
+          )
 
-            try {
-              testWith(FixtureParams(worker, queueUrl))
-            } finally {
-              worker.stop()
-            }
+          try {
+            testWith(FixtureParams(worker, queueUrl))
+          } finally {
+            worker.stop()
           }
         }
       }
