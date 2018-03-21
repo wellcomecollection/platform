751e29d4844ad0ce10fd52474c4bc842389e487c
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-21 16:40:23 +0000

    Tidy IndentityState code

--- common/src/main/scala/uk/ac/wellcome/models/IdentityState.scala
+++ common/src/main/scala/uk/ac/wellcome/models/IdentityState.scala
@@ -10,12 +10,13 @@ case class Identified[T](agent: T,
                          identifiers: List[SourceIdentifier])
     extends IdentityState[T]
     with IdentifiedOrUnidentifiable[T]
+
 case class Identifiable[T](agent: T,
                            sourceIdentifier: SourceIdentifier,
                            identifiers: List[SourceIdentifier])
     extends IdentityState[T]
     with IdentifieableOrUnidentifiable[T]
-case class Unidentifiable[T](agent: T)
-    extends IdentityState[T]
+
+case class Unidentifiable[T](agent: T) extends IdentityState[T]
     with IdentifiedOrUnidentifiable[T]
     with IdentifieableOrUnidentifiable[T]
