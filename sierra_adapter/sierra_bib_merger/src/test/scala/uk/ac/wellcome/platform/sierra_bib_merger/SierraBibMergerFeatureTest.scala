f7cfc94d077738c530381ae8fa83d530f6aeb71b
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-22 13:43:59 +0000

    sierra_bib_merger: rewrite feature test to use fixtures for isolation

--- sierra_adapter/sierra_bib_merger/src/test/scala/uk/ac/wellcome/platform/sierra_bib_merger/SierraBibMergerFeatureTest.scala
+++ sierra_adapter/sierra_bib_merger/src/test/scala/uk/ac/wellcome/platform/sierra_bib_merger/SierraBibMergerFeatureTest.scala
@@ -1,51 +1,29 @@
 package uk.ac.wellcome.platform.sierra_bib_merger
 
-import scala.concurrent.Await
-import scala.concurrent.duration._
-import akka.actor.ActorSystem
-import com.gu.scanamo.syntax._
-import com.gu.scanamo.{DynamoFormat, Scanamo}
-import com.twitter.finatra.http.EmbeddedHttpServer
-import com.twitter.inject.server.FeatureTestMixin
-import io.circe.{Decoder, Encoder}
-import org.scalatest.{FunSpec, Suite}
+import org.scalatest.{FunSpec, Matchers}
 import org.scalatest.concurrent.{Eventually, ScalaFutures}
 import org.scalatest.mockito.MockitoSugar
-import uk.ac.wellcome.models.aws.SQSMessage
-import uk.ac.wellcome.test.utils.{
-  AmazonCloudWatchFlag,
-  ExtendedPatience,
-  SQSLocal
-}
+import uk.ac.wellcome.test.utils.{ExtendedPatience, JsonTestUtil}
 import uk.ac.wellcome.models.transformable.SierraTransformable
 import uk.ac.wellcome.models.transformable.sierra.SierraBibRecord
-import uk.ac.wellcome.storage.{HybridRecord, VersionedHybridStoreLocal}
 import uk.ac.wellcome.utils.JsonUtil._
 import uk.ac.wellcome.dynamo._
 import uk.ac.wellcome.models.SourceMetadata
-import uk.ac.wellcome.sierra_adapter.test_utils.SourceDataVHSLocal
+import uk.ac.wellcome.test.fixtures.{LocalVersionedHybridStore, SqsFixtures}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class SierraBibMergerFeatureTest
     extends FunSpec
-    with FeatureTestMixin
-    with AmazonCloudWatchFlag
+    with Matchers
     with Eventually
     with MockitoSugar
     with ExtendedPatience
     with ScalaFutures
-    with SourceDataVHSLocal {
-
-  override protected def server = new EmbeddedHttpServer(
-    new Server(),
-    flags = Map(
-      "aws.sqs.queue.url" -> queueUrl,
-      "aws.sqs.waitTime" -> "1",
-      "aws.s3.bucketName" -> bucketName
-    ) ++ sqsLocalFlags ++ cloudWatchLocalEndpointFlag ++ dynamoDbLocalEndpointFlags(
-      tableName) ++ s3LocalFlags
-  )
+    with JsonTestUtil
+    with SqsFixtures
+    with fixtures.Server
+    with LocalVersionedHybridStore {
 
   def bibRecordString(id: String,
                       updatedDate: String,
@@ -81,165 +59,249 @@ class SierraBibMergerFeatureTest
     """.stripMargin
 
   it("should store a bib in the hybrid store") {
-    val id = "1000001"
-    val record = SierraBibRecord(
-      id = id,
-      data = bibRecordString(
-        id = id,
-        updatedDate = "2001-01-01T01:01:01Z",
-        title = "One ocelot on our oval"
-      ),
-      modifiedDate = "2001-01-01T01:01:01Z"
-    )
+    withLocalSqsQueue { queueUrl =>
+      withLocalS3Bucket { bucketName =>
+        withLocalDynamoDbTable { tableName =>
+          val flags = sqsLocalFlags(queueUrl) ++ s3LocalFlags(bucketName) ++ dynamoDbLocalEndpointFlags(tableName)
+          withServer(flags) { _ =>
+            withVersionedHybridStore[SierraTransformable](bucketName, tableName) { hybridStore =>
+              val id = "1000001"
+              val record = SierraBibRecord(
+                id = id,
+                data = bibRecordString(
+                  id = id,
+                  updatedDate = "2001-01-01T01:01:01Z",
+                  title = "One ocelot on our oval"
+                ),
+                modifiedDate = "2001-01-01T01:01:01Z"
+              )
 
-    sendMessageToSQS(toJson(record).get)
+              sendMessageToSQS(toJson(record).get, queueUrl = queueUrl)
 
-    val expectedSierraTransformable = SierraTransformable(bibRecord = record)
+              val expectedSierraTransformable = SierraTransformable(bibRecord = record)
 
-    assertStored(expectedSierraTransformable)
+              assertJsonStringsAreEqual(
+                getJsonFor[SierraTransformable](bucketName, tableName, expectedSierraTransformable),
+                toJson(expectedSierraTransformable).get
+              )
+            }
+          }
+        }
+      }
+    }
   }
 
   it("stores multiple bibs from SQS") {
-    val id1 = "1000001"
-    val record1 = SierraBibRecord(
-      id = id1,
-      data = bibRecordString(
-        id = id1,
-        updatedDate = "2001-01-01T01:01:01Z",
-        title = "The first ferret of four"
-      ),
-      modifiedDate = "2001-01-01T01:01:01Z"
-    )
+    withLocalSqsQueue { queueUrl =>
+      withLocalS3Bucket { bucketName =>
+        withLocalDynamoDbTable { tableName =>
+          val flags = sqsLocalFlags(queueUrl) ++ s3LocalFlags(bucketName) ++ dynamoDbLocalEndpointFlags(tableName)
+          withServer(flags) { _ =>
+            withVersionedHybridStore[SierraTransformable](bucketName, tableName) { hybridStore =>
+              val id1 = "1000001"
+              val record1 = SierraBibRecord(
+                id = id1,
+                data = bibRecordString(
+                  id = id1,
+                  updatedDate = "2001-01-01T01:01:01Z",
+                  title = "The first ferret of four"
+                ),
+                modifiedDate = "2001-01-01T01:01:01Z"
+              )
 
-    sendMessageToSQS(toJson(record1).get)
+              sendMessageToSQS(toJson(record1).get, queueUrl = queueUrl)
 
-    val expectedSierraTransformable1 = SierraTransformable(bibRecord = record1)
+              val expectedSierraTransformable1 = SierraTransformable(bibRecord = record1)
 
-    val id2 = "2000002"
-    val record2 = SierraBibRecord(
-      id = id2,
-      data = bibRecordString(
-        id = id2,
-        updatedDate = "2002-02-02T02:02:02Z",
-        title = "The second swan of a set"
-      ),
-      modifiedDate = "2002-02-02T02:02:02Z"
-    )
+              val id2 = "2000002"
+              val record2 = SierraBibRecord(
+                id = id2,
+                data = bibRecordString(
+                  id = id2,
+                  updatedDate = "2002-02-02T02:02:02Z",
+                  title = "The second swan of a set"
+                ),
+                modifiedDate = "2002-02-02T02:02:02Z"
+              )
 
-    sendMessageToSQS(toJson(record2).get)
+              sendMessageToSQS(toJson(record2).get, queueUrl = queueUrl)
 
-    val expectedSierraTransformable2 = SierraTransformable(bibRecord = record2)
+              val expectedSierraTransformable2 = SierraTransformable(bibRecord = record2)
 
-    assertStored(expectedSierraTransformable1)
-    assertStored(expectedSierraTransformable2)
+              assertJsonStringsAreEqual(
+                getJsonFor[SierraTransformable](bucketName, tableName, expectedSierraTransformable1),
+                toJson(expectedSierraTransformable1).get
+              )
+              assertJsonStringsAreEqual(
+                getJsonFor[SierraTransformable](bucketName, tableName, expectedSierraTransformable2),
+                toJson(expectedSierraTransformable2).get
+              )
+            }
+          }
+        }
+      }
+    }
   }
 
   it("updates a bib if a newer version is sent to SQS") {
-    val id = "3000003"
-    val oldBibRecord = SierraBibRecord(
-      id = id,
-      data = bibRecordString(
-        id = id,
-        updatedDate = "2003-03-03T03:03:03Z",
-        title = "Old orangutans outside an office"
-      ),
-      modifiedDate = "2003-03-03T03:03:03Z"
-    )
+    withLocalSqsQueue { queueUrl =>
+      withLocalS3Bucket { bucketName =>
+        withLocalDynamoDbTable { tableName =>
+          val flags = sqsLocalFlags(queueUrl) ++ s3LocalFlags(bucketName) ++ dynamoDbLocalEndpointFlags(tableName)
+          withServer(flags) { _ =>
+            withVersionedHybridStore[SierraTransformable](bucketName, tableName) { hybridStore =>
+              val id = "3000003"
+              val oldBibRecord = SierraBibRecord(
+                id = id,
+                data = bibRecordString(
+                  id = id,
+                  updatedDate = "2003-03-03T03:03:03Z",
+                  title = "Old orangutans outside an office"
+                ),
+                modifiedDate = "2003-03-03T03:03:03Z"
+              )
 
-    val oldRecord = SierraTransformable(bibRecord = oldBibRecord)
+              val oldRecord = SierraTransformable(bibRecord = oldBibRecord)
 
-    val newTitle = "A number of new narwhals near Newmarket"
-    val newUpdatedDate = "2004-04-04T04:04:04Z"
-    val record = SierraBibRecord(
-      id = id,
-      data = bibRecordString(
-        id = id,
-        updatedDate = newUpdatedDate,
-        title = newTitle
-      ),
-      modifiedDate = newUpdatedDate
-    )
+              val newTitle = "A number of new narwhals near Newmarket"
+              val newUpdatedDate = "2004-04-04T04:04:04Z"
+              val record = SierraBibRecord(
+                id = id,
+                data = bibRecordString(
+                  id = id,
+                  updatedDate = newUpdatedDate,
+                  title = newTitle
+                ),
+                modifiedDate = newUpdatedDate
+              )
 
-    hybridStore
-      .updateRecord(oldRecord.id)(oldRecord)(identity)(
-        SourceMetadata(oldRecord.sourceName))
-      .map { _ =>
-        sendMessageToSQS(toJson(record).get)
+              hybridStore
+                .updateRecord(oldRecord.id)(oldRecord)(identity)(
+                  SourceMetadata(oldRecord.sourceName))
+                .map { _ =>
+                  sendMessageToSQS(toJson(record).get, queueUrl = queueUrl)
+                }
+
+              val expectedSierraTransformable = SierraTransformable(bibRecord = record)
+
+              assertJsonStringsAreEqual(
+                getJsonFor[SierraTransformable](bucketName, tableName, expectedSierraTransformable),
+                toJson(expectedSierraTransformable).get
+              )
+            }
+          }
+        }
       }
-
-    val expectedSierraTransformable = SierraTransformable(bibRecord = record)
-
-    assertStored(expectedSierraTransformable)
+    }
   }
 
   it("does not update a bib if an older version is sent to SQS") {
-    val id = "6000006"
-    val newBibRecord = SierraBibRecord(
-      id = id,
-      data = bibRecordString(
-        id = id,
-        updatedDate = "2006-06-06T06:06:06Z",
-        title = "A presence of pristine porpoises"
-      ),
-      modifiedDate = "2006-06-06T06:06:06Z"
-    )
+    withLocalSqsQueue { queueUrl =>
+      withLocalS3Bucket { bucketName =>
+        withLocalDynamoDbTable { tableName =>
+          val flags = sqsLocalFlags(queueUrl) ++ s3LocalFlags(bucketName) ++ dynamoDbLocalEndpointFlags(tableName)
+          withServer(flags) { _ =>
+            withVersionedHybridStore[SierraTransformable](bucketName, tableName) { hybridStore =>
+              val id = "6000006"
+              val newBibRecord = SierraBibRecord(
+                id = id,
+                data = bibRecordString(
+                  id = id,
+                  updatedDate = "2006-06-06T06:06:06Z",
+                  title = "A presence of pristine porpoises"
+                ),
+                modifiedDate = "2006-06-06T06:06:06Z"
+              )
 
-    val expectedSierraTransformable =
-      SierraTransformable(bibRecord = newBibRecord)
+              val expectedSierraTransformable =
+                SierraTransformable(bibRecord = newBibRecord)
 
-    val oldTitle = "A small selection of sad shellfish"
-    val oldUpdatedDate = "2001-01-01T01:01:01Z"
-    val record = SierraBibRecord(
-      id = id,
-      data = bibRecordString(
-        id = id,
-        updatedDate = oldUpdatedDate,
-        title = oldTitle
-      ),
-      modifiedDate = oldUpdatedDate
-    )
+              val oldTitle = "A small selection of sad shellfish"
+              val oldUpdatedDate = "2001-01-01T01:01:01Z"
+              val record = SierraBibRecord(
+                id = id,
+                data = bibRecordString(
+                  id = id,
+                  updatedDate = oldUpdatedDate,
+                  title = oldTitle
+                ),
+                modifiedDate = oldUpdatedDate
+              )
 
-    hybridStore
-      .updateRecord(expectedSierraTransformable.id)(
-        expectedSierraTransformable)(identity)(
-        SourceMetadata(expectedSierraTransformable.sourceName))
-      .map { _ =>
-        sendMessageToSQS(toJson(record).get)
+              hybridStore
+                .updateRecord(expectedSierraTransformable.id)(
+                  expectedSierraTransformable)(identity)(
+                  SourceMetadata(expectedSierraTransformable.sourceName))
+                .map { _ =>
+                  sendMessageToSQS(toJson(record).get, queueUrl = queueUrl)
+                }
+
+              // Blocking in Scala is generally a bad idea; we do it here so there's
+              // enough time for this update to have gone through (if it was going to).
+              Thread.sleep(5000)
+
+              assertJsonStringsAreEqual(
+                getJsonFor[SierraTransformable](bucketName, tableName, expectedSierraTransformable),
+                toJson(expectedSierraTransformable).get
+              )
+            }
+          }
+        }
       }
-
-    // Blocking in Scala is generally a bad idea; we do it here so there's
-    // enough time for this update to have gone through (if it was going to).
-    Thread.sleep(5000)
-
-    assertStored(expectedSierraTransformable)
+    }
   }
 
   it("stores a bib from SQS if the ID already exists but no bibData") {
-    val id = "7000007"
-    val newRecord = SierraTransformable(sourceId = id)
+    withLocalSqsQueue { queueUrl =>
+      withLocalS3Bucket { bucketName =>
+        withLocalDynamoDbTable { tableName =>
+          val flags = sqsLocalFlags(queueUrl) ++ s3LocalFlags(bucketName) ++ dynamoDbLocalEndpointFlags(tableName)
+          withServer(flags) { _ =>
+            withVersionedHybridStore[SierraTransformable](bucketName, tableName) { hybridStore =>
+              val id = "7000007"
+              val newRecord = SierraTransformable(sourceId = id)
 
-    val title = "Inside an inquisitive igloo of ice imps"
-    val updatedDate = "2007-07-07T07:07:07Z"
-    val record = SierraBibRecord(
-      id = id,
-      data = bibRecordString(
-        id = id,
-        title = title,
-        updatedDate = updatedDate
-      ),
-      modifiedDate = updatedDate
-    )
+              val title = "Inside an inquisitive igloo of ice imps"
+              val updatedDate = "2007-07-07T07:07:07Z"
+              val record = SierraBibRecord(
+                id = id,
+                data = bibRecordString(
+                  id = id,
+                  title = title,
+                  updatedDate = updatedDate
+                ),
+                modifiedDate = updatedDate
+              )
 
-    val future =
-      hybridStore.updateRecord(newRecord.id)(newRecord)(identity)(
-        SourceMetadata(newRecord.sourceName))
+              val future =
+                hybridStore.updateRecord(newRecord.id)(newRecord)(identity)(
+                  SourceMetadata(newRecord.sourceName))
 
-    future.map { _ =>
-      sendMessageToSQS(toJson(record).get)
+              future.map { _ =>
+                sendMessageToSQS(toJson(record).get, queueUrl = queueUrl)
+              }
+
+              val expectedSierraTransformable = SierraTransformable(bibRecord = record)
+
+              assertJsonStringsAreEqual(
+                getJsonFor[SierraTransformable](bucketName, tableName, expectedSierraTransformable),
+                toJson(expectedSierraTransformable).get
+              )
+            }
+          }
+        }
+      }
     }
+  }
 
-    val expectedSierraTransformable = SierraTransformable(bibRecord = record)
-
-    assertStored(expectedSierraTransformable)
+  private def sendMessageToSQS(message: String, queueUrl: String) = {
+    val message = SQSMessage(
+      subject = Some("Test message sent by SierraBibMergerWorkerServiceTest"),
+      body = message,
+      topic = "topic",
+      messageType = "messageType",
+      timestamp = "2001-01-01T01:01:01Z"
+    )
+    sqsClient.sendMessage(queueUrl, toJson(message).get)
   }
 }
