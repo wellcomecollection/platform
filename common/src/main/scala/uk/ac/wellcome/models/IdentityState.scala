631f81d631fc2ff87164f544c3ed8dc8ba669cc0
caylaa <alicefuzier@gmail.com>
2018-03-26 13:44:10 +0100

    IdentifiedOrUnidentifiable -> Displayable IdentifieableOrUnidentifiable -> MaybeDisplayable

--- common/src/main/scala/uk/ac/wellcome/models/IdentityState.scala
+++ common/src/main/scala/uk/ac/wellcome/models/IdentityState.scala
@@ -2,22 +2,22 @@ package uk.ac.wellcome.models
 
 sealed trait IdentityState[+T]
 
-sealed trait IdentifieableOrUnidentifiable[+T] extends IdentityState[T]
-sealed trait IdentifiedOrUnidentifiable[+T] extends IdentityState[T]
+sealed trait MaybeDisplayable[+T] extends IdentityState[T]
+sealed trait Displayable[+T] extends IdentityState[T]
 
 case class Identified[T](agent: T,
                          canonicalId: String,
                          identifiers: List[SourceIdentifier])
     extends IdentityState[T]
-    with IdentifiedOrUnidentifiable[T]
+    with Displayable[T]
 
 case class Identifiable[T](agent: T,
                            sourceIdentifier: SourceIdentifier,
                            identifiers: List[SourceIdentifier])
     extends IdentityState[T]
-    with IdentifieableOrUnidentifiable[T]
+    with MaybeDisplayable[T]
 
 case class Unidentifiable[T](agent: T)
     extends IdentityState[T]
-    with IdentifiedOrUnidentifiable[T]
-    with IdentifieableOrUnidentifiable[T]
+    with Displayable[T]
+    with MaybeDisplayable[T]
