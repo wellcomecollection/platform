430b258df88efc93f26c677b9f8f856bb8d9e542
Alex Chan <a.chan@wellcome.ac.uk>
2018-03-28 12:36:22 +0100

    Give the Scala compiler help, it clearly needs it

--- data_api/snapshot_convertor/src/main/scala/uk/ac/wellcome/platform/snapshot_convertor/flow/ElasticsearchHitToDisplayWorkFlow.scala
+++ data_api/snapshot_convertor/src/main/scala/uk/ac/wellcome/platform/snapshot_convertor/flow/ElasticsearchHitToDisplayWorkFlow.scala
@@ -3,6 +3,7 @@ package uk.ac.wellcome.platform.snapshot_convertor.flow
 import akka.NotUsed
 import akka.stream.scaladsl.Flow
 import com.twitter.inject.Logging
+import io.circe.Decoder
 import io.circe.generic.extras.Configuration
 import uk.ac.wellcome.display.models.DisplayWork
 import uk.ac.wellcome.models.{IdentifiedWork, WorksIncludes}
@@ -46,10 +47,12 @@ object ElasticsearchHitToDisplayWorkFlow extends Logging {
     items = true
   )
 
+  implicit val decoder: Decoder[ElasticsearchHit] = Decoder[ElasticsearchHit]
+
   def apply()(implicit executionContext: ExecutionContext): Flow[String, DisplayWork, NotUsed] =
     Flow.fromFunction({ line =>
       val hit = fromJson[ElasticsearchHit](line) match {
-        case Success(hit) => hit
+        case Success(hit: ElasticsearchHit) => hit
         case Failure(parseFailure) => {
           warn("Failed to parse work metadata!", parseFailure)
           throw parseFailure
