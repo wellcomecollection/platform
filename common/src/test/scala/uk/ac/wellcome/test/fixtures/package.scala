f710dfc8b3d23b4215cb72f028c037ce28fcf38f
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-28 12:42:12 +0100

    [wip] start composing fixtures

--- common/src/test/scala/uk/ac/wellcome/test/fixtures/package.scala
+++ common/src/test/scala/uk/ac/wellcome/test/fixtures/package.scala
@@ -4,4 +4,9 @@ package object fixtures {
 
   type TestWith[T, R] = T => R
 
+  type Fixture[T, R] = () => TestWith[T, R] => R
+
+  def composeFixtures[T1, T2, R](fixture1: Fixture[T1, R], fixture2: Fixture[T2, R]): Fixture[(T1, T2), R] =
+    ???
+
 }
