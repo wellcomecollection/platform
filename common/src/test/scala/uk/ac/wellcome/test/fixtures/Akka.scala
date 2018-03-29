9be5f41ccd67f25798146bd5320f4f8a1aeca510
Alex Chan <alex@alexwlchan.net>
2018-03-29 07:04:28 +0100

    Add a new Akka fixture for materializer

--- common/src/test/scala/uk/ac/wellcome/test/fixtures/Akka.scala
+++ common/src/test/scala/uk/ac/wellcome/test/fixtures/Akka.scala
@@ -1,6 +1,7 @@
 package uk.ac.wellcome.test.fixtures
 
 import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, Materializer}
 import org.scalatest.concurrent.Eventually
 
 trait Akka extends Eventually {
@@ -16,4 +17,14 @@ trait Akka extends Eventually {
     }
   }
 
+  def withMaterializer[R](actorSystem: ActorSystem)(
+    testWith: TestWith[Materializer, R]): R = {
+    val materializer = ActorMaterializer()(actorSystem)
+
+    try {
+      testWith(materializer)
+    } finally {
+      materializer.shutdown()
+    }
+  }
 }
