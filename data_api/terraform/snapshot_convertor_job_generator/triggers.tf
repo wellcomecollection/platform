a4b8567f3567336246897300f3488e4e93deaa7a
Alex Chan <a.chan@wellcome.ac.uk>
2018-03-29 13:09:15 +0100

    data_api: mixed Terraform fixes

--- data_api/terraform/snapshot_convertor_job_generator/triggers.tf
+++ data_api/terraform/snapshot_convertor_job_generator/triggers.tf
@@ -5,6 +5,5 @@ resource "aws_s3_bucket_notification" "private_data_bucket_notification" {
     lambda_function_arn = "${module.snapshot_convertor_job_generator_lambda.arn}"
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "elasticdump/"
-    filter_suffix       = ".json"
   }
 }
