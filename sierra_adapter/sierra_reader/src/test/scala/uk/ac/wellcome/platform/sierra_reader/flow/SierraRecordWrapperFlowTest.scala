71b7d550510e88f34facfb9e2ecc956e88509ee1
Alex Chan <alex@alexwlchan.net>
2018-03-29 08:08:59 +0100

    SierraRecordWrapperFlowTest.scala: push more into the fixture

--- sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/flow/SierraRecordWrapperFlowTest.scala
+++ sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/flow/SierraRecordWrapperFlowTest.scala
@@ -22,131 +22,121 @@ class SierraRecordWrapperFlowTest
     with ExtendedPatience
     with Matchers {
 
-  private def withRecordWrapperFlow(actorSystem: ActorSystem)(
+  private def withRecordWrapperFlow(
     testWith: TestWith[Flow[Json, SierraRecord, NotUsed], Assertion]) = {
-    implicit val executionContext: ExecutionContextExecutor =
-      actorSystem.dispatcher
-
-    val wrapperFlow = SierraRecordWrapperFlow()
-
-    testWith(wrapperFlow)
-  }
-
-  it("creates a SierraRecord from a bib") {
     withActorSystem { actorSystem =>
       withRecordWrapperFlow(actorSystem) { wrapperFlow =>
         withMaterializer(actorSystem) { materializer =>
-          val id = "100001"
-          val updatedDate = "2013-12-13T12:43:16Z"
-          val json = parse(s"""
-          |{
-          | "id": "$id",
-          | "updatedDate": "$updatedDate"
-          |}
-        """.stripMargin).right.get
+          implicit val executionContext: ExecutionContextExecutor =
+            actorSystem.dispatcher
 
-          val expectedRecord = SierraRecord(
-            id = id,
-            data = json.noSpaces,
-            modifiedDate = updatedDate
-          )
+          val wrapperFlow = SierraRecordWrapperFlow()
 
-          val futureRecord = Source
-            .single(json)
-            .via(wrapperFlow)
-            .runWith(Sink.head)
-
-          whenReady(futureRecord) { sierraRecord =>
-            sierraRecord shouldBe expectedRecord
-          }
+          testWith(wrapperFlow)
         }
+      }
+    }
+  }
+
+  it("creates a SierraRecord from a bib") {
+    withRecordWrapperFlow { wrapperFlow =>
+      val id = "100001"
+      val updatedDate = "2013-12-13T12:43:16Z"
+      val json = parse(s"""
+      |{
+      | "id": "$id",
+      | "updatedDate": "$updatedDate"
+      |}
+    """.stripMargin).right.get
+
+      val expectedRecord = SierraRecord(
+        id = id,
+        data = json.noSpaces,
+        modifiedDate = updatedDate
+      )
+
+      val futureRecord = Source
+        .single(json)
+        .via(wrapperFlow)
+        .runWith(Sink.head)
+
+      whenReady(futureRecord) { sierraRecord =>
+        sierraRecord shouldBe expectedRecord
       }
     }
   }
 
   it("creates a SierraRecord from an item") {
-    withActorSystem { actorSystem =>
-      withRecordWrapperFlow(actorSystem) { wrapperFlow =>
-        withMaterializer(actorSystem) { materializer =>
-          val id = "400004"
-          val updatedDate = "2014-04-14T14:14:14Z"
-          val json = parse(s"""
-          |{
-          | "id": "$id",
-          | "updatedDate": "$updatedDate",
-          | "bibIds": ["4", "44", "444", "4444"]
-          |}
-        """.stripMargin).right.get
+    withRecordWrapperFlow { wrapperFlow =>
+      val id = "400004"
+      val updatedDate = "2014-04-14T14:14:14Z"
+      val json = parse(s"""
+      |{
+      | "id": "$id",
+      | "updatedDate": "$updatedDate",
+      | "bibIds": ["4", "44", "444", "4444"]
+      |}
+    """.stripMargin).right.get
 
-          val expectedRecord = SierraRecord(
-            id = id,
-            data = json.noSpaces,
-            modifiedDate = updatedDate
-          )
+      val expectedRecord = SierraRecord(
+        id = id,
+        data = json.noSpaces,
+        modifiedDate = updatedDate
+      )
 
-          val futureRecord = Source
-            .single(json)
-            .via(wrapperFlow)
-            .runWith(Sink.head)
+      val futureRecord = Source
+        .single(json)
+        .via(wrapperFlow)
+        .runWith(Sink.head)
 
-          whenReady(futureRecord) { sierraRecord =>
-            sierraRecord shouldBe expectedRecord
-          }
-        }
+      whenReady(futureRecord) { sierraRecord =>
+        sierraRecord shouldBe expectedRecord
       }
     }
   }
 
   it("is able to handle deleted bibs") {
-    withActorSystem { actorSystem =>
-      withRecordWrapperFlow(actorSystem) { wrapperFlow =>
-        withMaterializer(actorSystem) { materializer =>
-          val id = "1357947"
-          val deletedDate = "2014-01-31"
-          val json = parse(s"""{
-                            |  "id" : "$id",
-                            |  "deletedDate" : "$deletedDate",
-                            |  "deleted" : true
-                            |}""".stripMargin).right.get
+    withRecordWrapperFlow { wrapperFlow =>
+      val id = "1357947"
+      val deletedDate = "2014-01-31"
+      val json = parse(s"""{
+                        |  "id" : "$id",
+                        |  "deletedDate" : "$deletedDate",
+                        |  "deleted" : true
+                        |}""".stripMargin).right.get
 
-          val expectedRecord = SierraRecord(
-            id = id,
-            data = json.noSpaces,
-            modifiedDate = s"${deletedDate}T00:00:00Z"
-          )
+      val expectedRecord = SierraRecord(
+        id = id,
+        data = json.noSpaces,
+        modifiedDate = s"${deletedDate}T00:00:00Z"
+      )
 
-          val futureRecord = Source
-            .single(json)
-            .via(wrapperFlow)
-            .runWith(Sink.head)
+      val futureRecord = Source
+        .single(json)
+        .via(wrapperFlow)
+        .runWith(Sink.head)
 
-          whenReady(futureRecord) { sierraRecord =>
-            sierraRecord shouldBe expectedRecord
-          }
-        }
+      whenReady(futureRecord) { sierraRecord =>
+        sierraRecord shouldBe expectedRecord
       }
     }
   }
 
   it("fails the stream if the record contains invalid JSON") {
-    withActorSystem { actorSystem =>
-      withRecordWrapperFlow(actorSystem) { wrapperFlow =>
-        withMaterializer(actorSystem) { materializer =>
-          val invalidSierraJson = parse(s"""{
-          | "missing": ["id", "updatedDate"],
-          | "reason": "This JSON will not pass!",
-          |  "comment": "XML is coming!"
-          |}""".stripMargin).right.get
+    withRecordWrapperFlow { wrapperFlow =>
+      val invalidSierraJson = parse(s"""{
+      | "missing": ["id", "updatedDate"],
+      | "reason": "This JSON will not pass!",
+      |  "comment": "XML is coming!"
+      |}""".stripMargin).right.get
 
-          val futureUnit = Source
-            .single(invalidSierraJson)
-            .via(wrapperFlow)
-            .runWith(Sink.head)
+      val futureUnit = Source
+        .single(invalidSierraJson)
+        .via(wrapperFlow)
+        .runWith(Sink.head)
 
-          whenReady(futureUnit.failed) { _ =>
-            true shouldBe true
-          }
-        }
+      whenReady(futureUnit.failed) { _ =>
+        true shouldBe true
       }
     }
   }
