4e936b6f0fdba24007ad05d872f04a43425954e5
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-29 10:42:14 +0100

    ServerTest.scala: add the fixtures back to the ServerTest

--- data_api/snapshot_convertor/src/test/scala/uk/ac/wellcome/platform/snapshot_convertor/ServerTest.scala
+++ data_api/snapshot_convertor/src/test/scala/uk/ac/wellcome/platform/snapshot_convertor/ServerTest.scala
@@ -3,8 +3,15 @@ package uk.ac.wellcome.platform.snapshot_convertor
 import com.twitter.finagle.http.Status.Ok
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.FunSpec
+import uk.ac.wellcome.test.fixtures.{S3, SNS, SQS}
 
-class ServerTest extends FunSpec with ScalaFutures with fixtures.Server {
+class ServerTest
+    extends FunSpec
+    with S3
+    with SNS
+    with SQS
+    with ScalaFutures
+    with fixtures.Server {
 
   it("shows the healthcheck message") {
     withLocalSqsQueue { queueUrl =>
