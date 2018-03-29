ac72447e7f7204ebfb6920967ff28dd3606711e1
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-03-29 08:58:49 +0000

    Apply auto-formatting rules

--- data_api/snapshot_convertor/src/test/scala/uk/ac/wellcome/platform/snapshot_convertor/ServerTest.scala
+++ data_api/snapshot_convertor/src/test/scala/uk/ac/wellcome/platform/snapshot_convertor/ServerTest.scala
@@ -4,16 +4,14 @@ import com.twitter.finagle.http.Status.Ok
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.FunSpec
 
-class ServerTest
-    extends FunSpec
-    with ScalaFutures
-    with fixtures.Server {
+class ServerTest extends FunSpec with ScalaFutures with fixtures.Server {
 
   it("shows the healthcheck message") {
     withLocalSqsQueue { queueUrl =>
       withLocalSnsTopic { topicArn =>
         withLocalS3Bucket { bucketName =>
-          val flags = snsLocalFlags(topicArn) ++ sqsLocalFlags(queueUrl) ++ s3LocalFlags(bucketName)
+          val flags = snsLocalFlags(topicArn) ++ sqsLocalFlags(queueUrl) ++ s3LocalFlags(
+            bucketName)
           withServer(flags) { server =>
             server.httpGet(
               path = "/management/healthcheck",
