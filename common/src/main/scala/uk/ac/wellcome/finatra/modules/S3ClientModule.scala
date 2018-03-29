6edefaeef8411fe08a21532229762ec3595479f5
Alex Chan <alex@alexwlchan.net>
2018-03-29 10:52:31 +0100

    this is in a different module now

--- common/src/main/scala/uk/ac/wellcome/finatra/modules/S3ClientModule.scala
+++ common/src/main/scala/uk/ac/wellcome/finatra/modules/S3ClientModule.scala
@@ -2,17 +2,7 @@ package uk.ac.wellcome.finatra.modules
 
 import javax.inject.Singleton
 
-import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import akka.stream.alpakka.s3.{MemoryBufferType, S3Settings}
-import akka.stream.alpakka.s3.scaladsl.S3Client
-import com.amazonaws.auth.{
-  AWSStaticCredentialsProvider,
-  BasicAWSCredentials,
-  DefaultAWSCredentialsProviderChain
-}
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
-import com.amazonaws.regions.AwsRegionProvider
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 import com.google.inject.Provides
 import com.twitter.inject.TwitterModule
@@ -44,43 +34,6 @@ object S3ClientModule extends TwitterModule {
         .withEndpointConfiguration(
           new EndpointConfiguration(s3Endpoint(), awsConfig.region))
         .build()
-  }
-
-  @Singleton
-  @Provides
-  def providesAkkaS3Client(awsConfig: AWSConfig,
-                           actorSystem: ActorSystem): S3Client = {
-    val regionProvider =
-      new AwsRegionProvider {
-        def getRegion: String = awsConfig.region
-      }
-
-    val credentialsProvider = if (s3Endpoint().isEmpty) {
-      DefaultAWSCredentialsProviderChain.getInstance()
-    } else {
-      new AWSStaticCredentialsProvider(
-        new BasicAWSCredentials(accessKey(), secretKey())
-      )
-    }
-
-    val actorMaterializer = ActorMaterializer()(actorSystem)
-    val endpointUrl = if (s3Endpoint().isEmpty) {
-      None
-    } else {
-      Some(s3Endpoint())
-    }
-
-    val settings = new S3Settings(
-      bufferType = MemoryBufferType,
-      proxy = None,
-      credentialsProvider = credentialsProvider,
-      s3RegionProvider = regionProvider,
-      pathStyleAccess = true,
-      endpointUrl = endpointUrl
-    )
-
-    logger.debug(s"creating S3 Akka client with settings=[$settings]")
-    new S3Client(settings)(actorSystem, actorMaterializer)
   }
 
 }
