9546a91aeeb1aeb625688cc37c007340c723b8e0
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-04-03 16:27:42 +0100

    snapshot_converter: add a test to catch the EntityStreamSizeException error

--- data_api/snapshot_convertor/src/test/scala/uk/ac/wellcome/platform/snapshot_convertor/source/S3SourceTest.scala
+++ data_api/snapshot_convertor/src/test/scala/uk/ac/wellcome/platform/snapshot_convertor/source/S3SourceTest.scala
@@ -52,6 +52,52 @@ class S3SourceTest
     }
   }
 
+  // This test is meant to catch an error we saw when we first turned on
+  // the snapshot convertor:
+  //
+  //    akka.http.scaladsl.model.EntityStreamSizeException:
+  //    EntityStreamSizeException: actual entity size (Some(19403836)) exceeded
+  //    content length limit (8388608 bytes)! You can configure this by setting
+  //    `akka.http.[server|client].parsing.max-content-length` or calling
+  //    `HttpEntity.withSizeLimit` before materializing the dataBytes stream.
+  //
+  // With the original code, we were unable to read anything more than
+  // an 8MB file from S3.  This test deliberately creates a very large file,
+  // and tries to stream it back out.
+  //
+  it("reads files that are larger than 8MB large from S3") {
+    withActorSystem { actorSystem =>
+      implicit val system = actorSystem
+      implicit val materializer = ActorMaterializer()
+
+      withLocalS3Bucket { bucketName =>
+        withS3AkkaClient(actorSystem, materializer) { akkaS3client =>
+
+          // The aim here is to increase entropy in the source file, as a
+          // way to increase the gzip-compressed size.
+          val expectedLines = (1 to 1000000).map {
+            idx => s"${idx} ${100 - idx} ${1000 - idx} ${10000 - idx} ${100000 - idx}"
+          }
+
+          val content = expectedLines.mkString("\n")
+
+          withGzipCompressedS3Key(bucketName, content) { key =>
+            val source = S3Source(
+              s3client = akkaS3client,
+              bucketName = bucketName,
+              key = key
+            )
+
+            val future = source.runWith(Sink.seq)
+            whenReady(future) { result =>
+              result.toList shouldBe expectedLines
+            }
+          }
+        }
+      }
+    }
+  }
+
   it("returns a failed future if it the S3 file is not gzip-compressed") {
     withActorSystem { actorSystem =>
       implicit val system = actorSystem
