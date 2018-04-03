c9d0209c029216516154befbd9ddf0d4120c775c
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-04-03 12:55:21 +0100

    sierra_adapter: add a generator that finds the missing windows

--- sierra_adapter/test_build_missing_windows.py
+++ sierra_adapter/test_build_missing_windows.py
@@ -1,6 +1,9 @@
 # -*- encoding: utf-8
 
-from build_missing_windows import sliding_window
+import datetime as dt
+
+from build_missing_windows import get_missing_windows, sliding_window
+from report_adapter_progress import Interval
 
 
 def test_sliding_window():
@@ -15,4 +18,44 @@ def test_sliding_window():
         (6, 7),
         (7, 8),
         (8, 9),
+    ]
+
+
+def test_missing_windows():
+    report = [
+        Interval(
+            start = dt.datetime(2000, 1, 1, 12, 0, 0),
+            end   = dt.datetime(2000, 1, 1, 13, 0, 0),
+            key   = 'report1.txt'
+        ),
+        Interval(
+            start = dt.datetime(2000, 1, 1, 13, 7, 0),
+            end   = dt.datetime(2000, 1, 1, 13, 18, 0),
+            key   = 'report1.txt'
+        ),
+        Interval(
+            start = dt.datetime(2000, 1, 1, 13, 25, 0),
+            end   = dt.datetime(2000, 1, 1, 13, 40, 0),
+            key   = 'report1.txt'
+        ),
+    ]
+
+    res = get_missing_windows(report)
+    assert list(res) == [
+        {
+            'start': dt.datetime(2000, 1, 1, 12, 59, 59).isoformat(),
+            'end':   dt.datetime(2000, 1, 1, 13, 4, 59).isoformat(),
+        },
+        {
+            'start': dt.datetime(2000, 1, 1, 13, 3, 59).isoformat(),
+            'end':   dt.datetime(2000, 1, 1, 13, 8, 59).isoformat(),
+        },
+        {
+            'start': dt.datetime(2000, 1, 1, 13, 17, 59).isoformat(),
+            'end':   dt.datetime(2000, 1, 1, 13, 22, 59).isoformat(),
+        },
+        {
+            'start': dt.datetime(2000, 1, 1, 13, 21, 59).isoformat(),
+            'end':   dt.datetime(2000, 1, 1, 13, 26, 59).isoformat(),
+        },
     ]
