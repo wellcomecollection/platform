b0d29c0fcbdced3f56a75676ee8793bdc310f459
Alex Chan <alex@alexwlchan.net>
2018-04-03 14:28:54 +0100

    id_minter: try to fix the flaky test fixture

--- catalogue_pipeline/id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/fixtures/IdentifersDatabase.scala
+++ catalogue_pipeline/id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/fixtures/IdentifersDatabase.scala
@@ -70,14 +70,38 @@ trait IdentifiersDatabase
     ).sortBy(_.field)
   }
 
+  // This is based on the implementation of alphanumeric in Scala.util.Random.
+  def alphabetic: Stream[Char] = {
+    def nextAlpha: Char = {
+      val chars = "abcdefghijklmnopqrstuvwxyz"
+      chars charAt (Random.nextInt(chars.length))
+    }
+
+    Stream continually nextAlpha
+  }
+
   def withIdentifiersDatabase[R](testWith: TestWith[DatabaseConfig, R]) = {
     Class.forName("com.mysql.jdbc.Driver")
     ConnectionPool.singleton(s"jdbc:mysql://$host:$port", username, password)
 
     implicit val session = AutoSession
 
-    val databaseName: String = Random.alphanumeric take 10 mkString
-    val tableName: String = Random.alphanumeric take 10 mkString
+    // Something in our MySQL Docker image gets upset by some database names,
+    // and throws an error of the form:
+    //
+    //    You have an error in your SQL syntax; check the manual that
+    //    corresponds to your MySQL server version for the right syntax to use
+    //
+    // This error can be reproduced by running "CREATE DATABASE <name>" inside
+    // the Docker image.  It's not clear what features of the name cause this
+    // error to occur, but so far we've only seen it in database names that
+    // include numbers.  We're guessing some arrangement of numbers causes the
+    // issue; for now we just use letters to try to work around this issue.
+    //
+    // The Oracle docs are not enlightening in this regard:
+    // https://docs.oracle.com/database/121/SQLRF/sql_elements008.htm#SQLRF00223
+    val databaseName: String = alphabetic take 10 mkString
+    val tableName: String = alphabetic take 10 mkString
 
     val identifiersDatabase: SQLSyntax = SQLSyntax.createUnsafely(databaseName)
     val identifiersTable: SQLSyntax = SQLSyntax.createUnsafely(tableName)
