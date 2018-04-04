13e8654c3967338d85c3f89a882d6cad79675c53
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-04-04 12:14:01 +0100

    snapshot_convertor: sort out the tests for large file uploads

--- data_api/snapshot_convertor/src/test/scala/uk/ac/wellcome/platform/snapshot_convertor/services/ConvertorServiceTest.scala
+++ data_api/snapshot_convertor/src/test/scala/uk/ac/wellcome/platform/snapshot_convertor/services/ConvertorServiceTest.scala
@@ -106,6 +106,83 @@ class ConvertorServiceTest
     }
   }
 
+  // This test is meant to catch an error we saw when we first turned on
+  // the snapshot convertor:
+  //
+  //    akka.http.scaladsl.model.EntityStreamSizeException:
+  //    EntityStreamSizeException: actual entity size (Some(19403836)) exceeded
+  //    content length limit (8388608 bytes)! You can configure this by setting
+  //    `akka.http.[server|client].parsing.max-content-length` or calling
+  //    `HttpEntity.withSizeLimit` before materializing the dataBytes stream.
+  //
+  // With the original code, we were unable to read anything more than
+  // an 8MB file from S3.  This test deliberately creates a very large file,
+  // and tries to stream it back out.
+  //
+  it("completes a very large conversion successfully") {
+    withLocalS3Bucket { bucketName =>
+      withActorSystem { actorSystem =>
+        implicit val materializer = ActorMaterializer()(actorSystem)
+        withS3AkkaClient(actorSystem, materializer) { s3AkkaClient =>
+          withConvertorService(bucketName, actorSystem, s3AkkaClient) {
+            convertorService =>
+              // Create a collection of works.  These three differ by version,
+              // if not anything more interesting!
+              val works = (1 to 100000).map { version =>
+                IdentifiedWork(
+                  canonicalId = "rbfhv6b4",
+                  title = Some("Rumblings from a rambunctious rodent"),
+                  sourceIdentifier = SourceIdentifier(
+                    identifierScheme = IdentifierSchemes.miroImageNumber,
+                    ontologyType = "work",
+                    value = "R0060400"
+                  ),
+                  version = version
+                )
+              }
+
+              val elasticsearchJsons = works.map { work =>
+                s"""{"_index": "jett4fvw", "_type": "work", "_id": "${work.canonicalId}", "_score": 1, "_source": ${toJson(
+                  work).get}}"""
+              }
+              val content = elasticsearchJsons.mkString("\n")
+
+              withGzipCompressedS3Key(bucketName, content) { objectKey =>
+                val conversionJob = ConversionJob(
+                  bucketName = bucketName,
+                  objectKey = objectKey
+                )
+
+                val future = convertorService.runConversion(conversionJob)
+
+                whenReady(future) { result =>
+                  val downloadFile =
+                    File.createTempFile("convertorServiceTest", ".txt.gz")
+                  s3Client.getObject(
+                    new GetObjectRequest(bucketName, "target.txt.gz"),
+                    downloadFile)
+
+                  val contents = readGzipFile(downloadFile.getPath)
+                  val expectedContents = works
+                    .map { DisplayWork(_, includes = AllWorksIncludes()) }
+                    .map { toJson(_).get }
+                    .mkString("\n") + "\n"
+
+                  contents shouldBe expectedContents
+
+                  result shouldBe CompletedConversionJob(
+                    conversionJob = conversionJob,
+                    targetLocation =
+                      s"http://localhost:33333/$bucketName/target.txt.gz"
+                  )
+                }
+              }
+          }
+        }
+      }
+    }
+  }
+
   it("returns a failed future if asked to convert a non-existent snapshot") {
     withLocalS3Bucket { bucketName =>
       withActorSystem { actorSystem =>
