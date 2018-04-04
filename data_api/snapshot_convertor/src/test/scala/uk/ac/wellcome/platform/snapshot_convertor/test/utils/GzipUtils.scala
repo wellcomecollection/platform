8fbe2e1b148c59ae779447918115da1783e2d121
Alex Chan <a.chan@wellcome.ac.uk>
2018-04-04 13:50:06 +0100

    snapshot_convertor: add a test that we can manage really large files

--- data_api/snapshot_convertor/src/test/scala/uk/ac/wellcome/platform/snapshot_convertor/test/utils/GzipUtils.scala
+++ data_api/snapshot_convertor/src/test/scala/uk/ac/wellcome/platform/snapshot_convertor/test/utils/GzipUtils.scala
@@ -45,7 +45,10 @@ trait GzipUtils extends S3 {
     // This performs "gzip foo.txt > foo.txt.gz" in a roundabout way.
     // Because we're using the busybox version of gzip, it actually cleans
     // up the old file and appends the .gz suffix for us.
-    s"gzip ${tmpfile.getPath}" !!
+    //
+    // We set the gzip compression level to the lowest possible (-1),
+    // so we can use smaller data sets in tests (and thus use less memory).
+    s"gzip -1 ${tmpfile.getPath}" !!
 
     new File(s"${tmpfile.getPath}.gz")
   }
