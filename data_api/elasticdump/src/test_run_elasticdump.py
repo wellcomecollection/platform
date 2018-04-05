259128b90a5bd5df132ba8e2a408203a5cebcf43
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-04-05 13:26:49 +0100

    elasticdump: Define an elasticsearch_index fixture

--- data_api/elasticdump/src/test_run_elasticdump.py
+++ data_api/elasticdump/src/test_run_elasticdump.py
@@ -4,8 +4,14 @@ import run_elasticdump
 import os
 
 
-def test_end_to_end(sqs_client, queue_url, s3_client, bucket):
-    sqs_client.send_message(QueueUrl=s3_client, MessageBody="")
+def test_end_to_end(
+    sqs_client,
+    queue_url,
+    s3_client,
+    bucket,
+    elasticsearch_index
+):
+    sqs_client.send_message(QueueUrl=queue_url, MessageBody="")
 
     os.environ['sqs_queue_url'] = s3_client
     os.environ['upload_bucket'] = bucket
