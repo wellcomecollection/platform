9808758a412dccb77c37e3f479fd9eb917a95469
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-04-05 10:21:36 +0100

    display-common: move the API Jackson module to the common lib

--- catalogue_api/api/src/main/scala/uk/ac/wellcome/platform/api/Server.scala
+++ catalogue_api/api/src/main/scala/uk/ac/wellcome/platform/api/Server.scala
@@ -2,35 +2,16 @@ package uk.ac.wellcome.platform.api
 
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.HttpServer
-import com.twitter.finatra.http.filters.{
-  CommonFilters,
-  LoggingMDCFilter,
-  TraceIdMDCFilter
-}
+import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
 import com.twitter.finatra.http.routing.HttpRouter
-import com.twitter.finatra.json.modules.FinatraJacksonModule
-import com.twitter.finatra.json.utils.CamelCasePropertyNamingStrategy
 import io.swagger.models.Swagger
+import uk.ac.wellcome.display.modules.DisplayJacksonModule
 import uk.ac.wellcome.elasticsearch.finatra.modules.ElasticClientModule
-import uk.ac.wellcome.finatra.modules._
 import uk.ac.wellcome.platform.api.controllers._
-import uk.ac.wellcome.platform.api.finatra.exceptions.{
-  CaseClassMappingExceptionWrapper,
-  ElasticsearchResponseExceptionMapper,
-  GeneralExceptionMapper
-}
-import uk.ac.wellcome.display.models.WorksIncludesDeserializerModule
+import uk.ac.wellcome.platform.api.finatra.exceptions.{CaseClassMappingExceptionWrapper, ElasticsearchResponseExceptionMapper, GeneralExceptionMapper}
 
 object ServerMain extends Server
 object ApiSwagger extends Swagger
-
-object ApiJacksonModule extends FinatraJacksonModule {
-  override val propertyNamingStrategy = CamelCasePropertyNamingStrategy
-  override val additionalJacksonModules = Seq(
-    new WorksIncludesDeserializerModule
-  )
-}
-
 class Server extends HttpServer {
   override val name = "uk.ac.wellcome.platform.api Platformapi"
   override val modules = Seq(ElasticClientModule)
@@ -55,7 +36,7 @@ class Server extends HttpServer {
     default = apiPrefix() + "/context.json",
     help = "API JSON-LD context")
 
-  override def jacksonModule = ApiJacksonModule
+  override def jacksonModule = DisplayJacksonModule
 
   override def configureHttp(router: HttpRouter) {
     router
