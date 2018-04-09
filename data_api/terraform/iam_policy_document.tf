94c07893aa167e545f7571b9ab0a704d437df835
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-04-09 11:46:14 +0000

    Apply auto-formatting rules

--- data_api/terraform/iam_policy_document.tf
+++ data_api/terraform/iam_policy_document.tf
@@ -44,7 +44,7 @@ data "aws_iam_policy_document" "public_data_bucket_get_access_policy" {
 
     principals {
       identifiers = ["*"]
-      type = "AWS"
+      type        = "AWS"
     }
 
     resources = [
