4244f66c3326247a9f880381f41b6180e84e1e7d
Alex Chan <alex@alexwlchan.net>
2018-04-10 12:15:13 +0100

    Expire CloudFront logs after 30 days (#1853)

--- loris/terraform/s3.tf
+++ loris/terraform/s3.tf
@@ -10,6 +10,15 @@ resource "aws_s3_bucket" "wellcomecollection-miro-images-public" {
   lifecycle {
     prevent_destroy = true
   }
+
+  lifecycle_rule {
+    id      = "expire_cloudfront_logs"
+    enabled = true
+
+    expiration {
+      days = 30
+    }
+  }
 }
 
 resource "aws_s3_bucket" "cloudfront_logs" {
