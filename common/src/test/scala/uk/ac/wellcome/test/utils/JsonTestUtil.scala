0eeeac5c0eab5b3dd850ce84bf2a5c6bafb6e19e
Alex Chan <a.chan@wellcome.ac.uk>
2018-04-11 13:25:55 +0100

    JsonTestUtil.scala: why not just extend Matchers?

--- common/src/test/scala/uk/ac/wellcome/test/utils/JsonTestUtil.scala
+++ common/src/test/scala/uk/ac/wellcome/test/utils/JsonTestUtil.scala
@@ -3,7 +3,7 @@ package uk.ac.wellcome.test.utils
 import org.scalatest.Matchers
 import io.circe.parser._
 
-trait JsonTestUtil { this: Matchers =>
+trait JsonTestUtil extends Matchers {
 
   def assertJsonStringsAreEqual(json1: String, json2: String) = {
     val tree1 = parse(json1).right.get
