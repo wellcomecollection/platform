4de4aa0f2cbd9ed068b6bc061c5c1292041653ea
Alex Chan <a.chan@wellcome.ac.uk>
2018-04-12 13:27:33 +0100

    loris: put the expiry policy on the correct bucket

--- loris/terraform/s3.tf
+++ loris/terraform/s3.tf
@@ -10,22 +10,4 @@ resource "aws_s3_bucket" "wellcomecollection-miro-images-public" {
   lifecycle {
     prevent_destroy = true
   }
-
-  lifecycle_rule {
-    id      = "expire_cloudfront_logs"
-    enabled = true
-
-    expiration {
-      days = 30
-    }
-  }
-}
-
-resource "aws_s3_bucket" "cloudfront_logs" {
-  bucket = "wellcomecollection-platform-logs-cloudfront"
-  acl    = "private"
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
