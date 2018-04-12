cb6cc4c267a457cdc911d0f9a2ffa410f30bd593
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-04-12 10:17:23 +0100

    update elasticdump test

--- data_api/elasticdump/test_run_elasticdump.py
+++ data_api/elasticdump/test_run_elasticdump.py
@@ -4,6 +4,7 @@ import gzip
 import json
 import subprocess
 
+import attr
 import requests
 import pytest
 
@@ -12,25 +13,25 @@ import run_elasticdump
 
 @pytest.mark.parametrize('environ_config, index, expected_url', [
     (
-        {
-            'es_username': 'elastic',
-            'es_password': 'changeme',
-            'es_hostname': 'example.org',
-            'es_port': 9200,
-        },
-        'index_with_no_scheme',
-        'https://elastic:changeme@example.org:9200/index_with_no_scheme'
+            {
+                'es_username': 'elastic',
+                'es_password': 'changeme',
+                'es_hostname': 'example.org',
+                'es_port': 9200,
+            },
+            'index_with_no_scheme',
+            'https://elastic:changeme@example.org:9200/index_with_no_scheme'
     ),
     (
-        {
-            'es_username': 'elastic',
-            'es_password': 'changeme',
-            'es_hostname': 'example.org',
-            'es_port': 9200,
-            'es_scheme': 'http://',
-        },
-        'index_with_http_scheme',
-        'http://elastic:changeme@example.org:9200/index_with_http_scheme'
+            {
+                'es_username': 'elastic',
+                'es_password': 'changeme',
+                'es_hostname': 'example.org',
+                'es_port': 9200,
+                'es_scheme': 'http://',
+            },
+            'index_with_http_scheme',
+            'http://elastic:changeme@example.org:9200/index_with_http_scheme'
     ),
 ])
 def test_build_elasticsearch_url(environ_config, index, expected_url):
@@ -49,39 +50,28 @@ def test_getting_no_messages_from_sqs_is_sysexit(sqs_client, queue_url):
         )
 
 
-def test_getting_only_one_message_from_sqs(sqs_client, queue_url):
-    messages = [
-        {'message': f'Hello {name}'}
-        for name in ('cat', 'dog')
-    ]
-    for m in messages:
-        sqs_client.send_message(QueueUrl=queue_url, MessageBody=json.dumps(m))
-
-    received_message = run_elasticdump.get_message(
-        sqs_client=sqs_client,
-        sqs_queue_url=queue_url
-    )
-    received_body = json.loads(received_message['Body'])
-    assert received_body in messages
-
-
 def test_end_to_end(
-    sqs_client,
-    queue_url,
-    s3_client,
-    bucket,
-    sqs_endpoint_url,
-    s3_endpoint_url,
-    elasticsearch_index,
-    elasticsearch_url,
-    elasticsearch_hostname
+        sqs_client,
+        queue_url,
+        s3_client,
+        bucket,
+        sqs_endpoint_url,
+        s3_endpoint_url,
+        elasticsearch_index,
+        elasticsearch_url,
+        elasticsearch_hostname
 ):
     sqs_client.send_message(
         QueueUrl=queue_url,
-        MessageBody=json.dumps({
-            'target_bucket': bucket,
-            'target_key': 'dump.txt.gz',
-        })
+        MessageBody=json.dumps(
+            attr.asdict(
+                run_elasticdump.SnapshotRequest(
+                    "time",
+                    bucket,
+                    elasticsearch_index
+                )
+            )
+        )
     )
 
     for i in range(10):
@@ -104,7 +94,6 @@ def test_end_to_end(
     subprocess.check_call([
         'docker', 'run', '--net', 'host',
         '--env', f'sqs_queue_url={queue_url}',
-        '--env', f'upload_bucket={bucket}',
         '--env', f'target_key=dump.txt.gz',
         '--env', f'key_prefix=blah/',
 
@@ -114,7 +103,6 @@ def test_end_to_end(
 
         '--env', f'local_s3_endpoint={s3_endpoint_url}',
         '--env', f'local_sqs_endpoint={sqs_endpoint_url}',
-        '--env', f'es_index={elasticsearch_index}',
         '--env', f'es_username=elastic',
         '--env', f'es_password=changeme',
         '--env', f'es_hostname={elasticsearch_hostname}',
@@ -141,3 +129,33 @@ def test_end_to_end(
         }
         for i in range(10)
     ]
+
+
+def test_getting_only_one_message_from_sqs(
+        sqs_client,
+        queue_url,
+        s3_client,
+        bucket,
+        sqs_endpoint_url,
+        s3_endpoint_url,
+        elasticsearch_index,
+        elasticsearch_url,
+        elasticsearch_hostname
+):
+    requests = [
+        attr.asdict(
+            run_elasticdump.SnapshotRequest(time, bucket, elasticsearch_index)
+        ) for time in ('now', 'then')
+    ]
+    for r in requests:
+        sqs_client.send_message(
+            QueueUrl=queue_url,
+            MessageBody=json.dumps(r)
+        )
+
+    received_message = run_elasticdump.get_message(
+        sqs_client=sqs_client,
+        sqs_queue_url=queue_url
+    )
+    received_body = json.loads(received_message['Body'])
+    assert received_body in requests
