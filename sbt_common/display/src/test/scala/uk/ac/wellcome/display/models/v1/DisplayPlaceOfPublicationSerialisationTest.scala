706fb5c8007cb397bd5e2fe338b67dcaa6a486a3
caylaa <alicefuzier@gmail.com>
2018-04-12 18:30:54 +0100

    Split the DisplayWorkV1 tests into multiple classes

---
+++ sbt_common/display/src/test/scala/uk/ac/wellcome/display/models/v1/DisplayPlaceOfPublicationSerialisationTest.scala
@@ -0,0 +1,51 @@
+package uk.ac.wellcome.display.models.v1
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.inject.Guice
+import org.scalatest.FunSpec
+import uk.ac.wellcome.display.models.{DisplaySerialisationTestBase, WorksUtil}
+import uk.ac.wellcome.display.modules.DisplayJacksonModule
+import uk.ac.wellcome.models.{IdentifiedWork, Place}
+import uk.ac.wellcome.test.utils.JsonTestUtil
+
+class DisplayPlaceOfPublicationSerialisationTest extends FunSpec
+  with DisplaySerialisationTestBase
+  with JsonTestUtil
+  with WorksUtil {
+  val injector = Guice.createInjector(DisplayJacksonModule)
+
+  val objectMapper = injector.getInstance(classOf[ObjectMapper])
+
+    it("serialises the placesOfPublication field") {
+      val work = IdentifiedWork(
+        canonicalId = "avfpwgrr",
+        sourceIdentifier = sourceIdentifier,
+        title = Some("Ahoy!  Armoured angelfish are attacking the armada!"),
+        placesOfPublication = List(Place("Durmstrang")),
+        version = 1
+      )
+      val displayWork = DisplayWorkV1(work)
+
+      val actualJson = objectMapper.writeValueAsString(displayWork)
+      val expectedJson = s"""
+                            |{
+                            |  "type": "Work",
+                            |  "id": "${work.canonicalId}",
+                            |  "title": "${work.title.get}",
+                            |  "creators": [ ],
+                            |  "subjects": [ ],
+                            |  "genres": [ ],
+                            |  "publishers": [],
+                            |  "placesOfPublication": [
+                            |    {
+                            |      "label": "${work.placesOfPublication.head.label}",
+                            |      "type": "Place"
+                            |    }
+                            |  ]
+                            |
+                             |}""".stripMargin
+
+      assertJsonStringsAreEqual(actualJson, expectedJson)
+    }
+
+}
---
+++ sbt_common/display/src/test/scala/uk/ac/wellcome/display/models/v1/DisplayPublishersSerialisationTest.scala
@@ -0,0 +1,64 @@
+package uk.ac.wellcome.display.models.v1
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.inject.Guice
+import org.scalatest.FunSpec
+import uk.ac.wellcome.display.models.{DisplaySerialisationTestBase, WorksUtil}
+import uk.ac.wellcome.display.modules.DisplayJacksonModule
+import uk.ac.wellcome.models._
+import uk.ac.wellcome.test.utils.JsonTestUtil
+
+class DisplayPublishersSerialisationTest
+  extends FunSpec
+    with DisplaySerialisationTestBase
+    with JsonTestUtil
+    with WorksUtil {
+  val injector = Guice.createInjector(DisplayJacksonModule)
+
+  val objectMapper = injector.getInstance(classOf[ObjectMapper])
+
+    it(
+      "includes the publishers field with a mixture of agents/organisations/persons") {
+      val work = IdentifiedWork(
+        canonicalId = "v9w6cz66",
+        sourceIdentifier = sourceIdentifier,
+        version = 1,
+        title = Some("Vultures vying for victory"),
+        publishers = List(
+          Unidentifiable(Agent("Vivian Violet")),
+          Unidentifiable(Organisation("Verily Volumes")),
+          Unidentifiable(
+            Person(
+              label = "Havelock Vetinari",
+              prefix = Some("Lord Patrician"),
+              numeration = Some("I")))
+        )
+      )
+      val displayWork = DisplayWorkV1(work)
+
+      val actualJson = objectMapper.writeValueAsString(displayWork)
+      val expectedJson = s"""
+                            |{
+                            |  "type": "Work",
+                            |  "id": "${work.canonicalId}",
+                            |  "title": "${work.title.get}",
+                            |  "creators": [ ],
+                            |  "subjects": [ ],
+                            |  "genres": [ ],
+                            |  "publishers": [
+                            |    ${identifiedOrUnidentifiable(
+        work.publishers(0),
+        abstractAgent)},
+                            |    ${identifiedOrUnidentifiable(
+        work.publishers(1),
+        abstractAgent)},
+                            |    ${identifiedOrUnidentifiable(
+        work.publishers(2),
+        abstractAgent)}
+                            |  ],
+                            |  "placesOfPublication": [ ]
+                            |}""".stripMargin
+
+      assertJsonStringsAreEqual(actualJson, expectedJson)
+    }
+}
