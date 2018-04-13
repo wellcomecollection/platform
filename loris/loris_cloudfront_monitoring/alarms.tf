f4d23db79adeb040f231120f33824c75f42bbd3d
Alex Chan <a.chan@wellcome.ac.uk>
2018-04-13 11:26:45 +0100

    Create a CloudFront error alarm topic, and subscribe the alarm to it

--- loris/loris_cloudfront_monitoring/alarms.tf
+++ loris/loris_cloudfront_monitoring/alarms.tf
@@ -2,11 +2,9 @@
 # 4xx errors may indicate a systemic problem with Loris (e.g. accidentally
 # deleting all the images in the S3 bucket!).
 #
-# So we look for an unusually high proportion of CloudFront 4xxs.
+# So we look for an unusually high number of CloudFront 4xxs.
 #
-# In the last three weeks before our last incident, we'd never seen more than
-# 1% of requests fail with a 4xx error, so 1.5% is a rough estimate of an
-# "unusual" number of requests.
+# This is a finger-in-the-air estimate based on historic CloudFront stats.
 #
 resource "aws_cloudwatch_metric_alarm" "cloudfront_4xx_alarm" {
   alarm_name = "loris-cloudfront_4xx_error"
@@ -17,11 +15,14 @@ resource "aws_cloudwatch_metric_alarm" "cloudfront_4xx_alarm" {
   namespace           = "AWS/CloudFront"
   period              = "60"
   statistic           = "Sum"
-  threshold           = "1.5"
+  threshold           = "2500"
   treat_missing_data  = "breaching"
 
   dimensions {
     DistributionId = "${local.loris_cloudfront_id}"
     Region         = "Global"
   }
+
+  alarm_description = "Monitors 4xx errors from the Loris CloudFront distro"
+  alarm_actions = ["${module.cloudfront_errors_topic.arn}"]
 }
