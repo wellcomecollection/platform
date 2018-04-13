1e18b0369f146ef2494f185af49daec2e439e5fb
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-04-13 12:00:31 +0100

    Try to set up a Lambda for post-to-slack on CloudFront errors

--- loris/loris_cloudfront_monitoring/locals.tf
+++ loris/loris_cloudfront_monitoring/locals.tf
@@ -1,3 +1,5 @@
 locals {
   loris_cloudfront_id = "${data.terraform_remote_state.loris.loris_cloudfront_id}"
+
+  lambda_error_alarm_arn     = "${data.terraform_remote_state.shared_infra.lambda_error_alarm_arn}"
 }
---
+++ loris/loris_cloudfront_monitoring/variables.tf
@@ -0,0 +1,5 @@
+variable "infra_bucket" {}
+
+variable "critical_slack_webhook" {}
+variable "non_critical_slack_webhook" {}
+variable "bitly_access_token" {}
