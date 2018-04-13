7ce565b863428c4cc28506d6282fac90054dc106
caylaa <alicefuzier@gmail.com>
2018-04-13 10:35:55 +0100

    Swagger docs for the contributor model in v2

--- sbt_common/display/src/main/scala/uk/ac/wellcome/display/models/v2/DisplayContributor.scala
+++ sbt_common/display/src/main/scala/uk/ac/wellcome/display/models/v2/DisplayContributor.scala
@@ -1,10 +1,22 @@
 package uk.ac.wellcome.display.models.v2
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.annotations.{ApiModel, ApiModelProperty}
 import uk.ac.wellcome.display.models.DisplayAbstractAgent
 
+@ApiModel(
+  value = "Contributor",
+  description =
+    "A contributor"
+)
 case class DisplayContributor (
-                              agent: DisplayAbstractAgent,
+                                @ApiModelProperty(
+                                  dataType = "uk.ac.wellcome.display.models.DisplayAbstractAgent",
+                                  value = "The agent.")
+                                agent: DisplayAbstractAgent,
+                                @ApiModelProperty(
+                                  dataType = "List[uk.ac.wellcome.display.models.v2.DisplayContributionRole]",
+                                  value = "The agent.")
                               roles: List[DisplayContributionRole],
   @JsonProperty("type") ontologyType: String = "Contributor"
                               )
