4e3cfa801037346ff683437a79a1f5495f379791
Alex Chan <alex@alexwlchan.net>
2018-04-13 11:30:09 +0100

    Remove some unused outputs from our Terraform definitions (#1885)
    * Remove two unused outputs from the Loris TF stack
    
    * Remove some unused outputs from the shared_infra TF stack
    
    * Remove an unused output from the catalogue_pipeline TF stack

--- catalogue_api/terraform/locals.tf
+++ catalogue_api/terraform/locals.tf
@@ -7,6 +7,4 @@ locals {
   alb_client_error_alarm_arn = "${data.terraform_remote_state.shared_infra.alb_client_error_alarm_arn}"
 
   bucket_alb_logs_id = "${data.terraform_remote_state.shared_infra.bucket_alb_logs_id}"
-
-  run_ecs_task_topic_arn = "${data.terraform_remote_state.shared_infra.run_ecs_task_topic_arn}"
 }
--- shared_infra/outputs.tf
+++ shared_infra/outputs.tf
@@ -2,18 +2,6 @@ output "lambda_error_alarm_arn" {
   value = "${module.lambda_error_alarm.arn}"
 }
 
-output "run_ecs_task_topic_arn" {
-  value = "${module.run_ecs_task.arn}"
-}
-
-output "run_ecs_task_topic_publish_policy" {
-  value = "${module.run_ecs_task.publish_policy}"
-}
-
-output "iam_policy_document_describe_services" {
-  value = "${data.aws_iam_policy_document.describe_services.json}"
-}
-
 output "ec2_instance_terminating_for_too_long_alarm_arn" {
   value = "${module.ec2_instance_terminating_for_too_long_alarm.arn}"
 }
@@ -44,22 +32,6 @@ output "ec2_terminating_topic_publish_policy" {
 
 output "bucket_alb_logs_id" {
   value = "${aws_s3_bucket.alb_logs.id}"
-}
-
-output "travis_ci_aws_id" {
-  value = "${aws_iam_access_key.travis_ci.id}"
-}
-
-output "travis_ci_aws_secret" {
-  value = "${aws_iam_access_key.travis_ci.encrypted_secret}"
-}
-
-output "bucket_wellcomecollection_images_name" {
-  value = "${aws_s3_bucket.wellcomecollection-images.id}"
-}
-
-output "bucket_wellcomecollection_images_arn" {
-  value = "${aws_s3_bucket.wellcomecollection-images.arn}"
 }
 
 output "terraform_apply_topic_name" {
