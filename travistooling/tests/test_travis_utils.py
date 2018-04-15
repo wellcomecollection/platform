91caaf2c7da8f8098237007aa5a5e33e04f68486
Alex Chan <alex@alexwlchan.net>
2018-04-15 16:13:30 +0100

    Try mocking the environment variables instead of replacing

--- travistooling/tests/test_travis_utils.py
+++ travistooling/tests/test_travis_utils.py
@@ -2,6 +2,7 @@
 
 import os
 
+import mock
 import pytest
 
 from travistooling import travis_utils
@@ -13,21 +14,23 @@ def test_not_in_travis_is_an_error():
 
 
 def test_travis_branch_name_on_master():
-    os.environ = {
+    mock_environ = {
         'TRAVIS_PULL_REQUEST': 'false',
         'TRAVIS_BRANCH': 'master',
         'TRAVIS_PULL_REQUEST_BRANCH': 'feature-branch',
     }
-    assert travis_utils.branch_name() == 'master'
+    with mock.patch.dict(os.environ, mock_environ, clear=True):
+        assert travis_utils.branch_name() == 'master'
 
 
 def test_travis_branch_name_on_pr():
-    os.environ = {
+    mock_environ = {
         'TRAVIS_PULL_REQUEST': 'true',
         'TRAVIS_BRANCH': 'master',
         'TRAVIS_PULL_REQUEST_BRANCH': 'feature-branch',
     }
-    assert travis_utils.branch_name() == 'feature-branch'
+    with mock.patch.dict(os.environ, mock_environ, clear=True):
+        assert travis_utils.branch_name() == 'feature-branch'
 
 
 def test_unpack_secrets():
@@ -36,17 +39,18 @@ def test_unpack_secrets():
 
 
 def test_unpack_secrets_with_no_secrets_file():
-    os.environ = {
+    mock_environ = {
         'encrypted_83630750896a_key': 'K3Y',
         'encrypted_83630750896a_iv': '1V',
     }
-    with pytest.raises(SystemExit) as err:
-        travis_utils.unpack_secrets()
+    with mock.patch.dict(os.environ, mock_environ, clear=True):
+        with pytest.raises(SystemExit) as err:
+            travis_utils.unpack_secrets()
     assert err.value.code == 1
 
 
 def test_unpack_secrets_with_no_env_vars():
-    os.environ = {}
-    with pytest.raises(SystemExit) as err:
-        travis_utils.unpack_secrets()
+    with mock.patch.dict(os.environ, {}, clear=True):
+        with pytest.raises(SystemExit) as err:
+            travis_utils.unpack_secrets()
     assert err.value.code == 2
