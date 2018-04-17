85ae1474ee483d7ff1f961ba0c530fdfd70f4e1e
Alex Chan <a.chan@wellcome.ac.uk>
2018-04-17 11:27:13 +0100

    Add circe Encoder/Decoder for ApiVersions

--- common/src/main/scala/uk/ac/wellcome/versions/ApiVersions.scala
+++ common/src/main/scala/uk/ac/wellcome/versions/ApiVersions.scala
@@ -1,5 +1,7 @@
 package uk.ac.wellcome.versions
 
+import io.circe.{Decoder, Encoder}
+
 object ApiVersions extends Enumeration {
   // We need to explicitly state the string representation of
   // each value as we've seen inconsistent behaviour where v2 would be
@@ -7,4 +9,7 @@ object ApiVersions extends Enumeration {
   val v1 = Value("v1")
   val v2 = Value("v2")
   val default = v2
+
+  implicit val decoder = Decoder.enumDecoder(ApiVersions)
+  implicit val encoder = Encoder.enumEncoder(ApiVersions)
 }
