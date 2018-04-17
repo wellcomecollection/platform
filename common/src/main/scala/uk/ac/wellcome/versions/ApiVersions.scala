1e95a2b49aa9895e37f2c851cc16baa419c7918f
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-04-17 11:46:04 +0100

    Explanatory comment

--- common/src/main/scala/uk/ac/wellcome/versions/ApiVersions.scala
+++ common/src/main/scala/uk/ac/wellcome/versions/ApiVersions.scala
@@ -1,6 +1,9 @@
 package uk.ac.wellcome.versions
 
 object ApiVersions extends Enumeration {
+  // We need to explicitly state the string representation of
+  // each value as we've seen inconsistent behaviour where v2 would be
+  // serialised as "v2" or "default" apparently unpredictably
   val v1 = Value("v1")
   val v2 = Value("v2")
   val default = v2
