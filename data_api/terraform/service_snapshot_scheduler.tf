702d96007d1fa61f9117b486d75f78ba25a556e7
caylaa <alicefuzier@gmail.com>
2018-04-18 10:24:50 +0100

    use es_index_v1 and es_index_v2 in data_api stack

--- data_api/terraform/service_snapshot_scheduler.tf
+++ data_api/terraform/service_snapshot_scheduler.tf
@@ -4,8 +4,8 @@ module "snapshot_scheduler" {
   lambda_error_alarm_arn = "${local.lambda_error_alarm_arn}"
   infra_bucket           = "${var.infra_bucket}"
 
-  es_index_v1 = "${local.es_index}"
-  es_index_v2 = "${local.es_index}"
+  es_index_v1 = "${local.es_index_v1}"
+  es_index_v2 = "${local.es_index_v2}"
 
   private_bucket_name = "${aws_s3_bucket.private_data.id}"
   public_bucket_name  = "${aws_s3_bucket.public_data.id}"
