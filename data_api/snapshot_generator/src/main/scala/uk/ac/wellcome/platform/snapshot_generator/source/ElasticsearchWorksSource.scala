173333b93cc2dec62aee01c1ed99dabe60e15ec6
caylaa <alicefuzier@gmail.com>
2018-04-19 13:45:45 +0100

    Be a bit more chatty

--- data_api/snapshot_generator/src/main/scala/uk/ac/wellcome/platform/snapshot_generator/source/ElasticsearchWorksSource.scala
+++ data_api/snapshot_generator/src/main/scala/uk/ac/wellcome/platform/snapshot_generator/source/ElasticsearchWorksSource.scala
@@ -2,17 +2,22 @@ package uk.ac.wellcome.platform.snapshot_generator.source
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import com.sksamuel.elastic4s.http.ElasticDsl.{search, termQuery}
 import com.sksamuel.elastic4s.http.HttpClient
 import com.sksamuel.elastic4s.http.search.SearchHit
 import com.sksamuel.elastic4s.streams.ReactiveElastic._
+import com.twitter.inject.Logging
 import uk.ac.wellcome.models.IdentifiedWork
 import uk.ac.wellcome.utils.JsonUtil._
 
-object ElasticsearchWorksSource {
+object ElasticsearchWorksSource extends Logging{
   def apply(elasticClient: HttpClient, indexName: String, itemType: String)(
-    implicit actorSystem: ActorSystem): Source[IdentifiedWork, NotUsed] = {
+    implicit actorSystem: ActorSystem): Source[IdentifiedWork, NotUsed] ={
+    val loggingSink = Flow[IdentifiedWork].grouped(1000).map(works => {
+      logger.info(s"Received ${works.length} works from $indexName")
+      works
+    }).to(Sink.ignore)
     Source
       .fromPublisher(
         elasticClient.publisher(
@@ -21,6 +26,6 @@ object ElasticsearchWorksSource {
             .scroll("10m")))
       .map { searchHit: SearchHit =>
         fromJson[IdentifiedWork](searchHit.sourceAsString).get
-      }
-  }
+      }.alsoTo(loggingSink)
+    }
 }
