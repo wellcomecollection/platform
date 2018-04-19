8afa3815ad829a4c544633babaf966ad44ac396d
caylaa <alicefuzier@gmail.com>
2018-04-19 13:46:23 +0100

    Increase queue visibility timeout

--- data_api/terraform/queues.tf
+++ data_api/terraform/queues.tf
@@ -1,9 +1,10 @@
 module "snapshot_generator_queue" {
-  source      = "git::https://github.com/wellcometrust/terraform-modules.git//sqs?ref=v1.1.0"
+  source      = "git::https://github.com/wellcometrust/terraform-modules.git//sqs?ref=v8.0.2"
   queue_name  = "snapshot_generator_queue"
   aws_region  = "${var.aws_region}"
   account_id  = "${data.aws_caller_identity.current.account_id}"
   topic_names = ["${module.snapshot_scheduler.topic_name}"]
 
   alarm_topic_arn = "${local.dlq_alarm_arn}"
+  visibility_timeout_seconds = 600
 }
