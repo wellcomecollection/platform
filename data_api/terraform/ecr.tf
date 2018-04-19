618743f6d3f11ae2cf82411dcdd4ad404b9c7c29
caylaa <alicefuzier@gmail.com>
2018-04-19 09:51:46 +0100

    Remove elasticdump

--- data_api/terraform/ecr.tf
+++ data_api/terraform/ecr.tf
@@ -2,8 +2,3 @@ module "ecr_repository_snapshot_generator" {
   source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
   name   = "snapshot_generator"
 }
-
-module "ecr_repository_elasticdump" {
-  source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
-  name   = "elasticdump"
-}
