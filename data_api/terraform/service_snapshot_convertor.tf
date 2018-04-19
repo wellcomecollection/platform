54d1a913986c7e5353331539cbd24b5df895d481
caylaa <alicefuzier@gmail.com>
2018-04-19 09:34:47 +0100

    Adding config variables to terraform configuration

--- data_api/terraform/service_snapshot_convertor.tf
+++ data_api/terraform/service_snapshot_convertor.tf
@@ -1,3 +1,12 @@
+data "template_file" "es_cluster_host_snapshot" {
+  template = "$${name}.$${region}.aws.found.io"
+
+  vars {
+    name   = "${var.es_config_snapshot["name"]}"
+    region = "${var.es_config_snapshot["region"]}"
+  }
+}
+
 module "snapshot_convertor" {
   source = "git::https://github.com/wellcometrust/terraform-modules.git//sqs_autoscaling_service?ref=v8.0.0"
   name   = "snapshot_convertor"
@@ -11,9 +20,18 @@ module "snapshot_convertor" {
   env_vars = {
     queue_url = "${module.snapshot_convertor_queue.id}"
     topic_arn = "${module.snapshot_conversion_complete_topic.arn}"
+    es_host           = "${data.template_file.es_cluster_host_snapshot.rendered}"
+    es_port           = "${var.es_config_snapshot["port"]}"
+    es_name           = "${var.es_config_snapshot["name"]}"
+    es_index_v1       = "${var.es_config_snapshot["index_v1"]}"
+    es_index_v2       = "${var.es_config_snapshot["index_v2"]}"
+    es_doc_type       = "${var.es_config_snapshot["doc_type"]}"
+    es_username       = "${var.es_config_snapshot["username"]}"
+    es_password       = "${var.es_config_snapshot["password"]}"
+    es_protocol       = "${var.es_config_snapshot["protocol"]}"
   }
 
-  env_vars_length = 2
+  env_vars_length = 11
 
   memory = 2048
   cpu    = 512
