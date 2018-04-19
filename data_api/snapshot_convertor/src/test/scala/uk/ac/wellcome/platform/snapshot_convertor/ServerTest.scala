8e108cd1ee2287b0e91b556427f08d7e49eeaf7a
caylaa <alicefuzier@gmail.com>
2018-04-19 08:28:58 +0100

    Add elasticsearch flags to server test

--- data_api/snapshot_convertor/src/test/scala/uk/ac/wellcome/platform/snapshot_convertor/ServerTest.scala
+++ data_api/snapshot_convertor/src/test/scala/uk/ac/wellcome/platform/snapshot_convertor/ServerTest.scala
@@ -3,6 +3,7 @@ package uk.ac.wellcome.platform.snapshot_convertor
 import com.twitter.finagle.http.Status.Ok
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.FunSpec
+import uk.ac.wellcome.elasticsearch.test.fixtures.ElasticsearchFixtures
 import uk.ac.wellcome.test.fixtures.{S3, SNS, SQS}
 
 class ServerTest
@@ -11,19 +12,25 @@ class ServerTest
     with SNS
     with SQS
     with ScalaFutures
+    with ElasticsearchFixtures
     with fixtures.Server {
+  val itemType = "work"
 
   it("shows the healthcheck message") {
     withLocalSqsQueue { queue =>
       withLocalSnsTopic { topic =>
         withLocalS3Bucket { bucket =>
-          val flags = snsLocalFlags(topic) ++ sqsLocalFlags(queue) ++ s3LocalFlags(
-            bucket)
-          withServer(flags) { server =>
-            server.httpGet(
-              path = "/management/healthcheck",
-              andExpect = Ok,
-              withJsonBody = """{"message": "ok"}""")
+          withLocalElasticsearchIndex(itemType = itemType) { indexNameV1 =>
+            withLocalElasticsearchIndex(itemType = itemType) { indexNameV2 =>
+              val flags = snsLocalFlags(topic) ++ sqsLocalFlags(queue) ++ s3LocalFlags(
+                bucket) ++ esLocalFlags(indexNameV1, indexNameV2, itemType)
+              withServer(flags) { server =>
+                server.httpGet(
+                  path = "/management/healthcheck",
+                  andExpect = Ok,
+                  withJsonBody = """{"message": "ok"}""")
+              }
+            }
           }
         }
       }
