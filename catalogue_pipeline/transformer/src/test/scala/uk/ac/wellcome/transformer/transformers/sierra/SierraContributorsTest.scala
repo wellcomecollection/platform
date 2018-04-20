e4e1eff4f18931376a80d69f80208eaffc13bda3
Alex Chan <a.chan@wellcome.ac.uk>
2018-04-20 09:55:00 +0100

    transformer: add tests that we get Person.label for contributors

--- catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraContributorsTest.scala
+++ catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraContributorsTest.scala
@@ -18,4 +18,87 @@ class SierraContributorsTest extends FunSpec with Matchers {
     val contributors = transformer.getContributors(bibData)
     contributors shouldBe List()
   }
+
+  describe("Person") {
+    it("gets the name from MARC tag 100 subfield $$a") {
+      val name = "Carol the Carrot"
+
+      val bibData = SierraBibData(
+        id = "7344264",
+        title = None,
+        varFields = List(
+          VarField(
+            fieldTag = "p",
+            marcTag = "100",
+            indicator1 = "",
+            indicator2 = "",
+            subfields = List(MarcSubfield(tag = "a", content = name))))
+      )
+
+      val contributors = transformer.getContributors(bibData)
+      contributors shouldBe List(Unidentifiable(Person(label = name)))
+    }
+
+    it("gets the name from MARC tag 700 subfield $$a") {
+      val name = "Bertrand the Beetroot"
+
+      val bibData = SierraBibData(
+        id = "4530272",
+        title = None,
+        varFields = List(
+          VarField(
+            fieldTag = "p",
+            marcTag = "700",
+            indicator1 = "",
+            indicator2 = "",
+            subfields = List(MarcSubfield(tag = "a", content = name))))
+      )
+
+      val contributors = transformer.getContributors(bibData)
+      contributors shouldBe List(Unidentifiable(Person(label = name)))
+    }
+
+    it("gets the name from MARC tags 100 and 700 subfield $$a in the right order") {
+      val name1 = "Alfie the Artichoke"
+      val name2 = "Alison the Apple"
+      val name3 = "Archie the Aubergine"
+
+      // The correct ordering is "everything from 100 first, then 700", and
+      // we deliberately pick an ordering that's different from that for
+      // the MARC fields, so we can check it really is applying this rule.
+      val bibData = SierraBibData(
+        id = "8261371",
+        title = None,
+        varFields = List(
+          VarField(
+            fieldTag = "p",
+            marcTag = "700",
+            indicator1 = "",
+            indicator2 = "",
+            subfields = List(MarcSubfield(tag = "a", content = name2))
+          ),
+          VarField(
+            fieldTag = "p",
+            marcTag = "100",
+            indicator1 = "",
+            indicator2 = "",
+            subfields = List(MarcSubfield(tag = "a", content = name1))
+          ),
+          VarField(
+            fieldTag = "p",
+            marcTag = "700",
+            indicator1 = "",
+            indicator2 = "",
+            subfields = List(MarcSubfield(tag = "a", content = name3))
+          )
+        )
+      )
+
+      val contributors = transformer.getContributors(bibData)
+      contributors shouldBe List(
+        Unidentifiable(Person(label = name1)),
+        Unidentifiable(Person(label = name2))
+      )
+    }
+  }
 }
