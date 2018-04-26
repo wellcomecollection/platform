34a0b36638f4131c3f61e79e632722c0689ae363
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-04-26 10:05:09 +0000

    Apply auto-formatting rules

--- common/src/test/scala/uk/ac/wellcome/models/ConceptTest.scala
+++ common/src/test/scala/uk/ac/wellcome/models/ConceptTest.scala
@@ -8,18 +8,18 @@ class ConceptTest extends FunSpec with Matchers with JsonTestUtil {
 
   val concept = Concept(label = "Woodwork")
   val expectedJson =
-      s"""{
+    s"""{
         "ontologyType": "Concept",
         "label": "Woodwork"
       }"""
 
   it("serialises Concepts to JSON") {
-      val actualJson = toJson(concept).get
-      assertJsonStringsAreEqual(actualJson, expectedJson)
+    val actualJson = toJson(concept).get
+    assertJsonStringsAreEqual(actualJson, expectedJson)
   }
 
   it("deserialises JSON as Concepts") {
-      val parsedConcept = fromJson[Concept](expectedJson).get
-      parsedConcept shouldBe concept
+    val parsedConcept = fromJson[Concept](expectedJson).get
+    parsedConcept shouldBe concept
   }
 }
