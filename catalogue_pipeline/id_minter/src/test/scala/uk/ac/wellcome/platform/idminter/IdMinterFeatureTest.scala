e330c29e75fe98827a9c2d3e31d76f2c615a7e0f
caylaa <alicefuzier@gmail.com>
2018-04-27 15:12:25 +0100

    Feature test asserting that the id_minter sends a message pointer to sns instead of the whole work

--- catalogue_pipeline/id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/IdMinterFeatureTest.scala
+++ catalogue_pipeline/id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/IdMinterFeatureTest.scala
@@ -2,22 +2,16 @@ package uk.ac.wellcome.platform.idminter
 
 import org.scalatest.concurrent.Eventually
 import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.messaging.message.MessagePointer
+import uk.ac.wellcome.messaging.sqs.SQSMessage
 import uk.ac.wellcome.messaging.test.fixtures.SQS.Queue
-import uk.ac.wellcome.messaging.test.fixtures.{
-  MessageInfo,
-  Messaging,
-  SNS,
-  SQS
-}
-import uk.ac.wellcome.models.work.internal.{
-  IdentifiedWork,
-  IdentifierSchemes,
-  SourceIdentifier,
-  UnidentifiedWork
-}
+import uk.ac.wellcome.messaging.test.fixtures.{MessageInfo, Messaging, SNS, SQS}
+import uk.ac.wellcome.models.work.internal.{IdentifiedWork, IdentifierSchemes, SourceIdentifier, UnidentifiedWork}
 import uk.ac.wellcome.storage.s3.S3ObjectLocation
 import uk.ac.wellcome.storage.test.fixtures.S3
+import uk.ac.wellcome.storage.test.fixtures.S3.Bucket
 import uk.ac.wellcome.test.utils.ExtendedPatience
+import uk.ac.wellcome.utils.JsonUtil
 import uk.ac.wellcome.utils.JsonUtil._
 
 import scala.collection.JavaConversions._
@@ -33,25 +27,6 @@ class IdMinterFeatureTest
     with ExtendedPatience
     with Eventually
     with Matchers {
-
-  private def assertMessageIsNotDeleted(queue: Queue): Unit = {
-    // After a message is read, it stays invisible for 1 second and then it gets sent again.
-    // So we wait for longer than the visibility timeout and then we assert that it has become
-    // invisible again, which means that the id_minter picked it up again,
-    // and so it wasn't deleted as part of the first run.
-    // TODO Write this test using dead letter queues once https://github.com/adamw/elasticmq/issues/69 is closed
-    Thread.sleep(2000)
-
-    eventually {
-      sqsClient
-        .getQueueAttributes(
-          queue.url,
-          List("ApproximateNumberOfMessagesNotVisible")
-        )
-        .getAttributes
-        .get("ApproximateNumberOfMessagesNotVisible") shouldBe "1"
-    }
-  }
 
   it("mints the same IDs where source identifiers match") {
     withLocalSqsQueue { queue =>
@@ -165,6 +140,50 @@ class IdMinterFeatureTest
           }
         }
       }
+    }
+  }
+
+  private def getWorksFromMessages(messages: List[MessageInfo]): List[IdentifiedWork] =
+    messages.map{ m =>
+      val messagePointer = fromJson[MessagePointer](m.message).get
+      val messageContent = getContentFromS3(Bucket(messagePointer.src.bucket), messagePointer.src.key)
+      fromJson[IdentifiedWork](messageContent).get
+    }
+
+  private def generateSqsMessage(MiroID: String): SQSMessage = {
+    val identifier =
+      SourceIdentifier(IdentifierSchemes.miroImageNumber, "Work", MiroID)
+
+    val work = UnidentifiedWork(
+      title = Some("A query about a queue of quails"),
+      sourceIdentifier = identifier,
+      version = 1,
+      identifiers = List(identifier))
+
+    SQSMessage(
+      Some("subject"),
+      JsonUtil.toJson(work).get,
+      "topic",
+      "messageType",
+      "timestamp")
+  }
+
+  private def assertMessageIsNotDeleted(queue: Queue): Unit = {
+    // After a message is read, it stays invisible for 1 second and then it gets sent again.
+    // So we wait for longer than the visibility timeout and then we assert that it has become
+    // invisible again, which means that the id_minter picked it up again,
+    // and so it wasn't deleted as part of the first run.
+    // TODO Write this test using dead letter queues once https://github.com/adamw/elasticmq/issues/69 is closed
+    Thread.sleep(2000)
+
+    eventually {
+      sqsClient
+        .getQueueAttributes(
+          queue.url,
+          List("ApproximateNumberOfMessagesNotVisible")
+        )
+        .getAttributes
+        .get("ApproximateNumberOfMessagesNotVisible") shouldBe "1"
     }
   }
 }
