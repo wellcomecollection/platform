3972b5e6afe8f7a937b32825b5aeeedb5f9d487d
Alex Chan <a.chan@wellcome.ac.uk>
2018-04-30 11:01:36 +0100

    data_science: finish a script for toggling the ASG size

--- data_science/scripts/toggle_asg.py
+++ data_science/scripts/toggle_asg.py
@@ -14,8 +14,29 @@ Actions:
 
 """
 
+import sys
+
+import boto3
 import docopt
 
 
 if __name__ == '__main__':
     args = docopt.docopt(__doc__)
+
+    asg_name = args['--name']
+
+    if args.get('--start'):
+        desired_size = 1
+    elif args.get('--stop'):
+        desired_size = 0
+    else:
+        print('Neither --start nor --stop flags supplied?  args=%r' % args)
+        sys.exit(1)
+
+    asg_client = boto3.client('autoscaling')
+    asg_client.update_auto_scaling_group(
+        AutoScalingGroupName=asg_name,
+        MinSize=desired_size,
+        MaxSize=desired_size,
+        DesiredCapacity=desired_size,
+    )
