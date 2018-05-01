df825e80c61d241b069cb6d044b83cb904443710
Alex Chan <alex@alexwlchan.net>
2018-05-01 16:43:58 +0100

    Modify MessageWriter to inject MessageConfig, and fix the tests

--- sbt_common/messaging/src/main/scala/uk/ac/wellcome/messaging/message/MessageWriter.scala
+++ sbt_common/messaging/src/main/scala/uk/ac/wellcome/messaging/message/MessageWriter.scala
@@ -2,6 +2,8 @@ package uk.ac.wellcome.messaging.message
 
 import java.net.URI
 
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.sns.AmazonSNS
 import com.google.inject.Inject
 import com.twitter.inject.Logging
 import uk.ac.wellcome.utils.GlobalExecutionContext.context
@@ -9,21 +11,33 @@ import uk.ac.wellcome.models.aws.S3Config
 
 import io.circe.Encoder
 import uk.ac.wellcome.messaging.sns.SNSWriter
-import uk.ac.wellcome.s3.S3ObjectStore
+import uk.ac.wellcome.s3.{KeyPrefixGenerator, S3ObjectStore}
 import uk.ac.wellcome.utils.JsonUtil._
 
 import scala.concurrent.{blocking, Future}
 
 class MessageWriter[T] @Inject()(
-  private val sns: SNSWriter,
-  private val s3Config: S3Config,
-  private val s3: S3ObjectStore[T]
+  messageConfig: MessageConfig,
+  snsClient: AmazonSNS,
+  s3Client: AmazonS3,
+  keyPrefixGenerator: KeyPrefixGenerator[T]
 ) extends Logging {
+
+  private val sns = new SNSWriter(
+    snsClient = snsClient,
+    snsConfig = messageConfig.snsConfig
+  )
+
+  private val s3 = new S3ObjectStore[T](
+    s3Client = s3Client,
+    s3Config = messageConfig.s3Config,
+    keyPrefixGenerator = keyPrefixGenerator
+  )
 
   def write(message: T, subject: String)(
     implicit encoder: Encoder[T]): Future[Unit] = {
 
-    val bucket = s3Config.bucketName
+    val bucket = messageConfig.s3Config.bucketName
 
     for {
       location <- s3.put(message)
--- sbt_common/messaging/src/test/scala/uk/ac/wellcome/messaging/message/MessageWriterTest.scala
+++ sbt_common/messaging/src/test/scala/uk/ac/wellcome/messaging/message/MessageWriterTest.scala
@@ -2,10 +2,10 @@ package uk.ac.wellcome.messaging.message
 
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest._
-import uk.ac.wellcome.messaging.sns.{SNSConfig, SNSWriter}
+import uk.ac.wellcome.messaging.sns.SNSConfig
 import uk.ac.wellcome.messaging.test.fixtures.Messaging
 import uk.ac.wellcome.models.aws.S3Config
-import uk.ac.wellcome.s3.{KeyPrefixGenerator, S3ObjectLocation, S3ObjectStore}
+import uk.ac.wellcome.s3.{KeyPrefixGenerator, S3ObjectLocation}
 import uk.ac.wellcome.utils.JsonUtil._
 
 import scala.util.Success
@@ -18,24 +18,30 @@ class MessageWriterTest
     with IntegrationPatience
     with Inside {
 
+  val keyPrefixGenerator: KeyPrefixGenerator[ExampleObject] =
+    new KeyPrefixGenerator[ExampleObject] {
+      override def generate(obj: ExampleObject): String = "/"
+    }
+
+  val message = ExampleObject("A message sent in the MessageWriterTest")
+  val subject = "message-writer-test-subject"
+
   it("sends messages") {
     withLocalSnsTopic { topic =>
       withLocalS3Bucket { bucket =>
         val s3Config = S3Config(bucketName = bucket.name)
-
         val snsConfig = SNSConfig(topic.arn)
-        val snsWriter = new SNSWriter(snsClient, snsConfig)
+        val messageConfig = MessageConfig(
+          s3Config = s3Config,
+          snsConfig = snsConfig
+        )
 
-        val s3ObjectStore = new S3ObjectStore[ExampleObject](
-          s3Client,
-          s3Config,
-          keyPrefixGenerator)
-
-        val messageWriter =
-          new MessageWriter[ExampleObject](snsWriter, s3Config, s3ObjectStore)
-
-        val message = ExampleObject("Some value")
-        val subject = "sns-writer-test-subject"
+        val messageWriter = new MessageWriter[ExampleObject](
+          messageConfig = messageConfig,
+          snsClient = snsClient,
+          s3Client = s3Client,
+          keyPrefixGenerator = keyPrefixGenerator
+        )
 
         val eventualAttempt = messageWriter.write(message, subject)
 
@@ -61,27 +67,23 @@ class MessageWriterTest
     }
   }
 
-  it("returns a failed future if it fails to publish to sns") {
+  it("returns a failed future if it fails to publish to SNS") {
     withLocalS3Bucket { bucket =>
       val s3Config = S3Config(bucketName = bucket.name)
       val snsConfig = SNSConfig(topicArn = "invalid-topic")
-      val snsWriter = new SNSWriter(snsClient, snsConfig)
+      val messageConfig = MessageConfig(
+        s3Config = s3Config,
+        snsConfig = snsConfig
+      )
 
-      val keyPrefixGenerator: KeyPrefixGenerator[ExampleObject] =
-        new KeyPrefixGenerator[ExampleObject] {
-          override def generate(obj: ExampleObject): String = "/"
-        }
+      val messageWriter = new MessageWriter[ExampleObject](
+        messageConfig = messageConfig,
+        snsClient = snsClient,
+        s3Client = s3Client,
+        keyPrefixGenerator = keyPrefixGenerator
+      )
 
-      val s3ObjectStore = new S3ObjectStore[ExampleObject](
-        s3Client,
-        s3Config,
-        keyPrefixGenerator)
-      val messages =
-        new MessageWriter[ExampleObject](snsWriter, s3Config, s3ObjectStore)
-
-      val message = ExampleObject("Some value")
-
-      val eventualAttempt = messages.write(message, "subject")
+      val eventualAttempt = messageWriter.write(message, subject)
 
       whenReady(eventualAttempt.failed) { ex =>
         ex shouldBe a[Throwable]
@@ -93,23 +95,19 @@ class MessageWriterTest
     withLocalSnsTopic { topic =>
       val s3Config = S3Config(bucketName = "invalid-bucket")
       val snsConfig = SNSConfig(topic.arn)
-      val snsWriter = new SNSWriter(snsClient, snsConfig)
+      val messageConfig = MessageConfig(
+        s3Config = s3Config,
+        snsConfig = snsConfig
+      )
 
-      val keyPrefixGenerator: KeyPrefixGenerator[ExampleObject] =
-        new KeyPrefixGenerator[ExampleObject] {
-          override def generate(obj: ExampleObject): String = "/"
-        }
+      val messageWriter = new MessageWriter[ExampleObject](
+        messageConfig = messageConfig,
+        snsClient = snsClient,
+        s3Client = s3Client,
+        keyPrefixGenerator = keyPrefixGenerator
+      )
 
-      val s3ObjectStore = new S3ObjectStore[ExampleObject](
-        s3Client,
-        s3Config,
-        keyPrefixGenerator)
-      val messages =
-        new MessageWriter[ExampleObject](snsWriter, s3Config, s3ObjectStore)
-
-      val message = ExampleObject("Some value")
-
-      val eventualAttempt = messages.write(message, "subject")
+      val eventualAttempt = messageWriter.write(message, subject)
 
       whenReady(eventualAttempt.failed) { ex =>
         ex shouldBe a[Throwable]
@@ -121,23 +119,19 @@ class MessageWriterTest
     withLocalSnsTopic { topic =>
       val s3Config = S3Config(bucketName = "invalid-bucket")
       val snsConfig = SNSConfig(topic.arn)
-      val snsWriter = new SNSWriter(snsClient, snsConfig)
+      val messageConfig = MessageConfig(
+        s3Config = s3Config,
+        snsConfig = snsConfig
+      )
 
-      val keyPrefixGenerator: KeyPrefixGenerator[ExampleObject] =
-        new KeyPrefixGenerator[ExampleObject] {
-          override def generate(obj: ExampleObject): String = "/"
-        }
+      val messageWriter = new MessageWriter[ExampleObject](
+        messageConfig = messageConfig,
+        snsClient = snsClient,
+        s3Client = s3Client,
+        keyPrefixGenerator = keyPrefixGenerator
+      )
 
-      val s3ObjectStore = new S3ObjectStore[ExampleObject](
-        s3Client,
-        s3Config,
-        keyPrefixGenerator)
-      val messages =
-        new MessageWriter[ExampleObject](snsWriter, s3Config, s3ObjectStore)
-
-      val message = ExampleObject("Some value")
-
-      val eventualAttempt = messages.write(message, "subject")
+      val eventualAttempt = messageWriter.write(message, subject)
 
       whenReady(eventualAttempt.failed) { _ =>
         listMessagesReceivedFromSNS(topic) should be('empty)
