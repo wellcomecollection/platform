f5dd293802772f072b0a2758e067bb662fe890bb
Alex Chan <a.chan@wellcome.ac.uk>
2018-05-02 16:46:56 +0100

    Remove endpoint configuration from the S3Config model

--- sbt_common/storage/src/main/scala/uk/ac/wellcome/storage/s3/S3ClientModule.scala
+++ sbt_common/storage/src/main/scala/uk/ac/wellcome/storage/s3/S3ClientModule.scala
@@ -11,21 +11,30 @@ import uk.ac.wellcome.models.aws.AWSConfig
 
 object S3ClientModule extends TwitterModule {
 
+  private val endpoint = flag[String](
+    "aws.s3.endpoint",
+    "",
+    "Endpoint of AWS S3. The region will be used if the endpoint is not provided")
+  private val accessKey =
+    flag[String]("aws.s3.accessKey", "", "AccessKey to access S3")
+  private val secretKey =
+    flag[String]("aws.s3.secretKey", "", "SecretKey to access S3")
+
   @Singleton
   @Provides
-  def providesS3Client(awsConfig: AWSConfig, s3Config: S3Config): AmazonS3 = {
+  def providesS3Client(awsConfig: AWSConfig): AmazonS3 = {
     val standardClient = AmazonS3ClientBuilder.standard
-    if (s3Config.endpoint.isEmpty)
+    if (endpoint().isEmpty)
       standardClient
         .withRegion(awsConfig.region)
         .build()
     else
       standardClient
         .withCredentials(new AWSStaticCredentialsProvider(
-          new BasicAWSCredentials(s3Config.accessKey, s3Config.secretKey)))
+          new BasicAWSCredentials(accessKey(), secretKey())))
         .withPathStyleAccessEnabled(true)
         .withEndpointConfiguration(
-          new EndpointConfiguration(s3Config.endpoint, awsConfig.region))
+          new EndpointConfiguration(endpoint(), awsConfig.region))
         .build()
   }
 
--- sbt_common/storage/src/main/scala/uk/ac/wellcome/storage/s3/S3Config.scala
+++ sbt_common/storage/src/main/scala/uk/ac/wellcome/storage/s3/S3Config.scala
@@ -1,8 +1,3 @@
 package uk.ac.wellcome.storage.s3
 
-case class S3Config(
-  bucketName: String,
-  endpoint: String = "",
-  accessKey: String = "",
-  secretKey: String = ""
-)
+case class S3Config(bucketName: String)
