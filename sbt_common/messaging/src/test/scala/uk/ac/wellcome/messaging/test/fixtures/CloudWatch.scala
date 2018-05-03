7b9a84d4eb07fc293a4222f279ef57e7b63f9404
Alex Chan <alex@alexwlchan.net>
2018-05-03 06:42:04 +0100

    Merge in the other CloudWatch fixture (why do we have two???)

--- sbt_common/messaging/src/test/scala/uk/ac/wellcome/messaging/test/fixtures/CloudWatch.scala
+++ sbt_common/messaging/src/test/scala/uk/ac/wellcome/messaging/test/fixtures/CloudWatch.scala
@@ -1,10 +1,37 @@
 package uk.ac.wellcome.messaging.test.fixtures
 
+import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
+import com.amazonaws.services.cloudwatch.{
+  AmazonCloudWatch,
+  AmazonCloudWatchClientBuilder
+}
 import org.scalatest.Suite
 
+import scala.concurrent.duration._
+
 trait CloudWatch { this: Suite =>
+  protected val awsNamespace: String = "test"
+  protected val flushInterval: FiniteDuration = 1 second
+
+  protected val localCloudWatchEndpointUrl = "http://localhost:4582"
+  protected val regionName = "eu-west-1"
+
+  protected val accessKey: String = "accessKey1"
+  protected val secretKey: String = "verySecretKey1"
+
   def cloudWatchLocalFlags =
     Map(
-      "aws.cloudWatch.endpoint" -> "http://localhost:6789"
+      "aws.cloudWatch.endpoint" -> localCloudWatchEndpointUrl
     )
+
+  private val credentials = new AWSStaticCredentialsProvider(
+    new BasicAWSCredentials(accessKey, secretKey))
+
+  val cloudWatchClient: AmazonCloudWatch = AmazonCloudWatchClientBuilder
+    .standard()
+    .withCredentials(credentials)
+    .withEndpointConfiguration(
+      new EndpointConfiguration(localCloudWatchEndpointUrl, regionName))
+    .build()
 }
