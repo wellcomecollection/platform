29587d8ac62d9f1a0bdb9dee8f544fc67e384a2b
Alex Chan <a.chan@wellcome.ac.uk>
2018-05-04 13:53:17 +0100

    Fix the other uses of S3ObjectStore.put

--- sbt_common/storage/src/main/scala/uk/ac/wellcome/storage/vhs/VersionedHybridStore.scala
+++ sbt_common/storage/src/main/scala/uk/ac/wellcome/storage/vhs/VersionedHybridStore.scala
@@ -117,7 +117,7 @@ class VersionedHybridStore[T <: Id] @Inject()(
       throw new IllegalArgumentException(
         "ID provided does not match ID in record.")
 
-    val futureUri = sourcedObjectStore.put(sourcedObject)
+    val futureUri = sourcedObjectStore.put(sourcedObject, keyPrefixGenerator.generate(sourcedObject))
 
     futureUri.flatMap {
       case S3ObjectLocation(_, key) => versionedDao.updateRecord(f(key))
