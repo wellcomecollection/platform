2dff98832c375f3ff221569b646cd483a03ed4f2
Alex Chan <alex@alexwlchan.net>
2018-05-06 09:07:36 +0100

    Use the new WorkIndexer fixture in WorkIndexerTest

--- catalogue_pipeline/ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/services/WorkIndexerTest.scala
+++ catalogue_pipeline/ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/services/WorkIndexerTest.scala
@@ -1,50 +1,37 @@
 package uk.ac.wellcome.platform.ingestor.services
 
-import akka.actor.ActorSystem
-import com.amazonaws.services.cloudwatch.AmazonCloudWatch
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{FunSpec, Matchers}
 import uk.ac.wellcome.elasticsearch.test.fixtures.ElasticsearchFixtures
-import uk.ac.wellcome.monitoring.MetricsSender
 import uk.ac.wellcome.models.work.internal.IdentifiedWork
 import uk.ac.wellcome.models.work.test.util.WorksUtil
+import uk.ac.wellcome.platform.ingestor.fixtures.WorkIndexerFixture
 import uk.ac.wellcome.utils.GlobalExecutionContext.context
 
 import scala.concurrent.Future
-import scala.concurrent.duration._
 
 class WorkIndexerTest
     extends FunSpec
     with ScalaFutures
     with Matchers
-    with MockitoSugar
     with ElasticsearchFixtures
     with WorksUtil {
 
-  val metricsSender: MetricsSender =
-    new MetricsSender(
-      namespace = "reindexer-tests",
-      100 milliseconds,
-      mock[AmazonCloudWatch],
-      ActorSystem())
-
-  val itemType = "work"
-
-  val workIndexer =
-    new WorkIndexer(itemType, elasticClient, metricsSender)
+  val esType = "work"
 
   it("inserts an identified Work into Elasticsearch") {
     val work = createVersionedWork()
 
-    withLocalElasticsearchIndex(itemType) { indexName =>
-      val future = workIndexer.indexWork(work, indexName)
+    withLocalElasticsearchIndex(esType) { indexName =>
+      withWorkIndexerFixtures(esType, elasticClient) { workIndexer =>
+        val future = workIndexer.indexWork(work, indexName)
 
-      whenReady(future) { _ =>
-        assertElasticsearchEventuallyHasWork(
-          work,
-          indexName = indexName,
-          itemType = itemType)
+        whenReady(future) { _ =>
+          assertElasticsearchEventuallyHasWork(
+            work,
+            indexName = indexName,
+            esType = esType)
+        }
       }
     }
   }
@@ -52,16 +39,18 @@ class WorkIndexerTest
   it("only adds one record when the same ID is ingested multiple times") {
     val work = createVersionedWork()
 
-    withLocalElasticsearchIndex(itemType) { indexName =>
-      val future = Future.sequence(
-        (1 to 2).map(_ => workIndexer.indexWork(work, indexName))
-      )
+    withLocalElasticsearchIndex(esType) { indexName =>
+      withWorkIndexerFixtures(esType, elasticClient) { workIndexer =>
+        val future = Future.sequence(
+          (1 to 2).map(_ => workIndexer.indexWork(work, indexName))
+        )
 
-      whenReady(future) { _ =>
-        assertElasticsearchEventuallyHasWork(
-          work,
-          indexName = indexName,
-          itemType = itemType)
+        whenReady(future) { _ =>
+          assertElasticsearchEventuallyHasWork(
+            work,
+            indexName = indexName,
+            esType = esType)
+        }
       }
     }
   }
@@ -70,19 +59,21 @@ class WorkIndexerTest
     val work = createVersionedWork(version = 3)
     val olderWork = work.copy(version = 1)
 
-    withLocalElasticsearchIndex(itemType) { indexName =>
-      insertIntoElasticsearch(indexName = indexName, itemType = itemType, work)
+    withLocalElasticsearchIndex(esType) { indexName =>
+      insertIntoElasticsearch(indexName = indexName, esType = esType, work)
 
-      val future = workIndexer.indexWork(olderWork, indexName)
+      withWorkIndexerFixtures(esType, elasticClient) { workIndexer =>
+        val future = workIndexer.indexWork(olderWork, indexName)
 
-      whenReady(future) { _ =>
-        // Give Elasticsearch enough time to ingest the work
-        Thread.sleep(700)
+        whenReady(future) { _ =>
+          // Give Elasticsearch enough time to ingest the work
+          Thread.sleep(700)
 
-        assertElasticsearchEventuallyHasWork(
-          work,
-          indexName = indexName,
-          itemType = itemType)
+          assertElasticsearchEventuallyHasWork(
+            work,
+            indexName = indexName,
+            esType = esType)
+        }
       }
     }
   }
@@ -91,16 +82,18 @@ class WorkIndexerTest
     val work = createVersionedWork(version = 3)
     val updatedWork = work.copy(title = Some("boring title"))
 
-    withLocalElasticsearchIndex(itemType) { indexName =>
-      insertIntoElasticsearch(indexName = indexName, itemType = itemType, work)
+    withLocalElasticsearchIndex(esType) { indexName =>
+      insertIntoElasticsearch(indexName = indexName, esType = esType, work)
 
-      val future = workIndexer.indexWork(updatedWork, indexName)
+      withWorkIndexerFixtures(esType, elasticClient) { workIndexer =>
+        val future = workIndexer.indexWork(updatedWork, indexName)
 
-      whenReady(future) { _ =>
-        assertElasticsearchEventuallyHasWork(
-          updatedWork,
-          indexName = indexName,
-          itemType = itemType)
+        whenReady(future) { _ =>
+          assertElasticsearchEventuallyHasWork(
+            updatedWork,
+            indexName = indexName,
+            esType = esType)
+        }
       }
     }
   }
