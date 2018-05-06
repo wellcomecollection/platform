d8e8b61f7c04d27c4cb6bdc4544a37662ed68ba6
Alex Chan <alex@alexwlchan.net>
2018-05-06 09:36:38 +0100

    More fixes in WorkIndexerTest

--- catalogue_pipeline/ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/services/WorkIndexerTest.scala
+++ catalogue_pipeline/ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/services/WorkIndexerTest.scala
@@ -1,11 +1,11 @@
 package uk.ac.wellcome.platform.ingestor.services
 
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.{Assertion, FunSpec, Matchers}
 import uk.ac.wellcome.elasticsearch.test.fixtures.ElasticsearchFixtures
 import uk.ac.wellcome.models.work.internal.IdentifiedWork
 import uk.ac.wellcome.models.work.test.util.WorksUtil
-import uk.ac.wellcome.platform.ingestor.fixtures.WorkIndexerFixture
+import uk.ac.wellcome.platform.ingestor.fixtures.WorkIndexerFixtures
 import uk.ac.wellcome.utils.GlobalExecutionContext.context
 
 import scala.concurrent.Future
@@ -15,7 +15,8 @@ class WorkIndexerTest
     with ScalaFutures
     with Matchers
     with ElasticsearchFixtures
-    with WorksUtil {
+    with WorksUtil
+    with WorkIndexerFixtures {
 
   val itemType = "work"
 
@@ -40,7 +41,7 @@ class WorkIndexerTest
     val work = createVersionedWork()
 
     withLocalElasticsearchIndex(itemType = itemType) { indexName =>
-      withWorkIndexerFixtures(itemType, elasticClient) { workIndexer =>
+      withWorkIndexerFixtures[Assertion](itemType, elasticClient) { workIndexer =>
         val future = Future.sequence(
           (1 to 2).map(_ => workIndexer.indexWork(work, indexName))
         )
