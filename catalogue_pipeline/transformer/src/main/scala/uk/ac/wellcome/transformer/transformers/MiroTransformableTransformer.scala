867273e0e7e03cf5be231f4cd7f0ca2c6130caf7
Alex Chan <alex@alexwlchan.net>
2018-05-08 09:17:21 +0100

    Modify the Miro transformer to use Subject + type params

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/MiroTransformableTransformer.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/MiroTransformableTransformer.scala
@@ -10,7 +10,8 @@ import scala.util.Try
 class MiroTransformableTransformer
     extends TransformableTransformer[MiroTransformable]
     with MiroContributors
-    with MiroGenres {
+    with MiroGenres
+    with MiroSubjects {
   // TODO this class is too big as the different test classes would suggest. Split it.
 
   override def transformForType(miroTransformable: MiroTransformable,
@@ -214,38 +215,6 @@ class MiroTransformableTransformer
         }
 
     miroIDList ++ sierraList ++ libraryRefsList
-  }
-
-  /* Populate the subjects field.  This is based on two fields in the XML,
-   *  <image_keywords> and <image_keywords_unauth>.  Both of these were
-   *  defined in part or whole by the human cataloguers, and in general do
-   *  not correspond to a controlled vocabulary.  (The latter was imported
-   *  directly from PhotoSoft.)
-   *
-   *  In some cases, these actually do correspond to controlled vocabs,
-   *  e.g. where keywords were pulled directly from Sierra -- but we don't
-   *  have enough information in Miro to determine which ones those are.
-   */
-  private def getSubjects(miroData: MiroTransformableData): List[Subject] = {
-    val keywords: List[Subject] = miroData.keywords match {
-      case Some(k) =>
-        k.map { keyword =>
-          Subject(label = keyword, concepts = List(Concept(keyword)))
-        }
-      case None =>
-        List()
-    }
-
-    val keywordsUnauth: List[Subject] = miroData.keywordsUnauth match {
-      case Some(k) =>
-        k.map { keyword =>
-          Subject(label = keyword, concepts = List(Concept(keyword)))
-        }
-      case None =>
-        List()
-    }
-
-    keywords ++ keywordsUnauth
   }
 
   private def getThumbnail(miroData: MiroTransformableData,
---
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/miro/MiroSubjects.scala
@@ -0,0 +1,45 @@
+package uk.ac.wellcome.transformer.transformers.miro
+
+import uk.ac.wellcome.models.work.internal.{AbstractConcept, Concept, Subject}
+import uk.ac.wellcome.transformer.source.MiroTransformableData
+
+trait MiroSubjects {
+
+  /* Populate the subjects field.  This is based on two fields in the XML,
+   *  <image_keywords> and <image_keywords_unauth>.  Both of these were
+   *  defined in part or whole by the human cataloguers, and in general do
+   *  not correspond to a controlled vocabulary.  (The latter was imported
+   *  directly from PhotoSoft.)
+   *
+   *  In some cases, these actually do correspond to controlled vocabs,
+   *  e.g. where keywords were pulled directly from Sierra -- but we don't
+   *  have enough information in Miro to determine which ones those are.
+   */
+  def getSubjects(miroData: MiroTransformableData): List[Subject[AbstractConcept]] = {
+    val keywords: List[Subject[AbstractConcept]] = miroData.keywords match {
+      case Some(k) =>
+        k.map { keyword =>
+          Subject[AbstractConcept](
+            label = keyword,
+            concepts = List(Concept(keyword))
+          )
+        }
+      case None =>
+        List()
+    }
+
+    val keywordsUnauth: List[Subject[AbstractConcept]] = miroData.keywordsUnauth match {
+      case Some(k) =>
+        k.map { keyword =>
+          Subject[AbstractConcept](
+            label = keyword,
+            concepts = List(Concept(keyword))
+          )
+        }
+      case None =>
+        List()
+    }
+
+    keywords ++ keywordsUnauth
+  }
+}
