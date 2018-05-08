7b17a78a6702ee3fb66b69d2275f341f98a2d077
caylaa <alicefuzier@gmail.com>
2018-05-08 13:33:17 +0100

    Extract into a function to improve readability

--- sbt_common/messaging/src/main/scala/uk/ac/wellcome/messaging/message/MessageStream.scala
+++ sbt_common/messaging/src/main/scala/uk/ac/wellcome/messaging/message/MessageStream.scala
@@ -8,6 +8,7 @@ import akka.stream.alpakka.sqs.scaladsl.{SqsAckSink, SqsSource}
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.sqs
 import com.amazonaws.services.sqs.AmazonSQSAsync
+import com.amazonaws.services.sqs.model.Message
 import grizzled.slf4j.Logging
 import io.circe.Decoder
 import uk.ac.wellcome.exceptions.GracefulFailureException
@@ -38,31 +39,35 @@ class MessageStream[T](actorSystem: ActorSystem,
     s3Config = messageReaderConfig.s3Config
   )
 
-  def foreach(name: String, process: T => Future[Unit])(
+  def foreach(streamName: String, process: T => Future[Unit])(
     implicit decoderT: Decoder[T]): Future[Done] =
     SqsSource(messageReaderConfig.sqsConfig.queueUrl)(sqsClient)
       .mapAsyncUnordered(10) { message =>
-        val processMessageFuture = for {
-          t <- read(message)
-          _ <- process(t)
-        } yield message
-
-        processMessageFuture.onFailure {
-          case exception: GracefulFailureException =>
-            logger.warn(s"Failure processing message", exception)
-          case exception: Exception =>
-            logger.error(s"Failure while processing message.", exception)
-            metricsSender.incrementCount(
-              s"${name}_MessageProcessingFailure",
-              1.0)
-        }
-        val metricName = s"${name}_ProcessMessage"
-        metricsSender.timeAndCount(metricName, () => processMessageFuture)
+        val metricName = s"${streamName}_ProcessMessage"
+        metricsSender.timeAndCount(metricName, () => readAndProcess(streamName, message, process))
       }
       .map { m =>
         (m, MessageAction.Delete)
       }
       .runWith(SqsAckSink(messageReaderConfig.sqsConfig.queueUrl)(sqsClient))
+
+  private def readAndProcess(streamName: String, message: Message, process: T => Future[Unit]) = {
+    val processMessageFuture = for {
+      t <- read(message)
+      _ <- process(t)
+    } yield message
+
+    processMessageFuture.onFailure {
+      case exception: GracefulFailureException =>
+        logger.warn(s"Failure processing message", exception)
+      case exception: Exception =>
+        logger.error(s"Failure while processing message.", exception)
+        metricsSender.incrementCount(
+          s"${streamName}_MessageProcessingFailure",
+          1.0)
+    }
+    processMessageFuture
+  }
 
   private def read(message: sqs.model.Message)(
     implicit decoderN: Decoder[NotificationMessage],
