4b8f89a6b48afbba00766a7e4c9c039deb03ce7f
Alex Chan <alex@alexwlchan.net>
2018-05-08 09:59:13 +0100

    Modify Work to use Genre with an IdentityState

--- sbt_common/internal_model/src/main/scala/uk/ac/wellcome/models/work/internal/Work.scala
+++ sbt_common/internal_model/src/main/scala/uk/ac/wellcome/models/work/internal/Work.scala
@@ -16,7 +16,7 @@ trait Work extends Versioned {
   val createdDate: Option[Period]
   val subjects: List[Subject[AbstractConcept]]
   val contributors: List[Contributor[IdentityState[AbstractAgent]]]
-  val genres: List[Genre[AbstractConcept]]
+  val genres: List[Genre[IdentityState[AbstractConcept]]]
   val thumbnail: Option[Location]
   val publishers: List[IdentityState[AbstractAgent]]
   val publicationDate: Option[Period]
@@ -39,7 +39,7 @@ case class UnidentifiedWork(
   createdDate: Option[Period] = None,
   subjects: List[Subject[AbstractConcept]] = Nil,
   contributors: List[Contributor[MaybeDisplayable[AbstractAgent]]] = Nil,
-  genres: List[Genre[AbstractConcept]] = Nil,
+  genres: List[Genre[MaybeDisplayable[AbstractConcept]]] = Nil,
   thumbnail: Option[Location] = None,
   items: List[UnidentifiedItem] = Nil,
   publishers: List[MaybeDisplayable[AbstractAgent]] = Nil,
@@ -65,7 +65,7 @@ case class IdentifiedWork(
   createdDate: Option[Period] = None,
   subjects: List[Subject[AbstractConcept]] = Nil,
   contributors: List[Contributor[Displayable[AbstractAgent]]] = Nil,
-  genres: List[Genre[AbstractConcept]] = Nil,
+  genres: List[Genre[Displayable[AbstractConcept]]] = Nil,
   thumbnail: Option[Location] = None,
   items: List[IdentifiedItem] = Nil,
   publishers: List[Displayable[AbstractAgent]] = Nil,
