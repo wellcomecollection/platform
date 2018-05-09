124ae4fffe42f0230c6e996e1b3a94fc0b79f2d0
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-05-09 07:21:07 +0000

    Apply auto-formatting rules

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/miro/MiroGenres.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/miro/MiroGenres.scala
@@ -4,8 +4,8 @@ import uk.ac.wellcome.models.work.internal._
 import uk.ac.wellcome.transformer.source.MiroTransformableData
 
 trait MiroGenres {
-  def getGenres(miroData: MiroTransformableData)
-    : List[Genre[Unidentifiable[Concept]]] = {
+  def getGenres(
+    miroData: MiroTransformableData): List[Genre[Unidentifiable[Concept]]] = {
     // Populate the genres field.  This is based on two fields in the XML,
     // <image_phys_format> and <image_lc_genre>.
     (miroData.physFormat.toList ++ miroData.lcGenre.toList).map { label =>
