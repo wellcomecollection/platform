b3e77074b8521f101948348694ea2c893904e144
Alex Chan <a.chan@wellcome.ac.uk>
2018-05-09 12:06:24 +0100

    Break up the SierraGenres logic

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraGenres.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraGenres.scala
@@ -1,53 +1,87 @@
 package uk.ac.wellcome.transformer.transformers.sierra
 
-import uk.ac.wellcome.models.work.internal.{
-  AbstractConcept,
-  Concept,
-  Genre,
-  MaybeDisplayable,
-  Period,
-  Place,
-  Unidentifiable
-}
-import uk.ac.wellcome.transformer.source.SierraBibData
+import uk.ac.wellcome.models.work.internal.{AbstractConcept, Concept, Genre, MaybeDisplayable, Period, Place, Unidentifiable}
+import uk.ac.wellcome.transformer.source.{MarcSubfield, SierraBibData}
 
 trait SierraGenres extends MarcUtils {
 
+  // Populate wwork:genres
+  //
+  // Use MARC field "655".
+  //
+  // Within a MARC 655 tag, there's:
+  //
+  //    - a primary concept (subfield $a); and
+  //    - subdivisions (subfields $v, $x, $y and $z)
+  //
+  // The primary concept can be identified, and the subdivisions serve
+  // to add extra context.
+  //
+  // We construct the Genre as follows:
+  //
+  //    - label is the concatenation of $a, $v, $x, $y and $z in order,
+  //      separated by a hyphen ' - '.
+  //    - concepts is a List[Concept] populated in order of the subfields:
+  //
+  //        * $a => Concept
+  //          Optionally with an identifier.  We look in subfield $0 for the
+  //          identifier value, then second indicator for the authority.
+  //
+  //        * $v => Concept
+  //        * $x => Concept
+  //        * $y => Period
+  //        * $z => Place
+  //
+  //      Note that only concepts from subfield $a are identified; everything
+  //      else is unidentified.
+  //
   def getGenres(
     bibData: SierraBibData): List[Genre[MaybeDisplayable[AbstractConcept]]] = {
     getGenresForMarcTag(bibData, "655")
   }
 
-  // Populate wwork:genres
-  //
-  // Use MARC field "655"
-  //
-  // Each Genre type is populated with label and concepts
-  //
-  //   - Genre.label is concatenated subfields a,v,x,y,z in order separated by a hyphen ' - '.
-  //   - Genre.concepts is a List populated with subfield 'a', then Concepts from subfields v,x,y,z in order with types:
-  //       * v => Concept
-  //       * x => Concept
-  //       * y => Period
-  //       * z => Place
-  //
   private def getGenresForMarcTag(bibData: SierraBibData, marcTag: String) = {
     val subfieldsList =
       getMatchingSubfields(bibData, marcTag, List("a", "v", "x", "y", "z"))
+
     subfieldsList.map(subfields => {
-      val (subfieldsA, rest) = subfields.partition(_.tag == "a")
-      val orderedSubfields = subfieldsA ++ rest
-      val label = orderedSubfields.map(_.content).mkString(" - ")
-      val concepts = orderedSubfields.map(subfield =>
-        subfield.tag match {
-          case "y" => Unidentifiable(Period(label = subfield.content))
-          case "z" => Unidentifiable(Place(label = subfield.content))
-          case _ => Unidentifiable(Concept(label = subfield.content))
-      })
+      val (primarySubfields, subdivisionSubfields) = subfields.partition { _.tag == "a" }
+
+      val label = getGenreLabel(primarySubfields, subdivisionSubfields)
+      val concepts: List[MaybeDisplayable[AbstractConcept]] = getPrimaryConcept(primarySubfields) ++ getSubdivisions(subdivisionSubfields)
+
       Genre[MaybeDisplayable[AbstractConcept]](
         label = label,
         concepts = concepts
       )
     })
+  }
+
+  // Get the genre label.  This is populated by the label of subfield $a, followed
+  // by other subfields, in the order they come from MARC.  The labels are
+  // joined by " - ".
+  private def getGenreLabel(primarySubfields: List[MarcSubfield], subdivisionSubfields: List[MarcSubfield]): String = {
+    val orderedSubfields = primarySubfields ++ subdivisionSubfields
+    orderedSubfields.map { _.content }.mkString(" - ")
+  }
+
+  // Extract the primary concept, which comes from subfield $a.  This is the
+  // only concept which might be identified.
+  private def getPrimaryConcept(primarySubfields: List[MarcSubfield]): List[MaybeDisplayable[Concept]] = {
+    primarySubfields.map { subfield =>
+      Unidentifiable(Concept(label = subfield.content))
+    }
+  }
+
+  // Extract the subdivisions, which come from everything except subfield $a.
+  // These are never identified.  We preserve the order from MARC.
+  private def getSubdivisions(subdivisionSubfields: List[MarcSubfield]): List[Unidentifiable[AbstractConcept]] = {
+    subdivisionSubfields.map { subfield =>
+      subfield.tag match {
+        case "v" | "w" => Unidentifiable(Concept(label = subfield.content))
+        case "y" => Unidentifiable(Period(label = subfield.content))
+        case "z" => Unidentifiable(Place(label = subfield.content))
+      }
+    }
   }
 }
