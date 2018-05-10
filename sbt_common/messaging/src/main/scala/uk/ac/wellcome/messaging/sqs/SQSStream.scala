b7f9ba7410edbf45ceb5b9d8ec4c6bbfb8326554
Alex Chan <alex@alexwlchan.net>
2018-05-10 11:54:42 +0100

    Use the correct logging import

--- sbt_common/messaging/src/main/scala/uk/ac/wellcome/messaging/sqs/SQSStream.scala
+++ sbt_common/messaging/src/main/scala/uk/ac/wellcome/messaging/sqs/SQSStream.scala
@@ -9,7 +9,7 @@ import com.amazonaws.services.sqs
 import com.amazonaws.services.sqs.AmazonSQSAsync
 import com.amazonaws.services.sqs.model.Message
 import com.google.inject.Inject
-import grizzled.slf4j.Logging
+import com.twitter.inject.Logging
 import io.circe.Decoder
 import uk.ac.wellcome.exceptions.GracefulFailureException
 import uk.ac.wellcome.monitoring.MetricsSender
