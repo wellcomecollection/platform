e76860c5ae53e4778e2fb70cbd6fcbd8a89f5ebe
Alex Chan <a.chan@wellcome.ac.uk>
2018-05-11 15:54:07 +0100

    Create a new IngestorModule

--- catalogue_pipeline/ingestor/src/main/scala/uk/ac/wellcome/platform/ingestor/Server.scala
+++ catalogue_pipeline/ingestor/src/main/scala/uk/ac/wellcome/platform/ingestor/Server.scala
@@ -4,13 +4,12 @@ import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
 import com.twitter.finatra.http.routing.HttpRouter
-import uk.ac.wellcome.elasticsearch.finatra.modules.ElasticClientModule
 import uk.ac.wellcome.finatra.controllers.ManagementController
 import uk.ac.wellcome.finatra.modules._
 import uk.ac.wellcome.messaging.message.MessageConfigModule
 import uk.ac.wellcome.monitoring.MetricsSenderModule
 import uk.ac.wellcome.messaging.sqs.{SQSClientModule, SQSReaderModule}
-import uk.ac.wellcome.platform.ingestor.finatra.modules.{IdentifiedWorkKeyPrefixGeneratorModule, IngestorWorkerModule, WorksIndexModule}
+import uk.ac.wellcome.platform.ingestor.finatra.modules.{IdentifiedWorkKeyPrefixGeneratorModule, IngestorModule, IngestorWorkerModule, WorksIndexModule}
 import uk.ac.wellcome.storage.s3.{S3ClientModule, S3ConfigModule}
 
 object ServerMain extends Server
@@ -27,7 +26,7 @@ class Server extends HttpServer {
     AkkaModule,
     SQSReaderModule,
     IngestorWorkerModule,
-    ElasticClientModule,
+    IngestorModule,
     WorksIndexModule,
     IdentifiedWorkKeyPrefixGeneratorModule
   )
