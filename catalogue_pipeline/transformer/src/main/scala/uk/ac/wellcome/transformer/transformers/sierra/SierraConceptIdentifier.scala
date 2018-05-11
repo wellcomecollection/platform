74332c949044897836057409901366e79f20bcf3
Alex Chan <a.chan@wellcome.ac.uk>
2018-05-11 10:59:18 +0100

    Add a comment explaining the purpose of this class

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraConceptIdentifier.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/transformer/transformers/sierra/SierraConceptIdentifier.scala
@@ -3,6 +3,22 @@ package uk.ac.wellcome.transformer.transformers.sierra
 import uk.ac.wellcome.models.work.internal.{IdentifierSchemes, SourceIdentifier}
 import uk.ac.wellcome.transformer.source.{MarcSubfield, VarField}
 
+// Implements logic for finding a source identifier for varFields with
+// MARC tag 648, 650, 651 and 655.  These are the fields we use for genre
+// and subject.
+//
+// The rules for these identifiers is moderately fiddly:
+//
+//   - Look in indicator 2.  Values 0 to 6 have hard-coded meanings.
+//   - If indicator 2 is 7, look in subfield $2 instead.
+//
+// The rules are the same for all four MARC tags, hence the shared object.
+//
+// https://www.loc.gov/marc/bibliographic/bd648.html
+// https://www.loc.gov/marc/bibliographic/bd650.html
+// https://www.loc.gov/marc/bibliographic/bd651.html
+// https://www.loc.gov/marc/bibliographic/bd655.html
+//
 object SierraConceptIdentifier {
 
   def maybeFindIdentifier(
