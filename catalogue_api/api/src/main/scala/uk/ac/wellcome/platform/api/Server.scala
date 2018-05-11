3406178fbd430dd52208183680a3ccf7d730d6dc
Alex Chan <a.chan@wellcome.ac.uk>
2018-05-11 15:49:00 +0100

    Load the new API module in the API server

--- catalogue_api/api/src/main/scala/uk/ac/wellcome/platform/api/Server.scala
+++ catalogue_api/api/src/main/scala/uk/ac/wellcome/platform/api/Server.scala
@@ -2,29 +2,21 @@ package uk.ac.wellcome.platform.api
 
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.HttpServer
-import com.twitter.finatra.http.filters.{
-  CommonFilters,
-  LoggingMDCFilter,
-  TraceIdMDCFilter
-}
+import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
 import com.twitter.finatra.http.routing.HttpRouter
 import uk.ac.wellcome.display.modules.DisplayJacksonModule
-import uk.ac.wellcome.elasticsearch.finatra.modules.ElasticClientModule
 import uk.ac.wellcome.platform.api.controllers._
-import uk.ac.wellcome.platform.api.finatra.exceptions.{
-  CaseClassMappingExceptionWrapper,
-  ElasticsearchResponseExceptionMapper,
-  GeneralExceptionMapper
-}
+import uk.ac.wellcome.platform.api.finatra.exceptions.{CaseClassMappingExceptionWrapper, ElasticsearchResponseExceptionMapper, GeneralExceptionMapper}
+import uk.ac.wellcome.platform.api.finatra.modules.ApiModule
 
 object ServerMain extends Server
 
 class Server extends HttpServer {
   override val name = "uk.ac.wellcome.platform.api Platformapi"
-  override val modules = Seq(ElasticClientModule)
+  override val modules = Seq(ApiModule)
 
   flag(name = "api.host", default = "localhost:8888", help = "API hostname")
-  flag(name = "api.scheme", default = "https", help = "API prototocol scheme")
+  flag(name = "api.scheme", default = "https", help = "API protocol scheme")
   flag(name = "api.pageSize", default = 10, help = "API default page size")
 
   private final val apiName =
