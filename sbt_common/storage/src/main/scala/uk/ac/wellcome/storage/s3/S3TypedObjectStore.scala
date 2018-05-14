64d9a85b6e36620f6576f23bf375a70187eb3e08
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-05-14 15:34:15 +0000

    Apply auto-formatting rules

--- sbt_common/storage/src/main/scala/uk/ac/wellcome/storage/s3/S3TypedObjectStore.scala
+++ sbt_common/storage/src/main/scala/uk/ac/wellcome/storage/s3/S3TypedObjectStore.scala
@@ -12,7 +12,8 @@ import scala.concurrent.Future
 class S3TypedObjectStore[T] @Inject()(
   s3Client: AmazonS3,
   s3Config: S3Config
-) extends Logging with S3Store[T] {
+) extends Logging
+    with S3Store[T] {
 
   val stringStore = new S3StringStore(
     s3Client = s3Client,
