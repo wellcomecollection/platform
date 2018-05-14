7f1abe47f64f945b0b4b0db9bbfd5b60d4509f05
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-05-14 09:12:03 +0000

    Apply auto-formatting rules

--- sbt_common/display/src/main/scala/uk/ac/wellcome/display/models/v2/DisplayGenre.scala
+++ sbt_common/display/src/main/scala/uk/ac/wellcome/display/models/v2/DisplayGenre.scala
@@ -2,18 +2,21 @@ package uk.ac.wellcome.display.models.v2
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.annotations.{ApiModel, ApiModelProperty}
-import uk.ac.wellcome.models.work.internal.{AbstractConcept, Displayable, Genre}
+import uk.ac.wellcome.models.work.internal.{
+  AbstractConcept,
+  Displayable,
+  Genre
+}
 
 @ApiModel(
   value = "Genre",
   description = "A genre"
 )
-case class DisplayGenre(@ApiModelProperty(
-  value = "A label given to a thing.")label: String,
-                        @ApiModelProperty(
-                          value = "Relates a genre to a list of concepts.")
-                        concepts: List[DisplayAbstractConcept],
-                        @JsonProperty("type") ontologyType: String = "Genre")
+case class DisplayGenre(
+  @ApiModelProperty(value = "A label given to a thing.") label: String,
+  @ApiModelProperty(value = "Relates a genre to a list of concepts.") concepts: List[
+    DisplayAbstractConcept],
+  @JsonProperty("type") ontologyType: String = "Genre")
 
 object DisplayGenre {
   def apply(genre: Genre[Displayable[AbstractConcept]]): DisplayGenre =
--- sbt_common/display/src/main/scala/uk/ac/wellcome/display/models/v2/DisplaySubject.scala
+++ sbt_common/display/src/main/scala/uk/ac/wellcome/display/models/v2/DisplaySubject.scala
@@ -2,20 +2,21 @@ package uk.ac.wellcome.display.models.v2
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.annotations.{ApiModel, ApiModelProperty}
-import uk.ac.wellcome.models.work.internal.{AbstractConcept, Displayable, Subject}
+import uk.ac.wellcome.models.work.internal.{
+  AbstractConcept,
+  Displayable,
+  Subject
+}
 
 @ApiModel(
   value = "Subject",
   description = "A subject"
 )
-case class DisplaySubject(@ApiModelProperty(
-  value = "A label given to a thing.")
-                           label: String,
-                          @ApiModelProperty(
-                            value = "Relates a subject to a list of concepts.")
-                          concepts: List[DisplayAbstractConcept],
-                          @JsonProperty("type") ontologyType: String =
-                            "Subject")
+case class DisplaySubject(
+  @ApiModelProperty(value = "A label given to a thing.") label: String,
+  @ApiModelProperty(value = "Relates a subject to a list of concepts.") concepts: List[
+    DisplayAbstractConcept],
+  @JsonProperty("type") ontologyType: String = "Subject")
 
 object DisplaySubject {
   def apply(subject: Subject[Displayable[AbstractConcept]]): DisplaySubject =
