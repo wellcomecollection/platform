b445748333d77ecfffb33ad2bcb83daa6d962f55
Alex Chan <alex@alexwlchan.net>
2018-05-15 09:12:16 +0100

    Use the new ServerFixtures trait throughout

--- sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/fixtures/Server.scala
+++ sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/fixtures/Server.scala
@@ -1,32 +1,12 @@
 package uk.ac.wellcome.platform.sierra_reader.fixtures
 
-import com.twitter.finatra.http.EmbeddedHttpServer
 import org.scalatest.Suite
 import uk.ac.wellcome.monitoring.test.fixtures.CloudWatch
 import uk.ac.wellcome.platform.sierra_reader.{Server => AppServer}
-import uk.ac.wellcome.test.fixtures.TestWith
+import uk.ac.wellcome.test.fixtures.ServerFixtures
 
-trait Server extends CloudWatch { this: Suite =>
-  def withServer[R](
-    flags: Map[String, String],
-    modifyServer: (EmbeddedHttpServer) => EmbeddedHttpServer = identity
-  )(testWith: TestWith[EmbeddedHttpServer, R]) = {
+trait Server extends ServerFixtures with CloudWatch { this: Suite =>
+  def newAppServer: () => AppServer = () => new AppServer()
 
-    val server: EmbeddedHttpServer = modifyServer(
-      new EmbeddedHttpServer(
-        new AppServer(),
-        flags = Map(
-          "aws.region" -> "localhost"
-        ) ++ flags ++ cloudWatchLocalFlags
-      )
-    )
-
-    server.start()
-
-    try {
-      testWith(server)
-    } finally {
-      server.close()
-    }
-  }
+  val defaultFlags: Map[String, String] = Map("aws.region" -> "localhost") ++ cloudWatchLocalFlags
 }
