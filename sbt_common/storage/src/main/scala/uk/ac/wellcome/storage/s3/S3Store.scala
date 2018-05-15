d0337124bfd8177cad7a878b4938a9d731c2ced7
Alex Chan <alex@alexwlchan.net>
2018-05-15 06:40:07 +0100

    Remove the S3Store trait for now
    Trying to get the implicits working is more effort than I care about
    in this patch.

--- sbt_common/storage/src/main/scala/uk/ac/wellcome/storage/s3/S3Store.scala
+++
@@ -1,9 +0,0 @@
-package uk.ac.wellcome.storage.s3
-
-import scala.concurrent.Future
-
-abstract trait S3Store[T] {
-  def put(t: T, keyPrefix: String): Future[S3ObjectLocation]
-
-  def get(s3ObjectLocation: S3ObjectLocation): Future[T]
-}
