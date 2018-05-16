b6f54cb55fbd260b9db9e6bd4b11aa871a138639
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-05-16 09:25:48 +0000

    Apply auto-formatting rules

--- sbt_common/storage/src/main/scala/uk/ac/wellcome/storage/dynamo/DynamoClientFactory.scala
+++ sbt_common/storage/src/main/scala/uk/ac/wellcome/storage/dynamo/DynamoClientFactory.scala
@@ -2,14 +2,16 @@ package uk.ac.wellcome.storage.dynamo
 
 import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
-import com.amazonaws.services.dynamodbv2.{AmazonDynamoDB, AmazonDynamoDBClientBuilder}
-
+import com.amazonaws.services.dynamodbv2.{
+  AmazonDynamoDB,
+  AmazonDynamoDBClientBuilder
+}
 
 object DynamoClientFactory {
   def create(region: String,
-                        endpoint: String,
-                        accessKey: String,
-                        secretKey: String): AmazonDynamoDB = {
+             endpoint: String,
+             accessKey: String,
+             secretKey: String): AmazonDynamoDB = {
     val standardClient = AmazonDynamoDBClientBuilder.standard
     if (endpoint.isEmpty)
       standardClient
@@ -20,8 +22,7 @@ object DynamoClientFactory {
         .withCredentials(
           new AWSStaticCredentialsProvider(
             new BasicAWSCredentials(accessKey, secretKey)))
-        .withEndpointConfiguration(
-          new EndpointConfiguration(endpoint, region))
+        .withEndpointConfiguration(new EndpointConfiguration(endpoint, region))
         .build()
   }
 }
--- sbt_common/storage/src/main/scala/uk/ac/wellcome/storage/s3/S3ClientFactory.scala
+++ sbt_common/storage/src/main/scala/uk/ac/wellcome/storage/s3/S3ClientFactory.scala
@@ -4,12 +4,11 @@ import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 
-
 object S3ClientFactory {
   def create(region: String,
-                    endpoint: String,
-                    accessKey: String,
-                    secretKey: String): AmazonS3 = {
+             endpoint: String,
+             accessKey: String,
+             secretKey: String): AmazonS3 = {
     val standardClient = AmazonS3ClientBuilder.standard
     if (endpoint.isEmpty)
       standardClient
@@ -20,8 +19,7 @@ object S3ClientFactory {
         .withCredentials(new AWSStaticCredentialsProvider(
           new BasicAWSCredentials(accessKey, secretKey)))
         .withPathStyleAccessEnabled(true)
-        .withEndpointConfiguration(
-          new EndpointConfiguration(endpoint, region))
+        .withEndpointConfiguration(new EndpointConfiguration(endpoint, region))
         .build()
   }
 }
