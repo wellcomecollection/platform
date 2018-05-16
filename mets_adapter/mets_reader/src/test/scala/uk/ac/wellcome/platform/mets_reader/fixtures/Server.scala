9553331f67bb5a7c183f806c9438dff784bf40fd
Alex Chan <a.chan@wellcome.ac.uk>
2018-05-16 12:20:59 +0100

    Create a bare minimum METS reader application

---
+++ mets_adapter/mets_reader/src/test/scala/uk/ac/wellcome/platform/mets_reader/fixtures/Server.scala
@@ -0,0 +1,13 @@
+package uk.ac.wellcome.platform.mets_reader.fixtures
+
+import org.scalatest.Suite
+import uk.ac.wellcome.monitoring.test.fixtures.CloudWatch
+import uk.ac.wellcome.platform.mets_reader.{Server => AppServer}
+import uk.ac.wellcome.test.fixtures.ServerFixtures
+
+trait Server extends ServerFixtures with CloudWatch { this: Suite =>
+  def newAppServer: () => AppServer = () => new AppServer()
+
+  val defaultFlags
+    : Map[String, String] = Map("aws.region" -> "localhost") ++ cloudWatchLocalFlags
+}
