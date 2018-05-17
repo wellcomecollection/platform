6641d9c3c1e295fc82124bc099bf7eb710252e6f
caylaa <alicefuzier@gmail.com>
2018-05-17 14:06:33 +0100

    Some fixes

--- common/src/test/scala/uk/ac/wellcome/test/fixtures/ServerFixtures.scala
+++ common/src/test/scala/uk/ac/wellcome/test/fixtures/ServerFixtures.scala
@@ -8,8 +8,7 @@ trait ServerFixtures { this: Suite =>
 
   def withServer[R](appServer: => Ports,
                     flags: Map[String, String],
-                    modifyServer: (EmbeddedHttpServer) => EmbeddedHttpServer =
-                      identity)(testWith: TestWith[EmbeddedHttpServer, R]) = {
+                    modifyServer: EmbeddedHttpServer => EmbeddedHttpServer)(testWith: TestWith[EmbeddedHttpServer, R]) = {
     val server: EmbeddedHttpServer = modifyServer(
       new EmbeddedHttpServer(
         appServer,
