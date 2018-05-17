0be6ca96a44ed20cf0e08a423b4eb22ea2b70b07
caylaa <alicefuzier@gmail.com>
2018-05-17 11:31:47 +0100

    Fix ingestor

--- catalogue_pipeline/ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/fixtures/Server.scala
+++ catalogue_pipeline/ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/fixtures/Server.scala
@@ -5,9 +5,6 @@ import uk.ac.wellcome.monitoring.test.fixtures.CloudWatch
 import uk.ac.wellcome.platform.ingestor.{Server => AppServer}
 import uk.ac.wellcome.test.fixtures.ServerFixtures
 
-trait Server extends ServerFixtures with CloudWatch { this: Suite =>
-  def newAppServer: () => AppServer = () => new AppServer()
-
-  val defaultFlags
-    : Map[String, String] = Map("aws.region" -> "localhost") ++ cloudWatchLocalFlags
+trait Server extends ServerFixtures with CloudWatch {this: Suite =>
+  def withServer(flags: Map[String, String]) = withServer(new AppServer, flags ++ Map("aws.region" -> "localhost") ++ cloudWatchLocalFlags)
 }
