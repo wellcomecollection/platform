ecf43cdab92d4ae3fba8643c6ae41b7ab61f4279
Alex Chan <alex@alexwlchan.net>
2018-05-17 10:16:06 +0100

    enforce type bounds

--- sbt_common/finatra_storage/src/main/scala/uk/ac/wellcome/finatra/storage/VHSModule.scala
+++ sbt_common/finatra_storage/src/main/scala/uk/ac/wellcome/finatra/storage/VHSModule.scala
@@ -6,6 +6,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.s3.AmazonS3
 import com.google.inject.Provides
 import com.twitter.inject.TwitterModule
+import uk.ac.wellcome.models.Id
 import uk.ac.wellcome.storage.s3.KeyPrefixGenerator
 import uk.ac.wellcome.storage.vhs.{VHSConfig, VersionedHybridStore}
 
@@ -18,10 +19,10 @@ object VHSModule extends TwitterModule {
 
   @Singleton
   @Provides
-  def providesVHS[T](vhsConfig: VHSConfig,
-                     s3Client: AmazonS3,
-                     keyPrefixGenerator: KeyPrefixGenerator[T],
-                     dynamoDbClient: AmazonDynamoDB): VersionedHybridStore[T] = new VersionedHybridStore[T](
+  def providesVHS[T <: Id](vhsConfig: VHSConfig,
+                           s3Client: AmazonS3,
+                           keyPrefixGenerator: KeyPrefixGenerator[T],
+                           dynamoDbClient: AmazonDynamoDB): VersionedHybridStore[T] = new VersionedHybridStore[T](
     vhsConfig = vhsConfig,
     s3Client = s3Client,
     keyPrefixGenerator = keyPrefixGenerator,
