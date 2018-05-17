5ba7fe3e9d5a37ce5f63a7850f4b9dd74a9aac5b
caylaa <alicefuzier@gmail.com>
2018-05-17 13:55:03 +0100

    Fix compilation

--- catalogue_api/api/src/test/scala/uk/ac/wellcome/platform/api/fixtures/Server.scala
+++ catalogue_api/api/src/test/scala/uk/ac/wellcome/platform/api/fixtures/Server.scala
@@ -1,10 +1,11 @@
 package uk.ac.wellcome.platform.api.fixtures
 
+import com.twitter.finatra.http.EmbeddedHttpServer
 import org.scalatest.Suite
 import uk.ac.wellcome.platform.api.{Server => AppServer}
-import uk.ac.wellcome.test.fixtures.ServerFixtures
+import uk.ac.wellcome.test.fixtures.{ServerFixtures, TestWith}
 
 trait Server extends ServerFixtures { this: Suite =>
-  def withServer[R](flags: Map[String, String]) =
-    withServer[R](new AppServer, flags)
+  def withServer[R](flags: Map[String, String])(testWith: TestWith[EmbeddedHttpServer, R]): R =
+    withServer[R](new AppServer, flags)(testWith)
 }
