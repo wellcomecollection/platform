71982e1a3949cf9ce51af290fb739f21cdc80ba0
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-05-18 13:23:41 +0100

    api ec up a level

--- catalogue_api/api/src/main/scala/uk/ac/wellcome/platform/api/GlobalExecutionContext.scala
+++ catalogue_api/api/src/main/scala/uk/ac/wellcome/platform/api/GlobalExecutionContext.scala
@@ -1,4 +1,4 @@
-package uk.ac.wellcome.platform.api.models
+package uk.ac.wellcome.platform.api
 
 import scala.concurrent.ExecutionContext
 
--- catalogue_api/api/src/main/scala/uk/ac/wellcome/platform/api/controllers/ManagementController.scala
+++ catalogue_api/api/src/main/scala/uk/ac/wellcome/platform/api/controllers/ManagementController.scala
@@ -6,7 +6,7 @@ import com.sksamuel.elastic4s.http.HttpClient
 import com.sksamuel.elastic4s.http.ElasticDsl._
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
-import uk.ac.wellcome.platform.api.models.GlobalExecutionContext.context
+import uk.ac.wellcome.platform.api.GlobalExecutionContext.context
 
 @Singleton
 class ManagementController @Inject()(
