c07dd238a86f554479a171b00a8c07c2814bd7c2
Alex Chan <alex@alexwlchan.net>
2018-05-19 09:05:17 +0100

    Okay, so we can actually get rid of ServerFixtures now

--- catalogue_api/api/src/test/scala/uk/ac/wellcome/platform/api/fixtures/Server.scala
+++ catalogue_api/api/src/test/scala/uk/ac/wellcome/platform/api/fixtures/Server.scala
@@ -3,10 +3,22 @@ package uk.ac.wellcome.platform.api.fixtures
 import com.twitter.finatra.http.EmbeddedHttpServer
 import org.scalatest.Suite
 import uk.ac.wellcome.platform.api.{Server => AppServer}
-import uk.ac.wellcome.test.fixtures.{ServerFixtures, TestWith}
+import uk.ac.wellcome.test.fixtures.TestWith
 
-trait Server extends ServerFixtures { this: Suite =>
+trait Server { this: Suite =>
   def withServer[R](flags: Map[String, String])(
-    testWith: TestWith[EmbeddedHttpServer, R]): R =
-    withServer[R](new AppServer, flags)(testWith)
+    testWith: TestWith[EmbeddedHttpServer, R]): R = {
+    val server: EmbeddedHttpServer = new EmbeddedHttpServer(
+      new AppServer(),
+      flags = flags
+    )
+
+    server.start()
+
+    try {
+      testWith(server)
+    } finally {
+      server.close()
+    }
+  }
 }
