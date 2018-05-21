d814c9c131f2c2957173302f7c4c1cbf2b719b45
Alex Chan <a.chan@wellcome.ac.uk>
2018-05-21 11:53:56 +0100

    Check update dates in the RFC headers

--- check_rfc_header.py
+++ check_rfc_header.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8
 
+import datetime as dt
 import os
 
-from travistooling import ROOT
+from travistooling import git, ROOT
 
 
 def get_rfc_readmes(repo):
@@ -14,18 +15,21 @@ def get_rfc_readmes(repo):
                 yield os.path.join(root, f)
 
 
-print('*** Checking RFC headers')
+if __name__ == '__main__':
+    print('*** Checking RFC headers')
 
-for f in get_rfc_readmes(ROOT):
-    print('*** Checking header for %s' % os.path.relpath(f, start=ROOT))
-    filename = os.path.basename(os.path.dirname(f))
-    number, name = filename.split('-', 1)
+    for f in get_rfc_readmes(ROOT):
+        print('*** Checking header for %s' % os.path.relpath(f, start=ROOT))
+        filename = os.path.basename(os.path.dirname(f))
+        number, name = filename.split('-', 1)
 
-    contents = open(f).read()
-    header = contents.splitlines()[:3]
+        contents = open(f).read()
+        header = contents.splitlines()[:3]
 
-    assert header[0].startswith('# RFC %03d: ' % int(number))
-    assert header[1] == ''
+        update_timestamp = git('log', '-1', '--format=%ct', f)
+        last_updated = dt.datetime.fromtimestamp(int(update_timestamp))
 
-    print(f, name)
-    print(header)
+        assert header[0].startswith('# RFC %03d: ' % int(number))
+        assert header[1] == ''
+        expected_date_str = '**Last updated: %s.**' % last_updated.strftime('%d %B %Y')
+        assert header[2] == expected_date_str, (header[2], expected_date_str)
