1bd2edac66bb2b49dfd91c1f1cf0d6bbe4410611
caylaa <alicefuzier@gmail.com>
2018-05-22 12:05:52 +0100

    Update finatra

--- sbt_common/display/src/main/scala/uk/ac/wellcome/display/modules/DisplayJacksonModule.scala
+++ sbt_common/display/src/main/scala/uk/ac/wellcome/display/modules/DisplayJacksonModule.scala
@@ -1,11 +1,11 @@
 package uk.ac.wellcome.display.modules
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.twitter.finatra.json.modules.FinatraJacksonModule
-import com.twitter.finatra.json.utils.CamelCasePropertyNamingStrategy
 import uk.ac.wellcome.display.models.WorksIncludesDeserializerModule
 
 object DisplayJacksonModule extends FinatraJacksonModule {
-  override val propertyNamingStrategy = CamelCasePropertyNamingStrategy
+  override val propertyNamingStrategy = PropertyNamingStrategy.LOWER_CAMEL_CASE
   override val additionalJacksonModules = Seq(
     new WorksIncludesDeserializerModule
   )
