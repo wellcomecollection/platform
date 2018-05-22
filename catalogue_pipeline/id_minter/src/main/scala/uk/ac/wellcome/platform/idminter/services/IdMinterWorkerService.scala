8b21af8a04578eb7cff64cfcaeb0597b52ac89a0
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-05-22 10:35:37 +0100

    Use streams in id_minter

--- catalogue_pipeline/id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/services/IdMinterWorkerService.scala
+++ catalogue_pipeline/id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/services/IdMinterWorkerService.scala
@@ -1,34 +1,28 @@
 package uk.ac.wellcome.platform.idminter.services
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Terminated}
 import com.google.inject.Inject
-import io.circe.{Decoder, Json}
-import uk.ac.wellcome.messaging.message.{
-  MessageReader,
-  MessageWorker,
-  MessageWriter
-}
-import uk.ac.wellcome.monitoring.MetricsSender
+import io.circe.Json
+import uk.ac.wellcome.messaging.message.{MessageStream, MessageWriter}
 import uk.ac.wellcome.platform.idminter.steps.IdEmbedder
 import uk.ac.wellcome.platform.idminter.GlobalExecutionContext.context
 
+import uk.ac.wellcome.utils.JsonUtil._
+
 import scala.concurrent.Future
-import scala.concurrent.duration._
 
 class IdMinterWorkerService @Inject()(
   idEmbedder: IdEmbedder,
   writer: MessageWriter[Json],
-  messageReader: MessageReader[Json],
-  system: ActorSystem,
-  metrics: MetricsSender
-) extends MessageWorker[Json](messageReader, system, metrics) {
+  messageStream: MessageStream[Json],
+  system: ActorSystem
+) {
 
-  override lazy val poll = 100 milliseconds
+  messageStream.foreach(this.getClass.getSimpleName, processMessage)
+
   val snsSubject = "identified-item"
 
-  override implicit val decoder: Decoder[Json] = Decoder.decodeJson
-
-  override def processMessage(json: Json): Future[Unit] =
+  def processMessage(json: Json): Future[Unit] =
     for {
       identifiedJson <- idEmbedder.embedId(json)
       _ <- writer.write(
@@ -36,4 +30,8 @@ class IdMinterWorkerService @Inject()(
         subject = s"source: ${this.getClass.getSimpleName}.processMessage"
       )
     } yield ()
+
+  def stop(): Future[Terminated] = {
+    system.terminate()
+  }
 }
