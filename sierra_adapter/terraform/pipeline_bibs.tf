0cc746fca6f5a303eac36283732407aea7b5b3d8
Alex Chan <a.chan@wellcome.ac.uk>
2018-05-22 16:19:48 +0100

    Bump the windows for more redundancy on the boundaries

--- sierra_adapter/terraform/pipeline_bibs.tf
+++ sierra_adapter/terraform/pipeline_bibs.tf
@@ -3,7 +3,7 @@ module "bibs_window_generator" {
 
   resource_type = "bibs"
 
-  window_length_minutes    = 30
+  window_length_minutes    = 31
   trigger_interval_minutes = 15
 
   lambda_error_alarm_arn = "${local.lambda_error_alarm_arn}"
