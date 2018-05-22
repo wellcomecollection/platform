33e8d7cc0f0d157ce44c1d262064e34b7d30da4c
Alex Chan <a.chan@wellcome.ac.uk>
2018-05-22 17:53:16 +0100

    Expunge KeyPrefixGenerator from the project

--- catalogue_pipeline/ingestor/src/main/scala/uk/ac/wellcome/platform/ingestor/modules/IdentifiedWorkModule.scala
+++ catalogue_pipeline/ingestor/src/main/scala/uk/ac/wellcome/platform/ingestor/modules/IdentifiedWorkModule.scala
@@ -6,16 +6,9 @@ import com.twitter.inject.TwitterModule
 import io.circe.generic.extras.semiauto.{deriveDecoder, deriveEncoder}
 import io.circe.{Decoder, Encoder}
 import uk.ac.wellcome.models.work.internal.IdentifiedWork
-import uk.ac.wellcome.storage.s3.KeyPrefixGenerator
-
 import uk.ac.wellcome.utils.JsonUtil._
 
 object IdentifiedWorkModule extends TwitterModule {
-  @Provides
-  @Singleton
-  def provideKeyPrefixGenerator(): KeyPrefixGenerator[IdentifiedWork] =
-    new IdentifiedWorkKeyPrefixGenerator()
-
   @Provides
   @Singleton
   def provideIdentifiedWorkDecoder(): Decoder[IdentifiedWork] =
@@ -25,11 +18,4 @@ object IdentifiedWorkModule extends TwitterModule {
   @Singleton
   def provideIdentifiedWorkEncoder(): Encoder[IdentifiedWork] =
     deriveEncoder[IdentifiedWork]
-}
-
-class IdentifiedWorkKeyPrefixGenerator
-    extends KeyPrefixGenerator[IdentifiedWork] {
-  override def generate(obj: IdentifiedWork): String = {
-    obj.sourceIdentifier.value.reverse.slice(0, 2)
-  }
 }
