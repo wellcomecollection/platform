c7c4cbfe6ef4982e658361f6aa5ddd2c6178c4aa
Alex Chan <alex@alexwlchan.net>
2018-05-24 11:32:56 +0100

    Hard-code all the identifiers we know about

--- sbt_common/internal_model/src/main/scala/uk/ac/wellcome/models/work/internal/IdentifierType.scala
+++ sbt_common/internal_model/src/main/scala/uk/ac/wellcome/models/work/internal/IdentifierType.scala
@@ -45,11 +45,32 @@ case object IdentifierType {
     }
     .fold(Map[String, IdentifierType]()) { (x, y) => x ++ y}
 
-  def apply(platformId: String): IdentifierType =
-    identifierTypeMap.get(platformId) match {
+  private def lookupPlatformId(id: String): IdentifierType =
+    identifierTypeMap.get(id) match {
       case Some(identifierType) => identifierType
       case None => throw GracefulFailureException(
-        new RuntimeException(s"Unrecognised identifier type: [$platformId]")
+        new RuntimeException(s"Unrecognised identifier type: [$id]")
       )
     }
+
+  // This isn't ideal, because we're duplicating the list from the CSV,
+  // but it gives us a bit of type-safety.
+  //
+  // If any of these values don't resolve, we fail immediately upon
+  // startup -- not when we try to load the value!
+  //
+  final val miroImageNumber = lookupPlatformId("MiroImageNumber")
+  final val wellcomeLibraryVideoDiskNumber = lookupPlatformId("WellcomeLibraryVideodiskNumber")
+  final val sierraSystemNumber = lookupPlatformId("SierraSystemNumber")
+  final val calmRefNo = lookupPlatformId("CALMRefNo")
+  final val calmAltRefNo = lookupPlatformId("CalmAltRefNo")
+  final val lcGraphicMaterials = lookupPlatformId("LCGMGPC")
+  final val lcSubjectHeadings = lookupPlatformId("lcsh")
+  final val lcNames = lookupPlatformId("LCNames")
+  final val mesh = lookupPlatformId("MESHId")
+  final val calmRecordId = lookupPlatformId("CALMRecordId")
+  final val preservicaGUID = lookupPlatformId("PerserviceGUID")
+  final val calmAuthorityCode = lookupPlatformId("CALMAuthorityCode")
+  final val isbn = lookupPlatformId("ISBN")
+  final val issn = lookupPlatformId("ISSN")
 }
