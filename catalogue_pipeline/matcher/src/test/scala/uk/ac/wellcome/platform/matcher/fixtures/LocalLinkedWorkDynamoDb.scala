894b06b886f729cd3ae804a0481bc5dfb5b558a8
caylaa <alicefuzier@gmail.com>
2018-05-24 11:27:58 +0100

    Use LocalDynamoDB

--- catalogue_pipeline/matcher/src/test/scala/uk/ac/wellcome/platform/matcher/fixtures/LocalLinkedWorkDynamoDb.scala
+++ catalogue_pipeline/matcher/src/test/scala/uk/ac/wellcome/platform/matcher/fixtures/LocalLinkedWorkDynamoDb.scala
@@ -1,62 +1,14 @@
 package uk.ac.wellcome.platform.matcher.fixtures
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.dynamodbv2.model._
 import com.amazonaws.services.dynamodbv2.util.TableUtils.waitUntilActive
-import org.scalatest.concurrent.Eventually
-import uk.ac.wellcome.storage.dynamo.DynamoClientFactory
+import uk.ac.wellcome.storage.test.fixtures.LocalDynamoDb
 import uk.ac.wellcome.storage.test.fixtures.LocalDynamoDb.Table
-import uk.ac.wellcome.test.fixtures._
-import uk.ac.wellcome.test.utils.ExtendedPatience
-
-import scala.collection.JavaConverters._
-import scala.util.Random
 
 trait LocalLinkedWorkDynamoDb
-  extends Eventually
-    with ExtendedPatience
+  extends LocalDynamoDb
 {
-
-  private val port = 45678
-  private val dynamoDBEndPoint = "http://localhost:" + port
-
-  private val regionName = "localhost"
-
-  private val accessKey = "access"
-  private val secretKey = "secret"
-
-  def dynamoDbLocalEndpointFlags(table: Table) = dynamoClientLocalFlags ++ Map(
-    "aws.dynamo.tableName" -> table.name,
-    "aws.dynamo.tableIndex" -> table.index
-  )
-
-  def dynamoClientLocalFlags = Map(
-    "aws.dynamoDb.endpoint" -> dynamoDBEndPoint,
-    "aws.dynamoDb.accessKey" -> accessKey,
-    "aws.dynamoDb.secretKey" -> secretKey,
-    "aws.dynamoDb.region" -> regionName
-  )
-
-  val dynamoDbClient: AmazonDynamoDB = DynamoClientFactory.create(
-    region = regionName,
-    endpoint = dynamoDBEndPoint,
-    accessKey = accessKey,
-    secretKey = secretKey
-  )
-
-  def withLocalDynamoDbTable[R] = fixture[Table, R](
-    create = {
-      val tableName = Random.alphanumeric.take(10).mkString
-      val indexName = Random.alphanumeric.take(10).mkString
-
-      createTable(Table(tableName, indexName))
-    },
-    destroy = { _ =>
-      deleteAllTables()
-    }
-  )
-
-  private def createTable(table: Table): Table = {
+  override def createTable(table: Table): Table = {
     dynamoDbClient.createTable(
       new CreateTableRequest()
         .withTableName(table.name)
@@ -91,13 +43,5 @@ trait LocalLinkedWorkDynamoDb
       waitUntilActive(dynamoDbClient, table.name)
     }
     table
-  }
-
-  private def deleteAllTables() = {
-    dynamoDbClient
-      .listTables()
-      .getTableNames
-      .asScala
-      .foreach(dynamoDbClient.deleteTable)
   }
 }
