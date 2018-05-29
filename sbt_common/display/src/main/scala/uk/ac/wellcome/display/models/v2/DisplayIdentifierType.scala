63bb521e3a35b5c94661e7eb723a5a06853c5da4
Alex Chan <a.chan@wellcome.ac.uk>
2018-05-29 13:04:34 +0100

    Add the initial DisplayIdentifierType model

---
+++ sbt_common/display/src/main/scala/uk/ac/wellcome/display/models/v2/DisplayIdentifierType.scala
@@ -0,0 +1,23 @@
+package uk.ac.wellcome.display.models.v2
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.annotations.{ApiModel, ApiModelProperty}
+import uk.ac.wellcome.models.work.internal.IdentifierType
+
+@ApiModel(
+  value = "IdentifierType"
+)
+case class DisplayIdentifierType(
+  @ApiModelProperty id: String,
+  @ApiModelProperty label: String
+) {
+  @JsonProperty("type") val ontologyType: String = "IdentifierType"
+}
+
+object DisplayIdentifierType {
+  def apply(identifierType: IdentifierType): DisplayIdentifierType =
+    DisplayIdentifierType(
+      id = identifierType.id,
+      label = identifierType.label
+    )
+}
