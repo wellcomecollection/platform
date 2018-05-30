11183bffe001bc21a6e84f17aa3a0124855f1c04
Alex Chan <alex@alexwlchan.net>
2018-05-30 10:59:03 +0100

    Another explanatory comment

--- sbt_common/display/src/test/scala/uk/ac/wellcome/display/models/v1/DisplayLicenseV1Test.scala
+++ sbt_common/display/src/test/scala/uk/ac/wellcome/display/models/v1/DisplayLicenseV1Test.scala
@@ -8,6 +8,9 @@ class DisplayLicenseV1Test extends FunSpec with Matchers {
   it("should read a License as a DisplayLicenseV1 correctly") {
     val displayLicense = DisplayLicenseV1(License_CCBY)
 
+    // These outputs are deliberately hard-coded -- because the V1 API
+    // is public, we shouldn't be changing the output even when the
+    // internal model changes.
     displayLicense.licenseType shouldBe "CC-BY"
     displayLicense.label shouldBe "Attribution 4.0 International (CC BY 4.0)"
     displayLicense.url shouldBe "http://creativecommons.org/licenses/by/4.0/"
