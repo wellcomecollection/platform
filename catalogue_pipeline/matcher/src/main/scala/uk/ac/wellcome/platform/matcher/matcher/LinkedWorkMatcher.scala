69828892d18afbb9820f63b2e57c307eb0af41a0
Alex Chan <alex@alexwlchan.net>
2018-05-31 07:57:21 +0100

    Collapse two methods into one, simplify

--- catalogue_pipeline/matcher/src/main/scala/uk/ac/wellcome/platform/matcher/matcher/LinkedWorkMatcher.scala
+++ catalogue_pipeline/matcher/src/main/scala/uk/ac/wellcome/platform/matcher/matcher/LinkedWorkMatcher.scala
@@ -2,12 +2,7 @@ package uk.ac.wellcome.platform.matcher.matcher
 
 import com.google.inject.Inject
 import uk.ac.wellcome.models.work.internal.UnidentifiedWork
-import uk.ac.wellcome.platform.matcher.models.{
-  IdentifierList,
-  LinkedWorksIdentifiersList,
-  WorkGraph,
-  WorkNodeUpdate
-}
+import uk.ac.wellcome.platform.matcher.models._
 import uk.ac.wellcome.platform.matcher.storage.WorkGraphStore
 import uk.ac.wellcome.platform.matcher.workgraph.LinkedWorkGraphUpdater
 import uk.ac.wellcome.storage.GlobalExecutionContext._
@@ -34,15 +29,11 @@ class LinkedWorkMatcher @Inject()(workGraphStore: WorkGraphStore) {
     }
   }
 
-  private def convertToIdentifiersList(updatedLinkedWorkGraph: WorkGraph) = {
-    groupBySetId(updatedLinkedWorkGraph).map {
-      case (_, linkedWorkList) =>
-        IdentifierList(linkedWorkList.map(_.id))
-    }.toSet
-  }
-
-  private def groupBySetId(updatedLinkedWorkGraph: WorkGraph) = {
-    updatedLinkedWorkGraph.nodes
-      .groupBy(_.componentId)
-  }
+  private def convertToIdentifiersList(workGraph: WorkGraph): Set[IdentifierList] =
+    workGraph.nodes
+      .groupBy { _.componentId }
+      .values
+      .map { (nodeSet: Set[WorkNode]) => nodeSet.map { _.id }}
+      .map { (nodeIds: Set[String]) => IdentifierList(nodeIds) }
+      .toSet
 }
