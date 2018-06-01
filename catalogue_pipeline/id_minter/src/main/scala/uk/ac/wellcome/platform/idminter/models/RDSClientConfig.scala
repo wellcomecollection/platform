d80fa344aa3e1d88d22f4eddf3f62f8810aafaa0
Alex Chan <a.chan@wellcome.ac.uk>
2018-06-01 12:09:56 +0100

    Use RDSClientConfig and module in the ID minter

---
+++ catalogue_pipeline/id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/models/RDSClientConfig.scala
@@ -0,0 +1,8 @@
+package uk.ac.wellcome.platform.idminter.models
+
+case class RDSClientConfig(
+  host: String,
+  port: String,
+  username: String,
+  password: String
+)
--- catalogue_pipeline/id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/modules/MysqlModule.scala
+++ catalogue_pipeline/id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/modules/MysqlModule.scala
@@ -3,29 +3,19 @@ package uk.ac.wellcome.platform.idminter.modules
 import com.google.inject.Provides
 import com.twitter.inject.TwitterModule
 import scalikejdbc.{ConnectionPool, DB}
+import uk.ac.wellcome.platform.idminter.models.RDSClientConfig
 
 object MysqlModule extends TwitterModule {
-
-  private val host =
-    flag[String]("aws.rds.host", "", "Host of the MySQL database")
-  private val port =
-    flag[String]("aws.rds.port", "3306", "Port of the MySQL database")
-  private val userName = flag[String](
-    "aws.rds.userName",
-    "",
-    "User to connect to the MySQL database")
-  private val password = flag[String](
-    "aws.rds.password",
-    "",
-    "Password to connect to the MySQL database")
+  override val modules = Seq(RDSClientConfigModule)
 
   @Provides
-  def providesDB(): DB = {
+  def providesDB(rdsClientConfig: RDSClientConfig): DB = {
     Class.forName("com.mysql.jdbc.Driver")
     ConnectionPool.singleton(
-      s"jdbc:mysql://${host()}:${port()}",
-      userName(),
-      password())
+      s"jdbc:mysql://${rdsClientConfig.host}:${rdsClientConfig.port}",
+      user = rdsClientConfig.username,
+      password = rdsClientConfig.password
+    )
     DB.connect()
   }
 }
