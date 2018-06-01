a18cf140de4d33be7f0c67bdf1bb5b24d7b02791
Alex Chan <a.chan@wellcome.ac.uk>
2018-06-01 16:23:28 +0100

    We don't need those flags any more

--- reindexer/reindex_worker/src/test/scala/uk/ac/wellcome/platform/reindex_worker/ReindexerFeatureTest.scala
+++ reindexer/reindex_worker/src/test/scala/uk/ac/wellcome/platform/reindex_worker/ReindexerFeatureTest.scala
@@ -91,8 +91,7 @@ class ReindexerFeatureTest
         withLocalDynamoDbTable { table =>
           val flags
             : Map[String, String] = snsLocalFlags(topic) ++ dynamoDbLocalEndpointFlags(
-            table) ++ sqsLocalFlags(queue) ++ Map(
-            "aws.dynamo.indexName" -> table.index)
+            table) ++ sqsLocalFlags(queue)
 
           withServer(flags) { _ =>
             val expectedRecords =
@@ -118,8 +117,7 @@ class ReindexerFeatureTest
         withLocalDynamoDbTable { table =>
           val flags
             : Map[String, String] = snsLocalFlags(topic) ++ dynamoDbLocalEndpointFlags(
-            table) ++ sqsLocalFlags(queue) ++ Map(
-            "aws.dynamo.indexName" -> table.index)
+            table) ++ sqsLocalFlags(queue)
 
           withServer(flags) { _ =>
             createReindexableData(queue, table)
@@ -152,8 +150,7 @@ class ReindexerFeatureTest
         withLocalDynamoDbTable { table =>
           val flags
             : Map[String, String] = snsLocalFlags(topic) ++ dynamoDbLocalEndpointFlags(table
-            .copy(name = "non_existent_table")) ++ sqsLocalFlags(queue) ++ Map(
-            "aws.dynamo.indexName" -> table.index)
+            .copy(name = "non_existent_table")) ++ sqsLocalFlags(queue)
 
           withServer(flags) { _ =>
             createReindexableData(queue, table)
