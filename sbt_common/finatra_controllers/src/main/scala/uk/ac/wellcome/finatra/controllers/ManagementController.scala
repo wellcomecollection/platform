a9bf3623afa8036ea4be04d74c58e1a5e3f2c67e
Robert Kenny <kenoir@users.noreply.github.com>
2018-06-04 12:54:33 +0100

    Adds /management/manifest endpoint exposing APP_NAME (#2175)
    * Adds /management/manifest endpoint exposing APP_NAME
    
    * Apply auto-formatting rules

--- sbt_common/finatra_controllers/src/main/scala/uk/ac/wellcome/finatra/controllers/ManagementController.scala
+++ sbt_common/finatra_controllers/src/main/scala/uk/ac/wellcome/finatra/controllers/ManagementController.scala
@@ -7,7 +7,13 @@ import com.twitter.finatra.http.Controller
 
 @Singleton
 class ManagementController @Inject()() extends Controller {
+  val appName = scala.util.Properties.envOrElse("APP_NAME", "not-specified")
+
   get("/management/healthcheck") { request: Request =>
     response.ok.json(Map("message" -> "ok"))
+  }
+
+  get("/management/manifest") { request: Request =>
+    response.ok.json(Map("name" -> appName))
   }
 }
