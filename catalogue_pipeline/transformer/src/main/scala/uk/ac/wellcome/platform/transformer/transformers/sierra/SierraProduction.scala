5e64f98dd02551f0142329e490112220ea03db1e
Alex Chan <a.chan@wellcome.ac.uk>
2018-06-06 17:02:50 +0100

    Write a first test for production events in Sierra

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/transformers/sierra/SierraProduction.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/transformers/sierra/SierraProduction.scala
@@ -4,7 +4,20 @@ import uk.ac.wellcome.models.work.internal.{AbstractAgent, MaybeDisplayable, Pro
 import uk.ac.wellcome.platform.transformer.source.SierraBibData
 
 trait SierraProduction {
+
+  // Populate wwork:production.
+  //
+  // Information about production can come from two fields in MARC: 260 & 264.
+  // At Wellcome, 260 is what was used historically -- 264 is what we're moving
+  // towards, using RDA rules.
+  //
+  // It is theoretically possible for a bib record to have both 260 and 264,
+  // but it would be a cataloguing error -- we should reject it, and flag it
+  // to the librarians.
+  //
   def getProduction(bibData: SierraBibData): List[ProductionEvent[MaybeDisplayable[AbstractAgent]]] = {
+//    val marc260fields = bibData.varFields.filter { _.fieldTag == Some("260") }
+//    val marc264fields = bibData.varFields.filter { _.fieldTag == Some("264") }
     List()
   }
 }
---
+++ catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/platform/transformer/transformers/sierra/SierraProductionTest.scala
@@ -0,0 +1,30 @@
+package uk.ac.wellcome.platform.transformer.transformers.sierra
+
+import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.models.work.internal.{AbstractAgent, MaybeDisplayable, ProductionEvent}
+import uk.ac.wellcome.platform.transformer.source.{SierraBibData, VarField}
+
+class SierraProductionTest extends FunSpec with Matchers {
+
+  it("returns an empty list if neither 260 nor 264 are present") {
+    transformVarFieldsAndAssertProductionIsCorrect(
+      varFields = List(),
+      expectedProduction = List()
+    )
+  }
+
+  private def transformVarFieldsAndAssertProductionIsCorrect(
+    varFields: List[VarField],
+    expectedProduction: List[ProductionEvent[MaybeDisplayable[AbstractAgent]]]
+  ) = {
+    val bibData = SierraBibData(
+      id = "p1000001",
+      title = Some("Practical production of poisonous panthers"),
+      varFields = varFields
+    )
+
+    transformer.getProduction(bibData) shouldBe expectedProduction
+  }
+
+  val transformer = new SierraProduction {}
+}
