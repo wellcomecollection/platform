6ab33a5e9481d775dba483a62c8bd1de6a2216aa
Alex Chan <a.chan@wellcome.ac.uk>
2018-06-06 12:46:37 +0100

    Add tests that we reject images with bad usage restrictions

---
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/transformers/miro/ShouldNotTransformException.scala
@@ -0,0 +1,3 @@
+package uk.ac.wellcome.platform.transformer.transformers.miro
+
+class ShouldNotTransformException(message: String) extends RuntimeException(message)
--- catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/platform/transformer/transformers/miro/MiroLicensesTest.scala
+++ catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/platform/transformer/transformers/miro/MiroLicensesTest.scala
@@ -9,5 +9,17 @@ class MiroLicensesTest extends FunSpec with Matchers {
     transformer.chooseLicense(useRestrictions = "CC-0") shouldBe License_CC0
   }
 
+  it("rejects restrictions 'Do not use'") {
+    intercept[ShouldNotTransformException] {
+      transformer.chooseLicense(useRestrictions = "Do not use")
+    }
+  }
+
+  it("rejects restrictions 'Image withdrawn, see notes'") {
+    intercept[ShouldNotTransformException] {
+      transformer.chooseLicense(useRestrictions = "Image withdrawn, see notes")
+    }
+  }
+
   val transformer = new MiroLicenses {}
 }
