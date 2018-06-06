8074b692d4af4922a55b5cf43ee45e4b0a25295b
Alex Chan <alex@alexwlchan.net>
2018-06-06 07:19:15 +0100

    Add an alarm on the snapshot generator queue to spot startup issues

--- data_api/terraform/queues.tf
+++ data_api/terraform/queues.tf
@@ -8,3 +8,24 @@ module "snapshot_generator_queue" {
   alarm_topic_arn            = "${local.dlq_alarm_arn}"
   visibility_timeout_seconds = 1800
 }
+
+# We'll get alarms from the snapshot generator DLQ, but it's also a problem
+# if the main queue starts backfilling -- it means snapshots aren't being
+# created correctly.
+#
+resource "aws_cloudwatch_metric_alarm" "snapshot_scheduler_queue_not_empty" {
+  alarm_name          = "snapshot_scheduler_queue_not_empty"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  namespace           = "AWS/SQS"
+  period              = 60
+  threshold           = 2
+  statistic           = "Average"
+
+  dimensions {
+    QueueName = "${aws_sqs_queue.snapshot_generator_queue.name}"
+  }
+
+  alarm_actions = ["${local.dlq_alarm_arn}"]
+}
