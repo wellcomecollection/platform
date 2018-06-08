8e016ee756e156121e6713a124c3243526cf2e96
Alex Chan <a.chan@wellcome.ac.uk>
2018-06-08 11:37:59 +0100

    Add an ExecutionContext to the WorkIndexer fixtures

--- catalogue_pipeline/ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/fixtures/WorkIndexerFixtures.scala
+++ catalogue_pipeline/ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/fixtures/WorkIndexerFixtures.scala
@@ -5,6 +5,8 @@ import org.scalatest.Suite
 import uk.ac.wellcome.platform.ingestor.services.WorkIndexer
 import uk.ac.wellcome.test.fixtures._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
 trait WorkIndexerFixtures extends Akka { this: Suite =>
   def withWorkIndexer[R](elasticClient: HttpClient)(
     testWith: TestWith[WorkIndexer, R]): R = {
