426c3c6551a4e21a4566e7c76877b9e635a6aec0
alicefuzier <alicefuzier@gmail.com>
2018-06-15 11:32:16 +0100

    WIP output one work

--- catalogue_pipeline/merger/src/main/scala/uk/ac/wellcome/platform/merger/services/MergerWorkerService.scala
+++ catalogue_pipeline/merger/src/main/scala/uk/ac/wellcome/platform/merger/services/MergerWorkerService.scala
@@ -2,22 +2,33 @@ package uk.ac.wellcome.platform.merger.services
 
 import akka.actor.ActorSystem
 import com.google.inject.Inject
-import uk.ac.wellcome.messaging.sns.NotificationMessage
+import uk.ac.wellcome.messaging.sns.{NotificationMessage, SNSWriter}
 import uk.ac.wellcome.messaging.sqs.SQSStream
 import uk.ac.wellcome.models.matcher.MatcherResult
+import uk.ac.wellcome.models.recorder.internal.RecorderWorkEntry
+import uk.ac.wellcome.storage.ObjectStore
+import uk.ac.wellcome.storage.vhs.{EmptyMetadata, VersionedHybridStore}
 import uk.ac.wellcome.utils.JsonUtil._
+import uk.ac.wellcome.storage.dynamo._
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class MergerWorkerService @Inject()(
-  system: ActorSystem,
-  sqsStream: SQSStream[NotificationMessage]
+                                     system: ActorSystem,
+                                     sqsStream: SQSStream[NotificationMessage],
+                                     vhs: VersionedHybridStore[RecorderWorkEntry, EmptyMetadata, ObjectStore[RecorderWorkEntry]],
+                                     SNSWriter: SNSWriter
 )(implicit context: ExecutionContext) {
 
   sqsStream.foreach(this.getClass.getSimpleName, processMessage)
 
   private def processMessage(message: NotificationMessage): Future[Unit] =
-    Future.fromTry(fromJson[MatcherResult](message.Message)).map(_ => ())
+    for {
+      matcherResult <-Future.fromTry(fromJson[MatcherResult] (message.Message))
+      maybeRecorderWorkEntry <- vhs.getRecord(matcherResult.works.head.identifiers.head.identifier)
+      work = maybeRecorderWorkEntry.get.work
+      _ <- SNSWriter.writeMessage(toJson(work).get, "merged-work")
+    } yield ()
 
   def stop() = system.terminate()
 }
