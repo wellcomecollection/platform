6acafce2e45ff57cc6e4e2a2ab1b0e97c8bc97aa
Alex Chan <alex@alexwlchan.net>
2018-06-19 10:04:11 +0100

    Fix the deep learning ASG, plus some minor script improvements (#2257)
    * Retry creating the tunnel until instances become available
    
    * Prettier handling of ^C
    
    * Add a --status flag to toggle_asg.py
    
    * Include a running instance count in --status
    
    * Use the fixed module versions

--- data_science/scripts/toggle_asg.py
+++ data_science/scripts/toggle_asg.py
@@ -3,11 +3,12 @@
 """
 Start/stop all the instances in an autoscaling group.
 
-Usage: toggle_asg.py (--start | --stop) [--type=(p2 | t2)]
+Usage: toggle_asg.py (--start | --stop | --status) [--type=(p2 | t2)]
 
 Actions:
   --start                 Start the autoscaling group (set the desired count to 1).
   --stop                  Stop the autoscaling group (set the desired count to 0).
+  --status                Report the current side of autoscaling group.
   --type=(p2 | t2)        AWS Instance type (valid values: p2,t2) defaults to t2
 
 """
@@ -17,28 +18,48 @@ import sys
 import boto3
 import docopt
 
-from asg_utils import discover_asg_name, set_asg_size
+from asg_utils import discover_asg, set_asg_size
 
 if __name__ == '__main__':
     args = docopt.docopt(__doc__)
-
-    if args.get('--start'):
-        desired_size = 1
-    elif args.get('--stop'):
-        desired_size = 0
-    else:
-        print('Neither --start nor --stop flags supplied?  args=%r' % args)
-        sys.exit(1)
 
     instance_type = args['--type'] or 't2'
     tag_name = 'jupyter-%s' % instance_type
 
     asg_client = boto3.client('autoscaling')
+    asg = discover_asg(asg_client=asg_client, tag_name=tag_name)
+    asg_name = asg['AutoScalingGroupName']
 
-    asg_name = discover_asg_name(asg_client=asg_client, tag_name=tag_name)
+    if args['--start'] or args['--stop']:
+        if args['--start']:
+            desired_size = 1
+        else:
+            desired_size = 0
 
-    set_asg_size(
-        asg_client=asg_client,
-        asg_name=asg_name,
-        desired_size=desired_size
-    )
+        set_asg_size(
+            asg_client=asg_client,
+            asg_name=asg_name,
+            desired_size=desired_size
+        )
+
+    elif args['--status']:
+        actual_size = asg['DesiredCapacity']
+        instance_count = len(asg['Instances'])
+
+        if instance_count == 1:
+            instance_str = '1 instance'
+        elif instance_count == 0:
+            instance_str = 'no instances'
+        else:
+            instance_str = '%d instances' % instance_count
+
+        print(
+            'The desired size of ASG group %r is %r, with %s running' %
+            (asg_name, actual_size, instance_str)
+        )
+
+    else:
+        print(
+            'Neither --start, --stop nor --status flags supplied?  args=%r' %
+            args)
+        sys.exit(1)
