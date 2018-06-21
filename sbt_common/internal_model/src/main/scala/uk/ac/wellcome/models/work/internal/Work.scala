e5f0599b8d8443007539cca44724ef41e188f8da
Alex Chan <alex@alexwlchan.net>
2018-06-21 14:46:41 +0100

    Add some whitespace to make fields in Work easier to read

--- sbt_common/internal_model/src/main/scala/uk/ac/wellcome/models/work/internal/Work.scala
+++ sbt_common/internal_model/src/main/scala/uk/ac/wellcome/models/work/internal/Work.scala
@@ -2,32 +2,35 @@ package uk.ac.wellcome.models.work.internal
 
 /** A representation of a work in our ontology */
 trait Work {
-  val title: Option[String]
   val sourceIdentifier: SourceIdentifier
-  val version: Int
   val identifiers: List[SourceIdentifier]
+
   val workType: Option[WorkType]
+  val title: Option[String]
   val description: Option[String]
   val physicalDescription: Option[String]
   val extent: Option[String]
   val lettering: Option[String]
   val createdDate: Option[Period]
   val subjects: List[Subject[IdentityState[AbstractConcept]]]
-  val contributors: List[Contributor[IdentityState[AbstractAgent]]]
   val genres: List[Genre[IdentityState[AbstractConcept]]]
+  val contributors: List[Contributor[IdentityState[AbstractAgent]]]
   val thumbnail: Option[Location]
   val production: List[ProductionEvent[IdentityState[AbstractAgent]]]
   val language: Option[Language]
   val dimensions: Option[String]
+
+  val version: Int
   val visible: Boolean
+
   val ontologyType: String
 }
 
 case class UnidentifiedWork(
-  title: Option[String],
   sourceIdentifier: SourceIdentifier,
-  version: Int,
   identifiers: List[SourceIdentifier] = List(),
+
+  title: Option[String],
   workType: Option[WorkType] = None,
   description: Option[String] = None,
   physicalDescription: Option[String] = None,
@@ -35,22 +38,24 @@ case class UnidentifiedWork(
   lettering: Option[String] = None,
   createdDate: Option[Period] = None,
   subjects: List[Subject[MaybeDisplayable[AbstractConcept]]] = Nil,
-  contributors: List[Contributor[MaybeDisplayable[AbstractAgent]]] = Nil,
   genres: List[Genre[MaybeDisplayable[AbstractConcept]]] = Nil,
+  contributors: List[Contributor[MaybeDisplayable[AbstractAgent]]] = Nil,
   thumbnail: Option[Location] = None,
   items: List[UnidentifiedItem] = Nil,
   production: List[ProductionEvent[MaybeDisplayable[AbstractAgent]]] = Nil,
   language: Option[Language] = None,
   dimensions: Option[String] = None,
+
+  version: Int,
   visible: Boolean = true,
-  ontologyType: String = "Work")
-    extends Work
+
+  ontologyType: String = "Work") extends Work
 
 case class IdentifiedWork(
   canonicalId: String,
   title: Option[String],
   sourceIdentifier: SourceIdentifier,
-  version: Int,
+
   identifiers: List[SourceIdentifier] = List(),
   workType: Option[WorkType] = None,
   description: Option[String] = None,
@@ -59,13 +64,15 @@ case class IdentifiedWork(
   lettering: Option[String] = None,
   createdDate: Option[Period] = None,
   subjects: List[Subject[Displayable[AbstractConcept]]] = Nil,
-  contributors: List[Contributor[Displayable[AbstractAgent]]] = Nil,
   genres: List[Genre[Displayable[AbstractConcept]]] = Nil,
+  contributors: List[Contributor[Displayable[AbstractAgent]]] = Nil,
   thumbnail: Option[Location] = None,
   items: List[IdentifiedItem] = Nil,
   production: List[ProductionEvent[Displayable[AbstractAgent]]] = Nil,
   language: Option[Language] = None,
   dimensions: Option[String] = None,
+
+  version: Int,
   visible: Boolean = true,
-  ontologyType: String = "Work")
-    extends Work
+
+  ontologyType: String = "Work") extends Work
