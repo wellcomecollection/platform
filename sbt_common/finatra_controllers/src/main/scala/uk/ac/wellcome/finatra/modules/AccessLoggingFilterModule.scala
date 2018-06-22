e70a13a04853aa03a35ba196f193dbb65b9af21d
Alex Chan <a.chan@wellcome.ac.uk>
2018-06-22 09:17:54 +0100

    Add an explanatory comment to the module

--- sbt_common/finatra_controllers/src/main/scala/uk/ac/wellcome/finatra/modules/AccessLoggingFilterModule.scala
+++ sbt_common/finatra_controllers/src/main/scala/uk/ac/wellcome/finatra/modules/AccessLoggingFilterModule.scala
@@ -7,6 +7,18 @@ import com.twitter.finatra.http.filters.AccessLoggingFilter
 import com.twitter.inject.{Injector, TwitterModule}
 import uk.ac.wellcome.finatra.filters.PlatformLoggingFilter
 
+/** Provides the [[PlatformLoggingFilter]] which doesn't log ALB healthchecks.
+  *
+  * To see where this injection is used:
+  *
+  *   - Note that all of our [[com.twitter.finatra.http.HttpServer]] instances
+  *     configure [[com.twitter.finatra.http.filters.CommonFilters]].
+  *   - The second parameter to [[com.twitter.finatra.http.filters.CommonFilters]]
+  *     is an instance of [[AccessLoggingFilter]].
+  *   - Normally that filter is provided by some builtin Finatra module --
+  *     by using this module, we can override the default filter.
+  *
+  */
 object AccessLoggingFilterModule extends TwitterModule {
 
   @Singleton
