385096cb69df9364473a7bf6a1e8ce8957c2f05b
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-06-27 13:42:10 +0100

    splitting up for release

---
+++ monitoring/network.tf
@@ -0,0 +1,15 @@
+module "network" {
+  source     = "git::https://github.com/wellcometrust/terraform.git//network?ref=v11.0.0"
+  name       = "${local.namespace}"
+  cidr_block = "${local.vpc_cidr_block}"
+  az_count   = "2"
+}
+
+resource "aws_service_discovery_private_dns_namespace" "namespace" {
+  name = "${local.namespace}"
+  vpc  = "${module.network.vpc_id}"
+}
+
+resource "aws_ecs_cluster" "monitoring" {
+  name = "monitoring_cluster"
+}
