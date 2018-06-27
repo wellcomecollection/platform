9d3d00cc1f7c0f76bc2af3ce7dfd53feb1fad817
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-06-27 17:38:35 +0100

    merge with master

--- data_science/terraform/ecr.tf
+++
@@ -1,4 +0,0 @@
-module "ecr_repository_palette_api" {
-  source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
-  name   = "palette_api"
-}
--- data_science/terraform/ecs.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_ecs_cluster" "data_science_experiments" {
-  name = "data_science_experiments_cluster"
-}
