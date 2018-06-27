0b24c74aa53ca3f722959be725c631e45c384b51
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-06-27 17:43:11 +0100

    fmt

--- data_science/palette_api/README.md
+++ data_science/palette_api/README.md
@@ -1,2 +1,3 @@
 # Palette app
+
 An MVP flask app, hosted on AWS, to demonstrate how palette search might work in practice
--- data_science/terraform/terraform.tf
+++ data_science/terraform/terraform.tf
@@ -8,13 +8,3 @@ terraform {
     region         = "eu-west-1"
   }
 }
-
-data "terraform_remote_state" "shared_infra" {
-  backend = "s3"
-
-  config {
-    bucket = "wellcomecollection-platform-infra"
-    key    = "terraform/shared_infra.tfstate"
-    region = "eu-west-1"
-  }
-}
