6897d36c65cc7f9d1650b55a0aa4a820d5289410
Alex Chan <alex@alexwlchan.net>
2018-06-28 12:03:41 +0100

    reindex_worker no longer sends a post-job notification to SNS

--- reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex_worker/services/ReindexWorkerService.scala
+++ reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex_worker/services/ReindexWorkerService.scala
@@ -2,7 +2,7 @@ package uk.ac.wellcome.platform.reindex_worker.services
 
 import akka.actor.ActorSystem
 import com.google.inject.Inject
-import uk.ac.wellcome.messaging.sns.{NotificationMessage, SNSWriter}
+import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.sqs.SQSStream
 import uk.ac.wellcome.platform.reindex_worker.GlobalExecutionContext.context
 import uk.ac.wellcome.platform.reindex_worker.models.{
@@ -15,7 +15,6 @@ import scala.concurrent.Future
 
 class ReindexWorkerService @Inject()(
   targetService: ReindexService,
-  snsWriter: SNSWriter,
   system: ActorSystem,
   sqsStream: SQSStream[NotificationMessage]
 ) {
@@ -25,11 +24,7 @@ class ReindexWorkerService @Inject()(
     for {
       reindexJob <- Future.fromTry(fromJson[ReindexJob](message.Message))
       _ <- targetService.runReindex(reindexJob = reindexJob)
-      message <- Future.fromTry(toJson(CompletedReindexJob(reindexJob)))
-      _ <- snsWriter.writeMessage(
-        subject = s"source: ${this.getClass.getSimpleName}.processMessage",
-        message = message
-      )
+      _ <- Future.fromTry(toJson(CompletedReindexJob(reindexJob)))
     } yield ()
 
   def stop() = system.terminate()
