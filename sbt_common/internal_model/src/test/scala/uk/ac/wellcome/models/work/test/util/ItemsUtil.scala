88a527cad9a1134387d3d31e83ed630a3440b257
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-02 13:10:27 +0100

    Add a separate trait for ItemsUtil

---
+++ sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/work/test/util/ItemsUtil.scala
@@ -0,0 +1,28 @@
+package uk.ac.wellcome.models.work.test.util
+
+import uk.ac.wellcome.models.work.internal._
+
+import scala.util.Random
+
+trait ItemsUtil {
+  private def sourceIdentifier = {
+    SourceIdentifier(
+      identifierType = IdentifierType("miro-image-number"),
+      value = (Random.alphanumeric take 10 mkString) toLowerCase,
+      ontologyType = "Item"
+    )
+  }
+
+  private def location = DigitalLocation(
+    locationType = LocationType("iiif-image"),
+    url = "https://iiif.wellcomecollection.org/image/M0000001.jpg/info.json",
+    license = License_CCBY
+  )
+
+  def createItem: Identified[Item] =
+    Identified(
+      canonicalId = (Random.alphanumeric take 10 mkString) toLowerCase,
+      sourceIdentifier = sourceIdentifier,
+      agent = Item(locations = List(location))
+    )
+}
