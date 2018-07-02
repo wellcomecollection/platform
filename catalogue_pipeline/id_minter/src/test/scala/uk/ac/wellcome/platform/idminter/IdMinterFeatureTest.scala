12df3cc9862089cf0bd5c5a5aeb320fa621ef7b0
alicefuzier <alicefuzier@gmail.com>
2018-07-02 11:00:25 +0100

    Check that the id minter can mint identifiers for a redirected work

--- catalogue_pipeline/id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/IdMinterFeatureTest.scala
+++ catalogue_pipeline/id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/IdMinterFeatureTest.scala
@@ -131,6 +131,56 @@ class IdMinterFeatureTest
     }
   }
 
+  it("mints an identifier for a UnidentifiedRedirectedWork") {
+    withLocalSqsQueue { queue =>
+      withLocalSnsTopic { topic =>
+        withLocalS3Bucket { bucket =>
+          withIdentifiersDatabase { identifiersTableConfig =>
+            val flags =
+              identifiersLocalDbFlags(identifiersTableConfig) ++
+                messagingLocalFlags(bucket, topic, queue)
+
+            withServer(flags) { _ =>
+              eventuallyTableExists(identifiersTableConfig)
+
+              val id = "id"
+              val identifier =
+                SourceIdentifier(
+                  identifierType = IdentifierType("sierra-system-number"),
+                  "Work",
+                  id)
+
+              val work = UnidentifiedRedirectedWork(
+                sourceIdentifier = identifier,
+                version = 1,
+                redirect = IdentifiableRedirect(sourceIdentifier = SourceIdentifier(IdentifierType("sierra-system-number"), "Work", "b1234567"))
+              )
+
+              val messageBody = put[UnidentifiedRedirectedWork](
+                obj = work,
+                location = ObjectLocation(
+                  namespace = bucket.name,
+                  key = s"redirected-work.json"
+                )
+              )
+              sqsClient.sendMessage(queue.url, messageBody)
+
+              eventually {
+                val messages = listMessagesReceivedFromSNS(topic)
+                messages.length shouldBe >=(1)
+
+                val work = get[IdentifiedRedirectedWork](messages.head)
+                work.sourceIdentifier shouldBe identifier
+                work.canonicalId shouldNot be(empty)
+                work.redirect.canonicalId shouldNot be(empty)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
   it("continues if something fails processing a message") {
     withLocalSqsQueue { queue =>
       withLocalSnsTopic { topic =>
