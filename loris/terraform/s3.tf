05419d9e099ecd0dffda7465c725f029687300af
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-07-04 13:47:50 +0100

    Remove old loris

--- loris/terraform/s3.tf
+++ loris/terraform/s3.tf
@@ -11,3 +11,20 @@ resource "aws_s3_bucket" "wellcomecollection-miro-images-public" {
     prevent_destroy = true
   }
 }
+
+data "aws_iam_policy_document" "wellcomecollection-miro-images-public" {
+  statement {
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.wellcomecollection-miro-images-public.id}/*",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+}
