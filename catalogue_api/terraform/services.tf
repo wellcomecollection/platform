29d05691460b327b59ad4916c82f800fd89cc11e
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-07-05 14:01:58 +0100

    add new logic for api switching

--- catalogue_api/terraform/services.tf
+++ catalogue_api/terraform/services.tf
@@ -60,16 +60,28 @@ module "api_remus_v1" {
 
 locals {
   romulus_api_is_pinned = "${var.pinned_romulus_api == "" ? "false" : "true" }"
-  remus_api_is_pinned   = "${var.pinned_remus_api == "" ? "false" : "true" }"
-
   romulus_nginx_is_pinned = "${var.pinned_romulus_api_nginx == "" ? "false" : "true" }"
+
+  remus_api_is_pinned   = "${var.pinned_remus_api == "" ? "false" : "true" }"
   remus_nginx_is_pinned   = "${var.pinned_remus_api_nginx == "" ? "false" : "true" }"
+  
+  romulus_api_release_id   = "${local.romulus_api_is_pinned == "true" ? var.pinned_romulus_api : var.release_ids["api"]}"
+  romulus_nginx_release_id = "${local.romulus_nginx_is_pinned == "true" ? var.pinned_romulus_api_nginx : var.release_ids["nginx_api"]}"
 
-  romulus_api_release_id   = "${var.production_api == "romulus" ? var.pinned_romulus_api : var.pinned_remus_api}"
-  romulus_nginx_release_id = "${var.production_api == "romulus" ? var.pinned_romulus_api_nginx : var.pinned_remus_api_nginx}"
+  remus_api_release_id       = "${local.remus_api_is_pinned == "true" ? var.pinned_romulus_api : var.release_ids["api"]}"
+  remus_nginx_release_id = "${local.remus_nginx_is_pinned == "true" ? var.pinned_romulus_api_nginx : var.release_ids["nginx_api"]}"
 
-  remus_api_release_id       = "${var.production_api == "romulus" ? var.pinned_romulus_api : var.pinned_remus_api}"
-  remus_app_nginx_release_id = "${var.production_api == "romulus" ? var.pinned_romulus_api_nginx : var.pinned_remus_api_nginx}"
+  romulus_app_uri   = "${module.ecr_repository_api.repository_url}:${local.romulus_api_release_id}"
+  romulus_nginx_uri = "${module.ecr_repository_api.repository_url}:${local.romulus_nginx_release_id}"
+
+  remus_app_uri   = "${module.ecr_repository_api.repository_url}:${local.remus_api_release_id}"
+  remus_nginx_uri = "${module.ecr_repository_api.repository_url}:${local.remus_nginx_release_id}"
+  
+  romulus_is_prod = "${var.production_api == "romulus" ? "true" : "false"}"
+  remus_is_prod   = "${var.production_api == "remus" ? "true" : "false"}"
+  
+  remus_hostname   = "${local.remus_is_prod == "true" ? var.api_prod_host : var.api_stage_host}"
+  romulus_hostname = "${local.romulus_is_prod == "true" ? var.api_prod_host : var.api_stage_host}"
 }
 
 resource "aws_service_discovery_private_dns_namespace" "namespace" {
@@ -110,6 +122,27 @@ module "api_romulus_delta" {
   alb_id           = "${module.load_balancer.id}"
   alb_listener_arn = "${module.load_balancer.https_listener_arn}"
 
-  sidecar_container_image = ""
-  app_container_image     = ""
+  sidecar_container_image = "${local.romulus_nginx_uri}"
+  app_container_image     = "${local.romulus_app_uri}"
+  
+  host_name = "${local.romulus_hostname}"
+}
+
+module "api_remus_delta" {
+  source = "service"
+
+  name            = "${local.namespace}-remus"
+  cluster_id      = "${aws_ecs_cluster.cluster.id}"
+  aws_region      = "${var.aws_region}"
+  vpc_id          = "${local.vpc_id}"
+  namespace_id    = "${aws_service_discovery_private_dns_namespace.namespace.id}"
+  private_subnets = "${local.private_subnets}"
+
+  alb_id           = "${module.load_balancer.id}"
+  alb_listener_arn = "${module.load_balancer.https_listener_arn}"
+
+  sidecar_container_image = "${local.remus_nginx_uri}"
+  app_container_image     = "${local.remus_app_uri}"
+
+  host_name = "${local.remus_hostname}"
 }
