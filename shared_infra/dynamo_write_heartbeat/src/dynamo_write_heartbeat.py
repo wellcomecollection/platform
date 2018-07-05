c03b40a8969e64e71604e9314d29e21c8b9fb7b3
mklander <mklanderyou@gmail.com>
2018-07-05 17:42:11 +0100

    support putItem into dynamoDB tables so secondary index capacity can be exercised.

--- shared_infra/dynamo_write_heartbeat/src/dynamo_write_heartbeat.py
+++ shared_infra/dynamo_write_heartbeat/src/dynamo_write_heartbeat.py
@@ -1,26 +1,63 @@
 # -*- encoding: utf-8 -*-
 """
-Run a no-op write on DynamoDB (delete a non existent item) to be run in a scheduled lambda to
-make DynamoDB scale down when there would otherwise be zero throughput and it otherwise wouldn't.
+Update a DynamoDB item, run in a scheduled lambda to make DynamoDB scale down when there would
+otherwise be zero throughput.
 
-TABLE_NAMES: comma separated list of dynamoDb tables to call
+HEARTBEAT_CONFIG: configuration
+[
+        {
+            '__heartbeat__': True,
+            'table_name': 'Table1',
+            'key': {'id': {'S': 'heartbeat-dummy-record-id-1'}},
+            'update_expression': "SET indexedId = :v",
+            'expression_attribute_values': {':v': {'S': 'value-1'}}
+        }
+]
 """
 
 import boto3
 import os
+import json
 
 from wellcome_aws_utils.lambda_utils import log_on_error
+
+CONFIG_ENV_KEY = 'HEARTBEAT_CONFIG'
+
+
+class HeartbeatConfig:
+    def __init__(self, table_name, key, update_expression, expression_attribute_values):
+        self.table_name = table_name
+        self.key = key
+        self.update_expression = update_expression
+        self.expression_attribute_values = expression_attribute_values
+
+def decode_config(dct):
+    if '__heartbeat__' in dct:
+        config_keys = ['__heartbeat__', 'table_name', 'key', 'update_expression', 'expression_attribute_values']
+        if all(elem in dct.keys() for elem in config_keys):
+            return HeartbeatConfig(table_name=dct['table_name'],
+                                   key=dct['key'],
+                                   update_expression=dct['update_expression'],
+                                   expression_attribute_values=dct['expression_attribute_values'])
+        else:
+            raise RuntimeError("Incomplete HeartbeatConfig {}".format(dct))
+    else:
+        return dct
 
 
 @log_on_error
 def main(event, context, dynamodb_client=None):
     try:
-        table_names = [t.strip() for t in os.environ['TABLE_NAMES'].split(',')]
+        config = json.loads(os.environ[CONFIG_ENV_KEY], object_hook=decode_config)
     except KeyError:
-        raise RuntimeError('TABLE_NAMES not found')
+        raise RuntimeError("{} not set".format(CONFIG_ENV_KEY))
+    except json.JSONDecodeError:
+        raise RuntimeError("config '{}' could not be parsed".format(os.environ[CONFIG_ENV_KEY]))
 
     dynamodb_client = dynamodb_client or boto3.client('dynamodb')
-    for table_name in table_names:
-        response = dynamodb_client.delete_item(TableName=table_name.strip(),
-                                               Key={'id': {'S': 'dummy-id-not-there'}})
-        print("Heartbeat, delete_item on dynamoDb table:{}, returned-status:{}".format(table_name, response['ResponseMetadata']['HTTPStatusCode']))
+    for heartbeat in config:
+        response = dynamodb_client.update_item(TableName=heartbeat.table_name,
+                                               Key=heartbeat.key,
+                                               UpdateExpression=heartbeat.update_expression,
+                                               ExpressionAttributeValues=heartbeat.expression_attribute_values)
+        print("Heartbeat, update_item on :{}, returned-status:{}".format(heartbeat.table_name, response['ResponseMetadata']['HTTPStatusCode']))
--- shared_infra/dynamo_write_heartbeat/src/test_dynamo_write_heartbeat.py
+++ shared_infra/dynamo_write_heartbeat/src/test_dynamo_write_heartbeat.py
@@ -3,54 +3,139 @@
 import pytest
 import os
 import dynamo_write_heartbeat
+import json
 
+HEARTBEAT_CONFIG_KEY = "HEARTBEAT_CONFIG"
 
-TABLE_NAMES = ['Test1', 'Test2']
-TABLE_NAME_STR = ','.join(TABLE_NAMES)
+TABLES = [{'name': 'Table1', 'index': 'Index1'},
+          {'name': 'Table2', 'index': 'Index2'},
+          {'name': 'Table3', 'index': 'Index3'}]
 
 
 @pytest.yield_fixture(autouse=True)
 def run_around_tests(dynamodb_client):
-    table_names = TABLE_NAMES
+    tables = TABLES
 
-    for table_name in table_names:
-        create_table(dynamodb_client, table_name)
+    for table in tables:
+        create_table(dynamodb_client, table['name'], table['index'])
     yield
-    for table_name in table_names:
-        dynamodb_client.delete_table(TableName=table_name)
-
-
-def test_executing_lambda_no_params(dynamodb_client):
-    with pytest.raises(RuntimeError) as e:
-        dynamo_write_heartbeat.main(event=None, context=None, dynamodb_client=dynamodb_client)
-    e.match('TABLE_NAMES not found')
+    for table in tables:
+        dynamodb_client.delete_table(TableName=table['name'])
 
 
 def test_executing_lambda(dynamodb_client):
-    os.environ.update({'TABLE_NAMES': TABLE_NAME_STR})
+    heartbeats_json_str = json.dumps([
+        {
+            '__heartbeat__': True,
+            'table_name': 'Table1',
+            'key': {'id': {'S': 'heartbeat-dummy-record-id-1'}},
+            'update_expression': "SET indexedId = :v",
+            'expression_attribute_values': {':v': {'S': 'value-1'}}
+        },
+        {
+            '__heartbeat__': True,
+            'table_name': 'Table3',
+            'key': {'id': {'S': 'heartbeat-dummy-record-id-3'}},
+            'update_expression': "SET indexedId = :v",
+            'expression_attribute_values': {':v': {'S': 'value-3'}}
+        }
+    ])
+    os.environ.update({HEARTBEAT_CONFIG_KEY: heartbeats_json_str})
     dynamo_write_heartbeat.main(event=None, context=None, dynamodb_client=dynamodb_client)
 
+    assert_heartbeat_record_in_table(dynamodb_client,
+                                     table_name='Table1',
+                                     key={'id': {'S': 'heartbeat-dummy-record-id-1'}},
+                                     value={'indexedId': {'S': 'value-1'}})
 
-def create_table(dynamodb, table_name):
+    table = dynamodb_client.describe_table(TableName='Table2')
+    assert table['Table']['ItemCount'] == 0
+
+    assert_heartbeat_record_in_table(dynamodb_client,
+                                     table_name='Table3',
+                                     key={'id': {'S': 'heartbeat-dummy-record-id-3'}},
+                                     value={'indexedId': {'S': 'value-3'}})
+
+
+
+def test_executing_lambda_config_not_provided(dynamodb_client):
+    del os.environ[HEARTBEAT_CONFIG_KEY]
+    with pytest.raises(RuntimeError) as e:
+        dynamo_write_heartbeat.main(event=None, context=None, dynamodb_client=dynamodb_client)
+    e.match("HEARTBEAT_CONFIG not set")
+
+
+def test_executing_lambda_config_not_json(dynamodb_client):
+    os.environ.update({HEARTBEAT_CONFIG_KEY: 'not-json'})
+    with pytest.raises(RuntimeError) as e:
+        dynamo_write_heartbeat.main(event=None, context=None, dynamodb_client=dynamodb_client)
+    e.match("config 'not-json' could not be parsed")
+
+
+def test_executing_lambda_config_incomplete(dynamodb_client):
+    heartbeats_json_str = json.dumps([
+        {
+            '__heartbeat__': True,
+            'table_name': 'Test1'
+        }
+    ])
+    os.environ.update({HEARTBEAT_CONFIG_KEY: heartbeats_json_str})
+    with pytest.raises(RuntimeError) as e:
+        dynamo_write_heartbeat.main(event=None, context=None, dynamodb_client=dynamodb_client)
+    e.match("Incomplete HeartbeatConfig {'__heartbeat__': True, 'table_name': 'Test1'}")
+
+
+def create_table(dynamodb_client, table_name, index_name):
     try:
-        dynamodb.create_table(TableName=table_name,
-                              KeySchema=[
-                                  {
-                                      'AttributeName': 'id',
-                                      'KeyType': 'HASH'
-                                  }
-                              ],
-                              AttributeDefinitions=[
-                                  {
-                                      'AttributeName': 'id',
-                                      'AttributeType': 'S'
-                                  }
-
-                              ],
-                              ProvisionedThroughput={
-                                  'ReadCapacityUnits': 1,
-                                  'WriteCapacityUnits': 1
-                              })
-        dynamodb.get_waiter('table_exists').wait(TableName=table_name)
-    except dynamodb.exceptions.ResourceInUseException:
+        key_schema = [
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            }
+        ]
+        attribute_definiutions = [
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'indexedId',
+                'AttributeType': 'S'
+            }
+        ]
+        provisioned_throughput = {
+            'ReadCapacityUnits': 1,
+            'WriteCapacityUnits': 1
+        }
+        gsi = [{
+            'IndexName': index_name,
+            'KeySchema': [
+                {
+                    'AttributeName': 'indexedId',
+                    'KeyType': 'HASH'
+                }
+            ],
+            'Projection': {
+                'ProjectionType': 'ALL'
+            },
+            'ProvisionedThroughput': {
+                'ReadCapacityUnits': 1,
+                'WriteCapacityUnits': 1
+            }
+        }]
+        dynamodb_client.create_table(TableName=table_name,
+                                     KeySchema=key_schema,
+                                     AttributeDefinitions=attribute_definiutions,
+                                     GlobalSecondaryIndexes=gsi,
+                                     ProvisionedThroughput=provisioned_throughput)
+        dynamodb_client.get_waiter('table_exists').wait(TableName=table_name)
+    except dynamodb_client.exceptions.ResourceInUseException:
         pass
+
+
+def assert_heartbeat_record_in_table(dynamodb_client, table_name, key, value):
+    table = dynamodb_client.describe_table(TableName=table_name)
+    assert table['Table']['ItemCount'] == 1
+    saved = dynamodb_client.get_item(TableName=table_name,
+                                     Key=key)
+    assert saved['Item'] == {**key, **value}
