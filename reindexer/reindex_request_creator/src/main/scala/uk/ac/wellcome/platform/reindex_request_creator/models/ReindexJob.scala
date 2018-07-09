fe56942d81033232066564066f6204cc10d9fb8a
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-09 10:06:58 +0100

    Rename the reindex_worker to the reindex_request_creator

---
+++ reindexer/reindex_request_creator/src/main/scala/uk/ac/wellcome/platform/reindex_request_creator/models/ReindexJob.scala
@@ -0,0 +1,11 @@
+package uk.ac.wellcome.platform.reindex_request_creator.models
+
+/** A request to identify all the shards in the table that need reindexing.
+  *
+  * @param shardId Name of the shard to reindex.
+  * @param desiredVersion Version to reindex everything in this shard to.
+  */
+case class ReindexJob(
+  shardId: String,
+  desiredVersion: Int
+)
