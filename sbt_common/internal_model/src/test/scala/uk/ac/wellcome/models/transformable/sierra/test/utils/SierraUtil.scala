3a5523c9350e1621640d77814397e7264e3515e4
Alex Chan <alex@alexwlchan.net>
2018-07-11 23:02:34 +0100

    Start creating SierraItemRecord instances with defaults

--- sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/sierra/test/utils/SierraUtil.scala
+++ sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/sierra/test/utils/SierraUtil.scala
@@ -2,13 +2,13 @@ package uk.ac.wellcome.models.transformable.sierra.test.utils
 
 import java.time.Instant
 
-import uk.ac.wellcome.models.transformable.sierra.SierraBibRecord
+import uk.ac.wellcome.models.transformable.sierra.{SierraBibRecord, SierraItemRecord}
 
 import scala.util.Random
 
 trait SierraUtil {
 
-  private def createSierraBibRecordStringWith(id: String): String =
+  private def createSierraRecordStringWith(id: String): String =
     s"""
        |{
        |  "id": "$id"
@@ -33,9 +33,36 @@ trait SierraUtil {
   ): SierraBibRecord =
     SierraBibRecord(
       id = id,
-      data = if (data == "") createSierraBibRecordStringWith(id = id) else data,
+      data = if (data == "") createSierraRecordStringWith(id = id) else data,
       modifiedDate = modifiedDate
     )
 
   def createSierraBibRecord: SierraBibRecord = createSierraBibRecordWith()
+
+  def createSierraItemRecordWith(
+    id: String = Random.alphanumeric take 7 mkString,
+    data: String = "",
+    modifiedDate: String,
+    bibIds: List[String] = List()
+  ): SierraItemRecord =
+    createSierraItemRecordWith(
+      id = id,
+      data = data,
+      modifiedDate = Instant.parse(modifiedDate),
+      bibIds = bibIds
+    )
+
+  def createSierraItemRecordWith(
+    id: String = Random.alphanumeric take 7 mkString,
+    data: String = "",
+    modifiedDate: Instant = Instant.now,
+    bibIds: List[String] = List()
+  ): SierraItemRecord =
+    SierraItemRecord(
+      id = id,
+      data = if (data == "") createSierraRecordStringWith(id = id) else data,
+      modifiedDate = modifiedDate,
+      bibIds = bibIds,
+      unlinkedBibIds = List()
+    )
 }
