87adea3cb671129ddf3cb7ea54c1cd54b84ad645
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-11 16:42:00 +0100

    SierraTransformableTransformer catches ShouldNotTransform exceptions

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/transformers/SierraTransformableTransformer.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/transformers/SierraTransformableTransformer.scala
@@ -1,12 +1,13 @@
 package uk.ac.wellcome.platform.transformer.transformers
 
-import uk.ac.wellcome.models.transformable.SierraTransformable
+import uk.ac.wellcome.models.transformable.{SierraTransformable, Transformable}
 import uk.ac.wellcome.models.work.internal._
 import uk.ac.wellcome.platform.transformer.source.SierraBibData
+import uk.ac.wellcome.platform.transformer.transformers.miro.ShouldNotTransformException
 import uk.ac.wellcome.platform.transformer.transformers.sierra._
 import uk.ac.wellcome.utils.JsonUtil._
 
-import scala.util.Success
+import scala.util.Try
 
 class SierraTransformableTransformer
     extends TransformableTransformer[SierraTransformable]
@@ -27,48 +28,60 @@ class SierraTransformableTransformer
     with SierraGenres
     with SierraMergeCandidates {
 
-  override def transformForType = {
+  override def transformForType: PartialFunction[(Transformable, Int), Try[TransformedBaseWork]] = {
     case (sierraTransformable: SierraTransformable, version: Int) =>
       sierraTransformable.maybeBibData
         .map { bibData =>
           debug(s"Attempting to transform ${bibData.id}")
 
+          val sourceIdentifier = SourceIdentifier(
+            identifierType = IdentifierType("sierra-system-number"),
+            ontologyType = "Work",
+            value = addCheckDigit(
+              bibData.id,
+              recordType = SierraRecordTypes.bibs
+            )
+          )
+
           fromJson[SierraBibData](bibData.data).map { sierraBibData =>
-            val identifier = SourceIdentifier(
-              identifierType = IdentifierType("sierra-system-number"),
-              ontologyType = "Work",
-              value = addCheckDigit(
-                sierraBibData.id,
-                recordType = SierraRecordTypes.bibs
-              ))
             if (!(sierraBibData.deleted || sierraBibData.suppressed)) {
-              Some(
-                UnidentifiedWork(
-                  sourceIdentifier = identifier,
-                  otherIdentifiers = getOtherIdentifiers(sierraBibData),
-                  mergeCandidates = getMergeCandidates(sierraBibData),
-                  title = getTitle(sierraBibData),
-                  workType = getWorkType(sierraBibData),
-                  description = getDescription(sierraBibData),
-                  physicalDescription = getPhysicalDescription(sierraBibData),
-                  extent = getExtent(sierraBibData),
-                  lettering = getLettering(sierraBibData),
-                  createdDate = None,
-                  subjects = getSubjects(sierraBibData),
-                  genres = getGenres(sierraBibData),
-                  contributors = getContributors(sierraBibData),
-                  thumbnail = None,
-                  production = getProduction(sierraBibData),
-                  language = getLanguage(sierraBibData),
-                  dimensions = getDimensions(sierraBibData),
-                  items = getItems(sierraTransformable),
-                  version = version
-                ))
+              UnidentifiedWork(
+                sourceIdentifier = sourceIdentifier,
+                otherIdentifiers = getOtherIdentifiers(sierraBibData),
+                mergeCandidates = getMergeCandidates(sierraBibData),
+                title = getTitle(sierraBibData),
+                workType = getWorkType(sierraBibData),
+                description = getDescription(sierraBibData),
+                physicalDescription = getPhysicalDescription(sierraBibData),
+                extent = getExtent(sierraBibData),
+                lettering = getLettering(sierraBibData),
+                createdDate = None,
+                subjects = getSubjects(sierraBibData),
+                genres = getGenres(sierraBibData),
+                contributors = getContributors(sierraBibData),
+                thumbnail = None,
+                production = getProduction(sierraBibData),
+                language = getLanguage(sierraBibData),
+                dimensions = getDimensions(sierraBibData),
+                items = getItems(sierraTransformable),
+                version = version
+              )
             } else {
-              Some(UnidentifiedInvisibleWork(identifier, version))
+              throw new ShouldNotTransformException(
+                s"Sierra record ${bibData.id} is either deleted or suppressed!"
+              )
             }
+          }.recover {
+            case e: ShouldNotTransformException =>
+              warn(s"Should not transform: ${e.getMessage}")
+              UnidentifiedInvisibleWork(
+                sourceIdentifier = sourceIdentifier,
+                version = version
+              )
           }
         }
+
+
         // A merged record can have both bibs and items.  If we only have
         // the item data so far, we don't have enough to build a Work, so we
         // return None.
