f969b71f6b8b83993973009d2e95e0c1b3bc42f2
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-11 16:49:25 +0100

    Fix the core of the tests that expects an Option

--- catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/platform/transformer/transformers/TransformableTestBase.scala
+++ catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/platform/transformer/transformers/TransformableTestBase.scala
@@ -4,15 +4,17 @@ import org.scalatest.Matchers
 import uk.ac.wellcome.models.transformable.Transformable
 import uk.ac.wellcome.models.work.internal.TransformedBaseWork
 
+import scala.util.Try
+
 trait TransformableTestBase[T <: Transformable] extends Matchers {
 
   val transformer: TransformableTransformer[T]
 
   def transformToWork(transformable: T): TransformedBaseWork = {
-    val triedMaybeWork = transformer.transform(transformable, version = 1)
-    if (triedMaybeWork.isFailure) triedMaybeWork.failed.get.printStackTrace()
-    triedMaybeWork.isSuccess shouldBe true
-    triedMaybeWork.get.get
+    val triedWork: Try[TransformedBaseWork] = transformer.transform(transformable, version = 1)
+    if (triedWork.isFailure) triedWork.failed.get.printStackTrace()
+    triedWork.isSuccess shouldBe true
+    triedWork.get
   }
 
   def assertTransformToWorkFails(transformable: T): Unit = {
