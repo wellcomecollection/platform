3e8924080f7baead1fb4b5640933ceded8636975
mklander <mklanderyou@gmail.com>
2018-07-12 12:37:58 +0100

    use sendNotificationToSQS helper

--- reindexer/reindex_request_processor/src/test/scala/uk/ac/wellcome/platform/reindex/processor/ReindexProcessorFeatureTest.scala
+++ reindexer/reindex_request_processor/src/test/scala/uk/ac/wellcome/platform/reindex/processor/ReindexProcessorFeatureTest.scala
@@ -3,7 +3,6 @@ package uk.ac.wellcome.platform.reindex.processor
 import com.gu.scanamo.Scanamo
 import com.gu.scanamo.syntax._
 import org.scalatest.FunSpec
-import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.test.fixtures.SQS
 import uk.ac.wellcome.models.reindexer.{ReindexRequest, ReindexableRecord}
 import uk.ac.wellcome.storage.test.fixtures.LocalDynamoDbVersioned
@@ -23,19 +22,13 @@ class ReindexProcessorFeatureTest
         val flags = sqsLocalFlags(queue) ++ dynamoDbLocalEndpointFlags(table)
         withServer(flags) { _ =>
           val id = "sierra/1234567"
-          val record =
-            ReindexableRecord(id = id, version = 1, reindexVersion = 10)
-          Scanamo.put(dynamoDbClient)(table.name)(record)
+          val record = ReindexableRecord(id = id, version = 1, reindexVersion = 10)
+
+          givenTableHasItem(record, table)
 
           val reindexRequest = ReindexRequest(id = id, desiredVersion = 11)
-          val message = toJson(
-            NotificationMessage(
-              "snsID",
-              "snsTopic",
-              "snsSubject",
-              toJson(reindexRequest).get)).get
 
-          sqsClient.sendMessage(queue.url, message)
+          sendNotificationToSQS(queue, reindexRequest)
 
           eventually {
             assertQueueEmpty(queue)
