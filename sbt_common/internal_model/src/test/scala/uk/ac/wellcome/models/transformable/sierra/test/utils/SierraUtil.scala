2440d6ed405ae676b0e2829019531206843495f2
Alex Chan <alex@alexwlchan.net>
2018-07-12 07:35:40 +0100

    Ensure we create IDs that look like real Sierra IDs

--- sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/sierra/test/utils/SierraUtil.scala
+++ sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/sierra/test/utils/SierraUtil.scala
@@ -7,6 +7,7 @@ import uk.ac.wellcome.models.transformable.sierra.{
   SierraItemRecord
 }
 
+import scala.collection.immutable.Stream
 import scala.util.Random
 
 trait SierraUtil {
@@ -17,6 +18,27 @@ trait SierraUtil {
   val olderDate: Instant = Instant.parse("1999-09-09T09:09:09Z")
   val newerDate: Instant = Instant.parse("2001-01-01T01:01:01Z")
 
+  /** Returns a random digit as a string.  This is copied from the
+    * definition of Random.alphanumeric.
+    *
+    * @return
+    */
+  private def randomNumeric: Stream[Char] = {
+    def nextAlphaNum: Char = {
+      val chars = "0123456789"
+      chars charAt Random.nextInt(chars.length)
+    }
+
+    Stream continually nextAlphaNum
+  }
+
+  /** Creates a Sierra ID, which must be a 7 digit number.  (Some of our
+    * code explicitly expects this format, and errors if it gets a
+    * non-numeric ID.)
+    */
+  private def createSierraId: String =
+    randomNumeric take 7 mkString
+
   private def createSierraRecordStringWith(id: String): String =
     s"""
        |{
@@ -25,7 +47,7 @@ trait SierraUtil {
     """.stripMargin
 
   def createSierraBibRecordWith(
-    id: String = Random.alphanumeric take 7 mkString,
+    id: String = createSierraId,
     data: String = "",
     modifiedDate: Instant = Instant.now
   ): SierraBibRecord =
@@ -38,10 +60,10 @@ trait SierraUtil {
   def createSierraBibRecord: SierraBibRecord = createSierraBibRecordWith()
 
   def createSierraItemRecordWith(
-    id: String = Random.alphanumeric take 7 mkString,
+    id: String = createSierraId,
     data: String = "",
     modifiedDate: Instant = Instant.now,
-    bibIds: List[String] = List(Random.alphanumeric take 7 mkString),
+    bibIds: List[String] = List(createSierraId),
     unlinkedBibIds: List[String] = List(),
     version: Int = 1
   ): SierraItemRecord =
