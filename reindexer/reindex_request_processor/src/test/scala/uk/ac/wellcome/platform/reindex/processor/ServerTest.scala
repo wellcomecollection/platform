13a7ae30ae4f22180677e38a28c9da7f003d49ed
mklander <mklanderyou@gmail.com>
2018-07-12 14:50:06 +0100

    server flags

--- reindexer/reindex_request_processor/src/test/scala/uk/ac/wellcome/platform/reindex/processor/ServerTest.scala
+++ reindexer/reindex_request_processor/src/test/scala/uk/ac/wellcome/platform/reindex/processor/ServerTest.scala
@@ -3,17 +3,20 @@ package uk.ac.wellcome.platform.reindex.processor
 import com.twitter.finagle.http.Status._
 import org.scalatest.FunSpec
 import uk.ac.wellcome.messaging.test.fixtures.SQS
+import uk.ac.wellcome.storage.test.fixtures.LocalDynamoDbVersioned
 
-class ServerTest extends FunSpec with fixtures.Server with SQS {
+class ServerTest extends FunSpec with fixtures.Server with SQS with LocalDynamoDbVersioned {
 
   it("shows the healthcheck message") {
     withLocalSqsQueue { queue =>
-      val flags = sqsLocalFlags(queue)
-      withServer(flags) { server =>
-        server.httpGet(
-          path = "/management/healthcheck",
-          andExpect = Ok,
-          withJsonBody = """{"message": "ok"}""")
+      withLocalDynamoDbTable { table =>
+        val flags = sqsLocalFlags(queue) ++ dynamoDbLocalEndpointFlags(table)
+        withServer(flags) { server =>
+          server.httpGet(
+            path = "/management/healthcheck",
+            andExpect = Ok,
+            withJsonBody = """{"message": "ok"}""")
+        }
       }
     }
   }
