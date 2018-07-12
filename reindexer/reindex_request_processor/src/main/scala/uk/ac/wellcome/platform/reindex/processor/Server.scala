8ea8cfffdf4ee97de932ddf8470c15ac5d2018f8
mklander <mklanderyou@gmail.com>
2018-07-12 18:13:37 +0100

    add sqs config

--- reindexer/reindex_request_processor/src/main/scala/uk/ac/wellcome/platform/reindex/processor/Server.scala
+++ reindexer/reindex_request_processor/src/main/scala/uk/ac/wellcome/platform/reindex/processor/Server.scala
@@ -2,15 +2,11 @@ package uk.ac.wellcome.platform.reindex.processor
 
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.HttpServer
-import com.twitter.finatra.http.filters.{
-  CommonFilters,
-  LoggingMDCFilter,
-  TraceIdMDCFilter
-}
+import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
 import com.twitter.finatra.http.routing.HttpRouter
 import uk.ac.wellcome.finatra.akka.AkkaModule
 import uk.ac.wellcome.finatra.controllers.ManagementController
-import uk.ac.wellcome.finatra.messaging.SQSClientModule
+import uk.ac.wellcome.finatra.messaging.{SQSClientModule, SQSConfigModule}
 import uk.ac.wellcome.finatra.monitoring.MetricsSenderModule
 import uk.ac.wellcome.finatra.storage.{DynamoClientModule, DynamoConfigModule}
 import uk.ac.wellcome.platform.reindex.processor.modules.ReindexerWorkerModule
@@ -24,6 +20,7 @@ class Server extends HttpServer {
   override val modules = Seq(
     AkkaModule,
     MetricsSenderModule,
+    SQSConfigModule,
     SQSClientModule,
     DynamoConfigModule,
     DynamoClientModule,
