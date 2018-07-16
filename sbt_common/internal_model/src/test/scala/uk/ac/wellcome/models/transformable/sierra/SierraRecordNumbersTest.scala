9726f56d6ecb806b6073bbdbcea6be13a27cf16a
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-16 15:11:59 +0100

    Move everything into a describe() block

--- sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/sierra/SierraRecordNumbersTest.scala
+++ sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/sierra/SierraRecordNumbersTest.scala
@@ -5,49 +5,51 @@ import org.scalatest.prop.TableDrivenPropertyChecks._
 
 class SierraRecordNumbersTest extends FunSpec with Matchers {
 
-  val testCases = Table(
-    // Example from the Sierra docs
-    ("1024364", "bibs", "b10243641"),
-    // Examples taken from catalogue records
-    ("1828888", "bibs", "b18288881"),
-    ("1828888", "items", "i18288881"),
-    // Example from a catalogue record with an "x" check digit
-    ("1840974", "items", "i1840974x")
-  )
+  describe("addCheckDigit") {
+    val testCases = Table(
+      // Example from the Sierra docs
+      ("1024364", "bibs", "b10243641"),
+      // Examples taken from catalogue records
+      ("1828888", "bibs", "b18288881"),
+      ("1828888", "items", "i18288881"),
+      // Example from a catalogue record with an "x" check digit
+      ("1840974", "items", "i1840974x")
+    )
 
-  it("correctly handles all of our test cases") {
-    forAll(testCases) {
-      (sierraId: String, recordType: String, expectedId: String) =>
-        val sierraRecordType = recordType match {
-          case "bibs"  => SierraRecordTypes.bibs
-          case "items" => SierraRecordTypes.items
-        }
+    it("correctly handles all of our test cases") {
+      forAll(testCases) {
+        (sierraId: String, recordType: String, expectedId: String) =>
+          val sierraRecordType = recordType match {
+            case "bibs" => SierraRecordTypes.bibs
+            case "items" => SierraRecordTypes.items
+          }
 
-        SierraRecordNumbers.addCheckDigit(sierraId, recordType = sierraRecordType) shouldBe expectedId
-    }
-  }
-
-  it("throws an error if passed a Sierra ID which is non-numeric") {
-    val caught = intercept[RuntimeException] {
-      SierraRecordNumbers.addCheckDigit("abcdefg", recordType = SierraRecordTypes.bibs)
+          SierraRecordNumbers.addCheckDigit(sierraId, recordType = sierraRecordType) shouldBe expectedId
+      }
     }
 
-    caught.getMessage shouldEqual "Expected 7-digit numeric ID, got abcdefg"
-  }
+    it("throws an error if passed a Sierra ID which is non-numeric") {
+      val caught = intercept[RuntimeException] {
+        SierraRecordNumbers.addCheckDigit("abcdefg", recordType = SierraRecordTypes.bibs)
+      }
 
-  it("throws an error if passed a Sierra ID which is too short") {
-    val caught = intercept[RuntimeException] {
-      SierraRecordNumbers.addCheckDigit("123", recordType = SierraRecordTypes.bibs)
+      caught.getMessage shouldEqual "Expected 7-digit numeric ID, got abcdefg"
     }
 
-    caught.getMessage shouldEqual "Expected 7-digit numeric ID, got 123"
-  }
+    it("throws an error if passed a Sierra ID which is too short") {
+      val caught = intercept[RuntimeException] {
+        SierraRecordNumbers.addCheckDigit("123", recordType = SierraRecordTypes.bibs)
+      }
 
-  it("throws an error if passed a Sierra ID which is too long") {
-    val caught = intercept[RuntimeException] {
-      SierraRecordNumbers.addCheckDigit("12345678", recordType = SierraRecordTypes.bibs)
+      caught.getMessage shouldEqual "Expected 7-digit numeric ID, got 123"
     }
 
-    caught.getMessage shouldEqual "Expected 7-digit numeric ID, got 12345678"
+    it("throws an error if passed a Sierra ID which is too long") {
+      val caught = intercept[RuntimeException] {
+        SierraRecordNumbers.addCheckDigit("12345678", recordType = SierraRecordTypes.bibs)
+      }
+
+      caught.getMessage shouldEqual "Expected 7-digit numeric ID, got 12345678"
+    }
   }
 }
