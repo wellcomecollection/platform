a83836a936e11deb8b34e9777142fc276cfc6a1b
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-16 13:27:46 +0100

    Add a test that exposes the Sierra reader bug

--- sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/services/SierraReaderWorkerServiceTest.scala
+++ sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/services/SierraReaderWorkerServiceTest.scala
@@ -157,25 +157,6 @@ class SierraReaderWorkerServiceTest
       fields = "updatedDate,deleted,deletedDate,bibIds,fixedFields,varFields",
       resourceType = SierraResourceTypes.items
     ) { fixtures =>
-      val prefix = "records_items/2013-12-10T17-16-35Z__2013-12-13T21-34-35Z"
-
-      // First we pre-populate S3 with files as if they'd come from a prior run of the reader.
-      s3Client.putObject(fixtures.bucket.name, s"$prefix/0000.json", "[]")
-      s3Client.putObject(
-        fixtures.bucket.name,
-        s"$prefix/0001.json",
-        """
-          |[
-          |  {
-          |    "id": "i1794165",
-          |    "modifiedDate": "2013-12-10T17:16:35Z",
-          |    "data": "{}"
-          |  }
-          |]
-        """.stripMargin
-      )
-
-      // Then we trigger the reader, and we expect it to fill in the rest.
       val body =
         """
           |{
@@ -184,25 +165,36 @@ class SierraReaderWorkerServiceTest
           |}
         """.stripMargin
 
+      // Do a complete run of the reader -- this gives us a set of JSON files
+      // to compare to.
       sendNotificationToSQS(queue = fixtures.queue, body = body)
 
-      val pageNames = List("0000.json", "0001.json", "0002.json", "0003.json")
-        .map { label =>
-          s"$prefix/$label"
-        } ++ List(
-        "windows_items_complete/2013-12-10T17-16-35Z__2013-12-13T21-34-35Z")
+      eventually {
+        assertQueueEmpty(queue = fixtures.queue)
+
+        // There are 157 item records in the Sierra wiremock, so we expect
+        // 5 files -- the four JSON files, and a window marker.
+        listKeysInBucket(bucket = fixtures.bucket) should have size 5
+      }
+
+      val expectedContents = getAllObjectContents(bucket = fixtures.bucket)
+
+      // Now, let's delete every key in the bucket _except_ the first --
+      // which we'll use to restart the window.
+      listKeysInBucket(bucket = fixtures.bucket)
+        .filterNot { _.endsWith("0000.json") }
+        .foreach { key => s3Client.deleteObject(fixtures.bucket.name, key) }
 
       eventually {
-        // There are 157 item records in the Sierra wiremock so we expect 4 files
-        listKeysInBucket(bucket = fixtures.bucket) shouldBe pageNames
+        listKeysInBucket(bucket = fixtures.bucket) should have size 1
+      }
 
-        // These two files were pre-populated -- we check the reader hasn't overwritten these
-        getRecordsFromS3(fixtures.bucket, pageNames(0)) should have size 0
-        getRecordsFromS3(fixtures.bucket, pageNames(1)) should have size 1
+      // Now, send a second message to the reader, and we'll see if it completes
+      // the window successfully.
+      sendNotificationToSQS(queue = fixtures.queue, body = body)
 
-        // We check the reader has filled these in correctly
-        getRecordsFromS3(fixtures.bucket, pageNames(2)) should have size 50
-        getRecordsFromS3(fixtures.bucket, pageNames(3)) should have size 7
+      eventually {
+        getAllObjectContents(bucket = fixtures.bucket) shouldBe expectedContents
       }
     }
   }
