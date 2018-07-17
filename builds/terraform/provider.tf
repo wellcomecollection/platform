00bb4624608a28d2a063dcee0440a1c8733f1067
Alex Chan <alex@alexwlchan.net>
2018-07-17 17:13:36 +0100

    Create the GitHub deploy key in Terraform

--- builds/terraform/provider.tf
+++ builds/terraform/provider.tf
@@ -2,3 +2,11 @@ provider "aws" {
   region  = "${var.aws_region}"
   version = "1.27.0"
 }
+
+# You need to set the GITHUB_TOKEN environment variable to use this provider;
+# there isn't a personal access token in our tfvars because they're per-user,
+# not organisation-wide.
+#
+provider "github" {
+  organization = "wellcometrust"
+}
