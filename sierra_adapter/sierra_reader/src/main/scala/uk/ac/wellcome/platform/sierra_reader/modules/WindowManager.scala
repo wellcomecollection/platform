2ee6af51c57fa5d8ab7b2a7eabb9d0bccf1677fc
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-18 15:05:52 +0100

    Use SierraRecordNumber in WindowStatus

--- sierra_adapter/sierra_reader/src/main/scala/uk/ac/wellcome/platform/sierra_reader/modules/WindowManager.scala
+++ sierra_adapter/sierra_reader/src/main/scala/uk/ac/wellcome/platform/sierra_reader/modules/WindowManager.scala
@@ -7,12 +7,14 @@ import org.apache.commons.io.IOUtils
 import uk.ac.wellcome.platform.sierra_reader.models.{SierraConfig, WindowStatus}
 import uk.ac.wellcome.utils.JsonUtil._
 import uk.ac.wellcome.exceptions.GracefulFailureException
+import uk.ac.wellcome.models.transformable.sierra.SierraRecordNumber
 import uk.ac.wellcome.sierra_adapter.models.SierraRecord
 import uk.ac.wellcome.storage.s3.S3Config
 import uk.ac.wellcome.utils.JsonUtil
 
 import scala.collection.JavaConverters._
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
 
 class WindowManager @Inject()(
   s3client: AmazonS3,
@@ -50,7 +52,7 @@ class WindowManager @Inject()(
 
         val lastBody = IOUtils.toString(
           s3client.getObject(s3Config.bucketName, key).getObjectContent)
-        val triedMaybeLastId =
+        val triedMaybeLastId: Try[Option[SierraRecordNumber]] =
           JsonUtil.fromJson[List[SierraRecord]](lastBody).map { r =>
             r.map { _.id }.sorted.lastOption
           }
@@ -58,13 +60,10 @@ class WindowManager @Inject()(
         info(s"Found latest ID in S3: $triedMaybeLastId")
         val triedStatus = triedMaybeLastId
           .map {
-            case Some(id) =>
-              // The Sierra IDs we store in S3 are prefixed with "b" or "i".
-              // Remove the first character
-              val unprefixedId = id.substring(1)
-
-              val newId = (unprefixedId.toInt + 1).toString
-              WindowStatus(id = Some(newId), offset = offset + 1)
+            case Some(id) => WindowStatus(
+              id = Some(id.increment),
+              offset = offset + 1
+            )
             case None =>
               throw GracefulFailureException(
                 new RuntimeException("Json did not contain an id"))
