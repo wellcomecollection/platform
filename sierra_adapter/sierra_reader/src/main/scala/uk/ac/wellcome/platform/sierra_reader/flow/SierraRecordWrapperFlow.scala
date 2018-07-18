a3f663e101ad34fa5131e6d20e165a0b1c6c89a4
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-18 18:31:07 +0100

    Tidy up a creation of a SierraRecord

--- sierra_adapter/sierra_reader/src/main/scala/uk/ac/wellcome/platform/sierra_reader/flow/SierraRecordWrapperFlow.scala
+++ sierra_adapter/sierra_reader/src/main/scala/uk/ac/wellcome/platform/sierra_reader/flow/SierraRecordWrapperFlow.scala
@@ -1,13 +1,14 @@
 package uk.ac.wellcome.platform.sierra_reader.flow
 
 import java.time.format.DateTimeFormatter
-import java.time.{LocalDate, ZoneOffset}
+import java.time.{Instant, LocalDate, ZoneOffset}
 
 import akka.NotUsed
 import akka.stream.scaladsl.Flow
 import com.twitter.inject.Logging
 import io.circe.Json
 import io.circe.optics.JsonPath.root
+import uk.ac.wellcome.models.transformable.sierra.SierraRecordNumber
 import uk.ac.wellcome.sierra_adapter.models.SierraRecord
 
 object SierraRecordWrapperFlow extends Logging {
@@ -19,20 +20,17 @@ object SierraRecordWrapperFlow extends Logging {
   private def createSierraRecord(json: Json): SierraRecord = {
     logger.debug(s"Creating record from ${json.noSpaces}")
     val maybeUpdatedDate = root.updatedDate.string.getOption(json)
-    maybeUpdatedDate match {
-      case Some(updatedDate) =>
-        SierraRecord(
-          id = getId(json),
-          data = json.noSpaces,
-          modifiedDate = updatedDate
-        )
-      case None =>
-        SierraRecord(
-          id = getId(json),
-          data = json.noSpaces,
-          modifiedDate = getDeletedDateTimeAtStartOfDay(json)
-        )
+
+    val modifiedDate: Instant = maybeUpdatedDate match {
+      case Some(updatedDate) => Instant.parse(updatedDate)
+      case None => getDeletedDateTimeAtStartOfDay(json)
     }
+
+    SierraRecord(
+      id = SierraRecordNumber(getId(json)),
+      data = json.noSpaces,
+      modifiedDate = modifiedDate
+    )
   }
 
   private def getDeletedDateTimeAtStartOfDay(json: Json) = {
