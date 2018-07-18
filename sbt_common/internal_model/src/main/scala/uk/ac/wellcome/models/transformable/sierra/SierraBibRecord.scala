a262b0574852021e1609fed16dbedebfe443dcc5
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-18 17:13:57 +0100

    Now we can drop those two apply methods from BibRecord

--- sbt_common/internal_model/src/main/scala/uk/ac/wellcome/models/transformable/sierra/SierraBibRecord.scala
+++ sbt_common/internal_model/src/main/scala/uk/ac/wellcome/models/transformable/sierra/SierraBibRecord.scala
@@ -7,19 +7,3 @@ case class SierraBibRecord(
   data: String,
   modifiedDate: Instant
 )
-
-object SierraBibRecord {
-  def apply(id: String, data: String, modifiedDate: String): SierraBibRecord =
-    SierraBibRecord(
-      id = id,
-      data = data,
-      modifiedDate = Instant.parse(modifiedDate)
-    )
-
-  def apply(id: String, data: String, modifiedDate: Instant): SierraBibRecord =
-    SierraBibRecord(
-      id = SierraRecordNumber(id),
-      data = data,
-      modifiedDate = modifiedDate
-    )
-}
