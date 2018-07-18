07eaf9da041e1ea4d38a20bf913b4a314e6c591c
alicefuzier <alicefuzier@gmail.com>
2018-07-18 15:39:59 +0100

    Delete old catalogue_pipeline cluster

--- catalogue_pipeline/terraform/outputs.tf
+++ catalogue_pipeline/terraform/outputs.tf
@@ -30,25 +30,6 @@ output "vhs_goobi_bucket_name" {
   value = "${module.vhs_goobi_mets.bucket_name}"
 }
 
-output "vpc_services_id" {
-  value = "${module.vpc_services.vpc_id}"
-}
-
-// TODO delete once the reindexer stack gets migrated to fargate
-output "alb_cloudwatch_id" {
-  value = "${module.catalogue_pipeline_cluster.alb_cloudwatch_id}"
-}
-
-output "alb_listener_https_arn" {
-  value = "${module.catalogue_pipeline_cluster.alb_listener_https_arn}"
-}
-
-output "alb_listener_http_arn" {
-  value = "${module.catalogue_pipeline_cluster.alb_listener_http_arn}"
-}
-
-//
-
 output "cluster_name" {
-  value = "${module.catalogue_pipeline_cluster.cluster_name}"
+  value = "${module.catalogue_pipeline.cluster_name}"
 }
