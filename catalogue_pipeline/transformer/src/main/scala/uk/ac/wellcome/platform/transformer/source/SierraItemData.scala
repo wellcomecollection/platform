69b3fc343c46a281665c264cc05fc8bcb51f133b
Alex Chan <alex@alexwlchan.net>
2018-07-20 10:00:27 +0100

    And allow deserialising SierraItemData

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/source/SierraItemData.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/source/SierraItemData.scala
@@ -1,13 +1,16 @@
 package uk.ac.wellcome.platform.transformer.source
 
+import io.circe.generic.extras.JsonKey
 import uk.ac.wellcome.models.transformable.sierra.SierraRecordNumber
 
 case class SierraItemData(
-  id: SierraRecordNumber,
+  @JsonKey("id") _id: String,
   deleted: Boolean = false,
   location: Option[SierraItemLocation] = None,
   fixedFields: Map[String, FixedField] = Map(),
   varFields: List[VarField] = List()
-)
+) {
+  val id = SierraRecordNumber(_id)
+}
 
 case class SierraItemLocation(code: String, name: String)
---
+++ catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/platform/transformer/source/SierraItemDataTest.scala
@@ -0,0 +1,22 @@
+package uk.ac.wellcome.platform.transformer.source
+
+import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.models.transformable.sierra.SierraRecordNumber
+import uk.ac.wellcome.models.transformable.sierra.test.utils.SierraUtil
+import uk.ac.wellcome.models.work.test.util.IdentifiersUtil
+import uk.ac.wellcome.utils.JsonUtil._
+
+class SierraItemDataTest extends FunSpec with Matchers with IdentifiersUtil with SierraUtil {
+  it("deserialises item data from Sierra correctly") {
+    val id = createSierraRecordNumberString
+    val record = fromJson[SierraItemData](
+      s"""
+         |{
+         |  "id": "$id",
+         |  "bibIds": ["${createSierraRecordNumberString}"]
+         |}
+      """.stripMargin).get
+
+    record.id shouldBe SierraRecordNumber(id)
+  }
+}
