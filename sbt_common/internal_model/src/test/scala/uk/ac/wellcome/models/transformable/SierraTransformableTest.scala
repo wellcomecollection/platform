3cd05abb0f4b7cb6ccc443bded70e8f0d569a941
Alex Chan <alex@alexwlchan.net>
2018-07-20 08:14:26 +0100

    Add a test that JSON encoding/decoding works

--- sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/SierraTransformableTest.scala
+++ sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/SierraTransformableTest.scala
@@ -1,19 +1,28 @@
 package uk.ac.wellcome.models.transformable
 
 import org.scalatest.{FunSpec, Matchers}
-import uk.ac.wellcome.models.transformable.sierra.SierraRecordNumber
 import uk.ac.wellcome.models.transformable.sierra.test.utils.SierraUtil
+import uk.ac.wellcome.utils.JsonUtil._
 
 class SierraTransformableTest extends FunSpec with Matchers with SierraUtil {
 
-  it("should allow creation of SierraTransformable with no data") {
-    SierraTransformable(sierraId = SierraRecordNumber("1111111"))
+  it("allows creating a SierraTransformable with no data") {
+    SierraTransformable(sierraId = createSierraRecordNumber)
   }
 
-  it("should allow creation from only a SierraBibRecord") {
+  it("allows creating from only a SierraBibRecord") {
     val bibRecord = createSierraBibRecord
     val mergedRecord = SierraTransformable(bibRecord = bibRecord)
     mergedRecord.sierraId shouldEqual bibRecord.id
     mergedRecord.maybeBibRecord.get shouldEqual bibRecord
+  }
+
+  it("can decode/encode a SierraTransformable via JSON") {
+    val transformable = SierraTransformable(sierraId = createSierraRecordNumber)
+
+    val jsonString = toJson(transformable).get
+    val parsedJson = fromJson[SierraTransformable](jsonString).get
+
+    parsedJson shouldBe transformable
   }
 }
