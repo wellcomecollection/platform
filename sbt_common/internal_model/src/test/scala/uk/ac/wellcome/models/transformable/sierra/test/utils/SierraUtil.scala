9c8f8e8a9c06cb815ffa7134f3aad09ee44cc60c
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-20 17:49:08 +0100

    Add util classes for creating instances of SierraBibData

---
+++ sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/sierra/test/utils/SierraUtil.scala
@@ -0,0 +1,21 @@
+package uk.ac.wellcome.models.transformable.sierra.test.utils
+
+import scala.util.Random
+
+trait SierraUtil {
+  /** Returns a random digit as a string.  This is copied from the
+    * definition of Random.alphanumeric.
+    */
+  private def randomNumeric: Stream[Char] = {
+    def nextDigit: Char = {
+      val chars = '0' to '9'
+      chars charAt Random.nextInt(chars.length)
+    }
+
+    Stream continually nextDigit
+  }
+
+  def createSierraRecordNumberString: String =
+    randomNumeric take 7 mkString
+
+}
