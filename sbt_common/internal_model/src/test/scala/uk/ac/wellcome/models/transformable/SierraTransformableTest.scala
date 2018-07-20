3af5a0b46b5d604c51247ea8143491346070de4e
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-20 13:27:19 +0100

    Add a test that SierraTransformable.id is working correctly

--- sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/SierraTransformableTest.scala
+++ sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/SierraTransformableTest.scala
@@ -4,6 +4,7 @@ import org.scalatest.{FunSpec, Matchers}
 import uk.ac.wellcome.models.transformable.sierra.test.utils.SierraUtil
 import uk.ac.wellcome.utils.JsonUtil._
 import uk.ac.wellcome.models.transformable.SierraTransformableCodec._
+import uk.ac.wellcome.models.transformable.sierra.SierraRecordNumber
 
 class SierraTransformableTest extends FunSpec with Matchers with SierraUtil {
 
@@ -16,6 +17,12 @@ class SierraTransformableTest extends FunSpec with Matchers with SierraUtil {
     val mergedRecord = SierraTransformable(bibRecord = bibRecord)
     mergedRecord.sierraId shouldEqual bibRecord.id
     mergedRecord.maybeBibRecord.get shouldEqual bibRecord
+  }
+
+  it("has the correct ID") {
+    val sierraId = createSierraRecordNumberString
+    val transformable = SierraTransformable(sierraId = SierraRecordNumber(sierraId))
+    transformable.id shouldBe s"sierra/$sierraId"
   }
 
   it("can serialise a SierraTransformable via JSON") {
