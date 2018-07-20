a915bd07b53af5102f61731c7619e4eb9dacad7e
Alex Chan <alex@alexwlchan.net>
2018-07-20 08:54:44 +0100

    Fix JSON encoding/decoding for SierraTransformable

--- sbt_common/internal_model/src/main/scala/uk/ac/wellcome/models/transformable/Transformable.scala
+++ sbt_common/internal_model/src/main/scala/uk/ac/wellcome/models/transformable/Transformable.scala
@@ -1,11 +1,10 @@
 package uk.ac.wellcome.models.transformable
 
+import io.circe._
+import io.circe.syntax._
 import uk.ac.wellcome.models.Sourced
-import uk.ac.wellcome.models.transformable.sierra.{
-  SierraBibRecord,
-  SierraItemRecord,
-  SierraRecordNumber
-}
+import uk.ac.wellcome.models.transformable.sierra.{SierraBibRecord, SierraItemRecord, SierraRecordNumber}
+import uk.ac.wellcome.utils.JsonUtil._
 
 sealed trait Transformable extends Sourced
 
@@ -21,10 +20,11 @@ case class MiroTransformable(sourceId: String,
   */
 case class SierraTransformable(
   sierraId: SierraRecordNumber,
-  sourceName: String = "sierra",
   maybeBibRecord: Option[SierraBibRecord] = None,
   itemRecords: Map[SierraRecordNumber, SierraItemRecord] = Map()
 ) extends Transformable {
+  val sourceName = "sierra"
+
   def sourceId: String =
     sierraId.withoutCheckDigit
 }
@@ -35,4 +35,41 @@ object SierraTransformable {
       sierraId = bibRecord.id,
       maybeBibRecord = Some(bibRecord)
     )
+}
+
+/** Provides a Circe encoder/decoder for SierraTransformable.
+  *
+  * Because the [[SierraTransformable.itemRecords]] field is keyed by
+  * [[SierraRecordNumber]] in our case class, but JSON only supports string
+  * keys, we need to turn the ID into a string when storing as JSON.
+  *
+  * To use these helpers, add
+  *
+  *     import uk.ac.wellcome.models.transformable.SierraTransformableCodec._
+  *
+  * to your imports.
+  *
+  */
+object SierraTransformableCodec {
+  implicit val itemRecordsDecoder: Decoder[Map[SierraRecordNumber, SierraItemRecord]] =
+    Decoder.instance[Map[SierraRecordNumber, SierraItemRecord]] { cursor: HCursor =>
+      cursor
+        .as[Map[String, SierraItemRecord]]
+        .map { itemRecordsByString: Map[String, SierraItemRecord] =>
+          itemRecordsByString
+            .map { case (id: String, itemRecord: SierraItemRecord) =>
+              SierraRecordNumber(id) -> itemRecord
+            }
+        }
+    }
+
+  implicit val itemRecordsEncoder: Encoder[Map[SierraRecordNumber, SierraItemRecord]] =
+    Encoder.instance[Map[SierraRecordNumber, SierraItemRecord]] {
+      itemRecords: Map[SierraRecordNumber, SierraItemRecord] =>
+        Json.fromFields(
+          itemRecords.map { case (id: SierraRecordNumber, itemRecord: SierraItemRecord) =>
+            id.withoutCheckDigit -> itemRecord.asJson
+          }
+        )
+    }
 }
--- sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/SierraTransformableTest.scala
+++ sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/SierraTransformableTest.scala
@@ -3,6 +3,7 @@ package uk.ac.wellcome.models.transformable
 import org.scalatest.{FunSpec, Matchers}
 import uk.ac.wellcome.models.transformable.sierra.test.utils.SierraUtil
 import uk.ac.wellcome.utils.JsonUtil._
+import uk.ac.wellcome.models.transformable.SierraTransformableCodec._
 
 class SierraTransformableTest extends FunSpec with Matchers with SierraUtil {
 
@@ -17,8 +18,17 @@ class SierraTransformableTest extends FunSpec with Matchers with SierraUtil {
     mergedRecord.maybeBibRecord.get shouldEqual bibRecord
   }
 
-  it("can decode/encode a SierraTransformable via JSON") {
-    val transformable = SierraTransformable(sierraId = createSierraRecordNumber)
+  it("can serialise a SierraTransformable via JSON") {
+    val itemRecords = (1 to 3)
+      .map { _ => createSierraItemRecord }
+      .map { itemRecord => itemRecord.id -> itemRecord }
+      .toMap
+
+    val transformable = SierraTransformable(
+      sierraId = createSierraRecordNumber,
+      maybeBibRecord = Some(createSierraBibRecord),
+      itemRecords = itemRecords
+    )
 
     val jsonString = toJson(transformable).get
     val parsedJson = fromJson[SierraTransformable](jsonString).get
