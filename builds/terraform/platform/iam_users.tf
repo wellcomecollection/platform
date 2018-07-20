f9c8e0a01f60b9ef6cc0fdba57b7c61f856dbaeb
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-07-20 09:41:24 +0000

    Apply auto-formatting rules

--- builds/terraform/platform/iam_users.tf
+++ builds/terraform/platform/iam_users.tf
@@ -3,7 +3,7 @@ resource "aws_iam_user" "travis_ci" {
 }
 
 resource "aws_iam_access_key" "travis_ci" {
-  user    = "${aws_iam_user.travis_ci.name}"
+  user = "${aws_iam_user.travis_ci.name}"
 }
 
 resource "aws_iam_user_policy" "travis_ci" {
