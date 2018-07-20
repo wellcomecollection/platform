fa0bce891d7a7d7e75225e51a44f8116f1d553b2
alicefuzier <alicefuzier@gmail.com>
2018-07-20 16:36:21 +0100

    wiretap terraform

--- catalogue_pipeline/terraform/pipeline/outputs.tf
+++ catalogue_pipeline/terraform/pipeline/outputs.tf
@@ -5,3 +5,27 @@ output "rds_access_security_group_id" {
 output "cluster_name" {
   value = "${var.namespace}"
 }
+
+output "transformer_topic_arn" {
+  value = "${module.transformer_topic.arn}"
+}
+
+output "es_ingest_topic_arn" {
+  value = "${module.es_ingest_topic.arn}"
+}
+
+output "transformed_works_topic_arn" {
+  value = "${module.transformed_works_topic.arn}"
+}
+
+output "recorded_works_topic_arn" {
+  value = "${module.recorded_works_topic.arn}"
+}
+
+output "merged_works_topic_arn" {
+  value = "${module.merged_works_topic.arn}"
+}
+
+output "matched_works_topic_arn" {
+  value = "${module.matched_works_topic.arn}"
+}
