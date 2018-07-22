d5bacbbb354f11dc4e507fa2903859b971bcc043
Alex Chan <alex@alexwlchan.net>
2018-07-22 15:38:27 +0100

    Create some helper methods for instances of SierraItemRecord

--- sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/sierra/test/utils/SierraUtil.scala
+++ sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/sierra/test/utils/SierraUtil.scala
@@ -1,8 +1,18 @@
 package uk.ac.wellcome.models.transformable.sierra.test.utils
 
+import java.time.Instant
+
+import uk.ac.wellcome.models.transformable.sierra.SierraItemRecord
+
 import scala.util.Random
 
 trait SierraUtil {
+
+  // A lot of Sierra tests (e.g. mergers) check the behaviour when merging
+  // a record with a newer version, or vice versa.  Provide two dates here
+  // for convenience.
+  val olderDate: Instant = Instant.parse("1999-09-09T09:09:09Z")
+  val newerDate: Instant = Instant.parse("2001-01-01T01:01:01Z")
 
   /** Returns a random digit as a string.  This is copied from the
     * definition of Random.alphanumeric.
@@ -19,4 +29,27 @@ trait SierraUtil {
   def createSierraRecordNumberString: String =
     randomNumeric take 7 mkString
 
+  def createSierraItemRecordWith(
+    id: String = createSierraRecordNumberString,
+    data: String = "",
+    modifiedDate: Instant = Instant.now,
+    bibIds: List[String] = List(),
+    unlinkedBibIds: List[String] = List(),
+    version: Int = 0
+  ): SierraItemRecord = {
+    val recordData = if (data == "") {
+      s"""{"id": "$id"}"""
+    } else data
+
+    SierraItemRecord(
+      id = id,
+      data = recordData,
+      modifiedDate = modifiedDate,
+      bibIds = bibIds,
+      unlinkedBibIds = unlinkedBibIds,
+      version = version
+    )
+  }
+
+  def createSierraItemRecord: SierraItemRecord = createSierraItemRecordWith()
 }
