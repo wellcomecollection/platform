00466c5cad35192dcf1a1e2132b48026d6fcc880
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-07-23 13:22:45 +0100

    Add the ability to toggle auto scale down in data_science stack

--- data_science/scripts/toggle_asg.py
+++ data_science/scripts/toggle_asg.py
@@ -3,12 +3,14 @@
 """
 Start/stop all the instances in an autoscaling group.
 
-Usage: toggle_asg.py (--start | --stop | --status) [--type=(p2 | t2)]
+Usage: toggle_asg.py (--start | --stop | --status | --keep-on | --allow-scale-down) [--type=(p2 | t2)]
 
 Actions:
   --start                 Start the autoscaling group (set the desired count to 1).
   --stop                  Stop the autoscaling group (set the desired count to 0).
   --status                Report the current side of autoscaling group.
+  --keep-on               Keeps the autoscaling group desired count at 1 (disables auto scale down)
+  --allow-scale-down      Allows the automatic scaling action to be set desired count to 0 (enables auto scale down)
   --type=(p2 | t2)        AWS Instance type (valid values: p2,t2) defaults to t2
 
 """
@@ -20,6 +22,42 @@ import docopt
 
 from asg_utils import discover_asg, set_asg_size
 
+
+def will_stay_up(client, scaling_group_name):
+    response = client.describe_scheduled_actions(
+        AutoScalingGroupName=scaling_group_name,
+        ScheduledActionNames=['ensure_down']
+    )
+
+    if(response['ScheduledUpdateGroupActions']):
+        desired_capacity = response['ScheduledUpdateGroupActions'][0]['DesiredCapacity']
+        will_stay_up = bool(desired_capacity)
+    else:
+        raise ValueError('No ScheduledUpdateGroupActions matching name "ensure_down"')
+
+    return will_stay_up
+
+
+def keep_on(client, scaling_group_name):
+    client.put_scheduled_update_group_action(
+        AutoScalingGroupName=scaling_group_name,
+        ScheduledActionName='ensure_down',
+        Recurrence='0 20 * * *',
+        DesiredCapacity=1
+    )
+    print("No scaling action will take place.")
+
+
+def allow_scale_down(client, scaling_group_name):
+    client.put_scheduled_update_group_action(
+        AutoScalingGroupName=scaling_group_name,
+        ScheduledActionName='ensure_down',
+        Recurrence='0 20 * * *',
+        DesiredCapacity=0
+    )
+    print("Scaling to 0 instances will take place this evening.")
+
+
 if __name__ == '__main__':
     args = docopt.docopt(__doc__)
 
@@ -30,6 +68,7 @@ if __name__ == '__main__':
     asg = discover_asg(asg_client=asg_client, tag_name=tag_name)
     asg_name = asg['AutoScalingGroupName']
 
+    # Starting or stopping
     if args['--start'] or args['--stop']:
         if args['--start']:
             desired_size = 1
@@ -42,6 +81,14 @@ if __name__ == '__main__':
             desired_size=desired_size
         )
 
+    # Scaling rules
+    elif args['--keep-on'] or args['--allow-scale-down']:
+        if args['--keep-on']:
+            keep_on(asg_client, asg_name)
+        else:
+            allow_scale_down(asg_client, asg_name)
+
+    # Report status
     elif args['--status']:
         actual_size = asg['DesiredCapacity']
         instance_count = len(asg['Instances'])
@@ -53,9 +100,19 @@ if __name__ == '__main__':
         else:
             instance_str = '%d instances' % instance_count
 
+        if(will_stay_up(asg_client, asg_name)):
+            scaling_status = 'OFF'
+        else:
+            scaling_status = 'ON'
+
         print(
             'The desired size of ASG group %r is %r, with %s running' %
             (asg_name, actual_size, instance_str)
+        )
+
+        print(
+            'The scaling rule to turn the ASG off overnight is %r' %
+            (scaling_status)
         )
 
     else:
