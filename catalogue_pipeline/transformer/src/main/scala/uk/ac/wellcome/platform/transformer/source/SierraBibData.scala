03e6c49877f33cad966e97f7e26f31e229ce9c59
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-24 13:17:03 +0100

    Okay, we can actually treat SierraRecordNumber as a string

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/source/SierraBibData.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/source/SierraBibData.scala
@@ -5,37 +5,12 @@ import uk.ac.wellcome.platform.transformer.source.sierra.{Country => SierraCount
 
 case class SierraBibData(
   id: SierraRecordNumber,
-  title: Option[String],
-  deleted: Boolean,
-  suppressed: Boolean,
-  country: Option[SierraCountry],
-  lang: Option[SierraLanguage],
-  materialType: Option[SierraMaterialType],
-  fixedFields: Map[String, FixedField],
-  varFields: List[VarField]
+  title: Option[String] = None,
+  deleted: Boolean = false,
+  suppressed: Boolean = false,
+  country: Option[SierraCountry] = None,
+  lang: Option[SierraLanguage] = None,
+  materialType: Option[SierraMaterialType] = None,
+  fixedFields: Map[String, FixedField] = Map(),
+  varFields: List[VarField] = List()
 )
-
-case object SierraBibData {
-  def apply(
-    id: String,
-    title: Option[String] = None,
-    deleted: Boolean = false,
-    suppressed: Boolean = false,
-    country: Option[SierraCountry] = None,
-    lang: Option[SierraLanguage] = None,
-    materialType: Option[SierraMaterialType] = None,
-    fixedFields: Map[String, FixedField] = Map(),
-    varFields: List[VarField] = List()
-  ): SierraBibData =
-    SierraBibData(
-      id = SierraRecordNumber(id),
-      title = title,
-      deleted = deleted,
-      suppressed = suppressed,
-      country = country,
-      lang = lang,
-      materialType = materialType,
-      fixedFields = fixedFields,
-      varFields = varFields
-    )
-}
