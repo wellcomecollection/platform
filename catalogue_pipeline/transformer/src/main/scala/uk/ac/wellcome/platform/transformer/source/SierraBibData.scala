d70949764884940f2067acad05879cfb2b39639a
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-24 10:45:38 +0100

    Try creating SierraBibNumber with a first-class record number

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/source/SierraBibData.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/source/SierraBibData.scala
@@ -4,7 +4,7 @@ import uk.ac.wellcome.models.transformable.sierra.SierraRecordNumber
 import uk.ac.wellcome.platform.transformer.source.sierra.{Country => SierraCountry, Language => SierraLanguage}
 
 case class SierraBibData(
-  id: String,
+  id: SierraRecordNumber,
   title: Option[String] = None,
   deleted: Boolean = false,
   suppressed: Boolean = false,
@@ -13,6 +13,29 @@ case class SierraBibData(
   materialType: Option[SierraMaterialType] = None,
   fixedFields: Map[String, FixedField] = Map(),
   varFields: List[VarField] = List()
-) {
-  val sierraId = SierraRecordNumber(id)
+)
+
+case object SierraBibData {
+  def apply(
+    id: String,
+    title: Option[String] = None,
+    deleted: Boolean = false,
+    suppressed: Boolean = false,
+    country: Option[SierraCountry] = None,
+    lang: Option[SierraLanguage] = None,
+    materialType: Option[SierraMaterialType] = None,
+    fixedFields: Map[String, FixedField] = Map(),
+    varFields: List[VarField] = List()
+  ): SierraBibData =
+    SierraBibData(
+      id = SierraRecordNumber(id),
+      title = title,
+      deleted = deleted,
+      suppressed = suppressed,
+      country = country,
+      lang = lang,
+      materialType = materialType,
+      fixedFields = fixedFields,
+      varFields = varFields
+    )
 }
