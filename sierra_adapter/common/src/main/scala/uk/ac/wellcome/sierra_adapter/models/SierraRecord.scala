4b2f13cfb52a20d9d7b11ac6afec56ac388573ed
Alex Chan <alex@alexwlchan.net>
2018-07-26 16:20:15 +0100

    And ditch the SierraItemRecord methods

--- sierra_adapter/common/src/main/scala/uk/ac/wellcome/sierra_adapter/models/SierraRecord.scala
+++ sierra_adapter/common/src/main/scala/uk/ac/wellcome/sierra_adapter/models/SierraRecord.scala
@@ -2,32 +2,4 @@ package uk.ac.wellcome.sierra_adapter.models
 
 import java.time.Instant
 
-import io.circe.optics.JsonPath.root
-import io.circe.parser._
-import uk.ac.wellcome.models.transformable.sierra.{
-  SierraBibRecord,
-  SierraItemRecord
-}
-
-import scala.util.Try
-
-case class SierraRecord(id: String, data: String, modifiedDate: Instant) {
-  def toItemRecord: Try[SierraItemRecord] =
-    for {
-      json <- parse(this.data).toTry
-      bibIdsJsonSeq = root.bibIds.arr
-        .getOption(json)
-        .getOrElse(throw new IllegalArgumentException(
-          "Json data did not contain bibIds"))
-      bibIds = bibIdsJsonSeq.map { json =>
-        json.asString.getOrElse(
-          throw new IllegalArgumentException("Found non string in bibIds"))
-      }.toList
-    } yield {
-      SierraItemRecord(
-        id = this.id,
-        data = this.data,
-        modifiedDate = this.modifiedDate,
-        bibIds = bibIds)
-    }
-}
+case class SierraRecord(id: String, data: String, modifiedDate: Instant)
