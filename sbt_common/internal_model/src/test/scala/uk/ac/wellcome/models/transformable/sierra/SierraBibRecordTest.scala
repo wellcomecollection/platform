b07fec3bd980a1968229e4cfcafccdad2f953d08
Alex Chan <alex@alexwlchan.net>
2018-07-26 06:58:02 +0100

    Make the SierraBibRecord tests more robust

--- sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/sierra/SierraBibRecordTest.scala
+++ sbt_common/internal_model/src/test/scala/uk/ac/wellcome/models/transformable/sierra/SierraBibRecordTest.scala
@@ -1,16 +1,25 @@
 package uk.ac.wellcome.models.transformable.sierra
 
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.{Assertion, FunSpec, Matchers}
+import uk.ac.wellcome.models.transformable.sierra.SierraRecordNumber._
 import uk.ac.wellcome.models.transformable.sierra.test.utils.SierraUtil
+import uk.ac.wellcome.test.utils.JsonTestUtil
 import uk.ac.wellcome.utils.JsonUtil._
 
-class SierraBibRecordTest extends FunSpec with Matchers with SierraUtil {
+class SierraBibRecordTest extends FunSpec with Matchers with JsonTestUtil with SierraUtil {
 
   it("can cast a SierraBibRecord to JSON and back again") {
     val originalRecord = createSierraBibRecord
 
     val jsonString = toJson(originalRecord).get
     val parsedRecord = fromJson[SierraBibRecord](jsonString).get
-    parsedRecord shouldEqual originalRecord
+    assertSierraBibRecordsAreEqual(parsedRecord, originalRecord)
+  }
+
+  private def assertSierraBibRecordsAreEqual(x: SierraBibRecord,
+                                             y: SierraBibRecord): Assertion = {
+    x.id shouldBe x.id
+    assertJsonStringsAreEqual(x.data, y.data)
+    x.modifiedDate shouldBe y.modifiedDate
   }
 }
