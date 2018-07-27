cbbf52f3ee411a21b162d9babcb2b78fd124c97d
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-27 16:26:23 +0100

    Use SierraBibNumber in SierraBibRecord

--- sbt_common/internal_model/src/main/scala/uk/ac/wellcome/models/transformable/sierra/SierraBibRecord.scala
+++ sbt_common/internal_model/src/main/scala/uk/ac/wellcome/models/transformable/sierra/SierraBibRecord.scala
@@ -3,7 +3,7 @@ package uk.ac.wellcome.models.transformable.sierra
 import java.time.Instant
 
 case class SierraBibRecord(
-  id: String,
+  id: SierraBibNumber,
   data: String,
   modifiedDate: Instant
 ) extends AbstractSierraRecord
