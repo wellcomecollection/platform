966f79b33a7b864c1cf111db92964a8dacdd138a
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-27 15:44:07 +0100

    Remove an unused import

--- catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/platform/transformer/SierraTransformerFeatureTest.scala
+++ catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/platform/transformer/SierraTransformerFeatureTest.scala
@@ -11,7 +11,6 @@ import uk.ac.wellcome.models.work.internal.{
 import uk.ac.wellcome.storage.fixtures.S3
 import uk.ac.wellcome.test.utils.ExtendedPatience
 import uk.ac.wellcome.platform.transformer.utils.TransformableMessageUtils
-import uk.ac.wellcome.utils.JsonUtil
 import uk.ac.wellcome.utils.JsonUtil._
 
 class SierraTransformerFeatureTest
