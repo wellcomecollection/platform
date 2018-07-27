2fd962823b8d79385433cc40774a41b6b3fc5b4b
tomcrane <tom@nanoplanet.com>
2018-07-27 18:06:46 +0100

    add example .sh for running convenience

---
+++ archive/bagger/example.sh
@@ -0,0 +1,20 @@
+# asset sources
+export METS_FILESYSTEM_ROOT=''
+export METS_BUCKET_NAME='xxxx'
+export WORKING_DIRECTORY='/tmp/_bagger'
+export DROP_BUCKET_NAME='yyyy'
+# aws
+export AWS_PUBLIC_KEY='xxxx'
+export AWS_SECRET_KEY='xxxx'
+export AWS_REGION='eu-west-1'
+# DLCS config
+export DLCS_ENTRY='https://api.dlcs.io/'
+export DLCS_API_KEY='xxxx'
+export DLCS_SECRET='xxxx'
+export DLCS_CUSTOMER_ID='99'
+export DLCS_SPACE='99'
+# DDS credentials
+export DDS_API_KEY='xxxx'
+export DDS_API_SECRET='xxxx'
+
+python bagger.py $1
