f9384cff83ef2844be6f211dbfd356d77857f403
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-07-27 11:09:16 +0000

    Apply auto-formatting rules

--- catalogue_pipeline/merger/src/main/scala/uk/ac/wellcome/platform/merger/services/Merger.scala
+++ catalogue_pipeline/merger/src/main/scala/uk/ac/wellcome/platform/merger/services/Merger.scala
@@ -25,17 +25,14 @@ class Merger extends Logging {
       // As the works are supplied by the matcher these are trusted to refer to the same work without verification.
       // However, it may be prudent to add extra checks before making the merge here.
       case (List(physicalWork), List(digitalWork)) =>
-        mergeAndRedirectWork(
-          physicalWork,
-          digitalWork)
+        mergeAndRedirectWork(physicalWork, digitalWork)
       case _ =>
         None
     }
   }
 
-  private def mergeAndRedirectWork(
-    physicalWork: UnidentifiedWork,
-    digitalWork: UnidentifiedWork) = {
+  private def mergeAndRedirectWork(physicalWork: UnidentifiedWork,
+                                   digitalWork: UnidentifiedWork) = {
     if (physicalWork.items.size == 1 && digitalWork.items.size == 1) {
       info(s"Merging ${describeWorkPair(physicalWork, digitalWork)} work pair.")
       Some(
@@ -52,13 +49,16 @@ class Merger extends Logging {
     }
   }
 
-  private def mergePhysicalAndDigitalWorkItems(physicalWork: UnidentifiedWork,
-                                               digitalWork: UnidentifiedWork) = {
+  private def mergePhysicalAndDigitalWorkItems(
+    physicalWork: UnidentifiedWork,
+    digitalWork: UnidentifiedWork) = {
     val mergedItem = (physicalWork.items, digitalWork.items) match {
       case (List(physicalItem), List(digitalItem)) =>
         physicalItem.copy(agent = physicalItem.agent.copy(
           locations = physicalItem.agent.locations ++ digitalItem.agent.locations))
-      case _ => throw new IllegalArgumentException(s"Cannot merge, physical and digital works must both have a single item (physicalWork:${physicalWork.sourceIdentifier}, digitalWork:${digitalWork.sourceIdentifier}")
+      case _ =>
+        throw new IllegalArgumentException(
+          s"Cannot merge, physical and digital works must both have a single item (physicalWork:${physicalWork.sourceIdentifier}, digitalWork:${digitalWork.sourceIdentifier}")
     }
     physicalWork.copy(items = List(mergedItem))
   }
