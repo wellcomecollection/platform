fee8cb98d6c89483a4d90363081d4a32a8091afa
Alex Chan <alex@alexwlchan.net>
2018-07-28 22:07:43 +0100

    Add key encoder/decoder for SierraTransformable

--- sbt_common/internal_model/src/main/scala/uk/ac/wellcome/models/transformable/Transformable.scala
+++ sbt_common/internal_model/src/main/scala/uk/ac/wellcome/models/transformable/Transformable.scala
@@ -1,5 +1,6 @@
 package uk.ac.wellcome.models.transformable
 
+import io.circe.{KeyDecoder, KeyEncoder}
 import uk.ac.wellcome.models.Sourced
 import uk.ac.wellcome.models.transformable.sierra.{SierraBibNumber, SierraBibRecord, SierraItemNumber, SierraItemRecord}
 
@@ -21,7 +22,20 @@ case class SierraTransformable(
   val sourceName = "sierra"
 }
 
-object SierraTransformable {
+case object SierraTransformable {
   def apply(bibRecord: SierraBibRecord): SierraTransformable =
     SierraTransformable(sierraId = bibRecord.id, maybeBibRecord = Some(bibRecord))
+
+  // Because the [[SierraTransformable.itemRecords]] field is keyed by
+  // [[SierraItemNumber]] in our case class, but JSON only supports string
+  // keys, we need to turn the ID into a string when storing as JSON.
+  //
+  // This is based on the "Custom key types" section of the Circe docs:
+  // https://circe.github.io/circe/codecs/custom-codecs.html#custom-key-types
+  //
+  implicit val keyEncoder: KeyEncoder[SierraItemNumber] =
+    (key: SierraItemNumber) => key.withoutCheckDigit
+
+  implicit val keyDecoder: KeyDecoder[SierraItemNumber] =
+    (key: String) => Some(SierraItemNumber(key))
 }
