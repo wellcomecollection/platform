a179568ddedbfea0873f0ed9ab6974d0ac8eda0f
Alex Chan <alex@alexwlchan.net>
2018-07-29 10:06:38 +0100

    And in fact, now we can combine those fixtures

--- catalogue_pipeline/ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/fixtures/WorkIndexerFixtures.scala
+++ catalogue_pipeline/ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/fixtures/WorkIndexerFixtures.scala
@@ -3,23 +3,14 @@ package uk.ac.wellcome.platform.ingestor.fixtures
 import com.sksamuel.elastic4s.http.HttpClient
 import org.scalatest.Suite
 import uk.ac.wellcome.platform.ingestor.services.WorkIndexer
-import uk.ac.wellcome.test.fixtures._
+import uk.ac.wellcome.test.fixtures.TestWith
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-trait WorkIndexerFixtures extends Akka { this: Suite =>
+trait WorkIndexerFixtures { this: Suite =>
   def withWorkIndexer[R](elasticClient: HttpClient)(
     testWith: TestWith[WorkIndexer, R]): R = {
     val workIndexer = new WorkIndexer(elasticClient = elasticClient)
     testWith(workIndexer)
-  }
-
-  def withWorkIndexerFixtures[R](elasticClient: HttpClient)(
-    testWith: TestWith[WorkIndexer, R]): R = {
-    withActorSystem { _ =>
-      withWorkIndexer(elasticClient = elasticClient) { workIndexer =>
-        testWith(workIndexer)
-      }
-    }
   }
 }
