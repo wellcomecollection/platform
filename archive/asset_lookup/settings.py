b82ba50ff86c47eb60951bf0ff8478f7ba2db2db
Adam Christie <fractos@gmail.com>
2018-07-30 10:49:40 +0100

    initial commit of asset_lookup folder.

---
+++ archive/asset_lookup/settings.py
@@ -0,0 +1,5 @@
+import os
+
+TABLE_NAME = os.getenv('TABLE_NAME')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
+AWS_REGION = os.getenv('AWS_REGION')
---
+++ archive/asset_lookup/tests.py
@@ -0,0 +1,69 @@
+import unittest
+import boto3
+from moto import mock_dynamodb2, mock_s3
+from endpoint import lambda_handler
+import settings
+
+class TestLambda(unittest.TestCase):
+
+  @mock_s3
+  @mock_dynamodb2
+  def test_lambda(self):
+
+    identifier = 'b12345678x'
+
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(settings.BUCKET_NAME)
+    bucket.create()
+    bucket.put_object(
+      Key = identifier,
+      Body = 'storage manifest content'
+    )
+
+    dynamodb_client = boto3.client('dynamodb', region_name=settings.AWS_REGION)
+    dynamodb_client.create_table(
+      TableName = settings.TABLE_NAME,
+      KeySchema = [
+        {
+          'AttributeName': 'identifier',
+          'KeyType': 'HASH'
+        }
+      ],
+      AttributeDefinitions = [
+        {
+          'AttributeName': 'identifier',
+          'AttributeType': 'S'
+        },
+        {
+          'AttributeName': 's3',
+          'AttributeType': 'S'
+        }
+      ],
+      ProvisionedThroughput={
+        'ReadCapacityUnits': 20,
+        'WriteCapacityUnits': 60
+      }
+    )
+
+    dynamodb_client.put_item(
+      TableName=settings.TABLE_NAME,
+      Item={
+        'identifier': {
+          'S': identifier
+        },
+        's3': {
+          'S': identifier
+        }
+      }
+    )
+
+    event = {
+      "identifier": identifier
+    }
+
+    result = lambda_handler(event, None)
+
+    self.assertEqual(result, "storage manifest content")
+
+if __name__ == '__main__':
+    unittest.main()
