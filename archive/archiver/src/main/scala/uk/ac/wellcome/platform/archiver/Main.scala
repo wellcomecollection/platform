16e413ae7d23c2576f9b510cd75802c0a6554c8f
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-07-31 16:56:32 +0100

    thinking about config

--- archive/archiver/src/main/scala/uk/ac/wellcome/platform/archiver/Main.scala
+++ archive/archiver/src/main/scala/uk/ac/wellcome/platform/archiver/Main.scala
@@ -1,6 +1,26 @@
 package uk.ac.wellcome.platform.archiver
 
+import akka.actor.ActorSystem
+import com.google.inject.name.Named
+import com.google.inject.{Guice, Inject}
+//import uk.ac.wellcome.platform.archiver.modules.{AkkaModule, AkkaS3ClientModule}
+
+case class Config @Inject()(@Named("conf") value: String)
 
 object Main extends App {
-  println("hi")
+  val system: ActorSystem = ActorSystem("archiver")
+  val injector = Guice.createInjector()
+
+  println(args.toList)
+
+  //  val s3Client = injector.getInstance(S3Client.getClass)
+
+  try {
+    val dummy = injector.getInstance(classOf[Config])
+
+    println(s"hi ${dummy.value}")
+
+  } finally {
+    system.terminate
+  }
 }
