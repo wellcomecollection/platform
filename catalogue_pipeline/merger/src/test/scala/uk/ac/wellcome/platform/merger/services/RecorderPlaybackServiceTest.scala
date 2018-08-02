1fbdfce3d70db6aa647f90b0f58ca18716e2c80a
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-02 15:37:27 +0100

    Tidy up the VHS fixture

--- catalogue_pipeline/merger/src/test/scala/uk/ac/wellcome/platform/merger/services/RecorderPlaybackServiceTest.scala
+++ catalogue_pipeline/merger/src/test/scala/uk/ac/wellcome/platform/merger/services/RecorderPlaybackServiceTest.scala
@@ -6,9 +6,7 @@ import uk.ac.wellcome.models.matcher.WorkIdentifier
 import uk.ac.wellcome.models.recorder.internal.RecorderWorkEntry
 import uk.ac.wellcome.platform.merger.MergerTestUtils
 import uk.ac.wellcome.storage.ObjectStore
-import uk.ac.wellcome.storage.fixtures.LocalDynamoDb.Table
 import uk.ac.wellcome.storage.fixtures.LocalVersionedHybridStore
-import uk.ac.wellcome.storage.fixtures.S3.Bucket
 import uk.ac.wellcome.storage.vhs.{EmptyMetadata, VersionedHybridStore}
 import uk.ac.wellcome.test.fixtures._
 import uk.ac.wellcome.json.JsonUtil._
@@ -20,17 +18,84 @@ class RecorderPlaybackServiceTest extends FunSpec with Matchers with ScalaFuture
   it("fetches a single RecorderWorkEntry") {
     val recorderWorkEntry = createRecorderWorkEntryWith(version = 1)
 
-    withLocalS3Bucket { bucket =>
-      withLocalDynamoDbTable { table =>
-        withRecorderVHS(bucket, table) { vhs =>
-          storeInVHS(vhs, recorderWorkEntry = recorderWorkEntry)
+    withRecorderVHS { vhs =>
+      storeInVHS(vhs, recorderWorkEntry = recorderWorkEntry)
 
-          val service = new RecorderPlaybackService(vhs)
+      val service = new RecorderPlaybackService(vhs)
 
-          whenReady(service.fetchAllRecorderWorkEntries(getWorkIdentifiers(recorderWorkEntry))) { result =>
-            result shouldBe List(Some(recorderWorkEntry))
-          }
-        }
+      whenReady(service.fetchAllRecorderWorkEntries(getWorkIdentifiers(recorderWorkEntry))) { result =>
+        result shouldBe List(Some(recorderWorkEntry))
+      }
+    }
+  }
+
+  it("throws an error if asked to fetch a missing entry") {
+    val recorderWorkEntry = createRecorderWorkEntryWith(version = 1)
+
+    withRecorderVHS { vhs =>
+      val service = new RecorderPlaybackService(vhs)
+
+      whenReady(service.fetchAllRecorderWorkEntries(getWorkIdentifiers(recorderWorkEntry)).failed) { result =>
+        result shouldBe a[NoSuchElementException]
+        result.getMessage shouldBe s"Work ${recorderWorkEntry.id} is not in VHS!"
+      }
+    }
+  }
+
+  it("returns None if asked to fetch a RecorderWorkEntry with version 0") {
+    val recorderWorkEntry = createRecorderWorkEntryWith(version = 0)
+
+    withRecorderVHS { vhs =>
+      val service = new RecorderPlaybackService(vhs)
+
+      whenReady(service.fetchAllRecorderWorkEntries(getWorkIdentifiers(recorderWorkEntry))) { result =>
+        result shouldBe List(None)
+      }
+    }
+  }
+
+  it("returns None if the version in VHS has a higher version") {
+    val work = createUnidentifiedWorkWith(version = 2)
+    val recorderWorkEntry = RecorderWorkEntry(work)
+
+    val workToStore = createUnidentifiedWorkWith(
+      sourceIdentifier = work.sourceIdentifier,
+      version = work.version + 1
+    )
+
+    withRecorderVHS { vhs =>
+      storeInVHS(vhs, recorderWorkEntry = RecorderWorkEntry(workToStore))
+
+      val service = new RecorderPlaybackService(vhs)
+
+      whenReady(service.fetchAllRecorderWorkEntries(getWorkIdentifiers(recorderWorkEntry))) { result =>
+        result shouldBe List(None)
+      }
+    }
+  }
+
+  it("gets a mixture of works as appropriate") {
+    val workEntriesToFetch = (1 to 3).map { _ => createRecorderWorkEntryWith(version = 1) }
+
+    val outdatedWorks = (4 to 5).map { _ => createUnidentifiedWorkWith(version = 1) }
+    val outdatedWorkEntriesToFetch = outdatedWorks.map { work => RecorderWorkEntry(work) }
+    val updatedWorks = outdatedWorks.map { work => work.copy(version = work.version + 1) }
+    val updatedWorkEntriesToStore = updatedWorks.map { work => RecorderWorkEntry(work) }
+
+    val zeroWorkEntries = (6 to 7).map { _ => createRecorderWorkEntryWith(version = 0) }
+
+    val allWorkEntries = (workEntriesToFetch ++ outdatedWorkEntriesToFetch ++ zeroWorkEntries).toList
+
+    withRecorderVHS { vhs =>
+      storeInVHS(
+        vhs,
+        entries = (workEntriesToFetch ++ updatedWorkEntriesToStore ++ zeroWorkEntries).toList
+      )
+
+      val service = new RecorderPlaybackService(vhs)
+
+      whenReady(service.fetchAllRecorderWorkEntries(getWorkIdentifiers(allWorkEntries: _*))) { result =>
+        result shouldBe (workEntriesToFetch ++ (4 to 7).map { _ => None }).toList
       }
     }
   }
@@ -43,12 +108,16 @@ class RecorderPlaybackServiceTest extends FunSpec with Matchers with ScalaFuture
       )}
       .toList
 
-  private def withRecorderVHS[R](bucket: Bucket, table: Table)(
+  private def withRecorderVHS[R](
     testWith: TestWith[VersionedHybridStore[RecorderWorkEntry,
                                             EmptyMetadata,
                                             ObjectStore[RecorderWorkEntry]], R]): R = {
-    withTypeVHS[RecorderWorkEntry, EmptyMetadata, R](bucket, table) {
-      vhs => testWith(vhs)
+    withLocalS3Bucket { bucket =>
+      withLocalDynamoDbTable { table =>
+        withTypeVHS[RecorderWorkEntry, EmptyMetadata, R](bucket, table) {
+          vhs => testWith(vhs)
+        }
+      }
     }
   }
 }
