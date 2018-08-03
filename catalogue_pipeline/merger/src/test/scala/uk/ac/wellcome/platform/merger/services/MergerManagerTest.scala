d45265c9739b6a1c997694337ba4a963dde149d1
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-03 10:38:41 +0100

    Add a couple of MergerManager tests

---
+++ catalogue_pipeline/merger/src/test/scala/uk/ac/wellcome/platform/merger/services/MergerManagerTest.scala
@@ -0,0 +1,55 @@
+package uk.ac.wellcome.platform.merger.services
+
+import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.models.recorder.internal.RecorderWorkEntry
+import uk.ac.wellcome.models.work.internal.{BaseWork, IdentifiableRedirect, UnidentifiedRedirectedWork, UnidentifiedWork}
+import uk.ac.wellcome.platform.merger.MergerTestUtils
+
+class MergerManagerTest extends FunSpec with Matchers with MergerTestUtils {
+
+  it("performs a merge with a single work") {
+    val workEntry = createRecorderWorkEntry
+
+    val result = MergerManager.applyMerge(
+      maybeWorkEntries = List(Some(workEntry)),
+      process = mergeTailIntoHead
+    )
+
+    result shouldBe List(workEntry.work)
+  }
+
+  it("performs a merge with multiple works") {
+    val workEntry = createRecorderWorkEntry
+    val otherEntries = (1 to 3).map { _ => createRecorderWorkEntry }
+
+    val workEntries = (workEntry +: otherEntries).map { Some(_) }.toList
+
+    val result = MergerManager.applyMerge(
+      maybeWorkEntries = workEntries,
+      process = mergeTailIntoHead
+    )
+
+    result.head shouldBe workEntry.work
+
+    result.tail.zip(otherEntries).map {
+      case (baseWork: BaseWork, workEntry: RecorderWorkEntry) =>
+        baseWork.sourceIdentifier shouldBe workEntry.work.sourceIdentifier
+
+        val redirect: IdentifiableRedirect = baseWork.asInstanceOf[IdentifiableRedirect]
+        redirect.sourceIdentifier shouldBe workEntry.work.sourceIdentifier
+    }
+
+  }
+
+  /** Make every work a redirect to the first work in the list, and leave
+    * the first work intact.
+    */
+  private def mergeTailIntoHead(works: Seq[UnidentifiedWork]): Seq[BaseWork] =
+    works.head +: works.tail.map { work =>
+      UnidentifiedRedirectedWork(
+        sourceIdentifier = work.sourceIdentifier,
+        version = work.version,
+        redirect = IdentifiableRedirect(works.head.sourceIdentifier)
+      )
+    }
+}
