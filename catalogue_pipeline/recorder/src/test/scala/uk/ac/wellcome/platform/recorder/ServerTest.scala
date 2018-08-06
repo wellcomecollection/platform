e78943ad611d4aa049bda805e55037090c148c02
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-06 13:30:13 +0100

    Fix the recorder tests (how did these ever work??)

--- catalogue_pipeline/recorder/src/test/scala/uk/ac/wellcome/platform/recorder/ServerTest.scala
+++ catalogue_pipeline/recorder/src/test/scala/uk/ac/wellcome/platform/recorder/ServerTest.scala
@@ -2,20 +2,20 @@ package uk.ac.wellcome.platform.recorder
 
 import com.twitter.finagle.http.Status._
 import org.scalatest.FunSpec
-import uk.ac.wellcome.messaging.test.fixtures.SQS
+import uk.ac.wellcome.messaging.test.fixtures.Messaging
 import uk.ac.wellcome.storage.fixtures.LocalVersionedHybridStore
 
 class ServerTest
     extends FunSpec
     with fixtures.Server
     with LocalVersionedHybridStore
-    with SQS {
+    with Messaging {
 
   it("shows the healthcheck message") {
     withLocalSqsQueue { queue =>
       withLocalS3Bucket { bucket =>
         withLocalDynamoDbTable { table =>
-          val flags = sqsLocalFlags(queue) ++ vhsLocalFlags(bucket, table)
+          val flags = messageReaderLocalFlags(bucket, queue) ++ vhsLocalFlags(bucket, table)
           withServer(flags) { server =>
             server.httpGet(
               path = "/management/healthcheck",
