50c14a2d9316b68429e1afa69da7adfcd11c9bb1
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-06 10:19:28 +0100

    Tidy up test descriptions in this transformer test

--- catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/platform/transformer/transformers/MiroTransformableTransformerTitleTest.scala
+++ catalogue_pipeline/transformer/src/test/scala/uk/ac/wellcome/platform/transformer/transformers/MiroTransformableTransformerTitleTest.scala
@@ -21,133 +21,112 @@ class MiroTransformableTransformerTitleTest
     )
   }
 
-  it("""
-    should use the title field in the V collection if the description field
-    is absent
-  """) {
-    val title = "A limerick about a lemming"
-    transformRecordAndCheckTitle(
-      data = s""""image_title": "$title"""",
-      expectedTitle = title,
-      miroCollection = "Images-V"
-    )
+  describe("titles on V images")
+    it("uses image_title if image_image_desc is absent") {
+      val title = "A limerick about a lemming"
+      transformRecordAndCheckTitle(
+        data = s""""image_title": "$title"""",
+        expectedTitle = title,
+        miroCollection = "Images-V"
+      )
+    }
+
+    it("uses image_title if image_title is not a prefix of image_image_desc") {
+      val title = "A tome about a turtle"
+      val description = "A story of a starfish"
+      transformRecordAndCheckTitle(
+        data = s"""
+          "image_title": "$title",
+          "image_image_desc": "$description"
+        """,
+        expectedTitle = title,
+        expectedDescription = Some(description),
+        miroCollection = "Images-V"
+      )
+    }
+
+    it("uses image_image_desc if image_title is a prefix of image_image_desc (one-line description)") {
+      val title = "An icon of an iguana"
+      val description = "An icon of an iguana is an intriguing image"
+      transformRecordAndCheckTitle(
+        data = s"""
+          "image_title": "$title",
+          "image_image_desc": "$description"
+        """,
+        expectedTitle = description,
+        expectedDescription = None,
+        miroCollection = "Images-V"
+      )
+    }
+
+    it("uses image_image_desc if image_title is a prefix of image_image_desc (multi-line description)") {
+      val title = "An icon of an iguana"
+      val longTitle = "An icon of an iguana is an intriguing image"
+      val descriptionBody = "Woodcut, by A.R. Tist.  Italian.  1897."
+      val description = s"$longTitle\\n\\n$descriptionBody"
+      transformRecordAndCheckTitle(
+        data = s"""
+          "image_title": "$title",
+          "image_image_desc": "$description"
+        """,
+        expectedTitle = longTitle,
+        expectedDescription = Some(descriptionBody),
+        miroCollection = "Images-V"
+      )
+    }
   }
 
-  it("""
-    should use the image_title field as the title on a V image if the
-    image_title is not a prefix of image_image_desc
-  """) {
-    val title = "A tome about a turtle"
-    val description = "A story of a starfish"
-    transformRecordAndCheckTitle(
-      data = s"""
-        "image_title": "$title",
-        "image_image_desc": "$description"
-      """,
-      expectedTitle = title,
-      expectedDescription = Some(description),
-      miroCollection = "Images-V"
-    )
-  }
+  describe("using the image_image_desc_academic if image_image_desc is unhelpful") {
+    it("mixed hyphens in title/description") {
+      val academicDescription = "An alibi for an academic"
+      transformRecordAndCheckTitle(
+        data = s"""
+          "image_title": "-",
+          "image_image_desc": "--",
+          "image_image_desc_academic": "$academicDescription"
+        """,
+        expectedTitle = academicDescription
+      )
+    }
 
-  it("""
-    should use the first line of image_image_desc as the title on a V image
-    if image_title is a prefix of said first line, and omit a description
-    entirely (one-line description)
-  """) {
-    val title = "An icon of an iguana"
-    val description = "An icon of an iguana is an intriguing image"
-    transformRecordAndCheckTitle(
-      data = s"""
-        "image_title": "$title",
-        "image_image_desc": "$description"
-      """,
-      expectedTitle = description,
-      expectedDescription = None,
-      miroCollection = "Images-V"
-    )
-  }
+    it("single hyphen in title/description") {
+      val academicDescription = "Using an upside-down umbrella"
+      transformRecordAndCheckTitle(
+        data = s"""
+          "image_title": "-",
+          "image_image_desc": "-",
+          "image_image_desc_academic": "$academicDescription"
+        """,
+        expectedTitle = academicDescription
+      )
+    }
 
-  it("""
-    should use the first line of image_image_desc as the title on a V image
-    if image_title is a prefix of said first line (multi-line description)
-  """) {
-    val title = "An icon of an iguana"
-    val longTitle = "An icon of an iguana is an intriguing image"
-    val descriptionBody = "Woodcut, by A.R. Tist.  Italian.  1897."
-    val description = s"$longTitle\\n\\n$descriptionBody"
-    transformRecordAndCheckTitle(
-      data = s"""
-        "image_title": "$title",
-        "image_image_desc": "$description"
-      """,
-      expectedTitle = longTitle,
-      expectedDescription = Some(descriptionBody),
-      miroCollection = "Images-V"
-    )
-  }
+    it("double hyphens in title/description") {
+      val academicDescription = "Dirty doubling of dastardly data"
+      transformRecordAndCheckTitle(
+        data = s"""
+          "image_title": "--",
+          "image_image_desc": "--",
+          "image_image_desc_academic": "$academicDescription"
+        """,
+        expectedTitle = academicDescription
+      )
+    }
 
-  it("""
-    should use the image_image_desc_academic if the image_image_desc field
-    doesn't contain useful data (one-line description)
-  """) {
-    val academicDescription = "An alibi for an academic"
-    transformRecordAndCheckTitle(
-      data = s"""
-        "image_title": "-",
-        "image_image_desc": "--",
-        "image_image_desc_academic": "$academicDescription"
-      """,
-      expectedTitle = academicDescription
-    )
-  }
-
-  it("""
-    should use the image_image_desc_academic if the image_image_desc field
-    doesn't contain useful data (single hyphen in the description)
-  """) {
-    val academicDescription = "Using an upside-down umbrella"
-    transformRecordAndCheckTitle(
-      data = s"""
-        "image_title": "-",
-        "image_image_desc": "-",
-        "image_image_desc_academic": "$academicDescription"
-      """,
-      expectedTitle = academicDescription
-    )
-  }
-
-  it("""
-    should use the image_image_desc_academic if the image_image_desc field
-    doesn't contain useful data (double hyphen in title and description)
-  """) {
-    val academicDescription = "Dirty doubling of dastardly data"
-    transformRecordAndCheckTitle(
-      data = s"""
-        "image_title": "--",
-        "image_image_desc": "--",
-        "image_image_desc_academic": "$academicDescription"
-      """,
-      expectedTitle = academicDescription
-    )
-  }
-
-  it("""
-    should use the image_image_desc_academic if the image_image_desc field
-    doesn't contain useful data (multi-line description)
-  """) {
-    val academicLabel = "A lithograph of a lecturer"
-    val academicBody = "The corpus of a chancellor"
-    val academicDescription = s"$academicLabel\\n\\n$academicBody"
-    transformRecordAndCheckTitle(
-      data = s"""
-        "image_title": "-",
-        "image_image_desc": "--",
-        "image_image_desc_academic": "$academicDescription"
-      """,
-      expectedTitle = academicLabel,
-      expectedDescription = Some(academicBody)
-    )
+    it("multi-line academic description") {
+      val academicLabel = "A lithograph of a lecturer"
+      val academicBody = "The corpus of a chancellor"
+      val academicDescription = s"$academicLabel\\n\\n$academicBody"
+      transformRecordAndCheckTitle(
+        data = s"""
+          "image_title": "-",
+          "image_image_desc": "--",
+          "image_image_desc_academic": "$academicDescription"
+        """,
+        expectedTitle = academicLabel,
+        expectedDescription = Some(academicBody)
+      )
+    }
   }
 
   it("uses image_image_desc if image_title is None") {
