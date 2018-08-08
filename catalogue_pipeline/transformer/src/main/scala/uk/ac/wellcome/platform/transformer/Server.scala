cd042af1aa842b15dd966660b9b226852e2fe527
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-08 12:14:21 +0100

    Add type parameters to our module

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/Server.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/Server.scala
@@ -18,7 +18,7 @@ import uk.ac.wellcome.finatra.messaging.{
 }
 import uk.ac.wellcome.finatra.monitoring.MetricsSenderModule
 import uk.ac.wellcome.finatra.storage.{S3ClientModule, S3ConfigModule}
-import uk.ac.wellcome.platform.transformer.modules.{TransformerWorkerModule, _}
+import uk.ac.wellcome.platform.transformer.modules._
 
 object ServerMain extends Server
 
@@ -31,7 +31,6 @@ class Server extends HttpServer {
     SQSClientModule,
     SQSConfigModule,
     SNSClientModule,
-    TransformerWorkerModule,
     ExecutionContextModule,
     TransformablesModule,
     S3ClientModule,
--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/modules/TransformablesModule.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/modules/TransformablesModule.scala
@@ -1,8 +1,10 @@
 package uk.ac.wellcome.platform.transformer.modules
 
+import akka.actor.{ActorSystem, Terminated}
 import com.google.inject.{Provides, Singleton}
 import com.twitter.inject.{Injector, TwitterModule}
 import io.circe.{KeyDecoder, KeyEncoder}
+import uk.ac.wellcome.finatra.akka.AkkaModule
 import uk.ac.wellcome.models.transformable.sierra.SierraItemNumber
 import uk.ac.wellcome.models.transformable.{
   MiroTransformable,
@@ -14,15 +16,14 @@ import uk.ac.wellcome.platform.transformer.models.TransformerSourceNames._
 import uk.ac.wellcome.storage.ObjectStore
 import uk.ac.wellcome.storage.s3.S3StorageBackend
 import uk.ac.wellcome.json.JsonUtil._
-import uk.ac.wellcome.platform.transformer.transformers.{
-  MiroTransformableTransformer,
-  SierraTransformableTransformer,
-  TransformableTransformer
-}
+import uk.ac.wellcome.platform.transformer.services.TransformerWorkerService
+import uk.ac.wellcome.platform.transformer.transformers.{MiroTransformableTransformer, SierraTransformableTransformer, TransformableTransformer}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 object TransformablesModule extends TwitterModule {
+  override val modules = Seq(AkkaModule)
+
   private val sourceNameFlag = flag[String](
     "transformer.sourceName", "Name of the transformer source")
 
@@ -64,4 +65,20 @@ object TransformablesModule extends TwitterModule {
       case TransformerSourceNames.miro   => new MiroTransformableTransformer
       case TransformerSourceNames.sierra => new SierraTransformableTransformer
     }
+
+  def singletonStartup(injector: Injector, transformerConfig: TransformerConfig): Unit =
+    super.singletonStartup(injector)
+
+  def singletonShutdown(injector: Injector, transformerConfig: TransformerConfig): Future[Terminated] = {
+    info("Terminating SQS worker")
+
+    val system = injector.instance[ActorSystem]
+    val workerService = transformerConfig.sourceName match {
+      case TransformerSourceNames.miro   => injector.instance[TransformerWorkerService[MiroTransformable]]
+      case TransformerSourceNames.sierra => injector.instance[TransformerWorkerService[SierraTransformable]]
+    }
+
+    workerService.stop()
+    system.terminate()
+  }
 }
