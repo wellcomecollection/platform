bde5abac6482a0bfef8e682071531afed2155bd8
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-08 11:48:03 +0100

    Add a provider for getting the transformer source name

--- catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/modules/TransformablesModule.scala
+++ catalogue_pipeline/transformer/src/main/scala/uk/ac/wellcome/platform/transformer/modules/TransformablesModule.scala
@@ -8,6 +8,8 @@ import uk.ac.wellcome.models.transformable.{
   MiroTransformable,
   SierraTransformable
 }
+import uk.ac.wellcome.platform.transformer.models.TransformerConfig
+import uk.ac.wellcome.platform.transformer.models.TransformerSourceNames._
 import uk.ac.wellcome.storage.ObjectStore
 import uk.ac.wellcome.storage.s3.S3StorageBackend
 import uk.ac.wellcome.json.JsonUtil._
@@ -15,6 +17,23 @@ import uk.ac.wellcome.json.JsonUtil._
 import scala.concurrent.ExecutionContext
 
 object TransformablesModule extends TwitterModule {
+  private val sourceNameFlag = flag[String](
+    "transformer.sourceName", "Name of the transformer source")
+
+  @Provides
+  @Singleton
+  def providesTransformerConfig(): TransformerConfig = {
+    val sourceName = sourceNameFlag() match {
+      case s: String if s == miro.toString  => miro
+      case s: String if s == sierra.toString => sierra
+      case s: String =>
+        throw new IllegalArgumentException(
+          s"$s is not a valid transformer source name")
+    }
+
+    TransformerConfig(sourceName = sourceName)
+  }
+
   @Provides
   @Singleton
   def provideMiroTransformableObjectStore(
