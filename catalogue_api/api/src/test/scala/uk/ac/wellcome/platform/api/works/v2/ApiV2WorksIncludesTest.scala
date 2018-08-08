50e4e5160d64fae95d68c95535c2d7a07dc74d01
alicefuzier <alicefuzier@gmail.com>
2018-08-08 11:31:14 +0100

    Test that genres include is allowed

--- catalogue_api/api/src/test/scala/uk/ac/wellcome/platform/api/works/v2/ApiV2WorksIncludesTest.scala
+++ catalogue_api/api/src/test/scala/uk/ac/wellcome/platform/api/works/v2/ApiV2WorksIncludesTest.scala
@@ -2,7 +2,7 @@ package uk.ac.wellcome.platform.api.works.v2
 
 import com.twitter.finagle.http.Status
 import com.twitter.finatra.http.EmbeddedHttpServer
-import uk.ac.wellcome.models.work.internal.{Concept, Subject, Unidentifiable}
+import uk.ac.wellcome.models.work.internal.{Concept, Genre, Subject, Unidentifiable}
 
 class ApiV2WorksIncludesTest extends ApiV2WorksTestBase {
   it(
@@ -192,6 +192,87 @@ class ApiV2WorksIncludesTest extends ApiV2WorksTestBase {
                  |     "contributors": [ ],
                  |     "subjects": [ ${subjects(subject)}],
                  |     "genres": [ ],
+                 |     "production": [ ]
+                 |   }
+          """.stripMargin
+          )
+        }
+    }
+  }
+
+  it(
+    "includes a list of genres on a list endpoint if we pass ?include=genres") {
+    withV2Api {
+      case (apiPrefix, _, indexNameV2, itemType, server: EmbeddedHttpServer) =>
+        val works = createIdentifiedWorks(count = 2).sortBy { _.canonicalId }
+
+
+        val genres1 = List(Genre("ornitology", List(Unidentifiable(Concept("ornitology")))))
+        val genres2 = List(Genre("flying cars", List(Unidentifiable(Concept("flying cars")))))
+        val work0 = works(0).copy(genres = genres1)
+        val work1 = works(1).copy(genres = genres2)
+
+        insertIntoElasticsearch(indexNameV2, itemType, work0, work1)
+
+        eventually {
+          server.httpGet(
+            path = s"/$apiPrefix/works?include=genres",
+            andExpect = Status.Ok,
+            withJsonBody =
+              s"""
+                 |{
+                 |  ${resultList(apiPrefix, totalResults = 2)},
+                 |  "results": [
+                 |   {
+                 |     "type": "Work",
+                 |     "id": "${work0.canonicalId}",
+                 |     "title": "${work0.title}",
+                 |     "contributors": [ ],
+                 |     "genres": [ ${genres(genres1)}],
+                 |     "subjects": [ ],
+                 |     "production": [ ]
+                 |   },
+                 |   {
+                 |     "type": "Work",
+                 |     "id": "${work1.canonicalId}",
+                 |     "title": "${work1.title}",
+                 |     "contributors": [ ],
+                 |     "genres": [ ${genres(genres2)}],
+                 |     "subjects": [ ],
+                 |     "production": [ ]
+                 |   }
+                 |  ]
+                 |}
+          """.stripMargin
+          )
+        }
+    }
+  }
+
+  it(
+    "includes a list of genres on a single work endpoint if we pass ?include=genres") {
+    withV2Api {
+      case (apiPrefix, _, indexNameV2, itemType, server: EmbeddedHttpServer) =>
+
+        val genre = List(Genre("ornitology", List(Unidentifiable(Concept("ornitology")))))
+        val work = createIdentifiedWork.copy(genres = genre)
+
+        insertIntoElasticsearch(indexNameV2, itemType, work)
+
+        eventually {
+          server.httpGet(
+            path = s"/$apiPrefix/works/${work.canonicalId}?include=genres",
+            andExpect = Status.Ok,
+            withJsonBody =
+              s"""
+                 |{
+                 |"@context": "https://localhost:8888/$apiPrefix/context.json",
+                 |     "type": "Work",
+                 |     "id": "${work.canonicalId}",
+                 |     "title": "${work.title}",
+                 |     "contributors": [ ],
+                 |     "genres": [ ${genres(genre)}],
+                 |     "subjects": [ ],
                  |     "production": [ ]
                  |   }
           """.stripMargin
