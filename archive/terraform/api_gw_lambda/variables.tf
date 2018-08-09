316527f0f6103fa1d4f897a81381e9a749a146c1
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-08-09 15:41:52 +0100

    Puts API gateway lambdas into their own module

---
+++ archive/terraform/api_gw_lambda/variables.tf
@@ -0,0 +1,15 @@
+variable "name" {}
+variable "description" {}
+
+variable "timeout" {
+  default = "60"
+}
+variable "memory_size" {
+  default = "1024"
+}
+variable "environment_variables" {
+  type = "map"
+  default = {}
+}
+variable "lambda_error_alarm_arn" {}
+variable "infra_bucket" {}
---
+++ archive/terraform/lambdas.tf
@@ -0,0 +1,9 @@
+module "archive_asset_loookup" {
+  source = "api_gw_lambda"
+
+  name = "archive_asset_loookup"
+  description = "Looks up archived assets."
+
+  lambda_error_alarm_arn = "${var.lambda_error_alarm_arn}"
+  infra_bucket = "${var.infra_bucket}"
+}
