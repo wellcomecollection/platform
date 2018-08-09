2365f045434a8416098d00dbcb10065466bd3e4f
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-08-09 15:44:21 +0100

    fmt

--- archive/terraform/api_gw_lambda/lambda.tf
+++ archive/terraform/api_gw_lambda/lambda.tf
@@ -1,8 +1,8 @@
 resource "aws_lambda_permission" "archive_asset_lookup_apigw" {
-  statement_id = "AllowExecutionFromAPIGateway"
-  action = "lambda:InvokeFunction"
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
   function_name = "${module.lambda.function_name}"
-  principal = "apigateway.amazonaws.com"
+  principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_api_gateway_deployment.test.execution_arn}/*/*"
 }
@@ -10,18 +10,18 @@ resource "aws_lambda_permission" "archive_asset_lookup_apigw" {
 module "lambda" {
   source = "git::https://github.com/wellcometrust/terraform.git//lambda?ref=v11.5.0"
 
-  name = "${var.name}"
+  name        = "${var.name}"
   module_name = "${var.name}"
   description = "${var.description}"
 
-  timeout = "${var.timeout}"
+  timeout     = "${var.timeout}"
   memory_size = "${var.memory_size}"
 
   environment_variables = "${var.environment_variables}"
 
   alarm_topic_arn = "${var.lambda_error_alarm_arn}"
-  s3_bucket = "${var.infra_bucket}"
-  s3_key = "lambdas/archive/archive_asset_lookup.zip"
+  s3_bucket       = "${var.infra_bucket}"
+  s3_key          = "lambdas/archive/archive_asset_lookup.zip"
 
   log_retention_in_days = 30
 }
--- archive/terraform/api_gw_lambda/variables.tf
+++ archive/terraform/api_gw_lambda/variables.tf
@@ -4,12 +4,15 @@ variable "description" {}
 variable "timeout" {
   default = "60"
 }
+
 variable "memory_size" {
   default = "1024"
 }
+
 variable "environment_variables" {
-  type = "map"
+  type    = "map"
   default = {}
 }
+
 variable "lambda_error_alarm_arn" {}
 variable "infra_bucket" {}
