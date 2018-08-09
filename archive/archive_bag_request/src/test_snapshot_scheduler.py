c9327093f94731bdfb7bf426938d2210df315510
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-08-09 15:45:33 +0100

    Adds archive_bag_request skeleton

---
+++ archive/archive_bag_request/src/test_snapshot_scheduler.py
@@ -0,0 +1,5 @@
+# -*- encoding: utf-8 -*-
+
+
+def test_passes():
+    assert True == True
--- archive/terraform/lambdas.tf
+++ archive/terraform/lambdas.tf
@@ -7,3 +7,13 @@ module "archive_asset_loookup" {
   lambda_error_alarm_arn = "${var.lambda_error_alarm_arn}"
   infra_bucket           = "${var.infra_bucket}"
 }
+
+module "archive_bag_request" {
+  source = "api_gw_lambda"
+
+  name        = "archive_bag_request"
+  description = "Requests bag ingests."
+
+  lambda_error_alarm_arn = "${var.lambda_error_alarm_arn}"
+  infra_bucket           = "${var.infra_bucket}"
+}
