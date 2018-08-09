b2544b608bde60ae8230fc7bf4c5335022cc40e8
mklander <mklanderyou@gmail.com>
2018-08-09 11:58:25 +0100

    separate archiver

---
+++ archive/archiver/src/main/scala/uk/ac/wellcome/platform/archiver/Archiver.scala
@@ -0,0 +1,40 @@
+package uk.ac.wellcome.platform.archiver
+
+import akka.actor.ActorSystem
+import akka.event.{Logging, LoggingAdapter}
+import akka.stream.ActorMaterializer
+import akka.stream.alpakka.s3.scaladsl.S3Client
+import akka.stream.scaladsl.Flow
+import com.google.inject.Injector
+import grizzled.slf4j.Logging
+import uk.ac.wellcome.messaging.sns.NotificationMessage
+import uk.ac.wellcome.platform.archiver.flow.{DownloadNotificationFlow, DownloadZipFlow, VerifiedBagUploaderFlow}
+import uk.ac.wellcome.platform.archiver.messaging.MessageStream
+import uk.ac.wellcome.platform.archiver.models.BagUploaderConfig
+import uk.ac.wellcome.json.JsonUtil._
+
+trait Archiver extends Logging {
+  val injector: Injector
+
+  def run() = {
+    val messageStream =
+      injector.getInstance(classOf[MessageStream[NotificationMessage, Object]])
+    val bagUploaderConfig = injector.getInstance(classOf[BagUploaderConfig])
+
+    implicit val s3Client: S3Client = injector.getInstance(classOf[S3Client])
+    implicit val actorSystem: ActorSystem = injector.getInstance(classOf[ActorSystem])
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    implicit val adapter: LoggingAdapter = Logging(actorSystem.eventStream, "customLogger")
+
+    val workFlow = Flow[NotificationMessage]
+      .log("notification")
+      .via(DownloadNotificationFlow())
+      .log("download notice")
+      .via(DownloadZipFlow())
+      .log("download zip")
+      .via(VerifiedBagUploaderFlow(bagUploaderConfig))
+      .log("archive verified")
+
+    messageStream.run("archiver", workFlow)
+  }
+}
--- archive/archiver/src/main/scala/uk/ac/wellcome/platform/archiver/Main.scala
+++ archive/archiver/src/main/scala/uk/ac/wellcome/platform/archiver/Main.scala
@@ -1,21 +1,6 @@
 package uk.ac.wellcome.platform.archiver
 
-import akka.actor.ActorSystem
-import akka.event.Logging
-import akka.stream.ActorMaterializer
-import akka.stream.alpakka.s3.scaladsl.S3Client
-import akka.stream.scaladsl.Flow
 import com.google.inject.{Guice, Injector}
-import grizzled.slf4j.Logging
-import uk.ac.wellcome.json.JsonUtil._
-import uk.ac.wellcome.messaging.sns.NotificationMessage
-import uk.ac.wellcome.platform.archiver.flow.{
-  DownloadNotificationFlow,
-  DownloadZipFlow,
-  VerifiedBagUploaderFlow
-}
-import uk.ac.wellcome.platform.archiver.messaging.MessageStream
-import uk.ac.wellcome.platform.archiver.models.BagUploaderConfig
 import uk.ac.wellcome.platform.archiver.modules._
 
 import scala.concurrent.Await
@@ -42,36 +27,5 @@ object Main extends App with Archiver {
       error("Fatal error:", e)
   } finally {
     info(s"Terminating worker.")
-  }
-}
-
-trait Archiver extends Logging {
-  val injector: Injector
-
-  def run() = {
-
-    val messageStream =
-      injector.getInstance(classOf[MessageStream[NotificationMessage, Object]])
-    val bagUploaderConfig = injector.getInstance(classOf[BagUploaderConfig])
-
-    implicit val s3Client = injector.getInstance(classOf[S3Client])
-    implicit val actorSystem = injector.getInstance(classOf[ActorSystem])
-    implicit val materializer = ActorMaterializer()
-    implicit val adapter = Logging(actorSystem.eventStream, "customLogger")
-
-    val downloadNotificationFlow = DownloadNotificationFlow()
-    val downloadZipFlow = DownloadZipFlow()
-    val verifiedBagUploaderFlow = VerifiedBagUploaderFlow(bagUploaderConfig)
-
-    val workFlow = Flow[NotificationMessage]
-      .log("notification")
-      .via(downloadNotificationFlow)
-      .log("download notice")
-      .via(downloadZipFlow)
-      .log("download zip")
-      .via(verifiedBagUploaderFlow)
-      .log("archive verified")
-
-    messageStream.run("archiver", workFlow)
   }
 }
