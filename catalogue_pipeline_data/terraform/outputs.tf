3e6f3799ae359f9031fe3e2d82c6f50414ade7b1
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-13 12:13:18 +0100

    Move the outputs to the data stack

---
+++ catalogue_pipeline_data/terraform/outputs.tf
@@ -0,0 +1,51 @@
+output "vhs_sourcedata_full_access_policy" {
+  value = "${module.vhs_sourcedata.full_access_policy}"
+}
+
+output "vhs_sourcedata_dynamodb_update_policy" {
+  value = "${module.vhs_sourcedata.dynamodb_update_policy}"
+}
+
+output "vhs_sourcedata_table_name" {
+  value = "${module.vhs_sourcedata.table_name}"
+}
+
+output "vhs_sourcedata_table_stream_arn" {
+  value = "${module.vhs_sourcedata.table_stream_arn}"
+}
+
+output "vhs_sourcedata_bucket_name" {
+  value = "${module.vhs_sourcedata.bucket_name}"
+}
+
+output "vhs_sierra_full_access_policy" {
+  value = "${module.vhs_sierra.full_access_policy}"
+}
+
+output "vhs_sierra_dynamodb_update_policy" {
+  value = "${module.vhs_sierra.dynamodb_update_policy}"
+}
+
+output "vhs_sierra_table_name" {
+  value = "${module.vhs_sierra.table_name}"
+}
+
+output "vhs_sierra_table_stream_arn" {
+  value = "${module.vhs_sierra.table_stream_arn}"
+}
+
+output "vhs_sierra_bucket_name" {
+  value = "${module.vhs_sierra.bucket_name}"
+}
+
+output "vhs_goobi_full_access_policy" {
+  value = "${module.vhs_goobi_mets.full_access_policy}"
+}
+
+output "vhs_goobi_table_name" {
+  value = "${module.vhs_goobi_mets.table_name}"
+}
+
+output "vhs_goobi_bucket_name" {
+  value = "${module.vhs_goobi_mets.bucket_name}"
+}
