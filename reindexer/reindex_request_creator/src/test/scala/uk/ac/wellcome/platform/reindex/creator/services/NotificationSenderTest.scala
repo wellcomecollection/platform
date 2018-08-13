434d8d5d6d89f2be588d17d9af1a5cccddbd20a2
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-13 16:26:18 +0100

    In fact, this test doesn't need patience at all

--- reindexer/reindex_request_creator/src/test/scala/uk/ac/wellcome/platform/reindex/creator/services/NotificationSenderTest.scala
+++ reindexer/reindex_request_creator/src/test/scala/uk/ac/wellcome/platform/reindex/creator/services/NotificationSenderTest.scala
@@ -1,7 +1,7 @@
 r eppackage uk.ac.wellcome.platform.reindex.creator.services
 
 import com.amazonaws.services.sns.model.AmazonSNSException
-import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{FunSpec, Matchers}
 import uk.ac.wellcome.messaging.test.fixtures.SNS
 import uk.ac.wellcome.messaging.test.fixtures.SNS.Topic
@@ -16,7 +16,6 @@ class NotificationSenderTest
     extends FunSpec
     with Matchers
     with ReindexFixtures
-    with IntegrationPatience
     with ScalaFutures
     with IntegrationPatience
     with SNS {
