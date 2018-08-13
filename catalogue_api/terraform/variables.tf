b8665453f45a5e54abf57652f1140d6b2a9b6b64
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-13 15:17:19 +0100

    And flip the API pins

--- catalogue_api/terraform/variables.tf
+++ catalogue_api/terraform/variables.tf
@@ -46,17 +46,17 @@ variable "es_cluster_credentials" {
 
 variable "production_api" {
   description = "Which version of the API is production? (romulus | remus)"
-  default     = "remus"
+  default     = "romulus"
 }
 
 variable "pinned_romulus_api" {
   description = "Which version of the API image to pin romulus to, if any"
-  default     = ""
+  default     = "304a9db0d4db377b953b040386a8cafa9d912d9f"
 }
 
 variable "pinned_romulus_api_nginx-delta" {
   description = "Which version of the nginx API image to pin romulus to, if any"
-  default     = ""
+  default     = "3dd8a423123e1d175dd44520fcf03435a5fc92c8"
 }
 
 variable "pinned_remus_api" {
