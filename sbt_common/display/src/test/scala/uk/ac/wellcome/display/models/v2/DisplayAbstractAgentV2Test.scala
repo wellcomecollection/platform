e4df16ad383ec452bf2e07df33cafe4dd8e53b70
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-15 17:02:58 +0100

    Keep fleshing out those tests!

--- sbt_common/display/src/test/scala/uk/ac/wellcome/display/models/v2/DisplayAbstractAgentV2Test.scala
+++ sbt_common/display/src/test/scala/uk/ac/wellcome/display/models/v2/DisplayAbstractAgentV2Test.scala
@@ -1,33 +1,66 @@
 package uk.ac.wellcome.display.models.v2
 
-import org.scalatest.Assertion
 import org.scalatest.{FunSpec, Matchers}
-import uk.ac.wellcome.models.work.internal.{Agent, Unidentifiable}
+import uk.ac.wellcome.models.work.internal.{Agent, Identified, SourceIdentifier, Unidentifiable}
 import uk.ac.wellcome.models.work.test.util.IdentifiersUtil
 
 class DisplayAbstractAgentV2Test extends FunSpec with Matchers with IdentifiersUtil {
 
+  val label: String = randomAlphanumeric(length = 25)
+  val sourceIdentifier: SourceIdentifier = createSourceIdentifier
+  val otherIdentifiers: List[SourceIdentifier] = (1 to 3).map { _ => createSourceIdentifier }.toList
+  val canonicalId: String = createCanonicalId
+
   describe("Agent") {
-    it("converts an Unidentifiable Agent to an AbstractAgent (includesIdentifiers = true)") {
-      checkUnidentifiableAgent(includesIdentifiers = true)
-    }
+    val unidentifiedAgent = Unidentifiable(Agent(label = label))
 
-    it("converts an Unidentifiable Agent to an AbstractAgent (includesIdentifiers = false)") {
-      checkUnidentifiableAgent(includesIdentifiers = false)
-    }
-  }
+    val identifiedAgent = Identified(
+      canonicalId = canonicalId,
+      sourceIdentifier = sourceIdentifier,
+      otherIdentifiers = otherIdentifiers,
+      agent = Agent(label = label)
+    )
 
-  private def checkUnidentifiableAgent(includesIdentifiers: Boolean): Assertion = {
-    val label = createLabel
-    val agent = Unidentifiable(Agent(label = label))
-    val expectedAgent = DisplayAgentV2(
+    val expectedUnidentifiedAgent: DisplayAgentV2 = DisplayAgentV2(
       id = None,
       identifiers = None,
       label = label
     )
 
-    DisplayAbstractAgentV2(agent, includesIdentifiers = includesIdentifiers) shouldBe expectedAgent
-  }
+    it("converts an Unidentifiable Agent to an AbstractAgent (includesIdentifiers = true)") {
+      DisplayAbstractAgentV2(
+        unidentifiedAgent,
+        includesIdentifiers = true) shouldBe expectedUnidentifiedAgent
+    }
 
-  private def createLabel: String = randomAlphanumeric(length = 25)
+    it("converts an Unidentifiable Agent to an AbstractAgent (includesIdentifiers = false)") {
+      DisplayAbstractAgentV2(
+        unidentifiedAgent,
+        includesIdentifiers = false) shouldBe expectedUnidentifiedAgent
+    }
+
+    it("converts an Identified Agent to an AbstractAgent (includesIdentifiers = true)") {
+      val expectedAgent = DisplayAgentV2(
+        id = Some(canonicalId),
+        identifiers = Some((List(sourceIdentifier) ++ otherIdentifiers).map { DisplayIdentifierV2(_) }),
+        label = label
+      )
+
+      DisplayAbstractAgentV2(
+        identifiedAgent,
+        includesIdentifiers = true) shouldBe expectedAgent
+    }
+
+    it("converts an Identified Agent to an AbstractAgent (includesIdentifiers = false)") {
+      val expectedAgent = DisplayAgentV2(
+        id = Some(canonicalId),
+        identifiers = None,
+        label = label
+      )
+
+      DisplayAbstractAgentV2(
+        identifiedAgent,
+        includesIdentifiers = false) shouldBe expectedAgent
+    }
+  }
 }
