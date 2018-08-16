dbd957c1b3810bcb23c3c9b6ffcc0b42bad1b313
Alex Chan <alex@alexwlchan.net>
2018-08-16 17:23:52 +0100

    Bump the visibility timeout on the reindexer queue

--- reindexer/terraform/queues.tf
+++ reindexer/terraform/queues.tf
@@ -5,23 +5,23 @@ module "reindex_request_creator_queue" {
   account_id  = "${data.aws_caller_identity.current.account_id}"
   topic_names = ["${module.reindex_jobs_topic.name}"]
 
-  # Each reindexer message requires up to 1500 calls to DynamoDB's PutItem API
-  # (one for every record in the shard).  Particularly when the reindexer has
-  # just started, it tends to hit the throughput limits, and the message
-  # hits the DLQ.
+  # The reindex_request_creator scans a source data table to look for
+  # records that need reindexing, and sends each of them as an SNS notification
+  # for the reindex_request_processor.
   #
-  # The long-term fix is to rearchitect the reindexer to behave in a better
-  # way, but for now we have this short-term fix:
+  # If it fails midway through, it rescans and resends *the entire shard* --
+  # leading to significantly more work for the processor!  If it's already
+  # sent a notification for a record, but the processor hasn't reindexed
+  # it yet, it gets resent.
   #
-  #   - Wait 2 minutes before a message can be reprocessed.  If it hits limits
-  #     on the first attempt, the table should have warmed up before it comes
-  #     round again.
-  #   - Retry 50 times per job, so we have to average 30 successful PutItem
-  #     calls per attempt.  This seems more feasible than the default.
+  # To that end, we have a very long visibility timeout on this queue -- so
+  # once it's sent a batch of notifications, the processor has a chance to
+  # pick them up before the creator sends a fresh batch.
   #
-  # TODO: Reduce these limits when we fix the reindexer.
+  # (tl;dr: There is a sensible reason for the timeout to be this long, and
+  #  we're not just trying to evade flakiness.)
   #
-  visibility_timeout_seconds = 120
+  visibility_timeout_seconds = 600
 
   max_receive_count = 50
   alarm_topic_arn   = "${local.dlq_alarm_arn}"
