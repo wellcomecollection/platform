68b351c7172917f5f45b4f013bc71c0a66f68794
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-17 10:54:39 +0100

    Start fleshing out the script

--- data_api/terraform/update_es_index.py
+++ data_api/terraform/update_es_index.py
@@ -5,4 +5,25 @@ This script updates the Terraform configuration in the data_api stack
 to use whatever Elasticsearch index is in use in the catalogue_api.
 """
 
-print('hello world')
+import os
+import subprocess
+
+import hcl
+
+
+ROOT = subprocess.check_output(
+    ['git', 'rev-parse', '--show-toplevel']).decode('utf8').strip()
+
+
+def api_terraform(name):
+    path = os.path.join(ROOT, 'catalogue_api', 'terraform', name)
+    return hcl.loads(open(path).read())
+
+
+if __name__ == '__main__':
+    api_config = api_terraform('api_config.tf')
+    index_config = api_index_config('index_config.tf')
+
+    from pprint import *
+    pprint(api_config)
+    pprint(index_config)
