2609b0915c8ed17142c59653bd2049b512738020
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-20 11:02:23 +0100

    Remove an unused import

--- catalogue_pipeline/matcher/src/main/scala/uk/ac/wellcome/platform/matcher/models/WorkUpdate.scala
+++ catalogue_pipeline/matcher/src/main/scala/uk/ac/wellcome/platform/matcher/models/WorkUpdate.scala
@@ -1,6 +1,6 @@
 package uk.ac.wellcome.platform.matcher.models
 
-import uk.ac.wellcome.models.work.internal.{SourceIdentifier, UnidentifiedWork}
+import uk.ac.wellcome.models.work.internal.UnidentifiedWork
 
 case class WorkUpdate(workId: String,
                       version: Int,
