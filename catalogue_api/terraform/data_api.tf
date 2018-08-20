23bd8ff2e121336a28e3eef0e314ec4fe194f1cd
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-20 17:14:13 +0100

    Pin the snapshot generator releases to match the prod API

--- catalogue_api/terraform/data_api.tf
+++ catalogue_api/terraform/data_api.tf
@@ -4,6 +4,8 @@ locals {
     index_v2 = "${var.production_api == "romulus" ? var.es_config_romulus["index_v2"] : var.es_config_remus["index_v2"]}"
     doc_type = "${var.production_api == "romulus" ? var.es_config_romulus["doc_type"] : var.es_config_remus["doc_type"]}"
   }
+
+  release_id = "${var.production_api == "romulus" ? var.pinned_romulus_api : var.pinned_remus_api}"
 }
 
 module "data_api" {
@@ -11,7 +13,6 @@ module "data_api" {
 
   aws_region   = "${var.aws_region}"
   infra_bucket = "${var.infra_bucket}"
-  release_ids  = "${var.release_ids}"
   key_name     = "${var.key_name}"
 
   data_acm_cert_arn = "${var.data_acm_cert_arn}"
@@ -19,4 +20,6 @@ module "data_api" {
   es_cluster_credentials = "${var.es_cluster_credentials}"
 
   es_config_snapshot = "${local.prod_es_config}"
+
+  snapshot_generator_release_id = "${local.release_id}"
 }
