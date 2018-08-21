60d52b02a607c00d2f672f76b3b80cfafc42ee95
alicefuzier <alicefuzier@gmail.com>
2018-08-21 15:22:54 +0100

    Pin containers

--- sierra_adapter/terraform/bib_merger/locals.tf
+++ sierra_adapter/terraform/bib_merger/locals.tf
@@ -1,4 +1,4 @@
 locals {
   resource_type_singular = "${replace("${var.resource_type}", "s", "")}"
-  container_image        = "${module.ecr_repository_sierra_merger.repository_url}:${var.release_id}"
+  container_image        = "${module.ecr_repository_sierra_merger.repository_url}:20e11e803698059bbec56d73b6d653a2c01c0e50"
 }
--- sierra_adapter/terraform/items_to_dynamo/locals.tf
+++ sierra_adapter/terraform/items_to_dynamo/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  container_image = "${module.ecr_repository_sierra_to_dynamo.repository_url}:${var.release_id}"
+  container_image = "${module.ecr_repository_sierra_to_dynamo.repository_url}:20e11e803698059bbec56d73b6d653a2c01c0e50"
 }
