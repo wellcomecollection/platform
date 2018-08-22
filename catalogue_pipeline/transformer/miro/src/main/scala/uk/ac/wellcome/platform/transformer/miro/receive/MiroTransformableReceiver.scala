f3727374b82e3dc1bd06f8f36b160422ee0f42cb
alicefuzier <alicefuzier@gmail.com>
2018-08-22 10:13:30 +0100

    WIP simplifying MiroTransformableReceiver

--- catalogue_pipeline/transformer/miro/src/main/scala/uk/ac/wellcome/platform/transformer/miro/receive/MiroTransformableReceiver.scala
+++ catalogue_pipeline/transformer/miro/src/main/scala/uk/ac/wellcome/platform/transformer/miro/receive/MiroTransformableReceiver.scala
@@ -4,16 +4,13 @@ import com.amazonaws.services.s3.AmazonS3
 import com.google.inject.Inject
 import com.twitter.inject.Logging
 import io.circe.ParsingFailure
-import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.messaging.message.MessageWriter
-import uk.ac.wellcome.messaging.sns.{NotificationMessage, PublishAttempt}
+import uk.ac.wellcome.messaging.sns.PublishAttempt
 import uk.ac.wellcome.models.transformable.MiroTransformable
 import uk.ac.wellcome.models.work.internal.TransformedBaseWork
 import uk.ac.wellcome.platform.transformer.exceptions.TransformerException
 import uk.ac.wellcome.platform.transformer.miro.MiroTransformableTransformer
 import uk.ac.wellcome.storage.s3.S3Config
-import uk.ac.wellcome.storage.vhs.HybridRecord
-import uk.ac.wellcome.storage.{ObjectLocation, ObjectStore}
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
@@ -23,8 +20,7 @@ class MiroTransformableReceiver @Inject()(
   s3Client: AmazonS3,
   s3Config: S3Config,
   miroTransformableTransformer: MiroTransformableTransformer)(
-  implicit miroTransformableStore: ObjectStore[MiroTransformable],
-  ec: ExecutionContext
+  implicit ec: ExecutionContext
 ) extends Logging {
 
   def receiveMessage(message: MiroTransformable): Future[Unit] = {
@@ -32,7 +28,7 @@ class MiroTransformableReceiver @Inject()(
 
     val futurePublishAttempt = for {
       work <- Future.fromTry(
-        transformTransformable(message, hybridRecord.version))
+        transformTransformable(message, hybridRecord.))
       publishResult <- publishMessage(work)
       _ = debug(
         s"Published work: ${work.sourceIdentifier} with message $publishResult")
@@ -46,17 +42,6 @@ class MiroTransformableReceiver @Inject()(
       }
       .map(_ => ())
 
-  }
-
-  private def getTransformable(
-    hybridRecord: HybridRecord
-  ) = {
-    val s3ObjectLocation = ObjectLocation(
-      namespace = s3Config.bucketName,
-      key = hybridRecord.s3key
-    )
-
-    miroTransformableStore.get(s3ObjectLocation)
   }
 
   private def transformTransformable(
