7b92748fc6d454fb1d70839762a38521a1bf234d
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-22 15:21:26 +0100

    Add a helper for creating instances of a SierraTransformable VHS

--- sierra_adapter/common/src/test/scala/uk/ac/wellcome/sierra_adapter/utils/SierraAdapterHelpers.scala
+++ sierra_adapter/common/src/test/scala/uk/ac/wellcome/sierra_adapter/utils/SierraAdapterHelpers.scala
@@ -11,9 +11,10 @@ import uk.ac.wellcome.storage.dynamo._
 import uk.ac.wellcome.storage.fixtures.LocalDynamoDb.Table
 import uk.ac.wellcome.storage.fixtures.LocalVersionedHybridStore
 import uk.ac.wellcome.storage.fixtures.S3.Bucket
-import uk.ac.wellcome.storage.vhs.{SourceMetadata, VersionedHybridStore}
+import uk.ac.wellcome.storage.vhs.{EmptyMetadata, VersionedHybridStore}
 import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.models.transformable.sierra.SierraItemRecord
+import uk.ac.wellcome.test.fixtures._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -23,12 +24,19 @@ trait SierraAdapterHelpers extends LocalVersionedHybridStore with Messaging {
                                         EmptyMetadata,
                                         ObjectStore[SierraTransformable]]
 
+  def withSierraVHS[R](bucket: Bucket, table: Table)(testWith: TestWith[SierraVHS, R]): R =
+    withTypeVHS[SierraTransformable,
+                EmptyMetadata,
+                R](bucket = bucket, table = table) { vhs =>
+      testWith(vhs)
+    }
+
   def storeInVHS(
     transformable: SierraTransformable,
     hybridStore: SierraVHS): Future[Unit] =
     hybridStore
       .updateRecord(id = transformable.sierraId.withoutCheckDigit)(
-        ifNotExisting = (transformable, SourceMetadata("sierra")))(
+        ifNotExisting = (transformable, EmptyMetadata()))(
         ifExisting = (t, m) =>
           throw new RuntimeException(
             s"Found record ${transformable.sierraId}, but VHS should be empty")
