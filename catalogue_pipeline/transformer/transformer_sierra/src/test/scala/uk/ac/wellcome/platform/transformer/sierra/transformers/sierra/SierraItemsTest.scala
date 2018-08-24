9513373323c218f643a21a0f98fd6ff0810977a5
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-24 15:20:49 +0100

    Rewrite the tests to use getItems()

--- catalogue_pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/sierra/SierraItemsTest.scala
+++ catalogue_pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/sierra/SierraItemsTest.scala
@@ -1,7 +1,9 @@
 package uk.ac.wellcome.platform.transformer.sierra.transformers.sierra
 
 import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.models.transformable.sierra.{SierraBibNumber, SierraItemNumber}
 import uk.ac.wellcome.models.work.internal._
+import uk.ac.wellcome.platform.transformer.sierra.source.{SierraBibData, SierraItemData}
 import uk.ac.wellcome.platform.transformer.sierra.source.sierra.SierraSourceLocation
 import uk.ac.wellcome.platform.transformer.sierra.utils.SierraDataGenerators
 
@@ -9,126 +11,124 @@ class SierraItemsTest extends FunSpec with Matchers with SierraDataGenerators {
 
   val transformer = new Object with SierraItems
 
-  describe("transformItemData") {
-    it("creates both forms of the Sierra ID in 'identifiers'") {
-      val item = createSierraItemData
-      val itemId = createSierraItemNumber
+  it("creates both forms of the Sierra ID in 'identifiers'") {
+    val itemData = createSierraItemData
+    val itemId = createSierraItemNumber
 
-      val sourceIdentifier1 = createSierraSourceIdentifierWith(
-        ontologyType = "Item",
-        value = itemId.withCheckDigit)
+    val sourceIdentifier1 = createSierraSourceIdentifierWith(
+      ontologyType = "Item",
+      value = itemId.withCheckDigit)
 
-      val sourceIdentifier2 = SourceIdentifier(
-        identifierType = IdentifierType("sierra-identifier"),
-        ontologyType = "Item",
-        value = itemId.withoutCheckDigit
+    val sourceIdentifier2 = SourceIdentifier(
+      identifierType = IdentifierType("sierra-identifier"),
+      ontologyType = "Item",
+      value = itemId.withoutCheckDigit
+    )
+
+    val expectedIdentifiers = List(sourceIdentifier1, sourceIdentifier2)
+
+    val transformedItem: MaybeDisplayable[Item] = getTransformedItems(
+      itemDataMap = Map(
+        itemId -> itemData
       )
+    ).head
 
-      val expectedIdentifiers = List(sourceIdentifier1, sourceIdentifier2)
-
-      val transformedItem = transformer.transformItemData(
-        itemId = itemId,
-        itemData = item
-      )
-
-      transformedItem shouldBe Identifiable(
-        sourceIdentifier = sourceIdentifier1,
-        otherIdentifiers = List(sourceIdentifier2),
-        agent = Item(locations = List()))
-
-      transformedItem.identifiers shouldBe expectedIdentifiers
-    }
-
-    it("uses the full Sierra system number as the source identifier") {
-      val itemId = createSierraItemNumber
-      val sourceIdentifier = createSierraSourceIdentifierWith(
-        ontologyType = "Item",
-        value = itemId.withCheckDigit
-      )
-      val sierraItemData = createSierraItemData
-
-      val transformedItem = transformer.transformItemData(
-        itemId = itemId,
-        itemData = sierraItemData
-      )
-      transformedItem.sourceIdentifier shouldBe sourceIdentifier
-    }
+    transformedItem
+      .asInstanceOf[Identifiable[Item]]
+      .identifiers shouldBe expectedIdentifiers
   }
 
-  describe("getItems") {
-    it("removes items with deleted=true") {
-      val item1 = createSierraItemDataWith(deleted = true)
-      val item2 = createSierraItemDataWith(deleted = false)
+  it("uses the full Sierra system number as the source identifier") {
+    val itemId = createSierraItemNumber
+    val sourceIdentifier = createSierraSourceIdentifierWith(
+      ontologyType = "Item",
+      value = itemId.withCheckDigit
+    )
+    val itemData = createSierraItemData
 
-      val itemDataMap = Map(
-        createSierraItemNumber -> item1,
-        createSierraItemNumber -> item2
-      )
+    val transformedItem: MaybeDisplayable[Item] = getTransformedItems(
+      itemDataMap = Map(itemId -> itemData)
+    ).head
 
-      transformer.getPhysicalItems(itemDataMap) should have size 1
-    }
+    transformedItem
+      .asInstanceOf[Identifiable[Item]]
+      .sourceIdentifier shouldBe sourceIdentifier
+  }
 
-    // Note: the following two tests are for historical reasons -- an old
-    // version of this code used "v"/"e-book" as the distinction for whether
-    // we would add a digital item.
-    //
-    // The current rule (2018-08-09) is to use the presence of the 'dlnk'
-    // location, but I left a modified version of these tests in place to
-    // check the old code wasn't left lying around!
+  it("removes items with deleted=true") {
+    val item1 = createSierraItemDataWith(deleted = true)
+    val item2 = createSierraItemDataWith(deleted = false)
 
-    it("does not create any items for an e-book record without the 'dlnk' location") {
-      val bibData = createSierraBibDataWith(
-        materialType = Some(createSierraMaterialTypeWith(code = "v"))
-      )
+    val itemDataMap = Map(
+      createSierraItemNumber -> item1,
+      createSierraItemNumber -> item2
+    )
 
-      transformer.getDigitalItems(bibId = createSierraBibNumber, bibData = bibData) shouldBe List()
-    }
+    getTransformedItems(itemDataMap = itemDataMap) should have size 1
+  }
 
-    it("does not create any items for a non e-book record without the 'dlnk' location") {
-      val bibData = createSierraBibDataWith(
-        materialType = Some(createSierraMaterialTypeWith(code = "x"))
-      )
+  // Note: the following two tests are for historical reasons -- an old
+  // version of this code used "v"/"e-book" as the distinction for whether
+  // we would add a digital item.
+  //
+  // The current rule (2018-08-09) is to use the presence of the 'dlnk'
+  // location, but I left a modified version of these tests in place to
+  // check the old code wasn't left lying around!
 
-      transformer.getDigitalItems(bibId = createSierraBibNumber, bibData = bibData) shouldBe List.empty
-    }
+  it("does not create any items for an e-book record without the 'dlnk' location") {
+    val bibData = createSierraBibDataWith(
+      materialType = Some(createSierraMaterialTypeWith(code = "v"))
+    )
 
-    it("ignores a digital item on a bib record without a 'dlnk' location") {
-      val bibData = createSierraBibDataWith(
-        locations = Some(List(
-          SierraSourceLocation("digi", "Digitised Collections")
-        ))
-      )
+    getTransformedItems(bibData = bibData) shouldBe List()
+  }
 
-      val result = transformer.getDigitalItems(
-        bibId = createSierraBibNumber,
-        bibData = bibData)
+  it("does not create any items for a non e-book record without the 'dlnk' location") {
+    val bibData = createSierraBibDataWith(
+      materialType = Some(createSierraMaterialTypeWith(code = "x"))
+    )
 
-      result shouldBe List()
-    }
+    getTransformedItems(bibData = bibData) shouldBe List()
+  }
 
-    it("adds a digital item on a bib record with a 'dlnk' location") {
-      val bibId = createSierraBibNumber
-      val bibData = createSierraBibDataWith(
-        locations = Some(List(
-          SierraSourceLocation("digi", "Digitised Collections"),
-          SierraSourceLocation("dlnk", "Digitised content")
-        ))
-      )
-
-      val result = transformer.getDigitalItems(
-        bibId = bibId,
-        bibData = bibData)
-
-      val expectedItems = List(
-        Unidentifiable(
-        agent = Item(
-        locations = List(DigitalLocation(
-          url = s"https://wellcomelibrary.org/iiif/${bibId.withCheckDigit}/manifest",
-          license = None,
-          locationType = LocationType("iiif-presentation"))))
+  it("ignores a digital item on a bib record without a 'dlnk' location") {
+    val bibData = createSierraBibDataWith(
+      locations = Some(List(
+        SierraSourceLocation("digi", "Digitised Collections")
       ))
+    )
 
-      result shouldBe expectedItems
-    }
+    getTransformedItems(bibData = bibData) shouldBe List()
   }
+
+  it("adds a digital item on a bib record with a 'dlnk' location") {
+    val bibId = createSierraBibNumber
+    val bibData = createSierraBibDataWith(
+      locations = Some(List(
+        SierraSourceLocation("digi", "Digitised Collections"),
+        SierraSourceLocation("dlnk", "Digitised content")
+      ))
+    )
+
+    val expectedItems = List(
+      Unidentifiable(
+      agent = Item(
+      locations = List(DigitalLocation(
+        url = s"https://wellcomelibrary.org/iiif/${bibId.withCheckDigit}/manifest",
+        license = None,
+        locationType = LocationType("iiif-presentation"))))
+    ))
+
+    getTransformedItems(bibId = bibId, bibData = bibData) shouldBe expectedItems
+  }
+
+  private def getTransformedItems(
+    bibId: SierraBibNumber = createSierraBibNumber,
+    bibData: SierraBibData = createSierraBibData,
+    itemDataMap: Map[SierraItemNumber, SierraItemData] = Map()): List[MaybeDisplayable[Item]] =
+    transformer.getItems(
+      bibId = bibId,
+      bibData = bibData,
+      itemDataMap = itemDataMap
+    )
 }
