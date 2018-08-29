ac0485c51be0c582bf6531c841c889a26de46198
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-29 15:07:46 +0100

    Tighten the restrictions on the EFS security group

--- data_science/terraform/notebooks/efs.tf
+++ data_science/terraform/notebooks/efs.tf
@@ -3,20 +3,6 @@ resource "aws_security_group" "efs_security_group" {
   description = "Allow traffic between data science VMs and EFS"
   vpc_id      = "${var.vpc_id}"
 
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags {
     Name = "${var.namespace}-efs"
   }
