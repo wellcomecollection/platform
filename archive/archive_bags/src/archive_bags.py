0c0af704e7d6bac72d030dd2b6f545b91e36cde3
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-08-30 17:34:11 +0100

    actually use a client

--- archive/archive_bags/src/archive_bags.py
+++ archive/archive_bags/src/archive_bags.py
@@ -6,6 +6,7 @@ Returns a manifest file requested by id in VHS where dynamodb stores the id to S
 import daiquiri
 import json
 import os
+import boto3
 from wellcome_aws_utils.lambda_utils import log_on_error
 
 TABLE_NAME = 'vhs-archive-manifests'
@@ -16,7 +17,10 @@ logger = daiquiri.getLogger()
 
 
 @log_on_error
-def main(event, _, dynamodb_client, s3_client):
+def main(event, _, dynamodb_client=None, s3_client=None):
+    dynamodb_client = dynamodb_client or boto3.client('dynamodb')
+    s3_client = s3_client or boto3.client('s3')
+
     vhs_table_name = os.environ['VHS_TABLE_NAME']
     vhs_bucket_name = os.environ['VHS_BUCKET_NAME']
 
