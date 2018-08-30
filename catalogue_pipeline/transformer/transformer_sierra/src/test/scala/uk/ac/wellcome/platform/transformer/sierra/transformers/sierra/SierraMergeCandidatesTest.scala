c88678b5bd081fc7d4f3b286b19f26eefdbe47df
Alex Chan <alex@alexwlchan.net>
2018-08-30 14:15:09 +0100

    Add a describe block to break up the tests

--- catalogue_pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/sierra/SierraMergeCandidatesTest.scala
+++ catalogue_pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/sierra/SierraMergeCandidatesTest.scala
@@ -19,82 +19,85 @@ class SierraMergeCandidatesTest
     with SierraDataGenerators {
 
   val transformer = new SierraMergeCandidates {}
-  it("extracts the bib number in 776$$w and adds it as a mergeCandidate") {
-    val mergeCandidateBibNumber = "b21414440"
-    val sierraData = createSierraBibDataWith(
-      varFields = List(
-        createVarFieldWith(
-          marcTag = "776",
-          subfields = List(
-            MarcSubfield(tag = "w", content = s"(UkLW)$mergeCandidateBibNumber")
+
+  describe("physical/digital Sierra work") {
+    it("extracts the bib number in 776$$w as a mergeCandidate") {
+      val mergeCandidateBibNumber = "b21414440"
+      val sierraData = createSierraBibDataWith(
+        varFields = List(
+          createVarFieldWith(
+            marcTag = "776",
+            subfields = List(
+              MarcSubfield(tag = "w", content = s"(UkLW)$mergeCandidateBibNumber")
+            )
           )
         )
       )
-    )
 
-    transformer.getMergeCandidates(sierraData) shouldBe List(
-      MergeCandidate(
-        SourceIdentifier(
-          IdentifierType("sierra-system-number"),
-          "Work",
-          mergeCandidateBibNumber)))
-  }
+      transformer.getMergeCandidates(sierraData) shouldBe List(
+        MergeCandidate(
+          SourceIdentifier(
+            IdentifierType("sierra-system-number"),
+            "Work",
+            mergeCandidateBibNumber)))
+    }
 
-  it("strips spaces in tag 776$$w and adds it as a mergeCandidate") {
-    val mergeCandidateBibNumber = "b21414440"
-    val sierraData = createSierraBibDataWith(
-      varFields = List(
-        createVarFieldWith(
-          marcTag = "776",
-          subfields = List(
-            MarcSubfield(
-              tag = "w",
-              content = s"(UkLW)  $mergeCandidateBibNumber")
+    it("strips spaces in tag 776$$w and adds it as a mergeCandidate") {
+      val mergeCandidateBibNumber = "b21414440"
+      val sierraData = createSierraBibDataWith(
+        varFields = List(
+          createVarFieldWith(
+            marcTag = "776",
+            subfields = List(
+              MarcSubfield(
+                tag = "w",
+                content = s"(UkLW)  $mergeCandidateBibNumber")
+            )
           )
         )
       )
-    )
 
-    transformer.getMergeCandidates(sierraData) shouldBe List(
-      MergeCandidate(
-        SourceIdentifier(
-          IdentifierType("sierra-system-number"),
-          "Work",
-          mergeCandidateBibNumber)))
+      transformer.getMergeCandidates(sierraData) shouldBe List(
+        MergeCandidate(
+          SourceIdentifier(
+            IdentifierType("sierra-system-number"),
+            "Work",
+            mergeCandidateBibNumber)))
+    }
+
+    it("returns an empty list if MARC tag 776 does not contain a subfield w") {
+      val sierraData = createSierraBibDataWith(
+        varFields = List(
+          createVarFieldWith(
+            marcTag = "776",
+            subfields = List(
+              MarcSubfield(tag = "a", content = s"blah blah")
+            )
+          )
+        )
+      )
+
+      transformer.getMergeCandidates(sierraData) shouldBe Nil
+    }
+
+    it("ignores values in 776$$w that aren't prefixed with (UkLW)") {
+      val sierraData = createSierraBibDataWith(
+        varFields = List(
+          createVarFieldWith(
+            marcTag = "776",
+            subfields = List(
+              MarcSubfield(tag = "w", content = s"(OCoLC)14322288")
+            )
+          )
+        )
+      )
+
+      transformer.getMergeCandidates(sierraData) shouldBe Nil
+    }
   }
 
-  it("returns an empty list if there is no marc tag 776") {
+  it("returns an empty list if there is no MARC tag 776") {
     val sierraData = createSierraBibDataWith(varFields = List())
-    transformer.getMergeCandidates(sierraData) shouldBe Nil
-  }
-
-  it("returns an empty list if marc tag 776 does not contain a subfield w") {
-    val sierraData = createSierraBibDataWith(
-      varFields = List(
-        createVarFieldWith(
-          marcTag = "776",
-          subfields = List(
-            MarcSubfield(tag = "a", content = s"blah blah")
-          )
-        )
-      )
-    )
-
-    transformer.getMergeCandidates(sierraData) shouldBe Nil
-  }
-
-  it("ignores values in 776$$w that aren't prefixed with (UkLW)") {
-    val sierraData = createSierraBibDataWith(
-      varFields = List(
-        createVarFieldWith(
-          marcTag = "776",
-          subfields = List(
-            MarcSubfield(tag = "w", content = s"(OCoLC)14322288")
-          )
-        )
-      )
-    )
-
     transformer.getMergeCandidates(sierraData) shouldBe Nil
   }
 }
