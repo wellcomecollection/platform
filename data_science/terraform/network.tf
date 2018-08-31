dac1975344b7f296bc065db809650047faffbd3c
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-31 10:16:09 +0100

    Add a VPC endpoint for S3 in the data_science stack

--- data_science/terraform/network.tf
+++ data_science/terraform/network.tf
@@ -4,3 +4,8 @@ module "network" {
   cidr_block = "${var.vpc_cidr_block}"
   az_count   = "2"
 }
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = "${module.network.vpc_id}"
+  service_name = "com.amazonaws.${var.aws_region}.s3"
+}
