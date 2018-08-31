9084d13e73d7eca0470fbb65be0e1a55608069ed
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-31 11:37:02 +0100

    Start fleshing out tests for the WorkPair stuff

---
+++ catalogue_pipeline/merger/src/test/scala/uk/ac/wellcome/platform/merger/pairwise_transforms/SierraPhysicalDigitalWorkPairTest.scala
@@ -0,0 +1,70 @@
+package uk.ac.wellcome.platform.merger.pairwise_transforms
+
+import org.scalatest.{Assertion, FunSpec}
+import uk.ac.wellcome.models.work.internal.{Identifiable, Item, Unidentifiable, UnidentifiedWork}
+import uk.ac.wellcome.platform.merger.MergerTestUtils
+
+class SierraPhysicalDigitalWorkPairTest extends FunSpec with MergerTestUtils {
+  val physicalWorkWithOneItem: UnidentifiedWork = createPhysicalWork
+  val digitalWorkWithOneItem: UnidentifiedWork = createDigitalWork
+
+  describe("does not merge if item counts are wrong") {
+    it("physical work has 0 items") {
+      val physicalWork = createUnidentifiedWorkWith(
+        items = List()
+      )
+
+      applyMerge(
+        physicalWork = physicalWork,
+        digitalWork = digitalWorkWithOneItem
+      ) shouldBe None
+    }
+
+    it("physical work has >1 items") {
+      val physicalWork = createUnidentifiedWorkWith(
+        items = List(createPhysicalItem, createPhysicalItem)
+      )
+
+      applyMerge(
+        physicalWork = physicalWork,
+        digitalWork = digitalWorkWithOneItem
+      ) shouldBe None
+    }
+
+    it("digital work has 0 items") {
+      val digitalWork = createUnidentifiedWorkWith(
+        items = List()
+      )
+
+      applyMerge(
+        physicalWork = physicalWorkWithOneItem,
+        digitalWork = digitalWork
+      ) shouldBe None
+    }
+
+    it("digital work has >1 items") {
+      val digitalWork = createUnidentifiedWorkWith(
+        items = List(createDigitalItem, createDigitalItem)
+      )
+
+      applyMerge(
+        physicalWork = physicalWorkWithOneItem,
+        digitalWork = digitalWork
+      ) shouldBe None
+    }
+  }
+
+  private def createPhysicalItem: Identifiable[Item] =
+    createIdentifiableItemWith(locations = List(createPhysicalLocation))
+
+  private def createDigitalItem: Unidentifiable[Item] =
+    createUnidentifiableItemWith(locations = List(createDigitalLocation))
+
+  private def applyMerge(
+    physicalWork: UnidentifiedWork,
+    digitalWork: UnidentifiedWork): Option[PairwiseResult] =
+    SierraPhysicalDigitalWorkPair.mergeAndRedirectWork(
+      physicalWork = physicalWork,
+      digitalWork = digitalWork
+    )
+}
