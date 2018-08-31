652c0bdb12d825177eb764afca6000dc47fc0fca
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-31 11:42:06 +0100

    Add a test for identifiers being copied

--- catalogue_pipeline/merger/src/test/scala/uk/ac/wellcome/platform/merger/pairwise_transforms/SierraPhysicalDigitalWorkPairTest.scala
+++ catalogue_pipeline/merger/src/test/scala/uk/ac/wellcome/platform/merger/pairwise_transforms/SierraPhysicalDigitalWorkPairTest.scala
@@ -1,12 +1,27 @@
 package uk.ac.wellcome.platform.merger.pairwise_transforms
 
-import org.scalatest.{Assertion, FunSpec}
+import org.scalatest.FunSpec
 import uk.ac.wellcome.models.work.internal.{Identifiable, Item, Unidentifiable, UnidentifiedWork}
 import uk.ac.wellcome.platform.merger.MergerTestUtils
 
 class SierraPhysicalDigitalWorkPairTest extends FunSpec with MergerTestUtils {
   val physicalWorkWithOneItem: UnidentifiedWork = createPhysicalWork
   val digitalWorkWithOneItem: UnidentifiedWork = createDigitalWork
+
+  it("copies the identifiers from the digital work to the physical work") {
+    val digitalWork = createUnidentifiedWorkWith(
+      otherIdentifiers = List(createSourceIdentifier, createSourceIdentifier),
+      items = List(createDigitalItem)
+    )
+
+    val result = applyMerge(
+      physicalWork = physicalWorkWithOneItem,
+      digitalWork = digitalWork
+    )
+
+    val newIdentifiers = result.get.mergedWork.identifiers
+    newIdentifiers shouldBe physicalWorkWithOneItem.identifiers ++ digitalWork.identifiers
+  }
 
   describe("does not merge if item counts are wrong") {
     it("physical work has 0 items") {
