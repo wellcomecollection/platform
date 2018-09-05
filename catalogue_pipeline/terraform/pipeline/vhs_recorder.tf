2d56429a51d4735e6c2cd9d684245222518d09fb
Alex Chan <a.chan@wellcome.ac.uk>
2018-09-05 14:45:13 +0100

    Increase the read capacity on the recorder VHS

--- catalogue_pipeline/terraform/pipeline/vhs_recorder.tf
+++ catalogue_pipeline/terraform/pipeline/vhs_recorder.tf
@@ -2,5 +2,8 @@ module "vhs_recorder" {
   source = "../../../catalogue_pipeline_data/terraform/vhs"
   name   = "${replace(var.namespace, "_", "-")}-Recorder"
 
+  table_read_max_capacity  = 300
+  table_write_max_capacity = 300
+
   table_stream_enabled = false
 }
