d52adc7904204ab4f71dfa7b43c95ec05e16eb1e
Alex Chan <a.chan@wellcome.ac.uk>
2018-09-06 13:04:05 +0100

    Bump the recorder capacity

--- catalogue_pipeline/terraform/pipeline/vhs_recorder.tf
+++ catalogue_pipeline/terraform/pipeline/vhs_recorder.tf
@@ -2,7 +2,7 @@ module "vhs_recorder" {
   source = "../../../catalogue_pipeline_data/terraform/vhs"
   name   = "${replace(var.namespace, "_", "-")}-Recorder"
 
-  table_read_max_capacity  = 600
+  table_read_max_capacity  = 1000
   table_write_max_capacity = 300
 
   table_stream_enabled = false
