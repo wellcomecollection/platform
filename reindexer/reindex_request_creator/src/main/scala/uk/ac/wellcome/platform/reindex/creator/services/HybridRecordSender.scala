c01e08ec3e4460db60a04e4953b19520903d3689
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-09-06 16:06:13 +0000

    Apply auto-formatting rules

--- reindexer/reindex_request_creator/src/main/scala/uk/ac/wellcome/platform/reindex/creator/services/HybridRecordSender.scala
+++ reindexer/reindex_request_creator/src/main/scala/uk/ac/wellcome/platform/reindex/creator/services/HybridRecordSender.scala
@@ -15,25 +15,26 @@ class HybridRecordSender @Inject()(snsWriter: SNSWriter)(
     Future.sequence {
       records
         .map { record: HybridRecord =>
-          snsWriter.writeMessage(
-            message = record,
-            subject = this.getClass.getSimpleName
-          )
-        .recover {
+          snsWriter
+            .writeMessage(
+              message = record,
+              subject = this.getClass.getSimpleName
+            )
+            .recover {
 
-          // We've seen a lot of reindexer errors of the form:
-          //
-          //    Caused by: org.apache.http.conn.ConnectionPoolTimeoutException:
-          //    Timeout waiting for connection from pool
-          //
-          // which logs a massive stack trace.  I'm trying to fix this by
-          // tweaking the parallelism, but reducing it to a ReindexerException
-          // should also reduce the amount of log spam.
-          //
-          case e: SdkClientException =>
-            throw ReindexerException(e)
+              // We've seen a lot of reindexer errors of the form:
+              //
+              //    Caused by: org.apache.http.conn.ConnectionPoolTimeoutException:
+              //    Timeout waiting for connection from pool
+              //
+              // which logs a massive stack trace.  I'm trying to fix this by
+              // tweaking the parallelism, but reducing it to a ReindexerException
+              // should also reduce the amount of log spam.
+              //
+              case e: SdkClientException =>
+                throw ReindexerException(e)
+            }
         }
-      }
     }
   }
 }
