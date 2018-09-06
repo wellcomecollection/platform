e3b1c53b77d11c65064cc6f06c3ab3d05706dbab
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-09-06 15:34:11 +0000

    Apply auto-formatting rules

--- catalogue_pipeline/terraform/pipeline/security_groups.tf
+++ catalogue_pipeline/terraform/pipeline/security_groups.tf
@@ -4,6 +4,6 @@ module "service_egress_security_group" {
   name        = "${var.namespace}_service_egress"
   description = "Allow traffic between services"
 
-  vpc_id      = "${var.vpc_id}"
-  subnet_ids  = "${var.subnets}"
+  vpc_id     = "${var.vpc_id}"
+  subnet_ids = "${var.subnets}"
 }
