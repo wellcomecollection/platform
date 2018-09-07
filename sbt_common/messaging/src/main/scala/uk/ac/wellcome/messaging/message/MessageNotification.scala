f9b8c55a46afe89f53384fd8a464a381a2ff57c8
Alex Chan <a.chan@wellcome.ac.uk>
2018-09-07 10:19:28 +0100

    Add a chunky comment to MessageNotification explaining the design

--- sbt_common/messaging/src/main/scala/uk/ac/wellcome/messaging/message/MessageNotification.scala
+++ sbt_common/messaging/src/main/scala/uk/ac/wellcome/messaging/message/MessageNotification.scala
@@ -2,6 +2,16 @@ package uk.ac.wellcome.messaging.message
 
 import uk.ac.wellcome.storage.ObjectLocation
 
+/** Most of our inter-application messages are fairly small, and can be
+  * sent inline on SNS/SQS.  But occasionally, we have an unusually large
+  * message that can't be sent this way.  In that case, we store a copy
+  * of the message in S3, and send an ObjectLocation telling the consumer
+  * where to find the main message.
+  *
+  * This trait is the base for the two notification classes -- so we
+  * can do `fromJson[MessageNotification]` in the consumer, and get the
+  * correct type back.
+  */
 sealed trait MessageNotification[T]
 
 /** Represents a message where the body of the message is stored in a
