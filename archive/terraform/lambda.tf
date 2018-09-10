472864e6b10f03ee539e7967d4ff3d5ac2317fa7
Alex Chan <a.chan@wellcome.ac.uk>
2018-09-10 14:56:07 +0100

    Get the new API Gateway Lambda module working

--- archive/terraform/lambda.tf
+++ archive/terraform/lambda.tf
@@ -9,6 +9,8 @@ module "lambda_archive_bags" {
     VHS_TABLE_NAME  = "${module.vhs_archive_manifest.table_name}"
     REGION          = "${var.aws_region}"
   }
+
+  api_gateway_execution_arn = "${aws_api_gateway_rest_api.archive_asset_lookup.execution_arn}"
 }
 
 module "lambda_archive_report_ingest_status" {
@@ -22,12 +24,14 @@ module "lambda_archive_report_ingest_status" {
     TABLE_NAME = "${aws_dynamodb_table.archive_progress_table.name}"
     REGION     = "${var.aws_region}"
   }
+
+  api_gateway_execution_arn = "${aws_api_gateway_rest_api.archive_asset_lookup.execution_arn}"
 }
 
 module "lambda_archive_start_ingest" {
   source = "apigw_lambda"
 
-  name        = "lambda_archive_start_ingest"
+  name        = "archive_start_ingest"
   description = "Receives POST messages that start the ingest process"
 
   environment_variables = {
@@ -35,4 +39,6 @@ module "lambda_archive_start_ingest" {
     TABLE_NAME = "${aws_dynamodb_table.archive_progress_table.name}"
     REGION     = "${var.aws_region}"
   }
+
+  api_gateway_execution_arn = "${aws_api_gateway_rest_api.archive_asset_lookup.execution_arn}"
 }
