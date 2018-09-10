bc99b4ec398d9c367f55a0f45ffa17380bd7febb
Alex Chan <a.chan@wellcome.ac.uk>
2018-09-10 09:45:13 +0100

    We need to wrap that in a Notification now

--- sbt_common/messaging/src/test/scala/uk/ac/wellcome/messaging/test/fixtures/Messaging.scala
+++ sbt_common/messaging/src/test/scala/uk/ac/wellcome/messaging/test/fixtures/Messaging.scala
@@ -83,8 +83,7 @@ trait Messaging
   def withMessageWriter[T, R](bucket: Bucket,
                               topic: Topic,
                               writerSnsClient: AmazonSNS = snsClient)(
-    testWith: TestWith[MessageWriter[T], R])(
-    implicit store: ObjectStore[T]): R = {
+    testWith: TestWith[MessageWriter[T], R])(implicit store: ObjectStore[T]): R = {
     val s3Config = S3Config(bucketName = bucket.name)
     val snsConfig = SNSConfig(topicArn = topic.arn)
     val messageConfig = MessageWriterConfig(
@@ -188,9 +187,9 @@ trait Messaging
       toJson(obj).get
     )
 
-    sendMessage[MessageNotification](
+    sendNotificationToSQS[MessageNotification](
       queue = queue,
-      obj = RemoteNotification(location)
+      message = RemoteNotification(location)
     )
   }
 }
