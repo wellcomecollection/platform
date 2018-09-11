d608ed039d505cb3a5f88db1153eaa8d02e19119
Alex Chan <a.chan@wellcome.ac.uk>
2018-09-11 13:53:39 +0100

    Return a JSON string there

--- archive/ingest_manager/src/views.py
+++ archive/ingest_manager/src/views.py
@@ -1,13 +1,16 @@
 # -*- encoding: utf-8
 
+from flask import jsonify
+
 from ingest_manager import app
 from report_ingest_status import report_ingest_status
 
 
 @app.route('/ingests/<guid>')
 def route_report_ingest_status(guid):
-    return report_ingest_status(
+    result = report_ingest_status(
         dynamodb_resource=app.config['dynamodb_resource'],
         table_name=app.config['dynamodb_table_name'],
         guid=guid
     )
+    return jsonify(result)
