e9899be3214ee70efc6c08acd02c4da27ebcc419
Alex Chan <a.chan@wellcome.ac.uk>
2018-09-11 09:55:49 +0100

    Pass the error prefixes into the Lambda env vars

--- archive/terraform/lambda.tf
+++ archive/terraform/lambda.tf
@@ -22,6 +22,10 @@ module "lambda_archive_report_ingest_status" {
   environment_variables = {
     TABLE_NAME = "${aws_dynamodb_table.archive_progress_table.name}"
     REGION     = "${var.aws_region}"
+
+    error_bad_request       = "${local.error_bad_request}"
+    error_disallowed_method = "${local.error_disallowed_method}"
+    error_not_found         = "${local.error_not_found}"
   }
 
   api_gateway_execution_arn = "${aws_api_gateway_rest_api.archive_asset_lookup.execution_arn}"
