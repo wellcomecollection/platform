cdd74283f22750e7ecc6f3a23604e74d5def2a80
Alex Chan <a.chan@wellcome.ac.uk>
2018-09-11 12:00:22 +0100

    Provide enough of a Dockerfile to get the app building

--- archive/ingest_manager/src/main.py
+++ archive/ingest_manager/src/main.py
@@ -1,1 +1,15 @@
 # -*- encoding: utf-8
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
