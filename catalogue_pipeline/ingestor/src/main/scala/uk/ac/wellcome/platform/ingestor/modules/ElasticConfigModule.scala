e84586fb1b35fffc29ae1f196fb857ad0d01966f
alicefuzier <alicefuzier@gmail.com>
2018-09-14 14:52:10 +0100

    Actually rename the flag in the ingestor

--- catalogue_pipeline/ingestor/src/main/scala/uk/ac/wellcome/platform/ingestor/modules/ElasticConfigModule.scala
+++ catalogue_pipeline/ingestor/src/main/scala/uk/ac/wellcome/platform/ingestor/modules/ElasticConfigModule.scala
@@ -8,7 +8,7 @@ object IngestElasticConfigModule extends TwitterModule {
   private val documentType =
     flag[String]("es.type", "item", "document type in Elasticsearch")
 
-  private val index = flag[String]("es.index.v1", "V1 ES index name")
+  private val index = flag[String]("es.index", "V1 ES index name")
 
   @Singleton
   @Provides
