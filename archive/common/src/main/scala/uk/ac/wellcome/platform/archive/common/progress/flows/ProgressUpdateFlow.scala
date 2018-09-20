9180c5dfc21d496d76e283571a9d7cd8607fedc7
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-09-20 14:07:40 +0100

    Adds progress update flow

---
+++ archive/common/src/main/scala/uk/ac/wellcome/platform/archive/common/progress/flows/ProgressUpdateFlow.scala
@@ -0,0 +1,21 @@
+package uk.ac.wellcome.platform.archive.common.progress.flows
+
+import akka.NotUsed
+import akka.stream.scaladsl.Flow
+import uk.ac.wellcome.platform.archive.common.progress.models.{FailedProgressUpdate, ProgressUpdate}
+import uk.ac.wellcome.platform.archive.common.progress.monitor.ProgressMonitor
+
+import scala.util.Try
+
+object ProgressUpdateFlow {
+  def apply(progressMonitor: ProgressMonitor):
+  Flow[ProgressUpdate, Either[FailedProgressUpdate, ProgressUpdate], NotUsed] = {
+
+    Flow[ProgressUpdate].map(progressUpdate => {
+      Try(progressMonitor.update(progressUpdate)).toEither
+        .left.map(e => FailedProgressUpdate(e, progressUpdate))
+        .right.map(_ => progressUpdate)
+    })
+
+  }
+}
