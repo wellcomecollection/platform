705fe6d3c789c42b06f7795a2149c71a684699c0
alicefuzier <alicefuzier@gmail.com>
2018-09-21 10:03:39 +0100

    Increase timeout again

--- catalogue_pipeline/matcher/src/test/scala/uk/ac/wellcome/platform/matcher/locking/DynamoRowLockDaoTest.scala
+++ catalogue_pipeline/matcher/src/test/scala/uk/ac/wellcome/platform/matcher/locking/DynamoRowLockDaoTest.scala
@@ -9,9 +9,10 @@ import com.amazonaws.services.dynamodbv2.model._
 import com.gu.scanamo._
 import org.mockito.Matchers.any
 import org.mockito.Mockito.when
-import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{FunSpec, Matchers}
 import uk.ac.wellcome.platform.matcher.fixtures.MatcherFixtures
+import uk.ac.wellcome.test.utils.ExtendedPatience
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -22,7 +23,7 @@ class DynamoRowLockDaoTest
     with Matchers
     with ScalaFutures
     with MatcherFixtures
-    with IntegrationPatience {
+    with ExtendedPatience {
 
   import com.gu.scanamo.syntax._
 
