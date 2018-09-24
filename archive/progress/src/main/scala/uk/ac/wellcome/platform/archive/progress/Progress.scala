20b8f438b2a0920c8e295ffba2cded542f2f1821
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-09-24 17:22:18 +0100

    Implement progress monitor flow

--- archive/progress/src/main/scala/uk/ac/wellcome/platform/archive/progress/Progress.scala
+++ archive/progress/src/main/scala/uk/ac/wellcome/platform/archive/progress/Progress.scala
@@ -3,52 +3,53 @@ package uk.ac.wellcome.platform.archive.progress
 import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.stream.scaladsl.Flow
+import com.amazonaws.services.sns.AmazonSNSAsync
 import com.google.inject.Injector
 import grizzled.slf4j.Logging
 import uk.ac.wellcome.json.JsonUtil._
+import uk.ac.wellcome.messaging.sns.SNSConfig
+import uk.ac.wellcome.platform.archive.common.flows.NotificationParsingFlow
 import uk.ac.wellcome.platform.archive.common.messaging.MessageStream
 import uk.ac.wellcome.platform.archive.common.models.NotificationMessage
+import uk.ac.wellcome.platform.archive.common.progress.flows.ProgressUpdateAndPublishFlow
 import uk.ac.wellcome.platform.archive.common.progress.models.ProgressUpdate
-
-import scala.util.{Failure, Success}
+import uk.ac.wellcome.platform.archive.common.progress.monitor.ProgressMonitor
 
 trait Progress extends Logging {
   val injector: Injector
 
   def run() = {
-    //    implicit val s3Client: S3Client = injector.getInstance(classOf[S3Client])
-    //    implicit val snsClient: AmazonSNSAsync =
-    //      injector.getInstance(classOf[AmazonSNSAsync])
-    //    val snsConfig = injector.getInstance(classOf[SNSConfig])
+
+    type StreamNotice = MessageStream[NotificationMessage, Object]
+
+    implicit val snsClient: AmazonSNSAsync =
+      injector.getInstance(classOf[AmazonSNSAsync])
+    val snsConfig = injector.getInstance(classOf[SNSConfig])
 
     implicit val system =
       injector.getInstance(classOf[ActorSystem])
-
-    //    implicit val materializer =
-    //      ActorMaterializer()
 
     implicit val adapter =
       Logging(system.eventStream, "customLogger")
 
     val messageStream =
-      injector.getInstance(classOf[MessageStream[NotificationMessage, Object]])
+      injector.getInstance(classOf[StreamNotice])
+
+    val progressMonitor = injector
+      .getInstance(classOf[ProgressMonitor])
+
+    val progressUpdateFlow = ProgressUpdateAndPublishFlow(
+      snsClient, snsConfig, progressMonitor
+    )
+
+    val parseNotification = NotificationParsingFlow[ProgressUpdate]()
 
     val workFlow =
       Flow[NotificationMessage]
         .log("notification message")
-        .map(parseNotification)
+        .via(parseNotification)
+        .via(progressUpdateFlow)
 
-    messageStream.run("archivist", workFlow)
-  }
-
-  private def parseNotification(message: NotificationMessage) = {
-    fromJson[ProgressUpdate](message.Message) match {
-      case Success(progressUpdate) => progressUpdate
-      case Failure(e) =>
-        throw new RuntimeException(
-          s"Failed to get progressUpdate from notification: ${e.getMessage}"
-        )
-    }
+    messageStream.run("progress", workFlow)
   }
 }
-
