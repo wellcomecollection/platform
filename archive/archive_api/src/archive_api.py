948d19e765aed88122378b1d2389f274da63cdb1
mklander <mklanderyou@gmail.com>
2018-09-24 16:05:11 +0100

    Flask-restful and Swagger

--- archive/archive_api/src/archive_api.py
+++ archive/archive_api/src/archive_api.py
@@ -6,6 +6,7 @@ import uuid
 
 import daiquiri
 from flask import Flask
+from flask_restplus import Api, Resource, fields
 from flask import jsonify, make_response, request, url_for
 from werkzeug.exceptions import BadRequest as BadRequestError
 
@@ -18,102 +19,119 @@ from create_ingest_progress import (
 )
 
 app = Flask(__name__)
-app.config.from_object('config.ProductionConfig')
+api = Api(app)
+
+if os.environ.get('FLASK_ENV') == 'development':
+    app.config.from_object('config.DevelopmentConfig')
+else:
+    app.config.from_object('config.ProductionConfig')
 
 daiquiri.setup(level=os.environ.get('LOG_LEVEL', 'INFO'))
 logger = daiquiri.getLogger()
+
+ns = api.namespace('ingests', description='')
+
+ingest_request_model = api.model('Ingest request', {
+    'type': fields.String(description='Type of the object', enum=['Ingest'], required=True),
+    'uploadUrl': fields.String(description='URL of uploaded bag', required=True),
+    'callbackUrl': fields.String(description='URL to use for callback on completion'),
+    'ingestType': fields.Nested(api.model('Ingest type', {
+        'type': fields.String(description='Type of the object', enum=['IngestType'], required=True),
+        'id': fields.String(description='identifier for ingest type', enum=['create'], required=True),
+    }))
+})
+
+error_model = api.model('Error', {
+    '@context': fields.String(description='Context URL'),
+    'errorType': fields.String(description='errorType'),
+    'httpStatus': fields.Integer(description='httpStatus'),
+    'label': fields.String(description='label'),
+    'description': fields.String(description='description'),
+    'type': fields.String(description='type'),
+})
+
+@ns.route('')
+class IngestCollection(Resource):
+    @api.expect(ingest_request_model, validate=True)
+    @api.response(202, 'Ingest created')
+    @api.response(400, 'Bad request', error_model)
+    def post(self):
+        upload_url = request.json['uploadUrl']
+        callback_url = request.json.get('callbackUrl')
+
+        try:
+            validate_url(upload_url, supported_schemes=['s3'], allow_fragment=False)
+        except ValueError as error:
+            raise BadRequestError(f"Invalid uploadUrl:{upload_url!r}, {error}")
+
+        if callback_url:
+            try:
+                validate_url(callback_url, supported_schemes=['http', 'https'])
+            except ValueError as error:
+                raise BadRequestError(f"Invalid callbackUrl:{callback_url!r}, {error}")
+
+        ingest_request_id = str(uuid.uuid4())
+        logger.debug('ingest_request_id=%r', ingest_request_id)
+
+        create_ingest_progress(
+            IngestProgress(ingest_request_id, upload_url, callback_url),
+            app.config['DYNAMODB_RESOURCE'],
+            app.config['DYNAMODB_TABLE_NAME'])
+
+        ingest_request_id = send_new_ingest_request(
+            sns_client=app.config['SNS_CLIENT'],
+            topic_arn=app.config['SNS_TOPIC_ARN'],
+            ingest_request_id=ingest_request_id,
+            upload_url=upload_url,
+            callback_url=callback_url
+        )
+
+        # Construct the URL where the user will be able to get the status
+        # of their ingest request.
+        location = url_for(
+            'route_report_ingest_status',
+            guid=ingest_request_id
+        )
+
+        # Now we set the Location response header.  There's no way to do this
+        # without constructing our own Response object, so that's what we do
+        # here.  See https://stackoverflow.com/q/25860304/1558022
+        resp = make_response('', 202)
+        resp.headers['Location'] = location
+        return resp
+
+
+@ns.route('/<id>')
+class IngestResource(Resource):
+    @api.response(200, 'Ingest found')
+    @api.response(404, 'Ingest not found', error_model)
+    def get(self, id):
+        result = report_ingest_status(
+            dynamodb_resource=app.config['DYNAMODB_RESOURCE'],
+            table_name=app.config['DYNAMODB_TABLE_NAME'],
+            guid=id
+        )
+        return jsonify(result)
 
 
 @app.route('/healthcheck')
 def route_report_healthcheck_status():
     return jsonify({'status': 'OK'})
 
-
-@app.route('/ingests/<guid>')
-def route_report_ingest_status(guid):
-    result = report_ingest_status(
-        dynamodb_resource=app.config['DYNAMODB_RESOURCE'],
-        table_name=app.config['DYNAMODB_TABLE_NAME'],
-        guid=guid
-    )
-    return jsonify(result)
-
-
-@app.route('/ingests', methods=['POST'])
-def route_request_new_ingest():
-    if not request.is_json:
-        raise BadRequestError('Mimetype expected to be application/json')
-
-    try:
-        request_data = request.get_json()
-        validate_type(request_data['type'])
-        validate_ingest_type(request_data['ingestType'])
-        upload_url = request_data['uploadUrl']
-        callback_url = request_data.get('callbackUrl')
-    except BadRequestError:
-        raise BadRequestError('Invalid json in request')
-    except KeyError as key_error:
-        raise BadRequestError(f'No {key_error.args[0]} parameter in request')
-    except ValueError as value_error:
-        raise BadRequestError(f'{value_error} in request')
-
-    # TODO: There is quite a lot of request validation logic which should be moved out
-    try:
-        validate_url(upload_url, supported_schemes=['s3'], allow_fragment=False)
-    except ValueError as error:
-        raise BadRequestError(f"Invalid uploadUrl:{upload_url!r}, {error}")
-
-    if callback_url:
-        try:
-            validate_url(callback_url, supported_schemes=['http', 'https'])
-        except ValueError as error:
-            raise BadRequestError(f"Invalid callbackUrl:{callback_url!r}, {error}")
-
-    ingest_request_id = str(uuid.uuid4())
-    logger.debug('ingest_request_id=%r', ingest_request_id)
-
-    create_ingest_progress(
-        IngestProgress(ingest_request_id, upload_url, callback_url),
-        app.config['DYNAMODB_RESOURCE'],
-        app.config['DYNAMODB_TABLE_NAME'])
-
-    ingest_request_id = send_new_ingest_request(
-        sns_client=app.config['SNS_CLIENT'],
-        topic_arn=app.config['SNS_TOPIC_ARN'],
-        ingest_request_id=ingest_request_id,
-        upload_url=upload_url,
-        callback_url=callback_url
-    )
-
-    # Construct the URL where the user will be able to get the status
-    # of their ingest request.
-    location = url_for(
-        'route_report_ingest_status',
-        guid=ingest_request_id
-    )
-
-    # Now we set the Location response header.  There's no way to do this
-    # without constructing our own Response object, so that's what we do
-    # here.  See https://stackoverflow.com/q/25860304/1558022
-    resp = make_response('', 202)
-    resp.headers['Location'] = location
-    return resp
-
-
-@app.errorhandler(BadRequestError)
-def bad_request_error(error):
-    return jsonify(status=400, message=error.description), 400
-
-
-def validate_type(type_value):
-    if type_value != 'Ingest':
-        raise ValueError(f'Expected \'type\'=\'Ingest\', got {type_value!r}')
-
-
-def validate_ingest_type(ingest_type_value):
-    valid_ingest_type = {'id': 'create', 'type': 'IngestType'}
-    if ingest_type_value != valid_ingest_type:
-        raise ValueError(f'Expected \'ingestType\'={valid_ingest_type!r}, got {ingest_type_value!r}')
+@app.errorhandler(Exception)
+@api.errorhandler(Exception)
+# @api.marshal_with(error_model)
+def default_error_handler(error):
+    error_response = {
+        '@context': 'https://....',
+        'errorType': 'http',
+        'httpStatus': getattr(error, 'code', 500),
+        'label': getattr(error, 'name', 'Internal Server Error'),
+        'type': 'Error',
+    }
+    if error_response['httpStatus'] != 500:
+        error_response['description'] = getattr(error, 'description', str(error))
+    return jsonify(error_response), error_response['httpStatus']
 
 
 def validate_url(url, supported_schemes=None, allow_fragment=True):
--- archive/archive_api/src/config.py
+++ archive/archive_api/src/config.py
@@ -12,7 +12,14 @@ class Config(object):
 
 
 class ProductionConfig(Config):
-    DYNAMODB_TABLE_NAME = os.environ['TABLE_NAME']
-    SNS_TOPIC_ARN = os.environ['TOPIC_ARN']
+    DYNAMODB_TABLE_NAME = os.environ.get('TABLE_NAME')
+    SNS_TOPIC_ARN = os.environ.get('TOPIC_ARN')
+    DYNAMODB_RESOURCE = boto3.resource('dynamodb')
+    SNS_CLIENT = boto3.client('sns')
+
+
+class DevelopmentConfig(Config):
+    DYNAMODB_TABLE_NAME = 'archive-storage-progress-table'
+    SNS_TOPIC_ARN = 'arn:aws:sns:eu-west-1:760097843905:archive-storage_archivist'
     DYNAMODB_RESOURCE = boto3.resource('dynamodb')
     SNS_CLIENT = boto3.client('sns')
