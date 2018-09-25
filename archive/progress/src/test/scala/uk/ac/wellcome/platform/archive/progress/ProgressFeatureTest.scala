73e5de596134e3815a72994dd92aa539e464281d
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-09-25 10:07:12 +0100

    add feature test for progress

--- archive/progress/src/test/scala/uk/ac/wellcome/platform/archive/progress/ProgressFeatureTest.scala
+++ archive/progress/src/test/scala/uk/ac/wellcome/platform/archive/progress/ProgressFeatureTest.scala
@@ -2,10 +2,13 @@ package uk.ac.wellcome.platform.archive.progress
 
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.monitoring.fixtures.MetricsSenderFixture
 import uk.ac.wellcome.platform.archive.common.progress.fixtures.ProgressMonitorFixture
+import uk.ac.wellcome.platform.archive.common.progress.models.{Progress => ProgressModel}
 import uk.ac.wellcome.platform.archive.progress.fixtures.{Progress => ProgressFixture}
 import uk.ac.wellcome.test.utils.ExtendedPatience
+
 
 class ProgressFeatureTest
   extends FunSpec
@@ -16,13 +19,39 @@ class ProgressFeatureTest
     with ProgressFixture
     with ExtendedPatience {
 
-  it("fails") {
-    withProgress {
+  it("updates an existing progress monitor to Completed") {
+    withProgressApp {
       case (qPair, topic, table, app) => {
+
         app.run()
 
-        eventually {
-          true shouldBe false
+        withProgressMonitor(table) { monitor =>
+          withProgress(monitor) { progress =>
+            withProgressUpdate(progress.id, ProgressModel.Completed) { update =>
+
+              sendNotificationToSQS(qPair.queue, update)
+
+              val expectedProgress = progress.update(update)
+
+              eventually {
+
+                assertSnsReceivesOnly(expectedProgress, topic)
+                assertProgressCreated(
+                  progress.id,
+                  uploadUrl,
+                  Some(callbackUrl),
+                  table = table
+                )
+
+                assertProgressRecordedRecentEvents(
+                  update.id,
+                  Seq(update.event.description),
+                  table
+                )
+
+              }
+            }
+          }
         }
       }
     }
