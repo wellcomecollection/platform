cecae280ac9fc060620b14dbbda964ef8dbce08d
Alex Chan <a.chan@wellcome.ac.uk>
2018-09-25 15:27:37 +0100

    Make sure we assign those attributes!

--- archive/archive_api/src/config.py
+++ archive/archive_api/src/config.py
@@ -13,11 +13,11 @@ class ArchiveAPIConfig(object):
     def __init__(self, development=False):
         try:
             if development:
-                DYNAMODB_TABLE_NAME = 'archive-storage-progress-table'
-                SNS_TOPIC_ARN = 'arn:aws:sns:eu-west-1:760097843905:archive-storage_archivist'
+                self.DYNAMODB_TABLE_NAME = 'archive-storage-progress-table'
+                self.SNS_TOPIC_ARN = 'arn:aws:sns:eu-west-1:760097843905:archive-storage_archivist'
             else:
-                DYNAMODB_TABLE_NAME = os.environ['TABLE_NAME']
-                SNS_TOPIC_ARN = os.environ['TOPIC_ARN']
+                self.DYNAMODB_TABLE_NAME = os.environ['TABLE_NAME']
+                self.SNS_TOPIC_ARN = os.environ['TOPIC_ARN']
         except KeyError as err:
             raise RuntimeError(
                 f'Unable to create config: {err!r}'
