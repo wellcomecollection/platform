de3c5cfb2b3c084e7c44b8080dc24bf8f19eb66f
Alex Chan <a.chan@wellcome.ac.uk>
2018-09-25 10:29:19 +0100

    Move the template files into a separate TF stack

--- data_science/terraform/notebooks/dlami/main.tf
+++ data_science/terraform/notebooks/dlami/main.tf
@@ -42,33 +42,6 @@ module "instance_profile" {
   name = "${var.name}"
 }
 
-data "template_file" "requirements" {
-  template = "${file("${path.module}/templates/requirements.txt")}"
-}
-
-data "template_file" "jupyter_config" {
-  template = "${file("${path.module}/templates/jupyter_notebook_config.py.tpl")}"
-
-  vars {
-    notebook_user   = "jupyter"
-    notebook_port   = "8888"
-    hashed_password = "${var.hashed_password}"
-    bucket_name     = "${var.bucket_name}"
-  }
-}
-
-data "template_file" "userdata" {
-  template = "${file("${path.module}/templates/userdata.sh.tpl")}"
-
-  vars {
-    jupyter_notebook_config = "${data.template_file.jupyter_config.rendered}"
-    requirements            = "${data.template_file.requirements.rendered}"
-    notebook_user           = "jupyter"
-    default_environment     = "${var.default_environment}"
-    efs_mount_id            = "${var.efs_mount_id}"
-  }
-}
-
 resource "aws_autoscaling_schedule" "scale_down" {
   scheduled_action_name  = "ensure_down"
   min_size               = 0
