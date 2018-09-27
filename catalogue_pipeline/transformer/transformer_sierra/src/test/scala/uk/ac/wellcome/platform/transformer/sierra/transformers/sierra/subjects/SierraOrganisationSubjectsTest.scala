c62a68889584cc16ca6f2f0a03a9e38f5bda39dc
Alex Chan <alex@alexwlchan.net>
2018-09-27 12:47:14 +0100

    Add a test for populating the Organisation label

--- catalogue_pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/sierra/subjects/SierraOrganisationSubjectsTest.scala
+++ catalogue_pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/sierra/subjects/SierraOrganisationSubjectsTest.scala
@@ -1,8 +1,9 @@
 package uk.ac.wellcome.platform.transformer.sierra.transformers.sierra.subjects
 
 import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.models.work.internal.Unidentifiable
 import uk.ac.wellcome.platform.transformer.sierra.generators.SierraDataGenerators
-import uk.ac.wellcome.platform.transformer.sierra.source.{MarcSubfield, VarField}
+import uk.ac.wellcome.platform.transformer.sierra.source.{MarcSubfield, SierraBibData, VarField}
 
 class SierraOrganisationSubjectsTest extends FunSpec with Matchers with SierraDataGenerators {
   it("returns an empty list if there are no instances of MARC tag 610") {
@@ -10,67 +11,84 @@ class SierraOrganisationSubjectsTest extends FunSpec with Matchers with SierraDa
     transformer.getSubjectsWithOrganisation(bibData) shouldBe List()
   }
 
-  it("uses subfields a, b, c, d and e as the label") {
-    val bibData = createSierraBibDataWith(
-      varFields = List(
-        createMarc610VarField(
-          subfields = List(
-            MarcSubfield(tag = "a", content = "United States."),
-            MarcSubfield(tag = "b", content = "Supreme Court,"),
-            MarcSubfield(tag = "c", content = "Washington, DC."),
-            MarcSubfield(tag = "d", content = "September 29, 2005,"),
-            MarcSubfield(tag = "e", content = "pictured.")
-          )
+  describe("label") {
+    it("uses subfields a, b, c, d and e as the label") {
+      val bibData = create610bibDataWith(
+        subfields = List(
+          MarcSubfield(tag = "a", content = "United States."),
+          MarcSubfield(tag = "b", content = "Supreme Court,"),
+          MarcSubfield(tag = "c", content = "Washington, DC."),
+          MarcSubfield(tag = "d", content = "September 29, 2005,"),
+          MarcSubfield(tag = "e", content = "pictured.")
         )
       )
-    )
 
-    val subjects = transformer.getSubjectsWithOrganisation(bibData)
-    subjects should have size 1
+      val subjects = transformer.getSubjectsWithOrganisation(bibData)
+      subjects should have size 1
 
-    subjects.head.label shouldBe "United States. Supreme Court, Washington, DC. September 29, 2005, pictured."
+      subjects.head.label shouldBe "United States. Supreme Court, Washington, DC. September 29, 2005, pictured."
+    }
+
+    it("uses repeated subfields for the label if necessary") {
+      // This is based on an example from the MARC spec
+      val bibData = create610bibDataWith(
+        subfields = List(
+          MarcSubfield(tag = "a", content = "United States."),
+          MarcSubfield(tag = "b", content = "Army."),
+          MarcSubfield(tag = "b", content = "Cavalry, 7th."),
+          MarcSubfield(tag = "b", content = "Company E,"),
+          MarcSubfield(tag = "e", content = "depicted.")
+        )
+      )
+
+      val subjects = transformer.getSubjectsWithOrganisation(bibData)
+      subjects should have size 1
+
+      subjects.head.label shouldBe "United States. Army. Cavalry, 7th. Company E, depicted."
+    }
   }
 
-  it("uses repeated subfields for the label if necessary") {
-    // This is based on an example from the MARC spec
-    val bibData = createSierraBibDataWith(
-      varFields = List(
-        createMarc610VarField(
-          subfields = List(
-            MarcSubfield(tag = "a", content = "United States."),
-            MarcSubfield(tag = "b", content = "Army."),
-            MarcSubfield(tag = "b", content = "Cavalry, 7th."),
-            MarcSubfield(tag = "b", content = "Company E,"),
-            MarcSubfield(tag = "e", content = "depicted.")
-          )
+  describe("concepts") {
+    it("creates an Organisation as the concept") {
+      val bibData = create610bibDataWith(
+        subfields = List(
+          MarcSubfield(tag = "a", content = "Wellcome Trust."),
         )
       )
-    )
 
-    val subjects = transformer.getSubjectsWithOrganisation(bibData)
-    subjects should have size 1
+      val subjects = transformer.getSubjectsWithOrganisation(bibData)
+      val concepts = subjects.head.concepts
+      concepts should have size 1
 
-    subjects.head.label shouldBe "United States. Army. Cavalry, 7th. Company E, depicted."
-  }
+      val maybeDisplayableOrganisation = concepts.head
+      maybeDisplayableOrganisation shouldBe a[Unidentifiable[_]]
+    }
 
-  it("creates an Organisation as the concept") {
-    val bibData = createSierraBibDataWith(
-      varFields = List(
-        createMarc610VarField(
-          subfields = List(
-            MarcSubfield(tag = "a", content = "Wellcome Trust."),
-          )
+    it("uses subfields a and b for the Organisation label") {
+      val bibData = create610bibDataWith(
+        subfields = List(
+          MarcSubfield(tag = "a", content = "Wellcome Trust."),
+          MarcSubfield(tag = "b", content = "Facilities,"),
+          MarcSubfield(tag = "b", content = "Health & Safety"),
+          MarcSubfield(tag = "c", content = "27 September 2018")
         )
       )
-    )
 
-    val subjects = transformer.getSubjectsWithOrganisation(bibData)
-    subjects should have size 1
-
-    subjects.head.concepts should have size 1
+      val subjects = transformer.getSubjectsWithOrganisation(bibData)
+      val concepts = subjects.head.concepts
+      val organisation = concepts.head.agent
+      organisation.label shouldBe "Wellcome Trust. Facilities, Health and Safety"
+    }
   }
 
   val transformer = new SierraOrganisationSubjects {}
+
+  private def create610bibDataWith(subfields: List[MarcSubfield]): SierraBibData =
+    createSierraBibDataWith(
+      varFields = List(
+        createMarc610VarField(subfields = subfields)
+      )
+    )
 
   private def createMarc610VarField(subfields: List[MarcSubfield]): VarField =
     VarField(
