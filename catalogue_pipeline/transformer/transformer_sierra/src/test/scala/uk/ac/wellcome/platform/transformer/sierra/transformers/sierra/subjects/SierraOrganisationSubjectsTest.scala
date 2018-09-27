94c25f98681cb3301f688c29368bcd9572c8ba5f
Alex Chan <alex@alexwlchan.net>
2018-09-27 12:28:43 +0100

    Add a test for the subject label

--- catalogue_pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/sierra/subjects/SierraOrganisationSubjectsTest.scala
+++ catalogue_pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/sierra/subjects/SierraOrganisationSubjectsTest.scala
@@ -2,11 +2,36 @@ package uk.ac.wellcome.platform.transformer.sierra.transformers.sierra.subjects
 
 import org.scalatest.{FunSpec, Matchers}
 import uk.ac.wellcome.platform.transformer.sierra.generators.SierraDataGenerators
+import uk.ac.wellcome.platform.transformer.sierra.source.{MarcSubfield, VarField}
 
 class SierraOrganisationSubjectsTest extends FunSpec with Matchers with SierraDataGenerators {
   it("returns an empty list if there are no instances of MARC tag 610") {
     val bibData = createSierraBibDataWith(varFields = List())
     transformer.getSubjectsWithOrganisation(bibData) shouldBe List()
+  }
+
+  it("uses subfields a, b, c, d and e as the label") {
+    val bibData = createSierraBibDataWith(
+      varFields = List(
+        VarField(
+          marcTag = "610",
+          indicator1 = "",
+          indicator2 = "",
+          subfields = List(
+            MarcSubfield(tag = "a", content = "United States."),
+            MarcSubfield(tag = "b", content = "Supreme Court,"),
+            MarcSubfield(tag = "c", content = "Washington, DC."),
+            MarcSubfield(tag = "d", content = "September 29, 2005,"),
+            MarcSubfield(tag = "e", content = "depicted.")
+          )
+        )
+      )
+    )
+
+    val subjects = transformer.getSubjectsWithOrganisation(bibData)
+    subjects should have size 1
+
+    subjects.head.label shouldBe "United States. Supreme Court, Washington, DC. September 29, 2005, depicted."
   }
 
   val transformer = new SierraOrganisationSubjects {}
