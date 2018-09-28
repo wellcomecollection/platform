94bea247cbc32aba797f8e5b12c0b70b23de1640
Alex Chan <a.chan@wellcome.ac.uk>
2018-09-28 13:36:54 +0100

    Flip the API for exposing MARC 610 subjects

--- catalogue_api/terraform/api_pins.tf
+++ catalogue_api/terraform/api_pins.tf
@@ -1,7 +1,7 @@
 locals {
-  production_api       = "remus"
-  pinned_romulus_api   = ""
-  pinned_romulus_nginx = ""
-  pinned_remus_api     = "f57489bccfe80d6d0ba2feb7c86cc340319ce4f7"
-  pinned_remus_nginx   = "3dd8a423123e1d175dd44520fcf03435a5fc92c8"
+  production_api       = "romulus"
+  pinned_remus_api     = ""
+  pinned_remus_nginx   = ""
+  pinned_romulus_api   = "7de3c580dc0778fe436b4fe3516ca5a9d0468e3f"
+  pinned_romulus_nginx = "3dd8a423123e1d175dd44520fcf03435a5fc92c8"
 }
