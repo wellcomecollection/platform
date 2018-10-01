75801a52473678acf10f33fcd2f92310594f551a
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-01 17:12:51 +0100

    And SQS

--- archive/call_bäckerei/src/main/scala/uk/ac/wellcome/platform/archive/call_backerei/config/ArgsConfigurator.scala
+++ archive/call_bäckerei/src/main/scala/uk/ac/wellcome/platform/archive/call_backerei/config/ArgsConfigurator.scala
@@ -2,37 +2,20 @@ package uk.ac.wellcome.platform.archive.call_backerei.config
 
 import org.rogach.scallop.{ScallopConf, ScallopOption}
 import uk.ac.wellcome.messaging.sns.SNSConfig
-import uk.ac.wellcome.messaging.sqs.SQSConfig
 import uk.ac.wellcome.platform.archive.call_backerei.models.CallBackereiConfig
-import uk.ac.wellcome.platform.archive.common.config.{CloudWatchClientConfigurator, MetricsConfigConfigurator, SnsClientConfigurator, SqsClientConfigurator}
-
-import scala.concurrent.duration._
-
-
+import uk.ac.wellcome.platform.archive.common.config._
 
 class ArgsConfigurator(val arguments: Seq[String])
   extends ScallopConf(arguments)
   with CloudWatchClientConfigurator
   with MetricsConfigConfigurator
   with SnsClientConfigurator
-  with SqsClientConfigurator {
+  with SqsClientConfigurator
+  with SqsConfigConfigurator {
 
   val topicArn: ScallopOption[String] = opt[String](required = true)
-  val sqsQueueUrl: ScallopOption[String] = opt[String](required = true)
-  val sqsWaitTimeSeconds = opt[Int](required = true, default = Some(20))
-  val sqsMaxMessages = opt[Int](required = true, default = Some(10))
-  val sqsParallelism = opt[Int](required = true, default = Some(10))
-
-
 
   verify()
-
-  val sqsConfig = SQSConfig(
-    sqsQueueUrl(),
-    sqsWaitTimeSeconds() seconds,
-    sqsMaxMessages(),
-    sqsParallelism()
-  )
 
   val snsConfig = SNSConfig(
     topicArn(),
