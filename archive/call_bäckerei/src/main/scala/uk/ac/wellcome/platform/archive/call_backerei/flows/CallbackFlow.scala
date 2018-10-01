7ef54797bfe009e8eb6e244d6456fa91e953872f
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-01 16:44:23 +0100

    Add a massive comment to make this easier to read

--- archive/call_bäckerei/src/main/scala/uk/ac/wellcome/platform/archive/call_backerei/flows/CallbackFlow.scala
+++ archive/call_bäckerei/src/main/scala/uk/ac/wellcome/platform/archive/call_backerei/flows/CallbackFlow.scala
@@ -11,17 +11,37 @@ import uk.ac.wellcome.platform.archive.common.progress.models.Progress
 
 import scala.util.Try
 
+/** Represents the payload sent to a callback URL.
+  *
+  * @param id ID of the ingest request that we're sending a callback for.
+  */
 case class CallbackPayload(id: String)
 
-case class CallbackResult(
+case class CallbackFlowResult(
   progress: Progress,
   httpResponse: Option[Try[HttpResponse]]
 )
 
+/** This flow receives a [[Progress]] instance, as received from
+  * an SQS queue, and makes the callback request (if necessary).
+  *
+  * After this flow has done, we need to know two things:
+  *   - The original Progress object
+  *   - Whether the callback succeeded (if necessary) -- we record
+  *     the callback result.
+  *
+  */
 object CallbackFlow {
-  def apply()(implicit actorSystem: ActorSystem): Flow[Progress, CallbackResult, NotUsed] = {
-    val http = Http().superPool[Progress]()
+  def apply()(implicit actorSystem: ActorSystem): Flow[Progress, CallbackFlowResult, NotUsed] = {
 
+    // This flow handles the case where there is a callback URL on
+    // the progress object.
+    //
+    // The HTTP pool takes an tuple (HttpRequest, Progress), and returns
+    // a (Try[HttpResponse], Progress) tuple -- preserving the original
+    // progress as context.
+    //
+    val http = Http().superPool[Progress]()
     val withCallbackUrlFlow = Flow[Progress]
       .collect {
         case progress@Progress(id, _, Some(callbackUrl), _, _, _, _) =>
@@ -30,20 +50,50 @@ object CallbackFlow {
       .via(http)
       .map {
         case (tryHttpResponse, progress) =>
-          CallbackResult(
+          CallbackFlowResult(
             progress = progress,
             httpResponse = Some(tryHttpResponse)
           )
       }
 
+    // This flow handles the case where there isn't a callback URL on
+    // the progress object -- there's nothing to do, just prepare the result.
+    //
     val withoutCallbackUrlFlow = Flow[Progress]
       .filter { _.callbackUrl.isEmpty }
       .map { progress: Progress =>
-        CallbackResult(progress = progress, httpResponse = None)
+        CallbackFlowResult(progress = progress, httpResponse = None)
       }
 
+    // This creates a custom Akka graph stage, and splits Progress objects
+    // based on whether they have a callback URL.
+    //
+    //                              Broadcast                     Merge
+    //
+    //                  +-----> withCallbackUrlFlow     ~~~> [CallbackFlowResult]
+    //                  |       (has a callback URL)
+    //    [Progress] ---+
+    //                  |
+    //                  +-----> withoutCallbackUrlFlow  ~~~> [CallbackFlowResult]
+    //                          (no callback URL)
+    //
+    // How it works:
+    //
+    //  1.  Every instance of Progress sent to the original flow is duplicated,
+    //      and sent to both `withCallbackUrlFlow` and `withoutCallbackUrlFlow`.
+    //      (This is the `Broadcast` component.)
+    //
+    //  2.  Each flow filters out half the messages, so each Progress is handled
+    //      by exactly once flow.
+    //
+    //  3.  Each flow processes Progress messages and turns them into instances of
+    //      CallbackFlowResult.
+    //
+    //  4.  The result is two flows that both take [Progress ~> CallbackFlowResult].
+    //      (This is the `Merge` component.)
+    //
     Flow.fromGraph(
-      GraphDSL.create(Broadcast[Progress](2), Merge[CallbackResult](2))(Keep.none) {
+      GraphDSL.create(Broadcast[Progress](2), Merge[CallbackFlowResult](2))(Keep.none) {
         implicit builder =>
           import GraphDSL.Implicits._
           (broadcast, merge) =>
