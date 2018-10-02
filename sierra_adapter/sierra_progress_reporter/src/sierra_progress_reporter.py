b7f3d0f572f0ea40fb6e5961c7960db86724aef6
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-02 12:16:44 +0100

    Skeleton of the progress reporter

---
+++ sierra_adapter/sierra_progress_reporter/src/sierra_progress_reporter.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+"""
+Query the S3 bucket containing Sierra progress reports, and log
+a report in Slack
+"""
+
+def main(event=None, _ctxt=None):
+    print('Hello world!')
---
+++ sierra_adapter/sierra_progress_reporter/src/test_sierra_progress_reporter.py
@@ -0,0 +1,4 @@
+# -*- encoding: utf-8 -*-
+
+def test1():
+    assert True
