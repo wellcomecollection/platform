b033c78142469db5e7986830424719f0e0de8f2b
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-02 12:25:45 +0100

    Add some code to list keys in an S3 bucket

--- sierra_adapter/sierra_progress_reporter/src/sierra_progress_reporter.py
+++ sierra_adapter/sierra_progress_reporter/src/sierra_progress_reporter.py
@@ -4,5 +4,18 @@ Query the S3 bucket containing Sierra progress reports, and log
 a report in Slack
 """
 
+import boto3
+
+
+def get_matching_s3_keys(s3_client, bucket, prefix):
+    """
+    Generate the keys in an S3 bucket that match a given prefix.
+    """
+    paginator = s3_client.get_paginator('list_objects')
+    for page in paginator.paginate(Bucket=bucket, Prefix=prefix):
+        for s3_object in page['Contents']:
+            yield s3_object['Key']
+
+
 def main(event=None, _ctxt=None):
     print('Hello world!')
