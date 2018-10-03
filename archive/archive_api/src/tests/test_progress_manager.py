cc3659826b517dc4ed031dd597e36df4ed2dcf17
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-03 14:44:55 +0100

    Add more tests for the happy path

--- archive/archive_api/src/tests/test_progress_manager.py
+++ archive/archive_api/src/tests/test_progress_manager.py
@@ -20,6 +20,12 @@ def sess():
 
 @pytest.fixture(scope='session')
 def progress_manager(sess):
+    # AWLC: Currently these cassettes were recorded by running against
+    # an imitation progress app written in Flask.
+    #
+    # At some point, swap out the cassettes for genuine responses from
+    # a real progress app.
+    #
     return ProgressManager(endpoint='http://localhost:6000', sess=sess)
 
 
@@ -27,4 +33,11 @@ def test_can_create_ingest_request(progress_manager):
     progress_manager.create_request(
         upload_url='http://example.org/',
         callback_url=None
+    )
+
+
+def test_can_create_ingest_request_with_callback_url(progress_manager):
+    progress_manager.create_request(
+        upload_url='http://example.org',
+        callback_url='http://callback.net?id=123'
     )
