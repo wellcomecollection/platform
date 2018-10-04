d74be26adef7d451411f62af77afdad198efde81
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-04 15:36:18 +0100

    And actually use the @context field

--- archive/archive_api/src/responses.py
+++ archive/archive_api/src/responses.py
@@ -1,5 +1,7 @@
 # -*- encoding: utf-8
 
+import json
+
 from flask import Response, jsonify
 
 
@@ -11,6 +13,20 @@ class ContextResponse(Response):
     For an explanation of how this works/is used, read
     https://blog.miguelgrinberg.com/post/customizing-the-flask-response-class
     """
+    context_url = "https://api.wellcomecollection.org/storage/v1/context.json"
+
+    def __init__(self, response, **kwargs):
+        # Here we unmarshal the response as provided by Flask-RESTPlus, add
+        # the @context parameter, then repack it.
+        rv = json.loads(response)
+
+        # The @context may already be provided if we've been through the
+        # force_type method below.
+        if "@context" in rv:
+            return super(ContextResponse, self).__init__(response, **kwargs)
+        else:
+            rv["@context"] = self.context_url
+            return super(ContextResponse, self).__init__(json.dumps(rv), **kwargs)
 
     @classmethod
     def force_type(cls, rv, environ=None):
@@ -18,7 +34,7 @@ class ContextResponse(Response):
         # serialised as JSON.
         assert isinstance(rv, dict)
 
-        assert "@context" not in rv
-        rv["@context"] = "https://api.wellcomecollection.org/storage/v1/context.json"
+        assert "@context" not in rv, rv
+        rv["@context"] = cls.context_url
 
         return super(ContextResponse, cls).force_type(jsonify(rv), environ)
