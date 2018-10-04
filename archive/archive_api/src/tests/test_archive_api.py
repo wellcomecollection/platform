2ec9c5e3c6f94b182461e778682de0f76f22f1a4
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-04 15:52:11 +0100

    Fix the healthcheck endpoint test

--- archive/archive_api/src/tests/test_archive_api.py
+++ archive/archive_api/src/tests/test_archive_api.py
@@ -210,7 +210,7 @@ class TestReportHealthStatus:
     def test_get_healthcheck_endpoint_is_200_OK(self, client):
         resp = client.get("/storage/v1/healthcheck")
         assert resp.status_code == 200
-        assert json.loads(resp.data) == {"status": "OK"}
+        assert json.loads(resp.data)["status"] == "OK"
 
 
 def ingests_post(upload_url=None, callback_url=None):
