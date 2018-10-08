c5e63e166c78ed3d9b3f23428c49a1d8f603b405
Alex Chan <alex@alexwlchan.net>
2018-10-08 13:33:11 +0100

    Create two helpers for the models

---
+++ archive/archive_api/src/models/__init__.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8
+
+import types
+
+from ._base import TypedModel
+
+
+def register_models(namespace, models):
+     """
+     Register all the models in a module against the given namespace.
+
+     We have to do this because Flask-RESTPlus doesn't register models
+     recursively -- so if we have Foo(id: str, bar: Bar), then registering
+     the Foo model doesn't get us the Bar model, and then the Swagger
+     fails to render.  Boo.
+     """
+     # How it works: for a module, the __dict__ contains a mapping from
+     # name to importable object.  By iterating over the values, we get
+     # everything that you can import from the module.
+     #
+     assert isinstance(models, types.ModuleType)
+     for m in models.__dict__.values():
+         if not isinstance(m, TypedModel):
+             continue
+
+         # This function gets called recursively (somewhere) -- this stops
+         # us triggering an infinite recursion.
+         if m.name not in namespace.models:
+             namespace.add_model(m.name, m)
+
+
+__all__ = [
+    "register_models",
+]
---
+++ archive/archive_api/src/models/_base.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8
+
+from flask_restplus import fields, Model
+
+
+class TypedModel(Model):
+    """
+    A thin wrapper around ``Model`` that adds a ``type`` field.
+    """
+
+    def __init__(self, name, model_fields, *args, **kwargs):
+
+        # When you use a model in ``@api.response``, it triggers an internal
+        # cloning operation, which passes the ``model_fields`` parameter as
+        # a list of 2-tuples.
+        #
+        # In that case, we should already have a ``type`` field, so we
+        # can skip adding it.
+        #
+        if isinstance(model_fields, dict) and "type" not in model_fields:
+            model_fields["type"] = fields.String(
+                description="Type of the object",
+                enum=[name],
+                default=name
+            )
+
+        super().__init__(name, model_fields, *args, **kwargs)
