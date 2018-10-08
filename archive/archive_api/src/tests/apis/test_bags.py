e9591fa826747abf243d48eaecd28efb720dbc72
Alex Chan <alex@alexwlchan.net>
2018-10-08 11:56:07 +0100

    Add extra tests around the /bags endpoint

--- archive/archive_api/src/tests/apis/test_bags.py
+++ archive/archive_api/src/tests/apis/test_bags.py
@@ -5,8 +5,13 @@ import json
 from helpers import assert_is_error_response
 
 
-def test_lookup_bag(
-    client, dynamodb_resource, s3_client, guid, bucket_bag, table_name_bag
+def test_returns_a_present_bag(
+    client,
+    dynamodb_resource,
+    table_name_bag,
+    s3_client,
+    bucket_bag,
+    guid
 ):
     stored_bag = {"id": guid}
 
@@ -19,16 +24,51 @@ def test_lookup_bag(
 
     resp = client.get(f"/storage/v1/bags/{guid}")
     assert resp.status_code == 200
-    assert json.loads(resp.data) == {
-        "@context": "https://api.wellcomecollection.org/storage/v1/context.json",
-        "id": guid,
-    }
+
+    rv = json.loads(resp.data)
+    assert rv["id"] == guid
+    assert rv["@context"] == "https://api.wellcomecollection.org/storage/v1/context.json"
+    assert rv["type"] == "Bag"
 
 
-def test_lookup_missing_item_is_404(client, guid):
-    resp = client.get(f"/storage/v1/bags/{guid}")
-    assert_is_error_response(
-        resp,
-        status=404,
-        description="Invalid id: No bag found for id=%r" % guid
+def test_returns_500_if_s3_object_missing(
+    client,
+    dynamodb_resource,
+    table_name_bag,
+    bucket_bag,
+    guid
+):
+    stored_bag = {"id": guid}
+
+    table = dynamodb_resource.Table(table_name_bag)
+    table.put_item(
+        Item={"id": guid, "location": {"key": guid, "namespace": bucket_bag}}
     )
+
+    resp = client.get(f"/storage/v1/bags/{guid}")
+    assert_is_error_response(resp, status=500)
+
+
+def test_returns_500_if_malformed_dynamodb(
+    client,
+    dynamodb_resource,
+    table_name_bag,
+    guid
+):
+    table = dynamodb_resource.Table(table_name_bag)
+    table.put_item(
+        Item={"id": guid, "location": {"k_y": guid, "n_m_s_c_e": "bukkit"}}
+    )
+
+    resp = client.get(f"/storage/v1/bags/{guid}")
+    assert_is_error_response(resp, status=500)
+
+
+def test_returns_404_if_no_such_bag(client, guid):
+    resp = client.get(f"/storage/v1/bags/{guid}")
+    assert_is_error_response(resp, status=404, description=f"No bag found for id='{guid}'")
+
+
+def test_returns_405_if_try_to_post(client):
+    resp = client.post(f"/storage/v1/bags/123")
+    assert_is_error_response(resp, status=405, description="The method is not allowed for the requested URL.")
