df39f06fe4863413e82d215eb11d7ebf755722eb
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-08 10:57:36 +0100

    Remove a pile of unused imports

--- archive/archive_api/src/views.py
+++ archive/archive_api/src/views.py
@@ -1,17 +1,6 @@
 # -*- encoding: utf-8
 
-
-from flask import jsonify, make_response, request
-from flask_restplus import Resource
-from werkzeug.exceptions import BadRequest as BadRequestError
-from werkzeug.exceptions import NotFound as NotFoundError
-
 from archive_api import app, api, logger
-from ingests import send_new_ingest_request
-import models
-from progress_manager import ProgressNotFoundError
-from storage import read_from_vhs, VHSNotFound
-import validators
 
 
 progress_manager = app.config["PROGRESS_MANAGER"]
