ee132cfcecb0469af3fad499677cc1858ce44535
Alex Chan <alex@alexwlchan.net>
2018-10-08 12:50:15 +0100

    Tighten up the tests; use more fully-fledged requests

--- archive/archive_api/src/tests/apis/test_ingests.py
+++ archive/archive_api/src/tests/apis/test_ingests.py
@@ -2,6 +2,8 @@
 
 import json
 
+import pytest
+
 from helpers import assert_is_error_response
 
 
@@ -42,125 +44,108 @@ class TestPOST:
     Tests for the POST /ingests endpoint.
     """
 
-    upload_url = "s3://example-bukkit/helloworld.zip"
-    callback_url = "https://example.com/post?callback"
-
-    def test_request_new_ingest_is_201(self, client):
-        resp = client.post(
-            "/storage/v1/ingests", json=_create_ingest_request(self.upload_url)
-        )
+    def test_request_new_ingest_is_201(self, client, ingest_request):
+        resp = client.post("/storage/v1/ingests", json=ingest_request)
         assert resp.status_code == 201
         assert resp.data == b""
 
-    def test_no_type_is_badrequest(self, client):
-        resp = client.post("/storage/v1/ingests", json={})
+    def test_no_type_is_badrequest(self, client, ingest_request):
+        del ingest_request["type"]
+        resp = client.post("/storage/v1/ingests", json=ingest_request)
         assert_is_error_response(
             resp,
             status=400,
-            description="'ingestType' is a required property, 'type' is a required property, 'uploadUrl' is a required property"
+            description="'type' is a required property"
         )
 
-    def test_invalid_type_is_badrequest(self, client):
-        resp = client.post("/storage/v1/ingests", json={"type": "UnexpectedType"})
+    def test_invalid_type_is_badrequest(self, client, ingest_request):
+        ingest_request["type"] = "UnexpectedType"
+        resp = client.post("/storage/v1/ingests", json=ingest_request)
         assert_is_error_response(
             resp,
             status=400,
-            description="'ingestType' is a required property, 'uploadUrl' is a required property, 'UnexpectedType' is not one of ['Ingest']"
+            description="'UnexpectedType' is not one of ['Ingest']"
         )
 
-    def test_no_ingest_type_is_badrequest(self, client):
-        resp = client.post("/storage/v1/ingests", json={"type": "Ingest", "uploadUrl": "http://example.net"})
+    def test_no_ingest_type_is_badrequest(self, client, ingest_request):
+        del ingest_request["ingestType"]
+        resp = client.post("/storage/v1/ingests", json=ingest_request)
         assert_is_error_response(
             resp,
             status=400,
             description="'ingestType' is a required property"
         )
 
-    def test_invalid_ingest_type_is_badrequest(self, client):
-        resp = client.post(
-            "/storage/v1/ingests",
-            json={"type": "Ingest", "ingestType": {"type": "UnexpectedIngestType"}},
-        )
+    def test_invalid_ingest_type_is_badrequest(self, client, ingest_request):
+        ingest_request["ingestType"] = {"type": "UnexpectedIngestType"}
+        resp = client.post("/storage/v1/ingests", json=ingest_request)
         assert_is_error_response(
             resp,
             status=400,
-            # TODO: Really?  You want an ID?
-            description="'uploadUrl' is a required property, 'id' is a required property, 'UnexpectedIngestType' is not one of ['IngestType']"
+            description="'UnexpectedIngestType' is not one of ['IngestType']"
         )
 
-    def test_no_uploadurl_is_badrequest(self, client):
-        resp = client.post("/storage/v1/ingests", json=_create_ingest_request())
+    def test_no_uploadurl_is_badrequest(self, client, ingest_request):
+        del ingest_request["uploadUrl"]
+        resp = client.post("/storage/v1/ingests", json=ingest_request)
         assert_is_error_response(
             resp,
             status=400,
             description="'uploadUrl' is a required property"
         )
 
-    def test_invalid_uploadurl_is_badrequest(self, client):
-        resp = client.post("/storage/v1/ingests", json=_create_ingest_request("not-a-url"))
+    def test_invalid_uploadurl_is_badrequest(self, client, ingest_request):
+        ingest_request["uploadUrl"] = "not-a-url"
+        resp = client.post("/storage/v1/ingests", json=ingest_request)
         assert_is_error_response(
             resp,
             status=400,
             description="Invalid uploadUrl:'not-a-url', is not a complete URL, '' is not a supported scheme ['s3']"
         )
 
-    def test_invalid_scheme_uploadurl_is_badrequest(self, client):
-        resp = client.post(
-            "/storage/v1/ingests",
-            json=_create_ingest_request("ftp://example-bukkit/helloworld.zip"),
-        )
+    def test_invalid_scheme_uploadurl_is_badrequest(self, client, ingest_request):
+        ingest_request["uploadUrl"] = "ftp://example-bukkit/helloworld.zip"
+        resp = client.post("/storage/v1/ingests", json=ingest_request)
         assert_is_error_response(
             resp,
             status=400,
             description="Invalid uploadUrl:'ftp://example-bukkit/helloworld.zip', 'ftp' is not a supported scheme ['s3']"
         )
 
-    def test_uploadurl_with_fragments_is_badrequest(self, client):
-        resp = client.post(
-            "/storage/v1/ingests",
-            json=_create_ingest_request("s3://example-bukkit/helloworld.zip#fragment"),
-        )
+    def test_uploadurl_with_fragments_is_badrequest(self, client, ingest_request):
+        ingest_request["uploadUrl"] = "s3://example-bukkit/helloworld.zip#fragment"
+        resp = client.post("/storage/v1/ingests", json=ingest_request)
         assert_is_error_response(
             resp,
             status=400,
             description="Invalid uploadUrl:'s3://example-bukkit/helloworld.zip#fragment', 'fragment' fragment is not allowed"
         )
 
-    def test_invalid_callback_url_is_badrequest(self, client):
-        resp = client.post(
-            "/storage/v1/ingests",
-            json=_create_ingest_request(self.upload_url, "not-a-url"),
-        )
+    def test_invalid_callback_url_is_badrequest(self, client, ingest_request):
+        ingest_request["callbackUrl"] = "not-a-url"
+        resp = client.post("/storage/v1/ingests", json=ingest_request)
         assert_is_error_response(
             resp,
             status=400,
             description="Invalid callbackUrl:'not-a-url', is not a complete URL, '' is not a supported scheme ['http', 'https']"
         )
 
-    def test_invalid_scheme_callback_url_is_badrequest(self, client):
-        resp = client.post(
-            "/storage/v1/ingests",
-            json=_create_ingest_request(self.upload_url, "s3://example.com"),
-        )
+    def test_invalid_scheme_callback_url_is_badrequest(self, client, ingest_request):
+        ingest_request["callbackUrl"] = "s3://example.com"
+        resp = client.post("/storage/v1/ingests", json=ingest_request)
         assert_is_error_response(
             resp,
             status=400,
             description="Invalid callbackUrl:'s3://example.com', 's3' is not a supported scheme ['http', 'https']"
         )
 
-    def test_request_allows_fragment_in_callback(self, client):
-        resp = client.post(
-            "/storage/v1/ingests",
-            json=_create_ingest_request(
-                self.upload_url, f"{self.callback_url}#fragment"
-            ),
-        )
+    def test_request_allows_fragment_in_callback(self, client, ingest_request):
+        ingest_request["callbackUrl"] += "#fragment"
+        resp = client.post("/storage/v1/ingests", json=ingest_request)
         assert resp.status_code == 201
 
-    def test_request_new_ingest_has_location_header(self, client):
-        resp = client.post(
-            "/storage/v1/ingests", json=_create_ingest_request(self.upload_url)
-        )
+    def test_request_new_ingest_has_location_header(self, client, ingest_request):
+        resp = client.post("/storage/v1/ingests", json=ingest_request)
         assert "Location" in resp.headers
 
         # TODO: This might need revisiting when we deploy the app behind
@@ -168,14 +153,15 @@ class TestPOST:
         new_location = resp.headers["Location"]
         assert new_location.startswith("http://localhost/storage/v1/ingests/")
 
-    def test_successful_request_sends_to_sns(self, client, sns_client):
-        resp = client.post(
-            "/storage/v1/ingests", json=_create_ingest_request(self.upload_url)
-        )
+    def test_successful_request_sends_to_sns(self, client, sns_client, ingest_request):
+        del ingest_request["callbackUrl"]
+        resp = client.post("/storage/v1/ingests", json=ingest_request)
 
         sns_messages = sns_client.list_messages()
         assert len(sns_messages) == 1
         message = sns_messages[0][":message"]
+
+        assert "archiveCompleteCallbackUrl" not in message
 
         assert message["zippedBagLocation"] == {
             "namespace": "example-bukkit",
@@ -186,18 +172,15 @@ class TestPOST:
         # the one we've been given to look up the request later.
         assert resp.headers["Location"].endswith(message["archiveRequestId"])
 
-    def test_successful_request_sends_to_sns_with_callback(self, client, sns_client):
-        client.post(
-            "/storage/v1/ingests",
-            json=_create_ingest_request(self.upload_url, self.callback_url),
-        )
+    def test_successful_request_sends_to_sns_with_callback(self, client, sns_client, ingest_request):
+        client.post("/storage/v1/ingests", json=ingest_request)
 
         sns_messages = sns_client.list_messages()
         assert len(sns_messages) == 1
         message = sns_messages[0][":message"]
 
         assert "archiveCompleteCallbackUrl" in message
-        assert message["archiveCompleteCallbackUrl"] == self.callback_url
+        assert message["archiveCompleteCallbackUrl"] == ingest_request["callbackUrl"]
 
         resp = client.get("/storage/v1/ingests")
         assert_is_error_response(
@@ -219,10 +202,14 @@ class TestPOST:
         )
 
 
-def _create_ingest_request(upload_url=None, callback_url=None):
-    request = {"type": "Ingest", "ingestType": {"id": "create", "type": "IngestType"}}
-    if upload_url:
-        request["uploadUrl"] = upload_url
-    if callback_url:
-        request["callbackUrl"] = callback_url
-    return request
+@pytest.fixture
+def ingest_request():
+    return {
+        "type": "Ingest",
+        "ingestType": {
+            "id": "create",
+            "type": "IngestType"
+        },
+        "uploadUrl": "s3://example-bukkit/helloworld.zip",
+        "callbackUrl": "https://example.com/post?callback"
+    }
