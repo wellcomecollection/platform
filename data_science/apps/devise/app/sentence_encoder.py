fe237b5ddb9b8c3e7d0b3193edde60ecd1bc1b51
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-10-09 09:14:40 +0000

    Apply auto-formatting rules

--- data_science/apps/devise/app/sentence_encoder.py
+++ data_science/apps/devise/app/sentence_encoder.py
@@ -1,5 +1,5 @@
 import nltk
-import numpy as np 
+import numpy as np
 import torch
 from torch import nn
 from utils import sentence_to_indexes
@@ -9,12 +9,11 @@ class SentenceEncoder(nn.Module):
     def __init__(self, index_to_wordvec):
         super(SentenceEncoder, self).__init__()
         self.index_to_wordvec = torch.FloatTensor(index_to_wordvec)
-        self.enc_lstm = nn.LSTM(input_size=300, 
-                                hidden_size=512, 
-                                num_layers=1,
-                                bidirectional=True)
-        
+        self.enc_lstm = nn.LSTM(
+            input_size=300, hidden_size=512, num_layers=1, bidirectional=True
+        )
+
     def forward(self, wv_batch):
         embedded, _ = self.enc_lstm(wv_batch)
-        max_pooled = torch.max(embedded, 1)[0] 
+        max_pooled = torch.max(embedded, 1)[0]
         return max_pooled
