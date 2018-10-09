031b3406e1de33085b5819fbbcf6e1891b8f84f6
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-09 15:35:29 +0100

    Add some comments to BagItemCreator, and kwargss

--- archive/archivist/src/main/scala/uk/ac/wellcome/platform/archive/archivist/bag/BagItemCreator.scala
+++ archive/archivist/src/main/scala/uk/ac/wellcome/platform/archive/archivist/bag/BagItemCreator.scala
@@ -7,21 +7,34 @@ import uk.ac.wellcome.platform.archive.archivist.models.errors.{
 import uk.ac.wellcome.platform.archive.common.models.{BagItem, EntryPath}
 
 object BagItemCreator {
+
+  /** Within a manifest, each entry in the bag is a single line, containing
+    * a checksum and the location.  For example:
+    *
+    *     676...8c32  data/b12345678.xml
+    *     593...5c5  data/alto/b12345678_0001.xml
+    *     26a...78c  data/alto/b12345678_0002.xml
+    *
+    * Given a single line, this method extracts the checksum and the
+    * location, or returns an error if the line is incorrectly formatted.
+    *
+    */
   def create(
-    fileChunk: String,
+    line: String,
     job: ArchiveJob,
     manifestName: String,
     delimiter: String
   ): Either[ArchiveError[ArchiveJob], BagItem] = {
-    val splitChunk = fileChunk.split(delimiter).map(_.trim)
+    val splitChunk = line.split(delimiter).map(_.trim)
 
     splitChunk match {
       case Array(checksum: String, key: String) =>
         Right(
           BagItem(
-            checksum,
-            EntryPath(key)
-          ))
+            checksum = checksum,
+            location = EntryPath(key)
+          )
+        )
       case _ =>
         Left(InvalidBagManifestError(job, manifestName))
     }
