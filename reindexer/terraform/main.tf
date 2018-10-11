a3cbd7659fc268643cb6147a3435117bae0cee4e
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-11 10:09:34 +0100

    Create a new sierra_items reindexer

--- reindexer/terraform/main.tf
+++ reindexer/terraform/main.tf
@@ -25,3 +25,17 @@ module "sierra_reindexer" {
   service_egress_security_group_id = "${aws_security_group.service_egress_security_group.id}"
   namespace_id                     = "${aws_service_discovery_private_dns_namespace.namespace.id}"
 }
+
+module "sierra_items_reindexer" {
+  source = "./reindex_worker"
+
+  namespace      = "sierra_items"
+  vhs_table_name = "${local.vhs_sierra_table_name}"
+
+  reindex_worker_container_image = "${local.reindex_worker_container_image}"
+
+  ecs_cluster_name                 = "${aws_ecs_cluster.cluster.name}"
+  ecs_cluster_id                   = "${aws_ecs_cluster.cluster.id}"
+  service_egress_security_group_id = "${aws_security_group.service_egress_security_group.id}"
+  namespace_id                     = "${aws_service_discovery_private_dns_namespace.namespace.id}"
+}
