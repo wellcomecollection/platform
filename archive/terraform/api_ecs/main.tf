8ce22497ff5ccee64892d3b5f9eafa854ac567d3
alicefuzier <alicefuzier@gmail.com>
2018-10-18 15:11:03 +0100

    Removing not needed permissions and environment variables

--- archive/terraform/api_ecs/main.tf
+++ archive/terraform/api_ecs/main.tf
@@ -22,9 +22,6 @@ module "api_ecs" {
 
     PROGRESS_MANAGER_ENDPOINT = "http://progress_http.archive-storage:9001"
     BAGS_MANAGER_ENDPOINT = "http://registrar_http.archive-storage:9001"
-
-    BAG_VHS_TABLE_NAME  = "${var.bag_vhs_table_name}"
-    BAG_VHS_BUCKET_NAME = "${var.bag_vhs_bucket_name}"
   }
 
   env_vars_length = 4
