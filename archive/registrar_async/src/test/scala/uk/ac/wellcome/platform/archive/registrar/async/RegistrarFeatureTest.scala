667b9dad9474645eda78acfe0f4dc05cad05798d
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-10-18 12:49:37 +0000

    Apply auto-formatting rules

--- archive/registrar_async/src/test/scala/uk/ac/wellcome/platform/archive/registrar/async/RegistrarFeatureTest.scala
+++ archive/registrar_async/src/test/scala/uk/ac/wellcome/platform/archive/registrar/async/RegistrarFeatureTest.scala
@@ -1,15 +1,26 @@
 package uk.ac.wellcome.platform.archive.registrar.async
 
 import org.scalatest.{FunSpec, Inside, Matchers}
-import org.scalatest.concurrent.{IntegrationPatience, PatienceConfiguration, ScalaFutures}
+import org.scalatest.concurrent.{
+  IntegrationPatience,
+  PatienceConfiguration,
+  ScalaFutures
+}
 import org.scalatest.time.{Millis, Seconds, Span}
 import uk.ac.wellcome.messaging.test.fixtures.SQS.QueuePair
 import uk.ac.wellcome.monitoring.fixtures.MetricsSenderFixture
 import uk.ac.wellcome.platform.archive.common.fixtures.RandomThings
-import uk.ac.wellcome.platform.archive.common.models.{ArchiveComplete, BagLocation, BagPath}
+import uk.ac.wellcome.platform.archive.common.models.{
+  ArchiveComplete,
+  BagLocation,
+  BagPath
+}
 import uk.ac.wellcome.platform.archive.common.progress.ProgressUpdateAssertions
 import uk.ac.wellcome.platform.archive.common.progress.models.progress.Progress
-import uk.ac.wellcome.platform.archive.registrar.async.fixtures.{RegistrarFixtures, RegistrationCompleteAssertions}
+import uk.ac.wellcome.platform.archive.registrar.async.fixtures.{
+  RegistrarFixtures,
+  RegistrationCompleteAssertions
+}
 import uk.ac.wellcome.platform.archive.registrar.async.models.RegistrationCompleteNotification
 import uk.ac.wellcome.storage.dynamo._
 import uk.ac.wellcome.json.JsonUtil._
