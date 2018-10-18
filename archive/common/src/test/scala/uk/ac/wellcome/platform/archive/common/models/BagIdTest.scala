d264ade99fe4291dac84bf90b36f6394ec7b8567
alicefuzier <alicefuzier@gmail.com>
2018-10-18 12:42:07 +0100

    Test that bagId gets serialised correctly

---
+++ archive/common/src/test/scala/uk/ac/wellcome/platform/archive/common/models/BagIdTest.scala
@@ -0,0 +1,19 @@
+package uk.ac.wellcome.platform.archive.common.models
+import org.scalatest.FunSpec
+import uk.ac.wellcome.json.JsonUtil._
+import uk.ac.wellcome.json.utils.JsonAssertions
+
+class BagIdTest extends FunSpec with JsonAssertions {
+  it("serialises with strings") {
+    val bagId = BagId(StorageSpace("digitised"), ExternalIdentifier("b1234567x"))
+    val expectedJson =
+      s"""
+         |{
+         |  "space": "digitised",
+         |  "externalIdentifier": "b1234567x"
+         |}
+       """.stripMargin
+    assertJsonStringsAreEqual(toJson(bagId).get, expectedJson)
+  }
+
+}
