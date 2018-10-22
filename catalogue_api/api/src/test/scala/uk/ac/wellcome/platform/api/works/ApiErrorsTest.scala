9e5f0a05c9742caaef342ba5a110446b4c5ffc8c
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-22 15:44:16 +0100

    Improve the quality of these tests

--- catalogue_api/api/src/test/scala/uk/ac/wellcome/platform/api/works/ApiErrorsTest.scala
+++ catalogue_api/api/src/test/scala/uk/ac/wellcome/platform/api/works/ApiErrorsTest.scala
@@ -8,15 +8,26 @@ import uk.ac.wellcome.test.fixtures.TestWith
 class ApiErrorsTest extends ApiWorksTestBase {
 
   it("returns a Not Found error if you try to get an API version") {
-    withServer(indexNameV1 = "not-important", indexNameV2 = "not-important") {
-      server =>
-        server.httpGet(
-          path = "/catalogue/v567/works",
-          andExpect = Status.NotFound,
-          withJsonBody = badRequest(
-            s"catalogue/${ApiVersions.default.toString}",
-            "v567 is not a valid API version")
-        )
+    withServer { server =>
+      server.httpGet(
+        path = "/catalogue/v567/works",
+        andExpect = Status.NotFound,
+        withJsonBody = badRequest(
+          s"catalogue/${ApiVersions.default.toString}",
+          "v567 is not a valid API version")
+      )
+    }
+  }
+
+  it("returns a Not Found error if you try to get an unrecognised path") {
+    withServer { server =>
+      server.httpGet(
+        path = "/foo/bar",
+        andExpect = Status.NotFound,
+        withJsonBody = badRequest(
+          s"catalogue/${ApiVersions.default.toString}",
+          "v567 is not a valid API version")
+      )
     }
   }
 
