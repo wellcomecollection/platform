d235a2b8a0e85b353ceecc4da20616683cb2939a
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-10-22 17:32:12 +0100

    iam fixes

--- reporting/terraform/iam_role_policy.tf
+++ reporting/terraform/iam_role_policy.tf
@@ -1,9 +1,9 @@
-resource "aws_iam_role_policy" "ecs_transformer_task_vhs" {
+resource "aws_iam_role_policy" "miro_vhs_read_transformer_example" {
   role   = "${module.lambda_transformer_example.role_name}"
   policy = "${local.miro_vhs_read_policy}"
 }
 
-resource "aws_iam_role_policy" "ecs_transformer_task_vhs" {
+resource "aws_iam_role_policy" "sierra_vhs_read_transformer_task_vhs" {
   role   = "${module.lambda_transformer_example.role_name}"
   policy = "${local.sierra_vhs_read_policy}"
 }
