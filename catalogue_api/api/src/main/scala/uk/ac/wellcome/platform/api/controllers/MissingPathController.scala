313622c34f5343b600f8b55e62e68fd202414a74
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-22 16:01:55 +0100

    Use a sensible default in this helper method

--- catalogue_api/api/src/main/scala/uk/ac/wellcome/platform/api/controllers/MissingPathController.scala
+++ catalogue_api/api/src/main/scala/uk/ac/wellcome/platform/api/controllers/MissingPathController.scala
@@ -3,7 +3,6 @@ package uk.ac.wellcome.platform.api.controllers
 import com.google.inject.{Inject, Singleton}
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
-import uk.ac.wellcome.display.models.ApiVersions
 import uk.ac.wellcome.platform.api.ContextHelper.buildContextUri
 import uk.ac.wellcome.platform.api.models.{ApiConfig, DisplayError, Error}
 import uk.ac.wellcome.platform.api.responses.ResultResponse
@@ -19,10 +18,7 @@ import uk.ac.wellcome.platform.api.responses.ResultResponse
   */
 @Singleton
 class MissingPathController @Inject()(apiConfig: ApiConfig) extends Controller {
-  val contextUri: String = buildContextUri(
-    apiConfig = apiConfig,
-    version = ApiVersions.default
-  )
+  val contextUri: String = buildContextUri(apiConfig = apiConfig)
 
   get("/:*") { request: Request =>
     val result = Error(
