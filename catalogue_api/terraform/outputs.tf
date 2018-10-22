a8af3f643e1da1f2c79932e3a7ab0610f204c5bb
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-22 13:11:34 +0100

    Nobody consumes either of these outputs

--- catalogue_api/terraform/outputs.tf
+++ catalogue_api/terraform/outputs.tf
@@ -1,11 +1,3 @@
-output "romulus_app_uri" {
-  value = "${local.romulus_app_uri}"
-}
-
-output "remus_app_uri" {
-  value = "${local.remus_app_uri}"
-}
-
 output "snapshots_bucket_arn" {
   value = "${module.data_api.snapshots_bucket_arn}"
 }
