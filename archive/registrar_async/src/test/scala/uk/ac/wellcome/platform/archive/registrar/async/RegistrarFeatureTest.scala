956d746ba82cfcf63990f6e3f0842f7d7f5b210f
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-10-24 17:11:48 +0000

    Apply auto-formatting rules

--- archive/registrar_async/src/test/scala/uk/ac/wellcome/platform/archive/registrar/async/RegistrarFeatureTest.scala
+++ archive/registrar_async/src/test/scala/uk/ac/wellcome/platform/archive/registrar/async/RegistrarFeatureTest.scala
@@ -2,15 +2,27 @@ package uk.ac.wellcome.platform.archive.registrar.async
 
 import java.time.Instant
 
-import org.scalatest.concurrent.{IntegrationPatience, PatienceConfiguration, ScalaFutures}
+import org.scalatest.concurrent.{
+  IntegrationPatience,
+  PatienceConfiguration,
+  ScalaFutures
+}
 import org.scalatest.time.{Millis, Seconds, Span}
 import org.scalatest.{FunSpec, Inside, Matchers}
 import uk.ac.wellcome.messaging.test.fixtures.SQS.QueuePair
 import uk.ac.wellcome.monitoring.fixtures.MetricsSenderFixture
 import uk.ac.wellcome.platform.archive.common.fixtures.RandomThings
-import uk.ac.wellcome.platform.archive.common.models.{ArchiveComplete, BagLocation, BagPath}
+import uk.ac.wellcome.platform.archive.common.models.{
+  ArchiveComplete,
+  BagLocation,
+  BagPath
+}
 import uk.ac.wellcome.platform.archive.common.progress.ProgressUpdateAssertions
-import uk.ac.wellcome.platform.archive.common.progress.models.{Progress, Resource, ResourceIdentifier}
+import uk.ac.wellcome.platform.archive.common.progress.models.{
+  Progress,
+  Resource,
+  ResourceIdentifier
+}
 import uk.ac.wellcome.platform.archive.registrar.async.fixtures.StorageManifestAssertions
 import uk.ac.wellcome.platform.archive.registrar.async.fixtures.RegistrarFixtures
 import uk.ac.wellcome.storage.dynamo._
