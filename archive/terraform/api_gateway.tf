d7b1a39bad79893178f1cc74c8e50fcfc8b7284e
alicefuzier <alicefuzier@gmail.com>
2018-10-29 14:57:15 +0000

    Rearrenge variables for readability

--- archive/terraform/api_gateway.tf
+++ archive/terraform/api_gateway.tf
@@ -20,22 +20,26 @@ resource "aws_api_gateway_deployment" "deployment" {
 
 module "ingests" {
   source = "api_gateway_resource"
+
   resource_name = "ingests"
-  authorizer_id = "${aws_api_gateway_authorizer.cognito.id}"
-  load_balancer_arn = "${module.progress_http.load_balancer_arn}"
-  storage_api_root_resource_id = "${aws_api_gateway_rest_api.api.root_resource_id}"
   forward_path = "progress"
+  load_balancer_arn = "${module.progress_http.load_balancer_arn}"
+
+  authorizer_id = "${aws_api_gateway_authorizer.cognito.id}"
+  storage_api_root_resource_id = "${aws_api_gateway_rest_api.api.root_resource_id}"
   storage_api_id = "${aws_api_gateway_rest_api.api.id}"
   cognito_storage_api_identifier = "${local.cognito_storage_api_identifier}"
 }
 
 module "bags" {
   source = "api_gateway_resource"
+
   resource_name = "bags"
-  authorizer_id = "${aws_api_gateway_authorizer.cognito.id}"
-  load_balancer_arn = "${module.registrar_http.load_balancer_arn}"
-  storage_api_root_resource_id = "${aws_api_gateway_rest_api.api.root_resource_id}"
   forward_path = "registrar"
+  load_balancer_arn = "${module.registrar_http.load_balancer_arn}"
+
+  authorizer_id = "${aws_api_gateway_authorizer.cognito.id}"
+  storage_api_root_resource_id = "${aws_api_gateway_rest_api.api.root_resource_id}"
   storage_api_id = "${aws_api_gateway_rest_api.api.id}"
   cognito_storage_api_identifier = "${local.cognito_storage_api_identifier}"
 }
