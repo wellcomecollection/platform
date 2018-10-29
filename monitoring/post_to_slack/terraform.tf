d963618225a43b649cce3c1babef8f8d1755743f
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-29 15:58:08 +0000

    Remove the snapshot alarm code from post_to_slack

--- monitoring/post_to_slack/terraform.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "catalogue_api" {
-  backend = "s3"
-
-  config {
-    bucket = "wellcomecollection-platform-infra"
-    key    = "terraform/catalogue_api.tfstate"
-    region = "eu-west-1"
-  }
-}
