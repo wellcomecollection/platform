1ee62f0a3959a250640a0665b77e199f36e306f6
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-30 11:39:59 +0000

    Flip the catalogue API to expose workType filters

--- catalogue_api/terraform/api_pins.tf
+++ catalogue_api/terraform/api_pins.tf
@@ -1,7 +1,7 @@
 locals {
-  production_api       = "remus"
-  pinned_remus_api     = "488c405a2c44fde1eef28f10bb505f4c1af8009f"
-  pinned_remus_nginx   = "3dd8a423123e1d175dd44520fcf03435a5fc92c8"
-  pinned_romulus_api   = ""
-  pinned_romulus_nginx = ""
+  production_api       = "romulus"
+  pinned_remus_api     = ""
+  pinned_remus_nginx   = ""
+  pinned_romulus_api   = "87a4a43d47f630ac1924f776b9f2ec059d57b879"
+  pinned_romulus_nginx = "3dd8a423123e1d175dd44520fcf03435a5fc92c8"
 }
