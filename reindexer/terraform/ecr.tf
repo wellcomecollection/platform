a37a44aa33af1cf026f5cca2873dfc8282a20eb2
mklander <mklanderyou@gmail.com>
2018-10-30 15:08:23 +0000

    removeold unused ecr

--- reindexer/terraform/ecr.tf
+++ reindexer/terraform/ecr.tf
@@ -2,9 +2,3 @@ module "ecr_repository_reindex_worker" {
   source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
   name   = "reindex_worker"
 }
-
-// TODO: remove when no longer needed
-module "ecr_repository_reindex_request_creator" {
-  source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
-  name   = "reindex_request_creator"
-}
