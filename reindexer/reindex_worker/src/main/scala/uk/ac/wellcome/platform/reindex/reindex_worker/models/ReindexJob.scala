33268b58511afeb5bc602acd188ce9b15d4c6561
Alex Chan <alex@alexwlchan.net>
2018-10-31 12:56:24 +0000

    Create two models for the different types of ReindexJob

--- reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex/reindex_worker/models/ReindexJob.scala
+++ reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex/reindex_worker/models/ReindexJob.scala
@@ -1,6 +1,23 @@
 package uk.ac.wellcome.platform.reindex.reindex_worker.models
 
-/** A request to identify all the shards in the table that need reindexing. */
-case class ReindexJob(segment: Int,
-                      totalSegments: Int,
-                      maxRecordsPerSegment: Option[Int] = None)
+/** A request to reindex some or all of the records in a table.
+  *
+  * The two classes represent the two ways you might use the reindexer:
+  *
+  *   1) For a "complete" reindex -- every record in the table should be sent
+  *      to the downstream applications.  Use this when you want to reprocess
+  *      the entire data set.
+  *   2) For a "partial" reindex -- when you want to test the downstream
+  *      applications without swamping them with records.  Use this for smoke tests.
+  *
+  */
+sealed trait ReindexJob
+
+case class CompleteReindexJob(
+  segment: Int,
+  totalSegments: Int
+) extends ReindexJob
+
+case class PartialReindexJob(
+  totalRecords: Int
+) extends ReindexJob
