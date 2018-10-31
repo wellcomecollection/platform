2be724868677fc6cdf5a5fa678483f834260a671
harrisonpim <harrison.stroma@gmail.com>
2018-10-31 12:00:31 +0000

    reformat transformer_example

--- reporting/transformer_example/src/transformer_example.py
+++ reporting/transformer_example/src/transformer_example.py
@@ -15,26 +15,14 @@ import certifi
 from elasticsearch import Elasticsearch
 
 
-# --------------------------------
-
-
-# Implement transformation here
-def _transform(record):
-    # do not do this!
-    return record
-
-
-# --------------------------------
+# Classes covering various record types ----------------------------------------
+def dict_to_location(d): return ObjectLocation(**d)
 
 
 @attrs
 class ObjectLocation(object):
     namespace = attrib()
     key = attrib()
-
-
-def dict_to_location(d):
-    return ObjectLocation(**d)
 
 
 @attrs
@@ -50,82 +38,107 @@ class ElasticsearchRecord(object):
     doc = attrib()
 
 
-def _extract_hybrid_record(raw_record):
-    return HybridRecord(**raw_record)
+# Some kind of transform -------------------------------------------------------
+def transform(record):
+    '''
+    Parameters
+    ----------
+    record : dict
+        the raw data from VHS in a malleable dict format
+        
+    Returns
+    -------
+    transformed_record : dict
+        record with necessary transformations applied, ready to be formatted for
+        elasticsearch ingestion
+    '''
+    return record
 
 
-def _get_record_from_s3(s3, object_location):
-    obj = s3.get_object(Bucket=object_location.namespace, Key=object_location.key)
-    return obj["Body"].read().decode("utf-8")
+# Moving records through the pipeline ------------------------------------------
+def hybrid_record_to_data_dict(s3, hybrid_record):
+    '''
+    takes an VHS/S3 location and returns its data as a record in ES format
 
+    Parameters
+    ----------
+    s3 : boto3.client
+    hybrid_record : HybridRecord
 
-def _build_es_record(s3, hybrid_record):
-    s3_record = _get_record_from_s3(s3, hybrid_record.location)
-    return ElasticsearchRecord(id=hybrid_record.id, doc=s3_record)
+    Returns
+    -------
+    data_dict : dict
+        object data in malleable dict format
+    '''
+    object_location = hybrid_record.location
+    s3_object = s3.get_object(Bucket=object_location.namespace, Key=object_location.key)
+    record = s3_object["Body"].read().decode("utf-8")
+    record_data = json.loads(record)['data']
+    return record_data
 
 
 def extract_records(s3, event):
-    raw_hybrid_records = [
-        json.loads(record["Sns"]["Message"]) for record in event["Records"]
-    ]
-    hybrid_records = [
-        _extract_hybrid_record(raw_record) for raw_record in raw_hybrid_records
-    ]
-    return [_build_es_record(s3, hybrid_record) for hybrid_record in hybrid_records]
+    '''
+    extracts records from VHS and prepares them for transformation
+   
+    Parameters
+    ----------
+    s3 : boto3.client
+    event : dict
+
+    Returns
+    -------
+    data_dicts : dict
+        a list of records in dict form, as specified by the input event
+    '''
+    raw_hybrid_records = [json.loads(record["Sns"]["Message"]) for record in event["Records"]]
+    hybrid_records = [HybridRecord(**record) for record in raw_hybrid_records]
+    data_dicts = [hybrid_record_to_data_dict(s3, hybrid_record) 
+                  for hybrid_record in hybrid_records]
+    return data_dicts, hybrid_records
 
 
-def transform_records(records):
-    return [_transform(record) for record in records]
+def transformed_dict_to_es(transformed_dict, hybrid_record):
+    '''
+    takes a transformed dict and prepares it for ingestion into elasticsearch
 
+    Parameters
+    ----------
+    transformed_dict : dict
+    hybrid_record : HybridRecord 
 
-def _load_record(es, index, doc_type, es_record):
-    return es.index(index=index, doc_type=doc_type, id=es_record.id, body=es_record.doc)
-
-
-def load_records(es, index, doc_type, docs):
-    return [_load_record(es, index, doc_type, doc) for doc in docs]
-
-
-def _run(elasticsearch_client, event, index, doc_type, s3_client):
-    records = extract_records(s3_client, event)
-    transformed_records = transform_records(records)
-    return load_records(elasticsearch_client, index, doc_type, transformed_records)
-
-
-def elasticsearch_client(url, username, password):
-    return Elasticsearch(
-        url, http_auth=(username, password), use_ssl=True, ca_certs=certifi.where()
-    )
-
-
-def main(
-    event,
-    _,
-    s3_client=None,
-    elasticsearch_client=None,
-    elasticsearch_index=None,
-    elasticsearch_doctype=None,
-):
+    Returns
+    -------
+    es_record : ElasticsearchRecord
+    '''
+    return ElasticsearchRecord(id=hybrid_record.id, doc=transformed_dict)
+    
+    
+# Move records with transforms applied -----------------------------------------
+def main(event, _, s3_client=None, es_client=None, index=None, doc_type=None):
+    '''
+    get records from VHS, apply the transformation to them, and shove them into 
+    an elasticsearch index
+    '''
     print(f"Event: {event}")
 
     s3_client = s3_client or boto3.client("s3")
+    index = index or os.environ["ES_INDEX"]
+    doc_type = doc_type or os.environ["ES_DOC_TYPE"]
+    es_client = es_client or Elasticsearch(url=os.environ["ES_URL"],
+                                           _ssl=True,
+                                           ca_certs=certifi.where(),
+                                           http_auth=(os.environ["ES_USER"],
+                                                      os.environ["ES_PASS"]))
 
-    elasticsearch_client = elasticsearch_client or elasticsearch_client(
-        url=os.environ["ES_URL"],
-        username=os.environ["ES_USER"],
-        password=os.environ["ES_PASS"],
-    )
+    data_dicts, hybrid_records = extract_records(s3_client, event)
 
-    elasticsearch_index = elasticsearch_index or os.environ["ES_INDEX"]
+    transformed_records = [transform(record) for record in data_dicts]
 
-    elasticsearch_doctype = elasticsearch_doctype or os.environ["ES_DOC_TYPE"]
+    es_records = [transformed_dict_to_es(data_dict, hybrid_record) 
+                  for data_dict, hybrid_record in zip(transformed_records, hybrid_records)]
 
-    results = _run(
-        elasticsearch_client=elasticsearch_client,
-        event=event,
-        index=elasticsearch_index,
-        doc_type=elasticsearch_doctype,
-        s3_client=s3_client,
-    )
+    results = [es_client.index(index=index, doc_type=doc_type, id=record.id, body=record)
+               for record in transformed_records]
 
     print(f"Result: {results}")
