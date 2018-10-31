a64d36f55fd58fed26f003df4c657a73bda70197
Alex Chan <alex@alexwlchan.net>
2018-10-31 09:46:41 +0000

    Switch the API to support image locationType filters.  Closes #2800.
    [skip ci]

--- catalogue_api/terraform/api_pins.tf
+++ catalogue_api/terraform/api_pins.tf
@@ -1,7 +1,7 @@
 locals {
-  production_api       = "romulus"
-  pinned_remus_api     = ""
-  pinned_remus_nginx   = ""
-  pinned_romulus_api   = "87a4a43d47f630ac1924f776b9f2ec059d57b879"
-  pinned_romulus_nginx = "3dd8a423123e1d175dd44520fcf03435a5fc92c8"
+  production_api       = "remus"
+  pinned_remus_api     = "889fd8bd6c24f853d9b03053dbd8d1547758afc8"
+  pinned_remus_nginx   = "3dd8a423123e1d175dd44520fcf03435a5fc92c8"
+  pinned_romulus_api   = ""
+  pinned_romulus_nginx = ""
 }
