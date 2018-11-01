ac380d608d713cb9d8927200dd1a47899773c62f
Alex Chan <alex@alexwlchan.net>
2018-11-01 10:14:46 +0000

    Switch the API to point at an up-to-date index

--- catalogue_api/terraform/api_pins.tf
+++ catalogue_api/terraform/api_pins.tf
@@ -1,7 +1,7 @@
 locals {
-  production_api       = "remus"
-  pinned_remus_api     = "889fd8bd6c24f853d9b03053dbd8d1547758afc8"
-  pinned_remus_nginx   = "3dd8a423123e1d175dd44520fcf03435a5fc92c8"
-  pinned_romulus_api   = ""
-  pinned_romulus_nginx = ""
+  production_api       = "romulus"
+  pinned_remus_api     = ""
+  pinned_remus_nginx   = ""
+  pinned_romulus_api   = "5d753c10ca58846ca67df73dd9998771b700757b"
+  pinned_romulus_nginx = "3dd8a423123e1d175dd44520fcf03435a5fc92c8"
 }
