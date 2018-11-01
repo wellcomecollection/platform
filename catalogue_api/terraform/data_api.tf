12de9158460f6d6b16bccc4a42012205c38a9ac2
Alex Chan <alex@alexwlchan.net>
2018-11-01 09:55:14 +0000

    Elasticsearch config isn't a variable, so don't do it that way

--- catalogue_api/terraform/data_api.tf
+++ catalogue_api/terraform/data_api.tf
@@ -1,8 +1,8 @@
 locals {
   prod_es_config = {
-    index_v1 = "${local.romulus_is_prod ? var.es_config_romulus["index_v1"] : var.es_config_remus["index_v1"]}"
-    index_v2 = "${local.romulus_is_prod ? var.es_config_romulus["index_v2"] : var.es_config_remus["index_v2"]}"
-    doc_type = "${local.romulus_is_prod ? var.es_config_romulus["doc_type"] : var.es_config_remus["doc_type"]}"
+    index_v1 = "${local.romulus_is_prod ? local.es_config_romulus["index_v1"] : local.es_config_remus["index_v1"]}"
+    index_v2 = "${local.romulus_is_prod ? local.es_config_romulus["index_v2"] : local.es_config_remus["index_v2"]}"
+    doc_type = "${local.romulus_is_prod ? local.es_config_romulus["doc_type"] : local.es_config_remus["doc_type"]}"
   }
 
   release_id = "${local.romulus_is_prod ? local.pinned_romulus_api : local.pinned_remus_api}"
