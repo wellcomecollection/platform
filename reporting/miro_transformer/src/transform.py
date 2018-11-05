87a802f42b3818a91e04961acae3aedf1ec2a9a4
harrisonpim <harrison.stroma@gmail.com>
2018-11-05 10:58:01 +0000

    use datetutil to parse dates, rather than datetime

--- reporting/miro_transformer/src/transform.py
+++ reporting/miro_transformer/src/transform.py
@@ -1,12 +1,11 @@
 import json
-from datetime import datetime
+from dateutil.parser import parse
 
-
-def transform(record):
+def transform(miro_transformable):
     """
     Parameters
     ----------
-    record : dict
+    miro_transformable : dict
         the raw data from VHS in a malleable dict format
 
     Returns
@@ -15,7 +14,7 @@ def transform(record):
         record with necessary transformations applied, ready to be formatted for
         elasticsearch ingestion
     """
-    original_data = json.loads(record["data"])
+    original_data = json.loads(miro_transformable["data"])
     transformed = drop_redundant_fields(original_data, keys_to_drop)
     transformed = clean_dates(transformed)
     return transformed
@@ -38,46 +37,48 @@ def clean_dates(data):
     alphabetically is rubbish
     """
     for key, value in data.items():
-        if "date" in key and value is not None:
-            if type(value) == str:
+        if '_date' in key and value is not None:
+            if isinstance(value, str):
                 data[key] = convert_date_to_iso(value)
-            elif type(value) == list:
+            elif isinstance(value, list):
                 data[key] = [convert_date_to_iso(date) for date in value]
+            else:
+                pass
     return data
 
 
 def convert_date_to_iso(date_string):
-    if date_string is None or date_string is "":
-        date = None
-    else:
-        date = datetime.strptime(date_string, "%d/%m/%Y").date().isoformat()
-    return date
+    try:
+        parsed_date = parse(date_string).date().isoformat()
+    except: 
+        parsed_date = date_string
+    return parsed_date
 
 
-keys_to_drop = [
-    "_index",
-    "_score",
-    "_type",
-    "all_draft",
-    "all_psoft_export",
-    "image_biomed_close_up",
-    "image_corp_commissioned_by",
-    "image_corp_contact_details",
-    "image_corp_institution",
-    "image_corp_keywords",
-    "image_corp_photographer_freelance",
-    "image_corp_purpose",
-    "image_corp_shoot_sheet_number",
-    "image_funding_type",
-    "image_iap_image_no",
-    "image_iap_invalid",
-    "image_icd_note",
-    "image_no_calc",
-    "image_phys_notes",
-    "image_source",
-    "image_tech_manipulated_date_from",
-    "image_tech_manipulated_date_to",
-    "image_tech_manipulated_date",
-    "image_web_img_filename",
-    "image_web_img_size",
-]
+keys_to_drop = (
+    '_index',
+    '_score',
+    '_type',
+    'all_draft',
+    'all_psoft_export',
+    'image_biomed_close_up',
+    'image_corp_commissioned_by',
+    'image_corp_contact_details',
+    'image_corp_institution',
+    'image_corp_keywords',
+    'image_corp_photographer_freelance',
+    'image_corp_purpose',
+    'image_corp_shoot_sheet_number',
+    'image_funding_type',
+    'image_iap_image_no',
+    'image_iap_invalid',
+    'image_icd_note',
+    'image_no_calc',
+    'image_phys_notes',
+    'image_source',
+    'image_tech_manipulated_date_from',
+    'image_tech_manipulated_date_to',
+    'image_tech_manipulated_date',
+    'image_web_img_filename',
+    'image_web_img_size'
+)
