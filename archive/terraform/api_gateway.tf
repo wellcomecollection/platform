b950f1ea56a5edfbb8e217652243f2e53a047a46
alicefuzier <alicefuzier@gmail.com>
2018-11-05 17:19:30 +0000

    Add context.json and serve it via api gateway out of s3

--- archive/terraform/api_gateway.tf
+++ archive/terraform/api_gateway.tf
@@ -46,6 +46,18 @@ module "bags" {
   cognito_storage_api_identifier = "${local.cognito_storage_api_identifier}"
 }
 
+module "context" {
+  source = "api_gateway_static_resource"
+
+  resource_name      = "context.json"
+  s3_key = "${aws_s3_bucket_object.context.key}"
+  bucket_name = "${aws_s3_bucket.storage_static_content.id}"
+
+  storage_api_root_resource_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  storage_api_id                 = "${aws_api_gateway_rest_api.api.id}"
+  aws_region = "${var.aws_region}"
+}
+
 resource "aws_api_gateway_vpc_link" "vpc_link" {
   name        = "${local.namespace}_vpc_link"
   target_arns = ["${aws_lb.network_load_balancer.arn}"]
