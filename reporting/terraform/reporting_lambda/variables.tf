9888ee8bcbb73efa71e868b574f9309b61b74d6a
harrisonpim <harrison.stroma@gmail.com>
2018-11-05 12:37:27 +0000

    makes the terraform modifications actually work...

--- reporting/terraform/reporting_lambda/variables.tf
+++ reporting/terraform/reporting_lambda/variables.tf
@@ -1,3 +1,14 @@
 variable "timeout" {
   default = 25
 }
+
+variable "name" {}
+variable "description" {}
+variable "environment_variables" {
+    type = "map"
+}
+variable "trigger_topic_arn" {}
+variable "error_alarm_topic_arn" {}
+variable "log_retention_in_days" {
+    default = 7
+}
