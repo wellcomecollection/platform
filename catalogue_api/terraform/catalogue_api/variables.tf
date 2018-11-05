05935b0f2c3cdc2cf83f3c353311a3f9101c786e
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-11-05 19:14:38 +0000

    Catalogue API uses API Gateway
    Using new ECS & API Gateway modules

---
+++ catalogue_api/terraform/catalogue_api/variables.tf
@@ -0,0 +1,32 @@
+variable "es_config" {
+  type = "map"
+}
+variable "subnets" {
+  type = "list"
+}
+
+variable "cluster_name" {}
+
+variable "namespace" {}
+variable "namespace_id" {}
+
+data "template_file" "es_cluster_host" {
+  template = "$${name}.$${region}.aws.found.io"
+
+  vars {
+    name   = "${var.es_cluster_credentials["name"]}"
+    region = "${var.es_cluster_credentials["region"]}"
+  }
+}
+
+variable "es_cluster_credentials" {
+  type = "map"
+}
+
+variable "vpc_id" {}
+variable "container_image" {}
+variable "container_port" {}
+variable "security_group_ids" {
+  type = "list"
+}
+variable "service_egress_security_group_id" {}
