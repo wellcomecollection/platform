cae6474c3f5895f519753cc59836d8cab3cee364
Alex Chan <alex@alexwlchan.net>
2018-11-06 10:13:48 +0000

    Expire old records in the Sierra adapter bucket

--- sierra_adapter/terraform/s3_sierra_adapter.tf
+++ sierra_adapter/terraform/s3_sierra_adapter.tf
@@ -4,4 +4,24 @@ resource "aws_s3_bucket" "sierra_adapter" {
   lifecycle {
     prevent_destroy = true
   }
+
+  lifecycle_rule {
+    id      = "expire_old_items"
+    prefix  = "records_items/"
+    enabled = true
+
+    expiration {
+      days = 7
+    }
+  }
+
+  lifecycle_rule {
+    id      = "expire_old_bibs"
+    prefix  = "records_bibs/"
+    enabled = true
+
+    expiration {
+      days = 7
+    }
+  }
 }
