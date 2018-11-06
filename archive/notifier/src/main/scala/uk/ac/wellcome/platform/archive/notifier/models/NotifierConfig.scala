439309d56812e12026caff73ef0641252238f9a8
Alex Chan <alex@alexwlchan.net>
2018-11-06 12:54:04 +0000

    Set up some config pieces + cleanups

--- archive/notifier/src/main/scala/uk/ac/wellcome/platform/archive/notifier/models/NotifierConfig.scala
+++ archive/notifier/src/main/scala/uk/ac/wellcome/platform/archive/notifier/models/NotifierConfig.scala
@@ -2,13 +2,4 @@ package uk.ac.wellcome.platform.archive.notifier.models
 
 import java.net.URL
 
-import uk.ac.wellcome.messaging.sns.SNSConfig
-import uk.ac.wellcome.messaging.sqs.SQSConfig
-import uk.ac.wellcome.monitoring.MetricsConfig
-import uk.ac.wellcome.platform.archive.common.modules.{
-  CloudwatchClientConfig,
-  SQSClientConfig,
-  SnsClientConfig
-}
-
 case class NotifierConfig(contextUrl: URL)
