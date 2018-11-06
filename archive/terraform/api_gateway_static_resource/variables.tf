1d28958364df9627aa85e614791dd22767c1dd6f
Alice Fuzier Cayla <alicefuzier@gmail.com>
2018-11-06 08:47:37 +0000

    Add context.json and serve it via api gateway out of s3 (#2957)
    * Add context.json and serve it via api gateway out of s3
    
    * Apply auto-formatting rules

---
+++ archive/terraform/api_gateway_static_resource/variables.tf
@@ -0,0 +1,11 @@
+variable "storage_api_id" {}
+
+variable "storage_api_root_resource_id" {}
+
+variable "resource_name" {}
+
+variable "aws_region" {}
+
+variable "bucket_name" {}
+
+variable "s3_key" {}
--- archive/terraform/s3.tf
+++ archive/terraform/s3.tf
@@ -11,3 +11,8 @@ resource "aws_s3_bucket" "ingest_storage" {
   bucket = "${local.ingest_bucket_name}"
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "storage_static_content" {
+  bucket = "${local.storage_static_content_bucket_name}"
+  acl    = "private"
+}
