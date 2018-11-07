632967817880e4ecc6d25766e353f079366cbf20
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-07 15:29:05 +0000

    And hey, we can clean up these two shared files as well

--- archive/progress_common/src/main/scala/uk/ac/wellcome/platform/archive/common/progress/modules/ProgressTrackerConfig.scala
+++
@@ -1,7 +0,0 @@
-package uk.ac.wellcome.platform.archive.common.progress.modules
-
-import uk.ac.wellcome.platform.archive.common.modules.DynamoClientConfig
-import uk.ac.wellcome.storage.dynamo.DynamoConfig
-
-case class ProgressTrackerConfig(dynamoConfig: DynamoConfig,
-                                 dynamoClientConfig: DynamoClientConfig)
