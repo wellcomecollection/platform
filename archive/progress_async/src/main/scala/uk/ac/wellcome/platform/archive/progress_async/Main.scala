1b3c104e2d8dc081202aff54338e67e0c395f857
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-07 14:00:42 +0000

    Use the new builders in the rest of progress-async

--- archive/progress_async/src/main/scala/uk/ac/wellcome/platform/archive/progress_async/Main.scala
+++ archive/progress_async/src/main/scala/uk/ac/wellcome/platform/archive/progress_async/Main.scala
@@ -1,90 +1,47 @@
 package uk.ac.wellcome.platform.archive.progress_async
 
-import akka.actor.ActorSystem
-import akka.event.Logging
-import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Flow
-import com.amazonaws.services.sns.AmazonSNS
-import com.google.inject.{Guice, Injector}
 import com.typesafe.config.ConfigFactory
 import grizzled.slf4j.Logging
-import uk.ac.wellcome.messaging.sns.SNSConfig
-import uk.ac.wellcome.platform.archive.common.config.builders.{AkkaBuilder, MessagingBuilder, MetricsBuilder, SQSBuilder}
-import uk.ac.wellcome.platform.archive.common.messaging.{MessageStream, NotificationParsingFlow}
+import uk.ac.wellcome.platform.archive.common.config.builders._
+import uk.ac.wellcome.platform.archive.common.messaging.NotificationParsingFlow
 import uk.ac.wellcome.platform.archive.common.models.NotificationMessage
-import uk.ac.wellcome.platform.archive.common.modules._
 import uk.ac.wellcome.platform.archive.common.progress.models.ProgressUpdate
-import uk.ac.wellcome.platform.archive.common.progress.modules.ProgressTrackerModule
 import uk.ac.wellcome.platform.archive.common.progress.monitor.ProgressTracker
 import uk.ac.wellcome.platform.archive.progress_async.flows.{CallbackNotificationFlow, ProgressUpdateFlow}
-import uk.ac.wellcome.platform.archive.progress_async.modules.{AppConfigModule, ConfigModule}
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
 object Main extends App with Logging {
-  val injector: Injector = Guice.createInjector(
-    new AppConfigModule(args),
-    ConfigModule,
-    AkkaModule,
-    CloudWatchClientModule,
-    SQSClientModule,
-    SNSClientModule,
-    ProgressTrackerModule,
-    MessageStreamModule
-  )
-
-  val actorSystem = AkkaBuilder.buildActorSystem()
-
   val config = ConfigFactory.load()
 
   val messageStream = MessagingBuilder.buildMessageStream[NotificationMessage, Unit](config)
 
-  def run() = {
-    type StreamNotice = MessageStream[NotificationMessage, Unit]
+  val parseNotificationFlow = NotificationParsingFlow[ProgressUpdate]()
 
-    implicit val snsClient: AmazonSNS =
-      injector.getInstance(classOf[AmazonSNS])
-    val snsConfig = injector.getInstance(classOf[SNSConfig])
+  val progressTracker = new ProgressTracker(
+    dynamoClient = DynamoBuilder.buildDynamoClient(config),
+    dynamoConfig = DynamoBuilder.buildDynamoConfig(config)
+  )
 
-    implicit val system =
-      injector.getInstance(classOf[ActorSystem])
+  val progressUpdateFlow = ProgressUpdateFlow(progressTracker)
 
-    implicit val materializer = ActorMaterializer()
+  val callbackNotificationFlow = CallbackNotificationFlow(
+    snsClient = SNSBuilder.buildSNSClient(config),
+    snsConfig = SNSBuilder.buildSNSConfig(config)
+  )
 
-    implicit val adapter =
-      Logging(system.eventStream, "customLogger")
-
-    val messageStream =
-      injector.getInstance(classOf[StreamNotice])
-
-    val progressTracker = injector
-      .getInstance(classOf[ProgressTracker])
-
-    val progressUpdateFlow =
-      ProgressUpdateFlow(progressTracker)
-
-    val parseNotificationFlow =
-      NotificationParsingFlow[ProgressUpdate]()
-
-    val callbackNotificationFlow =
-      CallbackNotificationFlow(snsClient, snsConfig)
-
-    val workFlow = Flow[NotificationMessage]
-      .log("notification message")
-      .via(parseNotificationFlow)
-      .via(progressUpdateFlow)
-      .via(callbackNotificationFlow)
-
-    messageStream.run("progress", workFlow)
-  }
-
-
+  val workFlow = Flow[NotificationMessage]
+    .log("notification message")
+    .via(parseNotificationFlow)
+    .via(progressUpdateFlow)
+    .via(callbackNotificationFlow)
 
   try {
     info(s"Starting worker.")
 
-    val app = run()
+    val app = messageStream.run("progress", workFlow)
 
     Await.result(app, Duration.Inf)
   } catch {
