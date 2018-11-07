6c5820b5fd3ea72be750e3db4c505ba469084dfb
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-07 11:47:33 +0000

    Reduce diff churn in GitHub

--- archive/registrar_http/src/main/scala/uk/ac/wellcome/platform/archive/registrar/http/Main.scala
+++ archive/registrar_http/src/main/scala/uk/ac/wellcome/platform/archive/registrar/http/Main.scala
@@ -1,16 +1,20 @@
 package uk.ac.wellcome.platform.archive.registrar.http
-
 import com.google.inject.{Guice, Injector}
-import uk.ac.wellcome.platform.archive.common.modules.{AkkaModule, DynamoClientModule}
+import uk.ac.wellcome.platform.archive.common.modules.{
+  AkkaModule,
+  DynamoClientModule
+}
 import uk.ac.wellcome.platform.archive.registrar.common.modules.VHSModule
-import uk.ac.wellcome.platform.archive.registrar.http.modules.{AkkaHttpApp, AppConfigModule, ConfigModule}
+import uk.ac.wellcome.platform.archive.registrar.http.modules.{
+  AkkaHttpApp,
+  AppConfigModule,
+  ConfigModule
+}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 object Main extends App with AkkaHttpApp {
-
-
   override val injector: Injector = Guice.createInjector(
     new AppConfigModule(args),
     ConfigModule,
