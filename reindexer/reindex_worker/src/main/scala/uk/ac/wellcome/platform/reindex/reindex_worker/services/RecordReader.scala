732a063d01d6abc2d6e35898a7cbf16e4a94fbde
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-09 16:14:40 +0000

    Get a ReindexWorker that's a bit less crummy

--- reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex/reindex_worker/services/RecordReader.scala
+++ reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex/reindex_worker/services/RecordReader.scala
@@ -4,21 +4,16 @@ import java.util
 
 import com.amazonaws.services.dynamodbv2.model.AttributeValue
 import com.google.inject.Inject
-import com.gu.scanamo.{DynamoFormat, ScanamoFree}
-import com.gu.scanamo.error.DynamoReadError
 import com.twitter.inject.Logging
 import uk.ac.wellcome.platform.reindex.reindex_worker.dynamo.{
   MaxRecordsScanner,
   ParallelScanner
 }
-import uk.ac.wellcome.platform.reindex.reindex_worker.exceptions.ReindexerException
 import uk.ac.wellcome.platform.reindex.reindex_worker.models.{
   CompleteReindexJob,
   PartialReindexJob,
   ReindexJob
 }
-import uk.ac.wellcome.storage.dynamo._
-import uk.ac.wellcome.storage.vhs.{HybridRecord, VHSIndexEntry}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -33,53 +28,18 @@ class RecordReader @Inject()(
 )(implicit ec: ExecutionContext)
     extends Logging {
 
-  def findRecordsForReindexing[M](reindexJob: ReindexJob)(
-    implicit hybridRecordDynamoFormat: DynamoFormat[HybridRecord],
-    metadataDynamoFormat: DynamoFormat[M]): Future[List[VHSIndexEntry[M]]] = {
+  def findRecordsForReindexing(reindexJob: ReindexJob): Future[List[util.Map[String, AttributeValue]]] = {
     debug(s"Finding records that need reindexing for $reindexJob")
 
-    for {
-      // We start by querying DynamoDB for every record we want to reindex.
-      // If we requested reindexing a particularly large shard, this might
-      // cause out-of-memory errors -- in practice, we're hoping that the
-      // shards/individual records are small enough for this not to be a problem.
-      attributeValues: List[util.Map[String, AttributeValue]] <- reindexJob match {
-        case CompleteReindexJob(segment, totalSegments) =>
-          parallelScanner
-            .scan(
-              segment = segment,
-              totalSegments = totalSegments
-            )
-        case PartialReindexJob(maxRecords) =>
-          maxRecordsScanner.scan(maxRecords = maxRecords)
-      }
-
-      recordsToReindex: List[VHSIndexEntry[M]] = attributeValues.map { av =>
-        parseResult[M](av)
-      }
-    } yield recordsToReindex
-  }
-
-  private def parseResult[M](attributeValues: util.Map[String, AttributeValue])(
-    implicit hybridRecordDynamoFormat: DynamoFormat[HybridRecord],
-    metadataDynamoFormat: DynamoFormat[M]): VHSIndexEntry[M] = {
-    // Take the Map[String, AttributeValue], and convert it into an
-    // instance of the case class `T`.  This is using a Scanamo helper --
-    // I worked this out by looking at [[ScanamoFree.get]].
-    //
-    // https://github.com/scanamo/scanamo/blob/12554b8e24ef8839d5e9dd9a4f42ae130e29b42b/scanamo/src/main/scala/com/gu/scanamo/ScanamoFree.scala#L62
-    //
-    val maybeHybridRecord: Either[DynamoReadError, HybridRecord] =
-      ScanamoFree.read[HybridRecord](attributeValues)
-    val maybeMetadata: Either[DynamoReadError, M] =
-      ScanamoFree.read[M](attributeValues)
-
-    (maybeHybridRecord, maybeMetadata) match {
-      case (Right(hybridRecord: HybridRecord), Right(metadata)) =>
-        VHSIndexEntry(hybridRecord = hybridRecord, metadata = metadata)
-      case _ =>
-        throw ReindexerException(
-          s"Error when parsing $attributeValues as VHSIndexEntry")
+    reindexJob match {
+      case CompleteReindexJob(segment, totalSegments) =>
+        parallelScanner
+          .scan(
+            segment = segment,
+            totalSegments = totalSegments
+          )
+      case PartialReindexJob(maxRecords) =>
+        maxRecordsScanner.scan(maxRecords = maxRecords)
     }
   }
 }
--- reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex/reindex_worker/services/ReindexWorker.scala
+++ reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex/reindex_worker/services/ReindexWorker.scala
@@ -1,16 +1,20 @@
 package uk.ac.wellcome.platform.reindex.reindex_worker.services
 
+import java.util
+
 import akka.actor.{ActorSystem, Terminated}
+import com.amazonaws.services.dynamodbv2.model.AttributeValue
 import com.google.inject.Inject
-import com.gu.scanamo.DynamoFormat
+import com.gu.scanamo.{DynamoFormat, ScanamoFree}
 import com.twitter.inject.annotations.Flag
 import io.circe.Encoder
 import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.sqs.SQSStream
 import uk.ac.wellcome.platform.reindex.reindex_worker.models.ReindexJob
 import uk.ac.wellcome.json.JsonUtil._
+import uk.ac.wellcome.platform.reindex.reindex_worker.exceptions.ReindexerException
 import uk.ac.wellcome.storage.dynamo._
-import uk.ac.wellcome.storage.vhs.{EmptyMetadata, VHSIndexEntry}
+import uk.ac.wellcome.storage.vhs.{EmptyMetadata, HybridRecord, VHSIndexEntry}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -37,19 +41,47 @@ class ReindexWorker @Inject()(
     for {
       reindexJob: ReindexJob <- Future.fromTry(
         fromJson[ReindexJob](message.body))
+      items <- recordReader.findRecordsForReindexing(reindexJob)
       _ <- tableMetadata match {
-        case "MiroMetadata" => processReindexJob[MiroMetadata](reindexJob)
-        case _              => processReindexJob[EmptyMetadata](reindexJob)
+        case "MiroMetadata" => processMiroMetadata(items)
+        case _ => processEmptyMetadata(items)
       }
     } yield ()
 
   def stop(): Future[Terminated] = system.terminate()
 
-  private def processReindexJob[M](reindexJob: ReindexJob)(
-    implicit dynamoFormat: DynamoFormat[M],
-    encoder: Encoder[VHSIndexEntry[M]]) =
-    for {
-      recordsToSend <- recordReader.findRecordsForReindexing[M](reindexJob)
-      _ <- bulkSNSWriter.sendToSNS[VHSIndexEntry[M]](recordsToSend)
-    } yield ()
+  private def processEmptyMetadata(
+    items: List[util.Map[String, AttributeValue]]): Future[Unit] =
+    bulkSNSWriter
+      .sendToSNS(items.map { parseAsCaseClass[HybridRecord] })
+      .map { _ => () }
+
+  private def processMiroMetadata(
+    items: List[util.Map[String, AttributeValue]]): Future[Unit] =
+    bulkSNSWriter
+      .sendToSNS(
+        items.map { av =>
+          val hybridRecord = parseAsCaseClass[HybridRecord](av)
+          val metadata = parseAsCaseClass[MiroMetadata](av)
+
+          VHSIndexEntry(hybridRecord, metadata)
+        }
+      )
+      .map { _ => () }
+
+  /** Take the Map[String, AttributeValue], and convert it into an
+    * instance of the case class `T`.  This is using a Scanamo helper --
+    * I worked this out by looking at `ScanamoFree.get`.
+    *
+    * https://github.com/scanamo/scanamo/blob/12554b8e24ef8839d5e9dd9a4f42ae130e29b42b/scanamo/src/main/scala/com/gu/scanamo/ScanamoFree.scala#L62
+    *
+    */
+  private def parseAsCaseClass[T](
+    attributeValues: util.Map[String, AttributeValue])(
+    implicit dynamoFormat: DynamoFormat[T]): T =
+    ScanamoFree.read[T](attributeValues) match {
+      case Right(t) => t
+      case Left(err) =>
+        throw ReindexerException(s"Error when parsing $attributeValues ($err)")
+    }
 }
