7710dd0977a28eca22f168369898e1b36ce8d37e
Alex Chan <alex@alexwlchan.net>
2018-11-11 13:27:57 +0000

    And sort out the Sierra reader feature test

--- sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/SierraReaderFeatureTest.scala
+++ sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/SierraReaderFeatureTest.scala
@@ -2,12 +2,19 @@ package uk.ac.wellcome.platform.sierra_reader
 
 import org.scalatest.concurrent.{Eventually, IntegrationPatience}
 import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.test.fixtures.SQS
+import uk.ac.wellcome.messaging.test.fixtures.SQS.Queue
+import uk.ac.wellcome.platform.sierra_reader.config.models.{ReaderConfig, SierraAPIConfig}
+import uk.ac.wellcome.platform.sierra_reader.models.SierraResourceTypes
+import uk.ac.wellcome.platform.sierra_reader.services.SierraReaderWorkerService
 import uk.ac.wellcome.storage.fixtures.S3
+import uk.ac.wellcome.storage.fixtures.S3.Bucket
+import uk.ac.wellcome.test.fixtures.{Akka, TestWith}
 
 class SierraReaderFeatureTest
     extends FunSpec
-    with fixtures.Server
+    with Akka
     with S3
     with SQS
     with Eventually
@@ -17,23 +24,17 @@ class SierraReaderFeatureTest
   it("reads bibs from Sierra and writes files to S3") {
     withLocalS3Bucket { bucket =>
       withLocalSqsQueue { queue =>
-        val flags = s3LocalFlags(bucket) ++ sqsLocalFlags(queue) ++ Map(
-          "reader.resourceType" -> "bibs",
-          "sierra.apiUrl" -> "http://localhost:8080",
-          "sierra.oauthKey" -> "key",
-          "sierra.oauthSecret" -> "secret",
-          "sierra.fields" -> "updatedDate,deletedDate,deleted,suppressed,author,title"
-        )
+        withWorkerService(bucket, queue) { service =>
+          service.run()
 
-        val body =
-          """
-            |{
-            | "start": "2013-12-10T17:16:35Z",
-            | "end": "2013-12-13T21:34:35Z"
-            |}
-          """.stripMargin
+          val body =
+            """
+              |{
+              | "start": "2013-12-10T17:16:35Z",
+              | "end": "2013-12-13T21:34:35Z"
+              |}
+            """.stripMargin
 
-        withServer(flags) { _ =>
           sendNotificationToSQS(queue = queue, body = body)
 
           eventually {
@@ -44,4 +45,28 @@ class SierraReaderFeatureTest
       }
     }
   }
+
+  private def withWorkerService[R](bucket: Bucket, queue: Queue)(testWith: TestWith[SierraReaderWorkerService, R]): R =
+    withActorSystem { actorSystem =>
+      withSQSStream[NotificationMessage, R](actorSystem, queue) { sqsStream =>
+        val workerService = new SierraReaderWorkerService(
+          actorSystem = actorSystem,
+          sqsStream = sqsStream,
+          s3client = s3Client,
+          s3Config = createS3ConfigWith(bucket),
+          readerConfig = ReaderConfig(
+            resourceType = SierraResourceTypes.bibs,
+            fields = "updatedDate,deletedDate,deleted,suppressed,author,title",
+            batchSize = 50
+          ),
+          sierraAPIConfig = SierraAPIConfig(
+            apiURL = "http://localhost:8080",
+            oauthKey = "key",
+            oauthSec = "secret"
+          )
+        )
+
+        testWith(workerService)
+      }
+    }
 }
