30156c91ce65efcea0bc11873f57c74988258ac2
Alex Chan <alex@alexwlchan.net>
2018-11-11 13:39:54 +0000

    Use more of the new workerService fixture

--- sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/SierraReaderFeatureTest.scala
+++ sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/SierraReaderFeatureTest.scala
@@ -2,24 +2,18 @@ package uk.ac.wellcome.platform.sierra_reader
 
 import org.scalatest.concurrent.{Eventually, IntegrationPatience}
 import org.scalatest.{FunSpec, Matchers}
-import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.test.fixtures.SQS
-import uk.ac.wellcome.messaging.test.fixtures.SQS.Queue
-import uk.ac.wellcome.platform.sierra_reader.config.models.{ReaderConfig, SierraAPIConfig}
-import uk.ac.wellcome.platform.sierra_reader.models.SierraResourceTypes
-import uk.ac.wellcome.platform.sierra_reader.services.SierraReaderWorkerService
+import uk.ac.wellcome.platform.sierra_reader.fixtures.WorkerServiceFixture
 import uk.ac.wellcome.storage.fixtures.S3
-import uk.ac.wellcome.storage.fixtures.S3.Bucket
-import uk.ac.wellcome.test.fixtures.{Akka, TestWith}
 
 class SierraReaderFeatureTest
     extends FunSpec
-    with Akka
     with S3
     with SQS
     with Eventually
     with Matchers
-    with IntegrationPatience {
+    with IntegrationPatience
+    with WorkerServiceFixture {
 
   it("reads bibs from Sierra and writes files to S3") {
     withLocalS3Bucket { bucket =>
@@ -45,28 +39,4 @@ class SierraReaderFeatureTest
       }
     }
   }
-
-  private def withWorkerService[R](bucket: Bucket, queue: Queue)(testWith: TestWith[SierraReaderWorkerService, R]): R =
-    withActorSystem { actorSystem =>
-      withSQSStream[NotificationMessage, R](actorSystem, queue) { sqsStream =>
-        val workerService = new SierraReaderWorkerService(
-          actorSystem = actorSystem,
-          sqsStream = sqsStream,
-          s3client = s3Client,
-          s3Config = createS3ConfigWith(bucket),
-          readerConfig = ReaderConfig(
-            resourceType = SierraResourceTypes.bibs,
-            fields = "updatedDate,deletedDate,deleted,suppressed,author,title",
-            batchSize = 50
-          ),
-          sierraAPIConfig = SierraAPIConfig(
-            apiURL = "http://localhost:8080",
-            oauthKey = "key",
-            oauthSec = "secret"
-          )
-        )
-
-        testWith(workerService)
-      }
-    }
 }
