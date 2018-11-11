9d7d4065d6c5e2960b0bcbf28af6f86b3725a03c
Alex Chan <alex@alexwlchan.net>
2018-11-11 14:33:51 +0000

    How about an implicit StorageBackend?

--- sierra_adapter/sierra_item_merger/src/main/scala/uk/ac/wellcome/platform/sierra_item_merger/Main.scala
+++ sierra_adapter/sierra_item_merger/src/main/scala/uk/ac/wellcome/platform/sierra_item_merger/Main.scala
@@ -6,6 +6,7 @@ import grizzled.slf4j.Logging
 import uk.ac.wellcome.config.core.builders.AkkaBuilder
 import uk.ac.wellcome.config.messaging.builders.{SNSBuilder, SQSBuilder}
 import uk.ac.wellcome.config.monitoring.builders.MetricsBuilder
+import uk.ac.wellcome.config.storage.builders.S3Builder
 import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.sqs.SQSStream
@@ -16,6 +17,7 @@ import uk.ac.wellcome.platform.sierra_item_merger.services.{
 }
 import uk.ac.wellcome.sierra_adapter.config.builders.SierraTransformableVHSBuilder
 import uk.ac.wellcome.storage.ObjectStore
+import uk.ac.wellcome.storage.s3.S3StorageBackend
 
 import scala.concurrent.{Await, ExecutionContext}
 import scala.concurrent.duration.Duration
@@ -39,6 +41,10 @@ object Main extends App with Logging {
 
   val updaterService = new SierraItemMergerUpdaterService(
     versionedHybridStore = versionedHybridStore
+  )
+
+  implicit val storageBackend: S3StorageBackend = new S3StorageBackend(
+    s3Client = S3Builder.buildS3Client(config)
   )
 
   val workerService = new SierraItemMergerWorkerService(
