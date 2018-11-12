dca6b5c2013824b70f8ebae8b010dd42c6ac790b
Alex Chan <alex@alexwlchan.net>
2018-11-12 23:06:03 +0000

    Remove an unused import

--- sierra_adapter/sierra_reader/src/main/scala/uk/ac/wellcome/platform/sierra_reader/Main.scala
+++ sierra_adapter/sierra_reader/src/main/scala/uk/ac/wellcome/platform/sierra_reader/Main.scala
@@ -8,7 +8,6 @@ import uk.ac.wellcome.config.messaging.builders.SQSBuilder
 import uk.ac.wellcome.config.monitoring.builders.MetricsBuilder
 import uk.ac.wellcome.config.storage.builders.S3Builder
 import uk.ac.wellcome.messaging.sns.NotificationMessage
-import uk.ac.wellcome.messaging.sqs.SQSStream
 import uk.ac.wellcome.platform.sierra_reader.config.builders.{
   ReaderConfigBuilder,
   SierraAPIConfigBuilder
