0fbe5d4a2b35b2728f317dc770d40a5655f05972
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-13 12:12:31 +0000

    Construct all the ID minter pieces

--- catalogue_pipeline/id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/Main.scala
+++ catalogue_pipeline/id_minter/src/main/scala/uk/ac/wellcome/platform/idminter/Main.scala
@@ -2,9 +2,10 @@ package uk.ac.wellcome.platform.idminter
 
 import com.typesafe.config.{Config, ConfigFactory}
 import grizzled.slf4j.Logging
+import io.circe.Json
+import uk.ac.wellcome.config.messaging.builders.MessagingBuilder
 import uk.ac.wellcome.platform.idminter.config.builders.{IdentifiersTableBuilder, RDSBuilder}
 import uk.ac.wellcome.platform.idminter.database.IdentifiersDao
-import uk.ac.wellcome.platform.idminter.models.IdentifiersTable
 import uk.ac.wellcome.platform.idminter.services.IdMinterWorkerService
 import uk.ac.wellcome.platform.idminter.steps.{IdEmbedder, IdentifierGenerator}
 
@@ -22,11 +23,13 @@ object Main extends App with Logging {
   )
 
   val idEmbedder = new IdEmbedder(
-
+    identifierGenerator = identifierGenerator
   )
 
   val workerService = new IdMinterWorkerService(
-
+    idEmbedder = idEmbedder,
+    writer = MessagingBuilder.buildMessageWriter[Json](config),
+    messageStream = MessagingBuilder.buildMessageStream[Json](config)
   )
 
   try {
--- sbt_common/config/messaging/src/main/scala/uk/ac/wellcome/config/messaging/builders/MessagingBuilder.scala
+++ sbt_common/config/messaging/src/main/scala/uk/ac/wellcome/config/messaging/builders/MessagingBuilder.scala
@@ -4,7 +4,7 @@ import com.typesafe.config.Config
 import uk.ac.wellcome.config.core.builders.AkkaBuilder
 import uk.ac.wellcome.config.monitoring.builders.MetricsBuilder
 import uk.ac.wellcome.config.storage.builders.S3Builder
-import uk.ac.wellcome.messaging.message.{MessageReaderConfig, MessageStream}
+import uk.ac.wellcome.messaging.message.{MessageReaderConfig, MessageStream, MessageWriter, MessageWriterConfig}
 import uk.ac.wellcome.storage.s3.S3StorageBackend
 import uk.ac.wellcome.storage.type_classes.SerialisationStrategy
 
@@ -34,6 +34,20 @@ object MessagingBuilder {
       s3Client = S3Builder.buildS3Client(config),
       messageReaderConfig = buildMessageReaderConfig(config),
       metricsSender = MetricsBuilder.buildMetricsSender(config)
+    )
+  }
+
+  def buildMessageWriterConfig(config: Config): MessageWriterConfig =
+    MessageWriterConfig(
+      snsConfig = SNSBuilder.buildSNSConfig(config, namespace = "message.reader"),
+      s3Config = S3Builder.buildS3Config(config, namespace = "message.reader")
+    )
+
+  def buildMessageWriter[T](config: Config): MessageWriter[T] = {
+    new MessageWriter[T](
+      messageConfig = buildMessageWriterConfig(config),
+      snsClient = SNSBuilder.buildSNSClient(config),
+      s3Client = S3Builder.buildS3Client(config)
     )
   }
 }
