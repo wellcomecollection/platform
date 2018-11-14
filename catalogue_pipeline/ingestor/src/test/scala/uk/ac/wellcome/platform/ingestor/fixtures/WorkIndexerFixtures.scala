6c87bbc6b2b226ca0fd4df48260464715a3b8798
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-14 10:07:30 +0000

    We don't need to pass the HttpClient as an argument here

--- catalogue_pipeline/ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/fixtures/WorkIndexerFixtures.scala
+++ catalogue_pipeline/ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/fixtures/WorkIndexerFixtures.scala
@@ -2,19 +2,20 @@ package uk.ac.wellcome.platform.ingestor.fixtures
 
 import com.sksamuel.elastic4s.http.HttpClient
 import org.scalatest.Suite
+import uk.ac.wellcome.elasticsearch.test.fixtures.ElasticsearchFixtures
 import uk.ac.wellcome.platform.ingestor.services.WorkIndexer
 import uk.ac.wellcome.test.fixtures._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-trait WorkIndexerFixtures extends Akka { this: Suite =>
+trait WorkIndexerFixtures extends Akka with ElasticsearchFixtures { this: Suite =>
   def withWorkIndexer[R](elasticClient: HttpClient)(
     testWith: TestWith[WorkIndexer, R]): R = {
     val workIndexer = new WorkIndexer(elasticClient = elasticClient)
     testWith(workIndexer)
   }
 
-  def withWorkIndexerFixtures[R](esType: String, elasticClient: HttpClient)(
+  def withWorkIndexerFixtures[R](esType: String)(
     testWith: TestWith[WorkIndexer, R]): R = {
     withActorSystem { actorSystem =>
       withWorkIndexer(elasticClient = elasticClient) { workIndexer =>
