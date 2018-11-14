2d5dbeb78314eed2e3478bc0022af486b360f4ca
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-14 10:08:52 +0000

    We don't use the ActorSystem we've just created, so ditch it

--- catalogue_pipeline/ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/fixtures/WorkIndexerFixtures.scala
+++ catalogue_pipeline/ingestor/src/test/scala/uk/ac/wellcome/platform/ingestor/fixtures/WorkIndexerFixtures.scala
@@ -3,22 +3,19 @@ package uk.ac.wellcome.platform.ingestor.fixtures
 import org.scalatest.Suite
 import uk.ac.wellcome.elasticsearch.test.fixtures.ElasticsearchFixtures
 import uk.ac.wellcome.platform.ingestor.services.WorkIndexer
-import uk.ac.wellcome.test.fixtures._
+import uk.ac.wellcome.test.fixtures.TestWith
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-trait WorkIndexerFixtures extends Akka with ElasticsearchFixtures { this: Suite =>
+trait WorkIndexerFixtures extends ElasticsearchFixtures { this: Suite =>
   def withWorkIndexer[R](testWith: TestWith[WorkIndexer, R]): R = {
     val workIndexer = new WorkIndexer(elasticClient = elasticClient)
     testWith(workIndexer)
   }
 
   def withWorkIndexerFixtures[R](esType: String)(
-    testWith: TestWith[WorkIndexer, R]): R = {
-    withActorSystem { actorSystem =>
-      withWorkIndexer { workIndexer =>
-        testWith(workIndexer)
-      }
+    testWith: TestWith[WorkIndexer, R]): R =
+    withWorkIndexer { workIndexer =>
+      testWith(workIndexer)
     }
-  }
 }
