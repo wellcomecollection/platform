4eaeaeba4d12d57c7d36a6b1989f3954ad041d84
harrisonpim <harrison.stroma@gmail.com>
2018-11-14 14:46:56 +0000

    minor formatting

--- reporting/sierra_transformer/src/transform.py
+++ reporting/sierra_transformer/src/transform.py
@@ -5,7 +5,7 @@ def transform(input_data):
     # only look at the bib data for now
     json_string = input_data['maybeBibRecord']['data']
     bib_record = json.loads(json_string)
-    
+
     # ignore varFields on first pass
     del bib_record['varFields']
 
@@ -20,13 +20,13 @@ def transform(input_data):
     # unpack fixedFields
     if 'fixedFields' in bib_record:
         for key, value in bib_record['fixedFields'].items():
-            bib_record[f'fixedFields_{key}_{value['label']}'] = value['value']
+            bib_record[f"fixedFields_{key}_{value['label']}"] = value['value']
         del bib_record['fixedFields']
 
     # unpack language
     if 'lang' in bib_record:
         bib_record['lang'] = bib_record['lang']['name']
-    
+
     # unpack locations
     if 'locations' in bib_record:
         bib_record['locations'] = [
