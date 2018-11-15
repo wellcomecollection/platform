9d705fd556945ba86200a8ce61e3057eecbacb4a
Alex Chan <alex@alexwlchan.net>
2018-11-15 22:57:15 +0000

    Call DisplayJsonUtil directly in this flow

--- data_api/snapshot_generator/src/main/scala/uk/ac/wellcome/platform/snapshot_generator/flow/DisplayWorkToJsonStringFlow.scala
+++ data_api/snapshot_generator/src/main/scala/uk/ac/wellcome/platform/snapshot_generator/flow/DisplayWorkToJsonStringFlow.scala
@@ -3,21 +3,14 @@ package uk.ac.wellcome.platform.snapshot_generator.flow
 import akka.NotUsed
 import akka.stream.scaladsl.Flow
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.twitter.inject.Logging
+import uk.ac.wellcome.display.json.DisplayJsonUtil
+import uk.ac.wellcome.display.json.DisplayJsonUtil._
 import uk.ac.wellcome.display.models.DisplayWork
 
-import scala.util.{Failure, Success, Try}
-
-object DisplayWorkToJsonStringFlow extends Logging {
+object DisplayWorkToJsonStringFlow {
 
   def apply(mapper: ObjectMapper): Flow[DisplayWork, String, NotUsed] =
     Flow.fromFunction({ work =>
-      Try(mapper.writeValueAsString(work)) match {
-        case Success(s: String) => s
-        case Failure(parseFailure) => {
-          warn(s"Failed to turn DisplayWork $work into JSON!", parseFailure)
-          throw parseFailure
-        }
-      }
+      DisplayJsonUtil.toJson(work)
     })
 }
