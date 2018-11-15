6f7083d03ac8c36b3f9237e6fc95075c30735696
harrisonpim <harrison.stroma@gmail.com>
2018-11-15 11:47:25 +0000

    list comprehension over orders to cover cases where n orders > 1

--- reporting/sierra_transformer/src/transform.py
+++ reporting/sierra_transformer/src/transform.py
@@ -47,28 +47,28 @@ def transform(input_data):
             location['name'] for location in bib_record['locations']
         ]
     except KeyError:
-        pass
+        bib_record['locations'] = None
 
     # unpack material types
     try:
         bib_record['materialType'] = bib_record['materialType']['code']
     except KeyError:
-        pass
+        bib_record['materialType'] = None
 
     # unpack orders
     try:
-        for order in bib_record['orders']:
-            bib_record['order_locations'] = order['location']
-            bib_record['order_dates'] = order['date']
+        bib_record['order_locations'] = [
+            order['location'] for order in bib_record['orders']
+        ]
+        bib_record['order_dates'] = [
+            order['date'] for order in bib_record['orders']]
+        ]
         del bib_record['orders']
     except KeyError:
-        bib_record['order_locations'] = None
-        bib_record['order_dates'] = None
+        pass
 
     # get rid of redundant norm fields
-    norm_fields = [
-        field for field in bib_record if field.startswith('norm')
-    ]
+    norm_fields = [field for field in bib_record if field.startswith('norm')]
 
     for field in norm_fields:
         del bib_record[field]
