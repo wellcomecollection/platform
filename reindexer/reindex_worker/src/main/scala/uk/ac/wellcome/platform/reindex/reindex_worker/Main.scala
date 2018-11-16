26814cdd75810af55dcb810ef545c3ea68a2db20
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-16 15:42:41 +0000

    And do the reindexer

--- reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex/reindex_worker/Main.scala
+++ reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex/reindex_worker/Main.scala
@@ -1,69 +1,49 @@
 package uk.ac.wellcome.platform.reindex.reindex_worker
 
 import akka.actor.ActorSystem
-import com.typesafe.config.{Config, ConfigFactory}
-import grizzled.slf4j.Logging
+import com.typesafe.config.Config
+import uk.ac.wellcome.WellcomeApp
 import uk.ac.wellcome.config.core.builders.AkkaBuilder
 import uk.ac.wellcome.config.messaging.builders.{SNSBuilder, SQSBuilder}
 import uk.ac.wellcome.config.storage.builders.DynamoBuilder
 import uk.ac.wellcome.messaging.sns.NotificationMessage
-import uk.ac.wellcome.platform.reindex.reindex_worker.dynamo.{
-  MaxRecordsScanner,
-  ParallelScanner,
-  ScanSpecScanner
-}
-import uk.ac.wellcome.platform.reindex.reindex_worker.services.{
-  BulkSNSSender,
-  RecordReader,
-  ReindexWorkerService
-}
+import uk.ac.wellcome.platform.reindex.reindex_worker.dynamo.{MaxRecordsScanner, ParallelScanner, ScanSpecScanner}
+import uk.ac.wellcome.platform.reindex.reindex_worker.services.{BulkSNSSender, RecordReader, ReindexWorkerService}
 
-import scala.concurrent.{Await, ExecutionContext}
-import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext
 
-object Main extends App with Logging {
-  val config: Config = ConfigFactory.load()
+object Main extends WellcomeApp {
+  def buildWorkerService(config: Config): ReindexWorkerService = {
+    implicit val actorSystem: ActorSystem =
+      AkkaBuilder.buildActorSystem()
+    implicit val executionContext: ExecutionContext =
+      AkkaBuilder.buildExecutionContext()
 
-  implicit val actorSystem: ActorSystem =
-    AkkaBuilder.buildActorSystem()
-  implicit val executionContext: ExecutionContext =
-    AkkaBuilder.buildExecutionContext()
-
-  val scanSpecScanner = new ScanSpecScanner(
-    dynamoDBClient = DynamoBuilder.buildDynamoClient(config)
-  )
-
-  val recordReader = new RecordReader(
-    maxRecordsScanner = new MaxRecordsScanner(
-      scanSpecScanner = scanSpecScanner,
-      dynamoConfig = DynamoBuilder.buildDynamoConfig(config)
-    ),
-    parallelScanner = new ParallelScanner(
-      scanSpecScanner = scanSpecScanner,
-      dynamoConfig = DynamoBuilder.buildDynamoConfig(config)
+    val scanSpecScanner = new ScanSpecScanner(
+      dynamoDBClient = DynamoBuilder.buildDynamoClient(config)
     )
-  )
 
-  val hybridRecordSender = new BulkSNSSender(
-    snsWriter = SNSBuilder.buildSNSWriter(config)
-  )
+    val recordReader = new RecordReader(
+      maxRecordsScanner = new MaxRecordsScanner(
+        scanSpecScanner = scanSpecScanner,
+        dynamoConfig = DynamoBuilder.buildDynamoConfig(config)
+      ),
+      parallelScanner = new ParallelScanner(
+        scanSpecScanner = scanSpecScanner,
+        dynamoConfig = DynamoBuilder.buildDynamoConfig(config)
+      )
+    )
 
-  val workerService = new ReindexWorkerService(
-    recordReader = recordReader,
-    bulkSNSSender = hybridRecordSender,
-    sqsStream = SQSBuilder.buildSQSStream[NotificationMessage](config)
-  )
+    val hybridRecordSender = new BulkSNSSender(
+      snsWriter = SNSBuilder.buildSNSWriter(config)
+    )
 
-  try {
-    info(s"Starting worker.")
-
-    val result = workerService.run()
-
-    Await.result(result, Duration.Inf)
-  } catch {
-    case e: Throwable =>
-      error("Fatal error:", e)
-  } finally {
-    info(s"Terminating worker.")
+    new ReindexWorkerService(
+      recordReader = recordReader,
+      bulkSNSSender = hybridRecordSender,
+      sqsStream = SQSBuilder.buildSQSStream[NotificationMessage](config)
+    )
   }
+
+  run()
 }
--- reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex/reindex_worker/services/ReindexWorkerService.scala
+++ reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex/reindex_worker/services/ReindexWorkerService.scala
@@ -2,6 +2,7 @@ package uk.ac.wellcome.platform.reindex.reindex_worker.services
 
 import akka.Done
 import akka.actor.ActorSystem
+import uk.ac.wellcome.WorkerService
 import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.sqs.SQSStream
 import uk.ac.wellcome.platform.reindex.reindex_worker.models.ReindexJob
@@ -13,7 +14,7 @@ class ReindexWorkerService(
   recordReader: RecordReader,
   bulkSNSSender: BulkSNSSender,
   sqsStream: SQSStream[NotificationMessage]
-)(implicit val actorSystem: ActorSystem, ec: ExecutionContext) {
+)(implicit val actorSystem: ActorSystem, ec: ExecutionContext) extends WorkerService {
 
   private def processMessage(message: NotificationMessage): Future[Unit] =
     for {
