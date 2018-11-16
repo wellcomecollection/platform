096239e4ef9109f796abea3ee957a015a86214d5
harrisonpim <harrison.stroma@gmail.com>
2018-11-16 17:16:46 +0000

    refactor unpacking transforms

--- reporting/sierra_transformer/src/test_transform.py
+++ reporting/sierra_transformer/src/test_transform.py
@@ -1,32 +1,78 @@
 import json
-from transform import transform
+import datetime
+from transform import transform, unpack, parse_year_int_to_date
 
 
-def raw_data():
-    return json.loads(
-        """
-        {
-        "catalogue_api_derivative": true,
-        "catalogue_api_derivative.bucket": "some-bucket",
-        "catalogue_api_derivative.key": "some-key",
-        "catalogue_api_master": true,
-        "catalogue_api_master.bucket": "some-bucket",
-        "catalogue_api_master.key": "some-key",
-        "catalogue_entry.id": "some-id",
-        "cold_store.bucket": null,
-        "cold_store.key": null,
-        "cold_store_master": false,
-        "extra_s3_objects": [],
-        "id": "some-id",
-        "tandem_vault.asset_id": null,
-        "tandem_vault.bucket": null,
-        "tandem_vault.key": null,
-        "tandem_vault_master": false
+def build_test_data(full=False):
+    bib_record = {"string_field": "some string",
+                  "boolean_field": True,
+                  "single_dict_single_subfield": {"a": "b"},
+                  "single_dict_multiple_subfields": {"a": "b", "x": "y"},
+                  "multiple_dicts_single_subfield": [{"a": "b"},
+                                                     {"a": "c"}],
+                  "multiple_dicts_multiple_subfields": [{"a": "b", "x": "y"},
+                                                        {"a": "c", "x": "z"}],
+                  "iso_date": "1970-01-01T00:00:00Z",
+                  "date_year": 1970,
+                  "empty_string": ""}
+
+    if full:
+        return {
+            "sierraId": {
+                "recordNumber": "3075974"
+            },
+            "maybeBibRecord": {
+                "id": {
+                    "recordNumber": "3075974"
+                },
+                "data": json.dumps(bib_record),
+                "modifiedDate": "2018-11-12T11:55:59Z"
+            },
+            "itemRecords": {}
         }
-        """
-    )
+
+    else:
+        return bib_record
 
 
-def test_json_parses():
-    transformed_data = transform(raw_data())
-    assert isinstance(transformed_data, dict)
+def test_parses_year_int_to_date_from():
+    raw_data = build_test_data()
+    parsed_from, _ = parse_year_int_to_date(raw_data, "date_year")
+    assert parsed_from == datetime.datetime(1970, 1, 1, 0, 0, 0)
+
+
+def test_parses_year_int_to_date_to():
+    raw_data = build_test_data()
+    _, parsed_to = parse_year_int_to_date(raw_data, "date_year")
+    assert parsed_to == datetime.datetime(1970, 12, 31, 23, 59, 59)
+
+
+def test_transform_preserves_unspecified_fields():
+    raw_data = build_test_data(full=True)
+    transformed = transform(raw_data)
+    raw_bib_record = json.loads(raw_data['maybeBibRecord']['data'])
+    assert transformed["string_field"] == raw_bib_record["string_field"]
+
+
+def test_unpacks_single_dict_single_subfield():
+    raw_data = build_test_data()
+    unpacked = unpack(raw_data, "single_dict_single_subfield", "a")
+    assert unpacked == [["b"]]
+
+
+def test_unpacks_single_dict_multiple_subfields():
+    raw_data = build_test_data()
+    unpacked = unpack(raw_data, "single_dict_multiple_subfields", ["a", "x"])
+    assert unpacked == [["b"], ["y"]]
+
+
+def test_unpacks_multiple_dicts_single_subfield():
+    raw_data = build_test_data()
+    unpacked = unpack(raw_data, "multiple_dicts_single_subfield", "a")
+    assert unpacked == [["b", "c"]]
+
+
+def test_unpacks_multiple_dicts_multiple_subfields():
+    raw_data = build_test_data()
+    unpacked = unpack(raw_data, "multiple_dicts_multiple_subfields", ["a", "x"])
+    assert unpacked == [["b", "c"], ["y", "z"]]
--- reporting/sierra_transformer/src/transform.py
+++ reporting/sierra_transformer/src/transform.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+from dateutil.parser import parse
 import json
 
 
@@ -15,18 +17,6 @@ def transform(input_data):
     except KeyError:
         pass
 
-    # unpack bibLevel
-    try:
-        bib_record['bibLevel'] = bib_record['bibLevel']['value']
-    except KeyError:
-        bib_record['bibLevel'] = None
-
-    # unpack country
-    try:
-        bib_record['country'] = bib_record['country']['name']
-    except KeyError:
-        bib_record['country'] = None
-
     # unpack fixedFields
     try:
         for key, value in bib_record['fixedFields'].items():
@@ -35,42 +25,129 @@ def transform(input_data):
     except KeyError:
         pass
 
-    # unpack language
+    # unpack bibLevel
+    bib_record['bibLevel_value'] = unpack(
+        record=bib_record,
+        field_name='bibLevel',
+        subfields_to_keep='value'
+    )
     try:
-        bib_record['lang'] = bib_record['lang']['name']
+        del bib_record['bibLevel']
     except KeyError:
-        bib_record['lang'] = None
+        pass
 
-    # unpack locations
+    # unpack country
+    bib_record['country_name'] = unpack(
+        record=bib_record,
+        field_name='country',
+        subfields_to_keep='name'
+    )
     try:
-        bib_record['locations'] = [
-            location['name'] for location in bib_record['locations']
-        ]
+        del bib_record['country']
     except KeyError:
-        bib_record['locations'] = None
+        pass
+
+    # unpack language
+    bib_record['lang_name'] = unpack(
+        record=bib_record,
+        field_name='lang',
+        subfields_to_keep='name'
+    )
+    try:
+        del bib_record['lang']
+    except KeyError:
+        pass
 
     # unpack material types
+    bib_record['materialType_code'] = unpack(
+        record=bib_record,
+        field_name='materialType',
+        subfields_to_keep='code'
+    )
     try:
-        bib_record['materialType'] = bib_record['materialType']['code']
+        del bib_record['materialType']
     except KeyError:
-        bib_record['materialType'] = None
+        pass
+
+    # unpack locations
+    locations_name, locations_code = unpack(
+        record=bib_record,
+        field_name='locations',
+        subfields_to_keep=['name', 'code']
+    )
+    bib_record['locations_name'] = locations_name
+    bib_record['locations_code'] = locations_code
+    try:
+        del bib_record['locations']
+    except KeyError:
+        pass
 
     # unpack orders
+    orders_date = unpack(
+        record=bib_record,
+        field_name='orders',
+        subfields_to_keep='date'
+    )
     try:
-        bib_record['order_locations'] = [
-            order['location'] for order in bib_record['orders']
-        ]
-        bib_record['order_dates'] = [
-            order['date'] for order in bib_record['orders']
-        ]
+        bib_record['orders_date'] = [parse(date) for date in orders_date]
+    except:
+        pass
+
+    try:
+        orders = bib_record['orders']
+        order_location_codes, order_location_names = unpack(
+            record=orders,
+            field_name='location',
+            subfields_to_keep=['name', 'code']
+        )
+        bib_record['order_location_codes'] = order_location_codes
+        bib_record['order_location_names'] = order_location_names
         del bib_record['orders']
+    except KeyError:
+        pass
+
+    # parse publish year
+    year_from, year_to = parse_year_int_to_date(bib_record, 'publishYear')
+    bib_record['publishYear_from'] = year_from
+    bib_record['publishYear_to'] = year_to
+    try:
+        del bib_record['publishYear']
     except KeyError:
         pass
 
     # get rid of redundant norm fields
     norm_fields = [field for field in bib_record if field.startswith('norm')]
-
     for field in norm_fields:
         del bib_record[field]
 
     return bib_record
+
+
+def parse_year_int_to_date(bib_record, field_name):
+    try:
+        year_from = datetime(bib_record[field_name], 1, 1)
+        year_to = year_from + timedelta(days=365) - timedelta(seconds=1)
+    except KeyError:
+        year_from = None
+        year_to = None
+    return year_from, year_to
+
+
+def unpack(record, field_name, subfields_to_keep):
+    if isinstance(subfields_to_keep, str):
+        subfields_to_keep = [subfields_to_keep]
+
+    try:
+        data_to_unpack = record[field_name]
+        if isinstance(data_to_unpack, dict):
+            data_to_unpack = [data_to_unpack]
+
+        unpacked = [
+            [field[subfield] for field in data_to_unpack]
+            for subfield in subfields_to_keep
+        ]
+
+    except (KeyError, TypeError):
+        unpacked = [[None] for subfield in subfields_to_keep]
+
+    return unpacked
