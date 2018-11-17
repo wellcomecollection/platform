2f70c039ef803d078fbe0da316d8d91eb1c4d73d
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-11-17 13:13:28 +0000

    module relative object path

--- archive/terraform/static_objects.tf
+++ archive/terraform/static_objects.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket_object" "context" {
   bucket = "${local.storage_static_content_bucket_name}"
-  key    = "context.json"
+  key    = "${path.module}/context.json"
   source = "../resources/context.json"
   etag   = "${md5(file("../resources/context.json"))}"
 }
