b588aba9e89596d1b8ec234232b3eea7bf932291
Robert Kenny <kenoir@users.noreply.github.com>
2018-11-19 12:40:52 +0000

    Remove old Catalogue API infra (#3053)
    * Remove old Catalogue API infra
    
    * Apply auto-formatting rules
    
    * Apply auto-formatting rules

--- catalogue_api/terraform/api_docs/iam.tf
+++ catalogue_api/terraform/api_docs/iam.tf
@@ -1,8 +1,3 @@
-module "ecs_api_iam" {
-  source = "git::https://github.com/wellcometrust/terraform.git//ecs_iam?ref=v1.0.0"
-  name   = "api"
-}
-
 module "ecs_update_api_docs_iam" {
   source = "git::https://github.com/wellcometrust/terraform.git//ecs_iam?ref=v1.0.0"
   name   = "update_api_docs"
