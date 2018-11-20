4fa686ad450f7110ff2c6ca8a068acd6bdcfd219
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-11-20 18:38:09 +0000

    remove defunct db

--- infrastructure/critical/terraform/rds.tf
+++ infrastructure/critical/terraform/rds.tf
@@ -1,35 +1,3 @@
-module "identifiers_delta_rds_cluster" {
-  source             = "git::https://github.com/wellcometrust/terraform.git//rds?ref=v13.0.0"
-  cluster_identifier = "identifiers-delta"
-  database_name      = "identifiers"
-  username           = "${var.rds_username}"
-  password           = "${var.rds_password}"
-  vpc_subnet_ids     = "${local.private_subnets}"
-  vpc_id             = "${local.vpc_id}"
-  admin_cidr_ingress = "${var.admin_cidr_ingress}"
-
-  db_access_security_group = ["${aws_security_group.rds_access_security_group.id}"]
-
-  vpc_security_group_ids = []
-}
-
-resource "aws_security_group" "rds_access_security_group" {
-  name        = "pipeline_rds_access_security_group"
-  description = "Allow traffic to rds database"
-  vpc_id      = "${local.vpc_id}"
-
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
-
-  tags {
-    Name = "pipeline-rds-access"
-  }
-}
-
 module "identifiers_rds_cluster" {
   source             = "git::https://github.com/wellcometrust/terraform.git//rds?ref=v13.0.0"
   cluster_identifier = "identifiers"
