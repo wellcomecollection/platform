9dc5f5d2d7be1115a67cf3b36c51ce57d041c46d
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-11-20 07:51:19 +0000

    makefile includes

--- infrastructure/critical/terraform/rds.tf
+++ infrastructure/critical/terraform/rds.tf
@@ -12,3 +12,21 @@ module "identifiers_delta_rds_cluster" {
 
   vpc_security_group_ids = []
 }
+
+resource "aws_security_group" "rds_access_security_group" {
+  name        = "pipeline_rds_access_security_group"
+  description = "Allow traffic to rds database"
+  vpc_id      = "${local.vpc_id}"
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  tags {
+    Name = "pipeline-rds-access"
+  }
+}
+
