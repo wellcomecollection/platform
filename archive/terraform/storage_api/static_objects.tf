6473e5a3a1f1eec1ff8b1bfe4fbabea1fa785be6
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-11-22 09:14:32 +0000

    use file to load content as source will change based on users file location

--- archive/terraform/storage_api/static_objects.tf
+++ archive/terraform/storage_api/static_objects.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket_object" "context" {
   bucket = "${var.storage_static_content_bucket_name}"
   key    = "static/context.json"
-  source = "${path.module}/context.json"
+  content = "${file("${path.module}/context.json")}"
   etag   = "${md5(file("${path.module}/context.json"))}"
 }
