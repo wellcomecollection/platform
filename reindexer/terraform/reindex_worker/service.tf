50cb70ce1d3cb59efa8915cab23b442d653ca257
alicefuzier <alicefuzier@gmail.com>
2018-11-22 14:07:44 +0000

    Don't need to set the parallelism so low

--- reindexer/terraform/reindex_worker/service.tf
+++ reindexer/terraform/reindex_worker/service.tf
@@ -18,11 +18,7 @@ module "service" {
     metrics_namespace         = "reindex_worker-${var.namespace}"
     dynamo_table_name         = "${var.vhs_table_name}"
 
-    # The reindex worker has to send lots of SNS notifications, and we've
-    # seen issues where we exhaust the HTTP connection pool.  Turning down
-    # the parallelism is an attempt to reduce the number of SNS messages in
-    # flight, and avoid these errors.
-    sqs_parallelism = 5
+    sqs_parallelism = 10
   }
 
   env_vars_length = 5
