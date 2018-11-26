48b3e37f7744877ede2ecbdb0cfd472dd86d0163
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-11-26 14:30:53 +0000

    Add datascience & monitoring VPCs

--- infrastructure/shared/terraform/vpcs.tf
+++ infrastructure/shared/terraform/vpcs.tf
@@ -34,3 +34,37 @@ module "storage_vpc_delta" {
   cidr_block_private         = "172.30.128.0/17"
   cidrsubnet_newbits_private = "2"
 }
+
+# Used by:
+# - Grafana service
+# - Various monitoring lambdas
+
+module "monitoring_vpc_delta" {
+  source = "github.com/wellcometrust/terraform//network/prebuilt/vpc/public-private-igw?ref=v16.1.0"
+
+  name = "monitoring-172-28-0-0-16"
+
+  cidr_block_vpc            = "172.28.0.0/16"
+  cidr_block_public         = "172.28.0.0/17"
+  cidrsubnet_newbits_public = "2"
+
+  cidr_block_private         = "172.28.128.0/17"
+  cidrsubnet_newbits_private = "2"
+}
+
+# Used by:
+# - Data science service
+# - Labs apps & data scientist infra
+
+module "datascience_vpc_delta" {
+  source = "github.com/wellcometrust/terraform//network/prebuilt/vpc/public-private-igw?ref=v16.1.0"
+
+  name = "datascience-172-27-0-0-16"
+
+  cidr_block_vpc            = "172.27.0.0/16"
+  cidr_block_public         = "172.27.0.0/17"
+  cidrsubnet_newbits_public = "2"
+
+  cidr_block_private         = "172.27.128.0/17"
+  cidrsubnet_newbits_private = "2"
+}
