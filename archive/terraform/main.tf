0cddc394fafef558000e3220daa24a36050abded
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-11-26 12:32:57 +0000

    fmt

--- archive/terraform/main.tf
+++ archive/terraform/main.tf
@@ -321,7 +321,6 @@ module "storage_api" {
     topic_arn                   = "${module.ingest_requests_topic.arn}"
     archive_progress_table_name = "${aws_dynamodb_table.archive_progress_table.name}"
   }
-
   ingests_env_vars_length            = 4
   ingests_nginx_container_image      = "${local.nginx_container_image}"
   ingests_nginx_container_port       = "9000"
