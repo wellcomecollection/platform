84a84a4e5e55c2755681adec684c9ffc3f2c9510
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-27 10:24:29 +0000

    Start creating a Miro VHS record receiver service

---
+++ catalogue_pipeline/transformer/transformer_miro/src/test/scala/uk/ac/wellcome/platform/transformer/miro/fixtures/MiroVHSRecordReceiverFixture.scala
@@ -0,0 +1,31 @@
+package uk.ac.wellcome.platform.transformer.miro.fixtures
+
+import com.amazonaws.services.sns.AmazonSNS
+import uk.ac.wellcome.messaging.test.fixtures.{Messaging, SNS}
+import uk.ac.wellcome.messaging.test.fixtures.SNS.Topic
+import uk.ac.wellcome.models.work.internal.TransformedBaseWork
+import uk.ac.wellcome.platform.transformer.miro.services.MiroVHSRecordReceiver
+import uk.ac.wellcome.platform.transformer.miro.source.MiroRecord
+import uk.ac.wellcome.storage.ObjectStore
+import uk.ac.wellcome.storage.fixtures.S3.Bucket
+import uk.ac.wellcome.test.fixtures.TestWith
+
+trait MiroVHSRecordReceiverFixture extends Messaging with SNS {
+  def withMiroVHSRecordReceiver[R](
+    topic: Topic,
+    bucket: Bucket,
+    snsClient: AmazonSNS = snsClient
+  )(testWith: TestWith[MiroVHSRecordReceiver, R])(
+    implicit objectStore: ObjectStore[MiroRecord]): R =
+    withMessageWriter[TransformedBaseWork, R](
+      bucket,
+      topic,
+      writerSnsClient = snsClient) { messageWriter =>
+      val recordReceiver = new MiroVHSRecordReceiver(
+        objectStore = objectStore,
+        messageWriter = messageWriter
+      )
+
+      testWith(recordReceiver)
+    }
+}
