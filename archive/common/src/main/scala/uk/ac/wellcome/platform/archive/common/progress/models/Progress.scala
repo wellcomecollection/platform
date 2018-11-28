6e3eb2f3428a3e5ce6d6ff1ce72eeadcad2378e8
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-28 14:28:36 +0000

    Create a race-free implementation of Progress.apply

--- archive/common/src/main/scala/uk/ac/wellcome/platform/archive/common/progress/models/Progress.scala
+++ archive/common/src/main/scala/uk/ac/wellcome/platform/archive/common/progress/models/Progress.scala
@@ -6,17 +6,49 @@ import java.util.UUID
 import uk.ac.wellcome.platform.archive.common.json.URIConverters
 import uk.ac.wellcome.platform.archive.common.models.{BagId, Namespace}
 
-case class Progress(id: UUID,
-                    sourceLocation: StorageLocation,
-                    space: Namespace,
-                    callback: Option[Callback] = None,
-                    status: Progress.Status = Progress.Accepted,
-                    bag: Option[BagId] = None,
-                    createdDate: Instant = Instant.now,
-                    lastModifiedDate: Instant = Instant.now,
-                    events: Seq[ProgressEvent] = Seq.empty)
+case class Progress(
+  id: UUID,
+  sourceLocation: StorageLocation,
+  space: Namespace,
+  callback: Option[Callback],
+  status: Progress.Status,
+  bag: Option[BagId],
+  createdDate: Instant,
+  lastModifiedDate: Instant,
+  events: Seq[ProgressEvent]
+)
 
 case object Progress extends URIConverters {
+
+  // The important bit of this apply() method is that it only creates the
+  // Instant for the created/modified date once, so it's always the same.
+  //
+  // If you set `Instant.now` as the default on the case class, it gets
+  // created twice -- and if you're unlucky, they'll be different.
+  def apply(
+    id: UUID,
+    sourceLocation: StorageLocation,
+    space: Namespace,
+    callback: Option[Callback] = None,
+    status: Progress.Status = Progress.Accepted,
+    bag: Option[BagId] = None,
+    events: Seq[ProgressEvent] = Seq.empty
+  ): Progress = {
+    val now = Instant.now
+
+    Progress(
+      id = id,
+      sourceLocation = sourceLocation,
+      space = space,
+      callback = callback,
+      status = status,
+      bag = bag,
+      createdDate = now,
+      lastModifiedDate = now,
+      events = events
+    )
+  }
+
   sealed trait Status
 
   private val acceptedString = "accepted"
