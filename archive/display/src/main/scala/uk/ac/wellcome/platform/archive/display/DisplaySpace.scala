218791ae6ad8d89a6edf4972feee614032a6052b
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-28 14:12:54 +0000

    Add an apply method for creating DisplaySpace from Namespace

--- archive/display/src/main/scala/uk/ac/wellcome/platform/archive/display/DisplaySpace.scala
+++ archive/display/src/main/scala/uk/ac/wellcome/platform/archive/display/DisplaySpace.scala
@@ -1,8 +1,14 @@
 package uk.ac.wellcome.platform.archive.display
 
 import io.circe.generic.extras.JsonKey
+import uk.ac.wellcome.platform.archive.common.models.Namespace
 
 case class DisplaySpace(
   id: String,
   @JsonKey("type") ontologyType: String = "Space"
 )
+
+case object DisplaySpace {
+  def apply(namespace: Namespace): DisplaySpace =
+    DisplaySpace(id = namespace.underlying)
+}
