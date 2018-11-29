78cf7a4d46fda645d273f6794c6786ac6f63d3e5
alicefuzier <alicefuzier@gmail.com>
2018-11-29 14:45:08 +0000

    Explanatory comment

--- catalogue_pipeline/ingestor/src/main/scala/uk/ac/wellcome/platform/ingestor/services/WorkIndexer.scala
+++ catalogue_pipeline/ingestor/src/main/scala/uk/ac/wellcome/platform/ingestor/services/WorkIndexer.scala
@@ -65,6 +65,26 @@ class WorkIndexer(
       }
   }
 
+  /**
+    * When the merger makes the decision to merge some works, it modifies the content of
+    * the affected works. Despite the content of these works being modified, their version
+    * remains the same. Instead, the merger sets the merged flag to true for the target work
+    * and changes the type of the other works to redirected.
+    *
+    * When we ingest those works, we need to make sure that the merger modified works
+    * are never overridden by unmerged works for the same version still running through
+    * the pipeline.
+    * We also need to make sure that, if a work is modified by a source in such a way that
+    * it shouldn't be merged (or redirected) anymore, the new unmerged version is ingested
+    * and never replaced by the previous merged version still running through the pipeline.
+    *
+    * We can do that by ingesting works into elastic search with a version derived by a
+    * combination of work version, merged flag and work type. More specifically, by
+    * multiplying the work version by 10, we make sure that a new version of a work
+    * always wins over previous versions (merged or unmerged).
+    * We make sure that a merger modified work always wins over other works with the same
+    * version, by adding one to work.version * 10.
+    */
   private def calculateEsVersion(work: IdentifiedBaseWork): Int = work match {
     case w: IdentifiedWork           => (w.version * 10) + w.merged
     case w: IdentifiedRedirectedWork => (w.version * 10) + 1
