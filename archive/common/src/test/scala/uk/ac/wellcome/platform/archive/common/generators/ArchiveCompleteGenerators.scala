9ddc6d6e8087fbe4421d7469ac111a105da66387
Alex Chan <alex@alexwlchan.net>
2018-11-29 08:33:31 +0000

    Add ArchiveCompleteGenerators in the style of our other generators

---
+++ archive/common/src/test/scala/uk/ac/wellcome/platform/archive/common/generators/ArchiveCompleteGenerators.scala
@@ -0,0 +1,19 @@
+package uk.ac.wellcome.platform.archive.common.generators
+
+import java.util.UUID
+
+import uk.ac.wellcome.platform.archive.common.fixtures.RandomThings
+import uk.ac.wellcome.platform.archive.common.models.{ArchiveComplete, BagLocation, StorageSpace}
+
+trait ArchiveCompleteGenerators extends RandomThings {
+  def createArchiveCompleteWith(
+    archiveRequestId: UUID = randomUUID,
+    space: StorageSpace = randomStorageSpace,
+    bagLocation: BagLocation
+  ): ArchiveComplete =
+    ArchiveComplete(
+      archiveRequestId = archiveRequestId,
+      space = space,
+      bagLocation = bagLocation
+    )
+}
--- archive/registrar_async/src/test/scala/uk/ac/wellcome/platform/archive/registrar/async/factories/StorageManifestFactoryTest.scala
+++ archive/registrar_async/src/test/scala/uk/ac/wellcome/platform/archive/registrar/async/factories/StorageManifestFactoryTest.scala
@@ -3,32 +3,31 @@ package uk.ac.wellcome.platform.archive.registrar.async.factories
 import com.amazonaws.services.s3.model.AmazonS3Exception
 import org.scalatest.{FunSpec, Inside}
 import uk.ac.wellcome.platform.archive.common.fixtures.{FileEntry, RandomThings}
+import uk.ac.wellcome.platform.archive.common.generators.ArchiveCompleteGenerators
 import uk.ac.wellcome.platform.archive.common.models.error.{
   DownloadError,
   InvalidBagManifestError
 }
-import uk.ac.wellcome.platform.archive.common.models.{
-  ArchiveComplete,
-  BagLocation,
-  BagPath
-}
+import uk.ac.wellcome.platform.archive.common.models.{BagLocation, BagPath}
 import uk.ac.wellcome.platform.archive.registrar.async.fixtures.BagLocationFixtures
 import uk.ac.wellcome.platform.archive.registrar.common.models._
+
 class StorageManifestFactoryTest
     extends FunSpec
+    with ArchiveCompleteGenerators
     with BagLocationFixtures
     with RandomThings
     with Inside {
   implicit val _ = s3Client
 
   it("returns a right of storage manifest if reading a bag location succeeds") {
-    val requestId = randomUUID
-
     withLocalS3Bucket { bucket =>
       withBag(bucket) {
         case (bagLocation, bagInfo, bagId) =>
-          val archiveComplete =
-            ArchiveComplete(requestId, bagId.space, bagLocation)
+          val archiveComplete = createArchiveCompleteWith(
+            space = bagId.space,
+            bagLocation = bagLocation
+          )
 
           val storageManifest =
             StorageManifestFactory.create(archiveComplete)
@@ -59,15 +58,12 @@ class StorageManifestFactoryTest
 
   describe("returning a left of registrar error ...") {
     it("if no files are at the BagLocation") {
-
-      val storageSpace = randomStorageSpace
-      val requestId = randomUUID
-
       withLocalS3Bucket { bucket =>
         val bagLocation =
           BagLocation(bucket.name, "archive", BagPath(s"space/b1234567"))
-        val archiveComplete =
-          ArchiveComplete(requestId, storageSpace, bagLocation)
+        val archiveComplete = createArchiveCompleteWith(
+          bagLocation = bagLocation
+        )
         val value = StorageManifestFactory.create(archiveComplete)
 
         inside(value) {
@@ -82,16 +78,15 @@ class StorageManifestFactoryTest
     }
 
     it("if the BagLocation has an invalid manifest") {
-      val requestId = randomUUID
-
       withLocalS3Bucket { bucket =>
         withBag(
           bucket,
           createDataManifest =
             _ => Some(FileEntry("manifest-sha256.txt", "bleeergh!"))) {
-          case (bagLocation, _, bagId) =>
-            val archiveComplete =
-              ArchiveComplete(requestId, bagId.space, bagLocation)
+          case (bagLocation, _, _) =>
+            val archiveComplete = createArchiveCompleteWith(
+              bagLocation = bagLocation
+            )
             val value = StorageManifestFactory.create(archiveComplete)
             value shouldBe Left(
               InvalidBagManifestError(archiveComplete, "manifest-sha256.txt"))
@@ -100,16 +95,15 @@ class StorageManifestFactoryTest
     }
 
     it("if the BagLocation has an invalid tagmanifest") {
-      val requestId = randomUUID
-
       withLocalS3Bucket { bucket =>
         withBag(
           bucket,
           createTagManifest =
             _ => Some(FileEntry("tagmanifest-sha256.txt", "blaaargh!"))) {
-          case (bagLocation, _, bagId) =>
-            val archiveComplete =
-              ArchiveComplete(requestId, bagId.space, bagLocation)
+          case (bagLocation, _, _) =>
+            val archiveComplete = createArchiveCompleteWith(
+              bagLocation = bagLocation
+            )
             val value = StorageManifestFactory.create(archiveComplete)
             value shouldBe Left(
               InvalidBagManifestError(
