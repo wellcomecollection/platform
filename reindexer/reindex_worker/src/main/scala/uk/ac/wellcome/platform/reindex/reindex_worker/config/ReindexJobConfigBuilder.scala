a9e539eb36782ce69b664efac7e9fa1478348c62
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-30 15:11:24 +0000

    Log it at startup for now

--- reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex/reindex_worker/config/ReindexJobConfigBuilder.scala
+++ reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex/reindex_worker/config/ReindexJobConfigBuilder.scala
@@ -1,15 +1,19 @@
 package uk.ac.wellcome.platform.reindex.reindex_worker.config
 
 import com.typesafe.config.Config
+import grizzled.slf4j.Logging
 import uk.ac.wellcome.config.core.builders.EnrichConfig._
 import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.platform.reindex.reindex_worker.models.ReindexJobConfig
 
-object ReindexJobConfigBuilder {
+object ReindexJobConfigBuilder extends Logging {
   def buildReindexJobConfigMap(config: Config): Map[String, ReindexJobConfig] = {
     val jsonString = config.required("reindexer.jobConfig")
-    fromJson[Map[String, ReindexJobConfig]](jsonString).getOrElse(
+    val configMap = fromJson[Map[String, ReindexJobConfig]](jsonString).getOrElse(
       throw new RuntimeException(s"Unable to parse reindexer.jobConfig: <<$jsonString>>")
     )
+
+    info(s"Read config map $configMap")
+    configMap
   }
 }
