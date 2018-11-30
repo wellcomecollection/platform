f785e9fc63072889186c1255a4eb937962fcf268
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-30 16:06:44 +0000

    Start working out the reindex information

--- reindexer/start_reindex.py
+++ reindexer/start_reindex.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8
 
+import math
+import sys
+
+import boto3
 import click
 
 
@@ -14,6 +18,46 @@ DESTINATIONS = [
     "catalogue",
     "reporting"
 ]
+
+
+def how_many_segments(table_name):
+    """
+    When we do a complete reindex, we need to tell the reindexer how many segments
+    to use.  Each segment should contain ~1000 records, so we don't exhaust the
+    memory in the reindexer.
+
+    (The reindexer loads the contents of each segment into memory, so choosing overly
+    large segment sizes causes it to fall over.)
+
+    """
+    dynamodb = boto3.client("dynamodb")
+    resp = dynamodb.describe_table(TableName=table_name)
+
+    # The item count isn't real-time; it gets updated every six hours or so.
+    # In practice it's the right order of magnitude: if the table has lots of churn,
+    # it's probably a bad time to reindex!
+    try:
+        item_count = resp["Table"]["ItemCount"]
+    except KeyError:
+        sys.exit("No such table {table_name!r}?")
+
+    return int(math.ceil(item_count / 900))
+
+
+def complete_reindex_parameters(total_segments):
+    for segment in range(total_segments):
+        yield {
+            "segment": segment,
+            "totalSegments": total_segments,
+            "type": "CompleteReindexParameters",
+        }
+
+
+def partial_reindex_parameters(max_records):
+    yield {
+        "maxRecords": max_records,
+        "type": "PartialReindexParameters"
+    }
 
 
 @click.command()
@@ -37,6 +81,15 @@ DESTINATIONS = [
     help="The reason to run this reindex"
 )
 def start_reindex(src, dst, mode, reason):
+    print(f"Starting a reindex {src!r} ~> {dst!r}")
+
+    if mode == "complete":
+        total_segments = how_many_segments(table_name=f"vhs-sourcedata-{src}")
+        parameters = complete_reindex_parameters(total_segments)
+    elif mode == "partial":
+        max_records = click.prompt("How many records do you want to send?", default=10)
+        parameters = partial_reindex_parameters(total_segments)
+
     print(src)
     print(dst)
     print(mode)
