45e55020c440639ea65471ee917b4f1298c870ed
Alex Chan <a.chan@wellcome.ac.uk>
2018-12-03 13:14:51 +0000

    Create the basic WellcomeApp trait

---
+++ sbt_common/common/src/main/scala/uk/ac/wellcome/WellcomeApp.scala
@@ -0,0 +1,22 @@
+package uk.ac.wellcome
+
+import grizzled.slf4j.Logging
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+trait WellcomeApp extends App with Logging {
+  def run(workerService: WorkerService) =
+    try {
+      info(s"Starting worker.")
+
+      val result = workerService.run()
+
+      Await.result(result, Duration.Inf)
+    } catch {
+      case e: Throwable =>
+        error("Fatal error:", e)
+    } finally {
+      info(s"Terminating worker.")
+    }
+}
