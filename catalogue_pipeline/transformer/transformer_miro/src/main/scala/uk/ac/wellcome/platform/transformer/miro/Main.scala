a8412e60d488775db121824a6e56a50e7b95e741
Alex Chan <a.chan@wellcome.ac.uk>
2018-12-03 13:31:30 +0000

    And push the WellcomeTypesafeApp trait out to a bunch of places

--- catalogue_pipeline/transformer/transformer_miro/src/main/scala/uk/ac/wellcome/platform/transformer/miro/Main.scala
+++ catalogue_pipeline/transformer/transformer_miro/src/main/scala/uk/ac/wellcome/platform/transformer/miro/Main.scala
@@ -1,8 +1,8 @@
 package uk.ac.wellcome.platform.transformer.miro
 
 import akka.actor.ActorSystem
-import com.typesafe.config.{Config, ConfigFactory}
-import grizzled.slf4j.Logging
+import com.typesafe.config.Config
+import uk.ac.wellcome.config.core.WellcomeTypesafeApp
 import uk.ac.wellcome.config.core.builders.AkkaBuilder
 import uk.ac.wellcome.config.messaging.builders.{MessagingBuilder, SQSBuilder}
 import uk.ac.wellcome.config.storage.builders.S3Builder
@@ -15,39 +15,25 @@ import uk.ac.wellcome.platform.transformer.miro.services.{
 }
 import uk.ac.wellcome.platform.transformer.miro.source.MiroRecord
 
-import scala.concurrent.{Await, ExecutionContext}
-import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext
 
-object Main extends App with Logging {
-  val config: Config = ConfigFactory.load()
+object Main extends WellcomeTypesafeApp {
+  runWithConfig { config: Config =>
+    implicit val actorSystem: ActorSystem =
+      AkkaBuilder.buildActorSystem()
+    implicit val executionContext: ExecutionContext =
+      AkkaBuilder.buildExecutionContext()
 
-  implicit val actorSystem: ActorSystem =
-    AkkaBuilder.buildActorSystem()
-  implicit val executionContext: ExecutionContext =
-    AkkaBuilder.buildExecutionContext()
+    val vhsRecordReceiver = new MiroVHSRecordReceiver(
+      messageWriter =
+        MessagingBuilder.buildMessageWriter[TransformedBaseWork](config),
+      objectStore = S3Builder.buildObjectStore[MiroRecord](config)
+    )
 
-  val vhsRecordReceiver = new MiroVHSRecordReceiver(
-    messageWriter =
-      MessagingBuilder.buildMessageWriter[TransformedBaseWork](config),
-    objectStore = S3Builder.buildObjectStore[MiroRecord](config)
-  )
-
-  val workerService = new MiroTransformerWorkerService(
-    vhsRecordReceiver = vhsRecordReceiver,
-    miroTransformer = new MiroTransformableTransformer,
-    sqsStream = SQSBuilder.buildSQSStream[NotificationMessage](config)
-  )
-
-  try {
-    info("Starting worker.")
-
-    val result = workerService.run()
-
-    Await.result(result, Duration.Inf)
-  } catch {
-    case e: Throwable =>
-      error("Fatal error:", e)
-  } finally {
-    info("Terminating worker.")
+    new MiroTransformerWorkerService(
+      vhsRecordReceiver = vhsRecordReceiver,
+      miroTransformer = new MiroTransformableTransformer,
+      sqsStream = SQSBuilder.buildSQSStream[NotificationMessage](config)
+    )
   }
 }
