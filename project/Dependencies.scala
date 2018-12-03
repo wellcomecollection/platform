9e6e3181726c1e6b9cf8baf23385b8c92d07b492
Alex Chan <a.chan@wellcome.ac.uk>
2018-12-03 14:52:25 +0000

    Upgrade to Elasticsearch 6 in libraries/API tests

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Dependencies {
     val mockito = "1.9.5"
     val scalatest = "3.0.1"
     val junitInterface = "0.11"
-    val elastic4s = "5.6.5"
+    val elastic4s = "6.4.0"
     val circeVersion = "0.9.0"
     val scalaCheckVersion = "1.13.4"
     val scalaCheckShapelessVersion = "1.1.6"
