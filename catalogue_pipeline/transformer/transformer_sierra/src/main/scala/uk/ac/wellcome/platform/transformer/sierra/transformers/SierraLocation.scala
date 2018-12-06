2b54599aa89aab15a73710ced4f180f9383b5e20
Alex Chan <a.chan@wellcome.ac.uk>
2018-12-06 12:07:36 +0000

    Make a best-guess effort to get the LocationType

--- catalogue_pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraLocation.scala
+++ catalogue_pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraLocation.scala
@@ -1,11 +1,12 @@
 package uk.ac.wellcome.platform.transformer.sierra.transformers
 
+import grizzled.slf4j.Logging
 import uk.ac.wellcome.models.work.internal._
 import uk.ac.wellcome.platform.transformer.sierra.exceptions.SierraTransformerException
 import uk.ac.wellcome.platform.transformer.sierra.source.SierraItemData
 import uk.ac.wellcome.platform.transformer.sierra.source.sierra.SierraSourceLocation
 
-trait SierraLocation {
+trait SierraLocation extends Logging {
   def getPhysicalLocation(itemData: SierraItemData): Option[PhysicalLocation] =
     itemData.location match {
       // We've seen records where the "location" field is populated in
@@ -15,14 +16,30 @@ trait SierraLocation {
       case Some(SierraSourceLocation("", ""))         => None
       case Some(SierraSourceLocation("none", "none")) => None
 
-      case Some(loc: SierraSourceLocation) =>
-        Some(
-          PhysicalLocation(
-            locationType = LocationType(loc.code),
-            label = loc.name
-          )
+      case Some(loc: SierraSourceLocation) => Some(
+        PhysicalLocation(
+          locationType = getLocationType(loc),
+          label = loc.name
         )
+      )
       case None => None
+    }
+
+  private def getLocationType(loc: SierraSourceLocation): LocationType =
+    try {
+      LocationType(loc.code)
+    } catch {
+      // Sometimes the cataloguers add new location types, which causes the pipeline
+      // to break if we don't add them to location-types.csv.
+      //
+      // Rather than playing whack-a-mole, just drop a warning and guess the label we
+      // would have put in the spreadsheet anyway.
+      case _: IllegalArgumentException =>
+        warn(s"No lookup provided for LocationType ${loc.code}, so using Sierra data")
+        LocationType(
+          id = loc.code,
+          label = loc.name
+        )
     }
 
   def getDigitalLocation(identifier: String): DigitalLocation = {
