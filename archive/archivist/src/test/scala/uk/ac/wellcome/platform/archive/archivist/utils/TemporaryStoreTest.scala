d87a2b2365f1856de00b9fe4d7bdf2edd2a0109f
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-12-17 15:34:02 +0000

    add test

---
+++ archive/archivist/src/test/scala/uk/ac/wellcome/platform/archive/archivist/utils/TemporaryStoreTest.scala
@@ -0,0 +1,49 @@
+package uk.ac.wellcome.platform.archive.archivist.utils
+
+import org.scalatest.FunSpec
+import uk.ac.wellcome.platform.archive.common.fixtures.RandomThings
+import uk.ac.wellcome.storage.ObjectLocation
+import uk.ac.wellcome.storage.fixtures.S3
+
+import scala.io.Source
+import scala.util.{Failure, Success}
+
+class TemporaryStoreTest
+  extends FunSpec
+    with S3
+    with RandomThings {
+
+  import TemporaryStore._
+
+  implicit val implicitS3Client = s3Client
+
+  describe("stores a file from an ObjectLocation") {
+    describe("with a valid ObjectLocation") {
+      it("stores that file and returns a Success[File]") {
+        withLocalS3Bucket { bucket =>
+          val key = randomAlphanumeric()
+          val content = randomAlphanumeric()
+
+          s3Client.putObject(bucket.name, key, content)
+
+          val testTempFile = ObjectLocation(bucket.name, key).storeTemporarily
+
+          testTempFile shouldBe a[Success[_]]
+
+          val fileContents = Source.fromFile(testTempFile.get).getLines.mkString
+          fileContents shouldBe (content)
+        }
+      }
+    }
+
+    describe("with an invalid ObjectLocation") {
+      it("returns a Failure[_]") {
+        val key = randomAlphanumeric()
+
+        val testTempFile = ObjectLocation("invalid_bucket", key).storeTemporarily
+
+        testTempFile shouldBe a[Failure[_]]
+      }
+    }
+  }
+}
