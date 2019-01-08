22a9ddbb10c376abc188851b23234645da1a8977
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-01-08 16:11:53 +0000

    use proper version idents for tf modules

--- infrastructure/critical/terraform/domains.tf
+++ infrastructure/critical/terraform/domains.tf
@@ -1,12 +1,12 @@
 module "domain_prod" {
-  source = "git::https://github.com/wellcometrust/terraform.git//api_gateway/modules/domain?ref=290a16393da3018435edcb26ba144582b6fb1b2b"
+  source = "git::https://github.com/wellcometrust/terraform.git//api_gateway/modules/domain?ref=v18.2.3"
 
   domain_name      = "api.wellcomecollection.org"
   cert_domain_name = "api.wellcomecollection.org"
 }
 
 module "domain_stage" {
-  source = "git::https://github.com/wellcometrust/terraform.git//api_gateway/modules/domain?ref=290a16393da3018435edcb26ba144582b6fb1b2b"
+  source = "git::https://github.com/wellcometrust/terraform.git//api_gateway/modules/domain?ref=v18.2.3"
 
   domain_name      = "api-stage.wellcomecollection.org"
   cert_domain_name = "api.wellcomecollection.org"
