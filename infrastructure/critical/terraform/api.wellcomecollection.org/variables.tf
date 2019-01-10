a6dc415dd7d4db2b0abe60f7e023de7334e79033
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-01-10 12:19:19 +0000

    Add api-stage cloudfront

---
+++ infrastructure/critical/terraform/api.wellcomecollection.org/variables.tf
@@ -0,0 +1,11 @@
+variable "subdomain" {
+  description = "A subdomain implementing the wellcomecollection.org APIs (probably api./api-stage.)"
+}
+
+variable "cert_domain" {
+  description = "The primary certificate domain for these APIs (probably api.wellcomecollection.org"
+}
+
+variable "public_api_bucket_domain_name" {}
+
+variable "description" {}
---
+++ infrastructure/critical/terraform/main.tf
@@ -0,0 +1,56 @@
+module "api-stage" {
+  source = "api.wellcomecollection.org"
+
+  subdomain   = "api-stage"
+  cert_domain = "api"
+
+  public_api_bucket_domain_name = "${aws_s3_bucket.public_api.bucket_domain_name}"
+
+  description = "Collection APIs staging"
+}
+
+module "api" {
+  source = "api.wellcomecollection.org"
+
+  subdomain   = "api"
+  cert_domain = "api"
+
+  public_api_bucket_domain_name = "${aws_s3_bucket.public_api.bucket_domain_name}"
+
+  description = "Collection APIs production"
+}
+
+// S3 origin for redirect to developers.wellcomecollection.org
+resource "aws_s3_bucket" "public_api" {
+  bucket = "wellcomecollection-public-api"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+
+  policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadForGetBucketObjects",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::wellcomecollection-public-api/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket       = "${aws_s3_bucket.public_api.bucket}"
+  key          = "index.html"
+  source       = "${path.module}/s3_objects/index.html"
+  etag         = "${md5(file("${path.module}/s3_objects/index.html"))}"
+  content_type = "text/html"
+}
