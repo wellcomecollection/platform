38cf2431155db367c97febfbcde5f8ac24059bbb
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-01-10 11:23:30 +0000

    Apply auto-formatting rules

--- catalogue_api/terraform/catalogue_api/services/remus.tf
+++ catalogue_api/terraform/catalogue_api/services/remus.tf
@@ -6,9 +6,9 @@ module "remus" {
   container_image = "${var.remus_container_image}"
   container_port  = "${var.container_port}"
 
-  namespace_id  = "${var.namespace_id}"
-  cluster_name  = "${var.cluster_name}"
-  vpc_id        = "${var.vpc_id}"
+  namespace_id = "${var.namespace_id}"
+  cluster_name = "${var.cluster_name}"
+  vpc_id       = "${var.vpc_id}"
 
   security_group_ids = [
     "${aws_security_group.service_lb_ingress_security_group.id}",
