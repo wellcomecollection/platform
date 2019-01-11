9f97f8748427ed931d1700939b8993bf71c4e3c3
Alex Chan <alex@alexwlchan.net>
2019-01-11 10:20:30 +0000

    Create a new egress security group with VPC endpoints

--- catalogue_pipeline/terraform/security_groups.tf
+++ catalogue_pipeline/terraform/security_groups.tf
@@ -7,3 +7,12 @@ module "service_egress_security_group" {
   vpc_id     = "${local.vpc_id}"
   subnet_ids = "${local.private_subnets}"
 }
+
+module "egress_security_group" {
+  source = "github.com/wellcometrust/terraform//network/prebuilt/vpc/egress_security_group?ref=c71887b"
+
+  name        = "catalogue_pipeline_services"
+
+  vpc_id     = "${local.vpc_id}"
+  subnet_ids = "${local.private_subnets}"
+}
