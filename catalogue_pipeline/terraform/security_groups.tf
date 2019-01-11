dba5a42e2b03b4986c2140af7556a417125f89f5
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-01-11 10:51:44 +0000

    Apply auto-formatting rules

--- catalogue_pipeline/terraform/security_groups.tf
+++ catalogue_pipeline/terraform/security_groups.tf
@@ -1,7 +1,7 @@
 module "egress_security_group" {
   source = "github.com/wellcometrust/terraform//network/prebuilt/vpc/egress_security_group?ref=c71887b"
 
-  name        = "catalogue_pipeline_services"
+  name = "catalogue_pipeline_services"
 
   vpc_id     = "${local.vpc_id}"
   subnet_ids = "${local.private_subnets}"
