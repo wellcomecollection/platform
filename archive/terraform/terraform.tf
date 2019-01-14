a794a6ce959baea5f70df9dd31979ad94e95fd25
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-01-14 09:56:04 +0000

    replace simplified terraform

---
+++ archive/terraform/terraform.tf
@@ -0,0 +1,41 @@
+# Terraform
+
+terraform {
+  required_version = ">= 0.9"
+
+  backend "s3" {
+    bucket         = "wellcomecollection-platform-infra"
+    key            = "terraform/archive.tfstate"
+    dynamodb_table = "terraform-locktable"
+    region         = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+  version = "1.42.0"
+
+  profile = "platform-admin"
+}
+
+# Data
+
+data "terraform_remote_state" "shared_infra" {
+  backend = "s3"
+
+  config {
+    bucket = "wellcomecollection-platform-infra"
+    key    = "terraform/shared_infra.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+data "terraform_remote_state" "infra_critical" {
+  backend = "s3"
+
+  config {
+    bucket = "wellcomecollection-platform-infra"
+    key    = "terraform/catalogue_pipeline_data.tfstate"
+    region = "eu-west-1"
+  }
+}
