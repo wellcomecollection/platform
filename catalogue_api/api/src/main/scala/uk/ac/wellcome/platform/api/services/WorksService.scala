6df924d6873f9f6634825abb5131ecd0d4d63fa2
Alex Chan <alex@alexwlchan.net>
2019-01-15 21:44:36 +0000

    Don’t throw a 500 error on high page numbers (#3266)
    * Add a (failing) test case for the high page numbers
    
    * Add a workaround for integer overflow in the API
    
    * Bump our version of elastic4s
    
    If you pass a negative "from" parameter in your Elasticsearch query,
    the error that comes from ES 6.5 can't be parsed by this version of
    elastic4s.
    
    In practice we should never do that!  But it can happen if a weird
    integer overflow sneaks through -- see the previous commit.
    
    * Improve this test label with a more accurate description
    
    * Add an assertion to spot other cases of integer overflow

--- catalogue_api/api/src/main/scala/uk/ac/wellcome/platform/api/services/WorksService.scala
+++ catalogue_api/api/src/main/scala/uk/ac/wellcome/platform/api/services/WorksService.scala
@@ -55,12 +55,63 @@ class WorksService @Inject()(searchService: ElasticsearchService)(
       }
 
   private def toElasticsearchQueryOptions(
-    worksSearchOptions: WorksSearchOptions): ElasticsearchQueryOptions =
+    worksSearchOptions: WorksSearchOptions): ElasticsearchQueryOptions = {
+
+    // Because we use Int for the pageSize and pageNumber, computing
+    //
+    //     from = (pageNumber - 1) * pageSize
+    //
+    // can potentially overflow and be negative or even wrap around.
+    // For example, pageNumber=2018634700 and pageSize=100 would return
+    // results if you don't handle this!
+    //
+    // If we are about to overflow, we pass the max possible int
+    // into the Elasticsearch query and let it bubble up from there.
+    // We could skip the query and throw here, because the user is almost
+    // certainly doing something wrong, but that would mean simulating an
+    // ES error or modifying our exception handling, and that seems more
+    // disruptive than just clamping the overflow.
+    //
+    // Note: the checks on "pageSize" mean we know it must be
+    // at most 100.
+
+    // How this check works:
+    //
+    //    1.  If pageNumber > MaxValue, then (pageNumber - 1) * pageSize is
+    //        probably bigger, as pageSize >= 1.
+    //
+    //    2.  Alternatively, we care about whether
+    //
+    //            pageSize * pageNumber > MaxValue
+    //
+    //        Since pageNumber is known positive, this is equivalent to
+    //
+    //            pageSize > MaxValue / pageNumber
+    //
+    //        And we know the division won't overflow because we have
+    //        pageValue < MaxValue by the first check.
+    //
+    val willOverflow =
+      (worksSearchOptions.pageNumber > Int.MaxValue) ||
+      (worksSearchOptions.pageSize > Int.MaxValue / worksSearchOptions.pageNumber)
+
+    val from = if (willOverflow) {
+      Int.MaxValue
+    } else {
+      (worksSearchOptions.pageNumber - 1) * worksSearchOptions.pageSize
+    }
+
+    assert(
+      from >= 0,
+      message = s"from = $from < 0, which is daft.  Has something overflowed?"
+    )
+
     ElasticsearchQueryOptions(
       filters = worksSearchOptions.filters,
       limit = worksSearchOptions.pageSize,
-      from = (worksSearchOptions.pageNumber - 1) * worksSearchOptions.pageSize
+      from = from
     )
+  }
 
   private def createResultList(searchResponse: SearchResponse): ResultList =
     ResultList(
