4607223a113fa97b6d3235df28112fb30eb8eb9d
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-01-17 16:54:44 +0000

    re-add old repos to ensure safe path to merge

--- storage/terraform/ecr.tf
+++ storage/terraform/ecr.tf
@@ -43,7 +43,29 @@ module "ecr_repository_callback_stub_server" {
   name   = "callback_stub_server"
 }
 
+# TODO: Delete this repos when publishing to new ones
+
 module "ecr_repository_archive_api" {
   source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
   name   = "archive_api"
+}
+
+module "ecr_repository_registrar_async" {
+  source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
+  name   = "registrar_async"
+}
+
+module "ecr_repository_registrar_http" {
+  source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
+  name   = "registrar_http"
+}
+
+module "ecr_repository_progress_async" {
+  source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
+  name   = "progress_async"
+}
+
+module "ecr_repository_progress_http" {
+  source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
+  name   = "progress_http"
 }
