1b9e268292044a2ce6cd1f06f6751e7902c35db2
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-01-22 17:17:59 +0000

    correct account

--- catalogue_api/terraform/terraform.tf
+++ catalogue_api/terraform/terraform.tf
@@ -3,7 +3,7 @@ provider "aws" {
   version = "1.33.0"
 
   assume_role {
-    role_arn = "arn:aws:iam::975596993436:role/developer"
+    role_arn = "arn:aws:iam::760097843905:role/developer"
   }
 }
 
