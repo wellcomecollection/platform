fe4701c304e8c8cd44d27263a62d09fe1355f87e
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-01-22 16:45:36 +0000

    use role arns everywhere

--- catalogue_api/terraform/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region  = "${var.aws_region}"
-  version = "1.33.0"
-}
-
-data "aws_caller_identity" "current" {}
--- catalogue_api/terraform/terraform.tf
+++ catalogue_api/terraform/terraform.tf
@@ -1,7 +1,20 @@
+provider "aws" {
+  region  = "${var.aws_region}"
+  version = "1.33.0"
+
+  assume_role {
+    role_arn = "arn:aws:iam::975596993436:role/developer"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
 terraform {
   required_version = ">= 0.9"
 
   backend "s3" {
+    role_arn = "arn:aws:iam::760097843905:role/developer"
+
     bucket         = "wellcomecollection-platform-infra"
     key            = "terraform/catalogue_api.tfstate"
     dynamodb_table = "terraform-locktable"
@@ -13,6 +26,8 @@ data "terraform_remote_state" "shared_infra" {
   backend = "s3"
 
   config {
+    role_arn = "arn:aws:iam::760097843905:role/developer"
+
     bucket = "wellcomecollection-platform-infra"
     key    = "terraform/shared_infra.tfstate"
     region = "eu-west-1"
@@ -23,6 +38,8 @@ data "terraform_remote_state" "infra_critical" {
   backend = "s3"
 
   config {
+    role_arn = "arn:aws:iam::760097843905:role/developer"
+
     bucket = "wellcomecollection-platform-infra"
     key    = "terraform/catalogue_pipeline_data.tfstate"
     region = "eu-west-1"
