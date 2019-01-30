47316165279896d1e8ce543e180c3d0007c297b4
Alex Chan <a.chan@wellcome.ac.uk>
2019-01-30 17:15:32 +0000

    Use a pinned version of the tf-modules

--- loris/terraform/stack/cluster_host.tf
+++ loris/terraform/stack/cluster_host.tf
@@ -1,5 +1,5 @@
 module "cluster_host" {
-  source = "git::https://github.com/wellcometrust/terraform.git//ecs/modules/ec2/prebuilt/ebs?ref=4a81e34"
+  source = "git::https://github.com/wellcometrust/terraform.git//ecs/modules/ec2/prebuilt/ebs?ref=v19.7.0"
 
   cluster_name = "${aws_ecs_cluster.cluster.name}"
   vpc_id       = "${var.vpc_id}"
