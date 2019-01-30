2ffa450f7fef0c2f618dd87d253973404b518507
Alex Chan <a.chan@wellcome.ac.uk>
2019-01-30 14:30:46 +0000

    This local variable is never used

--- loris/terraform/locals.tf
+++ loris/terraform/locals.tf
@@ -3,8 +3,6 @@ locals {
   public_subnets  = "${data.terraform_remote_state.shared_infra.catalogue_vpc_delta_public_subnets}"
   private_subnets = "${data.terraform_remote_state.shared_infra.catalogue_vpc_delta_private_subnets}"
 
-  bucket_alb_logs_id = "${data.terraform_remote_state.shared_infra.bucket_alb_logs_id}"
-
   cloudfront_logs_bucket_domain_name = "${data.terraform_remote_state.shared_infra.cloudfront_logs_bucket_domain_name}"
 
   loris_release_uri       = "${data.aws_ssm_parameter.loris_release_uri.value}"
