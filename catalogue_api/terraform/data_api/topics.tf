28e5b673e86d7f67858ba0d0260f91dea6502f7a
Robert Kenny <kenoir@users.noreply.github.com>
2019-01-31 12:13:26 +0000

    Update snapshot generator to use secrets (#3387)
    * update snapshot generator to use creds
    
    * use correct credentials
    
    * fix policies
    
    * keepalive
    
    * use proper ref
    
    * Apply auto-formatting rules

--- catalogue_api/terraform/data_api/topics.tf
+++ catalogue_api/terraform/data_api/topics.tf
@@ -1,14 +1,1 @@
-module "snapshot_generator_jobs_topic" {
-  source = "git::https://github.com/wellcometrust/terraform-modules.git//sns?ref=v1.0.0"
-  name   = "snapshot_generator_jobs"
-}
 
-module "snapshot_generation_complete_topic" {
-  source = "git::https://github.com/wellcometrust/terraform-modules.git//sns?ref=v1.0.0"
-  name   = "snapshot_generation_complete"
-}
-
-module "snapshot_alarm_topic" {
-  source = "git::https://github.com/wellcometrust/terraform-modules.git//sns?ref=v1.0.0"
-  name   = "snapshot_alarm"
-}
