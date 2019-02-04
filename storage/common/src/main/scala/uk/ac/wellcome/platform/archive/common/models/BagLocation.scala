0d83f0012af31e1858bac6663ecf795dba4b8178
Alex Chan <a.chan@wellcome.ac.uk>
2019-02-04 15:44:10 +0000

    Add the new FuzzyWuzzy model

--- storage/common/src/main/scala/uk/ac/wellcome/platform/archive/common/models/BagLocation.scala
+++ storage/common/src/main/scala/uk/ac/wellcome/platform/archive/common/models/BagLocation.scala
@@ -1,5 +1,20 @@
 package uk.ac.wellcome.platform.archive.common.models
 
+/** Represents the complete location of a Bag in S3.
+  *
+  * @param storageNamespace The name of the S3 bucket
+  * @param storagePrefix The global prefix for all objects created by the archivist
+  *                      (e.g. "archive")
+  * @param storageSpace The namespace from the ingest request (e.g. "digitised")
+  * @param bagPath The relative path to the bag (e.g. "b12345.zip")
+  */
+case class FuzzyWuzzy(
+  storageNamespace: String,
+  storagePrefix: String,
+  storageSpace: String,
+  bagPath: String
+)
+
 case class BagLocation(storageNamespace: String,
                        storageRootPath: String,
                        bagPath: BagPath) {
