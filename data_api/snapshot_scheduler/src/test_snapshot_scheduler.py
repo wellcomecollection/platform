31afaa7aee64e20058c9b425cadc6e026706c898
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-02-04 10:59:57 +0000

    remove v1 from snapshot

--- data_api/snapshot_scheduler/src/test_snapshot_scheduler.py
+++ data_api/snapshot_scheduler/src/test_snapshot_scheduler.py
@@ -18,13 +18,11 @@ class patched_datetime(dt.datetime):
 @mock.patch("datetime.datetime", patched_datetime)
 def test_writes_message_to_sqs(sns_client, topic_arn):
     public_bucket_name = "public-bukkit"
-    public_object_key_v1 = "v1/works.json.gz"
     public_object_key_v2 = "v2/works.json.gz"
 
     patched_os_environ = {
         "TOPIC_ARN": topic_arn,
         "PUBLIC_BUCKET_NAME": public_bucket_name,
-        "PUBLIC_OBJECT_KEY_V1": public_object_key_v1,
         "PUBLIC_OBJECT_KEY_V2": public_object_key_v2,
     }
 
@@ -32,13 +30,8 @@ def test_writes_message_to_sqs(sns_client, topic_arn):
         snapshot_scheduler.main(sns_client=sns_client)
 
     messages = sns_client.list_messages()
-    assert len(messages) == 2
+    assert len(messages) == 1
     assert [m[":message"] for m in messages] == [
-        {
-            "publicBucketName": public_bucket_name,
-            "publicObjectKey": public_object_key_v1,
-            "apiVersion": "v1",
-        },
         {
             "publicBucketName": public_bucket_name,
             "publicObjectKey": public_object_key_v2,
