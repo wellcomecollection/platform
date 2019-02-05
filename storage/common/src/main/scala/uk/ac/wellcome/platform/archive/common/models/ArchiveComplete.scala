d06df54c8e1b1cc9fa71e530e0fc07d6a4c04b97
mklander <mklanderyou@gmail.com>
2019-02-05 14:41:29 +0000

    remove ArchiveComplete model

--- storage/common/src/main/scala/uk/ac/wellcome/platform/archive/common/models/ArchiveComplete.scala
+++ storage/common/src/main/scala/uk/ac/wellcome/platform/archive/common/models/ArchiveComplete.scala
@@ -4,11 +4,6 @@ import java.util.UUID
 
 import uk.ac.wellcome.platform.archive.common.models.bagit.BagLocation
 
-case class ArchiveComplete(
-  archiveRequestId: UUID,
-  bagLocation: BagLocation
-)
-
 // TODO: Agree upon these names
 case class ReplicationRequest(
   archiveRequestId: UUID,
