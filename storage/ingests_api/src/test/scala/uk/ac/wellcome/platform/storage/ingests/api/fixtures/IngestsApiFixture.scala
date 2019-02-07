273ec2a00b6268d829c551b4939342d9244ed177
Alex Chan <alex@alexwlchan.net>
2019-02-07 11:50:01 +0000

    Reduce the repetition in the ingests API feature test

--- storage/ingests_api/src/test/scala/uk/ac/wellcome/platform/storage/ingests/api/fixtures/IngestsApiFixture.scala
+++ storage/ingests_api/src/test/scala/uk/ac/wellcome/platform/storage/ingests/api/fixtures/IngestsApiFixture.scala
@@ -2,23 +2,23 @@ package uk.ac.wellcome.platform.storage.ingests.api.fixtures
 
 import java.net.URL
 
-import akka.stream.QueueOfferResult
+import akka.http.scaladsl.model.{ContentTypes, HttpResponse, StatusCode, StatusCodes}
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.{ActorMaterializer, QueueOfferResult}
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import org.mockito.Mockito.{atLeastOnce, verify}
+import org.scalatest.Assertion
 import org.scalatest.concurrent.ScalaFutures
+import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.messaging.fixtures.SNS.Topic
 import uk.ac.wellcome.messaging.fixtures.{Messaging, SNS}
 import uk.ac.wellcome.monitoring.MetricsSender
 import uk.ac.wellcome.platform.archive.common.config.models.HTTPServerConfig
-import uk.ac.wellcome.platform.archive.common.fixtures.{
-  HttpFixtures,
-  RandomThings
-}
-import uk.ac.wellcome.platform.archive.common.progress.fixtures.{
-  ProgressGenerators,
-  ProgressTrackerFixture
-}
+import uk.ac.wellcome.platform.archive.common.fixtures.{HttpFixtures, RandomThings}
+import uk.ac.wellcome.platform.archive.common.progress.fixtures.{ProgressGenerators, ProgressTrackerFixture}
 import uk.ac.wellcome.platform.storage.ingests.api.IngestsApi
 import uk.ac.wellcome.platform.storage.ingests.api.http.{HttpMetricResults, HttpMetrics}
+import uk.ac.wellcome.platform.storage.ingests.api.model.ErrorResponse
 import uk.ac.wellcome.storage.fixtures.LocalDynamoDb.Table
 import uk.ac.wellcome.storage.fixtures.{LocalDynamoDb, S3}
 import uk.ac.wellcome.fixtures.TestWith
@@ -86,4 +86,25 @@ trait IngestsApiFixture
   def assertMetricSent(metricsSender: MetricsSender, result: HttpMetricResults.Value): Future[QueueOfferResult] =
     verify(metricsSender, atLeastOnce())
       .incrementCount(metricName = s"IngestsApi_HttpResponse_$result")
+
+  def assertResponseIsUserError(
+    response: HttpResponse,
+    description: String,
+    statusCode: StatusCode = StatusCodes.BadRequest,
+    label: String = "Bad Request")(
+    implicit materializer: ActorMaterializer): Assertion = {
+    response.status shouldBe statusCode
+    response.entity.contentType shouldBe ContentTypes.`application/json`
+
+    val progressFuture = Unmarshal(response.entity).to[ErrorResponse]
+
+    whenReady(progressFuture) { actualError =>
+      actualError shouldBe ErrorResponse(
+        context = "http://api.wellcomecollection.org/storage/v1/context.json",
+        httpStatus = statusCode.intValue(),
+        description = description,
+        label = label
+      )
+    }
+  }
 }
