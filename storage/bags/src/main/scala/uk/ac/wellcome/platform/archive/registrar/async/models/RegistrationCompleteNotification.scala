4166228d85b54cef4fa295a2f28f58a49af9d2cb
Alex Chan <a.chan@wellcome.ac.uk>
2019-02-08 13:51:26 +0000

    Remove an unused model

--- storage/bags/src/main/scala/uk/ac/wellcome/platform/archive/registrar/async/models/RegistrationCompleteNotification.scala
+++
@@ -1,7 +0,0 @@
-package uk.ac.wellcome.platform.archive.registrar.async.models
-import java.util.UUID
-
-import uk.ac.wellcome.platform.archive.common.models.bagit.BagId
-
-case class RegistrationCompleteNotification(archiveRequestId: UUID,
-                                            bagId: BagId)
