62ec55e8b847ca7b441ef861731bde6114e3fac2
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-02-08 11:21:23 +0000

    fmt

--- catalogue_api/terraform/release_uris.tf
+++ catalogue_api/terraform/release_uris.tf
@@ -1,6 +1,6 @@
 locals {
-  api_image = "${module.images.services["api"]}"
-  nginx_image     = "${module.images.services["nginx_api-gw"]}"
+  api_image   = "${module.images.services["api"]}"
+  nginx_image = "${module.images.services["nginx_api-gw"]}"
 
   snapshot_generator_image = "${module.images.services["snapshot_generator"]}"
   update_api_docs_image    = "${module.images.services["update_api_docs"]}"
@@ -9,13 +9,13 @@ locals {
 module "images" {
   source = "git::https://github.com/wellcometrust/terraform.git//ecs/modules/images?ref=v19.8.0"
 
-  project  = "catalogue_api"
-  label    = "latest"
+  project = "catalogue_api"
+  label   = "latest"
 
   services = [
     "api",
     "snapshot_generator",
     "update_api_docs",
-    "nginx_api-gw"
+    "nginx_api-gw",
   ]
 }
