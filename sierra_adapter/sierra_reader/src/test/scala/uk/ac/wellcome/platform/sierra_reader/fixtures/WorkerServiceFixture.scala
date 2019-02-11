b38b6409528b733d6700554f6a1a769494cf37b9
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-02-11 12:50:40 +0000

    Apply auto-formatting rules

--- sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/fixtures/WorkerServiceFixture.scala
+++ sierra_adapter/sierra_reader/src/test/scala/uk/ac/wellcome/platform/sierra_reader/fixtures/WorkerServiceFixture.scala
@@ -4,7 +4,10 @@ import uk.ac.wellcome.akka.fixtures.Akka
 import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.fixtures.SQS
 import uk.ac.wellcome.messaging.fixtures.SQS.Queue
-import uk.ac.wellcome.platform.sierra_reader.config.models.{ReaderConfig, SierraAPIConfig}
+import uk.ac.wellcome.platform.sierra_reader.config.models.{
+  ReaderConfig,
+  SierraAPIConfig
+}
 import uk.ac.wellcome.platform.sierra_reader.models.SierraResourceTypes
 import uk.ac.wellcome.platform.sierra_reader.services.SierraReaderWorkerService
 import uk.ac.wellcome.storage.fixtures.S3
