55104b0538b1e7bb926f643fe4e3c3d60af68a37
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-02-12 12:31:51 +0000

    increase ssm perm scope for travis

--- builds/terraform/platform/iam_policy_document.tf
+++ builds/terraform/platform/iam_policy_document.tf
@@ -65,7 +65,7 @@ data "aws_iam_policy_document" "travis_permissions" {
     ]
 
     resources = [
-      "arn:aws:ssm:eu-west-1:${local.account_id}:parameter/releases/*",
+      "arn:aws:ssm:eu-west-1:${local.account_id}:parameter/*",
     ]
   }
 }
