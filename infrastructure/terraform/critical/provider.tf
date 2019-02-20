bbb7c9b2742ff3a4aa34f3f3abf8bd0eb1312fd0
Alex Chan <a.chan@wellcome.ac.uk>
2019-02-20 16:26:02 +0000

    Fix running the infra_critical terraform with AssumeRole

--- infrastructure/terraform/critical/provider.tf
+++ infrastructure/terraform/critical/provider.tf
@@ -1,6 +1,10 @@
 provider "aws" {
   region  = "${var.aws_region}"
   version = "1.46.0"
+
+  assume_role {
+    role_arn = "arn:aws:iam::760097843905:role/admin"
+  }
 }
 
 provider "aws" {
@@ -9,6 +13,10 @@ provider "aws" {
   region = "us-east-1"
 
   version = "1.46.0"
+
+  assume_role {
+    role_arn = "arn:aws:iam::760097843905:role/admin"
+  }
 }
 
 data "aws_caller_identity" "current" {}
--- infrastructure/terraform/critical/terraform.tf
+++ infrastructure/terraform/critical/terraform.tf
@@ -2,6 +2,8 @@ terraform {
   required_version = ">= 0.9"
 
   backend "s3" {
+    role_arn = "arn:aws:iam::760097843905:role/developer"
+
     bucket         = "wellcomecollection-platform-infra"
     key            = "terraform/catalogue_pipeline_data.tfstate"
     dynamodb_table = "terraform-locktable"
@@ -13,6 +15,8 @@ data "terraform_remote_state" "shared_infra" {
   backend = "s3"
 
   config {
+    role_arn = "arn:aws:iam::760097843905:role/developer"
+
     bucket = "wellcomecollection-platform-infra"
     key    = "terraform/shared_infra.tfstate"
     region = "eu-west-1"
