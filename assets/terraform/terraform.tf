e64b7cf1a9b3b6fe91330c3a0104e7c32fc49376
Alex Chan <a.chan@wellcome.ac.uk>
2019-03-29 13:41:18 +0000

    Get the assets stack to plan successfully with AssumeRole

--- assets/terraform/terraform.tf
+++ assets/terraform/terraform.tf
@@ -2,6 +2,8 @@ terraform {
   required_version = ">= 0.11"
 
   backend "s3" {
+    role_arn = "arn:aws:iam::760097843905:role/developer"
+
     bucket         = "wellcomecollection-platform-infra"
     key            = "terraform/assets.tfstate"
     dynamodb_table = "terraform-locktable"
@@ -10,7 +12,10 @@ terraform {
 }
 
 provider "aws" {
-  profile = "platform-admin"
   region  = "${var.aws_region}"
-  version = "1.7.0"
+  version = "1.10.0"
+
+  assume_role {
+    role_arn = "arn:aws:iam::760097843905:role/admin"
+  }
 }
