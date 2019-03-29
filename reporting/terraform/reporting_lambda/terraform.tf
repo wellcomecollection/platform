3eecad0836f30b2ea341d85954fa34610d30c1b7
Alex Chan <a.chan@wellcome.ac.uk>
2019-03-29 16:40:47 +0000

    WIP get the reporting pipeline working

--- reporting/terraform/reporting_lambda/terraform.tf
+++ reporting/terraform/reporting_lambda/terraform.tf
@@ -2,6 +2,8 @@ data "terraform_remote_state" "shared_infra" {
   backend = "s3"
 
   config {
+    role_arn = "arn:aws:iam::760097843905:role/developer"
+
     bucket = "wellcomecollection-platform-infra"
     key    = "terraform/shared_infra.tfstate"
     region = "eu-west-1"
