1361a9d069bbbd8efcecf928b79d80aa9c5b245a
Alex Chan <a.chan@wellcome.ac.uk>
2019-04-23 10:35:36 +0100

    Sort out the AWS credentials in Travis

--- run_travis.sh
+++ run_travis.sh
@@ -5,6 +5,11 @@ set -o nounset
 
 openssl aes-256-cbc -K $encrypted_83630750896a_key -iv $encrypted_83630750896a_iv -in secrets.zip.enc -out secrets.zip -d
 unzip secrets.zip
+
+mkdir -p ~/.aws
+mv config ~/.aws/config
+mv credentials ~/.aws/credentials
+
 chmod 600 id_rsa
 
 make "$TASK"
