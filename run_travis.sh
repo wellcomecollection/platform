ba71fb22db478aa80585448fb2f421d46bc91b74
Alex Chan <a.chan@wellcome.ac.uk>
2019-04-23 10:24:30 +0100

    Another go at fixing the platform secrets

--- run_travis.sh
+++ run_travis.sh
@@ -3,4 +3,8 @@
 set -o errexit
 set -o nounset
 
+openssl aes-256-cbc -K $encrypted_83630750896a_key -iv $encrypted_83630750896a_iv -in secrets.zip.enc -out secrets.zip -d
+unzip secrets.zip
+chmod 600 id_rsa
+
 make "$TASK"
