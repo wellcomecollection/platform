32545888ab68321ea7e32733fdde5a8c9c2df50f
harrisonpim <harrison.stroma@gmail.com>
2019-05-22 12:24:37 +0100

    move reporting to github.com/wellcometrust/reporting

--- reporting/reporting_miro_inventory_transformer/src/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from elasticsearch import Elasticsearch
-
-
-@pytest.fixture
-def elasticsearch_client(elasticsearch_url):
-    yield Elasticsearch(elasticsearch_url, http_auth=("elastic", "changeme"))
