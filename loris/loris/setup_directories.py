5dca7f49b80ecaf0706a17d6d69414d44cca4d6a
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-09-18 05:28:54 +0000

    Apply auto-formatting rules

--- loris/loris/setup_directories.py
+++ loris/loris/setup_directories.py
@@ -6,16 +6,10 @@ import os
 import pathlib
 import sys
 
-sys.path.insert(
-    0, str(pathlib.Path(__file__).parent.parent / "loris")
-)
+sys.path.insert(0, str(pathlib.Path(__file__).parent.parent / "loris"))
 from user_commands import create_default_files_and_directories
 
 
 if __name__ == "__main__":
-    config = ConfigObj(
-        os.environ["LORIS_CONF_FILE"],
-        unrepr=True,
-        interpolation=False
-    )
+    config = ConfigObj(os.environ["LORIS_CONF_FILE"], unrepr=True, interpolation=False)
     create_default_files_and_directories(config)
