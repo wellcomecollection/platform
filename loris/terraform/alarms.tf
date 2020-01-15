bfa17ec225c34c828a28dd0f9dc9f883178a9048
jamie <j.parkinson@wellcome.ac.uk>
2020-01-15 12:03:42 +0000

    Link to re-enabling issue

--- loris/terraform/alarms.tf
+++ loris/terraform/alarms.tf
@@ -28,6 +28,7 @@ resource "aws_cloudwatch_metric_alarm" "cloudfront_4xx_alarm" {
 
   // This alarm action is disabled for now (2020-01-15) as it is being noisy
   // and drowning out our other alarms.
+  // It should be re-enabled eventually: https://github.com/wellcometrust/platform/issues/4170
   actions_enabled = false
 
   provider = "aws.us_east_1"
