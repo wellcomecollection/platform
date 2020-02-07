749523865fddc38962c4f315feef258d8cc8a189
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-02-07 12:48:16 +0000

    Remove unused reporting project

--- reporting/terraform/reporting_lambda/iam_role_policy.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_role_policy" "iam_role_policy" {
-  role   = "${module.reporting_lambda.role_name}"
-  policy = "${var.vhs_read_policy}"
-}
--- reporting/terraform/reporting_lambda/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "role_name" {
-  description = "Name of the IAM role for this Lambda"
-  value       = "${module.reporting_lambda.role_name}"
-}
