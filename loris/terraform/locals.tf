123074b8616a4086e6819823b614b967b7734009
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-02-07 14:00:35 +0000

    removes loris

--- loris/terraform/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  vpc_id          = "${data.terraform_remote_state.shared_infra.catalogue_vpc_delta_id}"
-  public_subnets  = "${data.terraform_remote_state.shared_infra.catalogue_vpc_delta_public_subnets}"
-  private_subnets = "${data.terraform_remote_state.shared_infra.catalogue_vpc_delta_private_subnets}"
-
-  cloudfront_logs_bucket_domain_name = "${data.terraform_remote_state.shared_infra.cloudfront_logs_bucket_domain_name}"
-}
--- loris/terraform/stack/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  listener_port = "443"
-}
