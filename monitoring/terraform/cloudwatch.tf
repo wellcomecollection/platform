0832c264b912a8fff49b8fb1e67525b42631c0ae
Alex Chan <a.chan@wellcome.ac.uk>
2020-03-11 12:11:50 +0000

    Remove a now-unused CloudWatch event rule

--- monitoring/terraform/cloudwatch.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_cloudwatch_event_rule" "every_5_minutes" {
-  name                = "every_5_minutes_lambdas"
-  description         = "Fires every 5 minutes"
-  schedule_expression = "rate(5 minutes)"
-}
