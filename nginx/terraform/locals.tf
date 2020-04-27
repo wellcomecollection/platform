65940141a0c60134d339eb56ead0ea7f95b8abf1
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-04-27 17:08:00 +0100

    fmt

--- nginx/terraform/locals.tf
+++ nginx/terraform/locals.tf
@@ -1,9 +1,9 @@
 locals {
-  catalogue_account_id   = "756629837203"
-  platform_account_id    = "760097843905"
-  storage_account_id     = "975596993436"
-  workflow_account_id    = "299497370133"
-  experience_account_id  = "130871440101"
+  catalogue_account_id  = "756629837203"
+  platform_account_id   = "760097843905"
+  storage_account_id    = "975596993436"
+  workflow_account_id   = "299497370133"
+  experience_account_id = "130871440101"
 
   namespace = "uk.ac.wellcome"
 }
